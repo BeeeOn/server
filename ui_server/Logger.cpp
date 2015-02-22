b136c62cff1096e1ec7446bafc4e9bfb44fd1c73
Pavel Vampola <xvampo01@stud.fit.vutbr.cz>
2015-02-22 13:46:45 +0100

    updated logger, changed soci include style (better poratbility - iotpro)
    logger is thread-safe, not overlaping, store in dir logs, files are sorted by date

--- ui_server/Logger.cpp
+++ ui_server/Logger.cpp
@@ -12,13 +12,60 @@ using namespace std;
 Logger::Logger() {
     _verbosityThreshold = 0;
     _verbosityLevel = 0;
-    _colored = false;
+    _colored = false; 
+    
+    _fileName =  getFileName(); 
+    
+    _currentFile.open ( _fileName );  
+/*
+std::ofstream of;
+std::ostream& out = condition ? std::cout : of.open(filename);
+*/
+
 }
-
-
 
 Logger::~Logger() {
+    _currentFile.close();
 }
+
+void Logger::changeFiles() {
+    _currentFile.close();
+    
+    _fileName =  getFileName(); 
+    
+    _currentFile.open ( _fileName );    
+}
+
+std::string Logger::getFileName() {
+    
+    #ifdef LOGS_TO_STDOUT
+       _fileName = "/dev/stdout" ;
+   #else
+       _fileName =  getFileNamebyDate(); 
+   #endif
+       return _fileName;
+}
+
+string Logger::getFileNamebyDate() {
+    
+    timeval tp;
+    time_t curtime;
+    
+    gettimeofday(&tp, 0);
+    
+    curtime = tp.tv_sec;
+    tm *t = localtime(&curtime);
+    
+    ostringstream convert; 
+    
+    _dayPrecidingChanges = t->tm_mday;
+            
+    convert<<LOGS_FILE << "/" << "log"<<t->tm_mday <<"_"<< t->tm_mon+1 <<"_"<< (t->tm_year+1900);
+    
+    return convert.str();
+}
+
+
 
 Logger& Logger::getInstance()
 {
@@ -42,47 +89,50 @@ void Logger::setVerbosityThreshold(int verbosityThreshold){
 void Logger::setVerbosityLevel(int verbosityLvl){
     _verbosityLevel = verbosityLvl;
 }
-void Logger::printTime(){
+void Logger::printTime(){   
     if(_verbosityLevel > _verbosityThreshold)
         return;
+    
+   
+    std::lock_guard<std::mutex> lck (_mtx);
     timeval tp;
     time_t curtime;
-    
+
     gettimeofday(&tp, 0);
-    
+
     curtime = tp.tv_sec;
     tm *t = localtime(&curtime);
     
-    if(_colored)
-		cout<<zkr::cc::bold;
+    if(_dayPrecidingChanges != t->tm_mday)
+        changeFiles();
     
-    cout<<">"<<_verbosityLevel<<"< " << std::this_thread::get_id() <<": ";
+    #ifdef COLORED_LOGGER
+        cout<<zkr::cc::bold;
+    #endif 
         
-    printf("%d.%d.%d %02d:%02d:%02d:%03ld ", t->tm_mday,t->tm_mon,(t->tm_year+1900),t->tm_hour, t->tm_min, t->tm_sec, tp.tv_usec/1000);
-    if(_colored)
-		cout<<zkr::cc::console;
-}
-/*
-Logger& Logger::operator<<(const string& s)
-{
-    print(s);
-    return *this;
-}
+    _currentFile<<">"<<_verbosityLevel<<"< " << std::this_thread::get_id() <<": ";
 
-Logger& Logger::operator<<(const int& i)
-{
-    stringstream ss;
-    ss << i;
-    string str = ss.str();
-    print(str);
- return *this;
- }
- -*/
+    //printf("%d.%d.%d %02d:%02d:%02d:%03ld ", t->tm_mday,(t->tm_mon+1),(t->tm_year+1900),t->tm_hour, t->tm_min, t->tm_sec, tp.tv_usec/1000);
+    _currentFile << t->tm_mday <<"." << (t->tm_mon+1) << "." << (t->tm_year+1900) <<" ";
+    _currentFile << t->tm_hour << ":" << t->tm_min << ":" << t->tm_sec << ":" << tp.tv_usec/1000<< " ";
+    
+    #ifdef COLORED_LOGGER
+            cout<<zkr::cc::console;
+    #endif
+  
+}
 
 
 Logger &Logger::operator<<(std::ostream& (*pf) (std::ostream&)){
     if(_verbosityLevel > _verbosityThreshold)
         return *this;   
-    cout<<pf;
+    
+    
+    std::lock_guard<std::mutex> lck (_mtx);
+    _currentFile<<pf;
+
+    if(_verbosityLevel == this->FATAL)
+            std::cerr << pf;
+    
     return *this;
 }
