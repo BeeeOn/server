e83042e0815d3f0c08ed204f5c8ee4d234118d12
Pavel Vampola <xvampo01@stud.fit.vutbr.cz>
2016-02-22 17:54:00 +0000

    add db script to make test DB, update some older sql scripts

--- pgmodeler/home7_alter_2.sql
+++ pgmodeler/home7_alter_2.sql
@@ -13,6 +13,19 @@ ALTER TABLE log ADD COLUMN gateway_id decimal(20,0) DEFAULT NULL;
 -- update new gateway_id columns with data                                 
 update module as mod set gateway_id = (select gateway_id from device where device_euid = mod.device_euid );     
 update log as l set gateway_id = (select gateway_id from device where device_euid = l.device_euid );                                
+        
+-- device PK change
+ALTER TABLE device DROP CONSTRAINT facilities_pk CASCADE;
+ALTER TABLE device ADD PRIMARY KEY (device_euid,gateway_id); -- device_pkey is created
+
+-- module PK change
+ALTER TABLE module DROP CONSTRAINT devices_pk CASCADE;
+ALTER TABLE module ADD PRIMARY KEY (device_euid, module_id, gateway_id); --module_pkey is created
+                                                          
+-- log PK change
+ALTER TABLE log DROP CONSTRAINT logs_pk;
+ALTER TABLE log ADD PRIMARY KEY (device_euid, module_id, gateway_id, measured_at); --log_pkey is created        
+        
            
 -- set FK on new gateway_id column         
 ALTER TABLE public.device ADD CONSTRAINT device_gateway FOREIGN KEY (gateway_id)
@@ -27,22 +40,9 @@ ALTER TABLE public.log ADD CONSTRAINT log_module_fk FOREIGN KEY (device_euid, mo
 REFERENCES public.module (device_euid, module_id, gateway_id) MATCH FULL
 ON DELETE CASCADE ON UPDATE NO ACTION;
 
--- device PK change
-ALTER TABLE device DROP CONSTRAINT facilities_pk CASCADE;
-ALTER TABLE device ADD PRIMARY KEY (device_euid,gateway_id); -- device_pkey is created
-
--- module PK change
-ALTER TABLE module DROP CONSTRAINT devices_pk CASCADE;
-ALTER TABLE module ADD PRIMARY KEY (device_euid, module_id, gateway_id); --module_pkey is created
-                                                          
--- log PK change
-ALTER TABLE log DROP CONSTRAINT logs_pk;
-ALTER TABLE log ADD PRIMARY KEY (device_euid, module_id, gateway_id, measured_at); --log_pkey is created
 
 -- delete old FK
 ALTER TABLE device DROP CONSTRAINT facilities_adapters CASCADE;
-ALTER TABLE module DROP CONSTRAINT devices_facilities CASCADE;
-ALTER TABLE log DROP CONSTRAINT logs_devices CASCADE;
 
 ALTER TABLE module ALTER COLUMN gateway_id SET NOT NULL;
 ALTER TABLE log ALTER COLUMN gateway_id SET NOT NULL;
--- pgmodeler/home7_alter_3.sql
+++ pgmodeler/home7_alter_3.sql
@@ -9,10 +9,10 @@ ALTER TABLE device ADD COLUMN status device_status DEFAULT 'available';
 ALTER TABLE module ADD COLUMN status module_status DEFAULT 'unavailable';
 
 -- delete all version of this function, some may not exists, but for sure
-DROP FUNCTION get_refresh_module_id(integer);
-DROP FUNCTION get_refresh_module_id(smallint);
-DROP FUNCTION get_refresh_module_id(bigint);
+DROP FUNCTION IF EXISTS get_refresh_module_id(integer);
+DROP FUNCTION IF EXISTS get_refresh_module_id(smallint);
+DROP FUNCTION IF EXISTS get_refresh_module_id(bigint);
 
-DROP FUNCTION get_refresh(numeric);
-DROP FUNCTION get_refresh(bigint);
-DROP FUNCTION get_refresh(smallint);
+DROP FUNCTION IF EXISTS get_refresh(numeric);
+DROP FUNCTION IF EXISTS get_refresh(bigint);
+DROP FUNCTION IF EXISTS get_refresh(smallint);
