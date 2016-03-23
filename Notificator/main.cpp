a8a0581d1948fcad516830139303d27ed427442d
Martin Novák <xnovak1c@stud.fit.vutbr.cz>
2016-03-23 14:45:34 +0100

    From now I will not comment until I stop testing Notificator.

--- Notificator/main.cpp
+++ Notificator/main.cpp
@@ -12,25 +12,21 @@
 
 int main()
 {   
-
-
     std::vector<std::string> service_reference_ids;
     service_reference_ids.push_back("f6MPWjr144I:APA91bE4_2j9YKMGD5iLiB5jn8UkcGWEfv3zOqcYWScYc7FflpNXuK9tW78hrfvwMdsBV0O97Kk_98ML2ml7tk3L63aZs2eIEApHRsoO5nRNUvJbgXOn0-WJ4COrdgawkn0TT0TwVR7R");
     
-
     int user_id = 9509;
     int notif_id = 8584;
     long timestamp = 1458737981;
     std::string message = "Testovaci URI notifikace.";
     std::string uri = "www.google.cz";
 
-    //std::shared_ptr<Notification> advert = std::make_shared<UriNotif>(user_id, notif_id, timestamp, message, uri);
+    std::shared_ptr<Notification> advert = std::make_shared<UriNotif>(user_id, notif_id, timestamp, message, uri);
 
-    //advert->sendGcm(&service_reference_ids);
-
+    advert->sendGcm(&service_reference_ids);
 
     //ids.push_back("APA91bF-u3AZ3bNl6jehq19oEhbVFGEaA_4x3jlzMtKwHzlakRcE9K_H_Rk8YuNSjp1K62dz-_sL09iQjlH8Z3JTsxbg1VbV_Gch0YhTuOg3U1bCib7pk4Xg4rHPeUrICiQ74RIB3qXxTjmTCeLBZor_Tr3pLuATUw");
-    
+    /*    
     Notification *notif = new WatchdogNotif(
         9509,                    // user ID (MrMaidx -> Martin Novak)
         9999,                  // message (notification) ID
@@ -42,7 +38,7 @@ int main()
         3);                   // rule ID
     
     notif->sendGcm(&service_reference_ids);
-    
+    */
  /*
     sleep(5);   
 
