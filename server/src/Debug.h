3ce1bdf957f926c7a7b96a40c20153cf5ceaa54c
Jan Viktorin <iviktorin@fit.vutbr.cz>
2016-07-23 00:27:56 +0200

    ui_server: introduce debug utilities
    Provide several macros and the class Trace which are helpful for
    debugging and tracing of the code. The Trace class is not an API
    and should not be used directly. Instead, there are 6 useful macros:
    
    * LOGGER_CLASS(instance)
    * LOGGER_FUNC(func)
    * TRACE_METHOD()
    * TRACE_FUNC()
    * _TRACE_METHOD(logger)
    * _TRACE_FUNC(logger)
    
    providing an API for debugging and tracing. The LOGGER_* macros
    are useful for creating appropriate loggers for a certain instance
    or function.
    
    Reviewed-by: Petr Stehlík <xstehl14@stud.fit.vutbr.cz>

---
+++ server/src/Debug.h
@@ -0,0 +1,164 @@
+#ifndef BEEEON_DEBUG_H
+#define BEEEON_DEBUG_H
+
+#ifdef defined(__clang__) || defined(__GLIBCXX__)
+#include <cxxabi.h> // abi::__cxa_demangle
+#endif
+
+#include <typeinfo>
+#include <cstring>
+#include <Poco/Exception.h>
+#include <Poco/Logger.h>
+
+namespace BeeeOn {
+
+/**
+ * Return the appropriate logger for the given class instance.
+ */
+#define LOGGER_CLASS(this) \
+	(Poco::Logger::get(BeeeOn::classDemangle(typeid((this)).name())))
+
+/**
+ * Return the appropriate logger for the given function name.
+ */
+#define LOGGER_FUNC(name)  (Poco::Logger::get((name)))
+
+#if defined(__clang__) || defined(__GLIBCXX__)
+/**
+ * Use abi::__cxa_demangle to demangle the class name from typeid.
+ * The abi may not be available on all compilers. The clang and
+ * gcc supports this (at least in c++11).
+ *
+ * The classDemangle function does more then just demangling. It
+ * also drops the template parameters. This leads to clear class
+ * short names useful e.g. for logging configuration. Also occasional
+ * pointer signs (*) are removed. Namespace prefixes are preserved.
+ *
+ * Example:
+ * Test<Other> -> 4TestI5OtherE -> Test
+ * Other -> 5Other -> Other
+ * Other * -> P5Other -> Other
+ * ?? -> PN6BeeeOn18DependencyInjectorE
+ *    -> BeeeOn::DependencyInjector* -> BeeeOn::DependencyInjector
+ *
+ * The ?? comes probably from this pointer.
+ */
+static inline const std::string classDemangle(const char *name)
+{
+	char *demangled = abi::__cxa_demangle(name, 0, 0, 0);
+	std::string s(demangled);
+	std::free(demangled);
+
+	std::size_t t;
+	
+	t = s.find("*<");
+	if (t != std::string::npos)
+		return s.substr(0, t);
+
+	t = s.find("<");
+	if (t != std::string::npos) {
+		std::size_t star = s.find("*");
+		if (star == std::string::npos)
+			return s.substr(0, t);
+
+		return s.substr(0, star < t? star : t);
+	}
+
+	t = s.find("*");
+	return t == std::string::npos? s : s.substr(0, t);
+}
+#else
+
+/**
+ * For unknown compilers, return the unmangled name because we
+ * don't know what can we do.
+ */
+static inline const std::string classDemangle(const std::string &name)
+{
+	return name;
+}
+#endif
+
+/**
+ * Tracing can be completele removed by specifying BEEEON_NO_TRACE
+ * during compilation.
+ */
+#ifndef BEEEON_NO_TRACE
+
+/**
+ * Tracer for automatic tracing of function calls. If instantiated
+ * at the beginning of a function, it logs the enter and leave
+ * events.
+ */
+class Trace {
+public:
+	Trace(Poco::Logger &logger, const char *func,
+			const char *file, int line):
+		m_logger(logger),
+		m_func(func),
+		m_file(file),
+		m_line(line)
+	{
+		if (m_logger.trace()) {
+			m_logger.trace(
+				Poco::Logger::format("enter $0", std::string(m_func)),
+				m_file, m_line);
+		}
+	}
+
+	~Trace()
+	{
+		if (m_logger.trace()) {
+			m_logger.trace(
+				Poco::Logger::format("leave $0", std::string(m_func)),
+				m_file, 0);
+		}
+	}
+
+private:
+	Poco::Logger &m_logger;
+	std::string m_func;
+	const char *m_file;
+	int m_line;
+};
+
+#define _INTERNAL_TRACE_METHOD(logger) BeeeOn::Trace \
+	_trace((logger), __func__, __FILE__, __LINE__)
+
+#define _INTERNAL_TRACE_FUNC(logger) BeeeOn::Trace \
+	__trace((logger), __func__, __FILE__, __LINE__)
+
+#else // BEEEON_NO_TRACE
+
+#define _INTERNAL_TRACE_METHOD(logger)
+#define _INTERNAL_TRACE_FUNC(logger)
+
+#endif // BEEEON_NO_TRACE
+
+/**
+ * Trace a method of the class instance "this".
+ * To reuse an existing logger, call _TRACE_METHOD(logger).
+ */
+#define TRACE_METHOD() \
+	_INTERNAL_TRACE_METHOD(LOGGER_CLASS(this))
+
+/**
+ * Trace with a custom logger given by caller.
+ */
+#define _TRACE_METHOD(logger) _INTERNAL_TRACE_METHOD(logger)
+
+/**
+ * Trace the current function.
+ * To reuse an existing logger, call _TRACE_FUNC(logger).
+ */
+#define TRACE_FUNC() \
+	_INTERNAL_TRACE_FUNC(LOGGER_FUNC(__func__))
+
+/**
+ * Trace with a custom logger given by caller.
+ */
+#define _TRACE_FUNC(logger) _INTERNAL_TRACE_FUNC(logger)
+
+}
+
+#endif
