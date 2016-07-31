12b03f244227ed3901fa35dfd1124512d37d61d0
Jan Viktorin <iviktorin@fit.vutbr.cz>
2016-07-31 02:42:34 +0200

    ui_server: new test fixture SessionManagerTest
    Reviewed-by: Petr Stehlík <xstehl14@stud.fit.vutbr.cz>

---
+++ server/test/server/SessionManagerTest.cpp
@@ -0,0 +1,60 @@
+#include <cppunit/extensions/HelperMacros.h>
+
+#include "server/SessionManager.h"
+#include "provider/MockRandomProvider.h"
+
+using namespace std;
+
+namespace BeeeOn {
+
+class SessionManagerTest : public CppUnit::TestFixture {
+	CPPUNIT_TEST_SUITE(SessionManagerTest);
+	CPPUNIT_TEST(testOpenClose);
+	CPPUNIT_TEST_SUITE_END();
+public:
+	void setUp();
+	void tearDown();
+	void testOpenClose();
+};
+
+CPPUNIT_TEST_SUITE_REGISTRATION(SessionManagerTest);
+
+void SessionManagerTest::setUp()
+{
+}
+
+void SessionManagerTest::tearDown()
+{
+}
+
+#define SESSION_ID64 string(\
+	"0123456789abcdef"  \
+	"0123456789abcdef"  \
+	"0123456789abcdef"  \
+	"0123456789abcdef")
+
+void SessionManagerTest::testOpenClose()
+{
+	MockRandomProvider mockRandomProvider;
+	mockRandomProvider.setNextRandom(SESSION_ID64);
+	InsecureRandomProvider randomProvider;
+	randomProvider.setProviderImpl(&mockRandomProvider);
+
+	SessionManager manager;
+	manager.setSecureRandomProvider(&randomProvider);
+
+	SessionManager::Info info;
+	info.insert(make_pair(SessionManager::INFO_EMAIL,
+				"test@exmaple.org"));
+
+	SessionID id = manager.open(info);
+	CPPUNIT_ASSERT(id.compare(SESSION_ID64) == 0);
+
+	SessionManager::Info infoLookup;
+	CPPUNIT_ASSERT(manager.lookup(id, infoLookup));
+
+	manager.close(id);
+	CPPUNIT_ASSERT(!manager.lookup(id, infoLookup));
+}
+
+}
