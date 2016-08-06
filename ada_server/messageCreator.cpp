BeeeOn/server

commit 41dadcb31b5b3a57cf6d13a9138a1024741bc016

commit ada_server
changes:
*implemented parsing and response for getparameters for list of devices
*done refactoring of code to enable better versioning of protocol
*implemented support of protocol version 1.1
*fixed crashing caused by wrong freeing memory in SSL
*fixed hanging after error during start

ada_server/messageCreator.cpp
@@ -96,29 +96,32 @@ std::string MessageCreator::CreateSearchMessage(std::string AdapterID, std::stri
 
 };
 
-std::string MessageCreator::CreateSwitchMessage(tmessage *Message)
+std::string MessageCreator::CreateSwitchMessage(tmessageV1_0 *Message)
 {
 	this->_log->WriteMessage(TRACE,"Entering " + this->_Name + "::CreateSwitchMessage");
 	xml_document *resp = new xml_document();
 	xml_node server_adapter = resp->append_child("server_adapter");
-	struct sockaddr_in antelope;
+	/*struct sockaddr_in antelope;
 	inet_aton(Message->DeviceIDstr.c_str(), &antelope.sin_addr);
 	in_addr_t DeviceIP = antelope.sin_addr.s_addr;
-	unsigned int DeviceIPint = ntohl (DeviceIP);
+	unsigned int DeviceIPint = ntohl (DeviceIP);*/
 	server_adapter.append_attribute("protocol_version") = "1.0";
 	server_adapter.append_attribute("state") = "set";
-	server_adapter.append_attribute("euid") = std::to_string(DeviceIPint).c_str();
+    unsigned long long DeviceIPint = std::stoll(Message->DeviceIDstr);
+    this->_log->WriteMessage(DEBUG,"Device euid in swtich message will be:" + int_to_hex(DeviceIPint));
+    server_adapter.append_attribute("euid") = std::to_string(DeviceIPint).c_str();
 	server_adapter.append_attribute("adapter_id") = std::to_string(Message->adapterINTid).c_str();
 
 	for (int i = 0; i < Message->values_count; i++)
 	{
 		xml_node value_node = server_adapter.append_child("value");
-		std::string stringType;
+		/*std::string stringType;
 		std::ostringstream os;
 		os << std::hex << Message->values[i].module_id;
-		stringType = os.str();
-		value_node.append_attribute("module_id") = stringType.c_str();
-		value_node.text().set(Message->values[i].measured_value);
+		stringType = os.str();*/
+		value_node.append_attribute("module_id") = int_to_hex(Message->values[i].module_id).c_str();
+        this->_log->WriteMessage(DEBUG,"Module id in swtich message will be:" + int_to_hex(Message->values[i].module_id));
+        value_node.text().set(Message->values[i].measured_value);
 
 
 		//value_node.append_attribute("type") = stringType.c_str();

ada_server/messageParsers.cpp
@@ -11,7 +11,7 @@
 
 using namespace pugi;
 
-tmessage* MessageParser::ReturnMessage()
+tmessageV1_0 * MessageParser::ReturnMessage()
 {
 	this->_log->WriteMessage(TRACE,"Entering " + this->_Name + "::ReturnMessage");
 	this->_log->WriteMessage(TRACE,"Exiting " + this->_Name + "::ReturnMessage");
@@ -111,10 +111,6 @@ void ProtocolV1MessageParser::GetState()
 	{
 		_message->state = DATA;
 	}
-  else if (temp_state.compare("getparameters") == 0)
-  {
-    _message->state = GET_PARAMS;
-  }
 	else
 	{
 		_message->state = UNKNOWN;
@@ -127,12 +123,15 @@ void ProtocolV1MessageParser::GetState()
 void ProtocolV1MessageParser::GetDeviceID()
 {
 	this->_log->WriteMessage(TRACE,"Entering " + this->_Name + "::GetDeviceID");
-	
-	this->_message->device_type = std::stoll(_device.attribute("device_id").as_string(),nullptr,16);   
-	       
-	this->_message->device_euid = std::stoll(_device.attribute("euid").as_string(),nullptr,16);
-  
-  
+	try {
+		this->_message->device_type = std::stoull(_device.attribute("device_id").as_string(), nullptr, 16);
+
+		this->_message->device_euid = std::stoull(_device.attribute("euid").as_string(), nullptr, 16);
+	}
+	catch (std::exception const &e)
+	{
+		this->_log->WriteMessage(ERR,"Device id not parsable!");
+	}
 	in_addr_t temp = htonl (_message->device_euid);
 	this->_log->WriteMessage(MSG,"Device id :" + std::to_string(this->_message->device_euid));
 	struct sockaddr_in antelope;
@@ -192,24 +191,32 @@ void ProtocolV1MessageParser::GetValuesCount()
 	this->_log->WriteMessage(TRACE,"Exiting " + this->_Name + "::GetValuesCount");
 }
 
-std::string ProtocolV1MessageParser::CreateAnswer(int value)
+std::string ProtocolV1MessageParser::CreateAnswer()
 {
 	this->_log->WriteMessage(TRACE,"Entering " + this->_Name + "::CreateAnswer");
 	xml_document *resp = new xml_document();
 	xml_node server_adapter = resp->append_child("server_adapter");
 	//server_adapter->set_name("server_adapter");
-	server_adapter.append_attribute("protocol_version");
-	server_adapter.append_attribute("state");
-	server_adapter.append_attribute("euid");
-	server_adapter.append_attribute("time");
-	std::stringstream s;
-	s.precision(2);
-	s << _message->cp_version;
-	server_adapter.attribute("protocol_version") = s.str().c_str();
-	server_adapter.attribute("state") = "update";
-  
-	server_adapter.attribute("euid") = int_to_hex(_message->device_euid).c_str();
-	server_adapter.attribute("time") = std::to_string(value).c_str();
+	server_adapter.append_attribute("protocol_version")= std::to_string(_message->cp_version).c_str();
+	if (this->_message->state==DATA)
+	{
+		server_adapter.append_attribute("state") = "update";
+		server_adapter.append_attribute("euid") = int_to_hex(_message->device_euid).c_str();
+		server_adapter.append_attribute("time") = std::to_string(this->_message->refresh).c_str();
+	}
+	else
+	{
+		server_adapter.append_attribute("state") = "register";
+		server_adapter.append_attribute("response");
+		if(_message->registerResult)
+		{
+			server_adapter.attribute("response") = "true";
+		}
+		else
+		{
+			server_adapter.attribute("response") = "false";
+		}
+	}
 	tstringXMLwriter writer;
 	resp->print(writer);
 	delete(resp);
@@ -249,7 +256,7 @@ ProtocolV1MessageParser::~ProtocolV1MessageParser()
 ProtocolV1MessageParser::ProtocolV1MessageParser(Loger *L)
 {
 	L->WriteMessage(TRACE,"Entering " + this->_Name + "::Constructor");
-	this->_message = new tmessage();
+	this->_message = new tmessageV1_0();
 	this->_log = L;
 	this->version = "1.0";
 	this->_log->WriteMessage(TRACE,"Exiting " + this->_Name + "::Constructor");
@@ -379,27 +386,27 @@ bool UIServerMessageParser::GetState()
 	if(reqType.compare("delete")==0)
 	{
 		this->_Message->state=DELETE;
-		this->_log->WriteMessage(MSG,"Request type is DELETE");
+		this->_log->WriteMessage(DEBUG,"Request type is DELETE");
 		this->_log->WriteMessage(TRACE,"Exiting " + this->_Name + "::GetState");
 		return (true);
 	}
 	if(reqType.compare("switch")==0)
 	{
 		this->_Message->state=SWITCH;
-		this->_log->WriteMessage(MSG,"Request type is SWITCH");
+		this->_log->WriteMessage(DEBUG,"Request type is SWITCH");
 		this->_log->WriteMessage(TRACE,"Exiting " + this->_Name + "::GetState");
 		return (true);
 	}
 	if (reqType.compare("listen")==0)
 	{
-		this->_log->WriteMessage(MSG,"Request type is LISTEN");
+		this->_log->WriteMessage(DEBUG,"Request type is LISTEN");
 		this->_Message->state=LISTEN;
 		this->_log->WriteMessage(TRACE,"Exiting " + this->_Name + "::GetState");
 		return (true);
 	}
   if (reqType.compare("search")==0)
 	{
-		this->_log->WriteMessage(MSG,"Request type is SEARCH");
+		this->_log->WriteMessage(DEBUG,"Request type is SEARCH");
 		this->_Message->state=SEARCH;
 		this->_log->WriteMessage(TRACE,"Exiting " + this->_Name + "::GetState");
 		return (true);
@@ -458,7 +465,7 @@ bool UIServerMessageParser::ParseMessage(std::string MSG)
 
 UIServerMessageParser::UIServerMessageParser(Loger *l)
 {
-	this->_Message = new tmessage();
+	this->_Message = new tmessageV1_0();
 	this->_log = l;
 }
 
@@ -467,4 +474,151 @@ UIServerMessageParser::~UIServerMessageParser()
 	delete this->_Message->values;
 	this->_Message->values =NULL;
 	delete this->_Message;
+}
+
+void ProtocolV1_1_MessageParser::GetState()
+{
+    this->_log->WriteMessage(TRACE,"Entering " + this->_Name + "::GetState");
+    std::string temp_state = _adapter->attribute("state").as_string();
+
+    if (temp_state.compare("register")==0)
+    {
+        _message->state = REGISTER;
+    }
+    else if (temp_state.compare("data")==0)
+    {
+        _message->state = DATA;
+    }
+    else if (temp_state.compare("getparameter")==0)
+    {
+        _message->state = GET_PARAMS;
+    }
+    else
+    {
+        _message->state = UNKNOWN;
+    }
+    this->_log->WriteMessage(TRACE,"Exiting " + this->_Name + "::GetState");
+}
+
+bool ProtocolV1_1_MessageParser::ParseMessage(pugi::xml_node *adapter, float FM, float CP)
+{
+	this->_log->WriteMessage(TRACE,"Entering " + this->_Name + "::parseMessage");
+	this->_adapter = adapter;
+	this->GetState();
+	switch (this->_message->state)
+	{
+		case DATA:
+		case REGISTER:
+			this->_log->WriteMessage(TRACE,"Exiting " + this->_Name + "::parseMessage");
+			return ProtocolV1MessageParser::ParseMessage(adapter, FM, CP);
+		case GET_PARAMS:
+			this->GetID();
+			this->_message->fm_version = FM;
+			this->_message->cp_version = CP;
+			this->_log->WriteMessage(TRACE,"Exiting " + this->_Name + "::parseMessage");
+			return this->GetParams();
+		default:
+			return false;
+	}
+}
+
+ProtocolV1_1_MessageParser::ProtocolV1_1_MessageParser(Loger *L)
+{
+		L->WriteMessage(TRACE,"Entering " + this->_Name + "::Constructor");
+		this->version="1.1";
+		this->_message = new tmessageV1_1;
+		this->_log = L;
+		this->_log->WriteMessage(TRACE,"Exiting " + this->_Name + "::Constructor");
+};
+
+bool ProtocolV1_1_MessageParser::GetParams()
+{
+	this->_log->WriteMessage(TRACE,"Entering " + this->_Name + "::GetParams");
+	xml_node parameter = this->_adapter->first_child();
+	while (parameter!=NULL)
+	{
+		tparams *param = new tparams();
+		param->id = parameter.attribute("id").as_int();
+		switch (param->id)
+		{
+			case 1001:
+			case 1002:
+				param->euid = parameter.attribute("euid").as_ullong();
+				break;
+			case 1000:
+			case 1003:
+				break;
+			default:
+				delete param;
+				this->_log->WriteMessage(WARN,"Unknown parameter id in gatparams message id " + std::to_string(param->id));
+		}
+		parameter = parameter.next_sibling();
+		((tmessageV1_1*)_message)->params->push_back(param);
+	}
+	this->_log->WriteMessage(TRACE,"Exiting " + this->_Name + "::GetParams");
+	return true;
+}
+
+std::string ProtocolV1_1_MessageParser::CreateAnswer()
+{
+	this->_log->WriteMessage(TRACE,"Entering " + this->_Name + "::CreateAnswer");
+	if (this->_message->state != GET_PARAMS)
+	{
+		this->_log->WriteMessage(TRACE,"Exiting " + this->_Name + "::CreateAnswer");
+		return ProtocolV1MessageParser::CreateAnswer();
+	}
+	else
+	{
+		xml_document *resp = new xml_document();
+		xml_node server_adapter = resp->append_child("server_adapter");
+		//server_adapter->set_name("server_adapter");
+		server_adapter.append_attribute("protocol_version")= std::to_string(_message->cp_version).c_str();
+		server_adapter.append_attribute("state") = "parameters";
+		std::vector<tparams*> *parameters = ((tmessageV1_1*)(this->_message))->params;
+		if (parameters != nullptr)
+		for (size_t i = 0; i < parameters->size();i++)
+		{
+			xml_node parameterNode = server_adapter.append_child("parameter");
+			parameterNode.append_attribute("id") = std::to_string(parameters->at(i)->id).c_str();
+			switch (parameters->at(i)->id)
+			{
+				case 1001:
+					break;
+				case 1002:
+					break;
+				case 1000:
+					break;
+				case 1003:
+					if (parameters->at(i)->deviceList != nullptr)
+					{
+						for (size_t j = 0; j < parameters->at(i)->deviceList->size(); j++)
+							parameterNode.append_child("value").text().set(
+									int_to_hex(parameters->at(i)->deviceList->at(j)).c_str());
+					}
+					break;
+				default:
+					parameterNode.append_child("value").text().set("Not supported");
+					break;
+			}
+
+		}
+		tstringXMLwriter writer;
+		resp->print(writer);
+		delete(resp);
+		std::string tmp = writer.result;
+		if (tmp[tmp.length()-1]=='\n')
+		{
+			tmp.erase(tmp.length()-1,1);
+		}
+		this->_log->WriteMessage(TRACE,"Exiting " + this->_Name + "::CreateAnswer");
+		return ("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" + tmp);
+	}
+}
+
+ProtocolV1_1_MessageParser::~ProtocolV1_1_MessageParser()
+{
+	this->_log->WriteMessage(TRACE,"Entering " + this->_Name + "::~ProtocolV1_1_MessageParser");
+	delete (messageV1_1*)(this->_message);
+	this->_message= nullptr;
+	this->_log->WriteMessage(TRACE,"Exiting " + this->_Name + "::~ProtocolV1_1_MessageParser");
 }

ada_server/DBHandler.cpp
@@ -11,7 +11,7 @@
 
 using namespace soci;
 
-void DBHandler::LogValue (tmessage *message)
+void DBHandler::LogValue (tmessageV1_0 *message)
 {
 	this->_log->WriteMessage(TRACE,"Entering " + this->_Name + "::LogValue");
 	for (int i = 0; i<message->values_count;i++) //for all values create text representation and call SQL querry
@@ -79,7 +79,7 @@ bool DBHandler::IsInDB(std::string tableName, std::string columnName, std::strin
 	}
 }
 
-bool DBHandler::InsertGateway(tmessage *message)
+bool DBHandler::InsertGateway(tmessageV1_0 *message)
 {
 	this->_log->WriteMessage(TRACE,"Entering " + this->_Name + "::InsertGateway");
 	try
@@ -102,7 +102,7 @@ bool DBHandler::InsertGateway(tmessage *message)
 	}
 }
 
-bool DBHandler::UpdateGateway(tmessage *message)
+bool DBHandler::UpdateGateway(tmessageV1_0 *message)
 {
 	this->_log->WriteMessage(TRACE,"Entering " + this->_Name + "::UpdateGateway");
 	try
@@ -135,7 +135,7 @@ bool DBHandler::UpdateGateway(tmessage *message)
 	}
 }
 
-bool DBHandler::InsertSenAct(tmessage *message)
+bool DBHandler::InsertSenAct(tmessageV1_0 *message)
 {
 	this->_log->WriteMessage(TRACE,"Entering " + this->_Name + "::InsertSenAct");
 	try
@@ -191,7 +191,7 @@ bool DBHandler::InsertSenAct(tmessage *message)
 	} */
 }
 
-bool DBHandler::UpdateSenAct(tmessage *message)
+bool DBHandler::UpdateSenAct(tmessageV1_0 *message)
 {
 	this->_log->WriteMessage(TRACE,"Entering " + this->_Name + "::UpdateSenAct");
 	try
@@ -251,7 +251,7 @@ bool DBHandler::UpdateSenAct(tmessage *message)
       }
       else if(moduleStatus == "unavailable")
       {
-  			statement stl = (_sql->prepare <<SQLQueries::UpdateModuleWithStatus,
+  			statement stl = (_sql->prepare<<SQLQueries::UpdateModuleWithStatus,
   								use(message->device_euid,"DEVICE_EUID"),
   								use(moduleStatus, "STATUS"),
   								use(message->values[i].module_id,"MODULE_ID"),
@@ -279,13 +279,13 @@ bool DBHandler::UpdateSenAct(tmessage *message)
 	}
 }
 
-int DBHandler::GetWakeUpTime(std::string record, long long int gateway_id)
+unsigned int DBHandler::GetWakeUpTime(std::string record, long long int gateway_id)
 {
 	this->_log->WriteMessage(TRACE,"Entering " + this->_Name + "::GetWakeUpTime");
 	try
 	{
 		indicator ind;
-		int retRec;
+		unsigned int retRec;
 		*_sql << SQLQueries::SelectTime,
 				into(retRec, ind),
 				use(record),
@@ -402,5 +402,32 @@ time_t DBHandler::GetLastTimestamp(long long unsigned int dev_euid)
 		this->_log->WriteMessage(ERR,ErrorMessage );
 		this->_log->WriteMessage(TRACE,"Exiting " + this->_Name + "::GetLastTimestamp");
 		return (retVal);
+	}
+}
+
+bool DBHandler::GetDevices(tmessageV1_1 *message)
+{
+	this->_log->WriteMessage(TRACE,"Entering " + this->_Name + "::GetDevices");
+	size_t count = 0;
+	try
+	{
+		*_sql << SQLQueries::SelectAllDevicesCount,
+				use(message->adapterINTid, "GATEWAY_ID"),
+				into(count);
+		message->params->at(message->processedParams)->deviceList = new std::vector<unsigned long long>(count);
+		*_sql << SQLQueries::SelectAllDevices,
+				use(message->adapterINTid, "GATEWAY_ID"),
+				into(*(message->params->at(message->processedParams)->deviceList));
+		this->_log->WriteMessage(TRACE,"Exiting " + this->_Name + "::GetDevices");
+		return (true);
+	}
+	catch(std::exception const &e)
+	{
+		this->_log->WriteMessage(ERR, "Error in query : " + _sql->get_last_query() );
+		std::string ErrorMessage = "Database Error : ";
+		ErrorMessage.append (e.what());
+		this->_log->WriteMessage(ERR,ErrorMessage );
+		this->_log->WriteMessage(TRACE,"Exiting " + this->_Name + "::GetDevices");
+		return (false);
 	}
 }

ada_server/DBHandler.h
@@ -43,32 +43,32 @@ class DBHandler
 		 * @param message - pointer to message content
 		 * @return bool on success/failure true/false
 		    */
-		bool InsertGateway(tmessage *message);
+		bool InsertGateway(tmessageV1_0 *message);
 		/** Method for inserting new facility and device to database
 		 * @param message - pointer to message content
 		 * @return bool on success/failure true/false
 			*/
-		bool InsertSenAct(tmessage *message);
+		bool InsertSenAct(tmessageV1_0 *message);
 		/** Method for updating adapter in database
 		 * @param message - pointer to message content
 		 * @return bool on success/failure true/false
 			*/
-		bool UpdateGateway(tmessage *message);
+		bool UpdateGateway(tmessageV1_0 *message);
 		/** Method for updating facility and device in database
 		 * @param message - pointer to message content
 		 * @return bool on success/failure true/false
 			*/
-		bool UpdateSenAct(tmessage *message);
+		bool UpdateSenAct(tmessageV1_0 *message);
 		/** Method for obtaining wakeup time from database
 		 * @param record - record which time are we looking for
 		 * @return number of seconds to next wakeup on failure/missing record returns default value 5
 			*/
-		int GetWakeUpTime(std::string record, long long int gateway_id);
+		unsigned int GetWakeUpTime(std::string record, long long int gateway_id);
 		std::string GetXmlDeviceParameters(std::string device_euid, long long int adapterid);
     /** Method for creating history row in database
 		 * @param message - pointer to message content
 			*/
-		void LogValue (tmessage *message);
+		void LogValue (tmessageV1_0 *message);
 		/** Method for obtaining socket number from DB
 		 * @param soc - pointer to int where is returned value
 		 * @param long int ID of adapter
@@ -78,12 +78,13 @@ class DBHandler
 		 * @param message - pointer to message content
 		 * @param long int ID of adapter
 			*/
-		bool UpdateAdapterPort(tmessage *message);
+		bool UpdateAdapterPort(tmessageV1_0 *message);
 
 		/** Method for obtaining last timestamp
 		* @param long long unsigned int containing device identification
 		* @return time_t value of the timestamp from database*/
 		time_t GetLastTimestamp(long long unsigned int dev_euid);
+        bool GetDevices(tmessageV1_1 *message);
 };
 
 

ada_server/Makefile
@@ -24,3 +24,5 @@ clean:
 	
 cleanlogs:
 		rm *.log
+
+rebuild: clean all

ada_server/adaServer.cpp
@@ -34,6 +34,31 @@ Loger *ReceiverLog;
 bool sigint =false;
 SSLContainer *sslCont;
 
+static void init_locks(void)
+{
+	int i;
+
+	lockarray=(pthread_mutex_t *)OPENSSL_malloc(CRYPTO_num_locks() *
+												sizeof(pthread_mutex_t));
+	for (i=0; i<CRYPTO_num_locks(); i++) {
+		pthread_mutex_init(&(lockarray[i]),NULL);
+	}
+
+	CRYPTO_set_id_callback((unsigned long (*)())thread_id);
+	CRYPTO_set_locking_callback((void (*)(int, int, const char*, int))lock_callback);
+}
+
+static void kill_locks(void)
+{
+	int i;
+
+	CRYPTO_set_locking_callback(NULL);
+	for (i=0; i<CRYPTO_num_locks(); i++)
+		pthread_mutex_destroy(&(lockarray[i]));
+
+	OPENSSL_free(lockarray);
+}
+
 void sig_handler(int signo) //on signals to turn of clear 
 {
 	if ((signo == SIGINT)||(signo == SIGTERM))
@@ -51,6 +76,7 @@ void sig_handler(int signo) //on signals to turn of clear
 		delete (SenderLog);
 		delete (ReceiverLog);
 		CRYPTO_cleanup_all_ex_data();
+		kill_locks();
 		ERR_remove_state(0);
 		ERR_free_strings();
 		EVP_cleanup();
@@ -166,6 +192,9 @@ int main(int argc, char **argv)  //main body of application
 		stopSCR<<"kill -SIGINT "<<proces_id<<std::endl;
 		stopSCR.close();
 	}
+	SSL_load_error_strings();
+	SSL_library_init();
+	OpenSSL_add_all_algorithms();
 	SenderLog = new Loger();
 	ReceiverLog = new Loger();
 	SenderLog->SetLogger(c->SenderVerbosity(),c->SenderMaxFiles(),c->SenderMaxLines(),c->SenderFileNaming(),c->SenderPath(),"SENDER",c->SenderToSTD());
@@ -188,6 +217,7 @@ int main(int argc, char **argv)  //main body of application
 	}
 	sslCont = new SSLContainer(ReceiverLog);
 	wpool = WorkerPool::CreatePool(ReceiverLog,SenderLog,connStr,c,sslCont);
+	init_locks();
 	if ((wpool==NULL)||(wpool->Limit()<=0))
 	{
 		SenderLog->WriteMessage(FATAL," [Main Process] 0 connections to DB unable to serve, terminating!");
@@ -224,7 +254,12 @@ int main(int argc, char **argv)  //main body of application
 		delete (wpool);
 		delete (sender);
 		delete (receiver);
-		//SenderThread->join();
+		CRYPTO_cleanup_all_ex_data();
+		kill_locks();
+		ERR_remove_state(0);
+		ERR_free_strings();
+		EVP_cleanup();
+		sem_destroy(&connectionSem);
 		delete (SenderThread);
 		delete (SenderLog);
 		delete (ReceiverLog);

ada_server/adaServerSender.cpp
@@ -24,7 +24,9 @@ AdaServerSender::AdaServerSender(sem_t *Sem, WorkerPool *workers, Loger *L,Confi
 
 AdaServerSender::~AdaServerSender()
 {
+	_log->WriteMessage(TRACE,"Entering " + this->_Name + "Destructor");
 	delete this->_L;
+	_log->WriteMessage(TRACE,"Exiting " + this->_Name + "Destructor");
 }
 
 void AdaServerSender::Start()

commit 09693123538f22bf94c56515bf4fd3d07a2a3a6e

ada_server: fix wrong param_id name
Fixes: 41dadcb31b5b ("commit ada_server")

Acked-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

ada_server/messageCreator.cpp
@@ -189,7 +189,7 @@ std::string MessageCreator::CreatePingMessage(tadapter *adapter)
 		server_adapter.append_attribute("protocol_version") = std::to_string(adapter->protocol_version).c_str();
 		server_adapter.append_attribute("state") = "getparameters";
 		xml_node parameter = server_adapter.append_child("parameter");
-		parameter.append_attribute("id") = "2000";
+		parameter.append_attribute("param_id") = "2000";
 		tstringXMLwriter writer;
 		resp->print(writer);
 		delete(resp);

ada_server/messageParsers.cpp
@@ -550,7 +550,7 @@ bool ProtocolV1_1_MessageParser::GetParams()
 	while (parameter!=NULL)
 	{
 		tparams *param = new tparams();
-		param->id = parameter.attribute("id").as_int();
+		param->id = parameter.attribute("param_id").as_int();
 		switch (param->id)
 		{
 			case 1001:
@@ -591,7 +591,7 @@ std::string ProtocolV1_1_MessageParser::CreateAnswer()
 		for (size_t i = 0; i < parameters->size();i++)
 		{
 			xml_node parameterNode = server_adapter.append_child("parameter");
-			parameterNode.append_attribute("id") = std::to_string(parameters->at(i)->id).c_str();
+			parameterNode.append_attribute("param_id") = std::to_string(parameters->at(i)->id).c_str();
 			switch (parameters->at(i)->id)
 			{
 				case 1001: