a7d74305adffd659b21f08917b0660885e0b8562
Jan Viktorin <iviktorin@fit.vutbr.cz>
2016-08-06 23:51:58 +0200

    SimpleID: new class wrapping long data type
    Wrap the long into SimpleID class to have a set of methods
    that can be called in generic template-based code. It also
    ensures a proper initialization of the ID value when comparing
    to a pure long or int data type.
    
    Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>
    Reviewed-by: Petr Stehlik <xstehl14@stud.fit.vutbr.cz>

---
+++ server/src/model/SimpleID.h
@@ -0,0 +1,103 @@
+#ifndef BEEEON_SIMPLE_ID_H
+#define BEEEON_SIMPLE_ID_H
+
+#include <string>
+#include <climits>
+#include <Poco/Exception.h>
+
+namespace BeeeOn {
+
+class SimpleID {
+public:
+	SimpleID():
+		m_value(0)
+	{
+	}
+
+	SimpleID(long value):
+		m_value(value)
+	{
+	}
+
+	SimpleID(const SimpleID &copy):
+		m_value(copy.m_value)
+	{
+	}
+
+	bool isNull() const
+	{
+		return m_value == 0;
+	}
+
+	operator long() const
+	{
+		return m_value;
+	}
+
+	operator int() const
+	{
+		if (INT_MIN > m_value || m_value > INT_MAX)
+			throw Poco::BadCastException("out of range of int");
+
+		return (int) m_value;
+	}
+
+	static SimpleID parse(const std::string &s)
+	{
+		return SimpleID(std::stoi(s));
+	}
+
+	std::string toString() const
+	{
+		return std::to_string(m_value);
+	}
+
+	SimpleID &operator ++()
+	{
+		++m_value;
+		return *this;
+	}
+
+	SimpleID &operator ++(int)
+	{
+		m_value++;
+		return *this;
+	}
+
+	bool operator !=(const SimpleID &id) const
+	{
+		return m_value != id.m_value;
+	}
+
+	bool operator ==(const SimpleID &id) const
+	{
+		return m_value == id.m_value;
+	}
+
+	bool operator <(const SimpleID &id) const
+	{
+		return m_value < id.m_value;
+	}
+
+	bool operator >(const SimpleID &id) const
+	{
+		return m_value > id.m_value;
+	}
+
+	bool operator <=(const SimpleID &id) const
+	{
+		return m_value <= id.m_value;
+	}
+
+	bool operator >=(const SimpleID &id) const
+	{
+		return m_value >= id.m_value;
+	}
+
+private:
+	long m_value = 0;
+};
+
+}
+
+#endif
