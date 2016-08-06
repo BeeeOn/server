BeeeOn/server

commit 41dadcb31b5b3a57cf6d13a9138a1024741bc016

commit ada_server
changes:
*implemented parsing and response for getparameters for list of devices
*done refactoring of code to enable better versioning of protocol
*implemented support of protocol version 1.1
*fixed crashing caused by wrong freeing memory in SSL
*fixed hanging after error during start

ada_server/DBHandler.cpp
@@ -11,7 +11,7 @@
 
 using namespace soci;
 
-void DBHandler::LogValue (tmessage *message)
+void DBHandler::LogValue (tmessageV1_0 *message)
 {
 	this->_log->WriteMessage(TRACE,"Entering " + this->_Name + "::LogValue");
 	for (int i = 0; i<message->values_count;i++) //for all values create text representation and call SQL querry
@@ -79,7 +79,7 @@ bool DBHandler::IsInDB(std::string tableName, std::string columnName, std::strin
 	}
 }
 
-bool DBHandler::InsertGateway(tmessage *message)
+bool DBHandler::InsertGateway(tmessageV1_0 *message)
 {
 	this->_log->WriteMessage(TRACE,"Entering " + this->_Name + "::InsertGateway");
 	try
@@ -102,7 +102,7 @@ bool DBHandler::InsertGateway(tmessage *message)
 	}
 }
 
-bool DBHandler::UpdateGateway(tmessage *message)
+bool DBHandler::UpdateGateway(tmessageV1_0 *message)
 {
 	this->_log->WriteMessage(TRACE,"Entering " + this->_Name + "::UpdateGateway");
 	try
@@ -135,7 +135,7 @@ bool DBHandler::UpdateGateway(tmessage *message)
 	}
 }
 
-bool DBHandler::InsertSenAct(tmessage *message)
+bool DBHandler::InsertSenAct(tmessageV1_0 *message)
 {
 	this->_log->WriteMessage(TRACE,"Entering " + this->_Name + "::InsertSenAct");
 	try
@@ -191,7 +191,7 @@ bool DBHandler::InsertSenAct(tmessage *message)
 	} */
 }
 
-bool DBHandler::UpdateSenAct(tmessage *message)
+bool DBHandler::UpdateSenAct(tmessageV1_0 *message)
 {
 	this->_log->WriteMessage(TRACE,"Entering " + this->_Name + "::UpdateSenAct");
 	try
@@ -251,7 +251,7 @@ bool DBHandler::UpdateSenAct(tmessage *message)
       }
       else if(moduleStatus == "unavailable")
       {
-  			statement stl = (_sql->prepare <<SQLQueries::UpdateModuleWithStatus,
+  			statement stl = (_sql->prepare<<SQLQueries::UpdateModuleWithStatus,
   								use(message->device_euid,"DEVICE_EUID"),
   								use(moduleStatus, "STATUS"),
   								use(message->values[i].module_id,"MODULE_ID"),
@@ -279,13 +279,13 @@ bool DBHandler::UpdateSenAct(tmessage *message)
 	}
 }
 
-int DBHandler::GetWakeUpTime(std::string record, long long int gateway_id)
+unsigned int DBHandler::GetWakeUpTime(std::string record, long long int gateway_id)
 {
 	this->_log->WriteMessage(TRACE,"Entering " + this->_Name + "::GetWakeUpTime");
 	try
 	{
 		indicator ind;
-		int retRec;
+		unsigned int retRec;
 		*_sql << SQLQueries::SelectTime,
 				into(retRec, ind),
 				use(record),
@@ -402,5 +402,32 @@ time_t DBHandler::GetLastTimestamp(long long unsigned int dev_euid)
 		this->_log->WriteMessage(ERR,ErrorMessage );
 		this->_log->WriteMessage(TRACE,"Exiting " + this->_Name + "::GetLastTimestamp");
 		return (retVal);
+	}
+}
+
+bool DBHandler::GetDevices(tmessageV1_1 *message)
+{
+	this->_log->WriteMessage(TRACE,"Entering " + this->_Name + "::GetDevices");
+	size_t count = 0;
+	try
+	{
+		*_sql << SQLQueries::SelectAllDevicesCount,
+				use(message->adapterINTid, "GATEWAY_ID"),
+				into(count);
+		message->params->at(message->processedParams)->deviceList = new std::vector<unsigned long long>(count);
+		*_sql << SQLQueries::SelectAllDevices,
+				use(message->adapterINTid, "GATEWAY_ID"),
+				into(*(message->params->at(message->processedParams)->deviceList));
+		this->_log->WriteMessage(TRACE,"Exiting " + this->_Name + "::GetDevices");
+		return (true);
+	}
+	catch(std::exception const &e)
+	{
+		this->_log->WriteMessage(ERR, "Error in query : " + _sql->get_last_query() );
+		std::string ErrorMessage = "Database Error : ";
+		ErrorMessage.append (e.what());
+		this->_log->WriteMessage(ERR,ErrorMessage );
+		this->_log->WriteMessage(TRACE,"Exiting " + this->_Name + "::GetDevices");
+		return (false);
 	}
 }

ada_server/DBHandler.h
@@ -43,32 +43,32 @@ class DBHandler
 		 * @param message - pointer to message content
 		 * @return bool on success/failure true/false
 		    */
-		bool InsertGateway(tmessage *message);
+		bool InsertGateway(tmessageV1_0 *message);
 		/** Method for inserting new facility and device to database
 		 * @param message - pointer to message content
 		 * @return bool on success/failure true/false
 			*/
-		bool InsertSenAct(tmessage *message);
+		bool InsertSenAct(tmessageV1_0 *message);
 		/** Method for updating adapter in database
 		 * @param message - pointer to message content
 		 * @return bool on success/failure true/false
 			*/
-		bool UpdateGateway(tmessage *message);
+		bool UpdateGateway(tmessageV1_0 *message);
 		/** Method for updating facility and device in database
 		 * @param message - pointer to message content
 		 * @return bool on success/failure true/false
 			*/
-		bool UpdateSenAct(tmessage *message);
+		bool UpdateSenAct(tmessageV1_0 *message);
 		/** Method for obtaining wakeup time from database
 		 * @param record - record which time are we looking for
 		 * @return number of seconds to next wakeup on failure/missing record returns default value 5
 			*/
-		int GetWakeUpTime(std::string record, long long int gateway_id);
+		unsigned int GetWakeUpTime(std::string record, long long int gateway_id);
 		std::string GetXmlDeviceParameters(std::string device_euid, long long int adapterid);
     /** Method for creating history row in database
 		 * @param message - pointer to message content
 			*/
-		void LogValue (tmessage *message);
+		void LogValue (tmessageV1_0 *message);
 		/** Method for obtaining socket number from DB
 		 * @param soc - pointer to int where is returned value
 		 * @param long int ID of adapter
@@ -78,12 +78,13 @@ class DBHandler
 		 * @param message - pointer to message content
 		 * @param long int ID of adapter
 			*/
-		bool UpdateAdapterPort(tmessage *message);
+		bool UpdateAdapterPort(tmessageV1_0 *message);
 
 		/** Method for obtaining last timestamp
 		* @param long long unsigned int containing device identification
 		* @return time_t value of the timestamp from database*/
 		time_t GetLastTimestamp(long long unsigned int dev_euid);
+        bool GetDevices(tmessageV1_1 *message);
 };
 
 

ada_server/Makefile
@@ -24,3 +24,5 @@ clean:
 	
 cleanlogs:
 		rm *.log
+
+rebuild: clean all

ada_server/adaServer.cpp
@@ -34,6 +34,31 @@ Loger *ReceiverLog;
 bool sigint =false;
 SSLContainer *sslCont;
 
+static void init_locks(void)
+{
+	int i;
+
+	lockarray=(pthread_mutex_t *)OPENSSL_malloc(CRYPTO_num_locks() *
+												sizeof(pthread_mutex_t));
+	for (i=0; i<CRYPTO_num_locks(); i++) {
+		pthread_mutex_init(&(lockarray[i]),NULL);
+	}
+
+	CRYPTO_set_id_callback((unsigned long (*)())thread_id);
+	CRYPTO_set_locking_callback((void (*)(int, int, const char*, int))lock_callback);
+}
+
+static void kill_locks(void)
+{
+	int i;
+
+	CRYPTO_set_locking_callback(NULL);
+	for (i=0; i<CRYPTO_num_locks(); i++)
+		pthread_mutex_destroy(&(lockarray[i]));
+
+	OPENSSL_free(lockarray);
+}
+
 void sig_handler(int signo) //on signals to turn of clear 
 {
 	if ((signo == SIGINT)||(signo == SIGTERM))
@@ -51,6 +76,7 @@ void sig_handler(int signo) //on signals to turn of clear
 		delete (SenderLog);
 		delete (ReceiverLog);
 		CRYPTO_cleanup_all_ex_data();
+		kill_locks();
 		ERR_remove_state(0);
 		ERR_free_strings();
 		EVP_cleanup();
@@ -166,6 +192,9 @@ int main(int argc, char **argv)  //main body of application
 		stopSCR<<"kill -SIGINT "<<proces_id<<std::endl;
 		stopSCR.close();
 	}
+	SSL_load_error_strings();
+	SSL_library_init();
+	OpenSSL_add_all_algorithms();
 	SenderLog = new Loger();
 	ReceiverLog = new Loger();
 	SenderLog->SetLogger(c->SenderVerbosity(),c->SenderMaxFiles(),c->SenderMaxLines(),c->SenderFileNaming(),c->SenderPath(),"SENDER",c->SenderToSTD());
@@ -188,6 +217,7 @@ int main(int argc, char **argv)  //main body of application
 	}
 	sslCont = new SSLContainer(ReceiverLog);
 	wpool = WorkerPool::CreatePool(ReceiverLog,SenderLog,connStr,c,sslCont);
+	init_locks();
 	if ((wpool==NULL)||(wpool->Limit()<=0))
 	{
 		SenderLog->WriteMessage(FATAL," [Main Process] 0 connections to DB unable to serve, terminating!");
@@ -224,7 +254,12 @@ int main(int argc, char **argv)  //main body of application
 		delete (wpool);
 		delete (sender);
 		delete (receiver);
-		//SenderThread->join();
+		CRYPTO_cleanup_all_ex_data();
+		kill_locks();
+		ERR_remove_state(0);
+		ERR_free_strings();
+		EVP_cleanup();
+		sem_destroy(&connectionSem);
 		delete (SenderThread);
 		delete (SenderLog);
 		delete (ReceiverLog);

ada_server/adaServerSender.cpp
@@ -24,7 +24,9 @@ AdaServerSender::AdaServerSender(sem_t *Sem, WorkerPool *workers, Loger *L,Confi
 
 AdaServerSender::~AdaServerSender()
 {
+	_log->WriteMessage(TRACE,"Entering " + this->_Name + "Destructor");
 	delete this->_L;
+	_log->WriteMessage(TRACE,"Exiting " + this->_Name + "Destructor");
 }
 
 void AdaServerSender::Start()

ada_server/connectionServer.cpp
@@ -12,6 +12,9 @@
 using namespace soci;
 using namespace pugi;
 
+
+
+
 void apps_ssl_info_callback()
 {
 	std::cout<<"CAllback called!"<<"\n";
@@ -20,9 +23,6 @@ void apps_ssl_info_callback()
 bool ConnectionServer::LoadCertificates()
 {
 	_log->WriteMessage(TRACE,"Entering " + this->_Name + "::LoadCertificates");
-	SSL_load_error_strings();
-	SSL_library_init();
-	OpenSSL_add_all_algorithms();
 	sslctx = SSL_CTX_new( TLSv1_server_method());
 	char errorBuffer[1000];
 	std::string msgBase = "Certificate error ";
@@ -159,14 +159,13 @@ void ConnectionServer::HandleConnection (in_addr IP)
 	int FMversion =  adapter.attribute("fw_version").as_int();
 	if ((CPversion == (float)0.1)||(CPversion == (float)1.0))  //according to version create parser
 	{
+		this->_log->WriteMessage(INFO,"Adapter protocol version is 1.0");
 		if (FMversion == 0)
 		{
 			MP = new ProtocolV1MessageParser(_log);
 		}
 		else
 		{
-			if (FMversion == 1)
-			{
 				try
 				{
 					MP = new ProtocolV1MessageParser(_log);
@@ -178,30 +177,23 @@ void ConnectionServer::HandleConnection (in_addr IP)
                     this->Cleanup();
 					return;
 				}
+		}
+	}
+		else
+		{
+			if (CPversion == (float)1.1)
+			{
+				this->_log->WriteMessage(INFO,"Adapter protocol version is 1.1");
+				MP = new ProtocolV1_1_MessageParser(_log);
 			}
 			else
 			{
-				try
-				{
-					MP = new ProtocolV1MessageParser(_log);
-				}
-				catch (std::exception &e)
-				{
-					_log->WriteMessage(ERR,"Unable to create space for Protocol parser exiting client won't be server!");
-					_log->WriteMessage(TRACE,"Exiting " + this->_Name + "::HandleConnection");
-                    this->Cleanup();
-					return;
-				}
+				_log->WriteMessage(WARN, "Unsupported protocol version");
+				_log->WriteMessage(TRACE, "Exiting " + this->_Name + "::HandleConnection");
+				this->Cleanup();
+				return;
 			}
 		}
-	}
-	else
-	{
-		_log->WriteMessage(WARN,"Unsupported protocol version");
-		_log->WriteMessage(TRACE,"Exiting " + this->_Name + "::HandleConnection");
-        this->Cleanup();
-		return;
-	}
 	if (!MP->ParseMessage(&adapter,FMversion,CPversion))  //parse message
 	{
 
@@ -215,85 +207,55 @@ void ConnectionServer::HandleConnection (in_addr IP)
 
 	if (parsedMessage->state==DATA) //on registration register agent
 	{
-		response = MP->CreateAnswer(this->GetData());
-		int Err;
-		_log->WriteMessage(MSG,"Response message: \n" + response);
-
-		// It was decided with adapter team, that at the end of each message will be \0
-		response.push_back('\0');
-
-		if((Err=SSL_write(cSSL, response.c_str(), response.size()))<=0)
-		{
-			_log->WriteMessage(ERR,"Writing to SSL failed :");
-		}
-		close(com_s);
+		this->GetData();
 	}
-  else if (parsedMessage->state==GET_PARAMS)
+    else
 	{
-    std::string xmlParameters = database->GetXmlDeviceParameters(std::to_string(this->parsedMessage->device_euid), this->parsedMessage->adapterINTid);
-		response = MP->CreateGetParametersAnswer(xmlParameters);
-		int Err;
-		_log->WriteMessage(MSG,"Response message: \n" + response);
-
-		// It was decided with adapter team, that at the end of each message will be \0
-		response.push_back('\0');
-
-		if((Err=SSL_write(cSSL, response.c_str(), response.size()))<=0)
+		if (parsedMessage->state == GET_PARAMS)
 		{
-			_log->WriteMessage(ERR,"Writing to SSL failed :");
-		}
-		close(com_s);
-	}
-	else if (parsedMessage->state==REGISTER)
-	{
-	    std::string resp;
-	    if (!database->IsInDB("gateway","gateway_id",std::to_string(parsedMessage->adapterINTid)))
-	    {
-			if (database->InsertGateway(this->parsedMessage))
-			{
-			  resp="<server_adapter protocol_version=\"1.0\" state=\"register\" response=\"true\"/>";
-			}
-			else
-			{
-			  resp="<server_adapter protocol_version=\"1.0\" state=\"register\" response=\"false\"/>";
-			}
-	    }
-	    else
-	    {
-	    	if (database->UpdateGateway(this->parsedMessage))
-			{
-			  	resp="<server_adapter protocol_version=\"1.0\" state=\"register\" response=\"true\"/>";
-			}
-			else
-			{
-			  	resp="<server_adapter protocol_version=\"1.0\" state=\"register\" response=\"false\"/>";
-			}
-
-	    }
-
-	    this->_sslcont->InsertSSL(parsedMessage->adapterINTid,cSSL);
-	    int Err;
-		_log->WriteMessage(INFO,"Going to send response to register MSG ");
-		_log->WriteMessage(INFO,"Response register MSG : " + resp);
-
-		// It was decided with adapter team, that at the end of each message will be \0
-	   	response.push_back('\0');
-
-	    if((Err=SSL_write(cSSL, resp.c_str(), resp.size()))<=0)
-		{
-			_log->WriteMessage(ERR,"Sending registration answer failed :");
+			this->GetData();
 		}
 		else
 		{
-			_log->WriteMessage(INFO,"Response msg successfully sent with number of bytes : " + std::to_string(Err));
+			if (parsedMessage->state == REGISTER)
+			{
+				if (!database->IsInDB("gateway", "gateway_id", std::to_string(parsedMessage->adapterINTid)))
+				{
+					this->parsedMessage->registerResult = database->InsertGateway(this->parsedMessage);
+				}
+				else
+				{
+					this->parsedMessage->registerResult = database->UpdateGateway(this->parsedMessage);
+				}
+
+				this->_sslcont->InsertSSL(parsedMessage->adapterINTid, cSSL);
+			}
 		}
-	    cSSL = SSL_new(sslctx);
 	}
-	if (parsedMessage->state==DATA) //we are receiving data so save them
+	int Err;
+	response = MP->CreateAnswer();
+	_log->WriteMessage(MSG,"Response message: \n" + response);
+
+	// It was decided with adapter team, that at the end of each message will be \0
+	response.push_back('\0');
+
+	if((Err=SSL_write(cSSL, response.c_str(), response.size()))<=0)
 	{
-		this->Notify(data);
-		this->StoreData();
-		database->LogValue(parsedMessage);
+		_log->WriteMessage(ERR,"Writing to SSL failed :");
+	}
+	if (parsedMessage->state==REGISTER)
+	{
+		cSSL = SSL_new(sslctx);
+	}
+	else
+	{
+		close(com_s);
+		if (parsedMessage->state==DATA) //we are receiving data so save them
+		{
+			this->Notify(data);
+			this->StoreData();
+			database->LogValue(parsedMessage);
+		}
 	}
 	this->Cleanup();
 	_log->WriteMessage(TRACE, "Exiting " + this->_Name + "::HandleConnection");
@@ -351,7 +313,10 @@ ConnectionServer::~ConnectionServer()
 	_log->WriteMessage(TRACE,"Entering " + this->_Name + "::Destructor");
 	if (cSSL != NULL)
 	{
-		close(SSL_get_fd(cSSL));
+		if (SSL_get_fd(cSSL)!= -1)
+		{
+			close (SSL_get_fd(cSSL));
+		}
 		SSL_shutdown(this->cSSL);
 		SSL_free(this->cSSL);
 	}
@@ -359,7 +324,7 @@ ConnectionServer::~ConnectionServer()
 	{
 		SSL_CTX_free(this->sslctx);
 	}
-	CRYPTO_cleanup_all_ex_data();
+	//CRYPTO_cleanup_all_ex_data();
 	ERR_remove_state(0);
 	ERR_free_strings();
 	EVP_cleanup();
@@ -397,20 +362,47 @@ void ConnectionServer::SetSocket (int s)
 int ConnectionServer::GetData()
 {
 	_log->WriteMessage(TRACE,"Entering " + this->_Name + "::GetData");
-	int wakeUpTime = 5;
-	if(!database->IsInDB("device","device_euid","'" + std::to_string(this->parsedMessage->device_euid) + "' AND gateway_id=" + std::to_string(this->parsedMessage->adapterINTid) + ""))
+	switch (this->parsedMessage->state)
 	{
-		wakeUpTime = 5;
+		case DATA:
+			if (!database->IsInDB("device", "device_euid",
+								  "'" + std::to_string(this->parsedMessage->device_euid) + "' AND gateway_id=" +
+								  std::to_string(this->parsedMessage->adapterINTid) + "")) {
+				this->parsedMessage->refresh = 5;
+			}
+			else
+			{
+				this->parsedMessage->refresh = database->GetWakeUpTime(std::to_string(this->parsedMessage->device_euid),
+													 this->parsedMessage->adapterINTid);
+			}
+			_log->WriteMessage(DEBUG, "refresh:" + std::to_string(this->parsedMessage->refresh));
+			if (this->parsedMessage->refresh > 100000)
+				this->parsedMessage->refresh = 10;
+			_log->WriteMessage(TRACE, "Exiting " + this->_Name + "::GetData");
+			return (0);
+		case GET_PARAMS:
+			for (size_t i=0;i<((messageV1_1*)this->parsedMessage)->params->size();i++)
+			{
+				switch (((messageV1_1 *) this->parsedMessage)->params->at(i)->id)
+				{
+					case 1001:
+						break;
+					case 1002:
+						break;
+					case 1000:
+						break;
+					case 1003:
+						database->GetDevices((messageV1_1 *) this->parsedMessage);
+						break;
+					default:
+						_log->WriteMessage(WARN, "Unsupported parameter id no routine to handle!");
+						break;
+				}
+			}
+			break;
+		default:
+			break;
 	}
-	else
-	{
-		wakeUpTime = database->GetWakeUpTime(std::to_string(this->parsedMessage->device_euid), this->parsedMessage->adapterINTid);
-	}
-	_log->WriteMessage(TRACE,"refresh:" + std::to_string(wakeUpTime));
-	if(wakeUpTime> 100000)
-		wakeUpTime = 10;
-	_log->WriteMessage(TRACE,"Exiting " + this->_Name + "::GetData");
-	return (wakeUpTime);
 }
 
 void ConnectionServer::Cleanup()
@@ -420,7 +412,7 @@ void ConnectionServer::Cleanup()
         delete (MP);
         MP=nullptr;
     }
-    CRYPTO_cleanup_all_ex_data();
+    //CRYPTO_cleanup_all_ex_data();
     ERR_free_strings();
     ERR_remove_state(0);
 }

ada_server/connectionServer.h
@@ -47,7 +47,7 @@ class ConnectionServer
 		int com_s;	/**< communication socket*/
 		DBHandler *database;    /**< reference to DBHandler object*/
 		MessageParser *MP;	/**< reference to MessageParser object*/
-		tmessage *parsedMessage; /**< reference to tmessage object for parsed meesage*/
+		tmessageV1_0 *parsedMessage; /**< reference to tmessageV1_0 object for parsed meesage*/
 		std::string response;		/**< text of response*/
 		 /**
 		   * Obtainng wakeup time

ada_server/listener.cpp
@@ -30,6 +30,7 @@ int Listener::Listen ()
 	{
 	  this->_log->WriteMessage(FATAL,"Error while binding socket");  //on error
 	  this->_log->WriteMessage(TRACE,"Exiting " + this->_Name + "::Listen");
+		this->_terminated = true;
 	  return (1);
 	}
 	else
@@ -40,6 +41,7 @@ int Listener::Listen ()
 	{
 		this->_log->WriteMessage(FATAL,"Unable to listen");
 		this->_log->WriteMessage(TRACE,"Exiting " + this->_Name + "::Listen");
+		this->_terminated = true;
 		return (1);
 	}
 	else
@@ -134,6 +136,8 @@ Listener::~Listener()
 
 void Listener::UnAccept()
 {
+	this->_log->WriteMessage(TRACE,"Entering " + this->_Name + "::UnAccept");
 	shutdown (s,SHUT_RDWR);
 	close(this->s);
+	this->_log->WriteMessage(TRACE,"Exiting " + this->_Name + "::UnAccept");
 }

commit 1fc99b693af3e165980c86123cc89fd78cd680ce

ada_server: fix crash when no devices found
The select of all devices seems to corrupt the initialized std::vector
when count == 0. Do not perform the second select to avoid this crash.

Fixes: 41dadcb31b5b ("commit ada_server")

Acked-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

ada_server/DBHandler.cpp
@@ -411,17 +411,24 @@ bool DBHandler::GetDevices(tmessageV1_1 *message)
 	size_t count = 0;
 	try
 	{
+		this->_log->WriteMessage (INFO, "select count(*)  from device where gateway_id = "
+                        + std::to_string(message->adapterINTid) + " and init = 1;");
+
 		*_sql << SQLQueries::SelectAllDevicesCount,
 				use(message->adapterINTid, "GATEWAY_ID"),
 				use(DEVICE_INITIALIZED, "DEVICE_INITIALIZED"),
 				into(count);
 		message->params->at(message->processedParams)->deviceList = new std::vector<unsigned long long>(count);
 		message->params->at(message->processedParams)->deviceIDList = new std::vector<unsigned long long>(count);
+
+		if (count > 0)
+		{
 		*_sql << SQLQueries::SelectAllDevices,
 				use(message->adapterINTid, "GATEWAY_ID"),
 				use(DEVICE_INITIALIZED, "DEVICE_INITIALIZED"),
 				into(*(message->params->at(message->processedParams)->deviceList)),
 				into(*(message->params->at(message->processedParams)->deviceIDList));
+		}
 		this->_log->WriteMessage(TRACE,"Exiting " + this->_Name + "::GetDevices");
 		return (true);
 	}