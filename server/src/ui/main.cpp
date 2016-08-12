BeeeOn/server

commit 97ea5f4a7a0a7dddabb60ddc1eb17198c997e534

ui_server: load logging configuration from file
Reviewed-by: Petr Stehlík <xstehl14@stud.fit.vutbr.cz>

server/src/ui/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <Poco/Logger.h>
 #include <Poco/Message.h>
+#include <Poco/File.h>
 #include <Poco/Util/Option.h>
 #include <Poco/Util/OptionSet.h>
 #include <Poco/Util/HelpFormatter.h>
@@ -16,6 +17,14 @@ using namespace BeeeOn;
 
 #define DEFAULT_PORT 8000
 
+#define LOCAL_CONFIG_FILE  "logging.ini"
+#define SYSTEM_CONFIG_FILE "/etc/beeeon/ui-server/logging.ini"
+
+static Option optLogging("logging", "l",
+		"logging configuration file to be used (xml, ini, properties)",
+		false,
+		"<file>",
+		true);
 static Option optPort("port", "p",
 		"server port to listen on",
 		false,
@@ -34,6 +43,8 @@ public:
 protected:
 	void handleOption(const string &name, const string &value)
 	{
+		if (name == "logging")
+			m_userLogging = value;
 		if (name == "help")
 			m_printHelp = true;
 		if (name == "port")
@@ -42,14 +53,30 @@ protected:
 		Application::handleOption(name, value);
 	}
 
+	void findAndLoadLogging()
+	{
+		File user(m_userLogging);
+		File local(LOCAL_CONFIG_FILE);
+		File system(SYSTEM_CONFIG_FILE);
+
+		if (!m_userLogging.empty() && user.exists())
+			loadConfiguration(user.path());
+		if (local.exists())
+			loadConfiguration(local.path());
+		else if (system.exists())
+			loadConfiguration(system.path());
+	}
+
 	void initialize(Application &app)
 	{
 		Logger::root().setLevel(Logger::parseLevel("trace"));
+		findAndLoadLogging();
 		Application::initialize(app);
 	}
 
 	void defineOptions(OptionSet &options)
 	{
+		options.addOption(optLogging);
 		options.addOption(optPort);
 		options.addOption(optHelp);
 		Application::defineOptions(options);
@@ -83,6 +110,7 @@ protected:
 private:
 	bool m_printHelp;
 	unsigned int m_serverPort;
+	string m_userLogging;
 };
 
 int main(int argc, char **argv)

server/src/logging.ini
@@ -1,0 +1,14 @@
+[logging]
+channels.console.class = ColorConsoleChannel
+channels.console.pattern = %s %H:%M:%S.%i %P [%p] %t (%U:%u)
+loggers.root.channel = console
+loggers.root.level = trace
+
+loggers.l0000.name = ApplicationStartup
+loggers.l0000.level = notice
+
+loggers.l0001.name = Application
+loggers.l0001.level = notice
+
+loggers.l0100.name = BeeeOn::Mongoose
+loggers.l0100.level = notice

commit cb5bbb2a8b735824ac650656d329437afde3a9b8

ui: user logging configuration overrides all
Configuration file passed explicitly on command line should
override the others.

Fixes: 97ea5f4a7a0a ("ui_server: load logging configuration from file")
Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>
Reviewed-by: Marek Beňo <xbenom01@stud.fit.vutbr.cz>
Reviewed-by: Petr Stehlík <xstehl14@stud.fit.vutbr.cz>

server/src/ui/main.cpp
@@ -72,7 +72,7 @@ protected:
 
 		if (!m_userLogging.empty() && user.exists())
 			loadConfiguration(user.path());
-		if (local.exists())
+		else if (local.exists())
 			loadConfiguration(local.path());
 		else if (system.exists())
 			loadConfiguration(system.path());