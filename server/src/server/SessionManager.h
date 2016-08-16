9d017438b276949181c8fbe257b2c00c1e158b27
Petr Stehlik <xstehl14@stud.fit.vutbr.cz>
2016-08-16 16:46:18 +0200

    Session: new class Session
    Signed-off-by: Petr Stehlik <xstehl14@stud.fit.vutbr.cz>
    Acked-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

--- server/src/server/SessionManager.h
+++ server/src/server/SessionManager.h
@@ -5,10 +5,46 @@
 
 #include "di/InjectorTarget.h"
 #include "provider/RandomProvider.h"
+#include "model/User.h"
 
 namespace BeeeOn {
 
 typedef std::string SessionID;
+typedef std::string Email;
+
+/**
+ * Base class to hold session data (userID, sessionID).
+ * Email is only for future use. Currently useless.
+ */
+class Session
+{
+public:
+	Session(const UserID &userID, const SessionID &sessionID) :
+		m_userID(userID),
+		m_sessionID(sessionID)
+	{
+	}
+
+	const Email email() const
+	{
+		return m_email;
+	}
+
+	const UserID userID() const
+	{
+		return m_userID;
+	}
+
+	const SessionID sessionID() const
+	{
+		return m_sessionID;
+	}
+
+private:
+	Email m_email;
+	UserID m_userID;
+	SessionID m_sessionID;
+};
 
 /**
  * A user session management.
