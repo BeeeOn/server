516330332122a9217f7ab7d1044d44bbeabeae50
Jan Viktorin <iviktorin@fit.vutbr.cz>
2016-08-16 12:58:59 +0200

    GatewayService: integrate RPC on gateways
    Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

--- server/src/service/GatewayService.h
+++ server/src/service/GatewayService.h
@@ -4,6 +4,7 @@
 #include <Poco/Exception.h>
 #include <Poco/Logger.h>
 #include "dao/GatewayDao.h"
+#include "rpc/GatewayRPC.h"
 #include "di/InjectorTarget.h"
 #include "Debug.h"
 
@@ -12,10 +13,13 @@ namespace BeeeOn {
 class GatewayService : public AbstractInjectorTarget {
 public:
 	GatewayService():
-		m_dao(&NullGatewayDao::instance())
+		m_dao(&NullGatewayDao::instance()),
+		m_rpc(&NullGatewayRPC::instance())
 	{
 		injector<GatewayService, GatewayDao>("gatewayDao",
 				&GatewayService::setGatewayDao);
+		injector<GatewayService, GatewayRPC>("gatewayRPC",
+				&GatewayService::setGatewayRPC);
 	}
 
 	void setGatewayDao(GatewayDao *dao)
@@ -26,14 +30,38 @@ public:
 			m_dao = dao;
 	}
 
+	void setGatewayRPC(GatewayRPC *rpc)
+	{
+		if (rpc == NULL)
+			m_rpc = &NullGatewayRPC::instance();
+		else
+			m_rpc = rpc;
+	}
+
 	bool fetch(Gateway &gateway)
 	{
 		TRACE_METHOD();
 		return m_dao->fetch(gateway);
 	}
 
+	void scanDevices(Gateway &gateway)
+	{
+		m_rpc->sendListen(gateway);
+	}
+
+	void unpairDevice(Gateway &gateway, Device &device)
+	{
+		m_rpc->unpairDevice(gateway, device);
+	}
+
+	void pingGateway(Gateway &gateway)
+	{
+		m_rpc->pingGateway(gateway);
+	}
+
 private:
 	GatewayDao *m_dao;
+	GatewayRPC *m_rpc;
 };
 
 }
