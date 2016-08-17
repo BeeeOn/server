BeeeOn/server

commit 01275862938ecf99e3cc6a763072ad43a4548ba4

Base64: new class
Provide Base64 utility functions encode and decode.

Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>
Reviewed-by: Petr Stehlík <xstehl14@stud.fit.vutbr.cz>

server/test/CMakeLists.txt
@@ -18,6 +18,8 @@ file(GLOB TEST_SOURCES
 	${PROJECT_SOURCE_DIR}/service/AuthServiceTest.cpp
 	${PROJECT_SOURCE_DIR}/provider/GoogleAuthProviderTest.cpp
 	${PROJECT_SOURCE_DIR}/util/DAMMTest.cpp
+	${PROJECT_SOURCE_DIR}/dao/SociManagerTest.cpp
+	${PROJECT_SOURCE_DIR}/util/Base64Test.cpp
 )
 
 include_directories(

server/src/util/Base64.h
@@ -1,0 +1,51 @@
+#ifndef BEEEON_BASE64_H
+#define BEEEON_BASE64_H
+
+#include <sstream>
+#include <Poco/Base64Encoder.h>
+#include <Poco/Base64Decoder.h>
+#include <Poco/StreamCopier.h>
+
+namespace BeeeOn {
+
+class Base64 {
+public:
+	static std::string encode(char *b, size_t blen)
+	{
+		std::ostringstream sOut;
+		Poco::Base64Encoder base64(sOut);
+		base64.write(b, blen);
+		base64.close();
+
+		return sOut.str();
+	}
+
+	static size_t decode(const std::string &s, char *b, size_t bcap)
+	{
+		std::istringstream sIn(s);
+		Poco::Base64Decoder base64(sIn);
+
+		for (size_t i = 0; i < bcap; ++i) {
+			b[i] = base64.get();
+
+			if (!base64.good() || base64.eof())
+				return i;
+		}
+
+		return bcap;
+	}
+
+	static std::string decode(const std::string &s)
+	{
+		std::istringstream sIn(s);
+		Poco::Base64Decoder base64(sIn);
+
+		std::string result;
+		base64 >> result;
+		return result;
+	}
+};
+
+}
+
+#endif

server/test/util/Base64Test.cpp
@@ -1,0 +1,64 @@
+#ifndef BEEEON_BASE64_TEST_H
+#define BEEEON_BASE64_TEST_H
+
+#include <cstring>
+#include <cppunit/extensions/HelperMacros.h>
+
+#include "util/Base64.h"
+
+using namespace std;
+
+namespace BeeeOn {
+
+class Base64Test : public CppUnit::TestFixture {
+	CPPUNIT_TEST_SUITE(Base64Test);
+	CPPUNIT_TEST(testEncode);
+	CPPUNIT_TEST(testDecode);
+	CPPUNIT_TEST_SUITE_END();
+public:
+	void testEncode();
+	void testDecode();
+};
+
+CPPUNIT_TEST_SUITE_REGISTRATION(Base64Test);
+
+void Base64Test::testEncode()
+{
+	char buf[32] = {
+		'\x05', '\x16', '\xab', '\xff', '\x12', '\x95', '\x49', '\x90',
+		'\x34', '\x44', '\x17', '\x47', '\x77', '\x78', '\x77', '\x10',
+		'\x00', '\xed', '\xcc', '\x10', '\x89', '\xa8', '\xaa', '\xa8',
+		'\x01', '\xdc', '\xc2', '\x47', '\x8b', '\x78', '\x77', '\x10',
+	};
+
+	const string expect =
+		"XHgwNVx4MTZceGFiXHhmZlx4MTJceDk"
+		"1XHg0OVx4OTBceDM0XHg0NFx4MTdceD"
+		"Q3XHg3N1x4NzhceDc3XHgxMFx4MDBce"
+		"GVkXHhjY1x4MTBceDg5XHhhOFx4YWFc"
+		"eGE4XHgwMVx4ZGNceGMyXHg0N1x4OGJ"
+		"ceDc4XHg3N1x4MTAK";
+
+	const string &result = Base64::encode(buf, sizeof(buf));
+	CPPUNIT_ASSERT(result.compare(expect));
+}
+
+void Base64Test::testDecode()
+{
+	const string data = "YW55IGFyYml0cmFyeSB0ZXN0aW5nIHRleHQK";
+	char buf[32];
+	size_t len;
+
+	const char expect[] = {
+		'a', 'n', 'y', ' ', 'a', 'r', 'b', 'i',
+		't', 'r', 'a', 'r', 'y', ' ', 't', 'e',
+		's', 't', 'i', 'n', 'g', ' ', 't', 'e', 'x', 't', '\n'};
+
+	len = Base64::decode(data, buf, sizeof(buf));
+	CPPUNIT_ASSERT_EQUAL(sizeof(expect), len);
+	CPPUNIT_ASSERT(std::memcmp(buf, expect, sizeof(expect)) == 0);
+}
+
+}
+
+#endif

commit dd3030593227263a7735524aa6be8b5b9acdb082

SociManager: new class
SociManager manages a pool of connections to a database. All soci
backends can be used if configured. The SociManager also optionally
executes an initialization SQL script. This is useful for deployment
to a new server, to execute update commands or to setup a testing
environment.

The SociManagerTest has accidently been included for building of
tests in 01275862938e.

Fixes: 01275862938e ("Base64: new class")
Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

server/src/CMakeLists.txt
@@ -31,6 +31,7 @@ file(GLOB SOURCES
 	${PROJECT_SOURCE_DIR}/service/GatewayService.cpp
 	${PROJECT_SOURCE_DIR}/service/LocationService.cpp
 	${PROJECT_SOURCE_DIR}/util/DAMM.cpp
+	${PROJECT_SOURCE_DIR}/dao/SociManager.cpp
 )
 
 include_directories(

server/src/dao/SociManager.cpp
@@ -1,0 +1,53 @@
+#include <Poco/FileStream.h>
+#include <Poco/StreamCopier.h>
+#include "dao/SociManager.h"
+
+using namespace std;
+using namespace soci;
+using namespace Poco;
+using namespace BeeeOn;
+
+connection_pool &SociManager::pool()
+{
+	if (m_pool != NULL)
+		return *m_pool;
+
+	m_logger.information("initialize database (%s, %u)",
+			m_backend, m_poolSize);
+
+	m_pool = new connection_pool(m_poolSize);
+
+	for (unsigned int i = 0; i < m_poolSize; ++i) {
+		session &db = m_pool->at(i);
+
+		m_logger.debug("connecting session %u", i);
+		db.open(m_backend, m_conn);
+	}
+
+	m_logger.notice("database connected");
+
+	return *m_pool;
+}
+
+void SociManager::injectionDone()
+{
+	if (m_script.empty()) {
+		m_logger.debug("no initialization script, skipping");
+		return;
+	}
+
+	m_logger.debug("executing initial script: " + m_script);
+
+	FileInputStream script(m_script);
+	string content;
+	StreamCopier::copyToString(script, content);
+
+	session db(pool());
+	db.begin();
+	db << (content);
+	db.commit();
+
+	m_logger.notice("database initialized successfully");
+}
+
+BEEEON_OBJECT(SociManager, BeeeOn::SociManager)

server/src/dao/SociManager.h
@@ -1,0 +1,103 @@
+#ifndef BEEEON_SOCI_MANAGER_H
+#define BEEEON_SOCI_MANAGER_H
+
+#include <map>
+#include <Poco/Exception.h>
+#include <Poco/Logger.h>
+#include <soci.h>
+
+#include "di/InjectorTarget.h"
+#include "Debug.h"
+
+namespace BeeeOn {
+
+/**
+ * Manager of connection via soci. It manages the connection
+ * pool. Use it as a singleton for other Soci-derrived s.
+ */
+class SociManager : public AbstractInjectorTarget {
+public:
+	SociManager():
+		m_logger(LOGGER_CLASS(this)),
+		m_backend(""),
+		m_conn(""),
+		m_poolSize(1),
+		m_script(""),
+		m_pool(NULL)
+	{
+		textInjector("backend", (TextSetter)
+				&SociManager::setBackend);
+		textInjector("connector", (TextSetter)
+				&SociManager::setConnector);
+		numberInjector("poolSize", (NumberSetter)
+				&SociManager::setPoolSize);
+		textInjector("initScript", (TextSetter)
+				&SociManager::setInitScript);
+	}
+
+	~SociManager()
+	{
+		if (m_pool != NULL)
+			delete m_pool;
+	}
+
+	/**
+	 * Set the target backend (sqlite3, postgresql, ...).
+	 */
+	void setBackend(const std::string &backend)
+	{
+		m_backend = backend;
+	}
+
+	/**
+	 * Connection string. Backend specific.
+	 */
+	void setConnector(const std::string conn)
+	{
+		m_conn = conn;
+	}
+
+	void setPoolSize(int poolSize)
+	{
+		if (poolSize < 1) {
+			throw Poco::InvalidArgumentException(
+				"poolSize must be greater then 0");
+		}
+
+		m_poolSize = poolSize;
+	}
+
+	/**
+	 * Initialization SQL script file path.
+	 */
+	void setInitScript(const std::string &script)
+	{
+		m_script = script;
+	}
+
+	unsigned int poolSize() const
+	{
+		return m_poolSize;
+	}
+
+	soci::connection_pool &pool();
+
+protected:
+	/**
+	 * After the SociManager is set up, try to initialize
+	 * database if there is an initialization script specified.
+	 */
+	void injectionDone();
+
+private:
+	Poco::Logger &m_logger;
+	std::string m_backend;
+	std::string m_conn;
+	unsigned int m_poolSize;
+	std::string m_script;
+	soci::connection_pool *m_pool;
+};
+
+}
+
+#endif

server/src/ui/logging.ini
@@ -18,3 +18,6 @@ loggers.l0101.level = notice
 
 loggers.l0200.name = BeeeOn::DependencyInjector
 loggers.l0200.level = notice
+
+loggers.l0300.name = BeeeOn::SociManager
+loggers.l0300.level = notice

server/src/ui/server.ini
@@ -11,3 +11,9 @@ maxPerUser = 10
 [google]
 ;client_secret =
 ;client_id =
+
+[database]
+backend = postgresql
+connector = dbname=home8-testing
+pool = 20
+init = ${config.dir}schema/home8-init.sql

server/src/ui/services.xml
@@ -70,5 +70,13 @@
 		<instance name="deviceDao" class="BeeeOn::MockDeviceDao" />
 		<instance name="gatewayDao" class="BeeeOn::MockGatewayDao" />
 		<instance name="locationDao" class="BeeeOn::MockLocationDao" />
+
+		<instance name="sociManager"
+				class="BeeeOn::SociManager">
+			<set name="backend" text="${database.backend}" />
+			<set name="connector" text="${database.connector}" />
+			<set name="poolSize" number="${database.pool}" />
+			<set name="initScript" text="${database.init}" />
+		</instance>
 	</services>
 </system>

server/test/CMakeLists.txt
@@ -18,8 +18,8 @@ file(GLOB TEST_SOURCES
 	${PROJECT_SOURCE_DIR}/service/AuthServiceTest.cpp
 	${PROJECT_SOURCE_DIR}/provider/GoogleAuthProviderTest.cpp
 	${PROJECT_SOURCE_DIR}/util/DAMMTest.cpp
-	${PROJECT_SOURCE_DIR}/dao/SociManagerTest.cpp
 	${PROJECT_SOURCE_DIR}/util/Base64Test.cpp
+	${PROJECT_SOURCE_DIR}/dao/SociManagerTest.cpp
 )
 
 include_directories(

server/test/dao/SociManagerTest.cpp
@@ -1,0 +1,69 @@
+#include <cppunit/extensions/HelperMacros.h>
+
+#include "dao/SociManager.h"
+
+using namespace std;
+using namespace soci;
+
+namespace BeeeOn {
+
+class SociManagerTest : public CppUnit::TestFixture {
+	CPPUNIT_TEST_SUITE(SociManagerTest);
+	CPPUNIT_TEST(testSelect);
+	CPPUNIT_TEST(testSelectEmpty);
+	CPPUNIT_TEST_SUITE_END();
+
+public:
+	void setUp();
+	void tearDown();
+	void testSelect();
+	void testSelectEmpty();
+
+private:
+	SociManager *m_manager;
+};
+
+CPPUNIT_TEST_SUITE_REGISTRATION(SociManagerTest);
+
+void SociManagerTest::setUp()
+{
+	m_manager = new SociManager();
+	m_manager->setBackend("sqlite3");
+	m_manager->setConnector(":memory:");
+	m_manager->setPoolSize(1);
+
+	session db(m_manager->pool());
+	db << "create table test ("
+		<< "  id integer primary key,"
+		<< "  name varchar(200),"
+		<< "  valid boolean"
+		<< ");";
+	db << "insert into test (id, name, valid) values (1, 'first', 1)";
+}
+
+void SociManagerTest::tearDown()
+{
+	delete m_manager;
+}
+
+void SociManagerTest::testSelect()
+{
+	session db(m_manager->pool());
+	row r;
+
+	db << "select * from test where id = 1", into(r);
+
+	CPPUNIT_ASSERT_EQUAL(1, r.get<int>("id"));
+	CPPUNIT_ASSERT(r.get<string>("name").compare("first") == 0);
+	CPPUNIT_ASSERT(r.get<int>("valid"));
+}
+
+void SociManagerTest::testSelectEmpty()
+{
+	session db(m_manager->pool());
+
+	rowset<row> rs = db.prepare << "select * from test where id = 2;";
+	CPPUNIT_ASSERT(rs.begin() == rs.end());
+}
+
+}

server/test/logging.ini
@@ -18,3 +18,6 @@ loggers.l0300.level = notice
 
 loggers.l0400.name = BeeeOn::SessionManager
 loggers.l0400.level = notice
+
+loggers.l0500.name = BeeeOn::SociManager
+loggers.l0500.level = warning