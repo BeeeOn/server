BeeeOn/server

commit a9566b30de4a242c006864c44e574cb1568e9d82

GatewayID: new class with test fixture
Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

server/src/model/GatewayID.cpp
@@ -1,0 +1,79 @@
+#include <Poco/Ascii.h>
+#include <Poco/Random.h>
+#include <Poco/Exception.h>
+
+#include "util/DAMM.h"
+#include "model/GatewayID.h"
+
+using namespace std;
+using namespace Poco;
+using namespace BeeeOn;
+
+GatewayID::GatewayID(int version, uint64_t data)
+{
+	if (version <= 0 || version >= 10) {
+		throw InvalidArgumentException(
+			"invalid version, must be: 1 < version < 10");
+	}
+
+	string tmp;
+	tmp.append(to_string(version));
+	tmp.append(to_string(data));
+	tmp.append(to_string(DAMM::compute(tmp)));
+
+	m_value = stol(tmp);
+}
+
+GatewayID GatewayID::parse(const string &s)
+{
+	if (s.size() != GatewayID::LENGTH) {
+		throw InvalidArgumentException(
+			"expected string of length "
+			+ std::to_string(LENGTH));
+	}
+
+	if (!Ascii::isDigit(s.at(0))) {
+		throw InvalidArgumentException(
+			"version must be a decimal digit");
+	}
+
+	const char version = s.at(0) - '0';
+	if (version <= 0 || version >= 10) {
+		throw InvalidArgumentException(
+			"invalid version, must be: 1 < version < 10");
+	}
+
+	const long data = stol(s.substr(1, s.size() - 2));
+	if (data < 0) {
+		throw InvalidArgumentException(
+			"invalid data, must not be negative");
+	}
+
+	if (!Ascii::isDigit(s.at(s.size() - 1))) {
+		throw InvalidArgumentException(
+			"check digit must be a decimal digit");
+	}
+
+	const int sum = s.at(s.size() - 1) - '0';
+	const int result = DAMM::compute(s.substr(0, s.size() - 1));
+	if (result != sum)
+		throw InvalidArgumentException("invalid checksum");
+
+	return GatewayID(version, data);
+}
+
+GatewayID GatewayID::random(int version)
+{
+	Random rnd;
+	rnd.seed();
+
+	uint64_t data = 0;
+
+	for (int i = 0; i < LENGTH - 2; ++i) {
+		int c = rnd.nextChar();
+		data *= 10;
+		data += c;
+	}
+
+	return GatewayID(version, data);
+}

server/src/CMakeLists.txt
@@ -19,6 +19,7 @@ file(GLOB SOURCES
 	${PROJECT_SOURCE_DIR}/provider/RandomProvider.cpp
 	${PROJECT_SOURCE_DIR}/provider/GoogleAuthProvider.cpp
 	${PROJECT_SOURCE_DIR}/server/SessionManager.cpp
+	${PROJECT_SOURCE_DIR}/model/GatewayID.cpp
 	${PROJECT_SOURCE_DIR}/dao/PlaceDao.cpp
 	${PROJECT_SOURCE_DIR}/dao/UserDao.cpp
 	${PROJECT_SOURCE_DIR}/dao/DeviceDao.cpp

server/src/model/GatewayID.h
@@ -1,0 +1,87 @@
+#ifndef BEEEON_GATEWAY_ID_H
+#define BEEEON_GATEWAY_ID_H
+
+#include <cstdint>
+#include <Poco/Exception.h>
+
+namespace BeeeOn {
+
+class GatewayID {
+public:
+	enum {
+		LENGTH = 16
+	};
+
+	GatewayID():
+		m_value(0)
+	{
+	}
+
+	GatewayID(uint64_t value):
+		m_value(value)
+	{
+	}
+
+	GatewayID(int version, uint64_t data);
+
+	bool isNull() const
+	{
+		return m_value == 0;
+	}
+
+	int version() const
+	{
+		return std::to_string(m_value).at(0) - '0';
+	}
+
+	unsigned long data() const
+	{
+		return std::stol(std::to_string(m_value / 10).substr(1));
+	}
+
+	static GatewayID parse(const std::string &s);
+
+	std::string toString() const
+	{
+		return std::to_string(m_value);
+	}
+
+	bool operator !=(const GatewayID &id) const
+	{
+		return m_value != id.m_value;
+	}
+
+	bool operator ==(const GatewayID &id) const
+	{
+		return m_value == id.m_value;
+	}
+
+	bool operator <(const GatewayID &id) const
+	{
+		return m_value < id.m_value;
+	}
+
+	bool operator >(const GatewayID &id) const
+	{
+		return m_value > id.m_value;
+	}
+
+	bool operator <=(const GatewayID &id) const
+	{
+		return m_value <= id.m_value;
+	}
+
+	bool operator >=(const GatewayID &id) const
+	{
+		return m_value >= id.m_value;
+	}
+
+	static GatewayID random(int version = 1);
+
+private:
+	uint64_t m_value;
+};
+
+}
+
+#endif

server/test/CMakeLists.txt
@@ -14,6 +14,7 @@ file(GLOB TEST_SOURCES
 	${PROJECT_SOURCE_DIR}/server/SessionManagerTest.cpp
 	${PROJECT_SOURCE_DIR}/di/DependencyInjectorTest.cpp
 	${PROJECT_SOURCE_DIR}/model/SerializerTest.cpp
+	${PROJECT_SOURCE_DIR}/model/GatewayIDTest.cpp
 	${PROJECT_SOURCE_DIR}/service/AuthServiceTest.cpp
 	${PROJECT_SOURCE_DIR}/provider/GoogleAuthProviderTest.cpp
 	${PROJECT_SOURCE_DIR}/util/DAMMTest.cpp

server/test/model/GatewayIDTest.cpp
@@ -1,0 +1,47 @@
+#include <cppunit/extensions/HelperMacros.h>
+
+#include "model/GatewayID.h"
+
+namespace BeeeOn {
+
+class GatewayIDTest : public CppUnit::TestFixture {
+	CPPUNIT_TEST_SUITE(GatewayIDTest);
+	CPPUNIT_TEST(testCreate);
+	CPPUNIT_TEST(testParse);
+	CPPUNIT_TEST_SUITE_END();
+public:
+	void setUp();
+	void tearDown();
+	void testCreate();
+	void testParse();
+};
+
+CPPUNIT_TEST_SUITE_REGISTRATION(GatewayIDTest);
+
+void GatewayIDTest::setUp()
+{
+}
+
+void GatewayIDTest::tearDown()
+{
+}
+
+void GatewayIDTest::testCreate()
+{
+	const GatewayID id(1, 61762224815122);
+
+	CPPUNIT_ASSERT_EQUAL(1, id.version());
+	CPPUNIT_ASSERT_EQUAL(61762224815122UL, id.data());
+	CPPUNIT_ASSERT(id.toString().compare("1617622248151223") == 0);
+}
+
+void GatewayIDTest::testParse()
+{
+	const GatewayID &id = GatewayID::parse("1617622248151223");
+
+	CPPUNIT_ASSERT_EQUAL(1, id.version());
+	CPPUNIT_ASSERT_EQUAL(61762224815122UL, id.data());
+	CPPUNIT_ASSERT(id.toString().compare("1617622248151223") == 0);
+}
+
+}

commit a8342716f5071d1c93f0eb4467ad162363ed6322

GatewayID: fix printing leading zeros
Leading zeros have been omitted when constructing a new GatewayID.
That was leading to IDs of invalid lengths.

Fixes: a9566b30de4a ("GatewayID: new class with test fixture")
Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

server/src/model/GatewayID.cpp
@@ -1,6 +1,7 @@
 #include <Poco/Ascii.h>
 #include <Poco/Random.h>
 #include <Poco/Exception.h>
+#include <Poco/Format.h>
 
 #include "util/DAMM.h"
 #include "model/GatewayID.h"
@@ -18,7 +19,7 @@ GatewayID::GatewayID(int version, uint64_t data)
 
 	string tmp;
 	tmp.append(to_string(version));
-	tmp.append(to_string(data));
+	tmp.append(format("%014Lu", (Poco::UInt64) data));
 	tmp.append(to_string(DAMM::compute(tmp)));
 
 	try {