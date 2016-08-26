420ebaa086eaabf9293597bd12f078a6f16bb5d9
Jan Viktorin <iviktorin@fit.vutbr.cz>
2016-08-26 12:42:32 +0200

    SessionCache: move implementation to cpp file
    Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

---
+++ server/src/server/SessionCache.cpp
@@ -0,0 +1,77 @@
+#include <Poco/Exception.h>
+
+#include "server/SessionCache.h"
+
+using namespace Poco;
+using namespace BeeeOn;
+
+SessionCache::SessionCache(unsigned int maxUserSessions)
+{
+	m_userSessions = maxUserSessions;
+}
+
+SessionCache::~SessionCache()
+{
+	m_userSessionSet.clear();
+}
+
+/**
+ * Adds new session with given sessionID to session table and creates
+ * new reverse record to map existing user's session.
+ */
+void SessionCache::add(const SessionID &sessionID, ExpirableSession &session)
+{
+	UserID userID = session.userID();
+	auto userSession = m_userSessionSet.find(userID);
+
+	if (userSession == m_userSessionSet.end()) {
+		std::set<SessionID> sessionSet;
+		sessionSet.emplace(sessionID);
+		m_userSessionSet.emplace(std::make_pair(userID, sessionSet));
+	} else {
+		if (userSession->second.size() > m_userSessions)
+			throw IllegalStateException("Max session size reached");
+
+		userSession->second.emplace(sessionID);
+	}
+
+	doAdd(sessionID, session);
+}
+
+/**
+ * Removes session from session and user's session tables.
+ */
+void SessionCache::remove(const SessionID &sessionID)
+{
+	auto it = _data.find(sessionID);
+
+	if (it != _data.end())
+	{
+		UserID userID = it->second->userID();
+
+		// Remove session from user map
+		removeSession(sessionID, userID);
+
+		// Remove session from session cache
+		doRemove(it);
+	}
+}
+
+/**
+ * Removes session record from set in reverse user session map
+ */
+void SessionCache::removeSession(const SessionID &sessionID, const UserID &userID)
+{
+	auto itUser = m_userSessionSet.find(userID);
+
+	if (itUser != m_userSessionSet.end()) {
+		auto itSession = itUser->second.find(sessionID);
+
+		if (itSession != itUser->second.end())
+			itUser->second.erase(itSession);
+
+		if (itUser->second.size() == 0)
+			m_userSessionSet.erase(itUser);
+	}
+}
+
--- server/src/server/SessionCache.h
+++ server/src/server/SessionCache.h
@@ -24,75 +24,24 @@ class SessionCache : public Poco::AbstractCache<SessionID,
 	Poco::UniqueExpireStrategy<SessionID, ExpirableSession>>
 {
 public:
-	SessionCache(unsigned int maxUserSessions)
-	{
-		m_userSessions = maxUserSessions;
-	}
-
-	~SessionCache()
-	{
-		m_userSessionSet.clear();
-	}
+	SessionCache(unsigned int maxUserSessions);
+	~SessionCache();
 
 	/**
 	 * Adds new session with given sessionID to session table and creates
 	 * new reverse record to map existing user's session.
 	 */
-	void add(const SessionID &sessionID, ExpirableSession &session)
-	{
-		UserID userID = session.userID();
-		auto userSession = m_userSessionSet.find(userID);
-
-		if (userSession == m_userSessionSet.end()) {
-			std::set<SessionID> sessionSet;
-			sessionSet.emplace(sessionID);
-			m_userSessionSet.emplace(std::make_pair(userID, sessionSet));
-		} else {
-			if (userSession->second.size() > m_userSessions)
-				throw Poco::IllegalStateException("Max session size reached");
-
-			userSession->second.emplace(sessionID);
-		}
-
-		doAdd(sessionID, session);
-	}
+	void add(const SessionID &sessionID, ExpirableSession &session);
 
 	/**
 	 * Removes session from session and user's session tables.
 	 */
-	void remove(const SessionID &sessionID)
-	{
-		auto it = _data.find(sessionID);
-
-		if (it != _data.end())
-		{
-			UserID userID = it->second->userID();
-
-			// Remove session from user map
-			removeSession(sessionID, userID);
-
-			// Remove session from session cache
-			doRemove(it);
-		}
-	}
+	void remove(const SessionID &sessionID);
 
 	/**
 	 * Removes session record from set in reverse user session map
 	 */
-	void removeSession(const SessionID &sessionID, const UserID &userID)
-	{
-		auto itUser = m_userSessionSet.find(userID);
-
-		if (itUser != m_userSessionSet.end()) {
-			auto itSession = itUser->second.find(sessionID);
-
-			if (itSession != itUser->second.end())
-				itUser->second.erase(itSession);
-
-			if (itUser->second.size() == 0)
-				m_userSessionSet.erase(itUser);
-		}
-	}
+	void removeSession(const SessionID &sessionID, const UserID &userID);
 
 private:
 	std::map<UserID, std::set<SessionID>> m_userSessionSet;
