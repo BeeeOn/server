BeeeOn/server

commit 813c35c69461e3e6d20729b17462820a339c7264

ui_server: new service GatewayService
Reviewed-by: Petr Stehlík <xstehl14@stud.fit.vutbr.cz>

server/src/service/GatewayService.h
@@ -1,0 +1,37 @@
+#ifndef BEEEON_GATEWAY_SERVICE_H
+#define BEEEON_GATEWAY_SERVICE_H
+
+#include <Poco/Exception.h>
+#include <Poco/Logger.h>
+#include "dao/GatewayDao.h"
+#include "di/InjectorTarget.h"
+#include "Debug.h"
+
+namespace BeeeOn {
+
+class GatewayService : public AbstractInjectorTarget {
+public:
+	GatewayService()
+	{
+		injector<GatewayService, GatewayDao>("gatewayDao",
+				&GatewayService::setGatewayDao);
+	}
+
+	void setGatewayDao(GatewayDao *dao)
+	{
+		m_dao = dao;
+	}
+
+	Gateway::Ptr get(GatewayID id, PlaceID placeId)
+	{
+		TRACE_METHOD();
+		return m_dao->get(id);
+	}
+
+private:
+	GatewayDao *m_dao;
+};
+
+}
+
+#endif

server/src/CMakeLists.txt
@@ -19,9 +19,11 @@ file(GLOB SOURCES
 	${PROJECT_SOURCE_DIR}/server/SessionManager.cpp
 	${PROJECT_SOURCE_DIR}/dao/UserDao.cpp
 	${PROJECT_SOURCE_DIR}/dao/DeviceDao.cpp
+	${PROJECT_SOURCE_DIR}/dao/GatewayDao.cpp
 	${PROJECT_SOURCE_DIR}/service/UserService.cpp
 	${PROJECT_SOURCE_DIR}/service/AuthService.cpp
 	${PROJECT_SOURCE_DIR}/service/DeviceService.cpp
+	${PROJECT_SOURCE_DIR}/service/GatewayService.cpp
 )
 
 include_directories(

server/src/dao/GatewayDao.cpp
@@ -1,0 +1,4 @@
+#include "dao/GatewayDao.h"
+
+BEEEON_OBJECT(NullGatewayDao, BeeeOn::NullGatewayDao)
+BEEEON_OBJECT(MockGatewayDao, BeeeOn::MockGatewayDao)

server/src/dao/GatewayDao.h
@@ -1,0 +1,27 @@
+#ifndef BEEEON_GATEWAY_DAO_H
+#define BEEEON_GATEWAY_DAO_H
+
+#include "di/InjectorTarget.h"
+#include "dao/NullDao.h"
+#include "dao/MockDao.h"
+#include "model/Place.h"
+#include "model/Gateway.h"
+
+namespace BeeeOn {
+
+class GatewayDao {
+public:
+	virtual Gateway::Ptr get(GatewayID id) = 0;
+};
+
+class NullGatewayDao : public AbstractInjectorTarget,
+	public NullDao<Gateway, GatewayDao> {
+};
+
+class MockGatewayDao : public AbstractInjectorTarget,
+	public MockDao<Gateway, GatewayDao> {
+};
+
+}
+
+#endif

server/src/model/Gateway.h
@@ -1,0 +1,33 @@
+#ifndef BEEEON_GATEWAY_H
+#define BEEEON_GATEWAY_H
+
+#include <Poco/SharedPtr.h>
+#include "model/Entity.h"
+
+namespace BeeeOn {
+
+/**
+ * Representation of a Gateway application entity.
+ */
+class Gateway : public Entity {
+public:
+	typedef Poco::SharedPtr<Gateway> Ptr;
+	typedef long ID;
+
+	Gateway(const std::string &data):
+		Entity(data)
+	{
+	}
+
+	Gateway(const Gateway &gateway):
+		Entity(gateway)
+	{
+	}
+};
+
+typedef Gateway::Collection GatewayCollection;
+typedef Gateway::ID GatewayID;
+
+}
+
+#endif

server/src/service/GatewayService.cpp
@@ -1,0 +1,3 @@
+#include "service/GatewayService.h"
+
+BEEEON_OBJECT(GatewayService, BeeeOn::GatewayService)

server/src/ui/UIServerModule.h
@@ -11,6 +11,7 @@
 #include "service/UserService.h"
 #include "service/AuthService.h"
 #include "service/DeviceService.h"
+#include "service/GatewayService.h"
 
 /**
  * Compilation-time configuration of the target underlying
@@ -88,6 +89,8 @@ public:
 				&UIServerModule::setAuthService);
 		injector<UIServerModule, DeviceService>("deviceService",
 				&UIServerModule::setDeviceService);
+		injector<UIServerModule, GatewayService>("gatewayService",
+				&UIServerModule::setGatewayService);
 	}
 
 	~UIServerModule() {
@@ -150,6 +153,16 @@ public:
 		return *m_deviceService;
 	}
 
+	void setGatewayService(GatewayService *service)
+	{
+		m_gatewayService = service;
+	}
+
+	GatewayService &gatewayService()
+	{
+		return *m_gatewayService;
+	}
+
 private:
 	Poco::SharedPtr<UIServerRequestHandlerFactory> m_factory;
 	UIRestServer *m_server;
@@ -157,6 +170,7 @@ private:
 	UserService *m_userService;
 	AuthService *m_authService;
 	DeviceService *m_deviceService;
+	GatewayService *m_gatewayService;
 };
 
 }

server/src/ui/services.xml
@@ -5,6 +5,7 @@
 			<set name="userService" ref="userService" />
 			<set name="authService" ref="authService" />
 			<set name="deviceService" ref="deviceService" />
+			<set name="gatewayService" ref="gatewayService" />
 		</instance>
 
 		<!-- Bussiness layer -->
@@ -21,6 +22,10 @@
 
 		<instance name="deviceService" class="BeeeOn::DeviceService">
 			<set name="deviceDao" ref="deviceDao" />
+		</instance>
+
+		<instance name="gatewayService" class="BeeeOn::GatewayService">
+			<set name="gatewayDao" ref="gatewayDao" />
 		</instance>
 
 		<!-- Data access layer -->
@@ -48,5 +53,6 @@
 
 		<instance name="userDao" class="BeeeOn::MockUserDao" />
 		<instance name="deviceDao" class="BeeeOn::MockDeviceDao" />
+		<instance name="gatewayDao" class="BeeeOn::MockGatewayDao" />
 	</services>
 </system>

commit 397db0532b241931e38bc0c90478cfbc69f68cbb

GatewayService: preset m_dao to NullGatewayDao singleton
Avoid cppcheck report:

warning	uninitMemberVar:
	Member variable 'GatewayService::m_dao' is not
	initialized in the constructor.

Fixes: 813c35c69461 ("ui_server: new service GatewayService")
Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

server/src/service/GatewayService.h
@@ -11,7 +11,8 @@ namespace BeeeOn {
 
 class GatewayService : public AbstractInjectorTarget {
 public:
-	GatewayService()
+	GatewayService():
+		m_dao(&NullGatewayDao::instance())
 	{
 		injector<GatewayService, GatewayDao>("gatewayDao",
 				&GatewayService::setGatewayDao);
@@ -19,7 +20,10 @@ public:
 
 	void setGatewayDao(GatewayDao *dao)
 	{
-		m_dao = dao;
+		if (dao == NULL)
+			m_dao = &NullGatewayDao::instance();
+		else
+			m_dao = dao;
 	}
 
 	bool fetch(Gateway &gateway)