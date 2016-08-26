BeeeOn/server

commit 1e4d361e449062a6f33492bdb0638bd96f300ce5

SessionManager: reimplement using SessionCache
Signed-off-by: Petr Stehlik <xstehl14@stud.fit.vutbr.cz>
Acked-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

server/src/server/SessionManager.cpp
@@ -10,58 +10,65 @@ BEEEON_OBJECT(SessionManager, BeeeOn::SessionManager)
 
 const SessionID SessionManager::open(const User &user)
 {
-	if (user.email().empty())
-		throw InvalidArgumentException("missing user e-mail");
+	TRACE_METHOD();
+
+	if (user.id().isNull()) {
+		if (m_logger.debug()) {
+			m_logger.debug("Missing user ID for: " + user.email(),
+					__FILE__, __LINE__);
+		}
+
+		throw InvalidArgumentException("missing user ID");
+	}
+
+	Timespan timespan(m_expireTime, 0);
+	char bSessionID[ID_LENGTH64];
 
 	// lock here for the rest of the method
 	RWLock::ScopedLock guard(m_lock, true);
 
-	IDTable::const_iterator it;
-	it = m_idTable.find(user.email());
+	m_random->randomBytesUnlocked(bSessionID, sizeof(bSessionID));
+	SessionID sessionID = Base64::encode(bSessionID, sizeof(bSessionID));
 
-	if (it != m_idTable.end())
-		throw ExistsException("session already exists");
+	ExpirableSession session(user.id(), sessionID, timespan);
 
 	char b[ID_LENGTH64];
 	m_random->randomBytesUnlocked(b, sizeof(b));
 	SessionID id = Base64::encode(b, sizeof(b));
-	m_idTable.insert(make_pair(user.email(), id));
-	m_table.insert(make_pair(id, user));
+	m_sessionCache->add(session.sessionID(), session);
 
-	return m_idTable.find(user.email())->second;
+	if (m_logger.debug()) {
+		m_logger.debug("Added session: " + session.sessionID(),
+				__FILE__, __LINE__);
+	}
+
+	return session.sessionID();
 }
 
-bool SessionManager::lookup(const SessionID &id, User &user)
+bool SessionManager::lookup(const SessionID &id, SessionPtr &session)
 {
+	TRACE_METHOD();
+
+	if (m_logger.debug())
+		m_logger.debug("Looking up session: " + id, __FILE__, __LINE__);
+
 	RWLock::ScopedLock guard(m_lock);
 
-	Table::const_iterator it;
-	it = m_table.find(id);
+	session = m_sessionCache->get(id);
 
-	if (it == m_table.end())
+	if (session.isNull())
 		return false;
 
-	user = it->second;
 	return true;
 }
 
 void SessionManager::close(const SessionID &id)
 {
+	TRACE_METHOD();
+
+	if (m_logger.debug())
+		m_logger.debug("Closing session: " + id, __FILE__, __LINE__);
+
 	RWLock::ScopedLock guard(m_lock, true);
-
-	Table::const_iterator it;
-	it = m_table.find(id);
-
-	if (it == m_table.end())
-		return;
-
-	const User &user = it->second;
-
-	IDTable::const_iterator email;
-	email = m_idTable.find(user.email());
-	if (email == m_idTable.end())
-		return;
-
-	m_table.erase(it);
-	m_idTable.erase(email);
+	m_sessionCache->remove(id);
 }

server/src/server/SessionManager.h
@@ -1,12 +1,20 @@
 #ifndef BEEEON_NULL_SESSION_MANAGER_H
 #define BEEEON_NULL_SESSION_MANAGER_H
+
 #include <map>
+
 #include <Poco/RWLock.h>
+#include <Poco/AbstractCache.h>
+#include <Poco/UniqueExpireStrategy.h>
+#include <Poco/ExpirationDecorator.h>
 #include <Poco/Timespan.h>
+#include <Poco/SharedPtr.h>
+#include <Poco/Logger.h>
 
 #include "di/InjectorTarget.h"
 #include "provider/RandomProvider.h"
 #include "model/User.h"
+#include "Debug.h"
 
 namespace BeeeOn {
 
@@ -71,19 +79,111 @@ private:
 	Poco::Timestamp m_tstamp;
 };
 
+/*
+ * SessionCache holds both session and its reverse records for each session
+ * in relation with its user (by their UserID).
+ *
+ * Extends Poco's UniqueExpireCache.
+ *
+ * Limits each user to have MAX_USER_SESSIONS to prevent DoS attacks.
+ *
+ * This class is NOT thread-safe
+ */
+class SessionCache : public Poco::AbstractCache<SessionID,
+	ExpirableSession,
+	Poco::UniqueExpireStrategy<SessionID, ExpirableSession>>
+{
+public:
+	SessionCache(unsigned int maxUserSessions)
+	{
+		m_userSessions = maxUserSessions;
+	}
+
+	~SessionCache()
+	{
+		m_userSessionSet.clear();
+	}
+
+	/**
+	 * Adds new session with given sessionID to session table and creates
+	 * new reverse record to map existing user's session.
+	 */
+	void add(const SessionID &sessionID, ExpirableSession &session)
+	{
+		UserID userID = session.userID();
+		auto userSession = m_userSessionSet.find(userID);
+
+		if (userSession == m_userSessionSet.end()) {
+			std::set<SessionID> sessionSet;
+			sessionSet.emplace(sessionID);
+			m_userSessionSet.emplace(std::make_pair(userID, sessionSet));
+		} else {
+			if (userSession->second.size() > m_userSessions)
+				throw Poco::IllegalStateException("Max session size reached");
+
+			userSession->second.emplace(sessionID);
+		}
+
+		doAdd(sessionID, session);
+	}
+
+	/**
+	 * Removes session from session and user's session tables.
+	 */
+	void remove(const SessionID &sessionID)
+	{
+		auto it = _data.find(sessionID);
+
+		if (it != _data.end())
+		{
+			UserID userID = it->second->userID();
+
+			// Remove session from user map
+			removeSession(sessionID, userID);
+
+			// Remove session from session cache
+			doRemove(it);
+		}
+	}
+
+	/**
+	 * Removes session record from set in reverse user session map
+	 */
+	void removeSession(const SessionID &sessionID, const UserID &userID)
+	{
+		auto itUser = m_userSessionSet.find(userID);
+
+		if (itUser != m_userSessionSet.end()) {
+			auto itSession = itUser->second.find(sessionID);
+
+			if (itSession != itUser->second.end())
+				itUser->second.erase(itSession);
+
+			if (itUser->second.size() == 0)
+				m_userSessionSet.erase(itUser);
+		}
+	}
+
+private:
+	std::map<UserID, std::set<SessionID>> m_userSessionSet;
+	Poco::RWLock m_lock;
+	unsigned int m_userSessions;
+};
+
 /**
  * A user session management.
  */
-class SessionManager : public AbstractInjectorTarget {
+class SessionManager : public AbstractInjectorTarget
+{
 public:
-	typedef std::map<SessionID, User> Table;
-	typedef std::map<std::string, SessionID> IDTable;
+	typedef Poco::SharedPtr<ExpirableSession> SessionPtr;
 
 	enum {
 		ID_LENGTH64 = 64, /**< 512 bits long session ID */
 	};
 
-	SessionManager()
+	SessionManager() :
+		m_logger(LOGGER_CLASS(this))
 	{
 		injector<SessionManager, SecureRandomProvider>(
 			"secureRandomProvider",
@@ -92,6 +192,11 @@ public:
 				(NumberSetter) &SessionManager::setSessionExpireTime);
 		numberInjector("maxUserSessions",
 				(NumberSetter) &SessionManager::setMaxUserSessions);
+	}
+
+	~SessionManager()
+	{
+		delete m_sessionCache;
 	}
 
 	void setSecureRandomProvider(SecureRandomProvider *provider)
@@ -106,6 +211,7 @@ public:
 
 	void setMaxUserSessions(const int maxUserSessions)
 	{
+		m_sessionCache = new SessionCache(maxUserSessions);
 	}
 
 	/**
@@ -120,7 +226,7 @@ public:
 	 * Lookup a session information by ID.
 	 * Returns false if there is no such session.
 	 */
-	bool lookup(const SessionID &id, User &user);
+	bool lookup(const SessionID &id, SessionPtr &info);
 
 	/**
 	 * Close session of the given ID.
@@ -129,16 +235,10 @@ public:
 	void close(const SessionID &id);
 
 private:
-	/**
-	 * Table: session ID -> Info.
-	 */
-	Table m_table;
-	/**
-	 * Reverse table: e-mail -> session ID.
-	 */
-	IDTable m_idTable;
 	Poco::RWLock m_lock;
+	Poco::Logger &m_logger;
 	SecureRandomProvider *m_random;
+	SessionCache *m_sessionCache;
 	unsigned int m_expireTime;
 };
 

server/test/server/SessionManagerTest.cpp
@@ -51,17 +51,17 @@ void SessionManagerTest::testOpenClose()
 
 	m_manager.setSecureRandomProvider(&randomProvider);
 
-	User user;
-	user.setEmail("test@example.org");
+	UserID userID = UserID::parse("824b4831-6ce4-4614-8e02-8380d6d92f95");
+	User user(userID);
 
 	SessionID id = m_manager.open(user);
 	CPPUNIT_ASSERT(Base64::decode(id).compare(SESSION_ID64) == 0);
 
-	User userLookup;
-	CPPUNIT_ASSERT(m_manager.lookup(id, userLookup));
+	SessionManager::SessionPtr infoLookup;
+	CPPUNIT_ASSERT(m_manager.lookup(id, infoLookup));
 
 	m_manager.close(id);
-	CPPUNIT_ASSERT(!m_manager.lookup(id, userLookup));
+	CPPUNIT_ASSERT(!m_manager.lookup(id, infoLookup));
 }
 
 }

commit 6e684cda141c634ac4da73dff3a924ec26ccb7cc

SessionManager: remove pointless session ID generation
It seems to be a relict of a wrong rebase.

Fixes: 1e4d361e4490 ("SessionManager: reimplement using SessionCache")
Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

server/src/server/SessionManager.cpp
@@ -31,10 +31,6 @@ const SessionID SessionManager::open(const User &user)
 	SessionID sessionID = Base64::encode(bSessionID, sizeof(bSessionID));
 
 	ExpirableSession session(user.id(), sessionID, timespan);
-
-	char b[ID_LENGTH64];
-	m_random->randomBytesUnlocked(b, sizeof(b));
-	SessionID id = Base64::encode(b, sizeof(b));
 	m_sessionCache->add(session.sessionID(), session);
 
 	if (m_logger.debug()) {