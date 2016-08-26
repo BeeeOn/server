BeeeOn/server

commit 5f95626292cffd4f7e96de7535049800374118fb

UIServerModule: use class Auth to handle authorization
Signed-off-by: Petr Stehlík <xstehl14@stud.fit.vutbr.cz>
Acked-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

server/src/ui/UIServerModule.cpp
@@ -11,6 +11,8 @@
 #include "UIServerModule.h"
 #include "Debug.h"
 
+#include "Auth.h"
+
 using namespace std;
 using namespace Poco;
 using namespace Poco::Net;
@@ -156,8 +158,8 @@ void factorySetup(UIServerRequestHandlerFactory &factory)
 	factory.noRoute(handleNoRoute);
 	factory.noOperation(handleNoOperation);
 	factory.sessionVerifier(verifyAuthorized);
-	factory.POST("/auth", handleAuth, false);
-	factory.DELETE("/auth", handleAuth);
+	factory.POST("/auth", Auth::handlePost, false);
+	factory.DELETE("/auth", Auth::handleDelete);
 	factory.POST("/users", handleCreateUser);
 	factory.GET("/users/:userId", handleGetUser);
 	factory.GET("/:placeId/devices/:deviceId", handleGetDevice);

commit 7e8826f1e7e7b3644d1ce367b87249ab62e8857a

UIServerModule: fix missing "ui/" in include
Fixes: 5f95626292cf ("UIServerModule: use class Auth to handle authorization")
Fixes: 1c2660bf0b12 ("ui_server: new class UIServerModule")
Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

server/src/ui/UIServerModule.cpp
@@ -8,10 +8,10 @@
 #include "service/AuthService.h"
 #include "server/Route.h"
 #include "model/JSONSerializer.h"
-#include "UIServerModule.h"
+#include "ui/UIServerModule.h"
 #include "Debug.h"
 
-#include "Auth.h"
+#include "ui/Auth.h"
 
 using namespace std;
 using namespace Poco;