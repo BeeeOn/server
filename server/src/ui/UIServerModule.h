BeeeOn/server

commit 1a275e5977d029af7e7c4c5eaff877de0e518a5a

ui_server: start up by DependencyInjector
Reviewed-by: Petr Stehlík <xstehl14@stud.fit.vutbr.cz>

server/src/ui/UIServerModule.h
@@ -5,6 +5,7 @@
 #include <Poco/SharedPtr.h>
 
 #include "server/RestRequestHandler.h"
+#include "di/InjectorTarget.h"
 
 /**
  * Compilation-time configuration of the target underlying
@@ -67,17 +68,24 @@ typedef UIRoute::Context UIRouteContext;
 
 void factorySetup(UIServerRequestHandlerFactory &factory);
 
-class UIServerModule {
+class UIServerModule : public AbstractInjectorTarget {
 public:
-	UIServerModule(unsigned int port):
+	UIServerModule(void):
 		m_factory(new UIServerRequestHandlerFactory(
-				*this, "ui-server")),
-		m_server(port, m_factory)
+				*this, "ui-server"))
 	{
 		factorySetup(*m_factory);
 	}
 
-	~UIServerModule() {}
+	~UIServerModule() {
+		if (m_server)
+			delete m_server;
+	}
+
+	void createServer(unsigned int port)
+	{
+		m_server = new UIRestServer(port, m_factory);
+	}
 
 	UIServerRequestHandlerFactory &factory()
 	{
@@ -86,12 +94,12 @@ public:
 
 	Server &server()
 	{
-		return m_server;
+		return *m_server;
 	}
 
 private:
 	Poco::SharedPtr<UIServerRequestHandlerFactory> m_factory;
-	UIRestServer m_server;
+	UIRestServer *m_server;
 };
 
 }

server/src/ui/UIServerModule.cpp
@@ -39,4 +39,6 @@ void factorySetup(UIServerRequestHandlerFactory &factory)
 	factory.sessionVerifier(verifyAuthorized);
 }
 
+BEEEON_OBJECT(UIServerModule, BeeeOn::UIServerModule)
+
 }

server/src/ui/main.cpp
@@ -8,6 +8,7 @@
 #include <Poco/Util/ServerApplication.h>
 
 #include "server/MongooseServer.h"
+#include "di/DependencyInjector.h"
 #include "UIServerModule.h"
 
 using namespace std;
@@ -20,6 +21,14 @@ using namespace BeeeOn;
 #define LOCAL_CONFIG_FILE  "logging.ini"
 #define SYSTEM_CONFIG_FILE "/etc/beeeon/ui-server/logging.ini"
 
+#define LOCAL_SERVICES_FILE  "services.xml"
+#define SYSTEM_SERVICES_FILE "/etc/beeeon/ui-server/services.xml"
+
+static Option optServices("services", "s",
+		"services configuration file to be used (xml, ini, properties)",
+		false,
+		"<file>",
+		true);
 static Option optLogging("logging", "l",
 		"logging configuration file to be used (xml, ini, properties)",
 		false,
@@ -43,6 +52,8 @@ public:
 protected:
 	void handleOption(const string &name, const string &value)
 	{
+		if (name == "services")
+			m_userServices = value;
 		if (name == "logging")
 			m_userLogging = value;
 		if (name == "help")
@@ -67,15 +78,43 @@ protected:
 			loadConfiguration(system.path());
 	}
 
+	void findAndLoadServices()
+	{
+		File user(m_userServices);
+		File local(LOCAL_SERVICES_FILE);
+		File system(SYSTEM_SERVICES_FILE);
+
+		if (!m_userServices.empty() && user.exists()) {
+			logger().notice(
+				"loading configuration from " + user.path(),
+				__FILE__, __LINE__);
+			loadConfiguration(user.path());
+		}
+		else if (local.exists()) {
+			logger().notice(
+				"loading configuration from " + local.path(),
+				__FILE__, __LINE__);
+			loadConfiguration(local.path());
+		}
+		else if (system.exists()) {
+			logger().notice(
+				"loading configuration from " + system.path(),
+				__FILE__, __LINE__);
+			loadConfiguration(system.path());
+		}
+	}
+
 	void initialize(Application &app)
 	{
 		Logger::root().setLevel(Logger::parseLevel("trace"));
 		findAndLoadLogging();
 		Application::initialize(app);
+		findAndLoadServices();
 	}
 
 	void defineOptions(OptionSet &options)
 	{
+		options.addOption(optServices);
 		options.addOption(optLogging);
 		options.addOption(optPort);
 		options.addOption(optHelp);
@@ -99,11 +138,18 @@ protected:
 		if (m_printHelp)
 			return printHelp();
 
-		UIServerModule module(m_serverPort);
-		module.server().start();
+		if (logger().debug())
+			ManifestSingleton::reportInfo(logger());
+
+		DependencyInjector injector(config().createView("services"));
+		UIServerModule *module = injector
+					.create<UIServerModule>("main");
+
+		module->createServer(m_serverPort);
+		module->server().start();
 
 		waitForTerminationRequest();
-		module.server().stop();
+		module->server().stop();
 		return EXIT_OK;
 	}
 
@@ -111,6 +157,7 @@ private:
 	bool m_printHelp;
 	unsigned int m_serverPort;
 	string m_userLogging;
+	string m_userServices;
 };
 
 int main(int argc, char **argv)

server/src/ui/services.xml
@@ -1,0 +1,6 @@
+<system>
+	<services>
+		<instance name="main" class="BeeeOn::UIServerModule">
+		</instance>
+	</services>
+</system>

commit cff55eac9b3736c9b206a0ec67fc1a920224a6ed

UIServerModule: fix uninitialized m_server
Fixes: 1a275e5977d0 ("ui_server: start up by DependencyInjector")
Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

server/src/ui/UIServerModule.h
@@ -79,6 +79,7 @@ void factorySetup(UIServerRequestHandlerFactory &factory);
 class UIServerModule : public AbstractInjectorTarget {
 public:
 	UIServerModule(void):
+		m_server(NULL),
 		m_factory(new UIServerRequestHandlerFactory(*this, "ui-server")),
 		m_logger(LOGGER_CLASS(this))
 	{