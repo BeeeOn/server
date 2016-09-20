37cfe31c06f0e45a73c068a60558acdae58a0c17
Jan Viktorin <iviktorin@fit.vutbr.cz>
2016-09-20 14:01:27 +0200

    t: ui: make ui-server port configurable
    Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

--- server/t/ui/config.py
+++ server/t/ui/config.py
@@ -1,11 +1,12 @@
 import json
+import os
 
 def import_libs():
 	from os import sys, path
 	sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
 
 ui_host = "localhost"
-ui_port = 8000
+ui_port = int(os.getenv("UI_SERVER_PORT", 8000))
 ui_session_per_user = 10
 
 PERMIT_LOGIN = json.dumps(
