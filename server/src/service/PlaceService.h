BeeeOn/server

commit 751234f848f5471aaa6ad497af5d27b155e06fc5

PlaceService: new class PlaceService
Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

server/src/service/PlaceService.h
@@ -1,0 +1,53 @@
+#ifndef BEEEON_PLACE_SERVICE_H
+#define BEEEON_PLACE_SERVICE_H
+
+#include "di/InjectorTarget.h"
+#include "model/Place.h"
+#include "dao/PlaceDao.h"
+
+namespace BeeeOn {
+
+class PlaceService : public AbstractInjectorTarget {
+public:
+	PlaceService():
+		m_placeDao(&NullPlaceDao::instance())
+	{
+		injector<PlaceService, PlaceDao>("placeDao",
+			&PlaceService::setPlaceDao);
+	}
+
+	void setPlaceDao(PlaceDao *dao)
+	{
+		if (dao == NULL)
+			m_placeDao = &NullPlaceDao::instance();
+		else
+			m_placeDao = dao;
+	}
+
+	void create(Place &place)
+	{
+		m_placeDao->create(place);
+	}
+
+	bool fetch(Place &place)
+	{
+		return m_placeDao->fetch(place);
+	}
+
+	bool update(Place &place)
+	{
+		return m_placeDao->update(place);
+	}
+
+	bool remove(const Place &place)
+	{
+		return m_placeDao->remove(place);
+	}
+
+private:
+	PlaceDao *m_placeDao;
+};
+
+}
+
+#endif

server/src/CMakeLists.txt
@@ -27,6 +27,7 @@ file(GLOB SOURCES
 	${PROJECT_SOURCE_DIR}/dao/DeviceDao.cpp
 	${PROJECT_SOURCE_DIR}/dao/GatewayDao.cpp
 	${PROJECT_SOURCE_DIR}/dao/LocationDao.cpp
+	${PROJECT_SOURCE_DIR}/service/PlaceService.cpp
 	${PROJECT_SOURCE_DIR}/service/UserService.cpp
 	${PROJECT_SOURCE_DIR}/service/AuthService.cpp
 	${PROJECT_SOURCE_DIR}/service/DeviceService.cpp

server/src/service/PlaceService.cpp
@@ -1,0 +1,3 @@
+#include "service/PlaceService.h"
+
+BEEEON_OBJECT(PlaceService, BeeeOn::PlaceService)

server/src/ui/UIServerModule.h
@@ -8,6 +8,7 @@
 #include "server/SessionManager.h"
 #include "server/SessionManager.h"
 #include "di/InjectorTarget.h"
+#include "service/PlaceService.h"
 #include "service/UserService.h"
 #include "service/AuthService.h"
 #include "service/DeviceService.h"
@@ -84,6 +85,8 @@ public:
 		factorySetup(*m_factory);
 		injector<UIServerModule, SessionManager>("sessionManager",
 				&UIServerModule::setSessionManager);
+		injector<UIServerModule, PlaceService>("placeService",
+				&UIServerModule::setPlaceService);
 		injector<UIServerModule, UserService>("userService",
 				&UIServerModule::setUserService);
 		injector<UIServerModule, AuthService>("authService",
@@ -124,6 +127,16 @@ public:
 	SessionManager &sessionManager()
 	{
 		return *m_sessionManager;
+	}
+
+	void setPlaceService(PlaceService *service)
+	{
+		m_placeService = service;
+	}
+
+	PlaceService &placeService()
+	{
+		return *m_placeService;
 	}
 
 	void setUserService(UserService *service)
@@ -185,6 +198,7 @@ private:
 	Poco::SharedPtr<UIServerRequestHandlerFactory> m_factory;
 	UIRestServer *m_server;
 	SessionManager *m_sessionManager;
+	PlaceService *m_placeService;
 	UserService *m_userService;
 	AuthService *m_authService;
 	DeviceService *m_deviceService;

server/src/ui/services.xml
@@ -2,6 +2,7 @@
 	<services>
 		<instance name="main" class="BeeeOn::UIServerModule">
 			<set name="sessionManager" ref="sessionManager" />
+			<set name="placeService" ref="placeService" />
 			<set name="userService" ref="userService" />
 			<set name="authService" ref="authService" />
 			<set name="deviceService" ref="deviceService" />
@@ -16,6 +17,10 @@
 		</instance>
 
 		<!-- Bussiness layer -->
+
+		<instance name="placeService" class="BeeeOn::PlaceService">
+			<set name="placeDao" ref="placeDao" />
+		</instance>
 
 		<instance name="userService" class="BeeeOn::UserService">
 			<set name="userDao" ref="userDao" />

commit 979a4d37139fdbcb30c7cca5453342979390e3e8

PlaceHandler: fetch place prior to delete
We return the deleted place after delete so it is necessary
to fetch it first. This introduces a race condition to be
later solved by Dao transactions.

Fixes: 751234f848f5 ("PlaceService: new class PlaceService")
Fixes: b49a12fae9dc ("PlaceHandler: new class")
Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

server/src/service/PlaceService.h
@@ -39,8 +39,11 @@ public:
 		return m_placeDao->update(place);
 	}
 
-	bool remove(const Place &place)
+	bool remove(Place &place)
 	{
+		if (!m_placeDao->fetch(place))
+			return false;
+
 		return m_placeDao->remove(place);
 	}
 