97b778c1d703a3a3adac090cfb0b2cb7207528e4
Jan Viktorin <iviktorin@fit.vutbr.cz>
2016-09-26 01:40:15 +0200

    GatewayXmlHandler: XML gateways endpoint
    Provides XML API to work with gateways. When registering
    a gateway a place is internally created (if it does not
    exist).
    
    Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

---
+++ server/src/xmlui/GatewayXmlHandler.h
@@ -0,0 +1,73 @@
+#ifndef BEEEON_XMLUI_GATEWAY_XML_HANDLER_H
+#define BEEEON_XMLUI_GATEWAY_XML_HANDLER_H
+
+#include "service/GatewayService.h"
+#include "service/PlaceService.h"
+#include "policy/GatewayAccessPolicy.h"
+#include "xmlui/AbstractXmlHandler.h"
+
+namespace BeeeOn {
+namespace XmlUI {
+
+class GatewayXmlHandler : public AbstractXmlHandler {
+public:
+	GatewayXmlHandler(const Poco::Net::StreamSocket &socket,
+			const Poco::AutoPtr<Poco::XML::Document> input,
+			BeeeOn::ExpirableSession::Ptr session,
+			BeeeOn::GatewayService &gatewayService,
+			BeeeOn::PlaceService &placeService,
+			BeeeOn::GatewayAccessPolicy &policy);
+	void handleInputImpl() override;
+	void handleRegister(Poco::XML::Element *gatewayNode);
+	void handleUnregister(Poco::XML::Element *gatewayNode);
+	void handleListen(Poco::XML::Element *gatewayNode);
+	void handleGet(Poco::XML::Element *gatewayNode);
+	void handleUpdate(Poco::XML::Element *gatewayNode);
+	void handleGetAll();
+
+private:
+	GatewayService &m_gatewayService;
+	PlaceService &m_placeService;
+	GatewayAccessPolicy &m_accessPolicy;
+};
+
+class GatewayXmlHandlerResolver : public AbstractXmlHandlerResolver {
+public:
+	GatewayXmlHandlerResolver();
+
+	bool canHandle(const Poco::XML::Element &root);
+	XmlRequestHandler *createHandler(
+			const Poco::Net::StreamSocket &socket,
+			const Poco::AutoPtr<Poco::XML::Document> input);
+
+	void setPlaceService(BeeeOn::PlaceService *service)
+	{
+		m_placeService = service;
+	}
+
+	void setGatewayService(BeeeOn::GatewayService *service)
+	{
+		m_gatewayService = service;
+	}
+
+	void setSessionManager(BeeeOn::SessionManager *manager)
+	{
+		m_sessionManager = manager;
+	}
+
+	void setAccessPolicy(BeeeOn::GatewayAccessPolicy *policy)
+	{
+		m_accessPolicy = policy;
+	}
+
+private:
+	BeeeOn::PlaceService *m_placeService;
+	BeeeOn::GatewayService *m_gatewayService;
+	BeeeOn::SessionManager *m_sessionManager;
+	BeeeOn::GatewayAccessPolicy *m_accessPolicy;
+};
+
+}
+}
+
+#endif
