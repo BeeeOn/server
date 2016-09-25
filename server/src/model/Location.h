BeeeOn/server

commit 23aa5089d41463563a2df798a1db73dae6426a62

model: do not inherit Entity
Dropping the original Entity concept for Place, User, Device, Gateway,
Location in behalf of Serializing by an external mechanism. The model
objects needs to define serialization procedures for certain use cases.

Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>
Reviewed-by: Marek Beňo <xbenom01@stud.fit.vutbr.cz>
Reviewed-by: Petr Stehlik <xstehl14@stud.fit.vutbr.cz>

server/src/model/Location.h
@@ -2,27 +2,58 @@
 #define BEEEON_LOCATION_H
 
 #include <Poco/SharedPtr.h>
-#include "model/Entity.h"
+#include "model/Collection.h"
 
 namespace BeeeOn {
 
 /**
  * Representation of a Location application entity.
  */
-class Location : public Entity {
+class Location {
 public:
 	typedef Poco::SharedPtr<Location> Ptr;
+	typedef BeeeOn::Collection<Location> Collection;
 	typedef long ID;
 
-	Location(const std::string &data):
-		Entity(data)
+	Location()
 	{
 	}
 
 	Location(const Location &copy):
-		Entity(copy)
+		m_name(copy.m_name)
 	{
 	}
+
+	void setName(const std::string &name)
+	{
+		m_name = name;
+	}
+
+	const std::string &name() const
+	{
+		return m_name;
+	}
+
+	/**
+	 * Frontend API only.
+	 */
+	template <typename Serializer>
+	void toWeb(Serializer &s) const
+	{
+		s.push("name", m_name);
+	}
+
+	/**
+	 * Frontend API only.
+	 */
+	template <typename Serializer>
+	void fromWeb(Serializer &s)
+	{
+		s.get("name", m_name);
+	}
+
+private:
+	std::string m_name;
 };
 
 typedef Location::Collection LocationCollection;

server/src/model/Device.h
@@ -2,27 +2,58 @@
 #define BEEEON_DEVICE_H
 
 #include <Poco/SharedPtr.h>
-#include "model/Entity.h"
+#include "model/Collection.h"
 
 namespace BeeeOn {
 
 /**
  * Representation of the Device application entity.
  */
-class Device : public Entity {
+class Device {
 public:
 	typedef Poco::SharedPtr<Device> Ptr;
+	typedef BeeeOn::Collection<Device> Collection;
 	typedef long ID;
 
-	Device(const std::string &data):
-		Entity(data)
+	Device()
 	{
 	}
 
-	Device(const Device &device):
-		Entity(device)
+	Device(const Device &copy):
+		m_name(copy.m_name)
 	{
 	}
+
+	void setName(const std::string &name)
+	{
+		m_name = name;
+	}
+
+	const std::string &name() const
+	{
+		return m_name;
+	}
+
+	/**
+	 * Frontend API only.
+	 */
+	template <typename Serializer>
+	void toWeb(Serializer &s) const
+	{
+		s.push("name", m_name);
+	}
+
+	/**
+	 * Frontend API only.
+	 */
+	template <typename Serializer>
+	void fromWeb(Serializer &s)
+	{
+		s.get("name", m_name);
+	}
+
+private:
+	std::string m_name;
 };
 
 typedef Device::Collection DeviceCollection;

server/src/model/Gateway.h
@@ -2,27 +2,58 @@
 #define BEEEON_GATEWAY_H
 
 #include <Poco/SharedPtr.h>
-#include "model/Entity.h"
+#include "model/Collection.h"
 
 namespace BeeeOn {
 
 /**
  * Representation of a Gateway application entity.
  */
-class Gateway : public Entity {
+class Gateway {
 public:
 	typedef Poco::SharedPtr<Gateway> Ptr;
+	typedef BeeeOn::Collection<Gateway> Collection;
 	typedef long ID;
 
-	Gateway(const std::string &data):
-		Entity(data)
+	Gateway()
 	{
 	}
 
-	Gateway(const Gateway &gateway):
-		Entity(gateway)
+	Gateway(const Gateway &copy):
+		m_name(copy.m_name)
 	{
 	}
+
+	void setName(const std::string &name)
+	{
+		m_name = name;
+	}
+
+	const std::string &name() const
+	{
+		return m_name;
+	}
+
+	/**
+	 * Frontend API only.
+	 */
+	template <typename Serializer>
+	void toWeb(Serializer &s) const
+	{
+		s.push("name", m_name);
+	}
+
+	/**
+	 * Frontend API only.
+	 */
+	template <typename Serializer>
+	void fromWeb(Serializer &s)
+	{
+		s.get("name", m_name);
+	}
+
+private:
+	std::string m_name;
 };
 
 typedef Gateway::Collection GatewayCollection;

server/src/model/Place.h
@@ -2,22 +2,58 @@
 #define BEEEON_PLACE_H
 
 #include <Poco/SharedPtr.h>
-#include "model/Entity.h"
+#include "model/Collection.h"
 
 namespace BeeeOn {
 
 /**
  * A place represents a real location (a building, home, ...).
  */
-class Place : public Entity {
+class Place {
 public:
 	typedef Poco::SharedPtr<Place> Ptr;
+	typedef BeeeOn::Collection<Place> Collection;
 	typedef long ID;
 
-	Place(const std::string &data):
-		Entity(data)
+	Place()
 	{
 	}
+
+	Place(const Place &copy):
+		m_name(copy.m_name)
+	{
+	}
+
+	void setName(const std::string &name)
+	{
+		m_name = name;
+	}
+
+	const std::string name() const
+	{
+		return m_name;
+	}
+
+	/**
+	 * Frontend API only.
+	 */
+	template <typename Serializer>
+	void toWeb(Serializer &s) const
+	{
+		s.push("name", name());
+	}
+
+	/**
+	 * Frontend API only.
+	 */
+	template <typename Serializer>
+	void fromWeb(Serializer &s)
+	{
+		s.get("name", m_name);
+	}
+
+private:
+	std::string m_name;
 };
 
 typedef Place::Collection PlaceCollection;

server/src/model/User.h
@@ -2,37 +2,70 @@
 #define BEEEON_USER_H
 
 #include <Poco/SharedPtr.h>
-#include "model/Entity.h"
+#include "model/Collection.h"
 
 namespace BeeeOn {
 
 /**
  * Representation of the User application entity.
  */
-class User : public Entity {
+class User {
 public:
 	typedef Poco::SharedPtr<User> Ptr;
+	typedef BeeeOn::Collection<User> Collection;
 	typedef long ID;
 
-	User(const std::string &data):
-		Entity(data)
+	User()
 	{
 	}
 
-	User(const User &user):
-		Entity(user)
+	User(ID id):
+		m_id(id)
 	{
+	}
+
+	User(const User &copy):
+		m_id(copy.m_id),
+		m_email(copy.m_email)
+	{
+	}
+
+	void setEmail(const std::string &email)
+	{
+		m_email = email;
 	}
 
 	const std::string email() const
 	{
-		return get("email");
+		return m_email;
 	}
 
-	const User::ID id() const
+	User::ID id() const
 	{
-		return get<User::ID>("id");
+		return m_id;
 	}
+
+	/**
+	 * Frontend API only.
+	 */
+	template <typename Serializer>
+	void toWeb(Serializer &s) const
+	{
+		s.push("email", m_email);
+	}
+
+	/**
+	 * Frontend API only.
+	 */
+	template <typename Serializer>
+	void fromWeb(Serializer &s)
+	{
+		s.get("email", m_email);
+	}
+
+private:
+	ID m_id;
+	std::string m_email;
 };
 
 typedef User::Collection UserCollection;

server/src/ui/UIServerModule.cpp
@@ -7,6 +7,7 @@
 
 #include "service/AuthService.h"
 #include "server/Route.h"
+#include "model/JSONSerializer.h"
 #include "UIServerModule.h"
 #include "Debug.h"
 
@@ -83,7 +84,10 @@ static void handleGetUser(UIRouteContext &context)
 
 	UserID id = stoi(it->second);
 	const User::Ptr user = context.userData().userService().get(id);
-	const string &userData = user->toString();
+
+	JSONObjectSerializer serializer;
+	user->toWeb(serializer);
+	const string &userData = serializer.toString();
 
 	context.response().sendBuffer(userData.c_str(), userData.size());
 }
@@ -92,10 +96,15 @@ static void handleCreateUser(UIRouteContext &context)
 {
 	std::string userData;
 	StreamCopier::copyToString(context.request().stream(), userData);
-	User user(userData);
+	JSONObjectSerializer deserializer;
+	User user;
+	user.fromWeb(deserializer);
 
 	User::Ptr created = context.userData().userService().create(user);
-	const string &result = created->toString();
+	JSONObjectSerializer serializer;
+	created->toWeb(serializer);
+	const string &result = serializer.toString();
+
 	context.response().sendBuffer(result.c_str(), result.size());
 }
 

server/test/service/AuthServiceTest.cpp
@@ -53,9 +53,9 @@ void AuthServiceTest::testPermitAuth()
 	manager.setSecureRandomProvider(&randomProvider);
 
 	MockUserDao dao;
-	User::Ptr user(new User(
-			"{\"id\": 0, \"email\": \"permit@example.org\"}"));
-	dao.storage().insert(make_pair(0, user));
+	User::Ptr user(new User(0));
+	user->setEmail("permit@example.org");
+	dao.storage().insert(make_pair(user->id(), user));
 
 	AuthService service;
 	PermitAuthProvider provider;

commit 6c770139288771258ee453c712bd0d31d324e454

Location: copy constructor copies ID as well
Fixes: 23aa5089d414 ("model: do not inherit Entity")
Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>
Reviewed-by: Petr Stehlík <xstehl14@stud.fit.vutbr.cz>

server/src/model/Location.h
@@ -27,6 +27,7 @@ public:
 	}
 
 	Location(const Location &copy):
+		m_id(copy.m_id),
 		m_name(copy.m_name),
 		m_place(copy.m_place)
 	{