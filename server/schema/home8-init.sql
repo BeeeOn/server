BeeeOn/server

commit 2b6693004cba5f1be60687f2d25dbca2b468938e

ui: schema: table for gateways
Signed-off-by: Petr Stehlík <xstehl14@stud.fit.vutbr.cz>
Acked-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

server/schema/home8-init.sql
@@ -3,3 +3,13 @@ CREATE TABLE places (
 	id      uuid NOT NULL primary key,
 	name    varchar(250)
 );
+
+DROP TABLE IF EXISTS gateways;
+CREATE TABLE gateways (
+	id          uuid NOT NULL primary key,
+	name        varchar(250),
+	place_id    uuid REFERENCES places(id),
+	altitude    double precision,
+	latitude    double precision,
+	longitude   double precision
+);

commit 2a9786755838d71ffebf057a3db859040b26a345

ui: schema: fix gateways.id to be decimal
The gateway ID is not an UUID, it is a special number formatted
as described in the class GatewayID.

Fixes: 2b6693004cba ("ui: schema: table for gateways")
Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

server/schema/home8-init.sql
@@ -6,7 +6,7 @@ CREATE TABLE places (
 
 DROP TABLE IF EXISTS gateways;
 CREATE TABLE gateways (
-	id          uuid NOT NULL primary key,
+	id          decimal(16, 0) NOT NULL primary key,
 	name        varchar(250),
 	place_id    uuid REFERENCES places(id),
 	altitude    double precision,