623627caffd0bced10a18efeec53571aed4bee60
Jan Viktorin <iviktorin@fit.vutbr.cz>
2016-09-26 16:13:07 +0200

    AuthXmlHandler: new class
    Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

---
+++ server/src/xmlui/AuthXmlHandler.cpp
@@ -0,0 +1,78 @@
+#include "xmlui/AuthXmlHandler.h"
+
+using namespace std;
+using namespace BeeeOn;
+using namespace BeeeOn::XmlUI;
+using namespace Poco;
+using namespace Poco::Net;
+using namespace Poco::XML;
+
+AuthXmlHandler::AuthXmlHandler(
+		const StreamSocket &socket,
+		const AutoPtr<Document> input,
+		BeeeOn::AuthService &service):
+	AbstractXmlHandler("accounts", socket, input),
+	m_authService(service)
+{
+}
+
+void AuthXmlHandler::handleInputImpl()
+{
+	Element *root = m_input->documentElement();
+	const string &type = root->getAttribute("type");
+
+	if (type == "logout") {
+		m_authService.logout(root->getAttribute("sessionid"));
+		resultSuccess();
+	}
+	else if (type == "login") {
+		Element *provider = root->getChildElement("provider");
+		if (provider == NULL) {
+			resultInvalidInput();
+			return;
+		}
+
+		AuthCodeCredentials credentials(
+			provider->getAttribute("name"),
+			provider->getAttribute("authCode")
+		);
+
+		const string session = m_authService.login(credentials);
+	
+		AttributesImpl attrs;
+		attrs.addAttribute("", "sessionid", "sessionid", "", session);
+		resultSimple(attrs, "ok");
+	}
+	else
+		resultInvalidInput();
+}
+
+AuthXmlHandlerResolver::AuthXmlHandlerResolver():
+	AbstractXmlHandlerResolver("accounts")
+{
+	injector<AuthXmlHandlerResolver, AuthService>("authService",
+			&AuthXmlHandlerResolver::setAuthService);
+}
+
+bool AuthXmlHandlerResolver::canHandle(const Element &root)
+{
+	if (!AbstractXmlHandlerResolver::canHandle(root))
+		return false;
+
+	if (root.getAttribute("type") == "login")
+		return true;
+
+	if (root.getAttribute("type") == "logout")
+		return true;
+
+	return false;
+}
+
+XmlRequestHandler *AuthXmlHandlerResolver::createHandler(
+		const StreamSocket &socket,
+		const AutoPtr<Document> input)
+{
+	return new AuthXmlHandler(socket, input, *m_authService);
+}
+
+BEEEON_OBJECT(AuthXmlHandlerResolver, BeeeOn::XmlUI::AuthXmlHandlerResolver)
