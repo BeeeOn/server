BeeeOn/server

commit b926573cf17120b723be20f1e1151430caf15d24

Session: separate Session into file
The Session-related code became quite big over time. It is useful
to separate it into its own Session.h.

Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

server/src/server/Session.h
@@ -1,0 +1,71 @@
+#ifndef BEEEON_SESSION_H
+#define BEEEON_SESSION_H
+
+#include <Poco/Timespan.h>
+
+namespace BeeeOn {
+
+typedef std::string SessionID;
+typedef std::string Email;
+
+/**
+ * Base class to hold session data (userID, sessionID).
+ * Email is only for future use. Currently useless.
+ */
+class Session
+{
+public:
+	Session(const UserID &userID, const SessionID &sessionID) :
+		m_userID(userID),
+		m_sessionID(sessionID)
+	{
+	}
+
+	const Email email() const
+	{
+		return m_email;
+	}
+
+	const UserID userID() const
+	{
+		return m_userID;
+	}
+
+	const SessionID sessionID() const
+	{
+		return m_sessionID;
+	}
+
+private:
+	Email m_email;
+	UserID m_userID;
+	SessionID m_sessionID;
+};
+
+/**
+ * Enables expirable session feature using Poco
+ */
+class ExpirableSession : public Session
+{
+public:
+	ExpirableSession(const UserID &userID, const SessionID &sessionID,
+			Poco::Timespan &timespan) :
+		Session(userID, sessionID),
+		m_tstamp()
+	{
+		// Set expiration time
+		m_tstamp += timespan;
+	}
+
+	const Poco::Timestamp& getExpiration() const
+	{
+		return m_tstamp;
+	}
+
+private:
+	Poco::Timestamp m_tstamp;
+};
+
+}
+
+#endif

server/src/server/SessionManager.h
@@ -6,77 +6,16 @@
 #include <Poco/RWLock.h>
 #include <Poco/AbstractCache.h>
 #include <Poco/UniqueExpireStrategy.h>
-#include <Poco/Timespan.h>
 #include <Poco/SharedPtr.h>
 #include <Poco/Logger.h>
 
 #include "di/InjectorTarget.h"
 #include "provider/RandomProvider.h"
 #include "model/User.h"
+#include "server/Session.h"
 #include "Debug.h"
 
 namespace BeeeOn {
-
-typedef std::string SessionID;
-typedef std::string Email;
-
-/**
- * Base class to hold session data (userID, sessionID).
- * Email is only for future use. Currently useless.
- */
-class Session
-{
-public:
-	Session(const UserID &userID, const SessionID &sessionID) :
-		m_userID(userID),
-		m_sessionID(sessionID)
-	{
-	}
-
-	const Email email() const
-	{
-		return m_email;
-	}
-
-	const UserID userID() const
-	{
-		return m_userID;
-	}
-
-	const SessionID sessionID() const
-	{
-		return m_sessionID;
-	}
-
-private:
-	Email m_email;
-	UserID m_userID;
-	SessionID m_sessionID;
-};
-
-/**
- * Enables expirable session feature using Poco
- */
-class ExpirableSession : public Session
-{
-public:
-	ExpirableSession(const UserID &userID, const SessionID &sessionID,
-			Poco::Timespan &timespan) :
-		Session(userID, sessionID),
-		m_tstamp()
-	{
-		// Set expiration time
-		m_tstamp += timespan;
-	}
-
-	const Poco::Timestamp& getExpiration() const
-	{
-		return m_tstamp;
-	}
-
-private:
-	Poco::Timestamp m_tstamp;
-};
 
 /*
  * SessionCache holds both session and its reverse records for each session

commit 60835989c7df4d17724df5d35b3475475306344d

Session: fix missing include
Fixes: b926573cf171 ("Session: separate Session into file")
Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

server/src/server/Session.h
@@ -4,6 +4,8 @@
 #include <Poco/Timespan.h>
 #include <Poco/SharedPtr.h>
 
+#include "model/User.h"
+
 namespace BeeeOn {
 
 typedef std::string SessionID;