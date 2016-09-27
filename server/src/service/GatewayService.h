BeeeOn/server

commit 3d18d9948149f3e618edc5d7636a15eabf084d8b

GatewayService: implement service operations
Implement operations typically used by the REST frontend:

* fetchFromPlace (GET)
* update (PUT)
* assignAndUpdate (POST)
* unassign (DELETE)

Signed-off-by: Petr Stehlík <xstehl14@stud.fit.vutbr.cz>
Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>
Acked-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

server/src/service/GatewayService.h
@@ -7,6 +7,8 @@
 #include "rpc/GatewayRPC.h"
 #include "di/InjectorTarget.h"
 #include "Debug.h"
+#include "model/Place.h"
+#include "model/Gateway.h"
 
 namespace BeeeOn {
 
@@ -42,6 +44,32 @@ public:
 	{
 		TRACE_METHOD();
 		return m_dao->fetch(gateway);
+	}
+
+	bool fetchFromPlace(Gateway &gateway, Place &place)
+	{
+		return m_dao->fetchFromPlace(gateway, place);
+	}
+
+	bool update(Gateway &gateway)
+	{
+		return m_dao->update(gateway);
+	}
+
+	bool assignAndUpdate(Gateway &gateway, const Place &place)
+	{
+		if (gateway.hasPlace())
+			return false;
+
+		return m_dao->assignAndUpdate(gateway, place);
+	}
+
+	bool unassign(Gateway &gateway, Place &place)
+	{
+		if (!m_dao->fetchFromPlace(gateway, place))
+			return false;
+
+		return m_dao->unassign(gateway);
 	}
 
 	void scanDevices(Gateway &gateway)

commit 5a92435af346ca6007069f9a8f3b606272882825

GatewayService: fix place attribute to be const
The place is never expected to be modified inside the GatewayService.

Fixes: 3d18d9948149 ("GatewayService: implement service operations")
Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

server/src/service/GatewayService.h
@@ -45,7 +45,7 @@ public:
 		return m_dao->fetch(gateway);
 	}
 
-	bool fetchFromPlace(Gateway &gateway, Place &place)
+	bool fetchFromPlace(Gateway &gateway, const Place &place)
 	{
 		return m_dao->fetchFromPlace(gateway, place);
 	}
@@ -63,7 +63,7 @@ public:
 		return m_dao->assignAndUpdate(gateway, place);
 	}
 
-	bool unassign(Gateway &gateway, Place &place)
+	bool unassign(Gateway &gateway, const Place &place)
 	{
 		if (!m_dao->fetchFromPlace(gateway, place))
 			return false;