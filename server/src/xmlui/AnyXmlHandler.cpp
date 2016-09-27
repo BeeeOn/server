cc4e49ca96c9e9f63b3677b68e15efb602edbc8d
Jan Viktorin <iviktorin@fit.vutbr.cz>
2016-09-27 21:51:37 +0200

    AnyXmlHandler: new class
    Handle any request as an invalid input.
    
    Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

---
+++ server/src/xmlui/AnyXmlHandler.cpp
@@ -0,0 +1,41 @@
+#include "xmlui/AnyXmlHandler.h"
+#include "Debug.h"
+
+using namespace std;
+using namespace BeeeOn;
+using namespace BeeeOn::XmlUI;
+using namespace Poco;
+using namespace Poco::Net;
+using namespace Poco::XML;
+
+AnyXmlHandler::AnyXmlHandler(
+		const StreamSocket &socket,
+		const AutoPtr<Document> input):
+	AbstractXmlHandler("", socket, input)
+{
+}
+
+void AnyXmlHandler::handleInputImpl()
+{
+	_TRACE_METHOD(m_logger);
+	resultInvalidInput();
+}
+
+AnyXmlHandlerResolver::AnyXmlHandlerResolver():
+	AbstractXmlHandlerResolver("")
+{
+}
+
+bool AnyXmlHandlerResolver::canHandle(const Element &root)
+{
+	return true;
+}
+
+XmlRequestHandler *AnyXmlHandlerResolver::createHandler(
+		const StreamSocket &socket,
+		const AutoPtr<Document> input)
+{
+	return new AnyXmlHandler(socket, input);
+}
+
+BEEEON_OBJECT(AnyXmlHandlerResolver, BeeeOn::XmlUI::AnyXmlHandlerResolver)
---
+++ server/src/xmlui/AnyXmlHandler.h
@@ -0,0 +1,31 @@
+#ifndef BEEEON_XMLUI_ANY_XML_HANDLER_H
+#define BEEEON_XMLUI_ANY_XML_HANDLER_H
+
+#include "xmlui/AbstractXmlHandler.h"
+
+namespace BeeeOn {
+namespace XmlUI {
+
+class AnyXmlHandler : public AbstractXmlHandler {
+public:
+	AnyXmlHandler(const Poco::Net::StreamSocket &socket,
+			const Poco::AutoPtr<Poco::XML::Document> input);
+
+	void handleInputImpl() override;
+};
+
+class AnyXmlHandlerResolver : public AbstractXmlHandlerResolver {
+public:
+	AnyXmlHandlerResolver();
+
+	bool canHandle(const Poco::XML::Element &input);
+	XmlRequestHandler *createHandler(
+			const Poco::Net::StreamSocket &socket,
+			const Poco::AutoPtr<Poco::XML::Document> input);
+};
+
+}
+}
+
+#endif
+
