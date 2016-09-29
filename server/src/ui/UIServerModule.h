BeeeOn/server

commit 7791f941907a7f1b7c43194d4f3fb063b43b905d

ui_server: new service UserService
Reviewed-by: Petr Stehlík <xstehl14@stud.fit.vutbr.cz>

server/src/ui/UIServerModule.h
@@ -6,7 +6,9 @@
 
 #include "server/RestRequestHandler.h"
 #include "server/SessionManager.h"
+#include "server/SessionManager.h"
 #include "di/InjectorTarget.h"
+#include "service/UserService.h"
 
 /**
  * Compilation-time configuration of the target underlying
@@ -78,6 +80,8 @@ public:
 		factorySetup(*m_factory);
 		injector<UIServerModule, SessionManager>("sessionManager",
 				&UIServerModule::setSessionManager);
+		injector<UIServerModule, UserService>("userService",
+				&UIServerModule::setUserService);
 	}
 
 	~UIServerModule() {
@@ -110,10 +114,21 @@ public:
 		return *m_sessionManager;
 	}
 
+	void setUserService(UserService *service)
+	{
+		m_userService = service;
+	}
+
+	UserService &userService()
+	{
+		return *m_userService;
+	}
+
 private:
 	Poco::SharedPtr<UIServerRequestHandlerFactory> m_factory;
 	UIRestServer *m_server;
 	SessionManager *m_sessionManager;
+	UserService *m_userService;
 };
 
 }

server/src/CMakeLists.txt
@@ -17,6 +17,8 @@ file(GLOB SOURCES
 	${PROJECT_SOURCE_DIR}/provider/AuthProvider.cpp
 	${PROJECT_SOURCE_DIR}/provider/RandomProvider.cpp
 	${PROJECT_SOURCE_DIR}/server/SessionManager.cpp
+	${PROJECT_SOURCE_DIR}/dao/UserDao.cpp
+	${PROJECT_SOURCE_DIR}/service/UserService.cpp
 )
 
 include_directories(

server/src/dao/UserDao.cpp
@@ -1,0 +1,4 @@
+#include "dao/UserDao.h"
+
+BEEEON_OBJECT(MockUserDao, BeeeOn::MockUserDao)
+BEEEON_OBJECT(NullUserDao, BeeeOn::NullUserDao)

server/src/dao/UserDao.h
@@ -1,0 +1,47 @@
+#ifndef BEEEON_USER_DAO_H
+#define BEEEON_USER_DAO_H
+
+#include "di/InjectorTarget.h"
+#include "dao/NullDao.h"
+#include "dao/MockDao.h"
+#include "model/User.h"
+
+namespace BeeeOn {
+
+class UserDao {
+public:
+	virtual UserID create(const User &user) = 0;
+	virtual User::Ptr get(UserID userId) = 0;
+	virtual User::Ptr getByEmail(const std::string &email) = 0;
+};
+
+class NullUserDao : public AbstractInjectorTarget,
+		public NullDao<User, UserDao> {
+public:
+	User::Ptr getByEmail(const std::string &email)
+	{
+		throw Poco::NotImplementedException(__func__);
+	}
+};
+
+class MockUserDao : public AbstractInjectorTarget,
+		public MockDao<User, UserDao> {
+public:
+	User::Ptr getByEmail(const std::string &email)
+	{
+		Storage::iterator it;
+
+		for (it = storage().begin(); it != storage().end(); ++it) {
+			User::Ptr user = it->second;
+
+			if (email.compare(user->email()) == 0)
+				return user;
+		}
+
+		return NULL;
+	}
+};
+
+}
+
+#endif

server/src/model/User.h
@@ -1,0 +1,43 @@
+#ifndef BEEEON_USER_H
+#define BEEEON_USER_H
+
+#include <Poco/SharedPtr.h>
+#include "model/Entity.h"
+
+namespace BeeeOn {
+
+/**
+ * Representation of the User application entity.
+ */
+class User : public Entity {
+public:
+	typedef Poco::SharedPtr<User> Ptr;
+	typedef long ID;
+
+	User(const std::string &data):
+		Entity(data)
+	{
+	}
+
+	User(const User &user):
+		Entity(user)
+	{
+	}
+
+	const std::string email() const
+	{
+		return get("email");
+	}
+
+	const User::ID id() const
+	{
+		return get<User::ID>("id");
+	}
+};
+
+typedef User::Collection UserCollection;
+typedef User::ID UserID;
+
+}
+
+#endif

server/src/service/UserService.cpp
@@ -1,0 +1,3 @@
+#include "service/UserService.h"
+
+BEEEON_OBJECT(UserService, BeeeOn::UserService)

server/src/service/UserService.h
@@ -1,0 +1,48 @@
+#ifndef BEEEON_USER_SERVICE_H
+#define BEEEON_USER_SERVICE_H
+
+#include <Poco/Exception.h>
+#include <Poco/Logger.h>
+#include "dao/UserDao.h"
+#include "di/InjectorTarget.h"
+#include "Debug.h"
+
+namespace BeeeOn {
+
+/**
+ * User management service. Its purpose is to ensure creating
+ * user profiles and operations strongly related to a user.
+ */
+class UserService : public AbstractInjectorTarget {
+public:
+	UserService()
+	{
+		injector<UserService, UserDao>("userDao",
+				&UserService::setUserDao);
+	}
+
+	void setUserDao(UserDao *dao)
+	{
+		m_dao = dao;
+	}
+
+	User::Ptr create(User &u)
+	{
+		TRACE_METHOD();
+		UserID id = m_dao->create(u);
+		return m_dao->get(id);
+	}
+
+	User::Ptr get(UserID id)
+	{
+		TRACE_METHOD();
+		return m_dao->get(id);
+	}
+
+private:
+	UserDao *m_dao;
+};
+
+}
+
+#endif

server/src/ui/UIServerModule.cpp
@@ -1,9 +1,12 @@
+#include <Poco/StreamCopier.h>
 #include <Poco/Net/NetException.h>
 
 #include "server/Route.h"
 #include "UIServerModule.h"
 #include "Debug.h"
 
+using namespace std;
+using namespace Poco;
 using namespace BeeeOn;
 
 void handleNoRoute(UIRouteContext &context)
@@ -30,6 +33,35 @@ static void verifyAuthorized(const UIRequest &request,
 	throw Poco::Net::NotAuthenticatedException("not implemented");
 }
 
+static void handleGetUser(UIRouteContext &context)
+{
+	UIRoute::Params::const_iterator it;
+
+	it = context.params().find("userId");
+	if (it == context.params().end()) {
+		context.response().setStatus(UIResponse::HTTP_NOT_FOUND);
+		context.response().setReason("Not found");
+		return;
+	}
+
+	UserID id = stoi(it->second);
+	const User::Ptr user = context.userData().userService().get(id);
+	const string &userData = user->toString();
+
+	context.response().sendBuffer(userData.c_str(), userData.size());
+}
+
+static void handleCreateUser(UIRouteContext &context)
+{
+	std::string userData;
+	StreamCopier::copyToString(context.request().stream(), userData);
+	User user(userData);
+
+	User::Ptr created = context.userData().userService().create(user);
+	const string &result = created->toString();
+	context.response().sendBuffer(result.c_str(), result.size());
+}
+
 namespace BeeeOn {
 
 void factorySetup(UIServerRequestHandlerFactory &factory)
@@ -37,6 +69,8 @@ void factorySetup(UIServerRequestHandlerFactory &factory)
 	factory.noRoute(handleNoRoute);
 	factory.noOperation(handleNoOperation);
 	factory.sessionVerifier(verifyAuthorized);
+	factory.POST("/users", handleCreateUser);
+	factory.GET("/users/:userId", handleGetUser);
 }
 
 BEEEON_OBJECT(UIServerModule, BeeeOn::UIServerModule)

commit 7c024c60b83bab6f7684ebe917395944abf8e7a6

UIServerModule: remove duplicate include
Fixes: 7791f941907a ("ui_server: new service UserService")
Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

server/src/ui/UIServerModule.h
@@ -6,7 +6,6 @@
 
 #include "server/RestRequestHandler.h"
 #include "server/SessionManager.h"
-#include "server/SessionManager.h"
 #include "di/InjectorTarget.h"
 #include "service/PlaceService.h"
 #include "service/UserService.h"