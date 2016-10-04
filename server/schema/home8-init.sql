5dcd4c80a3a0a266fb371dc46d504d4bd0b97d3a
Jan Viktorin <iviktorin@fit.vutbr.cz>
2016-10-04 16:46:36 +0200

    schema: add table verified_identities

--- server/schema/home8-init.sql
+++ server/schema/home8-init.sql
@@ -35,3 +35,14 @@ CREATE TABLE identities (
 	email       varchar(250) NOT NULL,
 	CONSTRAINT unique_identity UNIQUE (email)
 );
+
+DROP TABLE IF EXISTS verified_identities;
+CREATE TABLE verified_identities (
+	id          uuid NOT NOT primary key;
+	identity_id uuid NOT NULL REFERENCES identities(id),
+	user_id     uuid NOT NULL REFERENCES users(id),
+	provider    varchar(250) NOT NULL,
+	picture     varchar(250),
+	accessToken varchar(250)
+	CONSTRAINT unique_verified_identity UNIQUE (identity_id, provider)
+);
