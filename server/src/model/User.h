BeeeOn/server

commit 1efd107b7d55760c1ac2e85f39a3c29c2c3089ab

model: introduce m_id member into model objects
A model object may or may not know its unique ID. Allow to construct
the objects with or without the ID. Also, introduce a custom copy
constructor with the explicit ID parameter to copy a new data object
from an existing instance with different ID.

All model objects now use SimpleID instead of just a long.

Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>
Reviewed-by: Petr Stehlik <xstehl14@stud.fit.vutbr.cz>

server/src/model/User.h
@@ -2,6 +2,7 @@
 #define BEEEON_USER_H
 
 #include <Poco/SharedPtr.h>
+#include "model/SimpleID.h"
 #include "model/Collection.h"
 
 namespace BeeeOn {
@@ -13,19 +14,24 @@ class User {
 public:
 	typedef Poco::SharedPtr<User> Ptr;
 	typedef BeeeOn::Collection<User> Collection;
-	typedef long ID;
+	typedef SimpleID ID;
 
 	User()
 	{
 	}
 
-	User(ID id):
+	User(const ID &id):
 		m_id(id)
 	{
 	}
 
 	User(const User &copy):
-		m_id(copy.m_id),
+		m_email(copy.m_email)
+	{
+	}
+
+	User(const ID &id, const User &copy):
+		m_id(id),
 		m_email(copy.m_email)
 	{
 	}
@@ -40,7 +46,7 @@ public:
 		return m_email;
 	}
 
-	User::ID id() const
+	const ID &id() const
 	{
 		return m_id;
 	}

server/src/model/Device.h
@@ -2,6 +2,7 @@
 #define BEEEON_DEVICE_H
 
 #include <Poco/SharedPtr.h>
+#include <model/SimpleID.h>
 #include "model/Collection.h"
 
 namespace BeeeOn {
@@ -13,14 +14,24 @@ class Device {
 public:
 	typedef Poco::SharedPtr<Device> Ptr;
 	typedef BeeeOn::Collection<Device> Collection;
-	typedef long ID;
+	typedef SimpleID ID;
 
 	Device()
 	{
 	}
 
+	Device(const ID &id):
+		m_id(id)
+	{
+	}
+
 	Device(const Device &copy):
 		m_name(copy.m_name)
+	{
+	}
+
+	Device(const ID &id, const Device &copy):
+		m_id(id)
 	{
 	}
 
@@ -32,6 +43,11 @@ public:
 	const std::string &name() const
 	{
 		return m_name;
+	}
+
+	const ID &id() const
+	{
+		return m_id;
 	}
 
 	/**
@@ -53,6 +69,7 @@ public:
 	}
 
 private:
+	ID m_id;
 	std::string m_name;
 };
 

server/src/model/Gateway.h
@@ -2,6 +2,7 @@
 #define BEEEON_GATEWAY_H
 
 #include <Poco/SharedPtr.h>
+#include "model/SimpleID.h"
 #include "model/Collection.h"
 
 namespace BeeeOn {
@@ -13,14 +14,24 @@ class Gateway {
 public:
 	typedef Poco::SharedPtr<Gateway> Ptr;
 	typedef BeeeOn::Collection<Gateway> Collection;
-	typedef long ID;
+	typedef SimpleID ID;
 
 	Gateway()
 	{
 	}
 
+	Gateway(const ID &id):
+		m_id(id)
+	{
+	}
+
 	Gateway(const Gateway &copy):
 		m_name(copy.m_name)
+	{
+	}
+
+	Gateway(const ID &id, const Gateway &copy):
+		m_id(id)
 	{
 	}
 
@@ -32,6 +43,11 @@ public:
 	const std::string &name() const
 	{
 		return m_name;
+	}
+
+	const ID &id() const
+	{
+		return m_id;
 	}
 
 	/**
@@ -53,6 +69,7 @@ public:
 	}
 
 private:
+	ID m_id;
 	std::string m_name;
 };
 

server/src/model/Location.h
@@ -2,6 +2,7 @@
 #define BEEEON_LOCATION_H
 
 #include <Poco/SharedPtr.h>
+#include "model/SimpleID.h"
 #include "model/Collection.h"
 
 namespace BeeeOn {
@@ -13,13 +14,24 @@ class Location {
 public:
 	typedef Poco::SharedPtr<Location> Ptr;
 	typedef BeeeOn::Collection<Location> Collection;
-	typedef long ID;
+	typedef SimpleID ID;
 
 	Location()
 	{
 	}
 
+	Location(const ID &id):
+		m_id(id)
+	{
+	}
+
 	Location(const Location &copy):
+		m_name(copy.m_name)
+	{
+	}
+
+	Location(const ID &id, const Location &copy):
+		m_id(id),
 		m_name(copy.m_name)
 	{
 	}
@@ -32,6 +44,11 @@ public:
 	const std::string &name() const
 	{
 		return m_name;
+	}
+
+	const ID &id() const
+	{
+		return m_id;
 	}
 
 	/**
@@ -53,6 +70,7 @@ public:
 	}
 
 private:
+	ID m_id;
 	std::string m_name;
 };
 

server/src/model/Place.h
@@ -2,6 +2,7 @@
 #define BEEEON_PLACE_H
 
 #include <Poco/SharedPtr.h>
+#include "model/SimpleID.h"
 #include "model/Collection.h"
 
 namespace BeeeOn {
@@ -13,15 +14,32 @@ class Place {
 public:
 	typedef Poco::SharedPtr<Place> Ptr;
 	typedef BeeeOn::Collection<Place> Collection;
-	typedef long ID;
+	typedef SimpleID ID;
 
 	Place()
 	{
 	}
 
+	Place(ID id):
+		m_id(id)
+	{
+	}
+
 	Place(const Place &copy):
+		m_id(copy.m_id),
 		m_name(copy.m_name)
 	{
+	}
+
+	Place(ID id, const Place &copy):
+		m_id(id),
+		m_name(copy.m_name)
+	{
+	}
+
+	ID id() const
+	{
+		return m_id;
 	}
 
 	void setName(const std::string &name)
@@ -53,6 +71,7 @@ public:
 	}
 
 private:
+	ID m_id = 0;
 	std::string m_name;
 };
 

server/src/service/AuthService.cpp
@@ -39,7 +39,7 @@ const string AuthService::login(const Credentials &cred)
 	SessionManager::Info info;
 	info.insert(make_pair(SessionManager::INFO_EMAIL, email));
 	info.insert(make_pair(SessionManager::INFO_USER_ID,
-				to_string(user->id())));
+				user->id().toString()));
 	return m_sessionManager->open(info);
 }
 

server/src/ui/UIServerModule.cpp
@@ -82,7 +82,7 @@ static void handleGetUser(UIRouteContext &context)
 		return;
 	}
 
-	UserID id = stoi(it->second);
+	UserID id = UserID::parse(it->second);
 	const User::Ptr user = context.userData().userService().get(id);
 
 	JSONObjectSerializer serializer;
@@ -119,7 +119,7 @@ static void handleGetDevice(UIRouteContext &context)
 		return;
 	}
 
-	PlaceID placeId = stoi(it->second);
+	Place place(PlaceID::parse(it->second));
 
 	it = context.params().find("deviceId");
 	if (it == context.params().end()) {
@@ -128,10 +128,10 @@ static void handleGetDevice(UIRouteContext &context)
 		return;
 	}
 
-	DeviceID id = stoi(it->second);
+	DeviceID id = DeviceID::parse(it->second);
 
 	const Device::Ptr device = context
-		.userData().deviceService().get(id, placeId);
+		.userData().deviceService().get(id, place.id());
 	JSONObjectSerializer serializer;
 	device->toWeb(serializer);
 	const string &result = serializer.toString();

commit b3f5eb5ca3231d3dcccfef908dc4d078422686f2

User: fix copy constructor to copy id
Fixes: 1efd107b7d55 ("model: introduce m_id member into model objects")
Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

server/src/model/User.h
@@ -26,6 +26,7 @@ public:
 	}
 
 	User(const User &copy):
+		m_id(copy.m_id),
 		m_email(copy.m_email)
 	{
 	}