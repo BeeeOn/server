BeeeOn/server

commit 2d2c29a35af9a7e11091264aa560e2470b0dcd3e

IdentityDao: new class
Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

server/src/dao/IdentityDao.h
@@ -1,0 +1,51 @@
+#ifndef BEEEON_IDENTITY_DAO_H
+#define BEEEON_IDENTITY_DAO_H
+
+#include "di/InjectorTarget.h"
+#include "model/Identity.h"
+#include "dao/NullDao.h"
+#include "dao/MockDao.h"
+
+namespace BeeeOn {
+
+class IdentityDao {
+public:
+	virtual void create(Identity &identity) = 0;
+	virtual bool fetch(Identity &identity) = 0;
+	virtual bool fetchBy(Identity &identity,
+			const std::string &email) = 0;
+	virtual bool update(Identity &identity) = 0;
+	virtual bool remove(const Identity &identity) = 0;
+};
+
+class NullIdentityDao : public AbstractInjectorTarget,
+		public NullDao<Identity, IdentityDao> {
+public:
+	bool fetchBy(Identity &identity,
+			const std::string &email)
+	{
+		return fetch(identity);
+	}
+
+	IdentityDao &instance();
+};
+
+class MockIdentityDao : public AbstractInjectorTarget,
+		public MockDao<Identity, IdentityDao> {
+public:
+	void create(Identity &identity);
+	bool fetchBy(Identity &identity,
+			const std::string &email);
+	bool update(Identity &identity);
+protected:
+	IdentityID nextID()
+	{
+		return IdentityID::random();
+	}
+
+	Iterator findByEmail(const std::string &email);
+};
+
+}
+
+#endif

server/src/CMakeLists.txt
@@ -32,6 +32,7 @@ file(GLOB SOURCES
 	${PROJECT_SOURCE_DIR}/dao/DeviceDao.cpp
 	${PROJECT_SOURCE_DIR}/dao/GatewayDao.cpp
 	${PROJECT_SOURCE_DIR}/dao/LocationDao.cpp
+	${PROJECT_SOURCE_DIR}/dao/IdentityDao.cpp
 	${PROJECT_SOURCE_DIR}/service/PlaceService.cpp
 	${PROJECT_SOURCE_DIR}/service/UserService.cpp
 	${PROJECT_SOURCE_DIR}/service/AuthService.cpp

server/src/dao/IdentityDao.cpp
@@ -1,0 +1,69 @@
+#include <Poco/Exception.h>
+#include <Poco/SingletonHolder.h>
+
+#include "dao/IdentityDao.h"
+
+BEEEON_OBJECT(NullIdentityDao, BeeeOn::NullIdentityDao)
+BEEEON_OBJECT(MockIdentityDao, BeeeOn::MockIdentityDao)
+
+using namespace Poco;
+using namespace BeeeOn;
+
+IdentityDao &NullIdentityDao::instance()
+{
+	static Poco::SingletonHolder<NullIdentityDao> singleton;
+	return *singleton.get();
+}
+
+MockIdentityDao::Iterator MockIdentityDao::findByEmail(
+		const std::string &email)
+{
+	Iterator it;
+
+	for (it = storage().begin(); it != storage().end(); ++it) {
+		if (it->second->email() != email)
+			continue;
+
+		return it;
+	}
+
+	return storage().end();
+}
+
+void MockIdentityDao::create(Identity &identity)
+{
+	Iterator it = findByEmail(identity.email());
+
+	if (it != storage().end()) {
+		throw ExistsException("identity ("
+				+ identity.email()
+				+ ") already exists for "
+				+ it->second->id().toString());
+	}
+
+	MockDao<Identity, IdentityDao>::create(identity);
+}
+
+bool MockIdentityDao::fetchBy(Identity &identity,
+		const std::string &email)
+{
+	TRACE_METHOD();
+	Iterator it = findByEmail(email);
+
+	if (it == storage().end())
+		return false;
+
+	identity = *it->second;
+	return true;
+}
+
+bool MockIdentityDao::update(Identity &identity)
+{
+	Identity tmp;
+
+	if (!fetch(tmp))
+		return false;
+
+	identity.setEmail(tmp.email()); // avoid changing e-mail
+	return update(identity);
+}

server/src/ui/services.xml
@@ -107,11 +107,13 @@
 		<instance name="deviceDao" class="BeeeOn::MockDeviceDao" />
 		<instance name="mockGatewayDao" class="BeeeOn::MockGatewayDao" />
 		<instance name="mockLocationDao" class="BeeeOn::MockLocationDao" />
+		<instance name="mockIdentityDao" class="BeeeOn::MockIdentityDao" />
 
 		<alias name="placeDao" ref="mockPlaceDao" />
 		<alias name="userDao" ref="mockUserDao" />
 		<alias name="gatewayDao" ref="mockGatewayDao" />
 		<alias name="locationDao" ref="mockLocationDao" />
+		<alias name="identityDao" ref="mockIdentityDao" />
 
 		<instance name="sociManager"
 				class="BeeeOn::SociManager">

commit f5c213b6cc532a7a2309d32e7139206f8f6c5171

IdentityDao: make instance() method static
Fixes: 2d2c29a35af9 ("IdentityDao: new class")
Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

server/src/dao/IdentityDao.h
@@ -27,7 +27,7 @@ public:
 		return fetch(identity);
 	}
 
-	IdentityDao &instance();
+	static IdentityDao &instance();
 };
 
 class MockIdentityDao : public AbstractInjectorTarget,