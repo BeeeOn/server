0da74bdfc86618f31ec3d80879e955da3a4fff9e
Jan Viktorin <iviktorin@fit.vutbr.cz>
2016-10-05 02:02:20 +0200

    GatewayAccessPolicy: new interface
    Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

---
+++ server/src/policy/GatewayAccessPolicy.h
@@ -0,0 +1,29 @@
+#ifndef BEEEON_GATEWAY_ACCESS_POLICY_H
+#define BEEEON_GATEWAY_ACCESS_POLICY_H
+
+#include "server/Session.h"
+#include "model/Place.h"
+#include "model/Gateway.h"
+
+namespace BeeeOn {
+
+class GatewayAccessPolicy {
+public:
+	virtual void assureGet(
+			const ExpirableSession::Ptr session,
+			const Gateway &gateway) = 0;
+	virtual void assureAssignGateway(
+			const ExpirableSession::Ptr session,
+			const Place &place) = 0;
+	virtual void assureUnassign(
+			const ExpirableSession::Ptr session,
+			const Gateway &gateway) = 0;
+	virtual void assureUpdate(
+			const ExpirableSession::Ptr session,
+			const Gateway &gateway) = 0;
+};
+
+}
+
+#endif
+
