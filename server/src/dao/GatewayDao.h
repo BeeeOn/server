BeeeOn/server

commit 6c694a5e1f42d07823a6547cf6fe04013bc61189

GatewayDao: define operations on gateway
Operations on gateway are usually done in the context of a place.
We propagate the place argument as close as possible to the database
operations to have wider range of implementation possibilities.

Signed-off-by: Petr Stehlík <xstehl14@stud.fit.vutbr.cz>
Acked-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

server/src/dao/GatewayDao.h
@@ -11,7 +11,12 @@ namespace BeeeOn {
 
 class GatewayDao {
 public:
+	virtual bool insert(Gateway &gateway) = 0;
 	virtual bool fetch(Gateway &gateway) = 0;
+	virtual bool update(Gateway &gateway) = 0;
+	virtual bool assignAndUpdate(Gateway &gateway, const Place &place) = 0;
+	virtual bool unassign(Gateway &gateway) = 0;
+	virtual bool fetchFromPlace(Gateway &gateway, Place &place) = 0;
 };
 
 class NullGatewayDao : public AbstractInjectorTarget,
@@ -22,7 +27,24 @@ public:
 	 * unnecessary allocations.
 	 */
 	static GatewayDao &instance();
+
+	bool assignAndUpdate(Gateway &gateway, const Place &place)
+	{
+		return update(gateway);
+	}
+
+	bool unassign(Gateway &gateway)
+	{
+		return update(gateway);
+	}
+
+	bool fetchFromPlace(Gateway &gateway, Place &place)
+	{
+		return fetchFromPlace(gateway, place);
+	}
+
 };
+
 
 class MockGatewayDao : public AbstractInjectorTarget,
 	public MockDao<Gateway, GatewayDao> {
@@ -30,6 +52,42 @@ protected:
 	GatewayID nextID()
 	{
 		return GatewayID::random();
+	}
+
+	bool assignAndUpdate(Gateway &gateway, const Place &place)
+	{
+		gateway.setPlace(place);
+		return update(gateway);
+	}
+
+	/**
+	 * Create an empty Place and set it as new place for given gateway.
+	 * This generally means unassigning a gateway from place.
+	 */
+	bool unassign(Gateway &gateway)
+	{
+		Place place;
+		gateway.setPlace(place);
+
+		return update(gateway);
+	}
+
+	/**
+	 * Fetch a gateway from DB and compare the given placeID with
+	 * the fetched one.
+	 */
+	bool fetchFromPlace(Gateway &gateway, Place &place)
+	{
+		Gateway fetchedGateway(gateway);
+
+		bool result = fetch(fetchedGateway);
+
+		if (result && fetchedGateway.place().id() == place.id()) {
+			gateway = fetchedGateway;
+			return true;
+		}
+
+		return false;
 	}
 
 private:

commit 6a40aa7d4444890a784e79b07f11020c98414bd3

NullGatewayDao: fix fetchFromPlace
Fixes: 6c694a5e1f42 ("GatewayDao: define operations on gateway")
Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

server/src/dao/GatewayDao.h
@@ -40,7 +40,7 @@ public:
 
 	bool fetchFromPlace(Gateway &gateway, const Place &place)
 	{
-		return fetchFromPlace(gateway, place);
+		return fetch(gateway);
 	}
 
 };