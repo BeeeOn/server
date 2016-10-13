45cccffdff803a771275600a3e74b324540df5aa
Jan Viktorin <iviktorin@fit.vutbr.cz>
2016-10-14 01:07:16 +0200

    xmlui: Location: serialize Location and vector of Locations
    Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

--- server/src/xmlui/Serializing.h
+++ server/src/xmlui/Serializing.h
@@ -13,12 +13,17 @@ namespace XML {
 namespace BeeeOn {
 
 class Gateway;
+class Location;
 
 namespace XmlUI {
 
 void serialize(Poco::XML::XMLWriter &output, const Gateway &gateway);
 void serialize(Poco::XML::XMLWriter &output,
 		const std::vector<Gateway> &gateways);
+
+void serialize(Poco::XML::XMLWriter &output, const Location &location);
+void serialize(Poco::XML::XMLWriter &output,
+		const std::vector<Location> &locations);
 
 }
 }
