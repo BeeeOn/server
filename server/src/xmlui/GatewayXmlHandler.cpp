BeeeOn/server

commit 97b778c1d703a3a3adac090cfb0b2cb7207528e4

GatewayXmlHandler: XML gateways endpoint
Provides XML API to work with gateways. When registering
a gateway a place is internally created (if it does not
exist).

Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

server/src/xmlui/GatewayXmlHandler.cpp
@@ -1,0 +1,209 @@
+#include <vector>
+
+#include "xmlui/GatewayXmlHandler.h"
+#include "xmlui/XmlGatewayDeserializer.h"
+#include "xmlui/Serializing.h"
+#include "model/VerifiedIdentity.h"
+#include "model/Gateway.h"
+#include "model/Place.h"
+
+using namespace std;
+using namespace Poco;
+using namespace Poco::Net;
+using namespace Poco::XML;
+using namespace BeeeOn;
+using namespace BeeeOn::XmlUI;
+
+GatewayXmlHandler::GatewayXmlHandler(
+		const StreamSocket &socket,
+		const AutoPtr<Document> input,
+		ExpirableSession::Ptr session,
+		GatewayService &gatewayService,
+		PlaceService &placeService,
+		GatewayAccessPolicy &policy):
+	AbstractXmlHandler("gates", socket, input, session),
+	m_gatewayService(gatewayService),
+	m_placeService(placeService),
+	m_accessPolicy(policy)
+{
+}
+
+void GatewayXmlHandler::handleInputImpl()
+{
+	if (!requireSession())
+		return;
+
+	Element *root = m_input->documentElement();
+	const string &type = root->getAttribute("type");
+
+	if (type == "getall") {
+		handleGetAll();
+		return;
+	}
+
+	Element *gatewayNode = root->getChildElement("gate");
+	if (gatewayNode == NULL) {
+		resultInvalidInput();
+		return;
+	}
+
+	if (type == "register")
+		handleRegister(gatewayNode);
+	else if (type == "unregister")
+		handleUnregister(gatewayNode);
+	else if (type == "startlisten")
+		handleListen(gatewayNode);
+	else if (type == "get")
+		handleGet(gatewayNode);
+	else if (type == "update")
+		handleUpdate(gatewayNode);
+	else
+		resultInvalidInput();
+}
+
+void GatewayXmlHandler::handleRegister(Element *gatewayNode)
+{
+	Gateway gateway(GatewayID::parse(gatewayNode->getAttribute("id")));
+	VerifiedIdentity identity(session()->identityID());
+	XmlGatewayDeserializer deserializer(*gatewayNode);
+	User user(session()->userID());
+
+	// approved to everybody, such gateway is associated with a
+	// place where the user is admin or to a new implicit place
+	if (!m_gatewayService.registerGateway(
+			gateway, deserializer, identity)) {
+		resultNotOwned();
+		return;
+	}
+
+	resultSuccess();
+}
+
+void GatewayXmlHandler::handleUnregister(Element *gatewayNode)
+{
+	Gateway gateway(GatewayID::parse(gatewayNode->getAttribute("id")));
+	User user(session()->userID());
+
+	m_accessPolicy.assureUnassign(user, gateway);
+
+	if (!m_gatewayService.unassign(gateway, user)) {
+		resultNotOwned();
+		return;
+	}
+
+	resultSuccess();
+}
+
+void GatewayXmlHandler::handleListen(Element *gatewayNode)
+{
+	Gateway gateway(GatewayID::parse(gatewayNode->getAttribute("id")));
+	User user(session()->userID());
+
+	m_accessPolicy.assureScanDevices(user, gateway);
+
+	m_gatewayService.scanDevices(gateway);
+	resultSuccess();
+}
+
+void GatewayXmlHandler::handleGet(Element *gatewayNode)
+{
+	Gateway gateway(GatewayID::parse(gatewayNode->getAttribute("id")));
+	User user(session()->userID());
+
+	m_accessPolicy.assureGet(user, gateway);
+
+	if (!m_gatewayService.fetch(gateway)) {
+		resultNotFound();
+		return;
+	}
+
+	if (!gateway.hasPlace()) {
+		resultNotFound();
+		return;
+	}
+
+	resultDataStart();
+	serialize(m_output, gateway);
+	resultDataEnd();
+}
+
+void GatewayXmlHandler::handleUpdate(Element *gatewayNode)
+{
+	Gateway gateway(GatewayID::parse(gatewayNode->getAttribute("id")));
+	XmlGatewayDeserializer update(*gatewayNode);
+	User user(session()->userID());
+
+	m_accessPolicy.assureUpdate(user, gateway);
+
+	if (!m_gatewayService.update(gateway, update)) {
+		resultNotFound();
+		return;
+	}
+
+	resultSuccess();
+}
+
+void GatewayXmlHandler::handleGetAll()
+{
+	vector<Gateway> gateways;
+	User user(session()->userID());
+
+	m_gatewayService.fetchAccessible(gateways, user);
+
+	resultDataStart();
+	serialize(m_output, gateways);
+	resultDataEnd();
+}
+
+GatewayXmlHandlerResolver::GatewayXmlHandlerResolver():
+	AbstractXmlHandlerResolver("gates")
+{
+	injector<GatewayXmlHandlerResolver, GatewayService>(
+			"gatewayService",
+			&GatewayXmlHandlerResolver::setGatewayService);
+	injector<GatewayXmlHandlerResolver, PlaceService>(
+			"placeService",
+			&GatewayXmlHandlerResolver::setPlaceService);
+	injector<GatewayXmlHandlerResolver, GatewayAccessPolicy>(
+			"accessPolicy",
+			&GatewayXmlHandlerResolver::setAccessPolicy);
+	injector<GatewayXmlHandlerResolver, SessionManager>(
+			"sessionManager",
+			&GatewayXmlHandlerResolver::setSessionManager);
+}
+
+bool GatewayXmlHandlerResolver::canHandle(
+		const Element &root)
+{
+	const string &type = root.getAttribute("type");
+
+	if (type == "register")
+		return true;
+	if (type == "unregister")
+		return true;
+	if (type == "startlisten")
+		return true;
+	if (type == "get")
+		return true;
+	if (type == "update")
+		return true;
+	if (type == "getall")
+		return true;
+
+	return false;
+}
+
+XmlRequestHandler *GatewayXmlHandlerResolver::createHandler(
+		const StreamSocket &socket,
+		const AutoPtr<Document> input)
+{
+	ExpirableSession::Ptr session = lookupSession(
+			*m_sessionManager, input);
+	return new GatewayXmlHandler(
+			socket, input, session,
+			*m_gatewayService, *m_placeService,
+			*m_accessPolicy);
+}
+
+BEEEON_OBJECT(GatewayXmlHandlerResolver,
+		BeeeOn::XmlUI::GatewayXmlHandlerResolver)

server/conf/services.xml
@@ -37,6 +37,7 @@
 		<instance name="xmlui"
 				class="BeeeOn::XmlRequestHandlerFactory">
 			<add name="resolvers" ref="authXmlHandler" />
+			<add name="resolvers" ref="gatewayXmlHandler" />
 			<add name="resolvers" ref="anyXmlHandler" />
 		</instance>
 
@@ -46,6 +47,14 @@
 		<instance name="authXmlHandler"
 				class="BeeeOn::XmlUI::AuthXmlHandlerResolver">
 			<set name="authService" ref="authService" />
+		</instance>
+
+		<instance name="gatewayXmlHandler"
+				class="BeeeOn::XmlUI::GatewayXmlHandlerResolver">
+			<set name="gatewayService" ref="gatewayService" />
+			<set name="placeService" ref="placeService" />
+			<set name="accessPolicy" ref="accessPolicy" />
+			<set name="sessionManager" ref="sessionManager" />
 		</instance>
 
 		<instance name="uiMockInit"

server/src/CMakeLists.txt
@@ -83,6 +83,7 @@ add_executable(xmlui-server
 	xmlui/AnyXmlHandler.cpp
 	xmlui/AuthXmlHandler.cpp
 	xmlui/XmlGatewayDeserializer.cpp
+	xmlui/GatewayXmlHandler.cpp
 	ui/UIMockInit.cpp
 )
 

server/src/xmlui/GatewayXmlHandler.h
@@ -1,0 +1,73 @@
+#ifndef BEEEON_XMLUI_GATEWAY_XML_HANDLER_H
+#define BEEEON_XMLUI_GATEWAY_XML_HANDLER_H
+
+#include "service/GatewayService.h"
+#include "service/PlaceService.h"
+#include "policy/GatewayAccessPolicy.h"
+#include "xmlui/AbstractXmlHandler.h"
+
+namespace BeeeOn {
+namespace XmlUI {
+
+class GatewayXmlHandler : public AbstractXmlHandler {
+public:
+	GatewayXmlHandler(const Poco::Net::StreamSocket &socket,
+			const Poco::AutoPtr<Poco::XML::Document> input,
+			BeeeOn::ExpirableSession::Ptr session,
+			BeeeOn::GatewayService &gatewayService,
+			BeeeOn::PlaceService &placeService,
+			BeeeOn::GatewayAccessPolicy &policy);
+	void handleInputImpl() override;
+	void handleRegister(Poco::XML::Element *gatewayNode);
+	void handleUnregister(Poco::XML::Element *gatewayNode);
+	void handleListen(Poco::XML::Element *gatewayNode);
+	void handleGet(Poco::XML::Element *gatewayNode);
+	void handleUpdate(Poco::XML::Element *gatewayNode);
+	void handleGetAll();
+
+private:
+	GatewayService &m_gatewayService;
+	PlaceService &m_placeService;
+	GatewayAccessPolicy &m_accessPolicy;
+};
+
+class GatewayXmlHandlerResolver : public AbstractXmlHandlerResolver {
+public:
+	GatewayXmlHandlerResolver();
+
+	bool canHandle(const Poco::XML::Element &root);
+	XmlRequestHandler *createHandler(
+			const Poco::Net::StreamSocket &socket,
+			const Poco::AutoPtr<Poco::XML::Document> input);
+
+	void setPlaceService(BeeeOn::PlaceService *service)
+	{
+		m_placeService = service;
+	}
+
+	void setGatewayService(BeeeOn::GatewayService *service)
+	{
+		m_gatewayService = service;
+	}
+
+	void setSessionManager(BeeeOn::SessionManager *manager)
+	{
+		m_sessionManager = manager;
+	}
+
+	void setAccessPolicy(BeeeOn::GatewayAccessPolicy *policy)
+	{
+		m_accessPolicy = policy;
+	}
+
+private:
+	BeeeOn::PlaceService *m_placeService;
+	BeeeOn::GatewayService *m_gatewayService;
+	BeeeOn::SessionManager *m_sessionManager;
+	BeeeOn::GatewayAccessPolicy *m_accessPolicy;
+};
+
+}
+}
+
+#endif

commit 1a51d56020cc24c57f6f674cd4a8c4ed99211a8b

GatewayXmlHandlerResolver: fix canHandle does not call parent
Fixes: 97b778c1d703 ("GatewayXmlHandler: XML gateways endpoint")
Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

server/src/xmlui/GatewayXmlHandler.cpp
@@ -175,6 +175,9 @@ GatewayXmlHandlerResolver::GatewayXmlHandlerResolver():
 bool GatewayXmlHandlerResolver::canHandle(
 		const Element &root)
 {
+	if (!AbstractXmlHandlerResolver::canHandle(root))
+		return false;
+
 	const string &type = root.getAttribute("type");
 
 	if (type == "register")