432a18bc356109c6eb0e35b278034610f8924840
Jan Viktorin <iviktorin@fit.vutbr.cz>
2016-10-14 19:01:30 +0200

    ProfileXmlHandler: new class
    Handle request ns="accounts" type="getmyprofile".
    
    Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

---
+++ server/src/xmlui/ProfileXmlHandler.cpp
@@ -0,0 +1,86 @@
+#include <vector>
+
+#include "service/IdentityService.h"
+#include "xmlui/ProfileXmlHandler.h"
+#include "xmlui/Serializing.h"
+#include "model/VerifiedIdentity.h"
+#include "model/Identity.h"
+#include "model/User.h"
+
+using namespace std;
+using namespace Poco;
+using namespace Poco::Net;
+using namespace Poco::XML;
+using namespace BeeeOn;
+using namespace BeeeOn::XmlUI;
+
+ProfileXmlHandler::ProfileXmlHandler(
+		const StreamSocket &socket,
+		const AutoPtr<Document> input,
+		ExpirableSession::Ptr session,
+		IdentityService &identityService):
+	AbstractXmlHandler("accounts", socket, input, session),
+	m_identityService(identityService)
+{
+}
+
+void ProfileXmlHandler::handleInputImpl()
+{
+	if (!requireSession())
+		return;
+
+	Element *root = m_input->documentElement();
+	const string &type = root->getAttribute("type");
+
+	if (type == "getmyprofile")
+		handleMyProfile();
+	else
+		resultInvalidInput();
+}
+
+void ProfileXmlHandler::handleMyProfile()
+{
+	VerifiedIdentity identity(session()->identityID());
+	m_identityService.fetch(identity);
+
+	resultDataStart();
+	serializeMyself(m_output, identity);
+	resultDataEnd();
+}
+
+ProfileXmlHandlerResolver::ProfileXmlHandlerResolver():
+	AbstractXmlHandlerResolver("accounts")
+{
+	injector<ProfileXmlHandlerResolver, IdentityService>(
+			"identityService",
+			&ProfileXmlHandlerResolver::setIdentityService);
+	injector<ProfileXmlHandlerResolver, SessionManager>(
+			"sessionManager",
+			&ProfileXmlHandlerResolver::setSessionManager);
+}
+
+bool ProfileXmlHandlerResolver::canHandle(
+		const Element &root)
+{
+	const string &type = root.getAttribute("type");
+
+	if (type == "getmyprofile")
+		return true;
+
+	return false;
+}
+
+XmlRequestHandler *ProfileXmlHandlerResolver::createHandler(
+		const StreamSocket &socket,
+		const AutoPtr<Document> input)
+{
+	ExpirableSession::Ptr session = lookupSession(
+			*m_sessionManager, input);
+	return new ProfileXmlHandler(
+			socket, input, session,
+			*m_identityService);
+}
+
+BEEEON_OBJECT(ProfileXmlHandlerResolver,
+		BeeeOn::XmlUI::ProfileXmlHandlerResolver)
+
