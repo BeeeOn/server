0604d75789bb24c593d1ab72e01dc5e40c44d0df
Jan Viktorin <iviktorin@fit.vutbr.cz>
2016-10-17 19:29:00 +0200

    PolicyContext: introduce new interface
    A PolicyContext allows to abstract the application code from the
    real policy framework requirements. The AbstractPolicyContext
    is to simplify various implementations.
    
    Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

---
+++ server/src/policy/PolicyContext.h
@@ -0,0 +1,50 @@
+#ifndef BEEEON_POLICY_CONTEXT_H
+#define BEEEON_POLICY_CONTEXT_H
+
+#include <Poco/Exception.h>
+
+namespace BeeeOn {
+
+class User;
+
+/**
+ * Interface providing a context to the policy framework.
+ */
+class PolicyContext {
+public:
+	virtual const User &user() const = 0;
+};
+
+class AbstractPolicyContext : public PolicyContext {
+public:
+	AbstractPolicyContext(User &user):
+		m_user(&user)
+	{
+	}
+
+	AbstractPolicyContext():
+		m_user(0)
+	{
+	}
+
+	void setUser(User &user)
+	{
+		m_user = &user;
+	}
+
+	const User &user() const override
+	{
+		if (m_user)
+			return *m_user;
+
+		throw Poco::IllegalStateException(
+				"no user has been specified");
+	}
+
+private:
+	User *m_user;
+};
+
+}
+
+#endif
