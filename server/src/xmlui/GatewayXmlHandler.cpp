BeeeOn/server

commit f3810ca7a5e4773b6608d548bce2c9abca52c820

GatewayAccessPolicy: abstract User to PolicyContext
Do not expose the User as a prerequisite for checking the access
policy for Place. Pass PolicyContext instead. Take advantage of
deriving the PolicyContext by Single and Relation template classes.

Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

server/src/xmlui/GatewayXmlHandler.cpp
@@ -84,8 +84,9 @@ void GatewayXmlHandler::handleUnregister(Element *gatewayNode)
 	Gateway gateway(GatewayID::parse(gatewayNode->getAttribute("id")));
 	User user(session()->userID());
 	Relation<Gateway, User> input(gateway, user);
+	input.setUser(user);
 
-	m_accessPolicy.assureUnassign(user, gateway);
+	m_accessPolicy.assureUnassign(input, gateway);
 
 	if (!m_gatewayService.unassign(input)) {
 		resultNotOwned();
@@ -100,8 +101,9 @@ void GatewayXmlHandler::handleListen(Element *gatewayNode)
 	Gateway gateway(GatewayID::parse(gatewayNode->getAttribute("id")));
 	Single<Gateway> input(gateway);
 	User user(session()->userID());
+	input.setUser(user);
 
-	m_accessPolicy.assureScanDevices(user, gateway);
+	m_accessPolicy.assureScanDevices(input, gateway);
 
 	m_gatewayService.scanDevices(input);
 	resultSuccess();
@@ -112,8 +114,9 @@ void GatewayXmlHandler::handleGet(Element *gatewayNode)
 	Gateway gateway(GatewayID::parse(gatewayNode->getAttribute("id")));
 	Single<Gateway> input(gateway);
 	User user(session()->userID());
+	input.setUser(user);
 
-	m_accessPolicy.assureGet(user, gateway);
+	m_accessPolicy.assureGet(input, gateway);
 
 	if (!m_gatewayService.fetch(input)) {
 		resultNotFound();
@@ -136,8 +139,9 @@ void GatewayXmlHandler::handleUpdate(Element *gatewayNode)
 	XmlGatewayDeserializer update(*gatewayNode);
 	SingleWithData<Gateway> input(gateway, update);
 	User user(session()->userID());
+	input.setUser(user);
 
-	m_accessPolicy.assureUpdate(user, gateway);
+	m_accessPolicy.assureUpdate(input, gateway);
 
 	if (!m_gatewayService.update(input)) {
 		resultNotFound();

server/src/policy/DefaultAccessPolicy.cpp
@@ -76,15 +76,15 @@ void DefaultAccessPolicy::assureRemove(
 }
 
 void DefaultAccessPolicy::assureAssignGateway(
-		const User &user,
+		const PolicyContext &context,
 		const Place &place)
 {
 	assureAtLeast(
-		fetchAccessLevel(user, place), AccessLevel::admin());
+		fetchAccessLevel(context.user(), place), AccessLevel::admin());
 }
 
 void DefaultAccessPolicy::assureGet(
-		const User &user,
+		const PolicyContext &context,
 		const Gateway &gateway)
 {
 	Gateway tmp(gateway);
@@ -95,11 +95,11 @@ void DefaultAccessPolicy::assureGet(
 	const Place place(tmp.place());
 
 	assureAtLeast(
-		fetchAccessLevel(user, place), AccessLevel::guest());
+		fetchAccessLevel(context.user(), place), AccessLevel::guest());
 }
 
 void DefaultAccessPolicy::assureUnassign(
-		const User &user,
+		const PolicyContext &context,
 		const Gateway &gateway)
 {
 	Gateway tmp(gateway);
@@ -110,11 +110,11 @@ void DefaultAccessPolicy::assureUnassign(
 	const Place place(tmp.place());
 
 	assureAtLeast(
-		fetchAccessLevel(user, place), AccessLevel::admin());
+		fetchAccessLevel(context.user(), place), AccessLevel::admin());
 }
 
 void DefaultAccessPolicy::assureUpdate(
-		const User &user,
+		const PolicyContext &context,
 		const Gateway &gateway)
 {
 	Gateway tmp(gateway);
@@ -125,11 +125,11 @@ void DefaultAccessPolicy::assureUpdate(
 	const Place place(tmp.place());
 
 	assureAtLeast(
-		fetchAccessLevel(user, place), AccessLevel::user());
+		fetchAccessLevel(context.user(), place), AccessLevel::user());
 }
 
 void DefaultAccessPolicy::assureScanDevices(
-		const User &user,
+		const PolicyContext &context,
 		const Gateway &gateway)
 {
 	Gateway tmp(gateway);
@@ -140,7 +140,7 @@ void DefaultAccessPolicy::assureScanDevices(
 	const Place place(tmp.place());
 
 	assureAtLeast(
-		fetchAccessLevel(user, place), AccessLevel::user());
+		fetchAccessLevel(context.user(), place), AccessLevel::user());
 }
 
 void DefaultAccessPolicy::assureCreateLocation(

server/src/policy/DefaultAccessPolicy.h
@@ -26,15 +26,15 @@ public:
 	void assureRemove(const PolicyContext &context,
 		const Place &place) override;
 
-	void assureGet(const User &user,
+	void assureGet(const PolicyContext &context,
 		const Gateway &gateway) override;
-	void assureAssignGateway(const User &user,
+	void assureAssignGateway(const PolicyContext &context,
 		const Place &place) override;
-	void assureUnassign(const User &user,
+	void assureUnassign(const PolicyContext &context,
 		const Gateway &gateway) override;
-	void assureUpdate(const User &user,
+	void assureUpdate(const PolicyContext &context,
 		const Gateway &gateway) override;
-	void assureScanDevices(const User &user,
+	void assureScanDevices(const PolicyContext &context,
 		const Gateway &gateway) override;
 
 	void assureGet(const User &user,

server/src/policy/GatewayAccessPolicy.h
@@ -7,22 +7,24 @@
 
 namespace BeeeOn {
 
+class PolicyContext;
+
 class GatewayAccessPolicy {
 public:
 	virtual void assureGet(
-			const User &user,
+			const PolicyContext &context,
 			const Gateway &gateway) = 0;
 	virtual void assureAssignGateway(
-			const User &user,
+			const PolicyContext &context,
 			const Place &place) = 0;
 	virtual void assureUnassign(
-			const User &user,
+			const PolicyContext &context,
 			const Gateway &gateway) = 0;
 	virtual void assureUpdate(
-			const User &user,
+			const PolicyContext &context,
 			const Gateway &gateway) = 0;
 	virtual void assureScanDevices(
-			const User &user,
+			const PolicyContext &context,
 			const Gateway &gateway) = 0;
 };
 

server/src/ui/GatewayHandler.cpp
@@ -29,8 +29,9 @@ const string GatewayHandler::handleAssign(istream &in,
 	JSONGatewayDeserializer update(in);
 	RelationWithData<Gateway, Place> input(gateway, update, place);
 	User user(userId);
+	input.setUser(user);
 
-	m_accessPolicy->assureAssignGateway(user, place);
+	m_accessPolicy->assureAssignGateway(input, place);
 
 	if (!m_gatewayService->assignAndUpdate(input))
 		return "";
@@ -48,8 +49,9 @@ const string GatewayHandler::handleUpdate(istream &in,
 	JSONGatewayDeserializer update(in);
 	RelationWithData<Gateway, Place> input(gateway, update, place);
 	User user(userId);
+	input.setUser(user);
 
-	m_accessPolicy->assureUpdate(user, gateway);
+	m_accessPolicy->assureUpdate(input, gateway);
 
 	if (!m_gatewayService->updateInPlace(input)) {
 		throw Exception("failed to update gateway: "
@@ -68,8 +70,9 @@ const string GatewayHandler::handleGet(
 	Gateway gateway(GatewayID::parse(gatewayId));
 	Relation<Gateway, Place> input(gateway, place);
 	User user(userId);
+	input.setUser(user);
 
-	m_accessPolicy->assureGet(user, gateway);
+	m_accessPolicy->assureGet(input, gateway);
 
 	if (!m_gatewayService->fetchFromPlace(input))
 		return "";
@@ -86,8 +89,9 @@ const string GatewayHandler::handleDelete(
 	Gateway gateway(GatewayID::parse(gatewayId));
 	Relation<Gateway, Place> input(gateway, place);
 	User user(userId);
+	input.setUser(user);
 
-	m_accessPolicy->assureUnassign(user, gateway);
+	m_accessPolicy->assureUnassign(input, gateway);
 
 	if (!m_gatewayService->unassign(input))
 		return "";

commit 4ccea7a0cb8f327916bd31e0f88eb31e57b40a42

GatewayXmlHandler: provide user to PolicyContext
When the policy handling is moved to the service layer the tests
stop working because of a missing user instance.

Fixes: f3810ca7a5e4 ("GatewayAccessPolicy: abstract User to PolicyContext")
Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

server/src/xmlui/GatewayXmlHandler.cpp
@@ -68,6 +68,7 @@ void GatewayXmlHandler::handleRegister(Element *gatewayNode)
 	XmlGatewayDeserializer deserializer(*gatewayNode);
 	SingleWithData<Gateway> input(gateway, deserializer);
 	User user(session()->userID());
+	input.setUser(user);
 
 	// approved to everybody, such gateway is associated with a
 	// place where the user is admin or to a new implicit place
@@ -156,6 +157,7 @@ void GatewayXmlHandler::handleGetAll()
 	vector<Gateway> gateways;
 	User user(session()->userID());
 	Relation<vector<Gateway>, User> input(gateways, user);
+	input.setUser(user);
 
 	m_gatewayService.fetchAccessible(input);
 