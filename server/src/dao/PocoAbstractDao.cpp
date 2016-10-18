05bad8ca9fc1efb65c2a2a9ed6a74282ecefad86
Jan Viktorin <iviktorin@fit.vutbr.cz>
2016-10-18 11:22:47 +0200

    PocoAbstractDao: new class
    The PocoAbstractDao is a base class for all PocoSQL*Dao implementations.
    The important is the method execute() that handles exceptions and provides
    backtraces for easier debugging.
    
    Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

---
+++ server/src/dao/PocoAbstractDao.cpp
@@ -0,0 +1,73 @@
+#include <Poco/Exception.h>
+
+#include "dao/PocoAbstractDao.h"
+#include "dao/PocoDaoManager.h"
+#include "Debug.h"
+
+using namespace std;
+using namespace Poco;
+using namespace Poco::Data;
+using namespace BeeeOn;
+
+PocoAbstractDao::PocoAbstractDao():
+	m_logger(LOGGER_CLASS(this)),
+	m_manager(NULL)
+{
+	injector<PocoAbstractDao, PocoDaoManager>(
+		"daoManager",
+		&PocoAbstractDao::setDaoManager
+	);
+}
+
+PocoAbstractDao::~PocoAbstractDao()
+{
+}
+
+void PocoAbstractDao::setDaoManager(PocoDaoManager *manager)
+{
+	m_manager = manager;
+}
+
+PocoDaoManager &PocoAbstractDao::manager()
+{
+	if (m_manager)
+		return *m_manager;
+
+	throw IllegalStateException("missing manager");
+}
+
+size_t PocoAbstractDao::execute(Statement &sql)
+{
+	size_t result;
+
+	try {
+		result = sql.execute();
+
+		if (m_logger.debug()) {
+			string prefix("[result: ");
+			prefix += to_string(result) + "] ";
+			m_logger.debug(prefix + sql.toString(),
+					__FILE__, __LINE__);
+		}
+
+		return result;
+	}
+	catch (const Exception &e) {
+		m_logger.log(e, __FILE__, __LINE__);
+		m_logger.error(sql.toString(), __FILE__, __LINE__);
+		log_backtrace(m_logger);
+		throw;
+	}
+}
+
+void PocoAbstractDao::throwMissingId(const type_info &t)
+{
+	log_backtrace(m_logger);
+	throw InvalidArgumentException("missing id for "
+			+ BeeeOn::classDemangle(t.name()));
+}
+
+double PocoAbstractDao::nanWhenEmpty(const Poco::Dynamic::Var &v) const
+{
+	return v.isEmpty()? (double) NAN : v.convert<double>();
+}
