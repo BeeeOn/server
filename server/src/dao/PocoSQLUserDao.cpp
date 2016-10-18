e0be1db375125a55e5e5620f93aed7961b1ed3a1
Jan Viktorin <iviktorin@fit.vutbr.cz>
2016-10-18 15:37:31 +0200

    PocoSQLUserDao: new class
    Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

---
+++ server/src/dao/PocoSQLUserDao.cpp
@@ -0,0 +1,85 @@
+#include <Poco/Exception.h>
+#include <Poco/Data/Session.h>
+#include <Poco/Data/SessionPool.h>
+#include <Poco/Data/Statement.h>
+
+#include "dao/UserDao.h"
+#include "dao/PocoAbstractDao.h"
+#include "dao/PocoDaoManager.h"
+
+using namespace std;
+using namespace Poco;
+using namespace Poco::Data;
+using namespace Poco::Data::Keywords;
+using namespace BeeeOn;
+
+namespace BeeeOn {
+
+class PocoSQLUserDao :
+		public PocoAbstractDao,
+		public UserDao {
+public:
+	void create(User &user) override;
+	bool fetch(User &user) override;
+
+protected:
+	void create(Session &session, User &user);
+	bool fetch(Session &session, User &user);
+};
+
+}
+
+BEEEON_OBJECT(PocoSQLUserDao, BeeeOn::PocoSQLUserDao)
+
+void PocoSQLUserDao::create(User &user)
+{
+	Session session(manager().pool().get());
+	create(session, user);
+}
+
+bool PocoSQLUserDao::fetch(User &user)
+{
+	Session session(manager().pool().get());
+	return fetch(session, user);
+}
+
+void PocoSQLUserDao::create(Session &session, User &user)
+{
+	user = User(UserID::random(), user);
+	string id(user.id().toString());
+	string firstName(user.firstName());
+	string lastName(user.lastName());
+
+	Statement sql(session);
+	sql << "INSERT INTO users"
+		" (id, first_name, last_name)"
+		" VALUES"
+		" (:id, :first_name, :last_name)",
+		use(id, "id"),
+		use(firstName),
+		use(lastName);
+
+	execute(sql);
+}
+
+bool PocoSQLUserDao::fetch(Session &session, User &user)
+{
+	assureHasId(user);
+	string id(user.id().toString());
+	string firstName;
+	string lastName;
+
+	Statement sql(session);
+	sql << "SELECT first_name, last_name FROM users"
+		" WHERE id = :id",
+		use(id, "id"),
+		into(firstName),
+		into(lastName);
+
+	if (execute(sql) == 0)
+		return false;
+
+	user.setFirstName(firstName);
+	user.setLastName(lastName);
+	return true;
+}
