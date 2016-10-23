BeeeOn/server

commit 623627caffd0bced10a18efeec53571aed4bee60

AuthXmlHandler: new class
Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

server/src/xmlui/AuthXmlHandler.cpp
@@ -1,0 +1,78 @@
+#include "xmlui/AuthXmlHandler.h"
+
+using namespace std;
+using namespace BeeeOn;
+using namespace BeeeOn::XmlUI;
+using namespace Poco;
+using namespace Poco::Net;
+using namespace Poco::XML;
+
+AuthXmlHandler::AuthXmlHandler(
+		const StreamSocket &socket,
+		const AutoPtr<Document> input,
+		BeeeOn::AuthService &service):
+	AbstractXmlHandler("accounts", socket, input),
+	m_authService(service)
+{
+}
+
+void AuthXmlHandler::handleInputImpl()
+{
+	Element *root = m_input->documentElement();
+	const string &type = root->getAttribute("type");
+
+	if (type == "logout") {
+		m_authService.logout(root->getAttribute("sessionid"));
+		resultSuccess();
+	}
+	else if (type == "login") {
+		Element *provider = root->getChildElement("provider");
+		if (provider == NULL) {
+			resultInvalidInput();
+			return;
+		}
+
+		AuthCodeCredentials credentials(
+			provider->getAttribute("name"),
+			provider->getAttribute("authCode")
+		);
+
+		const string session = m_authService.login(credentials);
+	
+		AttributesImpl attrs;
+		attrs.addAttribute("", "sessionid", "sessionid", "", session);
+		resultSimple(attrs, "ok");
+	}
+	else
+		resultInvalidInput();
+}
+
+AuthXmlHandlerResolver::AuthXmlHandlerResolver():
+	AbstractXmlHandlerResolver("accounts")
+{
+	injector<AuthXmlHandlerResolver, AuthService>("authService",
+			&AuthXmlHandlerResolver::setAuthService);
+}
+
+bool AuthXmlHandlerResolver::canHandle(const Element &root)
+{
+	if (!AbstractXmlHandlerResolver::canHandle(root))
+		return false;
+
+	if (root.getAttribute("type") == "login")
+		return true;
+
+	if (root.getAttribute("type") == "logout")
+		return true;
+
+	return false;
+}
+
+XmlRequestHandler *AuthXmlHandlerResolver::createHandler(
+		const StreamSocket &socket,
+		const AutoPtr<Document> input)
+{
+	return new AuthXmlHandler(socket, input, *m_authService);
+}
+
+BEEEON_OBJECT(AuthXmlHandlerResolver, BeeeOn::XmlUI::AuthXmlHandlerResolver)

server/conf/services.xml
@@ -36,11 +36,17 @@
 
 		<instance name="xmlui"
 				class="BeeeOn::XmlRequestHandlerFactory">
+			<add name="resolvers" ref="authXmlHandler" />
 			<add name="resolvers" ref="anyXmlHandler" />
 		</instance>
 
 		<instance name="anyXmlHandler"
 				class="BeeeOn::XmlUI::AnyXmlHandlerResolver" />
+
+		<instance name="authXmlHandler"
+				class="BeeeOn::XmlUI::AuthXmlHandlerResolver">
+			<set name="authService" ref="authService" />
+		</instance>
 
 		<instance name="uiMockInit"
 				class="BeeeOn::UIMockInit"

server/src/CMakeLists.txt
@@ -77,6 +77,7 @@ add_executable(xmlui-server
 	xmlui/main.cpp
 	xmlui/AbstractXmlHandler.cpp
 	xmlui/AnyXmlHandler.cpp
+	xmlui/AuthXmlHandler.cpp
 	ui/UIMockInit.cpp
 )
 

server/src/xmlui/AuthXmlHandler.h
@@ -1,0 +1,43 @@
+#ifndef BEEEON_XMLUI_AUTH_XML_HANDLER_H
+#define BEEEON_XMLUI_AUTH_XML_HANDLER_H
+
+#include "xmlui/AbstractXmlHandler.h"
+#include "service/AuthService.h"
+
+namespace BeeeOn {
+namespace XmlUI {
+
+class AuthXmlHandler : public AbstractXmlHandler {
+public:
+	AuthXmlHandler(const Poco::Net::StreamSocket &socket,
+			const Poco::AutoPtr<Poco::XML::Document> input,
+			BeeeOn::AuthService &service);
+
+	void handleInputImpl() override;
+
+private:
+	BeeeOn::AuthService &m_authService;
+};
+
+class AuthXmlHandlerResolver : public AbstractXmlHandlerResolver {
+public:
+	AuthXmlHandlerResolver();
+
+	bool canHandle(const Poco::XML::Element &input);
+	XmlRequestHandler *createHandler(
+			const Poco::Net::StreamSocket &socket,
+			const Poco::AutoPtr<Poco::XML::Document> input);
+
+	void setAuthService(BeeeOn::AuthService *service)
+	{
+		m_authService = service;
+	}
+
+private:
+	BeeeOn::AuthService *m_authService;
+};
+
+}
+}
+
+#endif

commit ebe0af4271bbf14070c4b2905e75816da67e90ac

AuthXmlHandler: fix whitespace
Fixes: 623627caffd0 ("AuthXmlHandler: new class")
Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

server/src/xmlui/AuthXmlHandler.cpp
@@ -44,7 +44,7 @@ void AuthXmlHandler::handleInputImpl()
 			resultNotAuthenticated();
 			return;
 		}
-	
+
 		AttributesImpl attrs;
 		attrs.addAttribute("", "sessionid", "sessionid", "",
 				session->sessionID());