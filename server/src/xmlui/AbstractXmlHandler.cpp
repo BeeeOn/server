BeeeOn/server

commit a406c0060f5b042b9f824572e264bea195014e70

AbstractXmlHandler: new class
Base for all XML handlers that is aware of sessions and provides
many reusable methods for generating error messages etc.

Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

server/src/xmlui/AbstractXmlHandler.cpp
@@ -1,0 +1,198 @@
+#include <Poco/Exception.h>
+#include <Poco/Net/NetException.h>
+#include <Poco/SAX/AttributesImpl.h>
+
+#include "xmlui/AbstractXmlHandler.h"
+#include "Debug.h"
+
+using namespace std;
+using namespace Poco;
+using namespace Poco::Net;
+using namespace Poco::XML;
+using namespace BeeeOn;
+using namespace BeeeOn::XmlUI;
+
+AbstractXmlHandler::AbstractXmlHandler(
+		const string &ns,
+		const StreamSocket &socket,
+		const AutoPtr<Document> input):
+	BeeeOn::XmlRequestHandler(socket, input),
+	m_logger(LOGGER_CLASS(this)),
+	m_ns(ns)
+{
+}
+
+AbstractXmlHandler::AbstractXmlHandler(
+		const string &ns,
+		const StreamSocket &socket,
+		const AutoPtr<Document> input,
+		BeeeOn::ExpirableSession::Ptr session):
+	BeeeOn::XmlRequestHandler(socket, input),
+	m_logger(LOGGER_CLASS(this)),
+	m_ns(ns),
+	m_session(session)
+{
+}
+
+void AbstractXmlHandler::handleInput()
+{
+	try {
+		handleInputImpl();
+	}
+	catch (const NotAuthenticatedException &e) {
+		m_logger.log(e, __FILE__, __LINE__);
+		resultNotAuthenticated();
+	}
+	catch (const InvalidAccessException &e) {
+		m_logger.log(e, __FILE__, __LINE__);
+		resultForbidden();
+	}
+	catch (const InvalidArgumentException &e) {
+		m_logger.log(e, __FILE__, __LINE__);
+		resultInvalidInput();
+	}
+	catch (const Exception &e) {
+		m_logger.log(e, __FILE__, __LINE__);
+		resultUnexpected();
+	}
+	catch (const exception &e) {
+		m_logger.critical(e.what(), __FILE__, __LINE__);
+		resultUnexpected();
+	}
+	catch (const char *s) {
+		m_logger.critical(s, __FILE__, __LINE__);
+		resultUnexpected();
+	}
+	catch (...) {
+		m_logger.critical("unknown error, cought '...'",
+				__FILE__, __LINE__);
+		resultUnexpected();
+	}
+}
+
+bool AbstractXmlHandler::requireSession()
+{
+	if (!hasSession()) {
+		resultNotAuthenticated();
+		return false;
+	}
+
+	return true;
+}
+
+void AbstractXmlHandler::resultSimple(AttributesImpl &attrs,
+		const std::string &result)
+{
+	attrs.addAttribute("", "ns", "ns", "", m_ns);
+	attrs.addAttribute("", "version", "version", "", "1.0.0");
+	attrs.addAttribute("", "result", "result", "", result);
+
+	m_output.emptyElement("", "response", "response", attrs);
+}
+
+void AbstractXmlHandler::resultSuccess()
+{
+	AttributesImpl attrs;
+	resultSimple(attrs, "ok");
+}
+
+void AbstractXmlHandler::resultDataStart()
+{
+	AttributesImpl attrs;
+	attrs.addAttribute("", "ns", "ns", "", m_ns);
+	attrs.addAttribute("", "version", "version", "", "1.0.0");
+	attrs.addAttribute("", "result", "result", "", "data");
+
+	m_output.startElement("", "response", "response", attrs);
+}
+
+void AbstractXmlHandler::resultDataEnd()
+{
+	m_output.endElement("", "response", "response");
+}
+
+void AbstractXmlHandler::resultNotAuthenticated()
+{
+	resultError("20"); // resign
+}
+
+void AbstractXmlHandler::resultForbidden()
+{
+	resultError("1001");
+}
+
+void AbstractXmlHandler::resultUnexpected()
+{
+	resultError("998"); // unknown
+}
+
+void AbstractXmlHandler::resultInvalidInput()
+{
+	resultError("998"); // FIXME
+}
+
+
+void AbstractXmlHandler::resultNotFound()
+{
+	resultError("13");
+}
+
+void AbstractXmlHandler::resultAlreadyAssigned()
+{
+	resultError("31");
+}
+
+void AbstractXmlHandler::resultNotOwned()
+{
+	resultError("6");
+}
+
+void AbstractXmlHandler::resultError(const string &code)
+{
+	AttributesImpl attrs;
+	attrs.addAttribute("", "errcode", "errcode", "", code);
+	resultSimple(attrs, "error");
+}
+
+AbstractXmlHandlerResolver::AbstractXmlHandlerResolver(
+		const string &ns):
+	m_ns(ns)
+{
+}
+
+bool AbstractXmlHandlerResolver::canHandle(
+		const Document &doc)
+{
+	Element *root = doc.documentElement();
+	if (root == NULL)
+		return false;
+
+	return canHandle(*root);
+}
+
+bool AbstractXmlHandlerResolver::canHandle(
+		const Element &root)
+{
+	if (root.localName() != "request")
+		return false;
+
+	if (root.getAttribute("ns") != m_ns)
+		return false;
+
+	return true;
+
+}
+
+ExpirableSession::Ptr AbstractXmlHandlerResolver::lookupSession(
+		SessionManager &sessionManager,
+		const AutoPtr<Document> input)
+{
+	const Element *root = input->documentElement();
+	const string &sessionId = root->getAttribute("sessionid");
+	ExpirableSession::Ptr session;
+
+	if (!sessionManager.lookup(sessionId, session))
+		return NULL;
+
+	return session;
+}

server/src/xmlui/AbstractXmlHandler.h
@@ -1,0 +1,82 @@
+#ifndef BEEEON_XMLUI_ABSTRACT_XML_HANDLER_H
+#define BEEEON_XMLUI_ABSTRACT_XML_HANDLER_H
+
+#include <Poco/Logger.h>
+#include <Poco/SAX/AttributesImpl.h>
+
+#include "server/XmlRequestHandler.h"
+#include "server/Session.h"
+#include "server/SessionManager.h"
+
+namespace BeeeOn {
+namespace XmlUI {
+
+class AbstractXmlHandler : public BeeeOn::XmlRequestHandler {
+public:
+	AbstractXmlHandler(
+			const std::string &ns,
+			const Poco::Net::StreamSocket &socket,
+			const Poco::AutoPtr<Poco::XML::Document> input);
+	AbstractXmlHandler(
+			const std::string &ns,
+			const Poco::Net::StreamSocket &socket,
+			const Poco::AutoPtr<Poco::XML::Document> input,
+			BeeeOn::ExpirableSession::Ptr session);
+
+	virtual void handleInput();
+	virtual void handleInputImpl() = 0;
+
+	bool requireSession();
+
+	bool hasSession() const
+	{
+		return !m_session.isNull();
+	}
+
+	const BeeeOn::ExpirableSession::Ptr session() const
+	{
+		return m_session;
+	}
+
+	void resultSimple(Poco::XML::AttributesImpl &attrs,
+			const std::string &result);
+	void resultSuccess();
+	void resultNotAuthenticated();
+	void resultForbidden();
+	void resultInvalidInput();
+	void resultNotFound();
+	void resultAlreadyAssigned();
+	void resultNotOwned();
+	void resultUnexpected();
+	void resultError(const std::string &code);
+
+	void resultDataStart();
+	void resultDataEnd();
+
+protected:
+	Poco::Logger &m_logger;
+	const std::string m_ns;
+	BeeeOn::ExpirableSession::Ptr m_session;
+};
+
+class AbstractXmlHandlerResolver : public BeeeOn::XmlRequestHandlerResolver {
+public:
+	AbstractXmlHandlerResolver(
+			const std::string &ns);
+
+	virtual bool canHandle(const Poco::XML::Document &doc);
+	virtual bool canHandle(const Poco::XML::Element &root);
+
+protected:
+	BeeeOn::ExpirableSession::Ptr lookupSession(
+			BeeeOn::SessionManager &sessionManager,
+			const Poco::AutoPtr<Poco::XML::Document> input);
+
+protected:
+	const std::string m_ns;
+};
+
+}
+}
+
+#endif

server/src/CMakeLists.txt
@@ -75,6 +75,7 @@ add_executable(ui-server
 
 add_executable(xmlui-server
 	xmlui/main.cpp
+	xmlui/AbstractXmlHandler.cpp
 	ui/UIMockInit.cpp
 )
 
@@ -99,10 +100,21 @@ if (APPLE)
 		BeeeOnServer
 		${LIBS}
 	)
+	target_link_libraries(xmlui-server
+		-Wl,-all_load
+		BeeeOnServer
+		${LIBS}
+	)
 # The -Wl,--whole-archive is necessary for all libraries defining
 # BEEEON_OBJECTs (see di/InjectorTarget.h).
 else()
 	target_link_libraries(ui-server
+		-Wl,--whole-archive
+		BeeeOnServer
+		-Wl,--no-whole-archive
+		${LIBS}
+	)
+	target_link_libraries(xmlui-server
 		-Wl,--whole-archive
 		BeeeOnServer
 		-Wl,--no-whole-archive

commit ba3ddf88ec42c98083db1db331ebae9d5a4e36b7

AbstractXmlHandler: derive response type from request
The request/@type attribute is to appear in the response
so copy it to the output root element attribute set.

Fixes: a406c0060f5b ("AbstractXmlHandler: new class")
Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

server/src/xmlui/AbstractXmlHandler.cpp
@@ -84,12 +84,20 @@ bool AbstractXmlHandler::requireSession()
 	return true;
 }
 
+void AbstractXmlHandler::deriveType(Poco::XML::AttributesImpl &attrs)
+{
+	const Element *root = m_input->documentElement();
+	attrs.addAttribute("", "type", "type", "",
+			root->getAttribute("type"));
+}
+
 void AbstractXmlHandler::resultSimple(AttributesImpl &attrs,
 		const std::string &result)
 {
 	attrs.addAttribute("", "ns", "ns", "", m_ns);
 	attrs.addAttribute("", "version", "version", "", "1.0.0");
 	attrs.addAttribute("", "result", "result", "", result);
+	deriveType(attrs);
 
 	m_output.emptyElement("", "response", "response", attrs);
 }
@@ -106,6 +114,7 @@ void AbstractXmlHandler::resultDataStart()
 	attrs.addAttribute("", "ns", "ns", "", m_ns);
 	attrs.addAttribute("", "version", "version", "", "1.0.0");
 	attrs.addAttribute("", "result", "result", "", "data");
+	deriveType(attrs);
 
 	m_output.startElement("", "response", "response", attrs);
 }

server/src/xmlui/AbstractXmlHandler.h
@@ -38,6 +38,8 @@ public:
 		return m_session;
 	}
 
+	void deriveType(Poco::XML::AttributesImpl &attrs);
+
 	void resultSimple(Poco::XML::AttributesImpl &attrs,
 			const std::string &result);
 	void resultSuccess();