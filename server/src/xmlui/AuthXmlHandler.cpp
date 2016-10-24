BeeeOn/server

commit 623627caffd0bced10a18efeec53571aed4bee60

AuthXmlHandler: new class
Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

server/src/xmlui/AuthXmlHandler.cpp
@@ -1,0 +1,78 @@
+#include "xmlui/AuthXmlHandler.h"
+
+using namespace std;
+using namespace BeeeOn;
+using namespace BeeeOn::XmlUI;
+using namespace Poco;
+using namespace Poco::Net;
+using namespace Poco::XML;
+
+AuthXmlHandler::AuthXmlHandler(
+		const StreamSocket &socket,
+		const AutoPtr<Document> input,
+		BeeeOn::AuthService &service):
+	AbstractXmlHandler("accounts", socket, input),
+	m_authService(service)
+{
+}
+
+void AuthXmlHandler::handleInputImpl()
+{
+	Element *root = m_input->documentElement();
+	const string &type = root->getAttribute("type");
+
+	if (type == "logout") {
+		m_authService.logout(root->getAttribute("sessionid"));
+		resultSuccess();
+	}
+	else if (type == "login") {
+		Element *provider = root->getChildElement("provider");
+		if (provider == NULL) {
+			resultInvalidInput();
+			return;
+		}
+
+		AuthCodeCredentials credentials(
+			provider->getAttribute("name"),
+			provider->getAttribute("authCode")
+		);
+
+		const string session = m_authService.login(credentials);
+	
+		AttributesImpl attrs;
+		attrs.addAttribute("", "sessionid", "sessionid", "", session);
+		resultSimple(attrs, "ok");
+	}
+	else
+		resultInvalidInput();
+}
+
+AuthXmlHandlerResolver::AuthXmlHandlerResolver():
+	AbstractXmlHandlerResolver("accounts")
+{
+	injector<AuthXmlHandlerResolver, AuthService>("authService",
+			&AuthXmlHandlerResolver::setAuthService);
+}
+
+bool AuthXmlHandlerResolver::canHandle(const Element &root)
+{
+	if (!AbstractXmlHandlerResolver::canHandle(root))
+		return false;
+
+	if (root.getAttribute("type") == "login")
+		return true;
+
+	if (root.getAttribute("type") == "logout")
+		return true;
+
+	return false;
+}
+
+XmlRequestHandler *AuthXmlHandlerResolver::createHandler(
+		const StreamSocket &socket,
+		const AutoPtr<Document> input)
+{
+	return new AuthXmlHandler(socket, input, *m_authService);
+}
+
+BEEEON_OBJECT(AuthXmlHandlerResolver, BeeeOn::XmlUI::AuthXmlHandlerResolver)

server/conf/services.xml
@@ -36,11 +36,17 @@
 
 		<instance name="xmlui"
 				class="BeeeOn::XmlRequestHandlerFactory">
+			<add name="resolvers" ref="authXmlHandler" />
 			<add name="resolvers" ref="anyXmlHandler" />
 		</instance>
 
 		<instance name="anyXmlHandler"
 				class="BeeeOn::XmlUI::AnyXmlHandlerResolver" />
+
+		<instance name="authXmlHandler"
+				class="BeeeOn::XmlUI::AuthXmlHandlerResolver">
+			<set name="authService" ref="authService" />
+		</instance>
 
 		<instance name="uiMockInit"
 				class="BeeeOn::UIMockInit"

server/src/CMakeLists.txt
@@ -77,6 +77,7 @@ add_executable(xmlui-server
 	xmlui/main.cpp
 	xmlui/AbstractXmlHandler.cpp
 	xmlui/AnyXmlHandler.cpp
+	xmlui/AuthXmlHandler.cpp
 	ui/UIMockInit.cpp
 )
 

server/src/xmlui/AuthXmlHandler.h
@@ -1,0 +1,43 @@
+#ifndef BEEEON_XMLUI_AUTH_XML_HANDLER_H
+#define BEEEON_XMLUI_AUTH_XML_HANDLER_H
+
+#include "xmlui/AbstractXmlHandler.h"
+#include "service/AuthService.h"
+
+namespace BeeeOn {
+namespace XmlUI {
+
+class AuthXmlHandler : public AbstractXmlHandler {
+public:
+	AuthXmlHandler(const Poco::Net::StreamSocket &socket,
+			const Poco::AutoPtr<Poco::XML::Document> input,
+			BeeeOn::AuthService &service);
+
+	void handleInputImpl() override;
+
+private:
+	BeeeOn::AuthService &m_authService;
+};
+
+class AuthXmlHandlerResolver : public AbstractXmlHandlerResolver {
+public:
+	AuthXmlHandlerResolver();
+
+	bool canHandle(const Poco::XML::Element &input);
+	XmlRequestHandler *createHandler(
+			const Poco::Net::StreamSocket &socket,
+			const Poco::AutoPtr<Poco::XML::Document> input);
+
+	void setAuthService(BeeeOn::AuthService *service)
+	{
+		m_authService = service;
+	}
+
+private:
+	BeeeOn::AuthService *m_authService;
+};
+
+}
+}
+
+#endif

commit ac1b5e35412917b9f5a68dc416e08389d230602b

AuthXmlHandler: fix login response
According to the original implementation, the successful response
to the login request is result="data" and an element session with
an attribute id.

All tests are fixed to expect this behaviour.

Fixes: 623627caffd0 ("AuthXmlHandler: new class")
Fixes: 31cedda3558a ("t: xmlui: test login and logout")
Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

server/src/xmlui/AuthXmlHandler.cpp
@@ -45,10 +45,13 @@ void AuthXmlHandler::handleInputImpl()
 			return;
 		}
 
+		resultDataStart();
+
 		AttributesImpl attrs;
-		attrs.addAttribute("", "sessionid", "sessionid", "",
-				session->sessionID());
-		resultSimple(attrs, "ok");
+		attrs.addAttribute("", "id", "id", "", session->sessionID());
+		m_output.emptyElement("", "session", "session", attrs);
+
+		resultDataEnd();
 	}
 	else
 		resultInvalidInput();

server/t/xmlui.py
@@ -45,7 +45,11 @@ class Response:
 		return self.root.get("ns")
 
 	def sessionid(self):
-		return self.root.get("sessionid")
+		if self.root[0].tag != "session":
+			raise Exception("no 'session' tag: " +
+					self.root[0].tag)
+
+		return self.root[0].get("id")
 
 	def is_ok(self):
 		return self.root.get("result") == "ok"

server/t/xmlui/t0001-auth.py
@@ -15,6 +15,9 @@ def login(login):
 	if response.is_error():
 		return False, response.error_code()
 
+	if not response.is_data():
+		raise Exception("unexpected result of login: " + str(response))
+
 	return True, response.sessionid()
 
 def logout(session):

server/t/xmlui/t1001-gateway-register-unregister.py
@@ -16,11 +16,11 @@ class TestGatewayPlaceAssign(unittest.TestCase):
 		c = Connector(config.xmlui_host, config.xmlui_port)
 
 		response = c.request(config.PERMIT_LOGIN)
-		self.assertTrue(response.is_ok())
+		self.assertTrue(response.is_data())
 		self.session = response.sessionid()
 
 		response = c.request(config.PERMIT_LOGIN2)
-		self.assertTrue(response.is_ok())
+		self.assertTrue(response.is_data())
 		self.session2 = response.sessionid()
 
 	"""

server/t/xmlui/t1002-location-add-delete.py
@@ -17,7 +17,7 @@ class TestLocationAddDelete(unittest.TestCase):
 		c = Connector(config.xmlui_host, config.xmlui_port)
 
 		response = c.request(config.PERMIT_LOGIN)
-		self.assertTrue(response.is_ok())
+		self.assertTrue(response.is_data())
 		self.session = response.sessionid()
 
 		response = c.request(GatewayRegister(

server/t/xmlui/t2001-gateway-get-update.py
@@ -17,7 +17,7 @@ class TestGatewayGetUpdate(unittest.TestCase):
 		c = Connector(config.xmlui_host, config.xmlui_port)
 
 		response = c.request(config.PERMIT_LOGIN)
-		self.assertTrue(response.is_ok())
+		self.assertTrue(response.is_data())
 		self.session = response.sessionid()
 
 		# assure we have no gateway associated

server/t/xmlui/t2002-location-get-update.py
@@ -17,7 +17,7 @@ class TestLocationGetUpdate(unittest.TestCase):
 		c = Connector(config.xmlui_host, config.xmlui_port)
 
 		response = c.request(config.PERMIT_LOGIN)
-		self.assertTrue(response.is_ok())
+		self.assertTrue(response.is_data())
 		self.session = response.sessionid()
 
 		response = c.request(GatewayRegister(

server/t/xmlui/t2003-myprofile.py
@@ -16,11 +16,11 @@ class TestGetMyProfile(unittest.TestCase):
 		c = Connector(config.xmlui_host, config.xmlui_port)
 
 		response = c.request(config.PERMIT_LOGIN)
-		self.assertTrue(response.is_ok())
+		self.assertTrue(response.is_data())
 		self.session = response.sessionid()
 
 		response = c.request(config.PERMIT_LOGIN2)
-		self.assertTrue(response.is_ok())
+		self.assertTrue(response.is_data())
 		self.session2 = response.sessionid()
 
 	"""