BeeeOn/server

commit 20f92cbbf9eb2d8634b32b5187ba1925efb5b38a

GoogleAuthProvider: avoid JSONObjectSerializer
The JSONObjectSerializer is considered obsolete.

Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

server/src/provider/GoogleAuthProvider.cpp
@@ -8,10 +8,11 @@
 #include <Poco/Net/SSLManager.h>
 #include <Poco/Net/InvalidCertificateHandler.h>
 #include <Poco/Net/AcceptCertificateHandler.h>
+#include <Poco/JSON/Parser.h>
+#include <Poco/JSON/Object.h>
 #include <Poco/StreamCopier.h>
 
 #include "Debug.h"
-#include "model/JSONSerializer.h"
 #include "provider/GoogleAuthProvider.h"
 
 using namespace std;
@@ -19,6 +20,35 @@ using namespace Poco;
 using namespace Poco::JSON;
 using namespace Poco::Net;
 using namespace BeeeOn;
+
+bool GoogleAuthProvider::parseIdentity(const std::string &userInfo,
+		AuthResult &result)
+{
+	Parser parser;
+	Object::Ptr info = parser.parse(userInfo).extract<Object::Ptr>();
+
+	if (info->has("sub"))
+		result.setProviderID(info->getValue<string>("sub"));
+
+	if (info->has("email"))
+		result.setEmail(info->getValue<string>("email"));
+
+	if (result.email().empty() || result.providerID().empty())
+		return false;
+
+	string firstName;
+	string lastName;
+	string picture;
+
+	if (info->has("given_name"))
+		result.setFirstName(firstName);
+	if (info->has("family_name"))
+		result.setLastName(lastName);
+	if (info->has("picture"))
+		result.setPicture(picture);
+
+	return true;
+}
 
 bool GoogleAuthProvider::verifyAuthCode(const string &authCode, AuthResult &info)
 {
@@ -35,30 +65,9 @@ bool GoogleAuthProvider::verifyAuthCode(const string &authCode, AuthResult &info
 		return false;
 	}
 
-	JSONObjectSerializer userInfo(rawInfo);
-
-	if (userInfo.get("sub", google_id))
-		info.setProviderID(google_id);
-	if (userInfo.get("email", email))
-		info.setEmail(email);
-
-	if (email.empty() || google_id.empty())
-		return false;
-
 	info.setAccessToken(idToken);
 
-	string firstName;
-	string lastName;
-	string picture;
-
-	if (userInfo.get("given_name", firstName))
-		info.setFirstName(firstName);
-	if (userInfo.get("family_name", lastName))
-		info.setLastName(lastName);
-	if (userInfo.get("picture", picture))
-		info.setPicture(picture);
-
-	return true;
+	return parseIdentity(rawInfo, info);
 }
 
 string GoogleAuthProvider::requestIdToken(const string &authCode)
@@ -96,12 +105,14 @@ string GoogleAuthProvider::requestIdToken(const string &authCode)
 
 	m_logger.debug("response: " + receiveResponse, __FILE__, __LINE__);
 
-	JSONObjectSerializer userTokens(receiveResponse);
+	Parser parser;
+	Object::Ptr object = parser.parse(receiveResponse)
+			.extract<Object::Ptr>();
 	string idToken;
 
 	// TODO: verify signature of the ID token by Google certificate
-	if (userTokens.get("id_token", idToken))
-		return idToken;
+	if (object->has("id_token"))
+		return object->getValue<string>("id_token");
 
 	m_logger.error("No ID token to obtain user data", __FILE__, __LINE__);
 

server/src/provider/GoogleAuthProvider.h
@@ -47,6 +47,11 @@ private:
 	std::string requestIdToken(const std::string &token);
 
 	/**
+	 * Parse userInfo data and fill the AuthResult instance.
+	 */
+	bool parseIdentity(const std::string &userInfo, AuthResult &result);
+
+	/**
 	 * Handle response based on given session
 	 */
 	std::string handleResponse(Poco::Net::HTTPSClientSession &session);

commit 2d09c1309d53968e1903965f38ae6ff0306694d4

GoogleAuthProvider: fix extracting of user info
Fixes: 20f92cbbf9eb ("GoogleAuthProvider: avoid JSONObjectSerializer")
Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

server/src/provider/GoogleAuthProvider.cpp
@@ -30,16 +30,12 @@ bool GoogleAuthProvider::parseIdentity(const std::string &userInfo,
 	if (result.email().empty() || result.providerID().empty())
 		return false;
 
-	string firstName;
-	string lastName;
-	string picture;
-
 	if (info->has("given_name"))
-		result.setFirstName(firstName);
+		result.setFirstName(info->getValue<string>("given_name"));
 	if (info->has("family_name"))
-		result.setLastName(lastName);
+		result.setLastName(info->getValue<string>("family_name"));
 	if (info->has("picture"))
-		result.setPicture(picture);
+		result.setPicture(info->getValue<string>("picture"));
 
 	return true;
 }