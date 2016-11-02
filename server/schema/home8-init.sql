BeeeOn/server

commit 5dcd4c80a3a0a266fb371dc46d504d4bd0b97d3a

schema: add table verified_identities

server/schema/home8-init.sql
@@ -35,3 +35,14 @@ CREATE TABLE identities (
 	email       varchar(250) NOT NULL,
 	CONSTRAINT unique_identity UNIQUE (email)
 );
+
+DROP TABLE IF EXISTS verified_identities;
+CREATE TABLE verified_identities (
+	id          uuid NOT NOT primary key;
+	identity_id uuid NOT NULL REFERENCES identities(id),
+	user_id     uuid NOT NULL REFERENCES users(id),
+	provider    varchar(250) NOT NULL,
+	picture     varchar(250),
+	accessToken varchar(250)
+	CONSTRAINT unique_verified_identity UNIQUE (identity_id, provider)
+);

commit a1f82de0bfccf08422f5c2e8e47b9077222fedb3

schema: use underscore naming convention
Fixes: 5dcd4c80a3a0 ("schema: add table verified_identities")
Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

server/schema/home8-init.sql
@@ -42,7 +42,7 @@ CREATE TABLE verified_identities (
 	user_id     ${type_uuid} NOT NULL REFERENCES users(id),
 	provider    varchar(250) NOT NULL,
 	picture     varchar(250),
-	accessToken varchar(250),
+	access_token varchar(250),
 	CONSTRAINT unique_verified_identity UNIQUE (identity_id, provider)
 );
 