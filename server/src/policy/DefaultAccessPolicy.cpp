BeeeOn/server

commit af67014edfd5e0ae5d79b6b5e02bd68d6f0f5b33

DefaultAccessPolicy: implement GatewayAccessPolicy
Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

server/src/policy/DefaultAccessPolicy.cpp
@@ -11,6 +11,8 @@ DefaultAccessPolicy::DefaultAccessPolicy()
 {
 	injector<DefaultAccessPolicy, UserDao>("userDao",
 			&DefaultAccessPolicy::setUserDao);
+	injector<DefaultAccessPolicy, GatewayDao>("gatewayDao",
+			&DefaultAccessPolicy::setGatewayDao);
 	injector<DefaultAccessPolicy, RoleInPlaceDao>("roleInPlaceDao",
 			&DefaultAccessPolicy::setRoleInPlaceDao);
 }
@@ -68,4 +70,61 @@ void DefaultAccessPolicy::assureRemove(
 	assureAtLeast(
 		fetchAccessLevel(session, place),
 		AccessLevel::admin());
+}
+
+void DefaultAccessPolicy::assureAssignGateway(
+		const ExpirableSession::Ptr session,
+		const Place &place)
+{
+	assureAtLeast(
+		fetchAccessLevel(session, place),
+		AccessLevel::admin());
+}
+
+void DefaultAccessPolicy::assureGet(
+		const ExpirableSession::Ptr session,
+		const Gateway &gateway)
+{
+	Gateway tmp(gateway);
+	if (!m_gatewayDao->fetch(tmp))
+		throw InvalidAccessException("no such gateway "
+				+ gateway.id().toString());
+
+	const Place place(tmp.place());
+
+	assureAtLeast(
+		fetchAccessLevel(session, place),
+		AccessLevel::guest());
+}
+
+void DefaultAccessPolicy::assureUnassign(
+		const ExpirableSession::Ptr session,
+		const Gateway &gateway)
+{
+	Gateway tmp(gateway);
+	if (!m_gatewayDao->fetch(tmp))
+		throw InvalidAccessException("no such gateway "
+				+ gateway.id().toString());
+
+	const Place place(tmp.place());
+
+	assureAtLeast(
+		fetchAccessLevel(session, place),
+		AccessLevel::admin());
+}
+
+void DefaultAccessPolicy::assureUpdate(
+		const ExpirableSession::Ptr session,
+		const Gateway &gateway)
+{
+	Gateway tmp(gateway);
+	if (!m_gatewayDao->fetch(tmp))
+		throw InvalidAccessException("no such gateway "
+				+ gateway.id().toString());
+
+	const Place place(tmp.place());
+
+	assureAtLeast(
+		fetchAccessLevel(session, place),
+		AccessLevel::user());
 }

server/src/policy/DefaultAccessPolicy.h
@@ -4,13 +4,16 @@
 #include "di/InjectorTarget.h"
 #include "server/Session.h"
 #include "policy/PlaceAccessPolicy.h"
+#include "policy/GatewayAccessPolicy.h"
 #include "dao/RoleInPlaceDao.h"
+#include "dao/GatewayDao.h"
 #include "dao/UserDao.h"
 
 namespace BeeeOn {
 
 class DefaultAccessPolicy : public AbstractInjectorTarget,
-		public PlaceAccessPolicy {
+		public PlaceAccessPolicy,
+		public GatewayAccessPolicy {
 public:
 	DefaultAccessPolicy();
 
@@ -21,9 +24,23 @@ public:
 	void assureRemove(const ExpirableSession::Ptr session,
 		const Place &place) override;
 
+	void assureGet(const ExpirableSession::Ptr session,
+		const Gateway &gateway) override;
+	void assureAssignGateway(const ExpirableSession::Ptr session,
+		const Place &place) override;
+	void assureUnassign(const ExpirableSession::Ptr session,
+		const Gateway &gateway) override;
+	void assureUpdate(const ExpirableSession::Ptr session,
+		const Gateway &gateway) override;
+
 	void setUserDao(UserDao *dao)
 	{
 		m_userDao = dao;
+	}
+
+	void setGatewayDao(GatewayDao *dao)
+	{
+		m_gatewayDao = dao;
 	}
 
 	void setRoleInPlaceDao(RoleInPlaceDao *dao)
@@ -41,6 +58,7 @@ protected:
 
 private:
 	UserDao *m_userDao;
+	GatewayDao *m_gatewayDao;
 	RoleInPlaceDao *m_roleInPlaceDao;
 };
 

server/src/ui/services.xml
@@ -87,6 +87,7 @@
 		<instance name="accessPolicy"
 				class="BeeeOn::DefaultAccessPolicy">
 			<set name="userDao" ref="userDao" />
+			<set name="gatewayDao" ref="gatewayDao" />
 			<set name="roleInPlaceDao" ref="roleInPlaceDao" />
 		</instance>
 

commit 10849bc43ff441c738185d71a9038b98f6c0ace4

DefaultAccessPolicy: fix assureGet Gateway without Place
When a Gateway has no Place then there is no way how to
query for AccessLevel. We can decide that the gateway is
unreachable for the asking user.

This is difficult to test by functional tests. The bug
was find by when implementing SQL Dao layer.

Fixes: af67014edfd5 ("DefaultAccessPolicy: implement GatewayAccessPolicy")
Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

server/src/policy/DefaultAccessPolicy.cpp
@@ -92,6 +92,9 @@ void DefaultAccessPolicy::assureGet(
 		throw InvalidAccessException("no such gateway "
 				+ gateway.id().toString());
 
+	if (!tmp.hasPlace())
+		throw InvalidAccessException("gateways has no place");
+
 	const Place place(tmp.place());
 
 	assureAtLeast(