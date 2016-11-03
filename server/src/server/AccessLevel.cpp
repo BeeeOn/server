BeeeOn/server

commit c733769bd587348db7d7d40681d339b5714365c3

AccessLevel: new class
Represent the access level to the system based on 3 levels: admin,
user and guest. As a level is represented by a number and the
levels has gaps in between each other, it is possible to introduce
new roles in the future if needed.

Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

server/src/server/AccessLevel.cpp
@@ -1,0 +1,84 @@
+#include <Poco/String.h>
+
+#include "server/AccessLevel.h"
+
+using namespace std;
+using namespace Poco;
+using namespace BeeeOn;
+
+AccessLevel::AccessLevel():
+	m_value(NONE)
+{
+}
+
+AccessLevel::AccessLevel(unsigned int level):
+	m_value(level)
+{
+}
+
+AccessLevel::AccessLevel(const AccessLevel &level):
+	m_value(level.m_value)
+{
+}
+
+const string AccessLevel::toString() const
+{
+	if (m_value <= NONE)
+		return "none";
+
+	if (m_value <= ADMIN)
+		return "admin";
+
+	if (m_value <= USER)
+		return "user";
+
+	if (m_value <= GUEST)
+		return "guest";
+
+	return "any";
+}
+
+const AccessLevel &AccessLevel::any()
+{
+	static AccessLevel level(ANY);
+	return level;
+}
+
+const AccessLevel &AccessLevel::admin()
+{
+	static AccessLevel level(ADMIN);
+	return level;
+}
+
+const AccessLevel &AccessLevel::user()
+{
+	static AccessLevel level(USER);
+	return level;
+}
+
+const AccessLevel &AccessLevel::guest()
+{
+	static AccessLevel level(GUEST);
+	return level;
+}
+
+const AccessLevel &AccessLevel::none()
+{
+	static AccessLevel level(NONE);
+	return level;
+}
+
+const AccessLevel &AccessLevel::parse(const string &level)
+{
+
+	if (icompare(level, "any") == 0)
+		return any();
+	else if (icompare(level, "admin") == 0)
+		return admin();
+	else if (icompare(level, "user") == 0)
+		return user();
+	else if (icompare(level, "guest") == 0)
+		return guest();
+	else
+		return none();
+}

server/src/CMakeLists.txt
@@ -24,6 +24,7 @@ file(GLOB SOURCES
 	${PROJECT_SOURCE_DIR}/server/SessionVerifier.cpp
 	${PROJECT_SOURCE_DIR}/server/RestHandler.cpp
 	${PROJECT_SOURCE_DIR}/server/RestAuthHandler.cpp
+	${PROJECT_SOURCE_DIR}/server/AccessLevel.cpp
 	${PROJECT_SOURCE_DIR}/model/GatewayID.cpp
 	${PROJECT_SOURCE_DIR}/model/JSONSerializer.cpp
 	${PROJECT_SOURCE_DIR}/model/Identity.cpp

server/src/server/AccessLevel.h
@@ -1,0 +1,95 @@
+#ifndef BEEEON_ACCESS_LEVEL_H
+#define BEEEON_ACCESS_LEVEL_H
+
+#include <string>
+#include <climits>
+#include <Poco/Exception.h>
+
+namespace BeeeOn {
+
+class AccessLevel {
+public:
+	AccessLevel();
+	AccessLevel(unsigned int level);
+	AccessLevel(const AccessLevel &level);
+
+	enum {
+		ANY    = 0,
+		ADMIN  = 10,
+		USER   = 20,
+		GUEST  = 30,
+		NONE   = 100
+	};
+
+	/**
+	 * Parse level from string.
+	 */
+	static const AccessLevel &parse(const std::string &level);
+
+	static const AccessLevel &any();
+	static const AccessLevel &admin();
+	static const AccessLevel &user();
+	static const AccessLevel &guest();
+	static const AccessLevel &none();
+
+	/**
+	 * Print parsable string representation of the level.
+	 */
+	const std::string toString() const;
+
+	AccessLevel &operator =(const AccessLevel &level)
+	{
+		m_value = level.m_value;
+		return *this;
+	}
+
+	bool operator ==(const AccessLevel &level) const
+	{
+		return m_value == level.m_value;
+	}
+
+	bool operator !=(const AccessLevel &level) const
+	{
+		return m_value != level.m_value;
+	}
+
+	bool operator <(const AccessLevel &level) const
+	{
+		return m_value < level.m_value;
+	}
+
+	bool operator >(const AccessLevel &level) const
+	{
+		return m_value > level.m_value;
+	}
+
+	bool operator <=(const AccessLevel &level) const
+	{
+		return m_value <= level.m_value;
+	}
+
+	bool operator >=(const AccessLevel &level) const
+	{
+		return m_value <= level.m_value;
+	}
+
+	operator unsigned int() const
+	{
+		return m_value;
+	}
+
+	operator int() const
+	{
+		if (m_value > INT_MAX)
+			throw Poco::BadCastException("out of range of int");
+
+		return (int) m_value;
+	}
+
+private:
+	unsigned int m_value;
+};
+
+}
+
+#endif

commit 31e5de344bcd0dfc91e4af57c4cae5fad7f9a2e3

AccessLevel: fix method toString()
The conversion of NONE is wrong as NONE is the highest value.

Fixes: c733769bd587 ("AccessLevel: new class")
Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

server/src/server/AccessLevel.cpp
@@ -23,8 +23,8 @@ AccessLevel::AccessLevel(const AccessLevel &level):
 
 const string AccessLevel::toString() const
 {
-	if (m_value <= NONE)
-		return "none";
+	if (m_value <= ANY)
+		return "any";
 
 	if (m_value <= ADMIN)
 		return "admin";
@@ -35,7 +35,7 @@ const string AccessLevel::toString() const
 	if (m_value <= GUEST)
 		return "guest";
 
-	return "any";
+	return "none";
 }
 
 const AccessLevel &AccessLevel::any()