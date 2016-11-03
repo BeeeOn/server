BeeeOn/server

commit 7573f5062b4c9fe25dd07a5172aa27e35c858018

LocationXmlHandler: implement xmlui location endpoint
Implement request types: add, update, delete, getall.
No policy checks are implemented yet.

Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

server/src/xmlui/LocationXmlHandler.cpp
@@ -1,0 +1,157 @@
+#include "xmlui/LocationXmlHandler.h"
+#include "xmlui/XmlLocationDeserializer.h"
+#include "xmlui/Serializing.h"
+#include "model/Location.h"
+#include "model/Place.h"
+
+using namespace std;
+using namespace Poco;
+using namespace Poco::Net;
+using namespace Poco::XML;
+using namespace BeeeOn;
+using namespace BeeeOn::XmlUI;
+
+LocationXmlHandler::LocationXmlHandler(
+		const StreamSocket &socket,
+		const AutoPtr<Document> input,
+		ExpirableSession::Ptr session,
+		LocationService &locationService):
+	AbstractXmlHandler("locations", socket, input, session),
+	m_locationService(locationService)
+{
+}
+
+void LocationXmlHandler::handleInputImpl()
+{
+	if (!requireSession())
+		return;
+
+	Element *root = m_input->documentElement();
+	const string &type = root->getAttribute("type");
+	const string &gateid = root->getAttribute("gateid");
+
+	if (gateid.empty()) {
+		resultInvalidInput();
+		return;
+	}
+
+	if (type == "getall") {
+		handleGetAll(gateid);
+		return;
+	}
+
+	Element *locationNode = root->getChildElement("location");
+	if (locationNode == NULL) {
+		resultInvalidInput();
+		return;
+	}
+
+	if (type == "add")
+		handleAdd(gateid, locationNode);
+	else if (type == "delete")
+		handleDelete(gateid, locationNode);
+	else if (type == "update")
+		handleUpdate(gateid, locationNode);
+	else
+		resultInvalidInput();
+}
+
+void LocationXmlHandler::handleAdd(
+		const string &gateid,
+		Element *locationNode)
+{
+	Gateway gateway(GatewayID::parse(gateid));
+
+	Location location;
+	XmlLocationDeserializer deserializer(*locationNode);
+	
+	m_locationService.createIn(location, deserializer, gateway);
+	resultSuccess();
+}
+
+void LocationXmlHandler::handleDelete(
+		const string &gateid,
+		Element *locationNode)
+{
+	Gateway gateway(GatewayID::parse(gateid));
+	Location location(LocationID::parse(
+			locationNode->getAttribute("locationid")));
+
+	if (!m_locationService.removeFrom(location, gateway)) {
+		resultNotFound();
+		return;
+	}
+
+	resultSuccess();
+}
+
+void LocationXmlHandler::handleUpdate(
+		const string &gateid,
+		Element *locationNode)
+{
+	Gateway gateway(GatewayID::parse(gateid));
+	Location location(LocationID::parse(
+			locationNode->getAttribute("locationid")));
+	XmlLocationDeserializer update(*locationNode);
+
+	if (!m_locationService.updateIn(location, update, gateway)) {
+		resultNotFound();
+		return;
+	}
+
+	resultSuccess();
+}
+
+void LocationXmlHandler::handleGetAll(const string &gateid)
+{
+	Gateway gateway(GatewayID::parse(gateid));
+	vector<Location> locations;
+
+	m_locationService.fetchBy(locations, gateway);
+
+	resultDataStart();
+	serialize(m_output, locations);
+	resultDataEnd();
+}
+
+LocationXmlHandlerResolver::LocationXmlHandlerResolver():
+	AbstractXmlHandlerResolver("locations")
+{
+	injector<LocationXmlHandlerResolver, LocationService>(
+			"locationService",
+			&LocationXmlHandlerResolver::setLocationService);
+	injector<LocationXmlHandlerResolver, SessionManager>(
+			"sessionManager",
+			&LocationXmlHandlerResolver::setSessionManager);
+}
+
+bool LocationXmlHandlerResolver::canHandle(
+		const Element &root)
+{
+	if (!AbstractXmlHandlerResolver::canHandle(root))
+		return false;
+
+	if (root.getAttribute("type") == "add")
+		return true;
+	if (root.getAttribute("type") == "delete")
+		return true;
+	if (root.getAttribute("type") == "update")
+		return true;
+	if (root.getAttribute("type") == "getall")
+		return true;
+
+	return false;
+}
+
+XmlRequestHandler *LocationXmlHandlerResolver::createHandler(
+		const StreamSocket &socket,
+		const AutoPtr<Document> input)
+{
+	ExpirableSession::Ptr session = lookupSession(
+			*m_sessionManager, input);
+	return new LocationXmlHandler(
+			socket, input, session, *m_locationService);
+}
+
+BEEEON_OBJECT(LocationXmlHandlerResolver,
+		BeeeOn::XmlUI::LocationXmlHandlerResolver)

server/conf/services.xml
@@ -38,6 +38,7 @@
 				class="BeeeOn::XmlRequestHandlerFactory">
 			<add name="resolvers" ref="authXmlHandler" />
 			<add name="resolvers" ref="gatewayXmlHandler" />
+			<add name="resolvers" ref="locationXmlHandler" />
 			<add name="resolvers" ref="anyXmlHandler" />
 		</instance>
 
@@ -54,6 +55,12 @@
 			<set name="gatewayService" ref="gatewayService" />
 			<set name="placeService" ref="placeService" />
 			<set name="accessPolicy" ref="accessPolicy" />
+			<set name="sessionManager" ref="sessionManager" />
+		</instance>
+
+		<instance name="locationXmlHandler"
+				class="BeeeOn::XmlUI::LocationXmlHandlerResolver">
+			<set name="locationService" ref="locationService" />
 			<set name="sessionManager" ref="sessionManager" />
 		</instance>
 

server/src/CMakeLists.txt
@@ -86,6 +86,7 @@ add_executable(xmlui-server
 	xmlui/XmlGatewayDeserializer.cpp
 	xmlui/XmlLocationDeserializer.cpp
 	xmlui/GatewayXmlHandler.cpp
+	xmlui/LocationXmlHandler.cpp
 	ui/UIMockInit.cpp
 )
 

server/src/xmlui/LocationXmlHandler.h
@@ -1,0 +1,57 @@
+#ifndef BEEEON_XMLUI_LOCATION_XML_HANDLER_H
+#define BEEEON_XMLUI_LOCATION_XML_HANDLER_H
+
+#include "service/LocationService.h"
+#include "xmlui/AbstractXmlHandler.h"
+
+namespace BeeeOn {
+namespace XmlUI {
+
+class LocationXmlHandler : public AbstractXmlHandler {
+public:
+	LocationXmlHandler(const Poco::Net::StreamSocket &socket,
+			const Poco::AutoPtr<Poco::XML::Document> input,
+			BeeeOn::ExpirableSession::Ptr session,
+			BeeeOn::LocationService &locationService);
+	void handleInputImpl() override;
+	void handleAdd(const std::string &gateid,
+			Poco::XML::Element *locationNode);
+	void handleDelete(const std::string &gateid,
+			Poco::XML::Element *locationNode);
+	void handleUpdate(const std::string &gateid,
+			Poco::XML::Element *locationNode);
+	void handleGetAll(const std::string &gateid);
+
+private:
+	LocationService &m_locationService;
+};
+
+class LocationXmlHandlerResolver : public AbstractXmlHandlerResolver {
+public:
+	LocationXmlHandlerResolver();
+
+	bool canHandle(const Poco::XML::Element &root);
+	XmlRequestHandler *createHandler(
+			const Poco::Net::StreamSocket &socket,
+			const Poco::AutoPtr<Poco::XML::Document> input);
+
+	void setLocationService(BeeeOn::LocationService *service)
+	{
+		m_locationService = service;
+	}
+
+	void setSessionManager(BeeeOn::SessionManager *manager)
+	{
+		m_sessionManager = manager;
+	}
+
+private:
+	BeeeOn::LocationService *m_locationService;
+	BeeeOn::SessionManager *m_sessionManager;
+};
+
+}
+}
+
+#endif
+

commit 9db34df2f8a8cbf6e863c331636115c26a3d3641

LocationXmlHandler: fix result of request type "add"
The message ns="locations" type="add" returns data as a result.
The data contains information about the created location, especially
the assigned ID.

Fixes: 7573f5062b4c ("LocationXmlHandler: implement xmlui location endpoint")
Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

server/src/xmlui/LocationXmlHandler.cpp
@@ -70,7 +70,10 @@ void LocationXmlHandler::handleAdd(
 	input.setUser(user);
 	
 	m_locationService.createIn(input);
-	resultSuccess();
+
+	resultDataStart();
+	serialize(m_output, location);
+	resultDataEnd();
 }
 
 void LocationXmlHandler::handleDelete(

server/t/xmlui/t1002-location-add-delete.py
@@ -54,14 +54,9 @@ class TestLocationAddDelete(unittest.TestCase):
 			self.session,
 			name = "Livingroom"
 		))
-		self.assertTrue(response.is_ok())
-
-		response = c.request(LocationGetAll(
-			config.gateway_id,
-			self.session
-		))
 		self.assertTrue(response.is_data())
 		self.assertEqual(1, len(response.root))
+
 		self.assertEqual("Livingroom", response.root[0].get("name"))
 
 		response = c.request(LocationDelete(

server/t/xmlui/t2002-location-get-update.py
@@ -28,13 +28,17 @@ class TestLocationGetUpdate(unittest.TestCase):
 		self.assertTrue(response.is_ok())
 
 		names = ["Livingroom", "Kitchen", "Bathroom"]
+		self.locations = []
+
 		for name in names:
 			response = c.request(LocationAdd(
 				config.gateway_id,
 				self.session,
 				name = name
 			))
-			self.assertTrue(response.is_ok())
+			self.assertTrue(response.is_data())
+			self.assertEqual(1, len(response.root))
+			self.locations.append(response.root[0].get("id"))
 
 	"""
 	Remove the locations, unregister the gateway and destroy the session.
@@ -42,17 +46,10 @@ class TestLocationGetUpdate(unittest.TestCase):
 	def tearDown(self):
 		c = Connector(config.xmlui_host, config.xmlui_port)
 
-		response = c.request(LocationGetAll(
-			config.gateway_id,
-			self.session
-		))
-		self.assertTrue(response.is_data())
-		self.assertEqual(3, len(response.root))
-
-		for l in response.root:
+		for location in self.locations:
 			response = c.request(LocationDelete(
 				config.gateway_id,
-				l.get("locationid"),
+				location,
 				self.session
 			))
 			self.assertTrue(response.is_ok())