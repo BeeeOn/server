BeeeOn/server

commit 01275862938ecf99e3cc6a763072ad43a4548ba4

Base64: new class
Provide Base64 utility functions encode and decode.

Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>
Reviewed-by: Petr Stehlík <xstehl14@stud.fit.vutbr.cz>

server/src/util/Base64.h
@@ -1,0 +1,51 @@
+#ifndef BEEEON_BASE64_H
+#define BEEEON_BASE64_H
+
+#include <sstream>
+#include <Poco/Base64Encoder.h>
+#include <Poco/Base64Decoder.h>
+#include <Poco/StreamCopier.h>
+
+namespace BeeeOn {
+
+class Base64 {
+public:
+	static std::string encode(char *b, size_t blen)
+	{
+		std::ostringstream sOut;
+		Poco::Base64Encoder base64(sOut);
+		base64.write(b, blen);
+		base64.close();
+
+		return sOut.str();
+	}
+
+	static size_t decode(const std::string &s, char *b, size_t bcap)
+	{
+		std::istringstream sIn(s);
+		Poco::Base64Decoder base64(sIn);
+
+		for (size_t i = 0; i < bcap; ++i) {
+			b[i] = base64.get();
+
+			if (!base64.good() || base64.eof())
+				return i;
+		}
+
+		return bcap;
+	}
+
+	static std::string decode(const std::string &s)
+	{
+		std::istringstream sIn(s);
+		Poco::Base64Decoder base64(sIn);
+
+		std::string result;
+		base64 >> result;
+		return result;
+	}
+};
+
+}
+
+#endif

server/test/CMakeLists.txt
@@ -18,6 +18,8 @@ file(GLOB TEST_SOURCES
 	${PROJECT_SOURCE_DIR}/service/AuthServiceTest.cpp
 	${PROJECT_SOURCE_DIR}/provider/GoogleAuthProviderTest.cpp
 	${PROJECT_SOURCE_DIR}/util/DAMMTest.cpp
+	${PROJECT_SOURCE_DIR}/dao/SociManagerTest.cpp
+	${PROJECT_SOURCE_DIR}/util/Base64Test.cpp
 )
 
 include_directories(

server/test/util/Base64Test.cpp
@@ -1,0 +1,64 @@
+#ifndef BEEEON_BASE64_TEST_H
+#define BEEEON_BASE64_TEST_H
+
+#include <cstring>
+#include <cppunit/extensions/HelperMacros.h>
+
+#include "util/Base64.h"
+
+using namespace std;
+
+namespace BeeeOn {
+
+class Base64Test : public CppUnit::TestFixture {
+	CPPUNIT_TEST_SUITE(Base64Test);
+	CPPUNIT_TEST(testEncode);
+	CPPUNIT_TEST(testDecode);
+	CPPUNIT_TEST_SUITE_END();
+public:
+	void testEncode();
+	void testDecode();
+};
+
+CPPUNIT_TEST_SUITE_REGISTRATION(Base64Test);
+
+void Base64Test::testEncode()
+{
+	char buf[32] = {
+		'\x05', '\x16', '\xab', '\xff', '\x12', '\x95', '\x49', '\x90',
+		'\x34', '\x44', '\x17', '\x47', '\x77', '\x78', '\x77', '\x10',
+		'\x00', '\xed', '\xcc', '\x10', '\x89', '\xa8', '\xaa', '\xa8',
+		'\x01', '\xdc', '\xc2', '\x47', '\x8b', '\x78', '\x77', '\x10',
+	};
+
+	const string expect =
+		"XHgwNVx4MTZceGFiXHhmZlx4MTJceDk"
+		"1XHg0OVx4OTBceDM0XHg0NFx4MTdceD"
+		"Q3XHg3N1x4NzhceDc3XHgxMFx4MDBce"
+		"GVkXHhjY1x4MTBceDg5XHhhOFx4YWFc"
+		"eGE4XHgwMVx4ZGNceGMyXHg0N1x4OGJ"
+		"ceDc4XHg3N1x4MTAK";
+
+	const string &result = Base64::encode(buf, sizeof(buf));
+	CPPUNIT_ASSERT(result.compare(expect));
+}
+
+void Base64Test::testDecode()
+{
+	const string data = "YW55IGFyYml0cmFyeSB0ZXN0aW5nIHRleHQK";
+	char buf[32];
+	size_t len;
+
+	const char expect[] = {
+		'a', 'n', 'y', ' ', 'a', 'r', 'b', 'i',
+		't', 'r', 'a', 'r', 'y', ' ', 't', 'e',
+		's', 't', 'i', 'n', 'g', ' ', 't', 'e', 'x', 't', '\n'};
+
+	len = Base64::decode(data, buf, sizeof(buf));
+	CPPUNIT_ASSERT_EQUAL(sizeof(expect), len);
+	CPPUNIT_ASSERT(std::memcmp(buf, expect, sizeof(expect)) == 0);
+}
+
+}
+
+#endif

commit eeaac4b37b8b18b126dea89e111c68cf1cca20fe

Base64: fix decode not reading all input data
Fixes: 01275862938e ("Base64: new class")
Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

server/src/util/Base64.h
@@ -42,7 +42,7 @@ public:
 		Poco::Base64Decoder base64(sIn);
 
 		std::string result;
-		base64 >> result;
+		Poco::StreamCopier::copyToString(base64, result);
 		return result;
 	}
 };