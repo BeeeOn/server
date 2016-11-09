0671db5bf52a16a725d54324270a316276d41a69
Jan Viktorin <iviktorin@fit.vutbr.cz>
2016-11-09 22:22:24 +0100

    PocoSQLiteConnectorLoader: new class
    Loader of the SQLite Connector. It registers the connector with
    the Poco Data core.
    
    Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

---
+++ server/src/dao/PocoSQLiteConnectorLoader.h
@@ -0,0 +1,19 @@
+#ifndef BEEEON_POCO_SQLITE_CONNECTOR_LOADER_H
+#define BEEEON_POCO_SQLITE_CONNECTOR_LOADER_H
+
+#include "di/InjectorTarget.h"
+#include "dao/ConnectorLoader.h"
+
+namespace BeeeOn {
+
+class PocoSQLiteConnectorLoader :
+	public ConnectorLoader,
+	public AbstractInjectorTarget {
+public:
+	PocoSQLiteConnectorLoader();
+	virtual ~PocoSQLiteConnectorLoader();
+};
+
+}
+
+#endif
