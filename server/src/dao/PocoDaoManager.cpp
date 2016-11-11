BeeeOn/server

commit 7149d9dc7f86210e463c8427bd04d73151565d2d

PocoDaoManager: new class
PocoDaoManager represents an access point to a database.
The Dao implementations would obtain the SessionPool from
the PocoDaoManager to perform SQL queries. The PocoDaoManager
can initialize database by a startup SQL script.

The initial script is considered to be a template. So all occurences
of ${...} can be replaced before the actual execution. This might
help to provide a backend-independent SQL schema.

We occasionally log the session pool usage and provide information
about transaction properties of the initial session on startup.

Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

server/src/dao/PocoDaoManager.cpp
@@ -1,0 +1,176 @@
+#include <Poco/Logger.h>
+#include <Poco/Exception.h>
+#include <Poco/FileStream.h>
+#include <Poco/StreamCopier.h>
+#include <Poco/Data/Session.h>
+#include <Poco/Data/SessionPool.h>
+
+#include "dao/PocoDaoManager.h"
+#include "util/Template.h"
+#include "util/Occasionally.h"
+#include "Debug.h"
+
+BEEEON_OBJECT(PocoDaoManager, BeeeOn::PocoDaoManager)
+
+using namespace std;
+using namespace Poco;
+using namespace Poco::Data;
+using namespace Poco::Data::Keywords;
+using namespace BeeeOn;
+
+PocoDaoManager::PocoDaoManager():
+	m_logger(LOGGER_CLASS(this)),
+	m_minSessions(1),
+	m_maxSessions(32),
+	m_idleTime(60),
+	m_pool(NULL)
+{
+	textInjector("connector", (TextSetter)
+			&PocoDaoManager::setConnector);
+	textInjector("connectionString", (TextSetter)
+			&PocoDaoManager::setConnectionString);
+	numberInjector("minSessions", (NumberSetter)
+			&PocoDaoManager::setMinSessions);
+	numberInjector("maxSessions", (NumberSetter)
+			&PocoDaoManager::setMaxSessions);
+	numberInjector("idleTime", (NumberSetter)
+			&PocoDaoManager::setIdleTime);
+	textInjector("initScript", (TextSetter)
+			&PocoDaoManager::setInitScript);
+}
+
+void PocoDaoManager::setConnector(const std::string &connector)
+{
+	m_connector = connector;
+}
+
+void PocoDaoManager::setConnectionString(const std::string &conn)
+{
+	m_connectionString = conn;
+}
+
+void PocoDaoManager::setMinSessions(const int sessions)
+{
+	if (sessions < 1) {
+		throw InvalidArgumentException(
+				"minSessions must be positive");
+	}
+
+	m_minSessions = sessions;
+}
+
+void PocoDaoManager::setMaxSessions(const int sessions)
+{
+	if (sessions < 1) {
+		throw InvalidArgumentException(
+			"maxSessions must be positive");
+	}
+
+	m_maxSessions = sessions;
+}
+
+void PocoDaoManager::setIdleTime(const int seconds)
+{
+	if (seconds < 0) {
+		throw InvalidArgumentException(
+			"idleTime must be non-negative");
+	}
+
+	m_idleTime = seconds;
+}
+
+void PocoDaoManager::setInitScript(const std::string &script)
+{
+	m_script = script;
+}
+
+Poco::Data::SessionPool &PocoDaoManager::pool()
+{
+	static Occasionally occasionally;
+
+	if (m_pool == NULL)
+		initPool();
+
+	occasionally.execute([&]() {
+		m_logger.notice(
+			"[%s] "
+			"allocated: %d, "
+			"available: %d, "
+			"capacity: %d, "
+			"dead: %d",
+			string(m_pool->isActive()? "active" : "inactive"),
+			m_pool->allocated(),
+			m_pool->available(),
+			m_pool->capacity(),
+			m_pool->dead());
+	});
+
+	return *m_pool;
+}
+
+void PocoDaoManager::initPool()
+{
+	if (m_minSessions > m_maxSessions)
+		throw InvalidArgumentException("minSessions > maxSessions");
+
+	m_logger.notice("initialize database pool ("
+			+ m_connector + ", "
+			+ to_string(m_maxSessions) + ")",
+			__FILE__, __LINE__);
+
+	m_logger.debug("connection: "
+			+ m_connector + ", "
+			+ to_string(m_minSessions) + ", "
+			+ to_string(m_maxSessions) + ", "
+			+ to_string(m_idleTime),
+			__FILE__, __LINE__);
+
+	m_pool = new SessionPool(
+			m_connector,
+			m_connectionString,
+			m_minSessions,
+			m_maxSessions,
+			m_idleTime);
+
+	m_logger.notice("database pool initialized", __FILE__, __LINE__);
+}
+
+void PocoDaoManager::injectionDone()
+{
+	initPool();
+
+	if (m_script.empty()) {
+		m_logger.debug("no initialization script, skipping",
+				__FILE__, __LINE__);
+		return;
+	}
+
+	m_logger.debug("executing initialization script: " + m_script,
+			__FILE__, __LINE__);
+
+	FileInputStream inputStream(m_script);
+	Template script(inputStream);
+	map<string, string> context;
+
+	Session session(pool().get());
+
+	m_logger.notice("Session %s transact (isolation: %u) "
+			"login timeout: %z, "
+			"connection timeout: %z",
+			string(session.canTransact()? "can" : "cannot"),
+			(unsigned int) session.getTransactionIsolation(),
+			session.getLoginTimeout(),
+			session.getConnectionTimeout());
+
+	if (!session.canTransact()) {
+		m_logger.warning("Session cannot perform transactions",
+				__FILE__, __LINE__);
+	}
+
+	session.begin();
+	session << (script.apply(context)), now;
+	session.commit();
+
+	m_logger.information("database has been initialized successfully",
+			__FILE__, __LINE__);
+}

server/src/dao/PocoDaoManager.h
@@ -1,0 +1,49 @@
+#ifndef BEEEON_POCO_DAO_MANAGER_H
+#define BEEEON_POCO_DAO_MANAGER_H
+
+#include "di/InjectorTarget.h"
+
+namespace Poco {
+
+class Logger;
+
+namespace Data {
+
+class SessionPool;
+
+}
+}
+
+namespace BeeeOn {
+
+class PocoDaoManager : public AbstractInjectorTarget {
+public:
+	PocoDaoManager();
+
+	void setConnector(const std::string &connector);
+	void setConnectionString(const std::string &conn);
+	void setMinSessions(const int sessions);
+	void setMaxSessions(const int sessions);
+	void setIdleTime(const int seconds);
+	void setInitScript(const std::string &script);
+
+	Poco::Data::SessionPool &pool();
+
+protected:
+	void initPool();
+	void injectionDone() override;
+
+private:
+	Poco::Logger &m_logger;
+	std::string m_connector;
+	std::string m_connectionString;
+	int m_minSessions;
+	int m_maxSessions;
+	int m_idleTime;
+	std::string m_script;
+	Poco::Data::SessionPool *m_pool;
+};
+
+}
+
+#endif

server/conf/services.xml
@@ -200,5 +200,13 @@
 		<alias name="verifiedIdentityDao" ref="mockVerifiedIdentityDao" />
 		<alias name="roleInPlaceDao" ref="mockRoleInPlaceDao" />
 
+		<instance name="pocoDaoManager"
+				class="BeeeOn::PocoDaoManager">
+			<set name="connector" text="${database.backend}" />
+			<set name="connectionString" text="${database.connector}" />
+			<set name="maxSessions" number="${database.pool}" />
+			<set name="initScript" text="${database.init}" />
+		</instance>
+
 	</services>
 </system>

server/src/CMakeLists.txt
@@ -8,6 +8,7 @@ find_library (POCO_UTIL PocoUtil)
 find_library (POCO_NET PocoNet)
 find_library (POCO_JSON PocoJSON)
 find_library (POCO_XML PocoXML)
+find_library (POCO_DATA PocoData)
 
 set (CMAKE_CXX_FLAGS " ${CMAKE_CXX_FLAGS} -DBEEEON_SELECT_POCO")
 add_definitions(-std=c++11)
@@ -61,6 +62,7 @@ file(GLOB SOURCES
 	${PROJECT_SOURCE_DIR}/util/SecureXmlParser.cpp
 	${PROJECT_SOURCE_DIR}/util/Template.cpp
 	${PROJECT_SOURCE_DIR}/util/Occasionally.cpp
+	${PROJECT_SOURCE_DIR}/dao/PocoDaoManager.cpp
 	${PROJECT_SOURCE_DIR}/rpc/GatewayRPC.cpp
 )
 
@@ -102,6 +104,7 @@ set(LIBS
 	${POCO_NET}
 	${POCO_JSON}
 	${POCO_XML}
+	${POCO_DATA}
 	mongoose
 )
 

server/test/CMakeLists.txt
@@ -9,6 +9,7 @@ find_library (POCO_CRYPTO PocoCrypto)
 find_library (POCO_NET PocoNet)
 find_library (POCO_JSON PocoJSON)
 find_library (POCO_XML PocoXML)
+find_library (POCO_DATA PocoData)
 
 file(GLOB TEST_SOURCES
 	${PROJECT_SOURCE_DIR}/DebugTest.cpp
@@ -40,6 +41,7 @@ set(LIBS
 	${POCO_NET}
 	${POCO_JSON}
 	${POCO_XML}
+	${POCO_DATA}
 	mongoose
 	${CPP_UNIT}
 )

commit 94235acbd6dc6aad3e64eab375253a94d05afd17

PocoDaoManager: fix memory leak (pool)
The pool is allocated but never deallocated. Hide it into the SharedPtr
to free the pool when necessary.

It also fixes a very slow exit on request (Ctrl+C).

Fixes: 7149d9dc7f86 ("PocoDaoManager: new class")
Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

server/src/dao/PocoDaoManager.cpp
@@ -26,8 +26,7 @@ PocoDaoManager::PocoDaoManager():
 	m_minSessions(1),
 	m_maxSessions(32),
 	m_idleTime(60),
-	m_dialect(NULL),
-	m_pool(NULL)
+	m_dialect(NULL)
 {
 	injector<PocoDaoManager, ConnectorLoader>("connector",
 			&PocoDaoManager::setConnector);
@@ -100,7 +99,7 @@ Poco::Data::SessionPool &PocoDaoManager::pool()
 {
 	static Occasionally occasionally;
 
-	if (m_pool == NULL)
+	if (m_pool.isNull())
 		initPool();
 
 	occasionally.execute([&]() {

server/src/dao/PocoDaoManager.h
@@ -1,17 +1,14 @@
 #ifndef BEEEON_POCO_DAO_MANAGER_H
 #define BEEEON_POCO_DAO_MANAGER_H
 
+#include <Poco/SharedPtr.h>
+#include <Poco/Data/SessionPool.h>
 #include "di/InjectorTarget.h"
 
 namespace Poco {
 
 class Logger;
 
-namespace Data {
-
-class SessionPool;
-
-}
 }
 
 namespace BeeeOn {
@@ -46,7 +43,7 @@ private:
 	int m_idleTime;
 	std::string m_script;
 	SQLDialect *m_dialect;
-	Poco::Data::SessionPool *m_pool;
+	Poco::SharedPtr<Poco::Data::SessionPool> m_pool;
 };
 
 }