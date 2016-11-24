BeeeOn/server

commit 9cedc5776e7d4c21a2f13854ba88656ee757ce3b

OAuth2AuthProvider: configure SSL from SSLClient
There is no need to have multiple SSL contexts when asking
the same servers for authorization of a user. Also note that
doing that is not thread-safe because the InvalidCertificateHandler
registers itself globally to SSLManager. Moreover, using the
SSLManager::initializeClient is not thread-safe because it sets
a global context and handlers.

Note that we need to ensure that the GoogleAuthProvider accepts
the Google certificates. To enable this, the google.ssl.authority
should point to a CA storage.

The default redirect_uri is set to http://localhost as it seems
to be sufficient for now.

Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

server/conf/services.xml
@@ -156,6 +156,7 @@
 			<set name="client_secret" text="${google.client_secret}"/>
 			<set name="client_id" text="${google.client_id}"/>
 			<set name="redirect_uri" text="${google.redirect_uri}"/>
+			<set name="sslConfig" text="googleOAuthSSL"/>
 		</instance>
 
 		<instance name="pocoRandomProvider"
@@ -265,6 +266,11 @@
 			<set name="authority" text="${xmlui.ssl.authority}" />
 			<set name="verificationMode" text="${xmlui.ssl.verify_level}" />
 			<set name="loadDefaultCA" text="false" />
+		</instance>
+
+		<instance name="googleOAuthSSL" class="BeeeOn::SSLClient">
+			<set name="verificationMode" text="${google.ssl.verify_mode}" />
+			<set name="authority" text="${google.ssl.authority}" />
 		</instance>
 
 	</services>

server/src/provider/OAuth2AuthProvider.cpp
@@ -11,6 +11,7 @@
 #include <Poco/StreamCopier.h>
 
 #include "provider/OAuth2AuthProvider.h"
+#include "util/SSLClient.h"
 #include "Debug.h"
 
 using namespace std;
@@ -19,7 +20,8 @@ using namespace Poco;
 using namespace Poco::Net;
 
 OAuth2AuthProvider::OAuth2AuthProvider(const string &name):
-		AuthCodeAuthProvider(name)
+		AuthCodeAuthProvider(name),
+		m_sslConfig(0)
 {
 	textInjector("client_id",
 			(TextSetter) &OAuth2AuthProvider::setClientId);
@@ -27,19 +29,15 @@ OAuth2AuthProvider::OAuth2AuthProvider(const string &name):
 			(TextSetter) &OAuth2AuthProvider::setClientSecret);
 	textInjector("redirect_uri",
 			(TextSetter) &OAuth2AuthProvider::setRedirectURI);
+	textInjector("sslConfig",
+			(TextSetter) &OAuth2AuthProvider::setSSLConfig);
 }
 
 void OAuth2AuthProvider::initSSL()
 {
-	TRACE_METHOD();
-
-	/* Handles certificates for HTTPS communication
-	 * TODO: accept only google certificate, now accepts all certs!!!
-	 */
-	SSLManager::InvalidCertificateHandlerPtr ptrHandler (
-			new Poco::Net::AcceptCertificateHandler(false));
-	const Context::Ptr context = new Context(Context::CLIENT_USE, "");
-	SSLManager::instance().initializeClient(0, ptrHandler, context);
+	if (m_sslConfig == 0)
+		throw IllegalStateException(
+				"missing sslConfig, cannot use OAuth");
 }
 
 HTTPSClientSession *OAuth2AuthProvider::connectSecure(
@@ -48,7 +46,8 @@ HTTPSClientSession *OAuth2AuthProvider::connectSecure(
 {
 	try {
 		initSSL();
-		return new HTTPSClientSession(host, port);
+		return new HTTPSClientSession(
+				host, port, m_sslConfig->context());
 	} catch (const Exception &e) {
 		m_logger.log(e, __FILE__, __LINE__);
 		throw;

server/conf/server.ini
@@ -18,7 +18,10 @@ maxPerUser = 10
 [google]
 ;client_secret =
 ;client_id =
-;redirect_uri =
+redirect_uri = http://localhost
+ssl.verify_mode = relaxed
+; path to the CA location
+ssl.authority =
 
 [database]
 backend = sqlite

server/src/provider/OAuth2AuthProvider.h
@@ -1,6 +1,8 @@
 #ifndef BEEEON_OAUTH2_AUTH_PROVIDER_H
 #define BEEEON_OAUTH2_AUTH_PROVIDER_H
 
+#include <Poco/Net/Context.h>
+
 #include "provider/AuthProvider.h"
 
 namespace Poco {
@@ -12,6 +14,8 @@ class HTTPSClientSession;
 }
 
 namespace BeeeOn {
+
+class SSLClient;
 
 class OAuth2AuthProvider : public AuthCodeAuthProvider {
 public:
@@ -32,12 +36,12 @@ public:
 		m_redirectURI = uri;
 	}
 
-protected:
-	/**
-	 * Initialize SSL context for upcoming HTTPS requests
-	 */
-	void initSSL();
+	void setSSLConfig(SSLClient *config)
+	{
+		m_sslConfig = config;
+	}
 
+protected:
 	/**
 	 * Open HTTPS connection to the given host:port.
 	 */
@@ -55,10 +59,17 @@ protected:
 	 */
 	std::string convertResponseToString(std::istream &rs);
 
+private:
+	/**
+	 * Initialize SSL context for upcoming HTTPS requests
+	 */
+	void initSSL();
+
 protected:
 	std::string m_clientId;
 	std::string m_clientSecret;
 	std::string m_redirectURI;
+	SSLClient *m_sslConfig;
 };
 
 }

server/test/provider/GoogleAuthProviderTest.cpp
@@ -5,6 +5,7 @@
 
 #include "provider/GoogleAuthProvider.h"
 #include "provider/AuthProvider.h"
+#include "util/SSLClient.h"
 #include "cppunit/SkippableAutoRegisterSuite.h"
 
 using namespace std;
@@ -28,6 +29,7 @@ public:
 	void testVerifyAuthCode();
 private:
 	std::string googleAuthCode;
+	SSLClient *m_sslConfig;
 };
 
 CPPUNIT_TEST_SUITE_REGISTRATION_SKIPPABLE(GoogleAuthProviderTest,
@@ -59,6 +61,9 @@ void GoogleAuthProviderTest::testVerifyAuthCode()
 
 	TestableGoogleAuthProvider provider;
 	AuthResult info;
+	SSLClient sslConfig;
+
+	provider.setSSLConfig(&sslConfig);
 
 	CPPUNIT_ASSERT_MESSAGE("failed to authenticate user",
 			provider.verifyAuthCode(googleAuthCode, info));

commit 6f2cc1cc43387d829416ec182a1fee59c6e6f8f1

conf: fix sslConfig property for GoogleAuthProvider
Fixes: 9cedc5776e7d ("OAuth2AuthProvider: configure SSL from SSLClient")
Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

server/conf/services.xml
@@ -170,7 +170,7 @@
 			<set name="client_secret" text="${google.client_secret}"/>
 			<set name="client_id" text="${google.client_id}"/>
 			<set name="redirect_uri" text="${google.redirect_uri}"/>
-			<set name="sslConfig" text="googleOAuthSSL"/>
+			<set name="sslConfig" ref="googleOAuthSSL"/>
 		</instance>
 
 		<instance name="pocoRandomProvider"

server/src/provider/OAuth2AuthProvider.cpp
@@ -29,8 +29,10 @@ OAuth2AuthProvider::OAuth2AuthProvider(const string &name):
 			(TextSetter) &OAuth2AuthProvider::setClientSecret);
 	textInjector("redirect_uri",
 			(TextSetter) &OAuth2AuthProvider::setRedirectURI);
-	textInjector("sslConfig",
-			(TextSetter) &OAuth2AuthProvider::setSSLConfig);
+	injector<OAuth2AuthProvider, SSLClient>(
+		"sslConfig",
+		&OAuth2AuthProvider::setSSLConfig
+	);
 }
 
 void OAuth2AuthProvider::initSSL()