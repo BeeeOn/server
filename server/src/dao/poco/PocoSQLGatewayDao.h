b92b93c2f1f1c1599d38f84a42f2a5a5298768a3
Jan Viktorin <iviktorin@fit.vutbr.cz>
2016-12-08 10:26:54 +0100

    PocoSQLGatewayDao: extract header file
    Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

---
+++ server/src/dao/poco/PocoSQLGatewayDao.h
@@ -0,0 +1,45 @@
+#include "dao/GatewayDao.h"
+#include "dao/poco/PocoAbstractDao.h"
+
+namespace Poco {
+namespace Data {
+
+class Session;
+
+}
+}
+
+namespace BeeeOn {
+
+class PocoSQLGatewayDao :
+		public PocoAbstractDao,
+		public GatewayDao {
+public:
+	bool insert(Gateway &gateway) override;
+	bool fetch(Gateway &gateway) override;
+	bool update(Gateway &gateway) override;
+	bool assignAndUpdate(Gateway &gateway, const Place &place) override;
+	bool assign(Gateway &gateway, const Place &place) override;
+	bool unassign(Gateway &gateway) override;
+	bool fetchFromPlace(Gateway &gateway, const Place &place) override;
+	void fetchAccessible(
+			std::vector<Gateway> &gateways,
+			const User &user) override;
+
+protected:
+	bool insert(Poco::Data::Session &session, Gateway &gateway);
+	bool fetch(Poco::Data::Session &session, Gateway &gateway);
+	bool update(Poco::Data::Session &session, Gateway &gateway);
+	bool assignAndUpdate(Poco::Data::Session &session,
+			Gateway &gateway, const Place &place);
+	bool assign(Poco::Data::Session &session,
+			Gateway &gateway, const Place &place);
+	bool unassign(Poco::Data::Session &session, Gateway &gateway);
+	bool fetchFromPlace(Poco::Data::Session &session,
+			Gateway &gateway, const Place &place);
+	void fetchAccessible(Poco::Data::Session &session,
+			std::vector<Gateway> &gateways,
+			const User &user);
+};
+
+}
