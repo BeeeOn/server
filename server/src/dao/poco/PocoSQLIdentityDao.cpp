BeeeOn/server

commit f3304b87a2fc54f15a22d0e9819557f381340a50

PocoSQLIdentityDao: extract method parseSingle
Extraction of data from SELECT queries is to be refactored out
of the actual code. Poco auto-extracting framework is not used
here due to specific requirements.

Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

server/src/dao/poco/PocoSQLIdentityDao.cpp
@@ -2,6 +2,8 @@
 #include <Poco/Data/Session.h>
 #include <Poco/Data/SessionPool.h>
 #include <Poco/Data/Statement.h>
+#include <Poco/Data/RecordSet.h>
+#include <Poco/Data/Row.h>
 
 #include "dao/poco/PocoSQLIdentityDao.h"
 #include "dao/poco/PocoDaoManager.h"
@@ -71,8 +73,8 @@ bool PocoSQLIdentityDao::fetch(Session &session, Identity &identity)
 	if (execute(sql) == 0)
 		return false;
 
-	identity.setEmail(email);
-	return true;
+	RecordSet result(sql);
+	return parseSingle(result, identity);
 }
 
 bool PocoSQLIdentityDao::fetchBy(Session &session,
@@ -82,17 +84,15 @@ bool PocoSQLIdentityDao::fetchBy(Session &session,
 	string id;
 
 	Statement sql(session);
-	sql << "SELECT id FROM identities"
+	sql << "SELECT id, email FROM identities"
 		" WHERE email = :email",
-		use(searchEmail, "email"),
-		into(id);
+		use(searchEmail, "email");
 
 	if (execute(sql) == 0)
 		return false;
 
-	identity = Identity(IdentityID::parse(id));
-	identity.setEmail(email);
-	return true;
+	RecordSet result(sql);
+	return parseSingle(result, identity);
 }
 
 bool PocoSQLIdentityDao::remove(Session &session, const Identity &identity)
@@ -106,4 +106,23 @@ bool PocoSQLIdentityDao::remove(Session &session, const Identity &identity)
 		use(id, "id");
 
 	return execute(sql) > 0;
+}
+
+bool PocoSQLIdentityDao::parseSingle(RecordSet &result,
+		Identity &identity, const string &prefix)
+{
+	if (result.begin() == result.end())
+		return false;
+
+	return parseSingle(*result.begin(), identity, prefix);
+}
+
+bool PocoSQLIdentityDao::parseSingle(Row &result,
+		Identity &identity, const string &prefix)
+{
+	if (hasColumn(result, prefix + "id"))
+		identity = Identity(IdentityID::parse(result[prefix + "id"]));
+
+	identity.setEmail(result[prefix + "email"]);
+	return true;
 }

server/src/dao/poco/PocoSQLIdentityDao.h
@@ -5,6 +5,8 @@ namespace Poco {
 namespace Data {
 
 class Session;
+class RecordSet;
+class Row;
 
 }
 }
@@ -20,6 +22,11 @@ public:
 	bool fetchBy(Identity &identity,
 			const std::string &email) override;
 	bool remove(const Identity &identity) override;
+
+	static bool parseSingle(Poco::Data::RecordSet &result,
+			Identity &identity, const std::string &prefix = "");
+	static bool parseSingle(Poco::Data::Row &result,
+			Identity &identity, const std::string &prefix = "");
 
 protected:
 	void create(Poco::Data::Session &session, Identity &identity);

commit a9284307158ecf017acd2603c889fb5ad34f2c31

PocoSQLIdentityDao: drop into(email)
The into() is not to be used anymore. Data extraction is
done by parseSingle() method.

Fixes: f3304b87a2fc ("PocoSQLIdentityDao: extract method parseSingle")
Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

server/src/dao/poco/PocoSQLIdentityDao.cpp
@@ -59,12 +59,10 @@ bool PocoSQLIdentityDao::fetch(Session &session, Identity &identity)
 	assureHasId(identity);
 
 	string id(identity.id().toString());
-	string email;
 
 	Statement sql(session);
 	sql << findQuery("identities.fetch.by.id"),
-		use(id, "id"),
-		into(email);
+		use(id, "id");
 
 	if (execute(sql) == 0)
 		return false;