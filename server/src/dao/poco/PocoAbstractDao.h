474d1bae1fa102681d654fec4725d941f786d6e4
Jan Viktorin <iviktorin@fit.vutbr.cz>
2017-01-05 13:29:46 +0100

    PocoAbstractDao: define template method whenNull
    Generalize situations when we want to return a default
    value of data that are empty (or NULL).
    
    Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

--- server/src/dao/poco/PocoAbstractDao.h
+++ server/src/dao/poco/PocoAbstractDao.h
@@ -42,6 +42,12 @@ protected:
 	static bool hasColumn(const Poco::Data::RecordSet &result, const std::string &name);
 	static bool hasColumn(const Poco::Data::Row &result, const std::string &name);
 
+	template <typename D>
+	static D whenNull(const Poco::Dynamic::Var &v, const D def)
+	{
+		return v.isEmpty()? def : v.convert<D>();
+	}
+
 	std::size_t execute(Poco::Data::Statement &sql);
 	void throwMissingId(const std::type_info &t);
 
