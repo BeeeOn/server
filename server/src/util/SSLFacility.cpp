BeeeOn/server

commit 4159deaf52d53de57fbbfc16cf04263cd27e71fa

SSLFacility: provide property disabledProtocols
The Poco library (since 1.7.0) supports to disable certain protocol
implementations as described in the following commit:

   e9b2b3bcdd7e ("NetSSL: add support for disabling certain protocols")

Implement API to utilize this feature.

The SSLv2 and SSLv3 are disabled as default. This can be overrided by
calling setDisabledProtocols or setting property disabledProtocols via
dependency injection. E.g.

   <instance ... class="BeeeOn::SSLClient">
	<set name="disabledProtocols" text="sslv2,sslv3,tlsv1" />
   </instance>

The SSLFacility logs a warning when the disabledProtocols feature is
unavailable due to the old Poco library.

Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

server/src/util/SSLFacility.cpp
@@ -1,7 +1,9 @@
+#include <Poco/Version.h>
 #include <Poco/String.h>
 #include <Poco/Delegate.h>
 #include <Poco/StringTokenizer.h>
 #include <Poco/NumberParser.h>
+#include <Poco/NumberFormatter.h>
 #include <Poco/Net/Utility.h>
 #include <Poco/Net/SSLManager.h>
 
@@ -90,6 +92,12 @@ SSLFacility::SSLFacility():
 			&SSLFacility::setCipherList);
 	textInjector("sessionCache", (TextSetter)
 			&SSLFacility::setSessionCache);
+	textInjector("disabledProtocols", (TextSetter)
+			&SSLFacility::setDisabledProtocols);
+
+#if POCO_VERSION >= 0x01070000
+	m_disabledProtocols = Context::PROTO_SSLV2 | Context::PROTO_SSLV3;
+#endif
 }
 
 SSLFacility::~SSLFacility()
@@ -111,6 +119,14 @@ void SSLFacility::initContext()
 		m_context = createContext();
 	}
 
+#if POCO_VERSION >= 0x01070000
+	m_context->disableProtocols(m_disabledProtocols);
+#else
+	if (m_disabledProtocols)
+		m_logger.warning("protocols are not disabled, Poco library is too old",
+				__FILE__, __LINE__);
+#endif
+
 	m_logger.information("SSL context initialized",
 				__FILE__, __LINE__);
 
@@ -123,7 +139,7 @@ void SSLFacility::initContext()
 	m_logger.debug("verificationDepth: " + to_string(m_verificationDepth));
 	m_logger.debug("cihperList: " + m_cipherList);
 	m_logger.debug("sessionCache: " + to_string(m_sessionCache));
-	m_logger.debug("disableProtocols: " + to_string(m_disabledProtocols));
+	m_logger.debug("disableProtocols: " + NumberFormatter::formatHex(m_disabledProtocols, 10, true));
 }
 
 Context::Ptr SSLFacility::context()
@@ -180,4 +196,45 @@ void SSLFacility::setVerificationDepth(int depth)
 void SSLFacility::setCipherList(const string &list)
 {
 	m_cipherList = list;
+}
+
+static unsigned int parseProtocol(const string &protocol)
+{
+#if POCO_VERSION >= 0x01070000
+	if (!icompare(protocol, "sslv2"))
+		return Context::PROTO_SSLV2;
+	else if (!icompare(protocol, "sslv3"))
+		return Context::PROTO_SSLV3;
+	else if (!icompare(protocol, "tlsv1"))
+		return Context::PROTO_TLSV1;
+	else if (!icompare(protocol, "tlsv1_1"))
+		return Context::PROTO_TLSV1_1;
+	else if (!icompare(protocol, "tlsv1_2"))
+		return Context::PROTO_TLSV1_2;
+	else
+#endif
+		return 0;
+}
+
+void SSLFacility::setDisabledProtocols(const string &protocols)
+{
+	StringTokenizer tokenizer(protocols, ",;",
+		StringTokenizer::TOK_IGNORE_EMPTY | StringTokenizer::TOK_TRIM);
+
+	unsigned int disable = 0;
+
+	for (auto tok : tokenizer) {
+		m_logger.debug("parsing SSL protocol: " + tok,
+				__FILE__, __LINE__);
+
+		unsigned int ret = parseProtocol(tok);
+		if (ret == 0) {
+			m_logger.warning("failed to recognize SSL protocol: " + tok,
+					__FILE__, __LINE__);
+		}
+
+		disable |= ret;
+	}
+
+	m_disabledProtocols = disable;
 }

server/src/util/SSLFacility.h
@@ -86,6 +86,7 @@ public:
 	void setVerificationDepth(int depth);
 	void setCipherList(const std::string &list);
 	void setSessionCache(const std::string &enable);
+	void setDisabledProtocols(const std::string &protocols);
 
 	Poco::Net::Context::Ptr context();
 
@@ -110,6 +111,7 @@ protected:
 	int m_verificationDepth;
 	std::string m_cipherList;
 	bool m_sessionCache;
+	unsigned int m_disabledProtocols;
 };
 
 }

commit b23c1f6a121dd5115e4154bf3ba332339b6f9053

SSLFacility: improve warning about too old Poco
It is helpful to know what was the value POCO_VERSION
during compilation.

Fixes: 4159deaf52d5 ("SSLFacility: provide property disabledProtocols")
Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

server/src/util/SSLFacility.cpp
@@ -123,7 +123,8 @@ void SSLFacility::initContext()
 	m_context->disableProtocols(m_disabledProtocols);
 #else
 	if (m_disabledProtocols)
-		m_logger.warning("protocols are not disabled, Poco library is too old",
+		m_logger.warning("protocols are not disabled, linked against too old Poco library: "
+				+ NumberFormatter::formatHex(POCO_VERSION, 10, true),
 				__FILE__, __LINE__);
 #endif
 