5369626430ce20f5e463b4a771144f068a35ec51
Jan Viktorin <iviktorin@fit.vutbr.cz>
2017-01-06 13:19:47 +0100

    LocationService: support transactions
    Operations createIn, updateIn, remove, removeFrom, fetch,
    fetchFrom, fetchBy are now transactional.
    
    Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

--- server/src/service/LocationService.h
+++ server/src/service/LocationService.h
@@ -7,6 +7,7 @@
 #include <Poco/Logger.h>
 #include "service/Single.h"
 #include "service/Relation.h"
+#include "dao/Transactional.h"
 #include "di/InjectorTarget.h"
 #include "model/Location.h"
 #include "model/Place.h"
@@ -22,7 +23,7 @@ class LocationAccessPolicy;
 /**
  * Locations management.
  */
-class LocationService : public AbstractInjectorTarget {
+class LocationService : public Transactional {
 public:
 	LocationService();
 
@@ -30,16 +31,67 @@ public:
 	void setGatewayDao(GatewayDao *dao);
 	void setAccessPolicy(LocationAccessPolicy *policy);
 
-	void createIn(RelationWithData<Location, Place> &input);
-	void createIn(RelationWithData<Location, Gateway> &input);
-	bool fetch(Single<Location> &input);
-	bool fetchFrom(Relation<Location, Place> &input);
-	void fetchBy(Relation<std::vector<Location>, Gateway> &input);
-	bool updateIn(RelationWithData<Location, Place> &input);
-	bool updateIn(RelationWithData<Location, Gateway> &input);
-	bool remove(Single<Location> &input);
-	bool removeFrom(Relation<Location, Place> &input);
-	bool removeFrom(Relation<Location, Gateway> &input);
+	void createIn(RelationWithData<Location, Place> &input)
+	{
+		BEEEON_TRANSACTION(doCreateIn(input));
+	}
+
+	void createIn(RelationWithData<Location, Gateway> &input)
+	{
+		BEEEON_TRANSACTION(doCreateIn(input));
+	}
+
+	bool fetch(Single<Location> &input)
+	{
+		return BEEEON_TRANSACTION_RETURN(bool, doFetch(input));
+	}
+
+	bool fetchFrom(Relation<Location, Place> &input)
+	{
+		return BEEEON_TRANSACTION_RETURN(bool, doFetchFrom(input));
+	}
+
+	void fetchBy(Relation<std::vector<Location>, Gateway> &input)
+	{
+		BEEEON_TRANSACTION(doFetchBy(input));
+	}
+
+	bool updateIn(RelationWithData<Location, Place> &input)
+	{
+		return BEEEON_TRANSACTION_RETURN(bool, doUpdateIn(input));
+	}
+
+	bool updateIn(RelationWithData<Location, Gateway> &input)
+	{
+		return BEEEON_TRANSACTION_RETURN(bool, doUpdateIn(input));
+	}
+
+	bool remove(Single<Location> &input)
+	{
+		return BEEEON_TRANSACTION_RETURN(bool, doRemove(input));
+	}
+
+	bool removeFrom(Relation<Location, Place> &input)
+	{
+		return BEEEON_TRANSACTION_RETURN(bool, doRemoveFrom(input));
+	}
+
+	bool removeFrom(Relation<Location, Gateway> &input)
+	{
+		return BEEEON_TRANSACTION_RETURN(bool, doRemoveFrom(input));
+	}
+
+protected:
+	void doCreateIn(RelationWithData<Location, Place> &input);
+	void doCreateIn(RelationWithData<Location, Gateway> &input);
+	bool doFetch(Single<Location> &input);
+	bool doFetchFrom(Relation<Location, Place> &input);
+	void doFetchBy(Relation<std::vector<Location>, Gateway> &input);
+	bool doUpdateIn(RelationWithData<Location, Place> &input);
+	bool doUpdateIn(RelationWithData<Location, Gateway> &input);
+	bool doRemove(Single<Location> &input);
+	bool doRemoveFrom(Relation<Location, Place> &input);
+	bool doRemoveFrom(Relation<Location, Gateway> &input);
 
 private:
 	LocationDao *m_dao;
