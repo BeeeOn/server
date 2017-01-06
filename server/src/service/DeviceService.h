29c6a09124fd812ff036ed79a57ba8c5221e7512
Jan Viktorin <iviktorin@fit.vutbr.cz>
2017-01-06 13:19:47 +0100

    DeviceService: support transactions
    Operations fetch, fetchMany, fetchActiveBy, fetchInactiveBy,
    activate, unregister and update are now transactional.
    
    Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

--- server/src/service/DeviceService.h
+++ server/src/service/DeviceService.h
@@ -5,6 +5,7 @@
 #include <vector>
 
 #include "di/InjectorTarget.h"
+#include "dao/Transactional.h"
 #include "service/Relation.h"
 #include "model/Device.h"
 #include "model/Gateway.h"
@@ -18,7 +19,7 @@ class DeviceAccessPolicy;
 /**
  * Service for devices management.
  */
-class DeviceService : public AbstractInjectorTarget {
+class DeviceService : public Transactional {
 public:
 	DeviceService();
 
@@ -26,17 +27,57 @@ public:
 	void setGatewayRPC(GatewayRPC *rpc);
 	void setAccessPolicy(DeviceAccessPolicy *policy);
 
-	bool fetch(Relation<Device, Gateway> &input);
-	void fetchMany(Relation<std::list<Device>, Gateway> &input);
-	void fetchActiveBy(Relation<std::vector<Device>, Gateway> &input);
-	void fetchInactiveBy(Relation<std::vector<Device>, Gateway> &input);
+	bool fetch(Relation<Device, Gateway> &input)
+	{
+		return BEEEON_TRANSACTION_RETURN(bool, doFetch(input));
+	}
 
-	bool activate(Relation<Device, Gateway> &input);
-	bool unregister(Relation<Device, Gateway> &input);
-	bool update(RelationWithData<Device, Gateway> &input);
-	bool updateAndActivate(RelationWithData<Device, Gateway> &input);
+	void fetchMany(Relation<std::list<Device>, Gateway> &input)
+	{
+		BEEEON_TRANSACTION(doFetchMany(input));
+	}
+
+	void fetchActiveBy(Relation<std::vector<Device>, Gateway> &input)
+	{
+		BEEEON_TRANSACTION(doFetchActiveBy(input));
+	}
+
+	void fetchInactiveBy(Relation<std::vector<Device>, Gateway> &input)
+	{
+		BEEEON_TRANSACTION(doFetchInactiveBy(input));
+	}
+
+	bool activate(Relation<Device, Gateway> &input)
+	{
+		return BEEEON_TRANSACTION_RETURN(bool, doActivate(input));
+	}
+
+	bool unregister(Relation<Device, Gateway> &input)
+	{
+		return BEEEON_TRANSACTION_RETURN(bool, doUnregister(input));
+	}
+
+	bool update(RelationWithData<Device, Gateway> &input)
+	{
+		return BEEEON_TRANSACTION_RETURN(bool, doUpdate(input));
+	}
+
+	bool updateAndActivate(RelationWithData<Device, Gateway> &input)
+	{
+		return BEEEON_TRANSACTION_RETURN(bool, doUpdateAndActivate(input));
+	}
 
 protected:
+	bool doFetch(Relation<Device, Gateway> &input);
+	void doFetchMany(Relation<std::list<Device>, Gateway> &input);
+	void doFetchActiveBy(Relation<std::vector<Device>, Gateway> &input);
+	void doFetchInactiveBy(Relation<std::vector<Device>, Gateway> &input);
+
+	bool doActivate(Relation<Device, Gateway> &input);
+	bool doUnregister(Relation<Device, Gateway> &input);
+	bool doUpdate(RelationWithData<Device, Gateway> &input);
+	bool doUpdateAndActivate(RelationWithData<Device, Gateway> &input);
+
 	bool prepareUpdate(RelationWithData<Device, Gateway> &input);
 	bool tryActivateAndUpdate(Device &device,
 			const Gateway &gateway, bool forceUpdate = false);
