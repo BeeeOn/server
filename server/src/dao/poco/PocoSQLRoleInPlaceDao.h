0044ce061ef526c9ecd0b71fc617356596db3f40
Jan Viktorin <iviktorin@fit.vutbr.cz>
2017-01-06 16:33:26 +0100

    PocoSQLRoleInPlaceDao: migrate to use session()
    Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

--- server/src/dao/poco/PocoSQLRoleInPlaceDao.h
+++ server/src/dao/poco/PocoSQLRoleInPlaceDao.h
@@ -6,7 +6,6 @@
 namespace Poco {
 namespace Data {
 
-class Session;
 class RecordSet;
 class Row;
 
@@ -59,27 +58,6 @@ public:
 			collection.push_back(role);
 		}
 	}
-
-protected:
-	void create(Poco::Data::Session &session, RoleInPlace &role);
-	bool fetch(Poco::Data::Session &session, RoleInPlace &role);
-	void fetchBy(Poco::Data::Session &session,
-			std::vector<RoleInPlace> &roles,
-			const Place &place);
-	bool hasUsersExcept(Poco::Data::Session &session,
-			const Place &place,
-			const User &user);
-	bool update(Poco::Data::Session &session, RoleInPlace &role);
-	bool remove(Poco::Data::Session &session, const RoleInPlace &role);
-
-	AccessLevel fetchAccessLevel(Poco::Data::Session &session,
-			const Place &place,
-			const User &user);
-
-	void fetchAccessiblePlaces(Poco::Data::Session &session,
-			std::vector<Place> &list,
-			const User &user,
-			const AccessLevel &atLeast);
 
 private:
 	SQLQuery m_queryCreate           {"roles_in_place.create"};
