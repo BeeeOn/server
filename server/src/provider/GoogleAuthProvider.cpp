BeeeOn/server

commit 51b2fbd21ee5c6ed08f0b58255d6a88a2748ee4c

GoogleAuthProvider: new class
Class GoogleAuthProvider verifies Google's authorization code against their
API and stores user info in Result map.

Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>
Signed-off-by: Petr Stehlík <xstehl14@stud.fit.vutbr.cz>

server/src/provider/GoogleAuthProvider.cpp
@@ -1,0 +1,167 @@
+#include <Poco/Logger.h>
+#include <Poco/URI.h>
+#include <Poco/Net/Context.h>
+#include <Poco/Net/NetException.h>
+#include <Poco/Net/HTTPRequest.h>
+#include <Poco/Net/HTTPResponse.h>
+#include <Poco/Net/HTTPSClientSession.h>
+#include <Poco/Net/SSLManager.h>
+#include <Poco/Net/InvalidCertificateHandler.h>
+#include <Poco/Net/AcceptCertificateHandler.h>
+#include <Poco/StreamCopier.h>
+
+#include "Debug.h"
+#include "model/JSONSerializer.h"
+#include "provider/GoogleAuthProvider.h"
+
+using namespace std;
+using namespace Poco;
+using namespace Poco::JSON;
+using namespace Poco::Net;
+using namespace BeeeOn;
+
+bool GoogleAuthProvider::verifyToken(const string &token, Result &info)
+{
+	string idToken;
+	string rawInfo;
+	string google_id;
+	string email;
+
+	try {
+		idToken = requestIdToken(token);
+		rawInfo = fetchUserInfo(idToken);
+	} catch(const Exception &e) {
+		m_logger.log(e, __FILE__, __LINE__);
+		return false;
+	}
+
+	JSONObjectSerializer userInfo(rawInfo);
+
+	if (userInfo.get("sub", google_id))
+		info.insert(make_pair("google_id", google_id));
+	if (userInfo.get("email", email))
+		info.insert(make_pair("email", email));
+
+	if (email.empty() || google_id.empty())
+		return false;
+
+	return true;
+}
+
+string GoogleAuthProvider::requestIdToken(const string &token)
+{
+	TRACE_METHOD();
+
+	HTTPSClientSession *session;
+	URI uri(m_tokenUrl);
+
+	try {
+		initSSL();
+		session = new HTTPSClientSession(uri.getHost(), uri.getPort());
+	} catch (const Exception &e) {
+		m_logger.log(e, __FILE__, __LINE__);
+		throw;
+	}
+
+	string requestRaw = "code=" + token + "&"
+		"redirect_uri=postmessage&"
+		"client_id=" + m_clientId + "&"
+		"client_secret=" + m_clientSecret + "&"
+		"scope=&"	// No need to specify, defaults to userinfo.profile,userinfo.email
+		"grant_type=authorization_code";
+
+	m_logger.debug("request: " + requestRaw, __FILE__, __LINE__) ;
+
+	HTTPRequest req(HTTPRequest::HTTP_POST,
+			uri.getPathAndQuery(),
+			HTTPMessage::HTTP_1_1);
+	req.setContentType("application/x-www-form-urlencoded");
+	req.setContentLength(requestRaw.length());
+
+	session->sendRequest(req) << requestRaw;
+	string receiveResponse = handleResponse(*session);
+
+	m_logger.debug("response: " + receiveResponse, __FILE__, __LINE__);
+
+	JSONObjectSerializer userTokens(receiveResponse);
+	string idToken;
+
+	// TODO: verify signature of the ID token by Google certificate
+	if (userTokens.get("id_token", idToken))
+		return idToken;
+
+	m_logger.error("No ID token to obtain user data", __FILE__, __LINE__);
+
+	throw NotAuthenticatedException("Missing id_token field in response.");
+
+}
+
+string GoogleAuthProvider::fetchUserInfo(const string &token)
+{
+	TRACE_METHOD();
+
+	URI uri(m_tokenInfoUrl + token);
+	HTTPSClientSession *session;
+
+	try {
+		initSSL();
+		session = new HTTPSClientSession(uri.getHost(), uri.getPort());
+	} catch (const Exception &e) {
+		m_logger.log(e, __FILE__, __LINE__);
+		throw;
+	}
+
+	HTTPRequest req(HTTPRequest::HTTP_GET,
+			uri.getPathAndQuery(),
+			HTTPMessage::HTTP_1_1);
+
+	session->sendRequest(req);
+	string receiveResponse = handleResponse(*session);
+
+	m_logger.debug("response: " + receiveResponse, __FILE__, __LINE__);
+
+	return receiveResponse;
+}
+
+void GoogleAuthProvider::initSSL()
+{
+	TRACE_METHOD();
+
+	initializeSSL();
+
+	/* Handles certificates for HTTPS communication
+	 * TODO: accept only google certificate, now accepts all certs!!!
+	 */
+	SSLManager::InvalidCertificateHandlerPtr ptrHandler (
+			new Poco::Net::AcceptCertificateHandler(false));
+	const Context::Ptr context = new Context(Context::CLIENT_USE, "");
+	SSLManager::instance().initializeClient(0, ptrHandler, context);
+}
+
+string GoogleAuthProvider::convertResponseToString(istream &rs)
+{
+	string response;
+	stringstream ss;
+	StreamCopier::copyToString(rs, response);
+
+	return response;
+}
+
+string GoogleAuthProvider::handleResponse(HTTPSClientSession &session)
+{
+	HTTPResponse response;
+	istream &rs = session.receiveResponse(response);
+
+	m_logger.debug("response status: " + to_string(response.getStatus())
+			+ " " +	response.getReason(), __FILE__, __LINE__);
+
+	string receiveResponse = convertResponseToString(rs);
+	m_logger.debug("response: " + receiveResponse, __FILE__, __LINE__);
+
+	if (response.getStatus() != HTTPResponse::HTTP_OK) {
+		throw NotAuthenticatedException(
+			"failed to retrieve access token from Google APIs");
+	}
+
+	return receiveResponse;
+}

server/src/CMakeLists.txt
@@ -2,6 +2,8 @@ cmake_minimum_required (VERSION 2.8.11)
 project (ui-server C CXX)
 
 find_library (POCO_FOUNDATION PocoFoundation)
+find_library (POCO_SSL PocoNetSSL)
+find_library (POCO_CRYPTO PocoCrypto)
 find_library (POCO_UTIL PocoUtil)
 find_library (POCO_NET PocoNet)
 find_library (POCO_JSON PocoJSON)
@@ -15,6 +17,7 @@ file(GLOB SOURCES
 	${PROJECT_SOURCE_DIR}/di/DependencyInjector.cpp
 	${PROJECT_SOURCE_DIR}/provider/AuthProvider.cpp
 	${PROJECT_SOURCE_DIR}/provider/RandomProvider.cpp
+	${PROJECT_SOURCE_DIR}/provider/GoogleAuthProvider.cpp
 	${PROJECT_SOURCE_DIR}/server/SessionManager.cpp
 	${PROJECT_SOURCE_DIR}/dao/PlaceDao.cpp
 	${PROJECT_SOURCE_DIR}/dao/UserDao.cpp
@@ -43,6 +46,8 @@ target_link_libraries(ui-server
 	BeeeOnServer
 	-Wl,--no-whole-archive
 	${POCO_FOUNDATION}
+	${POCO_SSL}
+	${POCO_CRYPTO}
 	${POCO_UTIL}
 	${POCO_NET}
 	${POCO_JSON}

server/src/provider/AuthProvider.cpp
@@ -1,4 +1,6 @@
 #include "provider/AuthProvider.h"
 #include "provider/PermitAuthProvider.h"
+#include "provider/GoogleAuthProvider.h"
 
 BEEEON_OBJECT(PermitAuthProvider, BeeeOn::PermitAuthProvider)
+BEEEON_OBJECT(GoogleAuthProvider, BeeeOn::GoogleAuthProvider)

server/src/provider/GoogleAuthProvider.h
@@ -1,0 +1,76 @@
+#ifndef BEEEON_GOOGLE_AUTH_PROVIDER_H
+#define BEEEON_GOOGLE_AUTH_PROVIDER_H
+
+#include <Poco/Net/HTTPSClientSession.h>
+
+#include "provider/AuthProvider.h"
+
+namespace BeeeOn {
+
+/**
+ * Verify the user access token against Google servers and obtain
+ * information about her.
+ */
+class GoogleAuthProvider : public TokenAuthProvider {
+public:
+	GoogleAuthProvider():
+		TokenAuthProvider("google")
+	{
+		textInjector("client_id",
+				(TextSetter) &GoogleAuthProvider::setClientId);
+		textInjector("client_secret",
+				(TextSetter) &GoogleAuthProvider::setClientSecret);
+	}
+
+	void setClientId(const std::string &clientId)
+	{
+		m_clientId = clientId;
+	}
+
+	void setClientSecret(const std::string &clientSecret)
+	{
+		m_clientSecret = clientSecret;
+	}
+
+protected:
+	bool verifyToken(const std::string &token, Result &info);
+
+private:
+	/**
+	 * Get token info a.k.a. user info from Google API
+	 */
+	std::string fetchUserInfo(const std::string &token);
+
+	/**
+	 * Get access token from Google APIs
+	 */
+	std::string requestIdToken(const std::string &token);
+
+	/**
+	 * Handle response based on given session
+	 */
+	std::string handleResponse(Poco::Net::HTTPSClientSession &session);
+
+	/**
+	 * Initialize SSL context for upcoming HTTPS requests
+	 */
+	void initSSL();
+
+	/**
+	 * Convert istream response body to string
+	 */
+	std::string convertResponseToString(std::istream &rs);
+
+	std::string m_clientId;
+	std::string m_clientSecret;
+
+	// URL to get access and ID token from Google API
+	const std::string m_tokenUrl = "https://www.googleapis.com/oauth2/v4/token";
+	// URL to get user info using ID token
+	const std::string m_tokenInfoUrl =
+		"https://www.googleapis.com/oauth2/v3/tokeninfo?id_token=";
+};
+
+}
+
+#endif

server/src/ui/server.ini
@@ -2,3 +2,7 @@
 [ui]
 config.logging  = ${config.dir}logging.ini
 config.services = ${config.dir}services.xml
+
+[google]
+;client_secret =
+;client_id =

server/src/ui/services.xml
@@ -19,6 +19,7 @@
 			<set name="userDao" ref="userDao" />
 			<set name="sessionManager" ref="sessionManager" />
 			<add name="providers" ref="permitAuthProvider" />
+			<add name="providers" ref="googleAuthProvider" />
 		</instance>
 
 		<instance name="deviceService" class="BeeeOn::DeviceService">
@@ -43,6 +44,12 @@
 
 		<instance name="permitAuthProvider"
 				class="BeeeOn::PermitAuthProvider" />
+
+		<instance name="googleAuthProvider"
+				class="BeeeOn::GoogleAuthProvider">
+			<set name="client_secret" text="${google.client_secret}"/>
+			<set name="client_id" text="${google.client_id}"/>
+		</instance>
 
 		<instance name="pocoRandomProvider"
 				class="BeeeOn::PocoRandomProvider" />

commit 396f624b46179cd6f8c171ade8844a0dea1429ad

GoogleAuthProvider: fix memory leak
The HTTPSClientSession is allocated on heap and never free'd.
Utilize SharedPtr to deallocate automatically.

Fixes: 51b2fbd21ee5 ("GoogleAuthProvider: new class")
Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

server/src/provider/GoogleAuthProvider.cpp
@@ -64,7 +64,7 @@ string GoogleAuthProvider::requestIdToken(const string &authCode)
 {
 	TRACE_METHOD();
 
-	HTTPSClientSession *session;
+	SharedPtr<HTTPSClientSession> session;
 	URI uri(m_tokenUrl);
 
 	session = connectSecure(uri.getHost(), uri.getPort());
@@ -107,7 +107,7 @@ string GoogleAuthProvider::fetchUserInfo(const string &token)
 	TRACE_METHOD();
 
 	URI uri(m_tokenInfoUrl + token);
-	HTTPSClientSession *session;
+	SharedPtr<HTTPSClientSession> session;
 
 	session = connectSecure(uri.getHost(), uri.getPort());
 

server/src/provider/OAuth2AuthProvider.cpp
@@ -42,7 +42,7 @@ void OAuth2AuthProvider::initSSL()
 				"missing sslConfig, cannot use OAuth");
 }
 
-HTTPSClientSession *OAuth2AuthProvider::connectSecure(
+SharedPtr<HTTPSClientSession> OAuth2AuthProvider::connectSecure(
 		const std::string &host,
 		unsigned int port)
 {

server/src/provider/OAuth2AuthProvider.h
@@ -1,6 +1,7 @@
 #ifndef BEEEON_OAUTH2_AUTH_PROVIDER_H
 #define BEEEON_OAUTH2_AUTH_PROVIDER_H
 
+#include <Poco/SharedPtr.h>
 #include <Poco/Net/Context.h>
 
 #include "provider/AuthProvider.h"
@@ -45,7 +46,7 @@ protected:
 	/**
 	 * Open HTTPS connection to the given host:port.
 	 */
-	Poco::Net::HTTPSClientSession *connectSecure(
+	Poco::SharedPtr<Poco::Net::HTTPSClientSession> connectSecure(
 			const std::string &host,
 			unsigned int port);
 