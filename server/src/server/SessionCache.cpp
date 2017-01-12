BeeeOn/server

commit e5d97092ed74b2e6823f975232cc87f38b92634c

SessionCache: debug log when a session is removed
Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

server/src/server/SessionCache.cpp
@@ -89,6 +89,13 @@ void SessionCache::removeSession(const SessionID &sessionID, const UserID &userI
 
 		if (itUser->second.size() == 0)
 			m_userSessionSet.erase(itUser);
+
+		if (m_logger.debug()) {
+			m_logger.debug("user: " + userID.toString() +
+					" remove session, sessions: " +
+					std::to_string(itUser->second.size()),
+					__FILE__, __LINE__);
+		}
 	}
 }
 

commit d885c91995abfccb67dc2d72bc9fba6c4282f8e0

SessionCache: fix read of erased memory
The debug log reads count of remaining sessions for a user.
It reads the amount even after the user is deleted from the
m_userSessionSet. This leads to valgrind error

  "Invalid read of size 8"

Fixes: e5d97092ed74 ("SessionCache: debug log when a session is removed")
Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

server/src/server/SessionCache.cpp
@@ -87,15 +87,15 @@ void SessionCache::removeSession(const SessionID &sessionID, const UserID &userI
 		if (itSession != itUser->second.end())
 			itUser->second.erase(itSession);
 
-		if (itUser->second.size() == 0)
-			m_userSessionSet.erase(itUser);
-
 		if (logger().debug()) {
 			logger().debug("user: " + userID.toString() +
 					" remove session, sessions: " +
 					std::to_string(itUser->second.size()),
 					__FILE__, __LINE__);
 		}
+
+		if (itUser->second.size() == 0)
+			m_userSessionSet.erase(itUser);
 	}
 }
 