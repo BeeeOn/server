BeeeOn/server

commit a125e5fc547048207e26bc966cb42306ae20b32e

GatewayXmlHandler: implement handleGet via LegacyGateway
Squash of several closely related changes:

 * query: implement legacy_gateways.fetch.by.id
 * PocoSQLGatewayDao: implement fetch LegacyGateway
 * GatewayService: implement fetch of LegacyGateway

The query implements fetching of legacy gateways by ID. The
XML-UI API also requires to find out the access level to the
gateway by the user who is asking it.

The final handleGet implementations just exchanges using
of Gateway for LegacyGateway. The method overloading and
polymophism handle the rest.

Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

server/schema/home8-query.xml
@@ -1272,4 +1272,70 @@
 		</test>
 	</query>
 
+	<query id="legacy_gateways.fetch.by.id">
+		<define>
+		SELECT
+			g.name AS name,
+			g.place_id AS place_id,
+			p.name AS place_name,
+			g.altitude AS altitude,
+			g.latitude AS latitude,
+			g.longitude AS longitude,
+			g.roles_count AS roles_count,
+			g.devices_count AS devices_count,
+			g.owner_id AS owner_id,
+			(SELECT level FROM users_of_gateway
+				WHERE
+					gateway_id = g.id
+					AND
+					user_id = <value name="user_id" type="uuid" />)
+			AS access_level
+		FROM legacy_gateways AS g
+		LEFT JOIN places AS p ON g.place_id = p.id
+		WHERE g.id = <value name="id" type="int64" />
+		</define>
+		<test name="simple test">
+			<check name="fetch Test Gateway 2 as Pepa Goodman and check it is owned by Franta Kutil">
+				<call-query>
+					<arg name="user_id">'29d214d4-2057-4e06-b003-b839d0bb1df6'</arg>
+					<arg name="id">140548523101825</arg>
+				</call-query>
+				<expect>
+					<row>
+						<value name="name">Test Gateway 2</value>
+						<value name="place_id">bd8ea40c-91c9-4d8c-8c13-a8332c39a437</value>
+						<value name="place_name">Test Place 2</value>
+						<value name="altitude">-0.2</value>
+						<value name="latitude">0.2</value>
+						<value name="longitude">0.22</value>
+						<value name="roles_count">2</value>
+						<value name="devices_count">0</value>
+						<value name="owner_id">014ca448-26d7-4051-ad11-8c9e37343f0d</value>
+						<value name="access_level">10</value>
+					</row>
+				</expect>
+			</check>
+			<check name="fetch Test Gateway 1 as Jack Sparrow and check it is owned by Pepa Goodman">
+				<call-query>
+					<arg name="user_id">'d67713ff-de75-43c4-917b-88f0048b7ccf'</arg>
+					<arg name="id">181462068183316</arg>
+				</call-query>
+				<expect>
+					<row>
+						<value name="name">Test Gateway 1</value>
+						<value name="place_id">16de2f05-8af7-4729-8479-1973590cfa54</value>
+						<value name="place_name">Test Place 1</value>
+						<value name="altitude">-0.1</value>
+						<value name="latitude">0.1</value>
+						<value name="longitude">0.11</value>
+						<value name="roles_count">3</value>
+						<value name="devices_count">0</value>
+						<value name="owner_id">29d214d4-2057-4e06-b003-b839d0bb1df6</value>
+						<value name="access_level">30</value>
+					</row>
+				</expect>
+			</check>
+		</test>
+	</query>
+
 </query-set>

server/src/dao/poco/PocoSQLGatewayDao.cpp
@@ -32,6 +32,12 @@ bool PocoSQLGatewayDao::fetch(Gateway &gateway)
 	return fetch(session, gateway);
 }
 
+bool PocoSQLGatewayDao::fetch(LegacyGateway &gateway, const User &user)
+{
+	Session session(manager().pool().get());
+	return fetch(session, gateway, user);
+}
+
 bool PocoSQLGatewayDao::update(Gateway &gateway)
 {
 	Session session(manager().pool().get());
@@ -115,6 +121,26 @@ bool PocoSQLGatewayDao::fetch(Session &session, Gateway &gateway)
 
 	Statement sql(session);
 	sql << findQuery("gateways.fetch.by.id"),
+		use(id, "id");
+
+	if (execute(sql) == 0)
+		return false;
+
+	RecordSet result(sql);
+	return parseSingle(result, gateway);
+}
+
+bool PocoSQLGatewayDao::fetch(Session &session, LegacyGateway &gateway, const User &user)
+{
+	assureHasId(gateway);
+	assureHasId(user);
+
+	string id(gateway.id().toString());
+	string userID(user.id().toString());
+
+	Statement sql(session);
+	sql << findQuery("legacy_gateways.fetch.by.id"),
+		use(userID, "user_id"),
 		use(id, "id");
 
 	if (execute(sql) == 0)

server/src/dao/GatewayDao.h
@@ -8,6 +8,7 @@
 #include "model/User.h"
 #include "model/Place.h"
 #include "model/Gateway.h"
+#include "model/LegacyGateway.h"
 
 namespace BeeeOn {
 
@@ -15,6 +16,7 @@ class GatewayDao {
 public:
 	virtual bool insert(Gateway &gateway) = 0;
 	virtual bool fetch(Gateway &gateway) = 0;
+	virtual bool fetch(LegacyGateway &gateway, const User &user) = 0;
 	virtual bool update(Gateway &gateway) = 0;
 	virtual bool assignAndUpdate(Gateway &gateway, const Place &place) = 0;
 	virtual bool assign(Gateway &gateway, const Place &place) = 0;
@@ -34,6 +36,11 @@ public:
 	 */
 	static GatewayDao &instance();
 
+	bool fetch(LegacyGateway &gateway, const User &user)
+	{
+		throw Poco::NotImplementedException(__func__);
+	}
+
 	bool assignAndUpdate(Gateway &gateway, const Place &place)
 	{
 		return update(gateway);
@@ -51,7 +58,7 @@ public:
 
 	bool fetchFromPlace(Gateway &gateway, const Place &place)
 	{
-		return fetch(gateway);
+		throw Poco::NotImplementedException(__func__);
 	}
 
 	void fetchAccessible(std::vector<Gateway> &gateways,

server/src/dao/poco/PocoSQLGatewayDao.h
@@ -20,6 +20,7 @@ class PocoSQLGatewayDao :
 public:
 	bool insert(Gateway &gateway) override;
 	bool fetch(Gateway &gateway) override;
+	bool fetch(LegacyGateway &gateway, const User &user) override;
 	bool update(Gateway &gateway) override;
 	bool assignAndUpdate(Gateway &gateway, const Place &place) override;
 	bool assign(Gateway &gateway, const Place &place) override;
@@ -65,6 +66,7 @@ public:
 protected:
 	bool insert(Poco::Data::Session &session, Gateway &gateway);
 	bool fetch(Poco::Data::Session &session, Gateway &gateway);
+	bool fetch(Poco::Data::Session &session, LegacyGateway &gateway, const User &user);
 	bool update(Poco::Data::Session &session, Gateway &gateway);
 	bool assignAndUpdate(Poco::Data::Session &session,
 			Gateway &gateway, const Place &place);

server/src/service/GatewayService.cpp
@@ -131,6 +131,13 @@ bool GatewayService::fetch(Single<Gateway> &input)
 	return m_gatewayDao->fetch(input.target());
 }
 
+bool GatewayService::fetch(Single<LegacyGateway> &input)
+{
+	m_accessPolicy->assureGet(input, input.target());
+
+	return m_gatewayDao->fetch(input.target(), input.user());
+}
+
 bool GatewayService::fetchFromPlace(Relation<Gateway, Place> &input)
 {
 	m_accessPolicy->assureGet(input, input.target());

server/src/service/GatewayService.h
@@ -13,6 +13,7 @@
 #include "model/VerifiedIdentity.h"
 #include "model/Place.h"
 #include "model/Gateway.h"
+#include "model/LegacyGateway.h"
 
 namespace BeeeOn {
 
@@ -53,6 +54,7 @@ public:
 			const VerifiedIdentity &verifiedIdentity);
 
 	bool fetch(Single<Gateway> &input);
+	bool fetch(Single<LegacyGateway> &input);
 	bool fetchFromPlace(Relation<Gateway, Place> &input);
 	void fetchAccessible(Relation<std::vector<Gateway>, User> &input);
 

server/src/xmlui/GatewayXmlHandler.cpp
@@ -5,6 +5,7 @@
 #include "xmlui/Serializing.h"
 #include "model/VerifiedIdentity.h"
 #include "model/Gateway.h"
+#include "model/LegacyGateway.h"
 #include "model/Place.h"
 
 using namespace std;
@@ -104,8 +105,9 @@ void GatewayXmlHandler::handleListen(Element *gatewayNode)
 
 void GatewayXmlHandler::handleGet(Element *gatewayNode)
 {
-	Gateway gateway(GatewayID::parse(gatewayNode->getAttribute("id")));
-	Single<Gateway> input(gateway);
+	LegacyGateway gateway(GatewayID::parse(
+			gatewayNode->getAttribute("id")));
+	Single<LegacyGateway> input(gateway);
 	User user(session()->userID());
 	input.setUser(user);
 

commit 8b314c4e2323540144200811f6408d435c892ee0

LegacyGateway: fix fetching of owner name
The XML-UI protocol expects the name of gateway's owner to be
included in certain responses. We have already supported the
owner field, however, due to lack of testing there was a hidden
bug. The owner's first_name and last_name were not fetched
and the PocoSQLUserDao::parseIfIDNotNull was not called.

This patch fixes the behaviour and adds more testing about this
topic to the SQL query level and to integration tests.

Fixes: a125e5fc5470 ("GatewayXmlHandler: implement handleGet via LegacyGateway")
Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

server/schema/home8-query.xml
@@ -1422,6 +1422,8 @@
 			g.roles_count AS roles_count,
 			g.devices_count AS devices_count,
 			g.owner_id AS owner_id,
+			o.first_name AS owner_first_name,
+			o.last_name AS owner_last_name,
 			(SELECT level FROM users_of_gateway
 				WHERE
 					gateway_id = g.id
@@ -1431,6 +1433,7 @@
 		FROM legacy_gateways AS g
 		LEFT JOIN places AS p ON g.place_id = p.id
 		LEFT JOIN gateways_status AS s ON s.gateway_id = g.id
+		LEFT JOIN users AS o ON o.id = g.owner_id
 		WHERE
 			g.id = <value name="id" type="int64" />
 			AND
@@ -1457,6 +1460,8 @@
 						<value name="roles_count">2</value>
 						<value name="devices_count">0</value>
 						<value type="uuid" name="owner_id">014ca448-26d7-4051-ad11-8c9e37343f0d</value>
+						<value name="owner_first_name">Franta</value>
+						<value name="owner_last_name">Kutil</value>
 						<value name="access_level">10</value>
 					</row>
 				</expect>
@@ -1480,6 +1485,8 @@
 						<value name="roles_count">3</value>
 						<value name="devices_count">2</value>
 						<value type="uuid" name="owner_id">29d214d4-2057-4e06-b003-b839d0bb1df6</value>
+						<value name="owner_first_name">Pepa</value>
+						<value name="owner_last_name">Goodman</value>
 						<value name="access_level">30</value>
 					</row>
 				</expect>
@@ -1503,11 +1510,14 @@
 			g.roles_count AS roles_count,
 			g.devices_count AS devices_count,
 			g.owner_id AS owner_id,
+			o.first_name AS owner_first_name,
+			o.last_name AS owner_last_name,
 			u.level AS access_level
 		FROM legacy_gateways AS g
 		JOIN users_of_gateway AS u ON g.id = u.gateway_id
 		LEFT JOIN places AS p ON g.place_id = p.id
 		LEFT JOIN gateways_status AS s ON s.gateway_id = g.id
+		LEFT JOIN users AS o ON o.id = g.owner_id
 		WHERE
 			u.user_id = <value name="user_id" type="uuid" />
 			AND
@@ -1535,6 +1545,8 @@
 						<value name="roles_count">3</value>
 						<value name="devices_count">2</value>
 						<value type="uuid" name="owner_id">29d214d4-2057-4e06-b003-b839d0bb1df6</value>
+						<value name="owner_first_name">Pepa</value>
+						<value name="owner_last_name">Goodman</value>
 						<value name="access_level">30</value>
 					</row>
 				</expect>

server/src/dao/poco/PocoSQLGatewayDao.cpp
@@ -8,6 +8,7 @@
 
 #include "dao/poco/PocoSQLGatewayDao.h"
 #include "dao/poco/PocoSQLPlaceDao.h"
+#include "dao/poco/PocoSQLUserDao.h"
 #include "dao/poco/PocoDaoManager.h"
 
 using namespace std;
@@ -287,8 +288,10 @@ bool PocoSQLGatewayDao::parseSingle(Row &result, LegacyGateway &gateway,
 	if (!accessLevel.isEmpty())
 		gateway.setAccessLevel(AccessLevel(accessLevel.convert<unsigned int>()));
 
-	User owner(UserID::parse(result[prefix + "owner_id"]));
-	gateway.setOwner(owner);
+	User owner;
+	if (PocoSQLUserDao::parseIfIDNotNull(result, owner, prefix + "owner_"))
+		gateway.setOwner(owner);
+
 	gateway.setUserCount(result[prefix + "roles_count"].convert<unsigned int>());
 	gateway.setDeviceCount(result[prefix + "devices_count"].convert<unsigned int>());
 

server/t/xmlui/t1001-gateway-register-unregister.py
@@ -68,7 +68,7 @@ class TestGatewayPlaceAssign(unittest.TestCase):
 		self.assertEqual("", response.root[0].get("version"))
 		self.assertEqual("0.0.0.0", response.root[0].get("ip"))
 		self.assertEqual("admin", response.root[0].get("permission"))
-		self.assertIsNotNone(response.root[0].get("owner"))
+		self.assertEqual("Joe Doe", response.root[0].get("owner"))
 		self.assertEqual("0", response.root[0].get("timezone"))
 		self.assertEqual("available", response.root[0].get("status"))
 
@@ -140,7 +140,7 @@ class TestGatewayPlaceAssign(unittest.TestCase):
 		self.assertEqual("", response.root[0].get("version"))
 		self.assertEqual("0.0.0.0", response.root[0].get("ip"))
 		self.assertEqual("admin", response.root[0].get("permission"))
-		self.assertIsNotNone(response.root[0].get("owner"))
+		self.assertEqual("Joe Doe", response.root[0].get("owner"))
 		self.assertEqual("0", response.root[0].get("timezone"))
 		self.assertEqual("available", response.root[0].get("status"))
 

server/t/xmlui/t2001-gateway-get-update.py
@@ -85,7 +85,7 @@ class TestGatewayGetUpdate(unittest.TestCase):
 		self.assertEqual("", response.root[0].get("version"))
 		self.assertEqual("0.0.0.0", response.root[0].get("ip"))
 		self.assertEqual("admin", response.root[0].get("permission"))
-		self.assertIsNotNone(response.root[0].get("owner"))
+		self.assertEqual("Joe Doe", response.root[0].get("owner"))
 		self.assertEqual("0", response.root[0].get("timezone"))
 		self.assertEqual("available", response.root[0].get("status"))
 