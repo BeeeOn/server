d09cfb516ed8a524ebd3e56fabce303789379680
Jan Viktorin <iviktorin@fit.vutbr.cz>
2017-01-16 11:32:05 +0100

    XmlValueConsumer: implement ValueConsumer for xmlui
    Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

---
+++ server/src/xmlui/XmlValueConsumer.cpp
@@ -0,0 +1,40 @@
+#include <string>
+
+#include <Poco/Exception.h>
+#include <Poco/XML/XMLWriter.h>
+
+#include "xmlui/XmlValueConsumer.h"
+
+using namespace std;
+using namespace Poco;
+using namespace BeeeOn;
+using namespace BeeeOn::XmlUI;
+
+XmlValueConsumer::XmlValueConsumer(Poco::XML::XMLWriter &writer):
+	m_writer(writer),
+	m_info(NULL)
+{
+}
+
+void XmlValueConsumer::begin(const TypeInfo &info)
+{
+	m_info = &info;
+}
+
+const TypeInfo &XmlValueConsumer::info() const
+{
+	if (m_info == NULL)
+		throw IllegalStateException("info is missing, forgot to call begin()?");
+
+	return *m_info;
+}
+
+void XmlValueConsumer::single(const ValueAt &v)
+{
+	m_writer.dataElement("", "row", "row", to_string(v.atRaw()) + " " + info().asString(v.value()));
+}
+
+void XmlValueConsumer::end()
+{
+	m_info = NULL;
+}
---
+++ server/src/xmlui/XmlValueConsumer.h
@@ -0,0 +1,36 @@
+#ifndef BEEEON_XML_VALUE_CONSUMER_H
+#define BEEEON_XML_VALUE_CONSUMER_H
+
+#include "service/ValueConsumer.h"
+
+namespace Poco {
+namespace XML {
+
+class XMLWriter;
+
+}
+}
+
+namespace BeeeOn {
+namespace XmlUI {
+
+class XmlValueConsumer : public BeeeOn::ValueConsumer {
+public:
+	XmlValueConsumer(Poco::XML::XMLWriter &writer);
+
+	void begin(const TypeInfo &info) override;
+	void single(const ValueAt &v) override;
+	void end() override;
+
+protected:
+	const TypeInfo &info() const;
+
+private:
+	Poco::XML::XMLWriter &m_writer;
+	const TypeInfo *m_info;
+};
+
+}
+}
+
+#endif
