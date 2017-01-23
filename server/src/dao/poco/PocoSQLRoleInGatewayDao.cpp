02c25cdf9bd38a78f09702666531c0f058c8863d
Jan Viktorin <iviktorin@fit.vutbr.cz>
2017-01-23 14:43:24 +0100

    RoleInGatewayDao: new class
    Provide interface RoleInGatewayDao with implementations:
    
    * NullRoleInGatewayDao
    * PocoSQLRoleInGatewayDao
    
    The PocoSQLRoleInGatewayDao utilizies new SQL queries:
    
    * roles_in_gateway.create
    * roles_in_gateway.remove
    * roles_in_gateway.is.registered
    * roles_in_gateway.fetch.by.gateway_id
    * roles_in_gateway.fetch.access_level
    * roles_in_gateway.fetch.accessible.gateways
    
    provided with tests (except of roles_in_gateway.create).
    
    Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

---
+++ server/src/dao/poco/PocoSQLRoleInGatewayDao.cpp
@@ -0,0 +1,171 @@
+#include <Poco/Exception.h>
+#include <Poco/Data/Statement.h>
+#include <Poco/Data/RowIterator.h>
+#include <Poco/Data/Row.h>
+#include <Poco/Data/RecordSet.h>
+
+#include "dao/poco/PocoSQLRoleInGatewayDao.h"
+#include "dao/poco/PocoSQLGatewayDao.h"
+#include "dao/poco/PocoSQLIdentityDao.h"
+#include "dao/poco/PocoDaoManager.h"
+#include "model/User.h"
+#include "model/Gateway.h"
+
+using namespace std;
+using namespace Poco;
+using namespace Poco::Data;
+using namespace Poco::Data::Keywords;
+using namespace BeeeOn;
+
+BEEEON_OBJECT(PocoSQLRoleInGatewayDao, BeeeOn::PocoSQLRoleInGatewayDao)
+
+PocoSQLRoleInGatewayDao::PocoSQLRoleInGatewayDao()
+{
+	registerQuery(m_queryCreate);
+	registerQuery(m_queryRemove);
+	registerQuery(m_queryIsRegistered);
+	registerQuery(m_queryFetchByGatewayId);
+	registerQuery(m_queryFetchAccessLevel);
+	registerQuery(m_queryFetchAccessibleGateways);
+}
+
+void PocoSQLRoleInGatewayDao::create(RoleInGateway &role)
+{
+	assureHasId(role.gateway());
+	assureHasId(role.identity());
+
+	role.setId(RoleInGatewayID::random());
+	string id(role.id().toString());
+	string gatewayID(role.gateway().id().toString());
+	string identityID(role.identity().id().toString());
+	unsigned int level = role.level();
+	unsigned long created = role.created().timestamp().epochTime();
+
+	Statement sql = (session() << m_queryCreate(),
+		use(id, "id"),
+		use(gatewayID, "gateway_id"),
+		use(identityID, "identity_id"),
+		use(level, "level"),
+		use(created, "created")
+	);
+
+	execute(sql);
+}
+
+void PocoSQLRoleInGatewayDao::fetchBy(std::vector<RoleInGateway> &roles,
+		const Gateway &gateway)
+{
+	assureHasId(gateway);
+
+	string gatewayID(gateway.id().toString());
+
+	Statement sql = (session() << m_queryFetchByGatewayId(),
+		use(gatewayID, "gateway_id")
+	);
+
+	execute(sql);
+	RecordSet result(sql);
+	parseMany(result, roles);
+}
+
+bool PocoSQLRoleInGatewayDao::remove(const RoleInGateway &role)
+{
+	assureHasId(role);
+
+	string id(role.id().toString());
+
+	Statement sql = (session() << m_queryRemove(),
+		use(id, "id")
+	);
+
+	return execute(sql) > 0;
+}
+
+bool PocoSQLRoleInGatewayDao::isRegistered(
+		const Gateway &gateway)
+{
+	assureHasId(gateway);
+
+	string gatewayID(gateway.id().toString());
+	bool result = false;
+
+	Statement sql = (session() << m_queryIsRegistered(),
+		use(gatewayID, "gateway_id"),
+		into(result)
+	);
+
+	execute(sql);
+	return result;
+}
+
+AccessLevel PocoSQLRoleInGatewayDao::fetchAccessLevel(
+		const Gateway &gateway,
+		const User &user)
+{
+	assureHasId(gateway);
+	assureHasId(user);
+
+	string gatewayID(gateway.id().toString());
+	string userID(user.id().toString());
+	Nullable<unsigned int> level;
+
+	Statement sql = (session() << m_queryFetchAccessLevel(),
+		use(gatewayID, "gateway_id"),
+		use(userID, "user_id"),
+		into(level)
+	);
+
+	execute(sql);
+	return level.isNull()? AccessLevel::none() : AccessLevel(level);
+}
+
+void PocoSQLRoleInGatewayDao::fetchAccessibleGateways(
+		std::vector<Gateway> &list,
+		const User &user,
+		const AccessLevel &atLeast)
+{
+	assureHasId(user);
+
+	string userID(user.id().toString());
+	unsigned int level = atLeast;
+
+	Statement sql = (session() << m_queryFetchAccessibleGateways(),
+		use(level, "at_least"),
+		use(userID, "user_id")
+	);
+
+	execute(sql);
+	RecordSet result(sql);
+	PocoSQLGatewayDao::parseMany<Gateway>(result, list);
+}
+
+bool PocoSQLRoleInGatewayDao::parseSingle(RecordSet &result,
+		RoleInGateway &role, const string &prefix)
+{
+	if (result.begin() == result.end())
+		return false;
+
+	return parseSingle(*result.begin(), role, prefix);
+}
+
+bool PocoSQLRoleInGatewayDao::parseSingle(Row &result,
+		RoleInGateway &role, const string &prefix)
+{
+	if (hasColumn(result, prefix + "id"))
+		role.setId(RoleInGatewayID::parse(result[prefix + "id"]));
+
+	role.setLevel(AccessLevel(result[prefix + "level"].convert<unsigned int>()));
+	role.setCreated(Timestamp::fromEpochTime(result[prefix + "created"]));
+
+	Gateway gateway(GatewayID::parse(result[prefix + "gateway_id"]));
+	role.setGateway(gateway);
+
+	Identity identity;
+	if (!PocoSQLIdentityDao::parseIfIDNotNull(result, identity, prefix + "identity_"))
+		throw IllegalStateException("identity is incomplete in query request");
+
+	role.setIdentity(identity);
+
+	markLoaded(role);
+	return true;
+}
---
+++ server/src/dao/poco/PocoSQLRoleInGatewayDao.h
@@ -0,0 +1,70 @@
+#include "dao/RoleInGatewayDao.h"
+#include "dao/SQLQuery.h"
+#include "dao/poco/PocoAbstractDao.h"
+#include "Debug.h"
+
+namespace Poco {
+namespace Data {
+
+class RecordSet;
+class Row;
+
+}
+}
+
+namespace BeeeOn {
+
+class PocoSQLRoleInGatewayDao :
+		public PocoAbstractDao,
+		public RoleInGatewayDao {
+public:
+	PocoSQLRoleInGatewayDao();
+
+	void create(RoleInGateway &role) override;
+	void fetchBy(std::vector<RoleInGateway> &roles,
+			const Gateway &gateway) override;
+	bool remove(const RoleInGateway &role) override;
+
+	bool isRegistered(const Gateway &gateway) override;
+
+	AccessLevel fetchAccessLevel(
+			const Gateway &gateway,
+			const User &user) override;
+
+	void fetchAccessibleGateways(
+			std::vector<Gateway> &list,
+			const User &user,
+			const AccessLevel &atLeast = AccessLevel::any()) override;
+
+	static bool parseSingle(Poco::Data::RecordSet &result,
+			RoleInGateway &role, const std::string &prefix = "");
+	static bool parseSingle(Poco::Data::Row &result,
+			RoleInGateway &role, const std::string &prefix = "");
+
+	template <typename C>
+	static void parseMany(Poco::Data::RecordSet &result, C &collection)
+	{
+		for (auto row : result) {
+			RoleInGateway role;
+
+			if (!parseSingle(row, role)) {
+				LOGGER_FUNC(__func__)
+					.warning("skipping malformed data, query result: "
+						+ row.valuesToString(), __FILE__, __LINE__);
+				continue;
+			}
+
+			collection.push_back(role);
+		}
+	}
+
+private:
+	SQLQuery m_queryCreate           {"roles_in_gateway.create"};
+	SQLQuery m_queryRemove           {"roles_in_gateway.remove"};
+	SQLQuery m_queryIsRegistered     {"roles_in_gateway.is.registered"};
+	SQLQuery m_queryFetchByGatewayId   {"roles_in_gateway.fetch.by.gateway_id"};
+	SQLQuery m_queryFetchAccessLevel {"roles_in_gateway.fetch.access_level"};
+	SQLQuery m_queryFetchAccessibleGateways {"roles_in_gateway.fetch.accessible.gateways"};
+};
+
+}
