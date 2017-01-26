BeeeOn/server

commit ad979a8094d7da8c6ba97e3bf394a5f2cc10e801

DeviceXmlHandler: new class
Implement operations on Device to be compatible with the previous
ui-server. Operations:

  * get - selectively retreive information about devices for a gateway
  * getall - get all available devices for a gateway
  * getnew - get all inactive (discovered) devices for a gateway
  * update - update information about device and activate it if inactive
  * unregister - unregister device (make it inactive) and notify
                 interested facilities (Ada server, Gateway)

Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

server/src/xmlui/DeviceXmlHandler.cpp
@@ -1,0 +1,209 @@
+#include <Poco/DOM/Node.h>
+#include <Poco/DOM/Element.h>
+#include <Poco/DOM/NodeList.h>
+
+#include "xmlui/DeviceXmlHandler.h"
+#include "xmlui/XmlDeviceDeserializer.h"
+#include "xmlui/Serializing.h"
+#include "model/Device.h"
+
+using namespace std;
+using namespace Poco;
+using namespace Poco::Net;
+using namespace Poco::XML;
+using namespace BeeeOn;
+using namespace BeeeOn::XmlUI;
+
+DeviceXmlHandler::DeviceXmlHandler(const StreamSocket &socket,
+		const AutoPtr<Document> input,
+		ExpirableSession::Ptr session,
+		DeviceService &deviceService,
+		PlaceService &placeService):
+	AbstractXmlHandler("devices", socket, input, session),
+	m_deviceService(deviceService),
+	m_placeService(placeService)
+{
+}
+
+void DeviceXmlHandler::handleInputImpl()
+{
+	if (!requireSession())
+		return;
+
+	Element *root = m_input->documentElement();
+	const string &type = root->getAttribute("type");
+	const string &gateid = root->getAttribute("gateid");
+
+	if (type == "getall") {
+		handleGetAll(gateid);
+		return;
+	}
+	else if (type == "getnew") {
+		handleGetNew(gateid);
+		return;
+	}
+
+	Element *deviceNode = root->getChildElement("device");
+	if (deviceNode == NULL) {
+		resultInvalidInput();
+		return;
+	}
+
+	if (type == "get")
+		handleGet(gateid, root->childNodes());
+	else if (type == "unregister")
+		handleUnregister(gateid, deviceNode);
+	else if (type == "update")
+		handleUpdate(gateid, deviceNode);
+	else
+		resultInvalidInput();
+}
+
+void DeviceXmlHandler::handleUnregister(const string &gateid,
+		Element *deviceNode)
+{
+	Gateway gateway(GatewayID::parse(gateid));
+	Device device(DeviceID::parse(deviceNode->getAttribute("euid")));
+	Relation<Device, Gateway> input(device, gateway);
+	User user(session()->userID());
+	input.setUser(user);
+
+	if (m_deviceService.unregister(input))
+		resultSuccess();
+	else
+		resultUnexpected();
+}
+
+void DeviceXmlHandler::handleUpdate(const string &gateid,
+		Element *deviceNode)
+{
+	Gateway gateway(GatewayID::parse(gateid));
+	Device device(DeviceID::parse(deviceNode->getAttribute("euid")));
+	XmlDeviceDeserializer deserializer(*deviceNode);
+	RelationWithData<Device, Gateway> input(
+			device, deserializer, gateway);
+	User user(session()->userID());
+	input.setUser(user);
+
+	if (m_deviceService.updateAndActivate(input))
+		resultSuccess();
+	else
+		resultUnexpected();
+}
+
+void DeviceXmlHandler::handleGet(const string &gateid,
+		NodeList *nodes)
+{
+	Gateway gateway(GatewayID::parse(gateid));
+	list<Device> devices;
+
+	if (nodes == NULL) {
+		resultInvalidInput();
+		return;
+	}
+
+	for (unsigned long i = 0; i < nodes->length(); ++i) {
+		const Node *node = nodes->item(i);
+
+		if (node->nodeType() != Node::ELEMENT_NODE)
+			continue;
+
+		if (node->localName() != "device")
+			continue;
+
+		const Element *e = dynamic_cast<const Element *>(node);
+
+		Device device(DeviceID::parse(e->getAttribute("euid")));
+		devices.push_back(device);
+	}
+
+	Relation<list<Device>, Gateway> input(devices, gateway);
+	User user(session()->userID());
+	input.setUser(user);
+
+	m_deviceService.fetchMany(input);
+
+	resultDataStart();
+	serialize(m_output, devices);
+	resultDataEnd();
+}
+
+void DeviceXmlHandler::handleGetAll(const string &gateid)
+{
+	Gateway gateway(GatewayID::parse(gateid));
+	vector<Device> devices;
+	Relation<vector<Device>, Gateway> input(devices, gateway);
+	User user(session()->userID());
+	input.setUser(user);
+
+	m_deviceService.fetchActiveBy(input);
+
+	resultDataStart();
+	serialize(m_output, devices);
+	resultDataEnd();
+}
+
+void DeviceXmlHandler::handleGetNew(const string &gateid)
+{
+	Gateway gateway(GatewayID::parse(gateid));
+	vector<Device> devices;
+	Relation<vector<Device>, Gateway> input(devices, gateway);
+	User user(session()->userID());
+	input.setUser(user);
+
+	m_deviceService.fetchInactiveBy(input);
+
+	resultDataStart();
+	serialize(m_output, devices);
+	resultDataEnd();
+}
+
+DeviceXmlHandlerResolver::DeviceXmlHandlerResolver():
+	AbstractXmlHandlerResolver("devices")
+{
+	injector<DeviceXmlHandlerResolver, DeviceService>(
+			"deviceService",
+			&DeviceXmlHandlerResolver::setDeviceService);
+	injector<DeviceXmlHandlerResolver, PlaceService>(
+			"placeService",
+			&DeviceXmlHandlerResolver::setPlaceService);
+	injector<DeviceXmlHandlerResolver, SessionManager>(
+			"sessionManager",
+			&DeviceXmlHandlerResolver::setSessionManager);
+}
+
+bool DeviceXmlHandlerResolver::canHandle(
+		const Element &root)
+{
+	if (!AbstractXmlHandlerResolver::canHandle(root))
+		return false;
+
+	const string &type = root.getAttribute("type");
+
+	if (type == "get")
+		return true;
+	if (type == "getall")
+		return true;
+	if (type == "getnew")
+		return true;
+	if (type == "update")
+		return true;
+	if (type == "unregister")
+		return true;
+
+	return false;
+}
+
+XmlRequestHandler *DeviceXmlHandlerResolver::createHandler(
+		const StreamSocket &socket,
+		const AutoPtr<Document> input)
+{
+	ExpirableSession::Ptr session = lookupSession(
+			*m_sessionManager, input);
+	return new DeviceXmlHandler(
+			socket, input, session,
+			*m_deviceService, *m_placeService);
+}
+
+BEEEON_OBJECT(DeviceXmlHandlerResolver,
+		BeeeOn::XmlUI::DeviceXmlHandlerResolver)

server/src/xmlui/DeviceXmlHandler.h
@@ -1,0 +1,67 @@
+#ifndef BEEEON_XMLUI_DEVICE_XML_HANDLER_H
+#define BEEEON_XMLUI_DEVICE_XML_HANDLER_H
+
+#include "service/DeviceService.h"
+#include "service/PlaceService.h"
+#include "xmlui/AbstractXmlHandler.h"
+
+namespace BeeeOn {
+namespace XmlUI {
+
+class DeviceXmlHandler : public AbstractXmlHandler {
+public:
+	DeviceXmlHandler(const Poco::Net::StreamSocket &socket,
+			const Poco::AutoPtr<Poco::XML::Document> input,
+			BeeeOn::ExpirableSession::Ptr session,
+			BeeeOn::DeviceService &deviceService,
+			BeeeOn::PlaceService &placeService);
+	void handleInputImpl() override;
+
+	void handleUnregister(const std::string &gateid,
+			Poco::XML::Element *deviceNode);
+	void handleUpdate(const std::string &gateid,
+			Poco::XML::Element *deviceNode);
+	void handleGet(const std::string &gateid,
+			Poco::XML::NodeList *deviceNode);
+	void handleGetAll(const std::string &gateid);
+	void handleGetNew(const std::string &gateid);
+
+private:
+	DeviceService &m_deviceService;
+	PlaceService &m_placeService;
+};
+
+class DeviceXmlHandlerResolver : public AbstractXmlHandlerResolver {
+public:
+	DeviceXmlHandlerResolver();
+
+	bool canHandle(const Poco::XML::Element &root);
+	XmlRequestHandler *createHandler(
+			const Poco::Net::StreamSocket &socket,
+			const Poco::AutoPtr<Poco::XML::Document> input);
+
+	void setPlaceService(BeeeOn::PlaceService *service)
+	{
+		m_placeService = service;
+	}
+
+	void setDeviceService(BeeeOn::DeviceService *service)
+	{
+		m_deviceService = service;
+	}
+
+	void setSessionManager(BeeeOn::SessionManager *manager)
+	{
+		m_sessionManager = manager;
+	}
+
+private:
+	BeeeOn::PlaceService *m_placeService;
+	BeeeOn::DeviceService *m_deviceService;
+	BeeeOn::SessionManager *m_sessionManager;
+};
+
+}
+}
+
+#endif

server/conf/services.xml
@@ -35,6 +35,7 @@
 				class="BeeeOn::XmlRequestHandlerFactory">
 			<add name="resolvers" ref="authXmlHandler" />
 			<add name="resolvers" ref="gatewayXmlHandler" />
+			<add name="resolvers" ref="deviceXmlHandler" />
 			<add name="resolvers" ref="locationXmlHandler" />
 			<add name="resolvers" ref="profileXmlHandler" />
 			<add name="resolvers" ref="anyXmlHandler" />
@@ -58,6 +59,12 @@
 		<instance name="locationXmlHandler"
 				class="BeeeOn::XmlUI::LocationXmlHandlerResolver">
 			<set name="locationService" ref="locationService" />
+			<set name="sessionManager" ref="sessionManager" />
+		</instance>
+
+		<instance name="deviceXmlHandler"
+				class="BeeeOn::XmlUI::DeviceXmlHandlerResolver">
+			<set name="deviceService" ref="deviceService" />
 			<set name="sessionManager" ref="sessionManager" />
 		</instance>
 

server/src/CMakeLists.txt
@@ -114,6 +114,7 @@ add_executable(xmlui-server
 	xmlui/GatewayXmlHandler.cpp
 	xmlui/LocationXmlHandler.cpp
 	xmlui/ProfileXmlHandler.cpp
+	xmlui/DeviceXmlHandler.cpp
 	ui/UIMockInit.cpp
 	dao/PocoSQLiteConnectorLoader.cpp
 )

commit 2c6db96dcff2e0fcd607b7f7a39af2ad30c30627

DeviceXmlHandler: do not reference PlaceService
There is no reason to reference PlaceService in DeviceXmlHandler.
It seems like to be a copy & paste bug.

Fixes: ad979a8094d7 ("DeviceXmlHandler: new class")
Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

server/src/xmlui/DeviceXmlHandler.cpp
@@ -17,11 +17,9 @@ using namespace BeeeOn::XmlUI;
 DeviceXmlHandler::DeviceXmlHandler(const StreamSocket &socket,
 		const AutoPtr<Document> input,
 		ExpirableSession::Ptr session,
-		DeviceService &deviceService,
-		PlaceService &placeService):
+		DeviceService &deviceService):
 	AbstractXmlHandler("devices", socket, input, session),
-	m_deviceService(deviceService),
-	m_placeService(placeService)
+	m_deviceService(deviceService)
 {
 }
 
@@ -168,9 +166,6 @@ DeviceXmlHandlerResolver::DeviceXmlHandlerResolver():
 	injector<DeviceXmlHandlerResolver, DeviceService>(
 			"deviceService",
 			&DeviceXmlHandlerResolver::setDeviceService);
-	injector<DeviceXmlHandlerResolver, PlaceService>(
-			"placeService",
-			&DeviceXmlHandlerResolver::setPlaceService);
 	injector<DeviceXmlHandlerResolver, SessionManager>(
 			"sessionManager",
 			&DeviceXmlHandlerResolver::setSessionManager);
@@ -205,8 +200,7 @@ XmlRequestHandler *DeviceXmlHandlerResolver::createHandler(
 	ExpirableSession::Ptr session = lookupSession(
 			*m_sessionManager, input);
 	return new DeviceXmlHandler(
-			socket, input, session,
-			*m_deviceService, *m_placeService);
+			socket, input, session, *m_deviceService);
 }
 
 BEEEON_OBJECT(DeviceXmlHandlerResolver,

server/src/xmlui/DeviceXmlHandler.h
@@ -2,7 +2,6 @@
 #define BEEEON_XMLUI_DEVICE_XML_HANDLER_H
 
 #include "service/DeviceService.h"
-#include "service/PlaceService.h"
 #include "xmlui/AbstractXmlHandler.h"
 
 namespace BeeeOn {
@@ -13,8 +12,7 @@ public:
 	DeviceXmlHandler(const Poco::Net::StreamSocket &socket,
 			const Poco::AutoPtr<Poco::XML::Document> input,
 			BeeeOn::ExpirableSession::Ptr session,
-			BeeeOn::DeviceService &deviceService,
-			BeeeOn::PlaceService &placeService);
+			BeeeOn::DeviceService &deviceService);
 	void handleInputImpl() override;
 
 	void handleUnregister(const std::string &gateid,
@@ -27,7 +25,6 @@ public:
 
 private:
 	DeviceService &m_deviceService;
-	PlaceService &m_placeService;
 };
 
 class DeviceXmlHandlerResolver : public AbstractXmlHandlerResolver {
@@ -38,11 +35,6 @@ public:
 	XmlRequestHandler *createHandler(
 			const Poco::Net::StreamSocket &socket,
 			const Poco::AutoPtr<Poco::XML::Document> input);
-
-	void setPlaceService(BeeeOn::PlaceService *service)
-	{
-		m_placeService = service;
-	}
 
 	void setDeviceService(BeeeOn::DeviceService *service)
 	{
@@ -55,7 +47,6 @@ public:
 	}
 
 private:
-	BeeeOn::PlaceService *m_placeService;
 	BeeeOn::DeviceService *m_deviceService;
 	BeeeOn::SessionManager *m_sessionManager;
 };