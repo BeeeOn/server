BeeeOn/server

commit 4830372f99a64a272eb37b9a1e97c718ef40be87

RoleInGatewayDao: implement update()

server/src/dao/poco/PocoSQLRoleInGatewayDao.cpp
@@ -22,6 +22,7 @@ BEEEON_OBJECT(PocoSQLRoleInGatewayDao, BeeeOn::PocoSQLRoleInGatewayDao)
 PocoSQLRoleInGatewayDao::PocoSQLRoleInGatewayDao()
 {
 	registerQuery(m_queryCreate);
+	registerQuery(m_queryUpdate);
 	registerQuery(m_queryRemove);
 	registerQuery(m_queryRemoveUser);
 	registerQuery(m_queryRemoveAll);
@@ -55,6 +56,21 @@ void PocoSQLRoleInGatewayDao::create(RoleInGateway &role)
 	);
 
 	execute(sql);
+}
+
+bool PocoSQLRoleInGatewayDao::update(RoleInGateway &role)
+{
+	assureHasId(role);
+
+	string id(role.id().toString());
+	unsigned int level = role.level();
+
+	Statement sql = (session() << m_queryCreate(),
+		use(level, "level"),
+		use(id, "id")
+	);
+
+	return execute(sql) > 0;
 }
 
 bool PocoSQLRoleInGatewayDao::fetch(RoleInGateway &role)

server/schema/home8-query.xml
@@ -999,6 +999,32 @@
 		</test>
 	</query>
 
+	<query id="roles_in_gateway.update">
+		<define>
+		UPDATE roles_in_gateway SET
+			level = <value name="level" type="integer" />
+		WHERE
+			id = <value name="id" type="uuid" />
+		</define>
+		<test name="simple">
+			<check name="jack.sparrow@example.org is now admin for Test Gateway 1">
+				<call-query>
+					<arg name="level">10</arg>
+					<arg name="id">'aee235a1-ac89-4553-b185-d689faeeeee3'</arg>
+				</call-query>
+				<sql>SELECT level FROM roles_in_gateway WHERE id = 'aee235a1-ac89-4553-b185-d689faeeeee3'</sql>
+				<expect>
+					<row>
+						<value name="level">10</value>
+					</row>
+				</expect>
+			</check>
+			<teardown name="revert jack.sparrow@example.org to be guest">
+				<sql>UPDATE roles_in_gateway SET level = 30 WHERE id = 'aee235a1-ac89-4553-b185-d689faeeeee3'</sql>
+			</teardown>
+		</test>
+	</query>
+
 	<query id="roles_in_gateway.fetch.by.id">
 		<define>
 		SELECT

server/src/dao/RoleInGatewayDao.cpp
@@ -16,6 +16,11 @@ void NullRoleInGatewayDao::create(RoleInGateway &role)
 	throw NotImplementedException(__func__);
 }
 
+bool NullRoleInGatewayDao::update(RoleInGateway &role)
+{
+	throw NotImplementedException(__func__);
+}
+
 bool NullRoleInGatewayDao::fetch(RoleInGateway &role)
 {
 	throw NotImplementedException(__func__);

server/src/dao/RoleInGatewayDao.h
@@ -15,6 +15,7 @@ class User;
 class RoleInGatewayDao {
 public:
 	virtual void create(RoleInGateway &role) = 0;
+	virtual bool update(RoleInGateway &role) = 0;
 	virtual bool fetch(RoleInGateway &role) = 0;
 	virtual void fetchBy(std::vector<RoleInGateway> &roles,
 			const Gateway &gateway) = 0;
@@ -50,6 +51,7 @@ public:
 	NullRoleInGatewayDao();
 
 	void create(RoleInGateway &role) override;
+	bool update(RoleInGateway &role) override;
 	bool fetch(RoleInGateway &role) override;
 	void fetchBy(std::vector<RoleInGateway> &roles,
 			const Gateway &gateway) override;

server/src/dao/poco/PocoSQLRoleInGatewayDao.h
@@ -21,6 +21,7 @@ public:
 	PocoSQLRoleInGatewayDao();
 
 	void create(RoleInGateway &role) override;
+	bool update(RoleInGateway &role) override;
 	bool fetch(RoleInGateway &role) override;
 	void fetchBy(std::vector<RoleInGateway> &roles,
 			const Gateway &gateway) override;
@@ -68,6 +69,7 @@ public:
 
 private:
 	SQLQuery m_queryCreate           {"roles_in_gateway.create"};
+	SQLQuery m_queryUpdate           {"roles_in_gateway.update"};
 	SQLQuery m_queryRemove           {"roles_in_gateway.remove"};
 	SQLQuery m_queryRemoveUser       {"roles_in_gateway.remove.user"};
 	SQLQuery m_queryRemoveAll        {"roles_in_gateway.remove.all"};

commit ae5703c66266027495afd5e51c214f9cc8e5676c

PocoSQLRoleInGatewayDao: fix to call m_queryUpdate
The update() operation must use m_queryUpdate.

Fixes: 4830372f99a6 ("RoleInGatewayDao: implement update()")
Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

server/src/dao/poco/PocoSQLRoleInGatewayDao.cpp
@@ -65,7 +65,7 @@ bool PocoSQLRoleInGatewayDao::update(RoleInGateway &role)
 	string id(role.id().toString());
 	unsigned int level = role.level();
 
-	Statement sql = (session() << m_queryCreate(),
+	Statement sql = (session() << m_queryUpdate(),
 		use(level, "level"),
 		use(id, "id")
 	);