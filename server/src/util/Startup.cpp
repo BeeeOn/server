BeeeOn/server

commit c790804741ce12f80d1b5c3aebab67556f932121

Startup: report version (from GIT_ID)
Report program group, name and version on start up. There is however
an issue when building out-of-tree which is not primarily supported
at the moment. In such case, we have no Git identitifaction so the
string "out of tree" is printed instead.

Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

server/src/util/Startup.cpp
@@ -270,6 +270,17 @@ static void warnAboutPocoVersion(Logger &logger)
 	}
 }
 
+static void reportVersion(const string &group,
+		const string &name, Logger &logger)
+{
+	const string app(group + "/" + name);
+#ifdef GIT_ID
+	logger.information(app + " " + string("version ") + GIT_ID, __FILE__, __LINE__);
+#else
+	logger.information(app + " out of tree", __FILE__, __LINE__);
+#endif
+}
+
 void ServerStartup::notifyStarted()
 {
 	if (m_notifyPid >= 0) {
@@ -285,6 +296,7 @@ void ServerStartup::notifyStarted()
 
 int ServerStartup::main(const vector<string> &args)
 {
+	reportVersion(m_appGroup, m_appName, logger());
 	logger().notice("Poco library " + pocoVersion() + " (headers " + pocoLinkedVersion() + ")");
 	warnAboutPocoVersion(logger());
 

commit 7a019aab75e813ecec5fbbd9d7d0eed460605947

Startup: report version as notice log level
The notice level is the default one for the Application logger.
The version should be reported as default.

Fixes: c790804741ce ("Startup: report version (from GIT_ID)")
Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

server/src/util/Startup.cpp
@@ -275,9 +275,9 @@ static void reportVersion(const string &group,
 {
 	const string app(group + "/" + name);
 #ifdef GIT_ID
-	logger.information(app + " " + string("version ") + GIT_ID, __FILE__, __LINE__);
+	logger.notice(app + " " + string("version ") + GIT_ID, __FILE__, __LINE__);
 #else
-	logger.information(app + " out of tree", __FILE__, __LINE__);
+	logger.notice(app + " out of tree", __FILE__, __LINE__);
 #endif
 }
 