BeeeOn/server

commit 5836aae5b5fed90b6027b74a0e5df5535d448c50

RoleXmlHandler: new class
Support the following XML-UI messages:

* ns="gateusers" type="invite"
* ns="gateusers" type="getall"
* ns="gateusers" type="remove"
* ns="gateusers" type="updateaccess"

Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

server/src/xmlui/RoleXmlHandler.cpp
@@ -1,0 +1,166 @@
+#include <vector>
+
+#include "model/Identity.h"
+#include "model/Gateway.h"
+#include "model/RoleInGateway.h"
+#include "server/AccessLevel.h"
+#include "server/SessionManager.h"
+#include "service/RoleService.h"
+#include "xmlui/Serializing.h"
+#include "xmlui/RoleXmlHandler.h"
+#include "xmlui/XmlRoleInGatewayDeserializer.h"
+
+using namespace std;
+using namespace Poco;
+using namespace Poco::Net;
+using namespace Poco::XML;
+using namespace BeeeOn;
+using namespace BeeeOn::XmlUI;
+
+RoleXmlHandler::RoleXmlHandler(
+		const StreamSocket &socket,
+		const AutoPtr<Document> input,
+		ExpirableSession::Ptr session,
+		RoleService &roleService):
+	AbstractXmlHandler("gateusers", socket, input, session),
+	m_roleService(roleService)
+{
+}
+
+void RoleXmlHandler::handleInputImpl()
+{
+	if (!requireSession())
+		return;
+
+	Element *root = m_input->documentElement();
+	const string &type = root->getAttribute("type");
+	const string &gateid = root->getAttribute("gateid");
+
+	if (type == "getall") {
+		handleGetAll(gateid);
+		return;
+	}
+
+	Element *userNode = root->getChildElement("user");
+	if (userNode == NULL) {
+		resultInvalidInput();
+		return;
+	}
+
+	if (type == "invite")
+		handleInvite(gateid, userNode);
+	else if (type == "remove")
+		handleRemove(gateid, userNode);
+	else if (type == "updateaccess")
+		handleUpdateAccess(gateid, userNode);
+	else
+		resultInvalidInput();
+}
+
+void RoleXmlHandler::handleInvite(const string &gateid, Element *userNode)
+{
+	Gateway gateway(GatewayID::parse(gateid));
+
+	const string email(userNode->getAttribute("email"));
+	const string perm(userNode->getAttribute("permission"));
+
+	Identity identity;
+	identity.setEmail(email);
+	AccessLevel as(AccessLevel::parse(perm));
+
+	Relation<Identity, Gateway> input(identity, gateway);
+	User user(session()->userID());
+	input.setUser(user);
+
+	m_roleService.inviteIdentity(input, as);
+	resultSuccess();
+}
+
+void RoleXmlHandler::handleGetAll(const string &gateid)
+{
+	Gateway gateway(GatewayID::parse(gateid));
+	vector<RoleInGateway> roles;
+
+	Relation<vector<RoleInGateway>, Gateway> input(roles, gateway);
+	User user(session()->userID());
+	input.setUser(user);
+
+	m_roleService.list(input);
+
+	resultDataStart();
+	serialize(m_output, roles);
+	resultDataEnd();
+}
+
+void RoleXmlHandler::handleRemove(const string &gateid, Element *userNode)
+{
+	Gateway gateway(GatewayID::parse(gateid));
+	RoleInGateway role(RoleInGatewayID::parse(userNode->getAttribute("id")));
+
+	Single<RoleInGateway> input(role);
+	User user(session()->userID());
+	input.setUser(user);
+
+	m_roleService.remove(input);
+	resultSuccess();
+}
+
+void RoleXmlHandler::handleUpdateAccess(const string &gateid, Element *userNode)
+{
+	GatewayID::parse(gateid); // not needed, only check validity
+
+	RoleInGateway role(RoleInGatewayID::parse(userNode->getAttribute("id")));
+	XmlRoleInGatewayDeserializer deserializer(*userNode);
+
+	SingleWithData<RoleInGateway> input(role, deserializer);
+	User user(session()->userID());
+	input.setUser(user);
+
+	m_roleService.update(input);
+	resultSuccess();
+}
+
+RoleXmlHandlerResolver::RoleXmlHandlerResolver():
+	AbstractXmlHandlerResolver("gateusers")
+{
+	injector<RoleXmlHandlerResolver, RoleService>(
+		"roleService",
+		&RoleXmlHandlerResolver::setRoleService
+	);
+	injector<RoleXmlHandlerResolver, SessionManager>(
+		"sessionManager",
+		&RoleXmlHandlerResolver::setSessionManager
+	);
+}
+
+bool RoleXmlHandlerResolver::canHandle(const Element &root)
+{
+	if (!AbstractXmlHandlerResolver::canHandle(root))
+		return false;
+
+	const string &type = root.getAttribute("type");
+
+	if (type == "invite")
+		return true;
+	if (type == "getall")
+		return true;
+	if (type == "remove")
+		return true;
+	if (type == "updateaccess")
+		return true;
+
+	return false;
+}
+
+XmlRequestHandler *RoleXmlHandlerResolver::createHandler(
+		const StreamSocket &socket,
+		const AutoPtr<Document> input)
+{
+	ExpirableSession::Ptr session = lookupSession(
+			*m_sessionManager, input);
+	return new RoleXmlHandler(socket, input,
+			session, *m_roleService);
+}
+
+BEEEON_OBJECT(RoleXmlHandlerResolver,
+		BeeeOn::XmlUI::RoleXmlHandlerResolver)

server/conf/services.xml
@@ -23,6 +23,7 @@
 			<add name="resolvers" ref="deviceXmlHandler" />
 			<add name="resolvers" ref="locationXmlHandler" />
 			<add name="resolvers" ref="profileXmlHandler" />
+			<add name="resolvers" ref="roleXmlHandler" />
 			<add name="resolvers" ref="anyXmlHandler" />
 		</instance>
 
@@ -55,6 +56,12 @@
 		<instance name="profileXmlHandler"
 				class="BeeeOn::XmlUI::ProfileXmlHandlerResolver">
 			<set name="identityService" ref="identityService" />
+			<set name="sessionManager" ref="sessionManager" />
+		</instance>
+
+		<instance name="roleXmlHandler"
+				class="BeeeOn::XmlUI::RoleXmlHandlerResolver">
+			<set name="roleService" ref="roleService" />
 			<set name="sessionManager" ref="sessionManager" />
 		</instance>
 

server/src/CMakeLists.txt
@@ -136,6 +136,7 @@ add_executable(xmlui-server
 	xmlui/LocationXmlHandler.cpp
 	xmlui/ProfileXmlHandler.cpp
 	xmlui/DeviceXmlHandler.cpp
+	xmlui/RoleXmlHandler.cpp
 	ui/UIMockInit.cpp
 	dao/poco/PocoSQLiteConnectorLoader.cpp
 )

server/src/xmlui/RoleXmlHandler.h
@@ -1,0 +1,62 @@
+#ifndef BEEEON_XMLUI_ROLE_XML_HANDLER_H
+#define BEEEON_XMLUI_ROLE_XML_HANDLER_H
+
+#include "xmlui/AbstractXmlHandler.h"
+
+namespace BeeeOn {
+
+class RoleService;
+class SessionManager;
+
+namespace XmlUI {
+
+class RoleXmlHandler : public AbstractXmlHandler {
+public:
+	RoleXmlHandler(const Poco::Net::StreamSocket &socket,
+			const Poco::AutoPtr<Poco::XML::Document> input,
+			ExpirableSession::Ptr session,
+			RoleService &roleService);
+
+	void handleInputImpl() override;
+
+protected:
+	void handleInvite(const std::string &gateid,
+			Poco::XML::Element *userNode);
+	void handleGetAll(const std::string &gateid);
+	void handleRemove(const std::string &gateid,
+			Poco::XML::Element *userNode);
+	void handleUpdateAccess(const std::string &gateid,
+			Poco::XML::Element *userNode);
+
+private:
+	RoleService &m_roleService;
+};
+
+class RoleXmlHandlerResolver : public AbstractXmlHandlerResolver {
+public:
+	RoleXmlHandlerResolver();
+
+	bool canHandle(const Poco::XML::Element &root);
+	XmlRequestHandler *createHandler(
+			const Poco::Net::StreamSocket &socket,
+			const Poco::AutoPtr<Poco::XML::Document> input);
+
+	void setRoleService(RoleService *service)
+	{
+		m_roleService = service;
+	}
+
+	void setSessionManager(BeeeOn::SessionManager *manager)
+	{
+		m_sessionManager = manager;
+	}
+
+private:
+	RoleService *m_roleService;
+	SessionManager *m_sessionManager;
+};
+
+}
+}
+
+#endif

commit 5b404e6506ca3c00f6d17a4f7d86e88888a823d3

RoleXmlHandler: output LegacyRoleInGateway from getall request
The XML-UI API expects to include "name", "surname" and "imgurl".
Also, the "permission" field must contain "owner" for the role
that owns the gateway. This is accomplished by utilizing the
LegacyRoleInGateway and an appropriate serializing method.

As explained in commit:

81ce58405a0a ("query: implement legacy_roles_in_gateway.fetch.by.gateway_id")

the results are longer then expected when a user has more
verified identities for a single identity. This occures in
tests so the tests must be fixed appropriately.

Fixes: 5836aae5b5fe ("RoleXmlHandler: new class")
Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

server/src/xmlui/RoleXmlHandler.cpp
@@ -3,6 +3,7 @@
 #include "model/Identity.h"
 #include "model/Gateway.h"
 #include "model/RoleInGateway.h"
+#include "model/LegacyRoleInGateway.h"
 #include "server/AccessLevel.h"
 #include "server/SessionManager.h"
 #include "service/RoleService.h"
@@ -79,9 +80,9 @@ void RoleXmlHandler::handleInvite(const string &gateid, Element *userNode)
 void RoleXmlHandler::handleGetAll(const string &gateid)
 {
 	Gateway gateway(GatewayID::parse(gateid));
-	vector<RoleInGateway> roles;
+	vector<LegacyRoleInGateway> roles;
 
-	Relation<vector<RoleInGateway>, Gateway> input(roles, gateway);
+	Relation<vector<LegacyRoleInGateway>, Gateway> input(roles, gateway);
 	User user(session()->userID());
 	input.setUser(user);
 

server/t/xmlui/t1005-role-invite-remove.py
@@ -97,11 +97,16 @@ class TestRoleInviteRemove(unittest.TestCase):
 		))
 
 		self.assertTrue(response.is_data())
-		self.assertEqual(2, len(response.root))
+		self.assertEqual(4, len(response.root))
 		self.assertEqual("joe.doe@example.org", response.root[0].get("email"))
-		self.assertEqual("john.smith@example.org", response.root[1].get("email"))
+		self.assertEqual("joe.doe@example.org", response.root[1].get("email"))
+		self.assertEqual("john.smith@example.org", response.root[2].get("email"))
+		self.assertEqual("john.smith@example.org", response.root[3].get("email"))
 
-		johnRole = response.root[1].get("id")
+		self.assertEqual(response.root[0].get("id"), response.root[1].get("id"))
+		self.assertEqual(response.root[2].get("id"), response.root[3].get("id"))
+
+		johnRole = response.root[2].get("id")
 
 		response = self.conn.request(RoleRemove(
 			config.tmp_gateway_id,
@@ -122,7 +127,7 @@ class TestRoleInviteRemove(unittest.TestCase):
 		))
 
 		self.assertTrue(response.is_data())
-		self.assertEqual(1, len(response.root))
+		self.assertEqual(2, len(response.root))
 		self.assertEqual("joe.doe@example.org", response.root[0].get("email"))
 
 		response = self.conn.request(RoleRemove(
@@ -144,7 +149,7 @@ class TestRoleInviteRemove(unittest.TestCase):
 		))
 
 		self.assertTrue(response.is_data())
-		self.assertEqual(1, len(response.root))
+		self.assertEqual(2, len(response.root))
 		self.assertEqual("joe.doe@example.org", response.root[0].get("email"))
 
 		time.sleep(1) # wait to prevent db constraint violation