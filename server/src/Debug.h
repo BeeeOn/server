BeeeOn/server

commit c74d26a73c0171fd286209dd83b2c04ae0da556e

ui_server: add backtrace reporting functions
The glibc provides a way how to access the backtrace of function
calls. It can be useful for debugging of certain bugs or finding
sources of exceptions.

Example (wrapped, shortened):

build/ui-server(_ZN6BeeeOn16stdout_backtraceEv+0x1f)[0x43048d]
build/ui-server(_ZN6BeeeOn19TRestRequestHandlerINS_15MongooseR
		equestENS_16MongooseResponseEPvE13handleReque
		stERS1_RS2_+0x472)[0x43537c]
build/ui-server(_ZN6BeeeOn15TMongooseServerIPvE6handleEP13mg_c
		onnectionP12http_message+0x1c7)[0x4344d5]

[skipped many lines...]

/usr/lib/libPocoUtil.so.31(_ZN4Poco4Util11Application3runEv+0x1
		f)[0x7f117971712f]
build/ui-server(main+0x5a)[0x42ed48]
/usr/lib/libc.so.6(__libc_start_main+0xf1)[0x7f1178543741]
build/ui-server(_start+0x29)[0x42ec09]

Reviewed-by: Petr Stehlík <xstehl14@stud.fit.vutbr.cz>

server/src/Debug.h
@@ -1,7 +1,7 @@
 #ifndef BEEEON_DEBUG_H
 #define BEEEON_DEBUG_H
 
-#ifdef defined(__clang__) || defined(__GLIBCXX__)
+#if defined(__clang__) || defined(__GLIBCXX__)
 #include <cxxabi.h> // abi::__cxa_demangle
 #endif
 
@@ -160,5 +160,80 @@ private:
 #define _TRACE_FUNC(logger) _INTERNAL_TRACE_FUNC(logger)
 
 }
+
+#ifdef __GLIBC__
+#include <execinfo.h>
+#include <unistd.h>
+#include <cstdio>
+
+namespace BeeeOn {
+
+/**
+ * Print backtrace of the current function call hierarchy
+ * to stdout. Useful for very unexpected states and exceptions
+ * and when even the logging facilities fail.
+ */
+inline void stdout_backtrace()
+{
+	void *addr[128];
+	size_t count = backtrace(addr, 128);
+
+	backtrace_symbols_fd(addr, count, STDOUT_FILENO);
+}
+
+/**
+ * Log backtrace of the current function call hierarchy
+ * to stdout. Useful for very unexpected states and exceptions.
+ * All lines are logged as critical and with prefix "Backtrace: ".
+ */
+inline void __log_backtrace(Poco::Logger &l, const char *file, size_t line)
+{
+	void *addr[128];
+	size_t count = backtrace(addr, 128);
+	char **strings = backtrace_symbols(addr, count);
+	size_t i;
+
+	l.critical(
+		Poco::Logger::format("Backtrace: size $0", std::to_string(count)),
+		file, line);
+
+	for (i = 0; i < count; ++i) {
+		l.critical(Poco::Logger::format(
+				"Backtrace: > $0", std::string(strings[i])),
+			file, line);
+	}
+}
+
+}
+
+#else // unknown libc/c++ library
+
+namespace BeeeOn {
+
+/**
+ * No idea how to support backtracing for an unknown libc/c++ library.
+ */
+inline void stdout_backtrace()
+{
+	const char *msg = "no backtrace available\n";
+	std::write(fd, msg, sizeof(msg));
+}
+
+/**
+ * No idea how to support backtracing for an unknown libc/c++ library.
+ */
+inline void __log_backtrace(Poco::Logger &l, const char *file, size_t line)
+{
+	l.critical("Backtrace: not available", file, line);
+}
+
+}
+
+#endif
+
+/**
+ * Log backtrace to the given logger with level critical.
+ */
+#define log_backtrace(l) BeeeOn::__log_backtrace(l, __FILE__, __LINE__)
 
 #endif

commit 3ec6b1957cbd2127fea70eba0aa21044b2424f65

Debug: fix memory leak in __log_backtrace()
The backtrace_symbols returns malloc'd memory. Thus, it is
necessary to call free on it.

Fixes: c74d26a73c01 ("ui_server: add backtrace reporting functions")
Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

server/src/Debug.h
@@ -203,6 +203,8 @@ inline void __log_backtrace(Poco::Logger &l, const char *file, size_t line)
 				"Backtrace: > $0", std::string(strings[i])),
 			file, line);
 	}
+
+	free(strings);
 }
 
 }