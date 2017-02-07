BeeeOn/server

commit ad159202f5f019b23d6051d86915798381df06c1

xmlui: utilize SSLServer for SecureServerSocket configuration
The SSL configuration for the xmlui is now defined in the services.xml
file. User configuration stays in place in server.ini without any change.
The xmluiSSLServer name is loaded by xmlui main when configured to be
secure.

Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

server/src/server/SocketServer.cpp
@@ -26,9 +26,10 @@ SocketServer *SocketServer::createDefault(
 
 SocketServer *SocketServer::createSecure(
 	TCPServerConnectionFactory::Ptr factory,
+	Context::Ptr context,
 	UInt16 port)
 {
-	SecureServerSocket socket(port);
+	SecureServerSocket socket(port, 64, context);
 	TCPServerParams::Ptr params = new TCPServerParams();
 	return new SocketServer(factory, socket, params);
 }

server/src/server/SocketServer.h
@@ -6,6 +6,7 @@
 #include <Poco/Net/TCPServerParams.h>
 #include <Poco/Net/TCPServerConnection.h>
 #include <Poco/Net/TCPServerConnectionFactory.h>
+#include <Poco/Net/Context.h>
 
 #include "server/Server.h"
 
@@ -26,6 +27,7 @@ public:
 
 	static SocketServer *createSecure(
 		Poco::Net::TCPServerConnectionFactory::Ptr factory,
+		Poco::Net::Context::Ptr context,
 		Poco::UInt16 port = 443);
 
 private:

server/conf/services.xml
@@ -257,5 +257,15 @@
 			<set name="daoManager" ref="pocoDaoManager" />
 		</instance>
 
+		<!-- SSL configurations -->
+
+		<instance name="xmluiSSLServer" class="BeeeOn::SSLServer">
+			<set name="certificate" text="${xmlui.ssl.certificate}" />
+			<set name="privateKey" text="${xmlui.ssl.key}" />
+			<set name="authority" text="${xmlui.ssl.authority}" />
+			<set name="verificationMode" text="${xmlui.ssl.verify_level}" />
+			<set name="loadDefaultCA" text="false" />
+		</instance>
+
 	</services>
 </system>

server/src/util/NoKeyPassphraseHandler.h
@@ -1,24 +1,0 @@
-#ifndef BEEEON_NO_KEY_PASSPHRASE_HANDLER_H
-#define BEEEON_NO_KEY_PASSPHRASE_HANDLER_H
-
-#include <Poco/Net/PrivateKeyPassphraseHandler.h>
-
-namespace BeeeOn {
-
-class NoKeyPassphraseHandler :
-		public Poco::Net::PrivateKeyPassphraseHandler {
-public:
-	NoKeyPassphraseHandler(bool serverSide = true):
-		Poco::Net::PrivateKeyPassphraseHandler(serverSide)
-	{
-	}
-
-	void onPrivateKeyRequested(const void *sender, std::string &key)
-	{
-		key = "";
-	}
-};
-
-}
-
-#endif

server/src/xmlui/main.cpp
@@ -16,7 +16,7 @@
 #include "di/DependencyInjector.h"
 #include "server/SocketServer.h"
 #include "server/XmlRequestHandler.h"
-#include "util/NoKeyPassphraseHandler.h"
+#include "util/SSLServer.h"
 
 using namespace std;
 using namespace Poco;
@@ -204,48 +204,21 @@ protected:
 		return EXIT_OK;
 	}
 
-	void initSSL()
+	Context::Ptr initSSL(DependencyInjector &injector)
 	{
-		const string &certificate = config()
-			.getString("xmlui.ssl.certificate");
-		const string &key = config()
-			.getString("xmlui.ssl.key");
-		const string &authority = config()
-			.getString("xmlui.ssl.authority");
-		int mode = config().getInt("xmlui.ssl.verify_level",
-					Context::VERIFY_STRICT);
-
-		Context::Ptr context = new Context(
-				Context::SERVER_USE,
-				key,
-				certificate,
-				authority,
-				(Context::VerificationMode) mode);
-
-		logger().debug("key: " + key, __FILE__, __LINE__);
-		logger().debug("certificate: " + certificate,
-				__FILE__, __LINE__);
-		logger().debug("authority: " + authority,
-				__FILE__, __LINE__);
-		logger().debug("verify_level: " + to_string(mode),
-				__FILE__, __LINE__);
-
-		SharedPtr<PrivateKeyPassphraseHandler> pkeyHandler =
-				new NoKeyPassphraseHandler();
-		SharedPtr<InvalidCertificateHandler> certHandler =
-				new RejectCertificateHandler(true);
-		SSLManager::instance().initializeServer(
-				pkeyHandler, certHandler, context);
-
-		logger().notice("SSL context initialized");
+		SSLServer *sslServer = injector
+				.create<SSLServer>("xmluiSSLServer");
+		return sslServer->context();
 	}
 
-	SocketServer *createSocketServer(XmlRequestHandlerFactory *factory)
+	SocketServer *createSocketServer(
+			DependencyInjector &injector,
+			XmlRequestHandlerFactory *factory)
 	{
 		if (config().getBool("xmlui.ssl.enable", false)) {
-			initSSL();
+			Context::Ptr context = initSSL(injector);
 			return SocketServer::createSecure(factory,
-					m_serverPort);
+					context, m_serverPort);
 		}
 		else {
 			return SocketServer::createDefault(factory,
@@ -265,7 +238,7 @@ protected:
 		XmlRequestHandlerFactory *factory = injector
 			.create<XmlRequestHandlerFactory>("xmlui", true);
 
-		SocketServer *server = createSocketServer(factory);
+		SocketServer *server = createSocketServer(injector, factory);
 		server->start();
 
 		waitForTerminationRequest();

commit b54610f90089b161f63d8f1062e7e145c201f64b

SocketServer: provide setter of the server backlog
The backlog can now be set (e.g. from a configuration). Also, the backlog
value is unified for both secure and non-secure server socket. Before,
it was set to the secure server socket only just because there is no
other suitable constructor to pass the Context. Thus, the setup of both
server sockets is now equivalent.

The creation of TCPServer is extracted to its own method to make the
code shorter and better understandable.

Fixes: ad159202f5f0 ("xmlui: utilize SSLServer for SecureServerSocket configuration")
Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

server/src/server/SocketServer.cpp
@@ -14,6 +14,7 @@ using namespace BeeeOn;
 
 SocketServer::SocketServer():
 	m_port(0),
+	m_backlog(64),
 	m_sslConfig(NULL)
 {
 }
@@ -48,6 +49,16 @@ void SocketServer::setPort(int port)
 	m_port = (unsigned int) port;
 }
 
+void SocketServer::setBacklog(int backlog)
+{
+	if (backlog < 1) {
+		throw InvalidArgumentException(
+				"invalid backlog: " + to_string(backlog));
+	}
+
+	m_backlog = backlog;
+}
+
 void SocketServer::setFactory(TCPServerConnectionFactory::Ptr factory)
 {
 	m_factory = factory;
@@ -58,16 +69,21 @@ void SocketServer::setTCPParams(const TCPServerParams::Ptr params)
 	m_tcpParams = params;
 }
 
-void SocketServer::start()
+TCPServer *SocketServer::createServer()
 {
 	if (m_sslConfig == NULL) {
-		m_server = new TCPServer(m_factory,
-			ServerSocket(m_port), m_tcpParams);
+		ServerSocket socket(m_port, m_backlog);
+		return new TCPServer(m_factory, socket, m_tcpParams);
 	}
-	else {
-		m_server = new TCPServer(m_factory,
-			SecureServerSocket(m_port, 64, m_sslConfig->context()), m_tcpParams);
-	}
+
+	Context::Ptr context = m_sslConfig->context();
+	SecureServerSocket socket(m_port, m_backlog, context);
+	return new TCPServer(m_factory, socket, m_tcpParams);
+}
+
+void SocketServer::start()
+{
+	m_server = createServer();
 
 	LOGGER_CLASS(this).information(
 		"starting server on port "

server/src/server/SocketServer.h
@@ -21,6 +21,7 @@ public:
 
 	void setSSLConfig(SSLServer *config);
 	void setPort(int port);
+	void setBacklog(int backlog);
 	void setFactory(Poco::Net::TCPServerConnectionFactory::Ptr factory);
 	void setTCPParams(const Poco::Net::TCPServerParams::Ptr params);
 
@@ -32,8 +33,12 @@ public:
 		SSLServer *sslConfig,
 		Poco::UInt16 port);
 
+protected:
+	Poco::Net::TCPServer *createServer();
+
 private:
 	unsigned int m_port;
+	int m_backlog;
 	SSLServer *m_sslConfig;
 	Poco::Net::TCPServerConnectionFactory::Ptr m_factory;
 	Poco::Net::TCPServerParams::Ptr m_tcpParams;