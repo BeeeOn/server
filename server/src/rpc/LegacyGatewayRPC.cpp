7d6aaaa2315cf559afe22fa135202ebf1146ef9f
Jozef Halaj <xhalaj03@stud.fit.vutbr.cz>
2017-02-15 23:29:11 +0100

    LegacyGatewayRPC: new class
    Provide a GatewayRPC implementation of legacy UI-AdaServer API. Each
    call of the methods sendListen, unpairDevice and pingGateway create new
    connection to the AdaServer using SocketRPCConnector.
    
    Signed-off-by: Jozef Halaj <xhalaj03@stud.fit.vutbr.cz>
    Acked-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

---
+++ server/src/rpc/LegacyGatewayRPC.cpp
@@ -0,0 +1,178 @@
+#include <sstream>
+
+#include <Poco/Exception.h>
+#include <Poco/AutoPtr.h>
+#include <Poco/TeeStream.h>
+#include <Poco/StreamCopier.h>
+#include <Poco/SAX/AttributesImpl.h>
+#include <Poco/XML/XMLWriter.h>
+
+#include "di/Injectable.h"
+#include "rpc/LegacyGatewayRPC.h"
+#include "rpc/RPCConnector.h"
+
+using namespace std;
+using namespace Poco;
+using namespace Poco::XML;
+using namespace BeeeOn;
+
+LegacyGatewayRPC::LegacyGatewayRPC():
+	m_logStream(logger(), Poco::Message::PRIO_DEBUG)
+{
+}
+
+void LegacyGatewayRPC::sendListen(const Gateway &gateway)
+{
+	Mutex::ScopedLock guard(m_lock);
+	m_connector->open();
+
+	TeeOutputStream outStream(m_connector->send());
+	outStream.addStream(m_logStream);
+
+	XMLWriter xmlWriter(outStream, XMLWriter::CANONICAL_XML);
+	AttributesImpl attributes;
+
+	try {
+		xmlWriter.startDocument();
+		attributes.addAttribute("", "", "type", "", "listen");
+		xmlWriter.startElement("", "request", "", attributes);
+
+		attributes.clear();
+		attributes.addAttribute("", "", "id", "", gateway.id().toString());
+		xmlWriter.startElement("", "adapter", "", attributes);
+		xmlWriter.endElement("", "adapter", "");
+
+		xmlWriter.endElement("", "request", "");
+		xmlWriter.endDocument();
+
+		m_connector->flush();
+	}
+	catch (const Exception &e) {
+		m_logStream << endl;
+		m_connector->close();
+		e.rethrow();
+	}
+
+	m_logStream << endl;
+
+	stringstream response;
+	StreamCopier::copyStreamUnbuffered(m_connector->receive(), response);
+	m_connector->close();
+
+	parseResponse(response.str());
+}
+
+void LegacyGatewayRPC::unpairDevice(const Gateway &gateway, const Device &device)
+{
+	Mutex::ScopedLock guard(m_lock);
+	m_connector->open();
+
+	TeeOutputStream outStream(m_connector->send());
+	outStream.addStream(m_logStream);
+
+	XMLWriter xmlWriter(outStream, XMLWriter::CANONICAL_XML);
+	AttributesImpl attributes;
+
+	try {
+		xmlWriter.startDocument();
+		attributes.addAttribute("", "", "type", "", "delete");
+		xmlWriter.startElement("", "request", "", attributes);
+
+		attributes.clear();
+		attributes.addAttribute("", "", "id", "", device.id().toString());
+		attributes.addAttribute("", "", "onAdapter", "", gateway.id().toString());
+		xmlWriter.startElement("", "sensor", "", attributes);
+		xmlWriter.endElement("", "sensor", "");
+
+		xmlWriter.endElement("", "request", "");
+		xmlWriter.endDocument();
+
+		m_connector->flush();
+	}
+	catch (const Exception &e) {
+		m_logStream << endl;
+		m_connector->close();
+		e.rethrow();
+	}
+
+	m_logStream << endl;
+
+	stringstream response;
+	StreamCopier::copyStreamUnbuffered(m_connector->receive(), response);
+	m_connector->close();
+
+	parseResponse(response.str());
+}
+
+void LegacyGatewayRPC::pingGateway(const Gateway &gateway)
+{
+	Mutex::ScopedLock guard(m_lock);
+	m_connector->open();
+
+	TeeOutputStream outStream(m_connector->send());
+	outStream.addStream(m_logStream);
+
+	XMLWriter xmlWriter(outStream, XMLWriter::CANONICAL_XML);
+	AttributesImpl attributes;
+
+	try {
+		xmlWriter.startDocument();
+		attributes.addAttribute("", "", "type", "", "ping");
+		xmlWriter.startElement("", "request", "", attributes);
+
+		attributes.clear();
+		attributes.addAttribute("", "", "id", "", gateway.id().toString());
+		xmlWriter.startElement("", "adapter", "", attributes);
+		xmlWriter.endElement("", "adapter", "");
+
+		xmlWriter.endElement("", "request", "");
+		xmlWriter.endDocument();
+
+		m_connector->flush();
+	}
+	catch (const Exception &e) {
+		m_logStream << endl;
+		m_connector->close();
+		e.rethrow();
+	}
+
+	m_logStream << endl;
+
+	stringstream response;
+	StreamCopier::copyStreamUnbuffered(m_connector->receive(), response);
+	m_connector->close();
+
+	parseResponse(response.str());
+}
+
+void LegacyGatewayRPC::parseResponse(const string &response)
+{
+	if (response.length() == 0)
+		throw InvalidArgumentException("response is empty.");
+
+	if(logger().debug())
+		logger().debug(response);
+
+	AutoPtr<XML::Document> doc = m_xmlParser.parse(response);
+	string errorCode = doc->documentElement()->getAttribute("errorCode");
+
+	if (errorCode == "0")
+		return;
+	else if (errorCode == "1")
+		throw IllegalStateException("gateway was never connected to the server");
+	else if (errorCode == "2")
+		throw IllegalStateException("connection with gateway is broken");
+	else if (errorCode == "3")
+		throw IllegalStateException("error sending data to the gateway");
+	else if (errorCode == "4")
+		throw IllegalStateException("PING response has not arrived");
+	else if (errorCode == "5")
+		throw IllegalStateException("message is not supported by the gateway protocol");
+	else
+		throw IllegalStateException("illegal error code: " + errorCode);
+}
+
+BEEEON_OBJECT_BEGIN(BeeeOn, LegacyGatewayRPC)
+BEEEON_OBJECT_CASTABLE(GatewayRPC)
+BEEEON_OBJECT_REF("rpcConnector", &LegacyGatewayRPC::setRPCConnector)
+BEEEON_OBJECT_END(BeeeOn, LegacyGatewayRPC)
