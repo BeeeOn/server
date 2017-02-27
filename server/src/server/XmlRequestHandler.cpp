BeeeOn/server

commit 2621f46a74edcafad25c2b774c21053bfa2fbd44

XmlRequestHandler: new class
Handle XML messages coming to the TCP server.

Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

server/src/server/XmlRequestHandler.cpp
@@ -1,0 +1,119 @@
+#include <Poco/Exception.h>
+#include <Poco/FIFOBuffer.h>
+#include <Poco/Net/NetException.h>
+
+#include "Debug.h"
+#include "server/XmlRequestHandler.h"
+
+using namespace std;
+using namespace Poco;
+using namespace Poco::Net;
+using namespace Poco::XML;
+using namespace BeeeOn;
+
+XmlRequestHandler::XmlRequestHandler(
+		const StreamSocket &socket,
+		const AutoPtr<Document> input):
+	TCPServerConnection(socket),
+	m_logger(LOGGER_CLASS(this)),
+	m_input(input),
+	m_stream(socket),
+	m_output(m_stream, XMLWriter::Options::CANONICAL_XML)
+{
+}
+
+void XmlRequestHandler::run()
+{
+	_TRACE_METHOD(m_logger);
+
+	try {
+		m_output.startDocument();
+		handleInput();
+		m_output.endDocument();
+	} catch (const Exception &e) {
+		m_logger.log(e, __FILE__, __LINE__);
+	}
+}
+
+XmlRequestHandlerFactory::XmlRequestHandlerFactory():
+	m_logger(LOGGER_CLASS(this)),
+	m_maxLength(4096)
+{
+	init();
+}
+
+XmlRequestHandlerFactory::XmlRequestHandlerFactory(size_t maxLength):
+	m_logger(LOGGER_CLASS(this)),
+	m_maxLength(maxLength)
+{
+	init();
+}
+
+void XmlRequestHandlerFactory::init()
+{
+	injector<XmlRequestHandlerFactory, XmlRequestHandlerResolver>
+		("resolvers", &XmlRequestHandlerFactory::registerResolver);
+	numberInjector("maxBuffer", (NumberSetter)
+			&XmlRequestHandlerFactory::setMaxLength);
+}
+
+TCPServerConnection *XmlRequestHandlerFactory::createConnection(
+	const StreamSocket &socket)
+{
+	try {
+		StreamSocket readableSocket(socket);
+		return resolveRequest(socket, parseDocument(readableSocket));
+	}
+	catch (const Exception &e) {
+		m_logger.log(e, __FILE__, __LINE__);
+	}
+	catch (const exception &e) {
+		m_logger.critical(e.what(), __FILE__, __LINE__);
+	}
+
+	return NULL;
+}
+
+XmlRequestHandler *XmlRequestHandlerFactory::resolveRequest(
+		const StreamSocket &socket,
+		const AutoPtr<Document> input)
+{
+	for (auto resolver : m_resolvers) {
+		if (!resolver->canHandle(*input))
+			continue;
+
+		return resolver->createHandler(socket, input);
+	}
+
+	m_logger.critical("could not resolve handler", __FILE__, __LINE__);
+	return NULL;
+}
+
+void XmlRequestHandlerFactory::registerResolver(XmlRequestHandlerResolver *resolver)
+{
+	m_resolvers.push_back(resolver);
+}
+
+AutoPtr<Document> XmlRequestHandlerFactory::parseDocument(
+		StreamSocket &socket)
+{
+	FIFOBuffer buffer(m_maxLength > 1024? m_maxLength : 1024);
+	string xml;
+	int ret = 0;
+
+	do {
+		buffer.drain(ret);
+		ret = socket.receiveBytes(buffer);
+		if (ret <= 0) {
+			throw ConnectionResetException(
+				socket.peerAddress().toString());
+		}
+
+		xml += string(buffer.begin(), buffer.used());
+	} while(buffer.isFull() || xml.size() >= m_maxLength);
+
+	size_t length = xml.size() > m_maxLength? m_maxLength : xml.size();
+	return m_parser.parse(xml.c_str(), length);
+}
+
+BEEEON_OBJECT(XmlRequestHandlerFactory, BeeeOn::XmlRequestHandlerFactory)

server/conf/services.xml
@@ -34,6 +34,10 @@
 			<set name="sessionManager" ref="sessionManager" />
 		</instance>
 
+		<instance name="xmlui"
+				class="BeeeOn::XmlRequestHandlerFactory">
+		</instance>
+
 		<instance name="uiMockInit"
 				class="BeeeOn::UIMockInit"
 				init="early">

server/src/CMakeLists.txt
@@ -26,6 +26,7 @@ file(GLOB SOURCES
 	${PROJECT_SOURCE_DIR}/server/RestHandler.cpp
 	${PROJECT_SOURCE_DIR}/server/RestAuthHandler.cpp
 	${PROJECT_SOURCE_DIR}/server/AccessLevel.cpp
+	${PROJECT_SOURCE_DIR}/server/XmlRequestHandler.cpp
 	${PROJECT_SOURCE_DIR}/model/GatewayID.cpp
 	${PROJECT_SOURCE_DIR}/model/JSONSerializer.cpp
 	${PROJECT_SOURCE_DIR}/model/Identity.cpp

server/src/server/XmlRequestHandler.h
@@ -1,0 +1,85 @@
+#ifndef BEEEON_XML_REQUEST_HANDLER_H
+#define BEEEON_XML_REQUEST_HANDLER_H
+
+#include <unistd.h>
+
+#include <Poco/Logger.h>
+#include <Poco/Exception.h>
+
+#include <Poco/Net/StreamSocket.h>
+#include <Poco/Net/SocketStream.h>
+#include <Poco/Net/TCPServerConnection.h>
+#include <Poco/Net/TCPServerConnectionFactory.h>
+
+#include <Poco/DOM/Document.h>
+#include <Poco/XML/XMLWriter.h>
+
+#include "util/SecureXmlParser.h"
+#include "di/InjectorTarget.h"
+
+namespace BeeeOn {
+
+class XmlRequestHandler : public Poco::Net::TCPServerConnection {
+public:
+	XmlRequestHandler(
+		const Poco::Net::StreamSocket &socket,
+		const Poco::AutoPtr<Poco::XML::Document> input);
+	void run() override;
+	virtual void handleInput() = 0;
+
+protected:
+	Poco::Logger &m_logger;
+	const Poco::AutoPtr<Poco::XML::Document> m_input;
+	Poco::Net::SocketOutputStream m_stream;
+	Poco::XML::XMLWriter m_output;
+};
+
+class XmlRequestHandlerResolver : public BeeeOn::AbstractInjectorTarget {
+public:
+	virtual bool canHandle(const Poco::XML::Document &input) = 0;
+	virtual XmlRequestHandler *createHandler(
+			const Poco::Net::StreamSocket &socket,
+			const Poco::AutoPtr<Poco::XML::Document> input) = 0;
+};
+
+class XmlRequestHandlerFactory :
+	public Poco::Net::TCPServerConnectionFactory,
+	public BeeeOn::AbstractInjectorTarget {
+private:
+	void init();
+public:
+	XmlRequestHandlerFactory();
+	XmlRequestHandlerFactory(std::size_t maxLength);
+
+	Poco::Net::TCPServerConnection *createConnection(
+			const Poco::Net::StreamSocket &socket);
+
+	Poco::AutoPtr<Poco::XML::Document> parseDocument(
+			Poco::Net::StreamSocket &socket);
+
+	XmlRequestHandler *resolveRequest(
+			const Poco::Net::StreamSocket &socket,
+			const Poco::AutoPtr<Poco::XML::Document> input);
+
+	void registerResolver(XmlRequestHandlerResolver *resolver);
+
+	void setMaxLength(int length)
+	{
+		if (length <= 0) {
+			throw Poco::InvalidArgumentException(
+				"invalid max buffer length");
+		}
+
+		m_maxLength = length;
+	}
+
+private:
+	Poco::Logger &m_logger;
+	std::vector<XmlRequestHandlerResolver *> m_resolvers;
+	BeeeOn::SecureXmlParser m_parser;
+	std::size_t m_maxLength;
+};
+
+}
+
+#endif

commit 43d396cbb0440d537089b8ca0a07335fd30cca98

XmlRequestHandler: fix testing for max XML message size
The bug allows to DoS the server by allocating unlimited amount of
memory for a single message.

The DoS can be performed by sending a long payloads that would pass
the initial buffer.isFull() condition which is trivial to achieve.

Reported-by: Jozef Halaj <xhalaj03@stud.fit.vutbr.cz>
Fixes: 2621f46a74ed ("XmlRequestHandler: new class")
Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

server/src/server/XmlRequestHandler.cpp
@@ -126,7 +126,7 @@ AutoPtr<Document> XmlRequestHandlerFactory::parseDocument(
 		}
 
 		xml += string(buffer.begin(), buffer.used());
-	} while(buffer.isFull() || xml.size() >= m_maxLength);
+	} while(buffer.isFull() && xml.size() < m_maxLength);
 
 	size_t length = xml.size() > m_maxLength? m_maxLength : xml.size();
 	return m_parser.parse(xml.c_str(), length);