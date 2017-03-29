1efc7ceddb126b6aca6d8733baab0309ee8812ab
Jan Viktorin <iviktorin@fit.vutbr.cz>
2017-03-30 01:25:05 +0200

    t: restui: test gateways API
    The provided integration tests checks REST actions:
    
    * assign, unassign
    * list
    * detail
    
    Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

---
+++ server/t/restui/t2001-gateways-list-detail.py
@@ -0,0 +1,78 @@
+#! /usr/bin/env python3
+
+import config
+config.import_libs()
+
+import unittest
+import socket
+import json
+
+from rest import GET, POST, DELETE
+
+class TestGatewayListDetail(unittest.TestCase):
+	"""
+	Create a session for testing.
+	"""
+	def setUp(self):
+		req = POST(config.ui_host, config.ui_port, "/auth")
+		req.body(config.PERMIT_LOGIN)
+		response, content = req()
+
+		self.assertEqual(200, response.status)
+
+		result = json.loads(content)
+		self.assertEqual("success", result["status"])
+
+		self.session = result["data"]["id"]
+
+		req = POST(config.ui_host, config.ui_port, "/gateways")
+		req.authorize(self.session)
+		req.body(json.dumps(
+			{"id": config.gateway_id, "name": "Testing Gateway"}
+		))
+		response, _ = req()
+
+		self.assertEqual(201, response.status)
+
+	def tearDown(self):
+		req = DELETE(config.ui_host, config.ui_port, "/gateways/" + config.gateway_id)
+		req.authorize(self.session)
+		response, content = req()
+
+		self.assertEqual(204, response.status)
+
+		req = DELETE(config.ui_host, config.ui_port, "/auth")
+		req.authorize(self.session)
+		response, content = req()
+
+		self.assertEqual(204, response.status)
+
+	def test1_list(self):
+		req = GET(config.ui_host, config.ui_port, "/gateways")
+		req.authorize(self.session)
+		response, content = req()
+
+		self.assertEqual(200, response.status)
+		result = json.loads(content)
+		self.assertEqual("success", result["status"])
+
+		self.assertEqual(1, len(result["data"]))
+		self.assertEqual(config.gateway_id, result["data"][0]["id"])
+		self.assertEqual("Testing Gateway", result["data"][0]["name"])
+
+	def test2_detail(self):
+		req = GET(config.ui_host, config.ui_port, "/gateways/" + config.gateway_id)
+		req.authorize(self.session)
+		response, content = req()
+
+		self.assertEqual(200, response.status)
+		result = json.loads(content)
+		self.assertEqual("success", result["status"])
+
+		self.assertEqual(config.gateway_id, result["data"]["id"])
+		self.assertEqual("Testing Gateway", result["data"]["name"])
+
+if __name__ == '__main__':
+	import sys
+	import taprunner
+	unittest.main(testRunner=taprunner.TAPTestRunner(stream = sys.stdout))
