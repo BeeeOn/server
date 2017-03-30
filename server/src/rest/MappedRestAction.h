938a211a747dfa53584cb5588e7940febdc400b2
Jan Viktorin <iviktorin@fit.vutbr.cz>
2017-03-30 15:10:06 +0200

    MappedRestAction: new class
    Implementation of RestAction with mapping to an URI pattern.
    The MappedRestAction is accessible from outside with a HTTP
    server. Also, mapped action can have caching settings reflected
    in the HTTP communication. Other similar settings might be
    added in the future.
    
    Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

---
+++ server/src/rest/MappedRestAction.h
@@ -0,0 +1,39 @@
+#ifndef BEEEON_MAPPED_REST_ACTION_H
+#define BEEEON_MAPPED_REST_ACTION_H
+
+#include <map>
+
+#include <Poco/SharedPtr.h>
+
+#include "rest/RestAction.h"
+#include "rest/UriPattern.h"
+
+namespace BeeeOn {
+
+class MappedRestAction : public RestAction {
+public:
+	typedef Poco::SharedPtr<MappedRestAction> Ptr;
+	typedef std::map<std::string, std::string> Params;
+
+	MappedRestAction(
+		const std::string &handlerName,
+		const std::string &actionName,
+		const UriPattern &pattern);
+
+	void setCaching(int seconds);
+	int caching() const;
+
+	Poco::URI uri(const std::vector<std::string> &params = {}) const;
+
+	bool match(const Poco::URI &uri, Params &params) const;
+
+	static int parseCaching(const std::string &caching);
+
+private:
+	UriPattern m_uriPattern;
+	int m_caching;
+};
+
+}
+
+#endif
