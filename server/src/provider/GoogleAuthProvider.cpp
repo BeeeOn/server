BeeeOn/server

commit e72183976e09e42f6268756ccba7005192ad3f5a

GoogleAuthProvider: fill AuthResult from Google data

server/src/provider/GoogleAuthProvider.cpp
@@ -45,6 +45,19 @@ bool GoogleAuthProvider::verifyAuthCode(const string &authCode, AuthResult &info
 	if (email.empty() || google_id.empty())
 		return false;
 
+	info.setAccessToken(idToken);
+
+	string firstName;
+	string lastName;
+	string picture;
+
+	if (userInfo.get("given_name", firstName))
+		info.setFirstName(firstName);
+	if (userInfo.get("family_name", lastName))
+		info.setLastName(lastName);
+	if (userInfo.get("picture", picture))
+		info.setPicture(picture);
+
 	return true;
 }
 

commit 4842bd7612fa42fad18728e8e47fbb783e87cae5

GoogleAuthProvider: store access_token instead of id_token
The AuthResult expects access token and not the id token.
Fix this behaviour. This also fixes storing data into
database that respects varchar boundaries (i.e. not SQLite).

Fixes: e72183976e09 ("GoogleAuthProvider: fill AuthResult from Google data")
Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>
Reviewed-by: Peter Tisovčík <xtisov00@stud.fit.vutbr.cz>

server/src/provider/GoogleAuthProvider.cpp
@@ -50,7 +50,7 @@ bool GoogleAuthProvider::parseIdentity(const std::string &userInfo,
 	if (info->has("picture"))
 		result.setPicture(info->getValue<string>("picture"));
 
-	result.setAccessToken(tokens.idToken);
+	result.setAccessToken(tokens.accessToken);
 
 	return true;
 }