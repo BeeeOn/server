20628cddb71d4e22f9adb717b98b87850e5efbaa
Jan Viktorin <iviktorin@fit.vutbr.cz>
2017-07-11 02:07:45 +0200

    SqitchLine: new abstract class
    The SqitchLine represents a single line in a Sqitch plan. Each line
    can have an optional note. Also, the line number is preserved for
    debugging purposes. The line numbers allows to reconstruct also the
    empty lines in a Sqitch plan.
    
    The SqitchLine is sorted by the line number as default.
    
    Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

---
+++ server/src/sqitch/SqitchLine.h
@@ -0,0 +1,65 @@
+#ifndef BEEEON_SQITCH_LINE_H
+#define BEEEON_SQITCH_LINE_H
+
+#include <string>
+
+#include <Poco/SharedPtr.h>
+#include <Poco/Timestamp.h>
+
+namespace BeeeOn {
+
+class SqitchLine {
+public:
+	typedef Poco::SharedPtr<SqitchLine> Ptr;
+
+	SqitchLine(unsigned int line);
+	virtual ~SqitchLine();
+
+	unsigned int line() const;
+
+	virtual bool equals(const SqitchLine &other) const;
+	virtual bool lessThen(const SqitchLine &other) const;
+
+	void setNote(const std::string &note);
+	std::string note() const;
+
+	virtual std::string toString() const;
+
+	bool operator ==(const SqitchLine &other)
+	{
+		return equals(other);
+	}
+
+	bool operator !=(const SqitchLine &other)
+	{
+		return !equals(other);
+	}
+
+	bool operator <(const SqitchLine &other) const
+	{
+		return lessThen(other);
+	}
+
+	bool operator <=(const SqitchLine &other) const
+	{
+		return !other.lessThen(*this);
+	}
+
+	bool operator >(const SqitchLine &other) const
+	{
+		return other.lessThen(*this);
+	}
+
+	bool operator >=(const SqitchLine &other) const
+	{
+		return !lessThen(other);
+	}
+
+private:
+	unsigned int m_line;
+	std::string m_note;
+};
+
+}
+
+#endif
