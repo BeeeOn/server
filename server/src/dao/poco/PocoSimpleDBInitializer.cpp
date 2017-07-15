ff6b0095375c537d95ac439c286b171d164889b5
Jan Viktorin <iviktorin@fit.vutbr.cz>
2017-07-15 23:54:17 +0200

    PocoSimpleDBInitializer: new class
    Initialize database via a script specified by its file path.
    Such script is simply executed via the given session.
    
    Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

---
+++ server/src/dao/poco/PocoSimpleDBInitializer.cpp
@@ -0,0 +1,55 @@
+#include <Poco/FileStream.h>
+#include <Poco/Logger.h>
+#include <Poco/StreamCopier.h>
+#include <Poco/Data/Session.h>
+#include <Poco/Data/Statement.h>
+
+#include "di/Injectable.h"
+#include "dao/poco/PocoSimpleDBInitializer.h"
+
+BEEEON_OBJECT_BEGIN(BeeeOn, PocoSimpleDBInitializer)
+BEEEON_OBJECT_CASTABLE(PocoDBInitializer)
+BEEEON_OBJECT_TEXT("script", &PocoSimpleDBInitializer::loadScript)
+BEEEON_OBJECT_END(BeeeOn, PocoSimpleDBInitializer)
+
+using namespace std;
+using namespace Poco;
+using namespace Poco::Data;
+using namespace BeeeOn;
+
+void PocoSimpleDBInitializer::loadScript(const string &path)
+{
+	if (logger().debug()) {
+		logger().debug("loading script from " + path,
+				__FILE__, __LINE__);
+	}
+
+	FileInputStream in(path);
+	StreamCopier::copyToString(in, m_script);
+
+	m_file = path;
+}
+
+void PocoSimpleDBInitializer::initialize(Session &session)
+{
+	if (m_script.empty()) {
+		logger().notice("no script to be executed",
+				__FILE__, __LINE__);
+		return;
+	}
+
+	if (logger().trace()) {
+		logger().trace("executing script:\n" + m_script,
+				__FILE__, __LINE__);
+	}
+
+	const auto result = execute(session, m_script);
+
+	logger().notice(
+		"executed script "
+		+ m_file
+		+ " (result: "
+		+ to_string(result)
+		+ ")",
+		__FILE__, __LINE__);
+}
---
+++ server/src/dao/poco/PocoSimpleDBInitializer.h
@@ -0,0 +1,30 @@
+#ifndef BEEEON_POCO_SIMPLE_DB_INITIALIZER_H
+#define BEEEON_POCO_SIMPLE_DB_INITIALIZER_H
+
+#include <string>
+
+#include "dao/poco/PocoDBInitializer.h"
+#include "util/Loggable.h"
+
+namespace BeeeOn {
+
+class PocoSimpleDBInitializer : public PocoDBInitializer, Loggable {
+public:
+	/**
+	 * Execute the loaded script.
+	 */
+	void initialize(Poco::Data::Session &session) override;
+
+	/**
+	 * Load script from the given file path.
+	 */
+	void loadScript(const std::string &path);
+
+private:
+	std::string m_file;
+	std::string m_script;
+};
+
+}
+
+#endif
