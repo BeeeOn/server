1e20e073e9ebb08cd8a06a8fbb1912129cd3d4c1
Jan Viktorin <iviktorin@fit.vutbr.cz>
2017-07-27 16:35:00 +0200

    RoleService: separate interface and implementation
    Now, the RoleService is a pure interface. The default implementation
    is placed in the derived RoleServiceImpl class.
    
    Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

--- server/src/service/RoleService.h
+++ server/src/service/RoleService.h
@@ -1,9 +1,10 @@
 #ifndef BEEEON_ROLE_SERVICE_H
 #define BEEEON_ROLE_SERVICE_H
 
+#include <vector>
+
 #include "service/Single.h"
 #include "service/Relation.h"
-#include "transaction/Transactional.h"
 
 namespace BeeeOn {
 
@@ -13,65 +14,17 @@ class RoleInGateway;
 class LegacyRoleInGateway;
 class AccessLevel;
 
-class IdentityDao;
-class GatewayDao;
-class RoleInGatewayDao;
-
-class RoleAccessPolicy;
-class NotificationDispatcher;
-
-class RoleService : public Transactional {
+class RoleService {
 public:
-	RoleService();
+	virtual ~RoleService();
 
-	void setIdentityDao(IdentityDao *dao);
-	void setGatewayDao(GatewayDao *dao);
-	void setRoleInGatewayDao(RoleInGatewayDao *dao);
-	void setAccessPolicy(RoleAccessPolicy *policy);
-	void setNotificationDispatcher(NotificationDispatcher *service);
-
-	void inviteIdentity(
+	virtual void inviteIdentity(
 			Relation<Identity, Gateway> &input,
-			const AccessLevel &as)
-	{
-		BEEEON_TRANSACTION(doInviteIdentity(input, as));
-	}
-
-	void list(Relation<std::vector<RoleInGateway>, Gateway> &input)
-	{
-		BEEEON_TRANSACTION(doList(input));
-	}
-
-	void list(Relation<std::vector<LegacyRoleInGateway>, Gateway> &input)
-	{
-		BEEEON_TRANSACTION(doList(input));
-	}
-
-	void remove(Single<RoleInGateway> &input)
-	{
-		BEEEON_TRANSACTION(doRemove(input));
-	}
-
-	void update(SingleWithData<RoleInGateway> &input)
-	{
-		BEEEON_TRANSACTION(doUpdate(input));
-	}
-
-protected:
-	void doInviteIdentity(
-			Relation<Identity, Gateway> &input,
-			const AccessLevel &as);
-	void doList(Relation<std::vector<RoleInGateway>, Gateway> &input);
-	void doList(Relation<std::vector<LegacyRoleInGateway>, Gateway> &input);
-	void doRemove(Single<RoleInGateway> &input);
-	void doUpdate(SingleWithData<RoleInGateway> &input);
-
-private:
-	IdentityDao *m_identityDao;
-	GatewayDao *m_gatewayDao;
-	RoleInGatewayDao *m_roleInGatewayDao;
-	RoleAccessPolicy *m_accessPolicy;
-	NotificationDispatcher *m_notificationDispatcher;
+			const AccessLevel &as) = 0;
+	virtual void list(Relation<std::vector<RoleInGateway>, Gateway> &input) = 0;
+	virtual void list(Relation<std::vector<LegacyRoleInGateway>, Gateway> &input) = 0;
+	virtual void remove(Single<RoleInGateway> &input) = 0;
+	virtual void update(SingleWithData<RoleInGateway> &input) = 0;
 };
 
 }
---
+++ server/src/service/RoleServiceImpl.h
@@ -0,0 +1,78 @@
+#ifndef BEEEON_ROLE_SERVICE_IMPL_H
+#define BEEEON_ROLE_SERVICE_IMPL_H
+
+#include "service/RoleService.h"
+#include "transaction/Transactional.h"
+
+namespace BeeeOn {
+
+class Identity;
+class Gateway;
+class RoleInGateway;
+class LegacyRoleInGateway;
+class AccessLevel;
+
+class IdentityDao;
+class GatewayDao;
+class RoleInGatewayDao;
+
+class RoleAccessPolicy;
+class NotificationDispatcher;
+
+class RoleServiceImpl : public RoleService, public Transactional {
+public:
+	RoleServiceImpl();
+
+	void setIdentityDao(IdentityDao *dao);
+	void setGatewayDao(GatewayDao *dao);
+	void setRoleInGatewayDao(RoleInGatewayDao *dao);
+	void setAccessPolicy(RoleAccessPolicy *policy);
+	void setNotificationDispatcher(NotificationDispatcher *service);
+
+	void inviteIdentity(
+			Relation<Identity, Gateway> &input,
+			const AccessLevel &as)
+	{
+		BEEEON_TRANSACTION(doInviteIdentity(input, as));
+	}
+
+	void list(Relation<std::vector<RoleInGateway>, Gateway> &input)
+	{
+		BEEEON_TRANSACTION(doList(input));
+	}
+
+	void list(Relation<std::vector<LegacyRoleInGateway>, Gateway> &input)
+	{
+		BEEEON_TRANSACTION(doList(input));
+	}
+
+	void remove(Single<RoleInGateway> &input)
+	{
+		BEEEON_TRANSACTION(doRemove(input));
+	}
+
+	void update(SingleWithData<RoleInGateway> &input)
+	{
+		BEEEON_TRANSACTION(doUpdate(input));
+	}
+
+protected:
+	void doInviteIdentity(
+			Relation<Identity, Gateway> &input,
+			const AccessLevel &as);
+	void doList(Relation<std::vector<RoleInGateway>, Gateway> &input);
+	void doList(Relation<std::vector<LegacyRoleInGateway>, Gateway> &input);
+	void doRemove(Single<RoleInGateway> &input);
+	void doUpdate(SingleWithData<RoleInGateway> &input);
+
+private:
+	IdentityDao *m_identityDao;
+	GatewayDao *m_gatewayDao;
+	RoleInGatewayDao *m_roleInGatewayDao;
+	RoleAccessPolicy *m_accessPolicy;
+	NotificationDispatcher *m_notificationDispatcher;
+};
+
+}
+
+#endif
