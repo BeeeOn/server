90a41299ae0ccfac50f04612e2dbf4e8dc28ba9c
Jozef Halaj <xhalaj03@stud.fit.vutbr.cz>
2017-08-02 23:17:51 +0200

    GWResponseExpectedQueue: new class
    The GWResponseExpectedQueue provides the ability to detect missing
    gateway responses to sent requests.
    
    Each request sent to the gateway is registered to this queue by the
    RPCForwarder and unregistered after arriving. In case of the missing
    response, the timeout response is forwarded by the RPCForwarder
    (there is cross reference) and gateway connection is removed from
    the GatewayCommunicator if it is inactive.
    
    Signed-off-by: Jozef Halaj <xhalaj03@stud.fit.vutbr.cz>
    Acked-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

---
+++ server/src/gws/GWResponseExpectedQueue.cpp
@@ -0,0 +1,122 @@
+#include <Poco/Logger.h>
+
+#include "di/Injectable.h"
+#include "gws/GWResponseExpectedQueue.h"
+#include "util/LambdaTimerTask.h"
+
+BEEEON_OBJECT_BEGIN(BeeeOn, GWResponseExpectedQueue)
+BEEEON_OBJECT_CASTABLE(StoppableLoop)
+BEEEON_OBJECT_TIME("responseTimeout", &GWResponseExpectedQueue::setResponseTimeout)
+BEEEON_OBJECT_REF("gatewayCommunicator", &GWResponseExpectedQueue::setGatewayCommunicator)
+BEEEON_OBJECT_REF("rpcForwarder", &GWResponseExpectedQueue::setRPCForwarder)
+BEEEON_OBJECT_HOOK("cleanup", &GWResponseExpectedQueue::cleanup);
+BEEEON_OBJECT_END(BeeeOn, GWResponseExpectedQueue)
+
+using namespace std;
+using namespace Poco;
+using namespace Poco::Util;
+using namespace BeeeOn;
+
+TimerTask::Ptr GWResponseExpectedQueue::createResponseMissingTask(
+	const GatewayID &gatewayID, const GlobalID &responseID)
+{
+	Timestamp taskCreationTime;
+
+	return new LambdaTimerTask([gatewayID, responseID, taskCreationTime, this]()
+	{
+		if (logger().debug()) {
+			logger().debug("response " + responseID.toString()
+				+ " from gateway " + gatewayID.toString() + " is missing",
+				__FILE__, __LINE__);
+		}
+
+		ScopedLockWithUnlock<FastMutex> guard(m_mutex);
+		auto it = m_responses.find(make_pair(gatewayID, responseID));
+		if (it == m_responses.end())
+			return;
+
+		m_responses.erase(it);
+		guard.unlock();
+
+		m_rpcForwarder->forwardTimeout(gatewayID, responseID);
+		m_gatewayCommunicator->removeIfInactive(gatewayID, taskCreationTime);
+	});
+}
+
+void GWResponseExpectedQueue::registerResponse(const GatewayID &gatewayID,
+	const GlobalID &responseID)
+{
+	if (logger().debug()) {
+		logger().debug("expecting response " + responseID.toString()
+			+ "from gateway " + gatewayID.toString(),
+			__FILE__, __LINE__);
+	}
+
+	TimerTask::Ptr task = createResponseMissingTask(gatewayID, responseID);
+
+	FastMutex::ScopedLock guard(m_mutex);
+	m_responses.emplace(make_pair(gatewayID, responseID), task);
+
+	m_timer.schedule(task, Timestamp() + m_responseTimeout);
+}
+
+void GWResponseExpectedQueue::notifyDelivered(const GatewayID &gatewayID,
+	const GlobalID &responseID)
+{
+	FastMutex::ScopedLock guard(m_mutex);
+
+	if (logger().debug()) {
+		logger().debug("delivered response " + responseID.toString()
+					   + "from gateway " + gatewayID.toString(),
+			__FILE__, __LINE__);
+	}
+
+	auto it = m_responses.find(make_pair(gatewayID, responseID));
+	if (it == m_responses.end())
+		return;
+
+	it->second->cancel();
+	m_responses.erase(it);
+}
+
+void GWResponseExpectedQueue::start()
+{
+	logger().information("response expected queue is up", __FILE__, __LINE__);
+}
+
+void GWResponseExpectedQueue::stop()
+{
+	logger().information("stopping response expected queue", __FILE__, __LINE__);
+
+	m_timer.cancel(true);
+
+	FastMutex::ScopedLock guard(m_mutex);
+	m_responses.clear();
+
+	logger().information("response expected queue has stopped", __FILE__, __LINE__);
+}
+
+void GWResponseExpectedQueue::cleanup()
+{
+	m_gatewayCommunicator = nullptr;
+	m_rpcForwarder = nullptr;
+}
+
+void GWResponseExpectedQueue::setResponseTimeout(const Timespan &timeout)
+{
+	if (timeout < 0)
+		throw InvalidArgumentException("response timeout must be non negative");
+
+	m_responseTimeout = timeout;
+}
+
+void GWResponseExpectedQueue::setGatewayCommunicator(
+		GatewayCommunicator::Ptr communicator)
+{
+	m_gatewayCommunicator = communicator;
+}
+
+void GWResponseExpectedQueue::setRPCForwarder(RPCForwarder::Ptr forwarder)
+{
+	m_rpcForwarder = forwarder;
+}
---
+++ server/src/gws/GWResponseExpectedQueue.h
@@ -0,0 +1,84 @@
+#ifndef BEEEON_GW_RESPONSE_EXPECTED_QUEUE_H
+#define BEEEON_GW_RESPONSE_EXPECTED_QUEUE_H
+
+#include <Poco/Timespan.h>
+#include <Poco/Mutex.h>
+#include <Poco/SharedPtr.h>
+#include <Poco/Util/Timer.h>
+#include <Poco/Util/TimerTask.h>
+
+#include "gws/GatewayCommunicator.h"
+#include "gws/RPCForwarder.h"
+#include "loop/StoppableLoop.h"
+#include "util/Loggable.h"
+
+namespace BeeeOn {
+
+/**
+ * @brief This class is responsible for reaction on missing expected response
+ * from the gateway and detection of the broken connection.
+ *
+ * Each request sent to the gateway expect appropriate response. If response
+ * is not received till timeout, this fact must be forwarded to the UIServer.
+ * All expected responses are registered to this queue and unregistered after
+ * arriving. The unique response identifier is stored with response missing task
+ * object, which is scheduled in Poco::Util::Timer. The task represents reaction
+ * on the missing response, which means forwarding timeout response via the
+ * RPCForwarder and removing connection from the GatewayCommunicator, if it is
+ * inactive.
+ */
+class GWResponseExpectedQueue :
+	public StoppableLoop,
+	protected Loggable {
+public:
+	typedef Poco::SharedPtr<GWResponseExpectedQueue> Ptr;
+	typedef std::map<std::pair<GatewayID, GlobalID>,
+		Poco::Util::TimerTask::Ptr> ResponseMap;
+
+	void start() override;
+	void stop() override;
+
+	void setResponseTimeout(const Poco::Timespan &timeout);
+	void setGatewayCommunicator(GatewayCommunicator::Ptr communicator);
+	void setRPCForwarder(RPCForwarder::Ptr forwarder);
+
+	/**
+	 * @brief Add expected response to the queue
+	 * and schedule response missing task.
+	 */
+	void registerResponse(const GatewayID &gatewayID, const GlobalID &responseID);
+
+	/**
+	 * @brief Removes expected response from the queue
+	 * and cancel response missing task.
+	 */
+	void notifyDelivered(const GatewayID &gatewayID, const GlobalID &responseID);
+
+	/**
+	 * Break dependency cycles prior to destruction.
+	 */
+	void cleanup();
+
+private:
+	/**
+	 * @brief Creates a response missing task and returns it.
+	 *
+	 * The task forwards timeout response using RPCForwarder and remove
+	 * the gateway connection from the GatewayCommunicator if it is inactive.
+	 */
+	Poco::Util::TimerTask::Ptr createResponseMissingTask(const GatewayID &gatewayID,
+		const GlobalID &responseID);
+
+private:
+	Poco::Timespan m_responseTimeout;
+	GatewayCommunicator::Ptr m_gatewayCommunicator;
+	RPCForwarder::Ptr m_rpcForwarder;
+
+	ResponseMap m_responses;
+	Poco::FastMutex m_mutex;
+	Poco::Util::Timer m_timer;
+};
+
+}
+
+#endif
