BeeeOn/server

commit 587aac2c90aaed05b9f3d415565f908c376bd00f

VerifiedIdentityDao: new class
Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

server/src/dao/VerifiedIdentityDao.h
@@ -1,0 +1,52 @@
+#ifndef BEEEON_VERIFIED_IDENTITY_DAO_H
+#define BEEEON_VERIFIED_IDENTITY_DAO_H
+
+#include "di/InjectorTarget.h"
+#include "dao/IdentityDao.h"
+#include "dao/NullDao.h"
+#include "dao/MockDao.h"
+#include "model/VerifiedIdentity.h"
+
+namespace BeeeOn {
+
+class VerifiedIdentityDao {
+public:
+	virtual void create(VerifiedIdentity &identity) = 0;
+	virtual bool fetch(VerifiedIdentity &identity) = 0;
+	virtual bool fetchBy(VerifiedIdentity &identity,
+			const std::string email,
+			const std::string provider) = 0;
+	virtual bool update(VerifiedIdentity &identity) = 0;
+	virtual bool remove(const VerifiedIdentity &identity) = 0;
+};
+
+class NullVerifiedIdentityDao : public AbstractInjectorTarget,
+		public NullDao<VerifiedIdentity, VerifiedIdentityDao> {
+public:
+	virtual bool fetchBy(VerifiedIdentity &identity,
+			const std::string email,
+			const std::string provider)
+	{
+		return fetch(identity);
+	}
+
+	VerifiedIdentityDao &instance();
+};
+
+class MockVerifiedIdentityDao : public AbstractInjectorTarget,
+		public MockDao<VerifiedIdentity, VerifiedIdentityDao> {
+public:
+	bool fetchBy(VerifiedIdentity &identity,
+			const std::string email,
+			const std::string provider);
+
+protected:
+	VerifiedIdentityID nextID()
+	{
+		return VerifiedIdentityID::random();
+	}
+};
+
+}
+
+#endif

server/src/CMakeLists.txt
@@ -34,6 +34,7 @@ file(GLOB SOURCES
 	${PROJECT_SOURCE_DIR}/dao/GatewayDao.cpp
 	${PROJECT_SOURCE_DIR}/dao/LocationDao.cpp
 	${PROJECT_SOURCE_DIR}/dao/IdentityDao.cpp
+	${PROJECT_SOURCE_DIR}/dao/VerifiedIdentityDao.cpp
 	${PROJECT_SOURCE_DIR}/service/PlaceService.cpp
 	${PROJECT_SOURCE_DIR}/service/UserService.cpp
 	${PROJECT_SOURCE_DIR}/service/AuthService.cpp

server/src/dao/VerifiedIdentityDao.cpp
@@ -1,0 +1,27 @@
+#include "dao/VerifiedIdentityDao.h"
+
+BEEEON_OBJECT(MockVerifiedIdentityDao, BeeeOn::MockVerifiedIdentityDao)
+
+using namespace BeeeOn;
+
+bool MockVerifiedIdentityDao::fetchBy(VerifiedIdentity &identity,
+			const std::string email,
+			const std::string provider)
+{
+	Iterator it = storage().begin();
+
+	for (; it != storage().end(); ++it) {
+		const VerifiedIdentity &tmp = *it->second;
+
+		if (tmp.identity().email() != email)
+			continue;
+
+		if (tmp.provider() != provider)
+			continue;
+
+		identity = tmp;
+		return true;
+	}
+
+	return false;
+}

server/src/ui/services.xml
@@ -108,12 +108,15 @@
 		<instance name="mockGatewayDao" class="BeeeOn::MockGatewayDao" />
 		<instance name="mockLocationDao" class="BeeeOn::MockLocationDao" />
 		<instance name="mockIdentityDao" class="BeeeOn::MockIdentityDao" />
+		<instance name="mockVerifiedIdentityDao"
+				class="BeeeOn::MockVerifiedIdentityDao" />
 
 		<alias name="placeDao" ref="mockPlaceDao" />
 		<alias name="userDao" ref="mockUserDao" />
 		<alias name="gatewayDao" ref="mockGatewayDao" />
 		<alias name="locationDao" ref="mockLocationDao" />
 		<alias name="identityDao" ref="mockIdentityDao" />
+		<alias name="verifiedIdentityDao" ref="mockVerifiedIdentityDao" />
 
 		<instance name="sociManager"
 				class="BeeeOn::SociManager">

commit 60e11d3afea4490a11935ed1e02bdd9aa62043e4

VerifiedIdentityDao: drop include of IdentityDao
Fixes: 587aac2c90aa ("VerifiedIdentityDao: new class")
Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

server/src/dao/VerifiedIdentityDao.h
@@ -5,7 +5,6 @@
 
 #include <Poco/SharedPtr.h>
 
-#include "dao/IdentityDao.h"
 #include "dao/MockDao.h"
 #include "model/VerifiedIdentity.h"
 