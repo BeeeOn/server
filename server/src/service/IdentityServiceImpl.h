BeeeOn/server

commit 70a75ac824dac34c4aba5a59684a7afe29ec1420

IdentityService: separate interface and implementation
Now, the IdentityService is a pure interface. The default implementation
is placed in the derived IdentityServiceImpl class.

Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

server/src/service/IdentityServiceImpl.h
@@ -1,0 +1,40 @@
+#ifndef BEEEON_IDENTITY_SERVICE_IMPL_H
+#define BEEEON_IDENTITY_SERVICE_IMPL_H
+
+#include "dao/IdentityDao.h"
+#include "dao/VerifiedIdentityDao.h"
+#include "service/IdentityService.h"
+#include "transaction/Transactional.h"
+
+namespace BeeeOn {
+
+class IdentityServiceImpl : public IdentityService, public Transactional {
+public:
+	IdentityServiceImpl();
+
+	void setIdentityDao(IdentityDao *dao);
+	void setVerifiedIdentityDao(VerifiedIdentityDao *dao);
+
+	bool fetch(VerifiedIdentity &identity)
+	{
+		return BEEEON_TRANSACTION_RETURN(bool, doFetch(identity));
+	}
+
+	bool fetch(Identity &identity)
+	{
+		return BEEEON_TRANSACTION_RETURN(bool, doFetch(identity));
+	}
+
+protected:
+	bool doFetch(VerifiedIdentity &identity);
+	bool doFetch(Identity &identity);
+
+private:
+	IdentityDao *m_identityDao;
+	VerifiedIdentityDao *m_verifiedIdentityDao;
+};
+
+}
+
+#endif
+

server/conf/config.d/factory-services.xml
@@ -46,7 +46,7 @@
 			<set name="transactionManager" ref="transactionManager" />
 		</instance>
 
-		<instance name="identityService" class="BeeeOn::IdentityService">
+		<instance name="identityService" class="BeeeOn::IdentityServiceImpl">
 			<set name="identityDao" ref="identityDao" />
 			<set name="verifiedIdentityDao" ref="verifiedIdentityDao" />
 			<set name="transactionManager" ref="transactionManager" />

server/src/CMakeLists.txt
@@ -75,6 +75,7 @@ file(GLOB SOURCES
 	${PROJECT_SOURCE_DIR}/service/GatewayServiceImpl.cpp
 	${PROJECT_SOURCE_DIR}/service/LocationService.cpp
 	${PROJECT_SOURCE_DIR}/service/IdentityService.cpp
+	${PROJECT_SOURCE_DIR}/service/IdentityServiceImpl.cpp
 	${PROJECT_SOURCE_DIR}/service/RoleService.cpp
 	${PROJECT_SOURCE_DIR}/service/SensorHistoryService.cpp
 	${PROJECT_SOURCE_DIR}/service/JSONGatewayDeserializer.cpp

server/src/service/IdentityService.cpp
@@ -1,45 +1,7 @@
-#include "di/Injectable.h"
 #include "service/IdentityService.h"
-
-BEEEON_OBJECT_BEGIN(BeeeOn, IdentityService)
-BEEEON_OBJECT_REF("identityDao", &IdentityService::setIdentityDao)
-BEEEON_OBJECT_REF("verifiedIdentityDao", &IdentityService::setVerifiedIdentityDao)
-BEEEON_OBJECT_REF("transactionManager", &Transactional::setTransactionManager)
-BEEEON_OBJECT_END(BeeeOn, IdentityService)
 
 using namespace BeeeOn;
 
-IdentityService::IdentityService():
-	m_identityDao(&NullIdentityDao::instance()),
-	m_verifiedIdentityDao(&NullVerifiedIdentityDao::instance())
+IdentityService::~IdentityService()
 {
-}
-
-void IdentityService::setIdentityDao(IdentityDao *dao)
-{
-	if (dao == NULL)
-		m_identityDao = &NullIdentityDao::instance();
-	else
-		m_identityDao = dao;
-}
-
-void IdentityService::setVerifiedIdentityDao(VerifiedIdentityDao *dao)
-{
-	if (dao == NULL) {
-		m_verifiedIdentityDao =
-			&NullVerifiedIdentityDao::instance();
-	}
-	else {
-		m_verifiedIdentityDao = dao;
-	}
-}
-
-bool IdentityService::doFetch(VerifiedIdentity &identity)
-{
-	return m_verifiedIdentityDao->fetch(identity);
-}
-
-bool IdentityService::doFetch(Identity &identity)
-{
-	return m_identityDao->fetch(identity);
 }

server/src/service/IdentityService.h
@@ -1,36 +1,17 @@
 #ifndef BEEEON_IDENTITY_SERVICE_H
 #define BEEEON_IDENTITY_SERVICE_H
 
-#include "dao/IdentityDao.h"
-#include "dao/VerifiedIdentityDao.h"
-#include "transaction/Transactional.h"
-
 namespace BeeeOn {
 
-class IdentityService : public Transactional {
+class Identity;
+class VerifiedIdentity;
+
+class IdentityService {
 public:
-	IdentityService();
+	virtual ~IdentityService();
 
-	void setIdentityDao(IdentityDao *dao);
-	void setVerifiedIdentityDao(VerifiedIdentityDao *dao);
-
-	bool fetch(VerifiedIdentity &identity)
-	{
-		return BEEEON_TRANSACTION_RETURN(bool, doFetch(identity));
-	}
-
-	bool fetch(Identity &identity)
-	{
-		return BEEEON_TRANSACTION_RETURN(bool, doFetch(identity));
-	}
-
-protected:
-	bool doFetch(VerifiedIdentity &identity);
-	bool doFetch(Identity &identity);
-
-private:
-	IdentityDao *m_identityDao;
-	VerifiedIdentityDao *m_verifiedIdentityDao;
+	virtual bool fetch(VerifiedIdentity &identity) = 0;
+	virtual bool fetch(Identity &identity) = 0;
 };
 
 }

server/src/service/IdentityServiceImpl.cpp
@@ -1,0 +1,46 @@
+#include "di/Injectable.h"
+#include "service/IdentityServiceImpl.h"
+
+BEEEON_OBJECT_BEGIN(BeeeOn, IdentityServiceImpl)
+BEEEON_OBJECT_CASTABLE(IdentityService)
+BEEEON_OBJECT_REF("identityDao", &IdentityServiceImpl::setIdentityDao)
+BEEEON_OBJECT_REF("verifiedIdentityDao", &IdentityServiceImpl::setVerifiedIdentityDao)
+BEEEON_OBJECT_REF("transactionManager", &Transactional::setTransactionManager)
+BEEEON_OBJECT_END(BeeeOn, IdentityServiceImpl)
+
+using namespace BeeeOn;
+
+IdentityServiceImpl::IdentityServiceImpl():
+	m_identityDao(&NullIdentityDao::instance()),
+	m_verifiedIdentityDao(&NullVerifiedIdentityDao::instance())
+{
+}
+
+void IdentityServiceImpl::setIdentityDao(IdentityDao *dao)
+{
+	if (dao == NULL)
+		m_identityDao = &NullIdentityDao::instance();
+	else
+		m_identityDao = dao;
+}
+
+void IdentityServiceImpl::setVerifiedIdentityDao(VerifiedIdentityDao *dao)
+{
+	if (dao == NULL) {
+		m_verifiedIdentityDao =
+			&NullVerifiedIdentityDao::instance();
+	}
+	else {
+		m_verifiedIdentityDao = dao;
+	}
+}
+
+bool IdentityServiceImpl::doFetch(VerifiedIdentity &identity)
+{
+	return m_verifiedIdentityDao->fetch(identity);
+}
+
+bool IdentityServiceImpl::doFetch(Identity &identity)
+{
+	return m_identityDao->fetch(identity);
+}

commit c8d47f12bfc8856637388fd8616221e4ddbaddfc

IdentityServiceImpl: fix missing override keywords
Fixes: 70a75ac824da ("IdentityService: separate interface and implementation")
Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

server/src/service/IdentityServiceImpl.h
@@ -17,12 +17,12 @@ public:
 	void setVerifiedIdentityDao(VerifiedIdentityDao::Ptr dao);
 	void setAccessPolicy(IdentityAccessPolicy::Ptr accessPolicy);
 
-	bool fetch(Single<VerifiedIdentity> &input)
+	bool fetch(Single<VerifiedIdentity> &input) override
 	{
 		return BEEEON_TRANSACTION_RETURN(bool, doFetch(input));
 	}
 
-	bool fetch(Single<Identity> &input)
+	bool fetch(Single<Identity> &input) override
 	{
 		return BEEEON_TRANSACTION_RETURN(bool, doFetch(input));
 	}