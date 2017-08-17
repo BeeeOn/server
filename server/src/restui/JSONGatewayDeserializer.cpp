BeeeOn/server

commit ada56ab622497e6bfca4d3457567154439cd20c7

JSONGatewayDeserializer: new class
Implements deserialization of the Gateway according to the BeeeOn
Rest-UI API. It supports the following input formats:

1. GatewayRegister (full deserialization)

{
	"name": <String>,
	"altitude": <double|null>,
	"latitude": <double|null>,
	"longitude": <double|null>
}

Note that, the GatewayRegister format requires attribute "id". This
attribute must be parsed separately due to the GatewayService behaviour
(the Gateway instance must be created with a valid GatewayID.

2. GatewayUpdate (partial deserialization)

{
	"name": <String>,
	"altitude": <double|null>,
	"latitude": <double|null>,
	"longitude": <double|null>
}

The fields might be missing. The missing values are skipped (and thus
not updated).

Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

server/src/CMakeLists.txt
@@ -126,6 +126,7 @@ file(GLOB XMLUI_SOURCES
 
 file(GLOB RESTUI_SOURCES
 	restui/Serializing.cpp
+	restui/JSONGatewayDeserializer.cpp
 	ui/UIMockInit.cpp
 	dao/poco/PocoSQLiteConnectorLoader.cpp
 )

server/src/restui/JSONGatewayDeserializer.cpp
@@ -1,0 +1,54 @@
+#include <cmath>
+#include <Poco/Exception.h>
+
+#include "restui/JSONGatewayDeserializer.h"
+#include "util/JsonUtil.h"
+
+using namespace std;
+using namespace Poco;
+using namespace Poco::JSON;
+using namespace BeeeOn;
+
+JSONGatewayDeserializer::JSONGatewayDeserializer(
+		const Object::Ptr object):
+	m_object(object)
+{
+}
+
+void JSONGatewayDeserializer::partial(Gateway &gateway) const
+{
+	if (m_object->has("name"))
+		gateway.setName(JsonUtil::extract<string>(m_object, "name"));
+
+	if (m_object->has("longitude"))
+		gateway.setLongitude(JsonUtil::extract<double>(m_object, "longitude"));
+
+	if (m_object->has("latitude"))
+		gateway.setLatitude(JsonUtil::extract<double>(m_object, "latitude"));
+
+	if (m_object->has("altitude"))
+		gateway.setAltitude(JsonUtil::extract<double>(m_object, "altitude"));
+}
+
+void JSONGatewayDeserializer::full(Gateway &gateway) const
+{
+	if (!m_object->has("name"))
+		throw InvalidArgumentException("missing name for gateway");
+
+	gateway.setName(JsonUtil::extract<string>(m_object, "name"));
+
+	if (!m_object->has("longitude"))
+		gateway.setLongitude(NAN);
+	else
+		gateway.setLongitude(JsonUtil::extract<double>(m_object, "longitude"));
+
+	if (!m_object->has("latitude"))
+		gateway.setLatitude(NAN);
+	else
+		gateway.setLatitude(JsonUtil::extract<double>(m_object, "latitude"));
+
+	if (!m_object->has("altitude"))
+		gateway.setAltitude(NAN);
+	else
+		gateway.setAltitude(JsonUtil::extract<double>(m_object, "altitude"));
+}

server/src/restui/JSONGatewayDeserializer.h
@@ -1,0 +1,24 @@
+#ifndef BEEEON_JSON_GATEWAY_DESERIALIZER_H
+#define BEEEON_JSON_GATEWAY_DESERIALIZER_H
+
+#include <Poco/JSON/Object.h>
+
+#include "service/Deserializer.h"
+#include "model/Gateway.h"
+
+namespace BeeeOn {
+
+class JSONGatewayDeserializer : public Deserializer<Gateway> {
+public:
+	JSONGatewayDeserializer(const Poco::JSON::Object::Ptr object);
+
+	void partial(Gateway &gateway) const override;
+	void full(Gateway &gateway) const override;
+
+private:
+	const Poco::JSON::Object::Ptr m_object;
+};
+
+}
+
+#endif

commit c71ae63313c751a6d09178de577014dfe47f6f58

JSON*Deserializer: union accidentally created duplicates
The behaviour is unchanged however we do not use the JsonUtil because
it is unnecessary here. From the src/service/ implementations, we
preserve a richer set of constructors.

Fixes: ada56ab62249 ("JSONGatewayDeserializer: new class")
Fixes: d8aaa1811d29 ("LocationRestHandler: new class")
Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

server/src/CMakeLists.txt
@@ -84,8 +84,6 @@ file(GLOB SOURCES
 	${PROJECT_SOURCE_DIR}/service/RoleServiceImpl.cpp
 	${PROJECT_SOURCE_DIR}/service/SensorHistoryService.cpp
 	${PROJECT_SOURCE_DIR}/service/SensorHistoryServiceImpl.cpp
-	${PROJECT_SOURCE_DIR}/service/JSONGatewayDeserializer.cpp
-	${PROJECT_SOURCE_DIR}/service/JSONLocationDeserializer.cpp
 	${PROJECT_SOURCE_DIR}/policy/GatewayAccessPolicy.cpp
 	${PROJECT_SOURCE_DIR}/policy/IdentityAccessPolicy.cpp
 	${PROJECT_SOURCE_DIR}/policy/LocationAccessPolicy.cpp
@@ -158,7 +156,7 @@ file(GLOB XMLUI_SOURCES
 file(GLOB RESTUI_SOURCES
 	restui/Serializing.cpp
 	restui/JSONGatewayDeserializer.cpp
-	restui/JSONLocationDeseliazer.cpp
+	restui/JSONLocationDeserializer.cpp
 	restui/LocationRestHandler.cpp
 	restui/ProfileRestHandler.cpp
 	restui/GatewayRestHandler.cpp

server/src/restui/JSONGatewayDeserializer.cpp
@@ -1,54 +1,64 @@
-#include <cmath>
 #include <Poco/Exception.h>
+#include <Poco/JSON/Parser.h>
 
 #include "restui/JSONGatewayDeserializer.h"
-#include "util/JsonUtil.h"
 
 using namespace std;
 using namespace Poco;
 using namespace Poco::JSON;
 using namespace BeeeOn;
 
-JSONGatewayDeserializer::JSONGatewayDeserializer(
-		const Object::Ptr object):
-	m_object(object)
+JSONGatewayDeserializer::JSONGatewayDeserializer(const string &input)
+{
+	Parser parser;
+	m_data = parser.parse(input).extract<Object::Ptr>();
+}
+
+JSONGatewayDeserializer::JSONGatewayDeserializer(istream &input)
+{
+	Parser parser;
+	m_data = parser.parse(input).extract<Object::Ptr>();
+}
+
+JSONGatewayDeserializer::JSONGatewayDeserializer(const Object::Ptr data):
+	m_data(data)
 {
 }
 
 void JSONGatewayDeserializer::partial(Gateway &gateway) const
 {
-	if (m_object->has("name"))
-		gateway.setName(JsonUtil::extract<string>(m_object, "name"));
+	if (m_data->has("name"))
+		gateway.setName(m_data->getValue<string>("name"));
 
-	if (m_object->has("longitude"))
-		gateway.setLongitude(JsonUtil::extract<double>(m_object, "longitude"));
+	if (m_data->has("altitude"))
+		gateway.setAltitude(m_data->getValue<int>("altitude"));
 
-	if (m_object->has("latitude"))
-		gateway.setLatitude(JsonUtil::extract<double>(m_object, "latitude"));
+	if (m_data->has("latitude"))
+		gateway.setLatitude(m_data->getValue<double>("latitude"));
 
-	if (m_object->has("altitude"))
-		gateway.setAltitude(JsonUtil::extract<int>(m_object, "altitude"));
+	if (m_data->has("longitude"))
+		gateway.setLongitude(m_data->getValue<double>("longitude"));
 }
 
 void JSONGatewayDeserializer::full(Gateway &gateway) const
 {
-	if (!m_object->has("name"))
+	if (!m_data->has("name"))
 		throw InvalidArgumentException("missing name for gateway");
 
-	gateway.setName(JsonUtil::extract<string>(m_object, "name"));
+	gateway.setName(m_data->getValue<string>("name"));
 
-	if (!m_object->has("longitude"))
-		gateway.setLongitude(NAN);
-	else
-		gateway.setLongitude(JsonUtil::extract<double>(m_object, "longitude"));
-
-	if (!m_object->has("latitude"))
-		gateway.setLatitude(NAN);
-	else
-		gateway.setLatitude(JsonUtil::extract<double>(m_object, "latitude"));
-
-	if (!m_object->has("altitude"))
+	if (!m_data->has("altitude"))
 		gateway.setAltitude(0);
 	else
-		gateway.setAltitude(JsonUtil::extract<int>(m_object, "altitude"));
+		gateway.setAltitude(m_data->getValue<int>("altitude"));
+
+	if (!m_data->has("latitude"))
+		gateway.setLatitude(NAN);
+	else
+		gateway.setLatitude(m_data->getValue<double>("latitude"));
+
+	if (!m_data->has("longitude"))
+		gateway.setLongitude(NAN);
+	else
+		gateway.setLongitude(m_data->getValue<double>("longitude"));
 }

server/src/restui/JSONGatewayDeserializer.h
@@ -1,6 +1,7 @@
 #ifndef BEEEON_JSON_GATEWAY_DESERIALIZER_H
 #define BEEEON_JSON_GATEWAY_DESERIALIZER_H
 
+#include <istream>
 #include <Poco/JSON/Object.h>
 
 #include "service/Deserializer.h"
@@ -10,13 +11,15 @@ namespace BeeeOn {
 
 class JSONGatewayDeserializer : public Deserializer<Gateway> {
 public:
-	JSONGatewayDeserializer(const Poco::JSON::Object::Ptr object);
+	JSONGatewayDeserializer(const std::string &input);
+	JSONGatewayDeserializer(std::istream &input);
+	JSONGatewayDeserializer(const Poco::JSON::Object::Ptr data);
 
 	void partial(Gateway &gateway) const override;
 	void full(Gateway &gateway) const override;
 
 private:
-	const Poco::JSON::Object::Ptr m_object;
+	Poco::JSON::Object::Ptr m_data;
 };
 
 }

server/src/restui/JSONLocationDeserializer.cpp
@@ -1,30 +1,40 @@
-#include <cmath>
 #include <Poco/Exception.h>
+#include <Poco/JSON/Parser.h>
 
-#include "JSONLocationDeseliazer.h"
-#include "util/JsonUtil.h"
+#include "restui/JSONLocationDeserializer.h"
 
 using namespace std;
 using namespace Poco;
 using namespace Poco::JSON;
 using namespace BeeeOn;
 
-JSONLocationDeserializer::JSONLocationDeserializer(
-        const Object::Ptr object):
-        m_object(object)
+JSONLocationDeserializer::JSONLocationDeserializer(const string &input)
+{
+	Parser parser;
+	m_data = parser.parse(input).extract<Object::Ptr>();
+}
+
+JSONLocationDeserializer::JSONLocationDeserializer(istream &input)
+{
+	Parser parser;
+	m_data = parser.parse(input).extract<Object::Ptr>();
+}
+
+JSONLocationDeserializer::JSONLocationDeserializer(const Object::Ptr data):
+	m_data(data)
 {
 }
 
 void JSONLocationDeserializer::partial(Location &location) const
 {
-    if (m_object->has("name"))
-        location.setName(JsonUtil::extract<string>(m_object, "name"));
+	if (m_data->has("name"))
+		location.setName(m_data->getValue<string>("name"));
 }
 
 void JSONLocationDeserializer::full(Location &location) const
 {
-    if (!m_object->has("name"))
-        throw InvalidArgumentException("missing name for location");
+	if (!m_data->has("name"))
+		throw InvalidArgumentException("missing name for location");
 
-    location.setName(JsonUtil::extract<string>(m_object, "name"));
+	location.setName(m_data->getValue<string>("name"));
 }

server/src/restui/JSONLocationDeserializer.h
@@ -1,6 +1,7 @@
 #ifndef BEEEON_JSON_LOCATION_DESERIALIZER_H
 #define BEEEON_JSON_LOCATION_DESERIALIZER_H
 
+#include <istream>
 #include <Poco/JSON/Object.h>
 
 #include "service/Deserializer.h"
@@ -10,13 +11,15 @@ namespace BeeeOn {
 
 class JSONLocationDeserializer : public Deserializer<Location> {
 public:
-	JSONLocationDeserializer(const Poco::JSON::Object::Ptr object);
+	JSONLocationDeserializer(const std::string &input);
+	JSONLocationDeserializer(std::istream &input);
+	JSONLocationDeserializer(const Poco::JSON::Object::Ptr data);
 
 	void partial(Location &location) const override;
 	void full(Location &location) const override;
 
 private:
-	const Poco::JSON::Object::Ptr m_object;
+	Poco::JSON::Object::Ptr m_data;
 };
 
 }

server/src/service/JSONGatewayDeserializer.cpp
@@ -1,64 +1,0 @@
-#include <Poco/Exception.h>
-#include <Poco/JSON/Parser.h>
-
-#include "service/JSONGatewayDeserializer.h"
-
-using namespace std;
-using namespace Poco;
-using namespace Poco::JSON;
-using namespace BeeeOn;
-
-JSONGatewayDeserializer::JSONGatewayDeserializer(const string &input)
-{
-	Parser parser;
-	m_data = parser.parse(input).extract<Object::Ptr>();
-}
-
-JSONGatewayDeserializer::JSONGatewayDeserializer(istream &input)
-{
-	Parser parser;
-	m_data = parser.parse(input).extract<Object::Ptr>();
-}
-
-JSONGatewayDeserializer::JSONGatewayDeserializer(const Object::Ptr data):
-	m_data(data)
-{
-}
-
-void JSONGatewayDeserializer::partial(Gateway &gateway) const
-{
-	if (m_data->has("name"))
-		gateway.setName(m_data->getValue<string>("name"));
-
-	if (m_data->has("altitude"))
-		gateway.setAltitude(m_data->getValue<int>("altitude"));
-
-	if (m_data->has("latitude"))
-		gateway.setLatitude(m_data->getValue<double>("latitude"));
-
-	if (m_data->has("longitude"))
-		gateway.setLongitude(m_data->getValue<double>("longitude"));
-}
-
-void JSONGatewayDeserializer::full(Gateway &gateway) const
-{
-	if (!m_data->has("name"))
-		throw InvalidArgumentException("missing name for gateway");
-
-	gateway.setName(m_data->getValue<string>("name"));
-
-	if (!m_data->has("altitude"))
-		throw InvalidArgumentException("missing altitude for gateway");
-
-	gateway.setAltitude(m_data->getValue<int>("altitude"));
-
-	if (!m_data->has("latitude"))
-		throw InvalidArgumentException("missing latitude for gateway");
-
-	gateway.setLatitude(m_data->getValue<double>("latitude"));
-
-	if (!m_data->has("longitude"))
-		throw InvalidArgumentException("missing longitude for gateway");
-
-	gateway.setLongitude(m_data->getValue<double>("longitude"));
-}

server/src/service/JSONGatewayDeserializer.h
@@ -1,27 +1,0 @@
-#ifndef BEEEON_JSON_GATEWAY_DESERIALIZER_H
-#define BEEEON_JSON_GATEWAY_DESERIALIZER_H
-
-#include <istream>
-#include <Poco/JSON/Object.h>
-
-#include "service/Deserializer.h"
-#include "model/Gateway.h"
-
-namespace BeeeOn {
-
-class JSONGatewayDeserializer : public Deserializer<Gateway> {
-public:
-	JSONGatewayDeserializer(const std::string &input);
-	JSONGatewayDeserializer(std::istream &input);
-	JSONGatewayDeserializer(const Poco::JSON::Object::Ptr data);
-
-	void partial(Gateway &gateway) const override;
-	void full(Gateway &gateway) const override;
-
-private:
-	Poco::JSON::Object::Ptr m_data;
-};
-
-}
-
-#endif

server/src/service/JSONLocationDeserializer.cpp
@@ -1,40 +1,0 @@
-#include <Poco/Exception.h>
-#include <Poco/JSON/Parser.h>
-
-#include "service/JSONLocationDeserializer.h"
-
-using namespace std;
-using namespace Poco;
-using namespace Poco::JSON;
-using namespace BeeeOn;
-
-JSONLocationDeserializer::JSONLocationDeserializer(const string &input)
-{
-	Parser parser;
-	m_data = parser.parse(input).extract<Object::Ptr>();
-}
-
-JSONLocationDeserializer::JSONLocationDeserializer(istream &input)
-{
-	Parser parser;
-	m_data = parser.parse(input).extract<Object::Ptr>();
-}
-
-JSONLocationDeserializer::JSONLocationDeserializer(const Object::Ptr data):
-	m_data(data)
-{
-}
-
-void JSONLocationDeserializer::partial(Location &location) const
-{
-	if (m_data->has("name"))
-		location.setName(m_data->getValue<string>("name"));
-}
-
-void JSONLocationDeserializer::full(Location &location) const
-{
-	if (!m_data->has("name"))
-		throw InvalidArgumentException("missing name for location");
-
-	location.setName(m_data->getValue<string>("name"));
-}

server/src/service/JSONLocationDeserializer.h
@@ -1,27 +1,0 @@
-#ifndef BEEEON_JSON_LOCATION_DESERIALIZER_H
-#define BEEEON_JSON_LOCATION_DESERIALIZER_H
-
-#include <istream>
-#include <Poco/JSON/Object.h>
-
-#include "service/Deserializer.h"
-#include "model/Location.h"
-
-namespace BeeeOn {
-
-class JSONLocationDeserializer : public Deserializer<Location> {
-public:
-	JSONLocationDeserializer(const std::string &input);
-	JSONLocationDeserializer(std::istream &input);
-	JSONLocationDeserializer(const Poco::JSON::Object::Ptr data);
-
-	void partial(Location &location) const override;
-	void full(Location &location) const override;
-
-private:
-	Poco::JSON::Object::Ptr m_data;
-};
-
-}
-
-#endif