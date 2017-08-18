BeeeOn/server

commit 20a9d4692f439e27981bc8487f8a4058c92ab869

IcuTranslator: new class
Implement the Translator interface based on the icu library.
The resources with translations are represented by icu::ResourceBundle
instances. Each IcuTranslator is created for a certain locale and
bundle name. The icu library ensures the resource lookup and formatting.

The format arguments (Poco::Dynamic::Var) are on the fly converted
into icu::Formattable.

The ICU library (at least) 59+ creates a different date formats then
earlier versions.

Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

server/src/l10n/IcuTranslator.cpp
@@ -1,0 +1,217 @@
+#include <unicode/fmtable.h>
+#include <unicode/msgfmt.h>
+
+#include <Poco/DateTime.h>
+#include <Poco/Exception.h>
+#include <Poco/Logger.h>
+#include <Poco/Timestamp.h>
+
+#include "di/Injectable.h"
+#include "l10n/Locale.h"
+#include "l10n/IcuLocaleImpl.h"
+#include "l10n/IcuTranslator.h"
+#include "util/ClassInfo.h"
+
+BEEEON_OBJECT_BEGIN(BeeeOn, IcuTranslatorFactory)
+BEEEON_OBJECT_END(BeeeOn, IcuTranslatorFactory)
+
+using namespace std;
+using namespace Poco;
+using namespace Poco::Dynamic;
+using namespace BeeeOn;
+
+static void handleWarning(
+		Logger &logger,
+		const UErrorCode error,
+		const string &name,
+		const string &locale)
+{
+	if (logger.debug()) {
+		if (error == U_USING_FALLBACK_WARNING) {
+			logger.warning(string(u_errorName(error))
+				+ " for " + name + "/" + locale,
+				__FILE__, __LINE__);
+		}
+		else if (error == U_USING_DEFAULT_WARNING) {
+			logger.warning(string(u_errorName(error))
+				+ " for " + name + "/" + locale,
+				__FILE__, __LINE__);
+		}
+	}
+}
+
+void IcuTranslator::handleWarning(
+		const UErrorCode error,
+		const string &name,
+		const string &locale) const
+{
+	::handleWarning(logger(), error, name, locale);
+}
+
+static void handleError(
+		Logger &logger,
+		const UErrorCode error,
+		const string &name,
+		const string &locale)
+{
+	if (!U_SUCCESS(error)) {
+		string msg = string(u_errorName(error))
+			+ " for " + name + "/" + locale;
+
+		switch (error) {
+		case U_INVALID_FORMAT_ERROR:
+		case U_ILLEGAL_ARGUMENT_ERROR:
+		case U_MESSAGE_PARSE_ERROR:
+		case U_PARSE_ERROR:
+			throw InvalidArgumentException(msg, (int) error);
+
+		case U_INDEX_OUTOFBOUNDS_ERROR:
+			throw RangeException(msg, (int) error);
+
+		case U_MISSING_RESOURCE_ERROR:
+			throw NotFoundException(msg, (int) error);
+
+		default:
+			throw IllegalStateException(msg, (int) error);
+		}
+	}
+
+	::handleWarning(logger, error, name, locale);
+}
+
+void IcuTranslator::handleError(
+		const UErrorCode error,
+		const string &name,
+		const string &locale) const
+{
+	::handleError(logger(), error, name, locale);
+}
+
+string IcuTranslator::typeName(const UResType type) const
+{
+	switch (type) {
+	case URES_NONE:
+		return "none";
+	case URES_STRING:
+		return "string";
+	case URES_BINARY:
+		return "binary";
+	case URES_TABLE:
+		return "table";
+	case URES_INT:
+		return "int";
+	case URES_ARRAY:
+		return "array";
+	case URES_INT_VECTOR:
+		return "int-vector";
+	default:
+		return "<" + to_string((int) type) + ">";
+	}
+}
+
+static Formattable asFormattable(const Timestamp &ts)
+{
+	const double ms = ts.epochMicroseconds() / 1000.0;
+	return Formattable(ms, Formattable::kIsDate);
+}
+
+static Formattable asFormattable(const Var &var)
+{
+	if (var.isString())
+		return Formattable(var.convert<string>().c_str());
+
+	if (var.isBoolean())
+		return Formattable(var.convert<bool>());
+
+	if (var.isNumeric()) {
+		if (var.isInteger())
+			return Formattable(var.convert<long>());
+
+		return Formattable(var.convert<double>());
+	}
+
+	if (var.type() == typeid(Timestamp))
+		return asFormattable(var.extract<Timestamp>());
+
+	if (var.type() == typeid(DateTime))
+		return asFormattable(var.extract<DateTime>().timestamp());
+
+	const ClassInfo type(var.type());
+	throw InvalidArgumentException("cannot express " + type.name() + " as formattable");
+}
+
+IcuTranslator::IcuTranslator(Poco::SharedPtr<icu::ResourceBundle> bundle):
+	m_bundle(bundle)
+{
+}
+
+string IcuTranslator::applyArgs(
+	const icu::ResourceBundle &bundle,
+	const vector<Var> &args)
+{
+	UErrorCode error = U_ZERO_ERROR;
+	icu::UnicodeString pattern = bundle.getString(error);
+	handleError(error, bundle.getKey(), bundle.getName());
+
+	icu::MessageFormat format(pattern, bundle.getLocale(), error);
+	handleError(error, bundle.getKey(), bundle.getName());
+
+	std::vector<icu::Formattable> f;
+
+	for (const auto &arg : args)
+		f.push_back(asFormattable(arg));
+
+	icu::UnicodeString result;
+	icu::FieldPosition ignore;
+	format.format(f.data(), (int) args.size(), result, ignore, error);
+	handleError(error, bundle.getKey(), bundle.getName());
+
+	string utf8;
+	return result.toUTF8String(utf8);
+}
+
+string IcuTranslator::formatImpl(
+		const string &key,
+		const vector<Var> &args)
+{
+	UErrorCode error = U_ZERO_ERROR;
+	icu::ResourceBundle value = m_bundle->get(key.c_str(), error);
+	handleError(error, key, m_bundle->getName());
+
+	switch (value.getType()) {
+	case URES_STRING:
+		return applyArgs(value, args);
+
+	default:
+		logger().critical(
+			"requested resource "
+			+ key
+			+ " is of type "
+			+ typeName(value.getType()),
+			__FILE__, __LINE__);
+
+		return NoTranslator::formatImpl(key, args);
+	}
+}
+
+static IcuLocaleImpl asIcuLocale(const BeeeOn::Locale &locale)
+{
+	SharedPtr<IcuLocaleImpl> impl = locale.impl().cast<IcuLocaleImpl>();
+	if (!impl.isNull())
+		return *impl.get();
+	else
+		return IcuLocaleImpl::parse(locale.toString());
+}
+
+Translator *IcuTranslatorFactory::create(
+		const Locale &loc,
+		const string &name)
+{
+	const IcuLocaleImpl &locale = asIcuLocale(loc);
+
+	UErrorCode error = U_ZERO_ERROR;
+	icu::ResourceBundle bundle(name.c_str(), locale.icu(), error);
+	handleError(Loggable::forInstance(this), error, name, locale.toString());
+
+	return new IcuTranslator(bundle.clone());
+}

server/src/CMakeLists.txt
@@ -33,6 +33,7 @@ file(GLOB SOURCES
 	${PROJECT_SOURCE_DIR}/server/XmlRequestHandler.cpp
 	${PROJECT_SOURCE_DIR}/server/SocketServer.cpp
 	${PROJECT_SOURCE_DIR}/l10n/IcuLocaleImpl.cpp
+	${PROJECT_SOURCE_DIR}/l10n/IcuTranslator.cpp
 	${PROJECT_SOURCE_DIR}/model/Identity.cpp
 	${PROJECT_SOURCE_DIR}/model/VerifiedIdentity.cpp
 	${PROJECT_SOURCE_DIR}/model/RoleInGateway.cpp

server/src/l10n/IcuTranslator.h
@@ -1,0 +1,60 @@
+#ifndef BEEEON_ICU_TRANSLATOR_H
+#define BEEEON_ICU_TRANSLATOR_H
+
+#include <unicode/resbund.h>
+
+#include <Poco/SharedPtr.h>
+
+#include "l10n/NoTranslator.h"
+#include "util/Loggable.h"
+
+namespace BeeeOn {
+
+/**
+ * Utilize icu::ResourceBundle and icu::MessageFormat for localization
+ * of messages. Each message is identified by a key and accepts a number
+ * of arguments. The arguments are formatted into the message matching
+ * the key and translator's locale. The formatting rules are described
+ * at the following site:
+ *
+ *  http://icu-project.org/apiref/icu4c/classicu_1_1MessageFormat.html
+ */
+class IcuTranslator : public NoTranslator, Loggable {
+public:
+	IcuTranslator(Poco::SharedPtr<icu::ResourceBundle> bundle);
+
+protected:
+	std::string formatImpl(
+		const std::string &key,
+		const std::vector<Poco::Dynamic::Var> &args) override;
+
+	std::string applyArgs(
+		const icu::ResourceBundle &bundle,
+		const std::vector<Poco::Dynamic::Var> &args);
+
+	void handleError(
+		const UErrorCode error,
+		const std::string &name,
+		const std::string &locale) const;
+
+	void handleWarning(
+		const UErrorCode error,
+		const std::string &name,
+		const std::string &locale) const;
+
+	std::string typeName(const UResType type) const;
+
+private:
+	Poco::SharedPtr<icu::ResourceBundle> m_bundle;
+};
+
+class IcuTranslatorFactory : public TranslatorFactory {
+public:
+	Translator *create(
+		const Locale &locale,
+		const std::string &name = "") override;
+};
+
+}
+
+#endif

server/test/CMakeLists.txt
@@ -14,6 +14,7 @@ find_library (ICU_UC icuuc)
 
 file(GLOB TEST_SOURCES
 	${PROJECT_SOURCE_DIR}/l10n/IcuLocaleImplTest.cpp
+	${PROJECT_SOURCE_DIR}/l10n/IcuTranslatorTest.cpp
 	${PROJECT_SOURCE_DIR}/model/DevicePropertyTest.cpp
 	${PROJECT_SOURCE_DIR}/server/SessionManagerTest.cpp
 	${PROJECT_SOURCE_DIR}/service/AuthServiceImplTest.cpp
@@ -47,6 +48,9 @@ exec_program(
 	ARGS
 	-d l10n/test
 	l10n/test/res_index.txt
+	l10n/test/root.txt
+	l10n/test/en.txt
+	l10n/test/cs.txt
 )
 
 set(LIBS

server/test/l10n/IcuTranslatorTest.cpp
@@ -1,0 +1,169 @@
+#include <unicode/uvernum.h>
+
+#include <cppunit/extensions/HelperMacros.h>
+
+#include <Poco/Timestamp.h>
+#include <Poco/DateTime.h>
+
+#include "cppunit/BetterAssert.h"
+#include "l10n/IcuLocaleImpl.h"
+#include "l10n/IcuTranslator.h"
+#include "l10n/Locale.h"
+
+using namespace std;
+using namespace Poco;
+
+namespace BeeeOn {
+
+class IcuTranslatorTest : public CppUnit::TestFixture {
+	CPPUNIT_TEST_SUITE(IcuTranslatorTest);
+	CPPUNIT_TEST(testCreateNonExisting);
+	CPPUNIT_TEST(testCreateExisting);
+	CPPUNIT_TEST(testFormatMissing);
+	CPPUNIT_TEST(testFormatSimple);
+	CPPUNIT_TEST(testFormatDateTime);
+	CPPUNIT_TEST_SUITE_END();
+
+public:
+	IcuTranslatorTest();
+	void setUp();
+
+	void testAvailableLocales();
+	void testCreateNonExisting();
+	void testCreateExisting();
+	void testFormatMissing();
+	void testFormatSimple();
+	void testFormatDateTime();
+
+private:
+	IcuTranslatorFactory m_factory;
+	const Locale root;
+	const Locale en_US;
+	const Locale en_GB;
+	const Locale cs_CZ;
+};
+
+CPPUNIT_TEST_SUITE_REGISTRATION(IcuTranslatorTest);
+
+IcuTranslatorTest::IcuTranslatorTest():
+	root(new IcuLocaleImpl),
+	en_US(new IcuLocaleImpl("en", "US")),
+	en_GB(new IcuLocaleImpl("en", "GB")),
+	cs_CZ(new IcuLocaleImpl("cs", "CZ"))
+{
+}
+
+void IcuTranslatorTest::setUp()
+{
+	// Set environment variable ICU_DATA or assume we are
+	// running tests from the root of the project.
+	//
+	// The bundles are expected to be named as
+	//
+	//   test/l10n/test/<locale>.res
+	//
+	if (!strcmp(u_getDataDirectory(), ""))
+		u_setDataDirectory("test/l10n");
+}
+
+void IcuTranslatorTest::testCreateNonExisting()
+{
+	Locale locale(new IcuLocaleImpl);
+
+	CPPUNIT_ASSERT_THROW(
+		m_factory.create(locale, "non-existing"),
+		NotFoundException
+	);
+}
+
+void IcuTranslatorTest::testCreateExisting()
+{
+	SharedPtr<Translator> translator;
+
+	CPPUNIT_ASSERT_NO_THROW(translator = m_factory.create(root, "test"));
+	CPPUNIT_ASSERT_EQUAL("hello", translator->format("hello"));
+
+	CPPUNIT_ASSERT_NO_THROW(translator = m_factory.create(en_US, "test"));
+	CPPUNIT_ASSERT_EQUAL("Hello", translator->format("hello"));
+
+	CPPUNIT_ASSERT_NO_THROW(translator = m_factory.create(en_GB, "test"));
+	CPPUNIT_ASSERT_EQUAL("Hello", translator->format("hello"));
+
+	CPPUNIT_ASSERT_NO_THROW(translator = m_factory.create(cs_CZ, "test"));
+	CPPUNIT_ASSERT_EQUAL("Ahoj", translator->format("hello"));
+}
+
+void IcuTranslatorTest::testFormatMissing()
+{
+	SharedPtr<Translator> translator;
+
+	CPPUNIT_ASSERT_NO_THROW(translator = m_factory.create(root, "test"));
+	CPPUNIT_ASSERT_THROW(
+		translator->format("helloX"),
+		NotFoundException
+	);
+
+	// fallback to root locale
+	CPPUNIT_ASSERT_NO_THROW(translator = m_factory.create(en_US, "test"));
+	CPPUNIT_ASSERT_EQUAL("hello2", translator->format("hello2"));
+
+	// fallback to root locale
+	CPPUNIT_ASSERT_NO_THROW(translator = m_factory.create(cs_CZ, "test"));
+	CPPUNIT_ASSERT_EQUAL("hello2", translator->format("hello2"));
+}
+
+void IcuTranslatorTest::testFormatSimple()
+{
+	SharedPtr<Translator> translator;
+	CPPUNIT_ASSERT_NO_THROW(translator = m_factory.create(root, "test"));
+
+	CPPUNIT_ASSERT_EQUAL("Any: 1", translator->format("fmtany", 1));
+	CPPUNIT_ASSERT_EQUAL("Any: x", translator->format("fmtany", "x"));
+	CPPUNIT_ASSERT_EQUAL("Any: 1", translator->format("fmtany", true));
+	CPPUNIT_ASSERT_EQUAL("Any: 0", translator->format("fmtany", false));
+	CPPUNIT_ASSERT_EQUAL("Any: 2.5", translator->format("fmtany", 2.5));
+
+	CPPUNIT_ASSERT_EQUAL("Int: 5", translator->format("fmtint", 5));
+	CPPUNIT_ASSERT_THROW(
+		translator->format("fmtint", "5"),
+		InvalidArgumentException
+	);
+
+	CPPUNIT_ASSERT_EQUAL("Str: text", translator->format("fmtstr", "text"));
+	CPPUNIT_ASSERT_EQUAL("Str: 4", translator->format("fmtstr", 4));
+}
+
+void IcuTranslatorTest::testFormatDateTime()
+{
+	SharedPtr<Translator> translator;
+	CPPUNIT_ASSERT_NO_THROW(translator = m_factory.create(root, "test"));
+
+	Timestamp epoch(0);
+#if U_ICU_VERSION_MAJOR_NUM >= 59
+	CPPUNIT_ASSERT_EQUAL("Date: 1970 M01 1", translator->format("fmtdate", epoch));
+#else
+	CPPUNIT_ASSERT_EQUAL("Date: 1970 1 1", translator->format("fmtdate", epoch));
+#endif
+	CPPUNIT_ASSERT_EQUAL("Time: 01:00:00", translator->format("fmttime", epoch));
+	CPPUNIT_ASSERT_EQUAL("Str: 1970-01-01 01:00", translator->format("fmtstr", epoch));
+
+	DateTime dt(epoch);
+#if U_ICU_VERSION_MAJOR_NUM >= 59
+	CPPUNIT_ASSERT_EQUAL("Date: 1970 M01 1", translator->format("fmtdate", dt));
+#else
+	CPPUNIT_ASSERT_EQUAL("Date: 1970 1 1", translator->format("fmtdate", dt));
+#endif
+	CPPUNIT_ASSERT_EQUAL("Time: 01:00:00", translator->format("fmttime", dt));
+	CPPUNIT_ASSERT_EQUAL("Str: 1970-01-01 01:00", translator->format("fmtstr", dt));
+
+	Timestamp recent(1503005688 * Timespan::SECONDS);
+#if U_ICU_VERSION_MAJOR_NUM >= 59
+	CPPUNIT_ASSERT_EQUAL("Date: 2017 M08 17", translator->format("fmtdate", recent));
+#else
+	CPPUNIT_ASSERT_EQUAL("Date: 2017 8 17", translator->format("fmtdate", recent));
+#endif
+	CPPUNIT_ASSERT_EQUAL("Time: 23:34:48", translator->format("fmttime", recent));
+	CPPUNIT_ASSERT_EQUAL("Str: 2017-08-17 23:34", translator->format("fmtstr", recent));
+}
+
+}

commit c49595ba351ffe88ab6eab3edd9922767e98eb9f

IcuTranslatorFactory: make castable to TranslatorFactory
Fixes: 20a9d4692f43 ("IcuTranslator: new class")
Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

server/src/l10n/IcuTranslator.cpp
@@ -13,6 +13,7 @@
 #include "util/ClassInfo.h"
 
 BEEEON_OBJECT_BEGIN(BeeeOn, IcuTranslatorFactory)
+BEEEON_OBJECT_CASTABLE(TranslatorFactory)
 BEEEON_OBJECT_TEXT("bundleName", &IcuTranslatorFactory::setBundleName)
 BEEEON_OBJECT_END(BeeeOn, IcuTranslatorFactory)
 