BeeeOn/server

commit 7d6aaaa2315cf559afe22fa135202ebf1146ef9f

LegacyGatewayRPC: new class
Provide a GatewayRPC implementation of legacy UI-AdaServer API. Each
call of the methods sendListen, unpairDevice and pingGateway create new
connection to the AdaServer using SocketRPCConnector.

Signed-off-by: Jozef Halaj <xhalaj03@stud.fit.vutbr.cz>
Acked-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

server/conf/config.d/factory-dao.xml
@@ -36,6 +36,10 @@
 			<set name="deviceDao" ref="deviceDao" />
 		</instance>
 
+		<instance name="legacyGatewayRPC" class="BeeeOn::LegacyGatewayRPC">
+			<set name="rpcConnector" ref="adaServerConnector" />
+		</instance>
+
 		<alias name="gatewayRPC" ref="fakeGatewayRPC" />
 
 		<!-- Data access layer -->

server/src/rpc/LegacyGatewayRPC.cpp
@@ -1,0 +1,178 @@
+#include <sstream>
+
+#include <Poco/Exception.h>
+#include <Poco/AutoPtr.h>
+#include <Poco/TeeStream.h>
+#include <Poco/StreamCopier.h>
+#include <Poco/SAX/AttributesImpl.h>
+#include <Poco/XML/XMLWriter.h>
+
+#include "di/Injectable.h"
+#include "rpc/LegacyGatewayRPC.h"
+#include "rpc/RPCConnector.h"
+
+using namespace std;
+using namespace Poco;
+using namespace Poco::XML;
+using namespace BeeeOn;
+
+LegacyGatewayRPC::LegacyGatewayRPC():
+	m_logStream(logger(), Poco::Message::PRIO_DEBUG)
+{
+}
+
+void LegacyGatewayRPC::sendListen(const Gateway &gateway)
+{
+	Mutex::ScopedLock guard(m_lock);
+	m_connector->open();
+
+	TeeOutputStream outStream(m_connector->send());
+	outStream.addStream(m_logStream);
+
+	XMLWriter xmlWriter(outStream, XMLWriter::CANONICAL_XML);
+	AttributesImpl attributes;
+
+	try {
+		xmlWriter.startDocument();
+		attributes.addAttribute("", "", "type", "", "listen");
+		xmlWriter.startElement("", "request", "", attributes);
+
+		attributes.clear();
+		attributes.addAttribute("", "", "id", "", gateway.id().toString());
+		xmlWriter.startElement("", "adapter", "", attributes);
+		xmlWriter.endElement("", "adapter", "");
+
+		xmlWriter.endElement("", "request", "");
+		xmlWriter.endDocument();
+
+		m_connector->flush();
+	}
+	catch (const Exception &e) {
+		m_logStream << endl;
+		m_connector->close();
+		e.rethrow();
+	}
+
+	m_logStream << endl;
+
+	stringstream response;
+	StreamCopier::copyStreamUnbuffered(m_connector->receive(), response);
+	m_connector->close();
+
+	parseResponse(response.str());
+}
+
+void LegacyGatewayRPC::unpairDevice(const Gateway &gateway, const Device &device)
+{
+	Mutex::ScopedLock guard(m_lock);
+	m_connector->open();
+
+	TeeOutputStream outStream(m_connector->send());
+	outStream.addStream(m_logStream);
+
+	XMLWriter xmlWriter(outStream, XMLWriter::CANONICAL_XML);
+	AttributesImpl attributes;
+
+	try {
+		xmlWriter.startDocument();
+		attributes.addAttribute("", "", "type", "", "delete");
+		xmlWriter.startElement("", "request", "", attributes);
+
+		attributes.clear();
+		attributes.addAttribute("", "", "id", "", device.id().toString());
+		attributes.addAttribute("", "", "onAdapter", "", gateway.id().toString());
+		xmlWriter.startElement("", "sensor", "", attributes);
+		xmlWriter.endElement("", "sensor", "");
+
+		xmlWriter.endElement("", "request", "");
+		xmlWriter.endDocument();
+
+		m_connector->flush();
+	}
+	catch (const Exception &e) {
+		m_logStream << endl;
+		m_connector->close();
+		e.rethrow();
+	}
+
+	m_logStream << endl;
+
+	stringstream response;
+	StreamCopier::copyStreamUnbuffered(m_connector->receive(), response);
+	m_connector->close();
+
+	parseResponse(response.str());
+}
+
+void LegacyGatewayRPC::pingGateway(const Gateway &gateway)
+{
+	Mutex::ScopedLock guard(m_lock);
+	m_connector->open();
+
+	TeeOutputStream outStream(m_connector->send());
+	outStream.addStream(m_logStream);
+
+	XMLWriter xmlWriter(outStream, XMLWriter::CANONICAL_XML);
+	AttributesImpl attributes;
+
+	try {
+		xmlWriter.startDocument();
+		attributes.addAttribute("", "", "type", "", "ping");
+		xmlWriter.startElement("", "request", "", attributes);
+
+		attributes.clear();
+		attributes.addAttribute("", "", "id", "", gateway.id().toString());
+		xmlWriter.startElement("", "adapter", "", attributes);
+		xmlWriter.endElement("", "adapter", "");
+
+		xmlWriter.endElement("", "request", "");
+		xmlWriter.endDocument();
+
+		m_connector->flush();
+	}
+	catch (const Exception &e) {
+		m_logStream << endl;
+		m_connector->close();
+		e.rethrow();
+	}
+
+	m_logStream << endl;
+
+	stringstream response;
+	StreamCopier::copyStreamUnbuffered(m_connector->receive(), response);
+	m_connector->close();
+
+	parseResponse(response.str());
+}
+
+void LegacyGatewayRPC::parseResponse(const string &response)
+{
+	if (response.length() == 0)
+		throw InvalidArgumentException("response is empty.");
+
+	if(logger().debug())
+		logger().debug(response);
+
+	AutoPtr<XML::Document> doc = m_xmlParser.parse(response);
+	string errorCode = doc->documentElement()->getAttribute("errorCode");
+
+	if (errorCode == "0")
+		return;
+	else if (errorCode == "1")
+		throw IllegalStateException("gateway was never connected to the server");
+	else if (errorCode == "2")
+		throw IllegalStateException("connection with gateway is broken");
+	else if (errorCode == "3")
+		throw IllegalStateException("error sending data to the gateway");
+	else if (errorCode == "4")
+		throw IllegalStateException("PING response has not arrived");
+	else if (errorCode == "5")
+		throw IllegalStateException("message is not supported by the gateway protocol");
+	else
+		throw IllegalStateException("illegal error code: " + errorCode);
+}
+
+BEEEON_OBJECT_BEGIN(BeeeOn, LegacyGatewayRPC)
+BEEEON_OBJECT_CASTABLE(GatewayRPC)
+BEEEON_OBJECT_REF("rpcConnector", &LegacyGatewayRPC::setRPCConnector)
+BEEEON_OBJECT_END(BeeeOn, LegacyGatewayRPC)

server/src/rpc/LegacyGatewayRPC.h
@@ -1,0 +1,39 @@
+#ifndef BEEEON_LEGACY_GATEWAY_RPC_H
+#define BEEEON_LEGACY_GATEWAY_RPC_H
+
+#include <Poco/LogStream.h>
+#include <Poco/Mutex.h>
+
+#include "rpc/GatewayRPC.h"
+#include "util/SecureXmlParser.h"
+#include "util/Loggable.h"
+
+namespace BeeeOn {
+
+class RPCConnector;
+
+class LegacyGatewayRPC: public GatewayRPC, public Loggable {
+public:
+	LegacyGatewayRPC();
+
+	void setRPCConnector(RPCConnector *connector)
+	{
+		m_connector = connector;
+	}
+
+	void sendListen(const Gateway &gateway) override;
+	void unpairDevice(const Gateway &gateway, const Device &device) override;
+	void pingGateway(const Gateway &gateway) override;
+
+private:
+	void parseResponse(const std::string &response);
+
+	Poco::LogStream m_logStream;
+	Poco::Mutex m_lock;
+	RPCConnector *m_connector;
+	SecureXmlParser m_xmlParser;
+};
+
+}
+
+#endif

server/conf/config.d/logging.ini
@@ -133,6 +133,9 @@ loggers.LoggingObserver.level = debug
 loggers.FakeGatewayRPC.name = BeeeOn::FakeGatewayRPC
 loggers.FakeGatewayRPC.level = debug
 
+loggers.LegacyGatewayRPC.name = BeeeOn::LegacyGatewayRPC
+loggers.LegacyGatewayRPC.level = debug
+
 ; XML-UI API
 
 loggers.XmlRequestHandler.name = BeeeOn::XmlRequestHandlerFactory

server/src/CMakeLists.txt
@@ -85,6 +85,7 @@ file(GLOB SOURCES
 	${PROJECT_SOURCE_DIR}/rpc/SocketRPCConnector.cpp
 	${PROJECT_SOURCE_DIR}/rpc/GatewayRPC.cpp
 	${PROJECT_SOURCE_DIR}/rpc/FakeGatewayRPC.cpp
+	${PROJECT_SOURCE_DIR}/rpc/LegacyGatewayRPC.cpp
 	${PROJECT_SOURCE_DIR}/notification/NotificationDispatcher.cpp
 	${PROJECT_SOURCE_DIR}/notification/FirstLoginNotification.cpp
 	${PROJECT_SOURCE_DIR}/notification/InvitedNotification.cpp

commit 1243779504595a4f1f34785419d33a593580d3c5

LegacyGatewayRPC: fix waiting for slow response
When receiving a response, it can come in very short byte amounts
sometimes. The current implementation would not properly read such
response. Fix this by waiting for the </reply> tag. Note that there
is probably no other way how to solve it because the communication
protocol does not provide any other metadata.

Fixes: 7d6aaaa2315c ("LegacyGatewayRPC: new class")
Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>
Reviewed-by: Jozef Halaj <xhalaj03@stud.fit.vutbr.cz>

server/conf/config.d/factory-dao.xml
@@ -43,6 +43,7 @@
 
 		<instance name="legacyGatewayRPC" class="BeeeOn::LegacyGatewayRPC">
 			<set name="rpcConnector" ref="adaServerConnector" />
+			<set name="responseTimeout" number="${ada.legacy.responseTimeout}" />
 		</instance>
 
 		<alias name="gatewayRPC" ref="fakeGatewayRPC" />

server/conf/config.d/server.ini
@@ -62,6 +62,8 @@ plan = db/sqitch.plan
 baseDir = ${application.configDir}../db
 
 [ada]
+; response timeout in ms
+legacy.responseTimeout = 1000
 connector.host = localhost
 connector.port = 7081
 ; receive timeout in microseconds

server/src/rpc/LegacyGatewayRPC.cpp
@@ -3,6 +3,8 @@
 #include <Poco/Exception.h>
 #include <Poco/AutoPtr.h>
 #include <Poco/TeeStream.h>
+#include <Poco/Timespan.h>
+#include <Poco/Timestamp.h>
 #include <Poco/StreamCopier.h>
 #include <Poco/SAX/AttributesImpl.h>
 #include <Poco/XML/XMLWriter.h>
@@ -17,8 +19,17 @@ using namespace Poco::XML;
 using namespace BeeeOn;
 
 LegacyGatewayRPC::LegacyGatewayRPC():
+	m_timeout(0, 1000),
 	m_logStream(logger(), Poco::Message::PRIO_DEBUG)
 {
+}
+
+void LegacyGatewayRPC::setResponseTimeout(int ms)
+{
+	if (ms < 0)
+		throw InvalidArgumentException("response timeout must be non-negative");
+
+	m_timeout = Timespan(0, ms * 1000);
 }
 
 void LegacyGatewayRPC::sendListen(const Gateway &gateway)
@@ -133,8 +144,31 @@ void LegacyGatewayRPC::pingGateway(const Gateway &gateway)
 string LegacyGatewayRPC::receiveResponse()
 {
 	stringstream response;
-	StreamCopier::copyStreamUnbuffered(m_connector->receive(), response);
-	m_connector->close();
+	Timestamp started;
+
+	while (started.elapsed() < m_timeout.totalMicroseconds()) {
+		StreamCopier::copyStreamUnbuffered(m_connector->receive(), response);
+
+		const string &content = response.str();
+		const size_t endTag = content.rfind("</reply>");
+
+		if (endTag == string::npos)
+			continue;
+
+		m_connector->close();
+
+		const size_t expectedEnd = endTag + string("</reply>").size();
+		if (expectedEnd < content.size()) {
+			logger().warning(
+				"unexpected content after </replay>: "
+				+ to_string(content.size() - expectedEnd),
+				__FILE__, __LINE__);
+
+			return response.str().substr(0, expectedEnd + 1);
+		}
+
+		break;
+	}
 
 	return response.str();
 }
@@ -169,4 +203,5 @@ void LegacyGatewayRPC::parseResponse(const string &response)
 BEEEON_OBJECT_BEGIN(BeeeOn, LegacyGatewayRPC)
 BEEEON_OBJECT_CASTABLE(GatewayRPC)
 BEEEON_OBJECT_REF("rpcConnector", &LegacyGatewayRPC::setRPCConnector)
+BEEEON_OBJECT_NUMBER("responseTimeout", &LegacyGatewayRPC::setResponseTimeout)
 BEEEON_OBJECT_END(BeeeOn, LegacyGatewayRPC)

server/src/rpc/LegacyGatewayRPC.h
@@ -3,6 +3,7 @@
 
 #include <Poco/LogStream.h>
 #include <Poco/Mutex.h>
+#include <Poco/Timespan.h>
 
 #include "rpc/GatewayRPC.h"
 #include "util/SecureXmlParser.h"
@@ -25,10 +26,13 @@ public:
 	void unpairDevice(const Gateway &gateway, const Device &device) override;
 	void pingGateway(const Gateway &gateway) override;
 
+	void setResponseTimeout(int ms);
+
 private:
 	std::string receiveResponse();
 	void parseResponse(const std::string &response);
 
+	Poco::Timespan m_timeout;
 	Poco::LogStream m_logStream;
 	Poco::Mutex m_lock;
 	RPCConnector *m_connector;