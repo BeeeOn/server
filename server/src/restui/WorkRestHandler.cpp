BeeeOn/server

commit 65443e4695431f0068edbdb6654baf02b72aa7df

WorkRestHandler: new class
Define REST-UI endpoints:

* work.detail -> GET /work/$work_id
* work.update -> PUT /work/$work_id
* work.remove -> DELETE /work/$work_id

The provided test checks behaviour with non-existing work instances.
There is currently no way in this API to create a work instance.

Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

server/src/restui/WorkRestHandler.cpp
@@ -1,0 +1,81 @@
+#include <Poco/Exception.h>
+#include <Poco/JSON/PrintHandler.h>
+
+#include "di/Injectable.h"
+#include "rest/RestFlow.h"
+#include "restui/Serializing.h"
+#include "restui/WorkRestHandler.h"
+
+BEEEON_OBJECT_BEGIN(BeeeOn, RestUI, WorkRestHandler)
+BEEEON_OBJECT_CASTABLE(RestHandler)
+BEEEON_OBJECT_REF("workService", &WorkRestHandler::setWorkService)
+BEEEON_OBJECT_END(BeeeOn, RestUI, WorkRestHandler)
+
+using namespace std;
+using namespace Poco;
+using namespace Poco::JSON;
+using namespace BeeeOn;
+using namespace BeeeOn::RestUI;
+
+WorkRestHandler::WorkRestHandler():
+	JSONRestHandler("work")
+{
+	registerAction<WorkRestHandler>("detail", &WorkRestHandler::detail, {"work_id"});
+	registerAction<WorkRestHandler>("update", &WorkRestHandler::update, {"work_id"});
+	registerAction<WorkRestHandler>("delete", &WorkRestHandler::remove, {"work_id"});
+}
+
+void WorkRestHandler::setWorkService(WorkService::Ptr service)
+{
+	m_service = service;
+}
+
+void WorkRestHandler::detail(RestFlow &flow)
+{
+	Work work(WorkID::parse(flow.param("work_id")));
+	Single<Work> input(work);
+	User user(flow.session()->userID());
+	input.setUser(user);
+
+	if (!m_service->fetch(input))
+		throw NotFoundException("no such work " + work);
+
+	PrintHandler result(flow.response().stream());
+	beginSuccess(result, 200);
+	endSuccess(result);
+}
+
+void WorkRestHandler::update(RestFlow &flow)
+{
+	Work work(WorkID::parse(flow.param("work_id")));
+	Single<Work> input(work);
+	User user(flow.session()->userID());
+	input.setUser(user);
+
+	const Object::Ptr object = parseInput(flow);
+
+	const string &status = object->getValue<string>("status");
+	if (status == "canceled")
+		m_service->cancel(input);
+	else
+		throw InvalidArgumentException("unexpected status to update");
+
+	PrintHandler result(flow.response().stream());
+	beginSuccess(result, 200);
+	serialize(result, work);
+	endSuccess(result);
+}
+
+void WorkRestHandler::remove(RestFlow &flow)
+{
+	Work work(WorkID::parse(flow.param("work_id")));
+	Single<Work> input(work);
+	User user(flow.session()->userID());
+	input.setUser(user);
+
+	m_service->remove(input);
+
+	PrintHandler result(flow.response().stream());
+	beginSuccess(result, 204);
+	endSuccess(result);
+}

server/conf/config.d/factory-restui.xml
@@ -26,6 +26,7 @@
 			<add name="handlers" ref="uiProfileHandler" />
 			<add name="handlers" ref="uiLocationHandler" />
 			<add name="handlers" ref="uiRoleHandler" />
+			<add name="handlers" ref="uiWorkHandler" />
 		</instance>
 
 		<instance name="uiBuiltinHandler" class="BeeeOn::BuiltinRestHandler" />
@@ -49,6 +50,10 @@
 		<instance name="uiRoleHandler" class="BeeeOn::RestUI::RoleRestHandler">
 			<set name="roleService" ref="roleService" />
 			<set name="identityService" ref="identityService" />
+		</instance>
+
+		<instance name="uiWorkHandler" class="BeeeOn::RestUI::WorkRestHandler">
+			<set name="workService" ref="workService" />
 		</instance>
 	</factory>
 </system>

server/conf/restui-api.ini
@@ -77,3 +77,11 @@ api.call.roles.update.method = PUT
 
 api.call.roles.delete.uri = /gateways/$gateway_id/roles/$role_id
 api.call.roles.delete.method = DELETE
+
+api.call.work.detail.uri = /work/$work_id
+
+api.call.work.update.uri = /work/$work_id
+api.call.work.update.method = PUT
+
+api.call.work.delete.uri = /work/$work_id
+api.call.work.delete.method = DELETE

server/src/CMakeLists.txt
@@ -191,6 +191,7 @@ file(GLOB RESTUI_SOURCES
 	restui/ProfileRestHandler.cpp
 	restui/RoleRestHandler.cpp
 	restui/GatewayRestHandler.cpp
+	restui/WorkRestHandler.cpp
 	dao/poco/PocoSQLiteConnectorLoader.cpp
 )
 

server/src/restui/Serializing.cpp
@@ -10,6 +10,7 @@
 #include "model/User.h"
 #include "model/VerifiedIdentity.h"
 #include "restui/Serializing.h"
+#include "work/Work.h"
 
 using namespace std;
 using namespace Poco;
@@ -196,4 +197,56 @@ void BeeeOn::RestUI::serialize(PrintHandler &output, const std::vector<RoleInGat
 		serialize(output, role);
 
 	output.endArray();
+}
+
+void BeeeOn::RestUI::serialize(PrintHandler &output, const Work &work)
+{
+	output.startObject();
+
+	output.key("id");
+	output.value(work.id().toString());
+
+	output.key("status");
+
+	switch (work.state()) {
+	case Work::STATE_IDLE:
+		output.value("idle");
+		break;
+	case Work::STATE_SCHEDULED:
+		output.value("sleeping");
+		break;
+	case Work::STATE_EXECUTED:
+		output.value("processing");
+		break;
+	case Work::STATE_FINISHED:
+		output.value("successful");
+		break;
+	case Work::STATE_FAILED:
+		output.value("failed");
+		break;
+	case Work::STATE_CANCELED:
+		output.value("canceled");
+		break;
+	default:
+		throw IllegalStateException("invalid work state");
+	}
+
+	output.key("created");
+	output.value(work.created().epochTime());
+
+	if (Work::timestampValid(work.finished())) {
+		output.key("finished");
+		output.value(work.finished().epochTime());
+	}
+
+	Nullable<Timestamp> activationTime = work.activationTime();
+	if (!activationTime.isNull()) {
+		output.key("next_wakeup");
+		output.value(activationTime.value().epochTime());
+	}
+
+	output.key("owner_id");
+	output.value(work.owner().id().toString());
+
+	output.endObject();
 }

server/src/restui/Serializing.h
@@ -19,6 +19,7 @@ class LegacyRoleInGateway;
 class Location;
 class VerifiedIdentity;
 class RoleInGateway;
+class Work;
 
 namespace RestUI {
 
@@ -36,6 +37,8 @@ void serialize(Poco::JSON::PrintHandler  &output, const std::vector <Location> &
 void serialize(Poco::JSON::PrintHandler &output, const RoleInGateway &role);
 void serialize(Poco::JSON::PrintHandler &output, const LegacyRoleInGateway &role);
 void serialize(Poco::JSON::PrintHandler  &output, const std::vector <RoleInGateway> &roles);
+
+void serialize(Poco::JSON::PrintHandler &output, const Work &work);
 
 }
 

server/src/restui/WorkRestHandler.h
@@ -1,0 +1,27 @@
+#ifndef BEEEON_GATEWAY_REST_HANDLER_H
+#define BEEEON_GATEWAY_REST_HANDLER_H
+
+#include "rest/JSONRestHandler.h"
+#include "service/WorkService.h"
+
+namespace BeeeOn {
+namespace RestUI {
+
+class WorkRestHandler : public JSONRestHandler {
+public:
+	WorkRestHandler();
+
+	void setWorkService(WorkService::Ptr service);
+
+	void detail(RestFlow &flow);
+	void update(RestFlow &flow);
+	void remove(RestFlow &flow);
+
+private:
+	WorkService::Ptr m_service;
+};
+
+}
+}
+
+#endif

server/t/restui/t1003-work-access.py
@@ -1,0 +1,76 @@
+#! /usr/bin/env python3
+
+import config
+config.import_libs()
+
+import unittest
+import json
+
+from rest import POST, PUT, GET, DELETE
+
+class TestWorkAccess(unittest.TestCase):
+	"""
+	Create a session for testing.
+	"""
+	def setUp(self):
+		req = POST(config.ui_host, config.ui_port, "/auth")
+		req.body(config.PERMIT_LOGIN)
+		response, content = req()
+
+		self.assertEqual(200, response.status)
+
+		result = json.loads(content)
+		self.assertEqual("success", result["status"])
+
+		self.session = result["data"]["id"]
+
+	def tearDown(self):
+		req = DELETE(config.ui_host, config.ui_port, "/auth")
+		req.authorize(self.session)
+		response, content = req()
+
+		self.assertEqual(204, response.status)
+
+	"""
+	Reading any non-existing work leads to 404.
+	"""
+	def test1_get_non_existing(self):
+		req = GET(config.ui_host, config.ui_port, "/work/4891a1ec-2939-49f8-9207-4ea9f0499934")
+		req.authorize(self.session)
+
+		response, content = req()
+		result = json.loads(content)
+		self.assertEqual(404, response.status)
+		self.assertEqual("error", result["status"])
+
+	"""
+	Canceling any non-existing work leads to 404.
+	"""
+	def test2_cancel_non_existing(self):
+		req = PUT(config.ui_host, config.ui_port, "/work/4891a1ec-2939-49f8-9207-4ea9f0499934")
+		req.authorize(self.session)
+		req.body(json.dumps({
+			"status": "canceled"
+		}))
+
+		response, content = req()
+		result = json.loads(content)
+		self.assertEqual(404, response.status)
+		self.assertEqual("error", result["status"])
+
+	"""
+	Remove any non-existing work leads to 404.
+	"""
+	def test3_remove_non_existing(self):
+		req = DELETE(config.ui_host, config.ui_port, "/work/4891a1ec-2939-49f8-9207-4ea9f0499934")
+		req.authorize(self.session)
+
+		response, content = req()
+		result = json.loads(content)
+		self.assertEqual(404, response.status)
+		self.assertEqual("error", result["status"])
+
+if __name__ == '__main__':
+	import sys
+	import taprunner
+	unittest.main(testRunner=taprunner.TAPTestRunner(stream = sys.stdout))

commit 9fbbc0709bf6c8f16ecff9a215c1ee7040ef4bbf

WorkRestHandler: fix sending empty body in remove()
Fixes: 65443e469543 ("WorkRestHandler: new class")
Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

server/src/restui/WorkRestHandler.cpp
@@ -76,7 +76,5 @@ void WorkRestHandler::remove(RestFlow &flow)
 
 	m_service->remove(input);
 
-	PrintHandler result(flow.response().stream());
-	beginSuccess(result, 204);
-	endSuccess(result);
+	flow.response().setStatus(204);
 }