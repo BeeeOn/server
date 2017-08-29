BeeeOn/server

commit ca5a82e50536624f36aec013bed17b2da54a1cea

DeviceUnpairWork: rewrite to by fully asynchronous
The DeviceUnpairWork unpairs a remote device. It calls the GatewayRPC and expects
an asynchronous behaviour. I.e. it must call the ResultCall (lambda) in a different
thread (to avoid mutex deadlocks).

The implementation does not poll database anymore. It however performs a single
database access to assure that the device that is being unpaired still exists
in database. This may prevent some never-ending work under few unexpected
circumstances.

The ResultCall can be called repeatedly with new status information. The result
status is saved inside the DeviceUnpairWork (inside the Work instance). However,
no persistence is performed in this step. There is currently no facility to
handle it. (It might be probably added into the ExtendedWorkExecutor later).

When the result status is changed, the DeviceUnpairWork can be woken up.
In case of PENDING and ACCEPTED, no wakeup is done. Just, the result status
is updated inside the DeviceUnpairWork.

Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>
Reviewed-by: Jozef Halaj <xhalaj03@stud.fit.vutbr.cz>

server/src/work/DeviceUnpairWorkExecutor.cpp
@@ -2,6 +2,7 @@
 
 #include <Poco/Event.h>
 #include <Poco/Exception.h>
+#include <Poco/Net/NetException.h>
 
 #include "dao/DeviceDao.h"
 #include "di/Injectable.h"
@@ -12,18 +13,16 @@ BEEEON_OBJECT_BEGIN(BeeeOn, DeviceUnpairWorkExecutor)
 BEEEON_OBJECT_CASTABLE(WorkExecutor)
 BEEEON_OBJECT_REF("deviceDao", &DeviceUnpairWorkExecutor::setDeviceDao)
 BEEEON_OBJECT_REF("gatewayRPC", &DeviceUnpairWorkExecutor::setGatewayRPC)
-BEEEON_OBJECT_NUMBER("pollSleep", &DeviceUnpairWorkExecutor::setPollSleep)
-BEEEON_OBJECT_NUMBER("maxAttempts", &DeviceUnpairWorkExecutor::setMaxAttempts)
+BEEEON_OBJECT_REF("scheduler", &DeviceUnpairWorkExecutor::setScheduler)
 BEEEON_OBJECT_END(BeeeOn, DeviceUnpairWorkExecutor)
 
 using namespace std;
 using namespace Poco;
+using namespace Poco::Net;
 using namespace BeeeOn;
 
 DeviceUnpairWorkExecutor::DeviceUnpairWorkExecutor():
-	m_rpc(&NullGatewayRPC::instance()),
-	m_pollSleep(800),
-	m_maxAttempts(5)
+	m_rpc(&NullGatewayRPC::instance())
 {
 }
 
@@ -37,22 +36,6 @@ void DeviceUnpairWorkExecutor::setGatewayRPC(GatewayRPC *rpc)
 	m_rpc = rpc == NULL? &NullGatewayRPC::instance() : rpc;
 }
 
-void DeviceUnpairWorkExecutor::setPollSleep(int ms)
-{
-	if (ms < 0)
-		throw InvalidArgumentException("poll sleep must be non-negative");
-
-	m_pollSleep = ms * 1000;
-}
-
-void DeviceUnpairWorkExecutor::setMaxAttempts(int count)
-{
-	if (count < 0)
-		throw InvalidArgumentException("attempts count must be positive");
-
-	m_maxAttempts = count;
-}
-
 bool DeviceUnpairWorkExecutor::accepts(const Work::Ptr work) const
 {
 	if (work->content().type().is<DeviceUnpairWork>())
@@ -61,46 +44,30 @@ bool DeviceUnpairWorkExecutor::accepts(const Work::Ptr work) const
 	return false;
 }
 
-bool DeviceUnpairWorkExecutor::done(Device &device)
+void DeviceUnpairWorkExecutor::processResult(Work::Ptr work, DeviceUnpairWork &content)
 {
-	const Gateway &gateway = device.gateway();
+	Gateway gateway(content.gatewayID());
+	Device device(content.deviceID());
 
-	if (!m_dao->fetch(device, gateway)) {
-		if (logger().debug()) {
-			logger().debug("device " + device
-				+ " was not found in gateway " + gateway,
-				__FILE__, __LINE__);
-		}
+	switch (content.result()) {
+	case GatewayRPCResult::NOT_CONNECTED:
+		throw ConnectionAbortedException(
+			"failed to unpair device " + device + " on gateway " + gateway);
 
-		return true;
-	}
+	case GatewayRPCResult::TIMEOUT:
+		throw TimeoutException(
+			"failed to unpair device " + device + " on gateway " + gateway);
 
-	if (device.activeSince().isNull()) {
-		if (logger().debug()) {
-			logger().debug("device " + device
-				+ " is inactive in gateway " + gateway,
-				__FILE__, __LINE__);
-		}
+	case GatewayRPCResult::FAILED:
+		throw RuntimeException(
+			"failed to unpair device " + device + " on gateway " + gateway);
 
-		return true;
-	}
+	case GatewayRPCResult::SUCCESS:
+		return;
 
-	return false;
-}
-
-void DeviceUnpairWorkExecutor::checkAttempts(const DeviceUnpairWork &work)
-{
-	if (work.attempt() >= m_maxAttempts) {
-		if (logger().debug()) {
-			logger().debug(
-				"failed to unpair device " + work.deviceID().toString()
-				+ " after " + to_string(m_maxAttempts)
-				+ " attempts",
-				__FILE__, __LINE__);
-		}
-
-		throw TimeoutException("failed to unpair device "
-				+ work.deviceID().toString());
+	default:
+		suspend(work);
+		break;
 	}
 }
 
@@ -109,66 +76,48 @@ void DeviceUnpairWorkExecutor::execute(Work::Ptr work)
 	DeviceUnpairWork content = work->contentAs<DeviceUnpairWork>();
 
 	Device device(content.deviceID());
-	device.setGateway(Gateway(content.gatewayID()));
+	const Gateway gateway(content.gatewayID());
 
-	if (done(device))
-		return;
-
-	const Gateway &gateway = device.gateway();
-
-	checkAttempts(content);
-	content.setAttempt(content.attempt() + 1);
-
-	try {
-		//TODO this is temporary solution intended to use with blocking
-		// synchronous RPC like LegacyGatewayRPC
-
-		Event event;
-		GatewayRPCResult::Ptr localResult;
-
-		m_rpc->unpairDevice([&](GatewayRPCResult::Ptr result)
-			{
-				localResult = result;
-				event.set();
-			},
-			gateway,
-			device
-		);
-
-		while (1) {
-			event.wait();
-
-			switch (localResult->status()) {
-				case GatewayRPCResult::PENDING:
-				case GatewayRPCResult::ACCEPTED:
-					break;
-				case GatewayRPCResult::NOT_CONNECTED:
-					throw NotFoundException("gateway "
-							+ gateway.id().toString()
-							+ " is not connected");
-				case GatewayRPCResult::TIMEOUT:
-					throw TimeoutException("no response from gateway "
-							+ gateway.id().toString());
-				case GatewayRPCResult::FAILED:
-					throw Exception("unpair device "
-							+ device.id().toString()
-							+ " failed on gateway "
-							+ gateway.id().toString());
-				case GatewayRPCResult::SUCCESS:
-					return;
-			}
-		}
-
-	} catch (const Poco::Exception &e) {
-		logger().log(e, __FILE__, __LINE__);
-		// we will try again after suspend
+	if (!m_dao->fetch(device, gateway)) {
+		logger().warning("device "
+				+ device
+				+ " on gateway "
+				+ gateway
+				+ " not found during unpair",
+				__FILE__, __LINE__);
+		return; // treat as success
 	}
 
-	Timespan sleep(m_pollSleep * 1000);
-	if (device.refresh() >= Timespan(1, 0))
-		sleep = device.refresh();
+	if (content.hasResult()) {
+		processResult(work, content);
+		return;
+	}
 
-	work->setContent(content);
-	suspend(work, sleep);
+	WorkScheduler::Ptr scheduler = this->scheduler();
+	WorkLockManager::Ptr lockManager = this->lockManager();
 
+	m_rpc->unpairDevice([=](GatewayRPCResult::Ptr result) mutable
+		{
+			WorkWriteGuard accessGuard(lockManager->readWrite(work->id()));
+
+			// update result of the work
+			DeviceUnpairWork content = work->contentAs<DeviceUnpairWork>();
+			content.setResult(result->status());
+			work->setContent(content);
+
+			switch (result->status()) {
+			case GatewayRPCResult::PENDING:
+			case GatewayRPCResult::ACCEPTED:
+				// TODO persist
+				return; // no other action is needed
+			default:
+				scheduler->wakeup(work);
+				break;
+			}
+		},
+		gateway,
+		device
+	);
+
+	suspend(work);
 }

server/src/work/DeviceUnpairWork.cpp
@@ -28,16 +28,6 @@ DeviceUnpairWork::DeviceUnpairWork(const Object::Ptr json):
 		throw InvalidArgumentException("uncompatible work content");
 }
 
-void DeviceUnpairWork::setAttempt(unsigned int attempt)
-{
-	json()->set("attempt", attempt);
-}
-
-unsigned int DeviceUnpairWork::attempt() const
-{
-	return JsonUtil::extract<unsigned int>(json(), "attempt");
-}
-
 void DeviceUnpairWork::setGatewayID(const GatewayID &id)
 {
 	json()->set("gateway_id", id.toString());
@@ -60,4 +50,19 @@ DeviceID DeviceUnpairWork::deviceID() const
 	return DeviceID::parse(
 		JsonUtil::extract<string>(json(), "device_id")
 	);
+}
+
+void DeviceUnpairWork::setResult(const GatewayRPCResult::Status status)
+{
+	json()->set("rpc_result", static_cast<int>(status));
+}
+
+GatewayRPCResult::Status DeviceUnpairWork::result() const
+{
+	return GatewayRPCResult::convert(json()->getValue<int>("rpc_result"));
+}
+
+bool DeviceUnpairWork::hasResult() const
+{
+	return json()->has("rpc_result");
 }

server/src/work/DeviceUnpairWork.h
@@ -3,6 +3,7 @@
 
 #include "model/Device.h"
 #include "model/Gateway.h"
+#include "rpc/GatewayRPCResult.h"
 #include "work/WorkContent.h"
 
 namespace BeeeOn {
@@ -30,18 +31,9 @@ public:
 	void setDeviceID(const DeviceID &id);
 	DeviceID deviceID() const;
 
-	/**
-	 * Update the number of attempts done while trying to unpair a
-	 * device. To be called from DeviceUnpairWorkExecutor or some
-	 * persistance layer only.
-	 */
-	void setAttempt(unsigned int attempt);
-
-	/**
-	 * Return the number of this attempt. Initially, this is always 0.
-	 * After N executions, the value is incremented automatically.
-	 */
-	unsigned int attempt() const;
+	void setResult(const GatewayRPCResult::Status status);
+	GatewayRPCResult::Status result() const;
+	bool hasResult() const;
 };
 
 }

server/src/work/DeviceUnpairWorkExecutor.h
@@ -4,18 +4,17 @@
 #include "dao/DeviceDao.h"
 #include "work/DeviceUnpairWork.h"
 #include "util/Loggable.h"
-#include "work/WorkExecutor.h"
+#include "work/ExtendedWorkExecutor.h"
 
 namespace BeeeOn {
 
 class GatewayRPC;
 
 /**
- * Executes DeviceUnpairWork instances. It should be executed
- * in a transaction context to avoid concurrency issues.
+ * Executes DeviceUnpairWork instances.
  */
 class DeviceUnpairWorkExecutor :
-		public WorkExecutor,
+		public ExtendedWorkExecutor,
 		Loggable {
 public:
 	DeviceUnpairWorkExecutor();
@@ -23,42 +22,21 @@ public:
 	void setDeviceDao(DeviceDao::Ptr dao);
 	void setGatewayRPC(GatewayRPC *rpc);
 
-	/**
-	 * How long (in miliseconds) to sleep before database polling steps.
-	 * This sleep time is applicated when a device has no or unknown
-	 * refresh time.
-	 */
-	void setPollSleep(int ms);
-
-	/**
-	 * How many attempts to perform before we can consider
-	 * an executed work as failing. This corresponds to the
-	 * DeviceUnpairWork::attempts property.
-	 */
-	void setMaxAttempts(int count);
-
 	bool accepts(const Work::Ptr work) const override;
 	void execute(Work::Ptr work) override;
 
 protected:
 	/**
-	 * Test whether a task targeting the given device has finished
-	 * successfully.
+	 * Process the result of the work. It is assumed that the DeviceUnpairWork
+	 * contains a result of its operation.
+	 *
+	 * @see DeviceUnpairWork::hasResult()
 	 */
-	bool done(Device &device);
-
-	/**
-	 * Test whether we have exceeded the attempts count. In that case,
-	 * the Poco::TimeoutException is thrown and the task is marked as
-	 * failing.
-	 */
-	void checkAttempts(const DeviceUnpairWork &work);
+	void processResult(Work::Ptr work, DeviceUnpairWork &content);
 
 private:
 	DeviceDao::Ptr m_dao;
 	GatewayRPC *m_rpc;
-	unsigned long m_pollSleep;
-	unsigned int m_maxAttempts;
 };
 
 }

commit 82f850c4dbe6994113f63430f65497d74b8ddb90

DeviceUnpairWorkExecutor: utilize lockManager setter
Fixes: ca5a82e50536 ("DeviceUnpairWork: rewrite to by fully asynchronous")
Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

server/conf/config.d/factory-work.xml
@@ -19,6 +19,7 @@
 		<instance name="deviceUnpairExecutor" class="BeeeOn::DeviceUnpairWorkExecutor">
 			<set name="deviceDao" ref="deviceDao" />
 			<set name="gatewayRPC" ref="gatewayRPC" />
+			<set name="lockManager" ref="uiWorkLockManager" />
 		</instance>
 
 		<instance name="uiWorkFacade" class="BeeeOn::DefaultWorkFacade">

server/src/work/DeviceUnpairWorkExecutor.cpp
@@ -14,6 +14,7 @@ BEEEON_OBJECT_CASTABLE(WorkExecutor)
 BEEEON_OBJECT_REF("deviceDao", &DeviceUnpairWorkExecutor::setDeviceDao)
 BEEEON_OBJECT_REF("gatewayRPC", &DeviceUnpairWorkExecutor::setGatewayRPC)
 BEEEON_OBJECT_REF("scheduler", &DeviceUnpairWorkExecutor::setScheduler)
+BEEEON_OBJECT_REF("lockManager", &DeviceUnpairWorkExecutor::setLockManager)
 BEEEON_OBJECT_END(BeeeOn, DeviceUnpairWorkExecutor)
 
 using namespace std;