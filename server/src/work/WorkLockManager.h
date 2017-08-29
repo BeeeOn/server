BeeeOn/server

commit 5c0396442dec470244808bc2c8aa08ef18d3cea7

work: import back the Work framework
Finally, the gateway project is not going to use this implementation
and it only complicates development of the server. Thus, move the
work framework back into server.

Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

server/src/work/WorkLockManager.h
@@ -1,0 +1,145 @@
+#ifndef BEEEON_WORK_LOCK_MANAGER_H
+#define BEEEON_WORK_LOCK_MANAGER_H
+
+#include <functional>
+
+#include <Poco/Mutex.h>
+#include <Poco/RWLock.h>
+
+#include "work/Work.h"
+#include "util/HashedLock.h"
+#include "util/Loggable.h"
+
+namespace BeeeOn {
+
+class WorkWriteGuard;
+class WorkReadGuard;
+class WorkExecutionGuard;
+
+/**
+ * Manager of locks for Work instances. The WorkLockManager is to be shared
+ * among WorkScheduler, WorkRunner and all places that access the Work::Ptr
+ * instances in the system.
+ */
+class WorkLockManager : Loggable {
+public:
+	WorkLockManager();
+	WorkLockManager(unsigned int accessCount, unsigned int executionCount);
+
+	/**
+	 * Lock work of the given ID for reading.
+	 */
+	WorkReadGuard readOnly(const WorkID &id);
+
+	/**
+	 * Lock work of the given ID for writing.
+	 */
+	WorkWriteGuard readWrite(const WorkID &id);
+
+	/**
+	 * Lock work of the given ID for execution.
+	 */
+	WorkExecutionGuard execute(const WorkID &id);
+
+	/**
+	 * Set count of locks for access lock manager.
+	 */
+	void setAccessLockCount(int count);
+
+	/**
+	 * Set count of locks for execution lock manager.
+	 */
+	void setExecutionLockCount(int count);
+
+private:
+	typedef HashedLock<Poco::RWLock, WorkID> AccessLock;
+	AccessLock m_accessLock;
+	typedef HashedLock<Poco::FastMutex, WorkID> ExecutionLock;
+	ExecutionLock m_executionLock;
+};
+
+template <typename Lock>
+class WorkGuard {
+public:
+	WorkGuard(const WorkID &id, Lock &lock, Poco::Logger &logger,
+			std::function<void(Lock &lock)> unlock):
+		m_id(id),
+		m_lock(&lock),
+		m_logger(logger),
+		m_unlock(unlock)
+	{
+	}
+
+	WorkGuard(const WorkGuard &) = delete;
+
+	WorkGuard(WorkGuard &&from):
+		m_id(from.m_id),
+		m_lock(from.m_lock),
+		m_logger(from.m_logger),
+		m_unlock(from.m_unlock)
+	{
+		from.m_lock = NULL;
+	}
+
+	virtual ~WorkGuard()
+	{
+		unlock();
+	}
+
+	void unlock()
+	{
+		if (m_lock != NULL)
+			m_unlock(*m_lock);
+	}
+
+protected:
+	const WorkID &id() const
+	{
+		return m_id;
+	}
+
+	Poco::Logger &logger()
+	{
+		return m_logger;
+	}
+
+private:
+	const WorkID m_id;
+	Lock *m_lock;
+	Poco::Logger &m_logger;
+	std::function<void(Lock &lock)> m_unlock;
+};
+
+class WorkExecutionGuard : public WorkGuard<Poco::FastMutex> {
+public:
+	WorkExecutionGuard(const WorkID &id, Poco::FastMutex &lock, Poco::Logger &logger);
+	WorkExecutionGuard(const WorkExecutionGuard &) = delete;
+	WorkExecutionGuard(WorkExecutionGuard &&from);
+
+protected:
+	void doUnlock(Poco::FastMutex &lock);
+};
+
+class WorkWriteGuard : public WorkGuard<Poco::RWLock> {
+public:
+	WorkWriteGuard(const WorkID &id, Poco::RWLock &lock, Poco::Logger &logger);
+	WorkWriteGuard(const WorkWriteGuard &) = delete;
+	WorkWriteGuard(WorkWriteGuard &&from);
+
+protected:
+	void doUnlock(Poco::RWLock &lock);
+};
+
+class WorkReadGuard : public WorkGuard<Poco::RWLock> {
+public:
+	WorkReadGuard(const WorkID &id, Poco::RWLock &lock, Poco::Logger &logger);
+	WorkReadGuard(const WorkReadGuard &) = delete;
+	WorkReadGuard(WorkReadGuard &&from);
+
+protected:
+	void doUnlock(Poco::RWLock &lock);
+};
+
+}
+
+#endif

server/src/CMakeLists.txt
@@ -139,6 +139,24 @@ file(GLOB SOURCES
 	${PROJECT_SOURCE_DIR}/work/DeviceUnpairWorkExecutor.cpp
 )
 
+file(GLOB WORK_SOURCES
+	${PROJECT_SOURCE_DIR}/work/AbstractWorkBackup.cpp
+	${PROJECT_SOURCE_DIR}/work/BasicProcessor.cpp
+	${PROJECT_SOURCE_DIR}/work/BasicQueue.cpp
+	${PROJECT_SOURCE_DIR}/work/DefaultWorkFacade.cpp
+	${PROJECT_SOURCE_DIR}/work/GenericWorkRunner.cpp
+	${PROJECT_SOURCE_DIR}/work/Work.cpp
+	${PROJECT_SOURCE_DIR}/work/WorkBackup.cpp
+	${PROJECT_SOURCE_DIR}/work/WorkContent.cpp
+	${PROJECT_SOURCE_DIR}/work/WorkExecutor.cpp
+	${PROJECT_SOURCE_DIR}/work/WorkFacade.cpp
+	${PROJECT_SOURCE_DIR}/work/WorkFactory.cpp
+	${PROJECT_SOURCE_DIR}/work/WorkLockManager.cpp
+	${PROJECT_SOURCE_DIR}/work/WorkRunner.cpp
+	${PROJECT_SOURCE_DIR}/work/WorkScheduler.cpp
+	${PROJECT_SOURCE_DIR}/work/WorkSuspendThrowable.cpp
+)
+
 file(GLOB XMLUI_SOURCES
 	xmlui/Serializing.cpp
 	xmlui/XmlValueConsumer.cpp
@@ -181,6 +199,7 @@ include_directories(
 )
 
 add_library(BeeeOnServer SHARED ${SOURCES})
+add_library(BeeeOnWork SHARED ${WORK_SOURCES})
 
 add_executable(beeeon-server
 	main.cpp
@@ -232,7 +251,7 @@ else()
 	target_link_libraries(beeeon-server ${POCO_ODBC})
 endif()
 
-install(TARGETS beeeon-server BeeeOnServer
+install(TARGETS beeeon-server BeeeOnServer BeeeOnWork
 	RUNTIME DESTINATION bin
 	LIBRARY DESTINATION lib
 	ARCHIVE DESTINATION lib

server/src/work/AbstractWorkBackup.cpp
@@ -1,0 +1,44 @@
+#include <Poco/Exception.h>
+
+#include "work/AbstractWorkBackup.h"
+#include "work/WorkFactory.h"
+#include "Debug.h"
+
+using namespace Poco;
+using namespace BeeeOn;
+
+AbstractWorkBackup::AbstractWorkBackup()
+{
+}
+
+void AbstractWorkBackup::store(Work::Ptr work, bool transaction)
+{
+	const std::string type = WorkFactory::identify(typeid(*work));
+	doStore(work, type, transaction);
+}
+
+void AbstractWorkBackup::registerFactory(WorkFactory *factory)
+{
+	if (m_registry.find(factory->id()) == m_registry.end()) {
+		m_registry[factory->id()] = factory;
+	}
+	else {
+		throw InvalidArgumentException("factory " + factory->id()
+				+ " has been already registered");
+	}
+}
+
+Work *AbstractWorkBackup::create(
+		const std::string &type,
+		const WorkID &id)
+{
+	auto it = m_registry.find(type);
+	if (it == m_registry.end())
+		throw NotFoundException("no factory for work " + type);
+
+	WorkFactory *factory = it->second;
+	Work *work = factory->create();
+	work->setId(id);
+
+	return work;
+}

server/src/work/AbstractWorkBackup.h
@@ -1,0 +1,35 @@
+#ifndef BEEEON_ABSTRACT_WORK_REPOSITORY_H
+#define BEEEON_ABSTRACT_WORK_REPOSITORY_H
+
+#include <map>
+#include <typeinfo>
+
+#include "work/WorkBackup.h"
+
+namespace BeeeOn {
+
+class WorkFactory;
+
+class AbstractWorkBackup : public WorkBackup {
+public:
+	AbstractWorkBackup();
+
+	void store(Work::Ptr work, bool transaction = false) override;
+
+	void registerFactory(WorkFactory *factory);
+
+protected:
+	virtual void doStore(
+		Work::Ptr work,
+		const std::string &id,
+		bool transaction) = 0;
+
+	Work *create(const std::string &type, const WorkID &id);
+
+private:
+	std::map<std::string, WorkFactory *> m_registry;
+};
+
+}
+
+#endif

server/src/work/BasicProcessor.cpp
@@ -1,0 +1,350 @@
+#include <Poco/Exception.h>
+#include <Poco/Thread.h>
+#include <Poco/Logger.h>
+
+#include "di/Injectable.h"
+#include "work/BasicProcessor.h"
+#include "work/WorkBackup.h"
+#include "work/WorkExecutor.h"
+#include "work/WorkLockManager.h"
+#include "work/WorkRunner.h"
+#include "work/WorkScheduler.h"
+
+BEEEON_OBJECT_BEGIN(BeeeOn, BasicProcessor)
+BEEEON_OBJECT_CASTABLE(WorkScheduler)
+BEEEON_OBJECT_CASTABLE(StoppableRunnable)
+BEEEON_OBJECT_REF("backup", &BasicProcessor::setBackup)
+BEEEON_OBJECT_REF("runnerFactory", &BasicProcessor::setRunnerFactory)
+BEEEON_OBJECT_REF("lockManager", &BasicProcessor::setLockManager)
+BEEEON_OBJECT_REF("executors", &BasicProcessor::registerExecutor)
+BEEEON_OBJECT_NUMBER("minThreads", &BasicProcessor::setMinThreads)
+BEEEON_OBJECT_NUMBER("maxThreads", &BasicProcessor::setMaxThreads)
+BEEEON_OBJECT_NUMBER("threadIdleTime", &BasicProcessor::setThreadIdleTime)
+BEEEON_OBJECT_HOOK("done", &BasicProcessor::init)
+BEEEON_OBJECT_END(BeeeOn, BasicProcessor)
+
+using namespace std;
+using namespace Poco;
+using namespace BeeeOn;
+
+BasicProcessor::BasicProcessor():
+	m_backup(&EmptyWorkBackup::instance()),
+	m_runnerFactory(&NullWorkRunnerFactory::instance()),
+	m_shouldStop(0),
+	m_current(NULL),
+	m_minThreads(1),
+	m_maxThreads(16),
+	m_threadIdleTime(100)
+{
+}
+
+void BasicProcessor::setBackup(WorkBackup *backup)
+{
+	m_backup = backup? backup : &EmptyWorkBackup::instance();
+}
+
+void BasicProcessor::setRunnerFactory(WorkRunnerFactory *factory)
+{
+	m_runnerFactory = factory? factory : &NullWorkRunnerFactory::instance();
+}
+
+void BasicProcessor::setLockManager(WorkLockManager *manager)
+{
+	m_lockManager = manager;
+}
+
+void BasicProcessor::setMinThreads(int min)
+{
+	if (min < 0)
+		throw InvalidArgumentException("minThreads must be non-negative");
+
+	m_minThreads = min;
+}
+
+void BasicProcessor::setMaxThreads(int max)
+{
+	if (max < 0)
+		throw InvalidArgumentException("maxThreads must be non-negative");
+
+	m_maxThreads = max;
+}
+
+void BasicProcessor::setThreadIdleTime(int ms)
+{
+	if (ms <= 0)
+		throw InvalidArgumentException("threadIdleTime must be greater then zero");
+
+	if (ms > 0 && ms < 1000) {
+		logger().warning("threadIdleTime's granularity is 1000 ms, treating "
+				+ to_string(ms) + " as zero",
+				__FILE__, __LINE__);
+	}
+
+	m_threadIdleTime = ms / 1000;
+}
+
+void BasicProcessor::registerExecutor(WorkExecutor *executor)
+{
+	m_executors.push_back(executor);
+}
+
+void BasicProcessor::init()
+{
+	initQueue();
+	initPool();
+}
+
+void BasicProcessor::initQueue()
+{
+	vector<Work::Ptr> all;
+	m_backup->loadScheduled(all);
+
+	FastMutex::ScopedLock guard(m_queue.lock());
+
+	for (auto &work : all) {
+		if (work->state() == Work::STATE_EXECUTED) {
+			if (logger().debug()) {
+				logger().debug("work " + *work
+						+ " in state executed is marked as failed",
+						__FILE__, __LINE__);
+			}
+
+			// locking is unnecessary as this is to be called only during
+			// initialization when the BasicProcessor is inactive.
+
+			work->setState(Work::STATE_FAILED);
+			m_backup->store(work);
+			continue;
+		}
+		else if (work->state() >= Work::STATE_FINISHED)
+			continue;
+
+		WorkWriteGuard guard(m_lockManager->readWrite(work->id()));
+		m_queue.pushUnlocked(work);
+	}
+}
+
+void BasicProcessor::initPool()
+{
+	if (m_pool.isNull()) {
+		logger().notice("creating thread pool min: "
+			+ to_string(m_minThreads)
+			+ " max: "
+			+ to_string(m_maxThreads),
+			__FILE__, __LINE__);
+
+		m_pool = new ThreadPool(
+			m_minThreads,
+			m_maxThreads,
+			m_threadIdleTime
+		);
+	}
+}
+
+ThreadPool &BasicProcessor::pool()
+{
+	initPool();
+	return *m_pool;
+}
+
+void BasicProcessor::run()
+{
+	if (m_current != NULL)
+		return;
+
+	m_current = Thread::current();
+
+	if (m_current == NULL)
+		throw IllegalStateException("called outside of thread",
+				__FILE__, __LINE__);
+
+	m_current->setName("BasicProcessor");
+
+	while (!m_shouldStop) {
+		try {
+			dispatch();
+		}
+		catch (const Poco::Exception &e) {
+			logger().log(e, __FILE__, __LINE__);
+		}
+		catch (const std::exception &e) {
+			logger().critical(e.what(), __FILE__, __LINE__);
+			break;
+		}
+		catch (...) {
+			logger().critical("failed to dispatch", __FILE__, __LINE__);
+			break;
+		}
+	}
+}
+
+void BasicProcessor::stop()
+{
+	m_shouldStop = 1;
+	notify();
+}
+
+void BasicProcessor::dispatch()
+{
+	Work::Ptr work = m_queue.pop();
+	if (work.isNull()) {
+		if (logger().debug()) {
+			logger().debug("no work in the queue",
+					__FILE__, __LINE__);
+		}
+
+		return;
+	}
+
+	if (Work::timestampValid(work->finished())) {
+		if (logger().debug()) {
+			logger().debug("work " + *work
+				+ " has finished timestamp, skipping",
+				__FILE__, __LINE__);
+		}
+		return;
+	}
+
+	if (work->state() >= Work::STATE_FINISHED) {
+		logger().warning("work " + *work + " is done, ignoring...",
+				__FILE__, __LINE__);
+		return;
+	}
+
+	WorkExecutor *executor = NULL;
+
+	for (auto one : m_executors) {
+		if (!one->accepts(work))
+			continue;
+
+		executor = one;
+		break;
+	}
+
+	if (executor == NULL) {
+		logger().warning("no executor for work " + *work,
+				__FILE__, __LINE__);
+
+		work->setState(Work::STATE_FAILED);
+		m_backup->store(work);
+		return;
+	}
+
+	if (logger().debug())
+		logger().debug("executing work " + *work, __FILE__, __LINE__);
+
+	execute(executor, work);
+}
+
+void BasicProcessor::execute(WorkExecutor *executor, Work::Ptr work)
+{
+	WorkRunner *runner(m_runnerFactory->create(*this));
+	if (runner == NULL) {
+		logger().critical("runner was created as NULL",
+				__FILE__, __LINE__);
+		return;
+	}
+
+	if (logger().debug()) {
+		logger().debug("having runner, executing work " + *work + " in thread",
+				__FILE__, __LINE__);
+	}
+
+	runner->setExecutor(executor);
+	runner->setWork(work);
+	runner->setBackup(m_backup);
+
+	try {
+		while (!executeInThread(*runner, "Work:" + *work)) {
+			logger().warning("no thread to execute work " + *work);
+			m_current->trySleep(100);
+		}
+	} catch (...) {
+		// WorkRunner did not started otherwise it
+		// would delete itself automatically
+		delete runner;
+		throw;
+	}
+}
+
+bool BasicProcessor::executeInThread(Runnable &runnable, const string &name)
+{
+	if (logger().debug())
+		logger().debug("executing thread " + name, __FILE__, __LINE__);
+
+	try {
+		pool().start(runnable, name);
+	} catch (Poco::NoThreadAvailableException &e) {
+		return false;
+	}
+
+	return true;
+}
+
+void BasicProcessor::schedule(Work::Ptr work)
+{
+	if (logger().debug()) {
+		logger().debug("schedule work " + *work,
+				__FILE__, __LINE__);
+	}
+
+	FastMutex::ScopedLock guard(m_queue.lock());
+	WorkWriteGuard workGuard(m_lockManager->readWrite(work->id()));
+
+	m_queue.pushUnlocked(work);
+	m_backup->store(work);
+
+	wakeUpSelf();
+}
+
+void BasicProcessor::wakeup(Work::Ptr work)
+{
+	if (logger().debug()) {
+		logger().debug("wakeup work " + *work,
+				__FILE__, __LINE__);
+	}
+
+	FastMutex::ScopedLock guard(m_queue.lock());
+	WorkWriteGuard workGuard(m_lockManager->readWrite(work->id()));
+
+	m_queue.wakeupUnlocked(work);
+	m_backup->store(work);
+
+	wakeUpSelf();
+}
+
+void BasicProcessor::cancel(Work::Ptr work)
+{
+	if (logger().debug()) {
+		logger().debug("cancel work " + *work,
+				__FILE__, __LINE__);
+	}
+
+	FastMutex::ScopedLock guard(m_queue.lock());
+
+	// lock the work to cancel it safely
+	WorkWriteGuard workGuard(m_lockManager->readWrite(work->id()));
+
+	m_queue.cancelUnlocked(work);
+	work->setState(Work::STATE_CANCELED);
+	m_backup->store(work);
+
+	wakeUpSelf();
+}
+
+void BasicProcessor::notify()
+{
+	if (logger().debug()) {
+		logger().debug("notify work processor",
+				__FILE__, __LINE__);
+	}
+
+	m_queue.notify();
+	wakeUpSelf();
+}
+
+void BasicProcessor::wakeUpSelf()
+{
+	if(m_current)
+		m_current->wakeUp();
+}

server/src/work/BasicProcessor.h
@@ -1,0 +1,78 @@
+#ifndef BEEEON_BASIC_WORK_PROCESSOR_H
+#define BEEEON_BASIC_WORK_PROCESSOR_H
+
+#include <list>
+#include <string>
+
+#include <Poco/AtomicCounter.h>
+#include <Poco/Runnable.h>
+#include <Poco/Thread.h>
+#include <Poco/ThreadPool.h>
+
+#include "loop/StoppableRunnable.h"
+#include "util/Loggable.h"
+#include "work/BasicQueue.h"
+#include "work/WorkScheduler.h"
+
+namespace BeeeOn {
+
+class WorkBackup;
+class WorkExecutor;
+class WorkLockManager;
+class WorkRunner;
+class WorkRunnerFactory;
+
+class BasicProcessor :
+		public WorkScheduler,
+		public StoppableRunnable,
+		Loggable {
+public:
+	BasicProcessor();
+
+	void setBackup(WorkBackup *repository);
+	void setRunnerFactory(WorkRunnerFactory *factory);
+	void setLockManager(WorkLockManager *manager);
+
+	void setMinThreads(int min);
+	void setMaxThreads(int max);
+	void setThreadIdleTime(int ms);
+
+	void run() override;
+	void stop() override;
+
+	void schedule(Work::Ptr work) override;
+	void wakeup(Work::Ptr work) override;
+	void cancel(Work::Ptr work) override;
+	void notify() override;
+
+	void registerExecutor(WorkExecutor *executor);
+
+	void init();
+
+protected:
+	void initQueue();
+	void initPool();
+	Poco::ThreadPool &pool();
+	void dispatch();
+	void execute(WorkExecutor *executor, Work::Ptr work);
+	bool executeInThread(Poco::Runnable &runnable,
+			const std::string &name);
+	void wakeUpSelf();
+
+private:
+	WorkBackup *m_backup;
+	WorkRunnerFactory *m_runnerFactory;
+	WorkLockManager *m_lockManager;
+	std::list<WorkExecutor *> m_executors;
+	Poco::AtomicCounter m_shouldStop;
+	Poco::Thread *m_current;
+	Poco::SharedPtr<Poco::ThreadPool> m_pool;
+	int m_minThreads;
+	int m_maxThreads;
+	int m_threadIdleTime;
+	BasicQueue m_queue;
+};
+
+}
+
+#endif

server/src/work/BasicQueue.cpp
@@ -1,0 +1,232 @@
+#include <Poco/Exception.h>
+#include <Poco/Logger.h>
+
+#include "work/BasicQueue.h"
+#include "work/WorkBackup.h"
+
+using namespace std;
+using namespace Poco;
+using namespace BeeeOn;
+
+void BasicQueue::assertLocked()
+{
+	if (!m_lock.tryLock())
+		return;
+
+	m_lock.unlock();
+	throw IllegalStateException("lock is not held in the basic queue");
+}
+
+void BasicQueue::pushUnlocked(Work::Ptr work)
+{
+	assertLocked();
+
+	const Work::State state = work->state();
+	if (state >= Work::STATE_FINISHED) {
+		if (logger().debug()) {
+			logger().debug("ignore finished work " + *work,
+					__FILE__, __LINE__);
+		}
+
+		return;
+	}
+
+	doPushUnfinishedUnlocked(work);
+	m_wakeup.set();
+}
+
+void BasicQueue::activate(const Key &key, Record &record)
+{
+	if (logger().debug()) {
+		logger().debug("activating work " + *record.work
+				+ " priority " + to_string(key.priority)
+				+ " at " + to_string(key.at.epochMicroseconds()),
+				__FILE__, __LINE__);
+	}
+
+	record.ref = m_active.emplace(make_pair(key, record.work));
+}
+
+void BasicQueue::deactivate(Record &record)
+{
+	m_active.erase(record.ref);
+	record.ref = m_active.end();
+}
+
+void BasicQueue::doPushUnfinishedUnlocked(Work::Ptr work)
+{
+	const Nullable<Timestamp> time = work->activationTime();
+	const int priority = work->priority();
+	const WorkID id = work->id();
+
+	auto it = m_queue.find(id);
+
+	if (it != m_queue.end()) {
+		if (logger().debug())
+			logger().debug("reschedule work " + *work, __FILE__, __LINE__);
+
+		Record &record = it->second;
+
+
+		// if the work is active and the activation time has
+		// changed or is not specified anymore, remove that
+		// work from the active queue
+		if (record.ref != m_active.end()) {
+			const Key &key = record.ref->first;
+
+			if (time.isNull())
+				deactivate(record);
+			else if (time.value() != key.at)
+				deactivate(record);
+			else if (priority != key.priority)
+				deactivate(record);
+		}
+
+		// if the work is not active (or was recently deactivated)
+		// activate it if the activation time is specified
+		if (record.ref == m_active.end()) {
+			if (!time.isNull())
+				activate(Key{time, priority}, record);
+		}
+	}
+	else {
+		if (logger().debug())
+			logger().debug("new work " + *work, __FILE__, __LINE__);
+
+		Record record = {work, m_active.end()};
+
+		// activate if the activation time is specified
+		if (!time.isNull())
+			activate(Key{time, priority}, record);
+
+		m_queue.emplace(make_pair(id, record));
+	}
+
+	work->setState(Work::STATE_SCHEDULED);
+}
+
+void BasicQueue::wakeupUnlocked(Work::Ptr work)
+{
+	assertLocked();
+
+	if (m_queue.find(work->id()) == m_queue.end())
+		return; // no such work, no reason to wake up
+
+	work->setSleepDuration(0); // execute early
+	pushUnlocked(work);
+
+	m_wakeup.set();
+}
+
+void BasicQueue::cancelUnlocked(Work::Ptr work)
+{
+	assertLocked();
+
+	const WorkID id = work->id();
+
+	auto ref = m_queue.find(id);
+	if (ref != m_queue.end()) {
+		const Record &record = ref->second;
+		m_active.erase(record.ref);
+		m_queue.erase(ref);
+
+		m_wakeup.set();
+	}
+}
+
+pair<Timestamp, Work::Ptr> BasicQueue::popFast(const Timestamp &now)
+{
+	FastMutex::ScopedLock guard(m_lock);
+
+	const auto head = m_active.begin();
+	if (head == m_active.end()) // nothing to do
+		return make_pair(now - 1, SharedPtr<Work>());
+
+	const Key &key = head->first;
+	if (key.at > now) // too early to execute
+		return make_pair(key.at, SharedPtr<Work>());
+
+	Work::Ptr work = head->second;
+	const WorkID id = work->id();
+	const Timestamp at(key.at);
+
+	m_queue.erase(id);
+	m_active.erase(head);
+
+	return make_pair(at, work);
+}
+
+Work::Ptr BasicQueue::pop(bool blocking)
+{
+	bool shouldBlock = blocking;
+	Timestamp now;
+
+	do {
+		now.update();
+
+		pair<Timestamp, Work::Ptr> head = popFast(now);
+		const Timestamp &at = head.first;
+		Work::Ptr &work = head.second;
+
+		if (at <= now && !work.isNull()) {
+			if (logger().debug()) {
+				logger().debug("pop work " + *work,
+					__FILE__, __LINE__);
+			}
+
+			return work;
+		}
+		else if (shouldBlock) {
+			shouldBlock = block(now, at);
+
+			if (logger().debug()) {
+				logger().debug("woken up, should block again: "
+					+ to_string(shouldBlock),
+					__FILE__, __LINE__);
+			}
+		}
+		else
+			return NULL;
+	} while (1);
+}
+
+bool BasicQueue::block(
+		const Poco::Timestamp &now,
+		const Poco::Timestamp &next)
+{
+	if (next < now) {
+		if (logger().debug()) {
+			logger().debug("blocking until notified",
+					__FILE__, __LINE__);
+		}
+
+		try {
+			m_wakeup.wait();
+			return false;
+		}
+		catch (const TimeoutException &e) {
+			// no event, we should block more
+			return true;
+		}
+	}
+	else if (next > now) {
+		const long us = next - now;
+		const long sleep = (us / 1000) + ((us % 1000)? 1 : 0);
+
+		if (logger().debug()) {
+			logger().debug("blocking for "
+					+ to_string(sleep)
+					+ " ms",
+					__FILE__, __LINE__);
+		}
+
+		return m_wakeup.tryWait(sleep);
+	}
+
+	return false; // probably, no reason to block
+}
+
+void BasicQueue::notify()
+{
+	m_wakeup.set();
+}

server/src/work/BasicQueue.h
@@ -1,0 +1,157 @@
+#ifndef BEEEON_BASIC_QUEUE_H
+#define BEEEON_BASIC_QUEUE_H
+
+#include <map>
+
+#include <Poco/Event.h>
+#include <Poco/Mutex.h>
+#include <Poco/Timestamp.h>
+
+#include "util/Loggable.h"
+#include "work/Work.h"
+
+namespace BeeeOn {
+
+class BasicQueue : Loggable {
+public:
+	/**
+	 * Key used for ordering the queue for pop() operation.
+	 */
+	struct Key {
+		Poco::Timestamp at;
+		int priority;
+
+		enum {
+			EVERY_5SECS = 5
+		};
+
+		/**
+		 * First, compare all works based on 5 seconds interval.
+		 * If two works are in the same 5 seconds interval, then
+		 * apply priority and finally the whole time in us.
+		 */
+		bool operator <(const Key &key) const
+		{
+			std::time_t period = at.epochTime() / EVERY_5SECS;
+
+			if (period < key.at.epochTime() / EVERY_5SECS)
+				return true;
+
+			if (priority > key.priority) // higher is better
+				return true;
+
+			if (priority < key.priority) // higher is better
+				return false;
+
+			return at < key.at;
+		}
+	};
+
+	/**
+	 * Queue of active works. An active work must contain the
+	 * activation time. Otherwise it must wait until wakeup.
+	 */
+	typedef std::multimap<Key, Work::Ptr> ActiveQueue;
+
+	/**
+	 * Record with queue information about a work.
+	 */
+	struct Record {
+		Work::Ptr work;
+		ActiveQueue::iterator ref;
+	};
+
+	/**
+	 * Queue of all works waiting for execution.
+	 * Some of them may just sleep until an event wakes them up.
+	 */
+	typedef std::map<WorkID, Record> WaitQueue;
+
+	/**
+	 * Provide the internal queue lock for callers because
+	 * several operations might require custom locking connected
+	 * tightly with the queue.
+	 */
+	Poco::FastMutex &lock()
+	{
+		return m_lock;
+	}
+
+	/**
+	 * Push the given work into the queue.
+	 * The BasicQueue::lock() must be held.
+	 */
+	void pushUnlocked(Work::Ptr work);
+
+	/**
+	 * Wakeup the given work if it is in the queue.
+	 * The BasicQueue::lock() must be held.
+	 */
+	void wakeupUnlocked(Work::Ptr work);
+
+	/**
+	 * Cancel the given work if it is in the queue.
+	 * The BasicQueue::lock() must be held.
+	 *
+	 * The Work state is not changed in any way by this call.
+	 * You should likely call this method with work.executionLock()
+	 * held. to prevent cancelling an already executed work.
+	 */
+	void cancelUnlocked(Work::Ptr work);
+
+	/**
+	 * Pop the head of the queue if any. The call can block
+	 * until a work is available for processing. The blocking
+	 * pop() can be interrupted by calling to notify(). It
+	 * returns NULL in such case.
+	 */
+	Work::Ptr pop(bool blocking = true);
+
+	/**
+	 * Notify the queue that it should retry the blocking pop().
+	 */
+	void notify();
+
+protected:
+	void assertLocked();
+
+	/**
+	 * Do push with no Work access locking and no execution locking.
+	 * If the given work is in STATE_EXECUTED, the Work::executionLock()
+	 * must be held to prevent race conditions.
+	 */
+	void doPushUnfinishedUnlocked(Work::Ptr work);
+
+	/**
+	 * Pop the head of the queue if the Work is to be executed.
+	 * Otherwise, it returns timestamp of the next Work. If
+	 * the queue is empty, the returned timestamp is lower then now.
+	 */
+	std::pair<Poco::Timestamp, Work::Ptr> popFast(
+			const Poco::Timestamp &now);
+
+	/**
+	 * Block for next - now microseconds or block indefinitely
+	 * when now > next.
+	 *
+	 * @return true if blocking should be restarted when there is
+	 *    still no work to be returned.
+	 * @return false if blocking should be avoided due to notify()
+	 *    has been delivered.
+	 */
+	bool block(const Poco::Timestamp &now, const Poco::Timestamp &next);
+
+protected:
+	void activate(const Key &key, Record &work);
+	void deactivate(Record &record);
+
+private:
+	Poco::FastMutex m_lock;
+	WaitQueue m_queue;
+	ActiveQueue m_active;
+	Poco::Event m_wakeup;
+};
+
+}
+
+#endif

commit 7e5314b79fd69c608eb06b0935f394ccbff545df

WorkLockManager: clear lock after unlock()
This changes fixes double-unlocking which may lead to a wierd behaviour
in the work processing. Some locks cannot be unlocked multiple times.

Fixes: 5c0396442dec ("work: import back the Work framework")
Fixes: 365a2ba3bb40 ("WorkLockManager: new class")
Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

server/src/work/WorkLockManager.h
@@ -91,8 +91,10 @@ public:
 
 	void unlock()
 	{
-		if (m_lock != NULL)
+		if (m_lock != NULL) {
 			m_unlock(*m_lock);
+			m_lock = NULL;
+		}
 	}
 
 protected: