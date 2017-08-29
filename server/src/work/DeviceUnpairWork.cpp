BeeeOn/server

commit 90ff01573d230e000b39c430297925e11d703b4b

DeviceUnpairWork: derive from WorkContent
The DeviceUnpairWork does not inherit from Work anymore. This allows
to use WorkFacade that performs copy of each Work instance (which is
difficult when using inheritance).

Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

server/src/work/DeviceUnpairWork.cpp
@@ -1,35 +1,65 @@
+#include <Poco/Exception.h>
+
+#include "util/JsonUtil.h"
 #include "work/DeviceUnpairWork.h"
 
+BEEEON_CLASS(DeviceUnpairWork)
+
+using namespace std;
 using namespace Poco;
+using namespace Poco::JSON;
 using namespace BeeeOn;
 
 DeviceUnpairWork::DeviceUnpairWork():
-	m_attempt(0)
+	WorkContent(ClassInfo::forPointer(this))
 {
+	json()->set("attempt", 0);
 }
 
-DeviceUnpairWork::DeviceUnpairWork(const WorkID &id):
-	Work(id),
-	m_attempt(0)
+DeviceUnpairWork::DeviceUnpairWork(const WorkContent &content):
+	WorkContent(content)
 {
+	if (!type().is<DeviceUnpairWork>())
+		throw InvalidArgumentException("uncompatible work content");
+}
+
+DeviceUnpairWork::DeviceUnpairWork(const Object::Ptr json):
+	WorkContent(json)
+{
+	if (!type().is<DeviceUnpairWork>())
+		throw InvalidArgumentException("uncompatible work content");
 }
 
 void DeviceUnpairWork::setAttempt(unsigned int attempt)
 {
-	m_attempt = attempt;
+	json()->set("attempt", attempt);
 }
 
 unsigned int DeviceUnpairWork::attempt() const
 {
-	return m_attempt;
+	return JsonUtil::extract<unsigned int>(json(), "attempt");
 }
 
-void DeviceUnpairWork::setDevice(const Device &device)
+void DeviceUnpairWork::setGatewayID(const GatewayID &id)
 {
-	m_device = device;
+	json()->set("gateway_id", id.toString());
 }
 
-const Device &DeviceUnpairWork::device() const
+GatewayID DeviceUnpairWork::gatewayID() const
 {
-	return m_device;
+	return GatewayID::parse(
+		JsonUtil::extract<string>(json(), "gateway_id")
+	);
+}
+
+void DeviceUnpairWork::setDeviceID(const DeviceID &id)
+{
+	json()->set("device_id", id.toString());
+}
+
+DeviceID DeviceUnpairWork::deviceID() const
+{
+	return DeviceID::parse(
+		JsonUtil::extract<string>(json(), "device_id")
+	);
 }

server/src/service/DeviceService.cpp
@@ -12,7 +12,7 @@
 #include "dao/DevicePropertyDao.h"
 #include "rpc/GatewayRPC.h"
 #include "work/DeviceUnpairWork.h"
-#include "work/WorkScheduler.h"
+#include "work/WorkFacade.h"
 #include "Debug.h"
 
 BEEEON_OBJECT_BEGIN(BeeeOn, DeviceService)
@@ -20,7 +20,7 @@ BEEEON_OBJECT_REF("deviceDao", &DeviceService::setDeviceDao)
 BEEEON_OBJECT_REF("devicePropertyDao", &DeviceService::setDevicePropertyDao)
 BEEEON_OBJECT_REF("gatewayRPC", &DeviceService::setGatewayRPC)
 BEEEON_OBJECT_REF("accessPolicy", &DeviceService::setAccessPolicy)
-BEEEON_OBJECT_REF("workScheduler", &DeviceService::setWorkScheduler)
+BEEEON_OBJECT_REF("workFacade", &DeviceService::setWorkFacade)
 BEEEON_OBJECT_REF("transactionManager", &Transactional::setTransactionManager)
 BEEEON_OBJECT_END(BeeeOn, DeviceService)
 
@@ -32,7 +32,7 @@ DeviceService::DeviceService():
 	m_dao(&NullDeviceDao::instance()),
 	m_propertyDao(&NullDevicePropertyDao::instance()),
 	m_gatewayRPC(&NullGatewayRPC::instance()),
-	m_scheduler(&NullWorkScheduler::instance()),
+	m_workFacade(&NullWorkFacade::instance()),
 	m_policy(&NullDeviceAccessPolicy::instance())
 {
 }
@@ -52,9 +52,9 @@ void DeviceService::setGatewayRPC(GatewayRPC *rpc)
 	m_gatewayRPC = rpc? rpc : &NullGatewayRPC::instance();
 }
 
-void DeviceService::setWorkScheduler(WorkScheduler *scheduler)
+void DeviceService::setWorkFacade(WorkFacade *facade)
 {
-	m_scheduler = scheduler? scheduler : &NullWorkScheduler::instance();
+	m_workFacade = facade? facade : &NullWorkFacade::instance();
 }
 
 void DeviceService::setAccessPolicy(DeviceAccessPolicy *policy)
@@ -127,8 +127,11 @@ Work::Ptr DeviceService::doUnregister(Relation<Device, Gateway> &input)
 	if (!m_dao->fetch(device, input.base()))
 		return NULL;
 
-	DeviceUnpairWork::Ptr work(new DeviceUnpairWork(WorkID::random()));
-	work->setDevice(input.target());
+	Work::Ptr work(new Work(WorkID::random()));
+	DeviceUnpairWork content;
+	content.setGatewayID(device.gateway().id());
+	content.setDeviceID(device.id());
+	work->setContent(content);
 
 	m_scheduler->schedule(work);
 	return work;

server/src/work/DeviceUnpairWork.h
@@ -1,39 +1,34 @@
 #ifndef BEEEON_DEVICE_UNPAIR_WORK_H
 #define BEEEON_DEVICE_UNPAIR_WORK_H
 
-#include <Poco/SharedPtr.h>
-
 #include "model/Device.h"
-#include "work/Work.h"
+#include "model/Gateway.h"
+#include "work/WorkContent.h"
 
 namespace BeeeOn {
-
-class DeviceUnpairWorkExecutor;
 
 /**
  * Representation of a work that unpairs a device from its gateway.
  * The unpairing process usually requires some asynchronous communication
  * with other parts of the system.
  */
-class DeviceUnpairWork : public Work {
+class DeviceUnpairWork : public WorkContent {
 public:
-	typedef Poco::SharedPtr<DeviceUnpairWork> Ptr;
-
 	DeviceUnpairWork();
-	DeviceUnpairWork(const WorkID &id);
+	DeviceUnpairWork(const WorkContent &content);
+	DeviceUnpairWork(const Poco::JSON::Object::Ptr json);
 
 	/**
-	 * Set when creating the work instance. The device must
-	 * contain the owning Gateway.
+	 * ID of gateway where a device is to be unpaired from.
 	 */
-	void setDevice(const Device &device);
+	void setGatewayID(const GatewayID &id);
+	GatewayID gatewayID() const;
 
 	/**
-	 * Device holding the Gateway we want to unpair from.
-	 * We need Device::id() and Device::gateway().id() to be
-	 * valid.
+	 * ID of device to be unpaired.
 	 */
-	const Device &device() const;
+	void setDeviceID(const DeviceID &id);
+	DeviceID deviceID() const;
 
 	/**
 	 * Update the number of attempts done while trying to unpair a
@@ -47,10 +42,6 @@ public:
 	 * After N executions, the value is incremented automatically.
 	 */
 	unsigned int attempt() const;
-
-private:
-	unsigned int m_attempt;
-	Device m_device;
 };
 
 }

server/src/work/DeviceUnpairWorkExecutor.cpp
@@ -55,10 +55,10 @@ void DeviceUnpairWorkExecutor::setMaxAttempts(int count)
 
 bool DeviceUnpairWorkExecutor::accepts(const Work::Ptr work) const
 {
-	if (work.cast<DeviceUnpairWork>().isNull())
-		return false;
+	if (work->content().type().is<DeviceUnpairWork>())
+		return true;
 
-	return true;
+	return false;
 }
 
 bool DeviceUnpairWorkExecutor::done(Device &device)
@@ -88,33 +88,36 @@ bool DeviceUnpairWorkExecutor::done(Device &device)
 	return false;
 }
 
-void DeviceUnpairWorkExecutor::checkAttempts(DeviceUnpairWork::Ptr work)
+void DeviceUnpairWorkExecutor::checkAttempts(const DeviceUnpairWork &work)
 {
-	if (work->attempt() >= m_maxAttempts) {
+	if (work.attempt() >= m_maxAttempts) {
 		if (logger().debug()) {
 			logger().debug(
-				"failed to unpair device " + work->device()
+				"failed to unpair device " + work.deviceID().toString()
 				+ " after " + to_string(m_maxAttempts)
 				+ " attempts",
 				__FILE__, __LINE__);
 		}
 
-		throw TimeoutException("failed to unpair device " + work->device());
+		throw TimeoutException("failed to unpair device "
+				+ work.deviceID().toString());
 	}
 }
 
 void DeviceUnpairWorkExecutor::execute(Work::Ptr work)
 {
-	DeviceUnpairWork::Ptr unpair = work.cast<DeviceUnpairWork>();
-	Device device(unpair->device());
+	DeviceUnpairWork content = work->contentAs<DeviceUnpairWork>();
+
+	Device device(content.deviceID());
+	device.setGateway(Gateway(content.gatewayID()));
 
 	if (done(device))
 		return;
 
 	const Gateway &gateway = device.gateway();
 
-	checkAttempts(unpair);
-	unpair->setAttempt(unpair->attempt() + 1);
+	checkAttempts(content);
+	content.setAttempt(content.attempt() + 1);
 
 	try {
 		m_rpc->unpairDevice(gateway, device);
@@ -127,5 +130,6 @@ void DeviceUnpairWorkExecutor::execute(Work::Ptr work)
 	if (device.refresh() >= Timespan(1, 0))
 		sleep = device.refresh();
 
+	work->setContent(content);
 	suspend(work, sleep);
 }

server/src/work/DeviceUnpairWorkExecutor.h
@@ -52,7 +52,7 @@ protected:
 	 * the Poco::TimeoutException is thrown and the task is marked as
 	 * failing.
 	 */
-	void checkAttempts(DeviceUnpairWork::Ptr work);
+	void checkAttempts(const DeviceUnpairWork &work);
 
 private:
 	DeviceDao *m_dao;

commit 203a2588ede4dd342d84d5e899a225cca41d7e57

DeviceUnpairWork: fix exception messages
Fixes: 90ff01573d23 ("DeviceUnpairWork: derive from WorkContent")
Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

server/src/work/DeviceUnpairWork.cpp
@@ -18,14 +18,14 @@ DeviceUnpairWork::DeviceUnpairWork(const WorkContent &content):
 	WorkContent(content)
 {
 	if (!type().is<DeviceUnpairWork>())
-		throw InvalidArgumentException("uncompatible work content");
+		throw InvalidArgumentException("incompatible work content");
 }
 
 DeviceUnpairWork::DeviceUnpairWork(const Object::Ptr json):
 	WorkContent(json)
 {
 	if (!type().is<DeviceUnpairWork>())
-		throw InvalidArgumentException("uncompatible work content");
+		throw InvalidArgumentException("incompatible work content");
 }
 
 void DeviceUnpairWork::setGatewayID(const GatewayID &id)