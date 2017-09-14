BeeeOn/server

commit 966ed044cb1973a1fcd25623f5c624e11cb5bbab

IcuTranslator: implement key notation for lookup
The localized string can be now search via dot-notation. Thus
icu tables can be traversed:

 en {
   group {
     my {
       val { "Value" }
     }

     x { "Good" }
   }
 }

as

 IcuTranslator t = ...;
 t.format("group.my.val");
 t.format("group.x");

Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

server/src/l10n/IcuTranslator.cpp
@@ -4,6 +4,7 @@
 #include <Poco/DateTime.h>
 #include <Poco/Exception.h>
 #include <Poco/Logger.h>
+#include <Poco/StringTokenizer.h>
 #include <Poco/Timestamp.h>
 
 #include "di/Injectable.h"
@@ -172,28 +173,47 @@ string IcuTranslator::applyArgs(
 	return result.toUTF8String(utf8);
 }
 
-string IcuTranslator::formatImpl(
-		const string &key,
+string IcuTranslator::lookaupAndApplyArgs(
+		const string &originalKey,
+		vector<string>::const_iterator current,
+		const vector<string>::const_iterator end,
+		const ResourceBundle &bundle,
 		const vector<Var> &args)
 {
+	if (current == end)
+		return NoTranslator::formatImpl(originalKey, args);
+
 	UErrorCode error = U_ZERO_ERROR;
-	icu::ResourceBundle value = m_bundle->get(key.c_str(), error);
-	handleError(error, key, m_bundle->getName());
+	icu::ResourceBundle value = bundle.get(current->c_str(), error);
+	handleError(error, originalKey, bundle.getName());
 
 	switch (value.getType()) {
 	case URES_STRING:
 		return applyArgs(value, args);
 
+	case URES_TABLE:
+		return lookaupAndApplyArgs(originalKey, ++current, end, value, args);
+
 	default:
 		logger().critical(
 			"requested resource "
-			+ key
+			+ originalKey
 			+ " is of type "
 			+ typeName(value.getType()),
 			__FILE__, __LINE__);
 
-		return NoTranslator::formatImpl(key, args);
+		return NoTranslator::formatImpl(originalKey, args);
 	}
+}
+
+string IcuTranslator::formatImpl(
+		const string &key,
+		const vector<Var> &args)
+{
+	StringTokenizer keyList(key, ".",
+		StringTokenizer::TOK_IGNORE_EMPTY | StringTokenizer::TOK_TRIM);
+
+	return lookaupAndApplyArgs(key, keyList.begin(), keyList.end(), *m_bundle, args);
 }
 
 static IcuLocaleImpl asIcuLocale(const BeeeOn::Locale &locale)

server/src/l10n/IcuTranslator.h
@@ -28,6 +28,13 @@ protected:
 		const std::string &key,
 		const std::vector<Poco::Dynamic::Var> &args) override;
 
+	std::string lookaupAndApplyArgs(
+		const std::string &originalKey,
+		std::vector<std::string>::const_iterator current,
+		const std::vector<std::string>::const_iterator end,
+		const icu::ResourceBundle &bundle,
+		const std::vector<Poco::Dynamic::Var> &args);
+
 	std::string applyArgs(
 		const icu::ResourceBundle &bundle,
 		const std::vector<Poco::Dynamic::Var> &args);

commit f38d65651e3bbd63a80480a8610fceaddc2e63af

IcuTranslator: fix method name to lookupAndApplyArgs()
Fixes: 966ed044cb19 ("IcuTranslator: implement key notation for lookup"
Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

server/src/l10n/IcuTranslator.cpp
@@ -173,7 +173,7 @@ string IcuTranslator::applyArgs(
 	return result.toUTF8String(utf8);
 }
 
-string IcuTranslator::lookaupAndApplyArgs(
+string IcuTranslator::lookupAndApplyArgs(
 		const string &originalKey,
 		vector<string>::const_iterator current,
 		const vector<string>::const_iterator end,
@@ -198,7 +198,7 @@ string IcuTranslator::lookaupAndApplyArgs(
 		return applyArgs(value, args);
 
 	case URES_TABLE:
-		return lookaupAndApplyArgs(originalKey, ++current, end, value, args);
+		return lookupAndApplyArgs(originalKey, ++current, end, value, args);
 
 	default:
 		logger().critical(
@@ -219,7 +219,7 @@ string IcuTranslator::formatImpl(
 	StringTokenizer keyList(key, ".",
 		StringTokenizer::TOK_IGNORE_EMPTY | StringTokenizer::TOK_TRIM);
 
-	return lookaupAndApplyArgs(key, keyList.begin(), keyList.end(), *m_bundle, args);
+	return lookupAndApplyArgs(key, keyList.begin(), keyList.end(), *m_bundle, args);
 }
 
 static IcuLocaleImpl asIcuLocale(const BeeeOn::Locale &locale)

server/src/l10n/IcuTranslator.h
@@ -28,7 +28,7 @@ protected:
 		const std::string &key,
 		const std::vector<Poco::Dynamic::Var> &args) override;
 
-	std::string lookaupAndApplyArgs(
+	std::string lookupAndApplyArgs(
 		const std::string &originalKey,
 		std::vector<std::string>::const_iterator current,
 		const std::vector<std::string>::const_iterator end,