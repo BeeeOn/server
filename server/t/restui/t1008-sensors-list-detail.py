b29ba81ab7ea7ea89d70ed9a3f11f0a9c6e9878a
Jan Viktorin <iviktorin@fit.vutbr.cz>
2017-09-19 19:08:38 +0200

    t: restui: test sensors
    Test the following endpoints:
    
    * sensors.list    - existing, non-existing device
    * sensors.detail  - existing, non-existing, with and without history
    * sensors.current - existing, non-existing, with and without history
    * sensors.history - range=hour, range=start,stop
    
    Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

---
+++ server/t/restui/t1008-sensors-list-detail.py
@@ -0,0 +1,263 @@
+#! /usr/bin/env python3
+
+import config
+config.import_libs()
+
+import unittest
+import socket
+import json
+from datetime import datetime
+from datetime import timedelta
+
+from rest import GET, POST, PUT, DELETE
+
+class TestSensorsListDetail(unittest.TestCase):
+	"""
+	Create a session for testing and assign the gateway 1284174504043136.
+	"""
+	def setUp(self):
+		req = POST(config.ui_host, config.ui_port, "/auth")
+		req.body(config.PERMIT_LOGIN)
+		response, content = req()
+
+		self.assertEqual(200, response.status)
+
+		result = json.loads(content)
+		self.assertEqual("success", result["status"])
+		self.session = result["data"]["id"]
+
+		req = POST(config.ui_host, config.ui_port, "/gateways")
+		req.authorize(self.session)
+		req.body(json.dumps(
+			{"id": config.gateway_id, "name": "Testing Gateway"}
+		))
+		response, _ = req()
+
+		self.assertEqual(201, response.status)
+
+	"""
+	Deassign the gateway 1284174504043136 and close the session.
+	"""
+	def tearDown(self):
+		req = DELETE(config.ui_host, config.ui_port, "/gateways/" + config.gateway_id)
+		req.authorize(self.session)
+		response, content = req()
+
+		self.assertEqual(204, response.status)
+
+		req = DELETE(config.ui_host, config.ui_port, "/auth")
+		req.authorize(self.session)
+		response, content = req()
+
+		self.assertEqual(204, response.status)
+
+	def assert_sensor(self, sensor, id, name, type, order, has_current = True):
+		self.assertEqual(id, sensor["id"])
+		self.assertEqual(name, sensor["display_name"])
+		self.assertEqual(type, sensor["type_id"])
+		self.assertEqual(order, sensor["order"])
+
+		if has_current:
+			self.assertTrue("current" in sensor)
+		else:
+			self.assertFalse("current" in sensor)
+
+	def assert_find_sensor(self, sensors, id, name, type, order, has_current = True):
+		for sensor in sensors:
+			if sensor["id"] == id:
+				self.assert_sensor(sensor, id, name, type, order, has_current)
+				return
+
+		self.fail("no such sensor with id " + id)
+
+	"""
+	List all available sensors of device 4698890485085053774. There should be
+	5 sensors as specifed in the testing devices.xml.
+	"""
+	def test1_list_sensors_of_device_temperature(self):
+		req = GET(config.ui_host, config.ui_port,
+				"/gateways/1284174504043136/devices/0x4135d00019f5234e/sensors")
+		req.authorize(self.session)
+		response, content = req()
+
+		self.assertEqual(200, response.status)
+
+		result = json.loads(content)
+		self.assertEqual("success", result["status"])
+		self.assertEqual(5, len(result["data"]))
+
+		self.assert_find_sensor(result["data"], "0", "room temperature", "19", "0")
+		self.assert_find_sensor(result["data"], "1", "outside temperature", "19", "1")
+		self.assert_find_sensor(result["data"], "2", "room humidity", "8", "2")
+		self.assert_find_sensor(result["data"], "3", "battery", "2", "3", False)
+		self.assert_find_sensor(result["data"], "4", "signal", "16", "4", False)
+
+	"""
+	Ask for details about temperature sensor of device 4698890485085053774.
+	"""
+	def test2_detail_room_temperature(self):
+		req = GET(config.ui_host, config.ui_port,
+				"/gateways/1284174504043136/devices/0x4135d00019f5234e/sensors/0")
+		req.authorize(self.session)
+		response, content = req()
+
+		self.assertEqual(200, response.status)
+
+		result = json.loads(content)
+		self.assertEqual("success", result["status"])
+
+		self.assert_sensor(result["data"], "0", "room temperature", "19", "0")
+		self.assertEqual("19.500000", result["data"]["current"]["value"])
+
+	"""
+	Ask for current value of the temperature sensor of device 4698890485085053774.
+	"""
+	def test3_current_room_temperature(self):
+		req = GET(config.ui_host, config.ui_port,
+				"/gateways/1284174504043136/devices/0x4135d00019f5234e/sensors/0/current")
+		req.authorize(self.session)
+		response, content = req()
+
+		self.assertEqual(200, response.status)
+
+		result = json.loads(content)
+		self.assertEqual("success", result["status"])
+
+		self.assertEqual("19.500000", result["data"]["value"])
+
+	"""
+	List all available sensors of device 4931887558509748622. There should be
+	9 sensors as specifed in the testing devices.xml.
+	"""
+	def test4_list_sensors_of_device_unknown(self):
+		req = GET(config.ui_host, config.ui_port,
+				"/gateways/1284174504043136/devices/0x4471959aad24618e/sensors")
+		req.authorize(self.session)
+		response, content = req()
+
+		self.assertEqual(200, response.status)
+
+		result = json.loads(content)
+		self.assertEqual("success", result["status"])
+		self.assertEqual(9, len(result["data"]))
+
+		self.assert_find_sensor(result["data"], "0", "temperature", "19", "0", False)
+		self.assert_find_sensor(result["data"], "1", "humidity", "8", "1", False)
+		self.assert_find_sensor(result["data"], "2", "noise", "11", "2", False)
+		self.assert_find_sensor(result["data"], "3", "temperature", "19", "3", False)
+		self.assert_find_sensor(result["data"], "4", "humidity", "8", "4", False)
+		self.assert_find_sensor(result["data"], "5", "noise", "11", "5", False)
+		self.assert_find_sensor(result["data"], "6", "temperature", "19", "6", False)
+		self.assert_find_sensor(result["data"], "7", "humidity", "8", "7", False)
+		self.assert_find_sensor(result["data"], "8", "noise", "11", "8", False)
+
+	"""
+	Ask for details about temperature sensor of device 4931887558509748622.
+	"""
+	def test5_detail_unknown_temperature(self):
+		req = GET(config.ui_host, config.ui_port,
+				"/gateways/1284174504043136/devices/0x4471959aad24618e/sensors/0")
+		req.authorize(self.session)
+		response, content = req()
+
+		self.assertEqual(200, response.status)
+
+		result = json.loads(content)
+		self.assertEqual("success", result["status"])
+
+		self.assert_sensor(result["data"], "0", "temperature", "19", "0", False)
+		self.assertEqual("zone 1", result["data"]["group"])
+
+	"""
+	Ask for current value of the temperature sensor of device 4931887558509748622.
+	"""
+	def test6_current_unknown_temperature(self):
+		req = GET(config.ui_host, config.ui_port,
+				"/gateways/1284174504043136/devices/0x4471959aad24618e/sensors/0/current")
+		req.authorize(self.session)
+		response, content = req()
+
+		self.assertEqual(200, response.status)
+
+		result = json.loads(content)
+		self.assertEqual("success", result["status"])
+		self.assertTrue("data" in result)
+		self.assertIsNone(result["data"])
+
+	"""
+	There is no sensor of id 10 for device 4931887558509748622.
+	"""
+	def test7_detail_non_existing_sensor(self):
+		req = GET(config.ui_host, config.ui_port,
+				"/gateways/1284174504043136/devices/0x4471959aad24618e/sensors/10")
+		req.authorize(self.session)
+		response, content = req()
+
+		self.assertEqual(404, response.status)
+
+	"""
+	There is no sensor of id 10 for device 4931887558509748622.
+	"""
+	def test8_current_non_existing_sensor(self):
+		req = GET(config.ui_host, config.ui_port,
+				"/gateways/1284174504043136/devices/0x4471959aad24618e/sensors/10/current")
+		req.authorize(self.session)
+		response, content = req()
+
+		self.assertEqual(404, response.status)
+
+	"""
+	There is no such device 4931887558509748621.
+	"""
+	def test9_detail_non_existing_device(self):
+		req = GET(config.ui_host, config.ui_port,
+				"/gateways/1284174504043136/devices/0x4471959aad24618f/sensors/10")
+		req.authorize(self.session)
+		response, content = req()
+
+		self.assertEqual(404, response.status)
+
+	"""
+	There is no such device 4931887558509748621.
+	"""
+	def testa_list_non_existing_device(self):
+		req = GET(config.ui_host, config.ui_port,
+				"/gateways/1284174504043136/devices/0x4471959aad24618f/sensors")
+		req.authorize(self.session)
+		response, content = req()
+
+		self.assertEqual(404, response.status)
+
+	def testb_history_room_temperature_1hour(self):
+		req = GET(config.ui_host, config.ui_port,
+				"/gateways/1284174504043136/devices/0x4135d00019f5234e/sensors/0/history" +
+				"?range=hour")
+		req.authorize(self.session)
+		response, content = req()
+
+		self.assertEqual(200, response.status)
+
+		result = json.loads(content)
+		self.assertEqual("success", result["status"])
+		self.assertEqual(14, len(result["data"]))
+
+		# the same request with explicit timestamps
+		start = datetime.now() - timedelta(0, 60 * 60)
+		end = datetime.now()
+
+		req = GET(config.ui_host, config.ui_port,
+				"/gateways/1284174504043136/devices/0x4135d00019f5234e/sensors/0/history" +
+				"?range=%s,%s" % (start.strftime("%s"), end.strftime("%s")))
+		req.authorize(self.session)
+		response, content = req()
+
+		self.assertEqual(200, response.status)
+
+		result = json.loads(content)
+		self.assertEqual("success", result["status"])
+		self.assertEqual(14, len(result["data"]))
+
+if __name__ == '__main__':
+	import sys
+	import taprunner
+	unittest.main(testRunner=taprunner.TAPTestRunner(stream = sys.stdout))
