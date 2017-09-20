BeeeOn/server

commit 2421a7d8b58cc40595985fb03111f7e2d9f65492

PocoSQLSensorHistoryDao: new class
Implement basic queries:

* sensors_history.insert - insert single or many values
* sensors_history.fetch - fetch the most recent value
* sensors_history.fetch.huge - fetch potentially a huge
                               amount of history data

Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

server/src/dao/poco/PocoSQLSensorHistoryDao.cpp
@@ -1,0 +1,189 @@
+#include <Poco/Exception.h>
+#include <Poco/Timestamp.h>
+#include <Poco/Data/Statement.h>
+#include <Poco/Data/RecordSet.h>
+#include <Poco/Data/Row.h>
+#include <Poco/Data/RowIterator.h>
+
+#include "model/Device.h"
+#include "model/ModuleInfo.h"
+#include "model/ModuleValue.h"
+#include "dao/poco/PocoSQLSensorHistoryDao.h"
+#include "dao/poco/PocoDaoManager.h"
+#include "service/ValueConsumer.h"
+#include "util/TimeInterval.h"
+
+using namespace std;
+using namespace Poco;
+using namespace Poco::Data;
+using namespace Poco::Data::Keywords;
+using namespace BeeeOn;
+
+BEEEON_OBJECT(PocoSQLSensorHistoryDao, BeeeOn::PocoSQLSensorHistoryDao)
+
+PocoSQLSensorHistoryDao::PocoSQLSensorHistoryDao()
+{
+	registerQuery(m_queryInsert);
+	registerQuery(m_queryFetch);
+	registerQuery(m_queryFetchHuge);
+}
+
+bool PocoSQLSensorHistoryDao::insert(
+		const Device &device,
+		const Timestamp &at,
+		const ModuleValue &value)
+{
+	assureHasId(device);
+	assureHasId(device.gateway());
+
+	UInt64 deviceID(device.id());
+	UInt64 gatewayID(device.gateway().id());
+	unsigned int moduleID(value.module());
+	unsigned long timeAt = at.epochTime();
+	double v = value.value();
+
+	Statement sql = (session() << m_queryInsert(),
+		use(gatewayID, "gateway_id"),
+		use(deviceID, "device_id"),
+		use(moduleID, "module_id"),
+		use(timeAt, "at"),
+		use(v, "value")
+	);
+
+	if (execute(sql) == 0)
+		return false;
+
+	return true;
+}
+
+
+void PocoSQLSensorHistoryDao::insertMany(
+	const Device &device,
+	const Timestamp &at,
+	std::vector<ModuleValue> &values)
+{
+	assureHasId(device);
+	assureHasId(device.gateway());
+
+	if (values.empty())
+		return;
+
+	UInt64 deviceID(device.id());
+	UInt64 gatewayID(device.gateway().id());
+	unsigned int moduleID;
+	unsigned long timeAt = at.epochTime();
+	double v;
+
+	Statement sql = (session() << m_queryInsert(),
+		use(gatewayID, "gateway_id"),
+		use(deviceID, "device_id"),
+		use(moduleID, "module_id"),
+		use(timeAt, "at"),
+		use(v, "value")
+	);
+
+	for (auto &value : values) {
+		moduleID = value.module();
+		v = value.value();
+
+		if (execute(sql) == 0)
+			throw IllegalStateException(string(__func__) + " returned 0");
+	}
+}
+
+bool PocoSQLSensorHistoryDao::fetch(
+	const Device &device,
+	const ModuleInfo &module,
+	Timestamp &at,
+	double &value)
+{
+	assureHasId(device);
+	assureHasId(device.gateway());
+	// cannot assureHasId for module, SimpleID == 0 is allowed
+
+	UInt64 deviceID(device.id());
+	UInt64 gatewayID(device.gateway().id());
+	unsigned int moduleID(module.id());
+	unsigned long timeAt;
+
+	Statement sql = (session() << m_queryFetch(),
+		use(gatewayID, "gateway_id"),
+		use(deviceID, "device_id"),
+		use(moduleID, "module_id"),
+		into(timeAt),
+		into(value)
+	);
+
+	if (execute(sql) == 0)
+		return false;
+
+	at = timeAt;
+	return true;
+}
+
+void PocoSQLSensorHistoryDao::fetchHuge(
+	const Device &device,
+	const ModuleInfo &module,
+	const TimeInterval &range,
+	const Timespan &interval,
+	const Aggregator agg,
+	ValueConsumer &consumer)
+{
+	assureHasId(device);
+	assureHasId(device.gateway());
+	// cannot assureHasId for module, SimpleID == 0 is allowed
+
+	assureValid(agg);
+
+	UInt64 deviceID(device.id());
+	UInt64 gatewayID(device.gateway().id());
+	unsigned int moduleID(module.id());
+	unsigned long start = range.start().epochTime();
+	unsigned long end = range.end().epochTime();
+	unsigned long intervalSeconds = interval.totalSeconds();
+	unsigned long timeAt;
+	double avg;
+	double min;
+	double max;
+
+	Statement sql = (session() << m_queryFetchHuge(),
+		use(intervalSeconds, "at_div"),
+		use(intervalSeconds, "at_mul"),
+		use(gatewayID, "gateway_id"),
+		use(deviceID, "device_id"),
+		use(moduleID, "module_id"),
+		use(start, "start"),
+		use(end, "end"),
+		use(intervalSeconds, "interval"),
+		into(timeAt),
+		into(avg),
+		into(min),
+		into(max),
+		Poco::Data::Keywords::range(0, 1)
+	);
+
+	consumer.begin(*module.type());
+
+	try {
+		while (!sql.done()) {
+			execute(sql);
+
+			switch (agg) {
+			case AGG_AVG:
+				consumer.single(ValueAt(timeAt, avg));
+				break;
+			case AGG_MIN:
+				consumer.single(ValueAt(timeAt, min));
+				break;
+			case AGG_MAX:
+				consumer.single(ValueAt(timeAt, max));
+				break;
+			}
+		}
+	} catch (...) {
+		consumer.end();
+		throw;
+	}
+
+	consumer.end();
+}

server/conf/logging.ini
@@ -109,6 +109,9 @@ loggers.PocoSQLUserDao.level = debug
 loggers.PocoSQLRoleInGatewayDao.name = BeeeOn::PocoSQLRoleInGatewayDao
 loggers.PocoSQLRoleInGatewayDao.level = debug
 
+loggers.PocoSQLSensorHistoryDao.name = BeeeOn::PocoSQLSensorHistoryDao
+loggers.PocoSQLSensorHistoryDao.level = notice
+
 ; Providers
 
 loggers.GoogleAuthProvider.name = BeeeOn::GoogleAuthProvider

server/conf/services.xml
@@ -220,6 +220,7 @@
 		<alias name="identityDao" ref="sqlIdentityDao" />
 		<alias name="verifiedIdentityDao" ref="sqlVerifiedIdentityDao" />
 		<alias name="roleInGatewayDao" ref="sqlRoleInGatewayDao" />
+		<alias name="sensorHistoryDao" ref="sqlSensorHistoryDao" />
 
 		<instance name="pocoDaoManager"
 				class="BeeeOn::PocoDaoManager">
@@ -281,6 +282,12 @@
 			<set name="daoManager" ref="pocoDaoManager" />
 			<set name="transactionManager" ref="transactionManager" />
 			<set name="deviceInfoProvider" ref="deviceInfoProvider" />
+			<set name="sqlLoader" ref="queries" />
+		</instance>
+
+		<instance name="sqlSensorHistoryDao" class="BeeeOn::PocoSQLSensorHistoryDao">
+			<set name="daoManager" ref="pocoDaoManager" />
+			<set name="transactionManager" ref="transactionManager" />
 			<set name="sqlLoader" ref="queries" />
 		</instance>
 

server/schema/home8-query.xml
@@ -1993,4 +1993,203 @@
 		</test>
 	</query>
 
+	<query id="sensors_history.insert">
+		<define>
+		INSERT INTO sensors_history (gateway_id, device_id, module_id, at, value)
+		VALUES (
+			<value name="gateway_id" type="int64" />,
+			<value name="device_id" type="int64" />,
+			<value name="module_id" type="smallint" />,
+			<value name="at" type="bigint" />,
+			<value name="value" type="real" />
+		)
+		</define>
+		<test name="insert values">
+			<check name="for Test Device B, module 0">
+				<call-query>
+					<arg name="gateway_id">181462068183316</arg>
+					<arg name="device_id">2728733371</arg>
+					<arg name="module_id">0</arg>
+					<arg name="at">1483416275</arg>
+					<arg name="value">12.2</arg>
+				</call-query>
+				<sql>
+				SELECT at, value FROM sensors_history WHERE
+					gateway_id = 181462068183316
+					AND
+					device_id = 2728733371;
+				</sql>
+				<expect>
+					<row>
+						<value name="at">1483416275</value>
+						<value name="value">12.2</value>
+					</row>
+				</expect>
+			</check>
+			<teardown name="delete the single value for Test Device B">
+				<sql>DELETE FROM sensors_history WHERE
+					gateway_id = 181462068183316 AND device_id = 2728733371</sql>
+			</teardown>
+		</test>
+	</query>
+
+	<query id="sensors_history.fetch">
+		<define>
+		SELECT at, value
+		FROM sensors_history
+		WHERE
+			gateway_id = <value name="gateway_id" type="int64" />
+			AND
+			device_id = <value name="device_id" type="int64" />
+			AND
+			module_id = <value name="module_id" type="smallint" />
+		ORDER BY at DESC
+		LIMIT 1
+		</define>
+		<test name="fetch last value">
+			<check name="recent value is 29">
+				<call-query>
+					<arg name="gateway_id">181462068183316</arg>
+					<arg name="device_id">2731287621</arg>
+					<arg name="module_id">0</arg>
+				</call-query>
+				<expect>
+					<row>
+						<value name="at">1483441918</value>
+						<value type="real" name="value">29</value>
+					</row>
+				</expect>
+			</check>
+			<check name="no value">
+				<call-query>
+					<arg name="gateway_id">181462068183316</arg>
+					<arg name="device_id">2751463465</arg>
+					<arg name="module_id">0</arg>
+				</call-query>
+				<expect />
+			</check>
+		</test>
+	</query>
+
+	<query id="sensors_history.fetch.huge.interval">
+		<!--
+		To stay as engine-independent as possible, pass the interval 3 times
+		as at_div, at_mul and interval. This might be fixed in the future.
+		Think that some engines require '?' placeholders for prepared statements.
+		Others allows named placeholders where the order matters.
+		-->
+		<define>
+		SELECT
+			(at / <value name="at_div" type="bigint" />) * <value name="at_mul" type="bigint" /> AS at,
+			AVG(value) AS avg_value,
+			MIN(value) AS min_value,
+			MAX(value) AS max_value
+		FROM sensors_history
+		WHERE
+			gateway_id = <value name="gateway_id" type="int64" />
+			AND
+			device_id = <value name="device_id" type="int64" />
+			AND
+			module_id = <value name="module_id" type="smallint" />
+			AND
+			at &gt;= <value name="start" type="bigint" />
+			AND
+			at &lt;= <value name="end" type="bigint" />
+		GROUP BY (at / <value name="interval" type="bigint" />)
+		ORDER BY at DESC
+		</define>
+		<test name="all values constant">
+			<setup name="setup simple history of module 21">
+				<sql>
+				INSERT INTO sensors_history (gateway_id, device_id, module_id, at, value)
+				VALUES
+					(181462068183316, 2751463465, 21, 0, 4),
+					(181462068183316, 2751463465, 21, 5, 16),
+					(181462068183316, 2751463465, 21, 15, 10),
+					(181462068183316, 2751463465, 21, 20, 10)
+				</sql>
+			</setup>
+			<check name="trivial (no) aggregation">
+				<call-query>
+					<arg name="at_div">5</arg>
+					<arg name="at_mul">5</arg>
+					<arg name="gateway_id">181462068183316</arg>
+					<arg name="device_id">2751463465</arg>
+					<arg name="module_id">21</arg>
+					<arg name="start">0</arg>
+					<arg name="end">40</arg>
+					<arg name="interval">5</arg>
+				</call-query>
+				<expect>
+					<row>
+						<value name="at">20</value>
+						<value type="real" name="avg_value">10</value>
+						<value type="real" name="min_value">10</value>
+						<value type="real" name="max_value">10</value>
+					</row>
+					<row>
+						<value name="at">15</value>
+						<value type="real" name="avg_value">10</value>
+						<value type="real" name="min_value">10</value>
+						<value type="real" name="max_value">10</value>
+					</row>
+					<row>
+						<value name="at">5</value>
+						<value type="real" name="avg_value">16</value>
+						<value type="real" name="min_value">16</value>
+						<value type="real" name="max_value">16</value>
+					</row>
+					<row>
+						<value name="at">0</value>
+						<value type="real" name="avg_value">4</value>
+						<value type="real" name="min_value">4</value>
+						<value type="real" name="max_value">4</value>
+					</row>
+				</expect>
+			</check>
+			<check name="aggregation is multiple of 5 (note that we have no data between 5 and 15)">
+				<call-query>
+					<arg name="at_div">10</arg>
+					<arg name="at_mul">10</arg>
+					<arg name="gateway_id">181462068183316</arg>
+					<arg name="device_id">2751463465</arg>
+					<arg name="module_id">21</arg>
+					<arg name="start">0</arg>
+					<arg name="end">40</arg>
+					<arg name="interval">10</arg>
+				</call-query>
+				<expect>
+					<row>
+						<value name="at">20</value>
+						<value type="real" name="avg_value">10</value>
+						<value type="real" name="min_value">10</value>
+						<value type="real" name="max_value">10</value>
+					</row>
+					<row>
+						<value name="at">10</value>
+						<value type="real" name="avg_value">10</value>
+						<value type="real" name="min_value">10</value>
+						<value type="real" name="max_value">10</value>
+					</row>
+					<row>
+						<value name="at">0</value>
+						<value type="real" name="avg_value">10</value>
+						<value type="real" name="min_value">4</value>
+						<value type="real" name="max_value">16</value>
+					</row>
+				</expect>
+			</check>
+			<teardown name="delete testing values of module 21">
+				<sql>
+				DELETE FROM sensors_history WHERE
+					gateway_id = 181462068183316
+					AND
+					device_id = 2751463465
+					AND
+					module_id = 21
+				</sql>
+			</teardown>
+		</test>
+	</query>
+
 </query-set>

server/src/CMakeLists.txt
@@ -65,6 +65,7 @@ file(GLOB SOURCES
 	${PROJECT_SOURCE_DIR}/dao/poco/PocoSQLGatewayDao.cpp
 	${PROJECT_SOURCE_DIR}/dao/poco/PocoSQLLocationDao.cpp
 	${PROJECT_SOURCE_DIR}/dao/poco/PocoSQLDeviceDao.cpp
+	${PROJECT_SOURCE_DIR}/dao/poco/PocoSQLSensorHistoryDao.cpp
 	${PROJECT_SOURCE_DIR}/dao/ConnectorLoader.cpp
 	${PROJECT_SOURCE_DIR}/dao/SQLLoader.cpp
 	${PROJECT_SOURCE_DIR}/dao/SQLQuery.cpp

server/src/dao/poco/PocoSQLSensorHistoryDao.h
@@ -1,0 +1,54 @@
+#ifndef BEEEON_POCO_SQL_SENSOR_HISTORY_DAO_H
+#define BEEEON_POCO_SQL_SENSOR_HISTORY_DAO_H
+
+#include "dao/SensorHistoryDao.h"
+#include "dao/SQLQuery.h"
+#include "dao/poco/PocoAbstractDao.h"
+
+namespace Poco {
+namespace Data {
+
+class RecordSet;
+class Row;
+
+}
+}
+
+namespace BeeeOn {
+
+class PocoSQLSensorHistoryDao :
+	public PocoAbstractDao,
+	public SensorHistoryDao {
+public:
+	PocoSQLSensorHistoryDao();
+
+	bool insert(
+		const Device &device,
+		const Poco::Timestamp &at,
+		const ModuleValue &value) override;
+	void insertMany(
+		const Device &device,
+		const Poco::Timestamp &at,
+		std::vector<ModuleValue> &values) override;
+	bool fetch(
+		const Device &device,
+		const ModuleInfo &module,
+		Poco::Timestamp &at,
+		double &value) override;
+	void fetchHuge(
+		const Device &device,
+		const ModuleInfo &module,
+		const TimeInterval &range,
+		const Poco::Timespan &interval,
+		const Aggregator agg,
+		ValueConsumer &consumer) override;
+
+private:
+	SQLQuery m_queryInsert    {"sensors_history.insert"};
+	SQLQuery m_queryFetch     {"sensors_history.fetch"};
+	SQLQuery m_queryFetchHuge {"sensors_history.fetch.huge.interval"};
+};
+
+}
+
+#endif

commit 76b17337ab8b3106c6e59e31f93c96b0957f8a2f

PocoSQLSensorHistoryDao: fix parsing of timestamp
The method fetch() has never been properly tested. The method did not
convert the sensor data timestamp correctly back to the Poco::Timestamp.

Fixes: 2421a7d8b58c ("PocoSQLSensorHistoryDao: new class")
Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

server/src/dao/poco/PocoSQLSensorHistoryDao.cpp
@@ -123,7 +123,7 @@ bool PocoSQLSensorHistoryDao::fetch(
 	if (result.rowCount() == 0)
 		return false;
 
-	at = timeAt;
+	at = Timestamp::fromEpochTime(timeAt);
 	return true;
 }
 