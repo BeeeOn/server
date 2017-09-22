BeeeOn/server

commit 517a0cd46501fe49203ef02b9f060d6ebc977757

SensorRestHandler: new class
Define endpoints:

* sensors.list    -> GET /gateways/$gateway_id/devices/$device_id/sensors
* sensors.detail  -> GET /gateways/$gateway_id/devices/$device_id/sensors/$sensor_id
* sensors.current -> GET /gateways/$gateway_id/devices/$device_id/sensors/$sensor_id/current
* sensors.history -> GET /gateways/$gateway_id/devices/$device_id/sensors/$sensor_id/history

Signed-off-by: Martin Matejčík <xmatej46@stud.fit.vutbr.cz>
Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

server/src/restui/SensorRestHandler.cpp
@@ -1,0 +1,227 @@
+#include <list>
+
+#include <Poco/Exception.h>
+#include <Poco/StringTokenizer.h>
+#include <Poco/JSON/PrintHandler.h>
+
+#include "di/Injectable.h"
+#include "model/Sensor.h"
+#include "model/User.h"
+#include "model/ValueAt.h"
+#include "rest/RestFlow.h"
+#include "rest/RestLinker.h"
+#include "restui/JSONGatewayDeserializer.h"
+#include "restui/RestValueConsumer.h"
+#include "restui/SensorRestHandler.h"
+#include "restui/Serializing.h"
+#include "util/JsonUtil.h"
+#include "util/Sanitize.h"
+
+BEEEON_OBJECT_BEGIN(BeeeOn, RestUI, SensorRestHandler)
+BEEEON_OBJECT_CASTABLE(RestHandler)
+BEEEON_OBJECT_REF("sensorService", &SensorRestHandler::setSensorService)
+BEEEON_OBJECT_REF("sensorHistoryService", &SensorRestHandler::setSensorHistoryService)
+BEEEON_OBJECT_REF("translatorFactory", &SensorRestHandler::setTranslatorFactory)
+BEEEON_OBJECT_END(BeeeOn, RestUI, SensorRestHandler)
+
+using namespace std;
+using namespace Poco;
+using namespace Poco::JSON;
+using namespace BeeeOn;
+using namespace BeeeOn::RestUI;
+
+SensorRestHandler::SensorRestHandler():
+	JSONRestHandler("sensors")
+{
+	registerAction<SensorRestHandler>("list", &SensorRestHandler::list, {"gateway_id", "device_id"});
+	registerAction<SensorRestHandler>("detail", &SensorRestHandler::detail, {"gateway_id", "device_id", "sensor_id"});
+	registerAction<SensorRestHandler>("current", &SensorRestHandler::current, {"gateway_id", "device_id", "sensor_id"});
+	registerAction<SensorRestHandler>("history", &SensorRestHandler::history, {"gateway_id", "device_id", "sensor_id"});
+}
+
+void SensorRestHandler::setSensorService(SensorService::Ptr service)
+{
+	m_sensorService = service;
+}
+
+void SensorRestHandler::setSensorHistoryService(SensorHistoryService::Ptr service)
+{
+	m_sensorHistoryService = service;
+}
+
+void SensorRestHandler::setTranslatorFactory(TranslatorFactory::Ptr factory)
+{
+	m_translatorFactory = factory;
+}
+
+void SensorRestHandler::list(RestFlow &flow)
+{
+	Translator::Ptr translator =
+		m_translatorFactory->create(flow.session()->locale());
+
+	User user(flow.session()->userID());
+	Gateway gateway(GatewayID::parse(flow.param("gateway_id")));
+	Device device(DeviceID::parse(flow.param("device_id")));
+	device.setGateway(gateway);
+	std::list<Sensor> sensors;
+	Relation<std::list<Sensor>, Device> data(sensors, device);
+	data.setUser(user);
+
+	m_sensorService->fetchMany(data);
+
+	PrintHandler result(flow.response().stream());
+	beginSuccess(result, 200);
+	serialize(result, *translator, sensors);
+	endSuccess(result);
+}
+
+void SensorRestHandler::prepareDetail(RestFlow &flow, Sensor &sensor)
+{
+	User user(flow.session()->userID());
+
+	Gateway gateway(GatewayID::parse(flow.param("gateway_id")));
+	Device device(DeviceID::parse(flow.param("device_id")));
+	device.setGateway(gateway);
+	sensor.setId(SensorID::parse(flow.param("sensor_id")));
+
+	Relation<Sensor, Device> data(sensor, device);
+	data.setUser(user);
+
+	if (!m_sensorService->fetch(data))
+		throw NotFoundException("no such sensor " + sensor);
+}
+
+void SensorRestHandler::detail(RestFlow &flow)
+{
+	Translator::Ptr translator =
+		m_translatorFactory->create(flow.session()->locale());
+
+	Sensor sensor;
+	prepareDetail(flow, sensor);
+
+	PrintHandler result(flow.response().stream());
+	beginSuccess(result, 200);
+	serialize(result, *translator, sensor);
+	endSuccess(result);
+}
+
+void SensorRestHandler::current(RestFlow &flow)
+{
+	Sensor sensor;
+	prepareDetail(flow, sensor);
+
+	PrintHandler result(flow.response().stream());
+	beginSuccess(result, 200);
+
+	if (sensor.at().isNull()) {
+		result.null();
+	}
+	else {
+		RestValueConsumer consumer(result);
+		consumer.begin(*sensor.info().type());
+		consumer.single(ValueAt(sensor.at().value(), sensor.value()));
+		consumer.end();
+	}
+
+	endSuccess(result);
+}
+
+TimeInterval SensorRestHandler::parseRange(const string &input) const
+{
+	if (input == "hour")
+		return TimeInterval::past(1 * Timespan::HOURS);
+	if (input == "day")
+		return TimeInterval::past(1 * Timespan::DAYS);
+	if (input == "week")
+		return TimeInterval::past(7 * Timespan::DAYS);
+	if (input == "month")
+		return TimeInterval::past(30 * Timespan::DAYS);
+
+	StringTokenizer range(input, ",",
+		StringTokenizer::TOK_TRIM | StringTokenizer::TOK_IGNORE_EMPTY);
+
+	if (range.count() == 1) {
+		const Timespan seconds =
+			NumberParser::parseUnsigned64(range[0]) * Timespan::SECONDS;
+		return TimeInterval::past(seconds);
+	}
+	else if (range.count() == 2) {
+		const Timestamp start = Timestamp::fromEpochTime(
+			NumberParser::parseUnsigned64(range[0]));
+		const Timestamp end = Timestamp::fromEpochTime(
+			NumberParser::parseUnsigned64(range[1]));
+
+		return TimeInterval(start, end);
+	}
+
+	throw InvalidArgumentException("invalid range: " + input);
+}
+
+Timespan SensorRestHandler::parseInterval(const string &input) const
+{
+	return NumberParser::parseUnsigned(input) * Timespan::SECONDS;
+}
+
+string SensorRestHandler::parseAggregation(const string &input) const
+{
+	if (input == "avg")
+		return input;
+	if (input == "min")
+		return input;
+	if (input == "max")
+		return input;
+
+	throw InvalidArgumentException("invalid aggregation: " + input);
+}
+
+void SensorRestHandler::history(RestFlow &flow)
+{
+	User user(flow.session()->userID());
+	Gateway gateway(GatewayID::parse(flow.param("gateway_id")));
+	Device device(DeviceID::parse(flow.param("device_id")));
+	device.setGateway(gateway);
+	ModuleInfo module;
+	module.setId(ModuleInfoID::parse(flow.param("sensor_id")));
+	Relation<ModuleInfo, Device> input(module, device);
+	input.setUser(user);
+
+	URI::QueryParameters queryParams = flow.uri().getQueryParameters();
+
+	TimeInterval range = TimeInterval::past(1 * Timespan::HOURS);
+	string aggregation = "avg";
+	Timespan interval = 15 * Timespan::SECONDS;
+
+	for (auto &item : queryParams) {
+		if (item.first == "range")
+			range = parseRange(Sanitize::common(item.second));
+		else if (item.first == "aggregation")
+			aggregation = parseAggregation(Sanitize::strict(item.second));
+		else if (item.first == "interval")
+			interval = parseInterval(Sanitize::strict(item.second));
+		else
+			throw InvalidArgumentException("unknown query parameter");
+	}
+
+	PrintHandler result(flow.response().stream());
+	beginSuccess(result, 200);
+	result.startArray();
+
+	RestValueConsumer consumer(result);
+
+	try {
+		m_sensorHistoryService->fetchRange(input, range, interval, aggregation, consumer);
+	}
+	catch (...) {
+		// in case of a failure, finish the probably incomplete result
+		// to at least not break the client processing, it is too late
+		// to signalize any failures
+		result.endArray();
+		endSuccess(result);
+
+		throw; // log the failure by upper layers
+	}
+
+	result.endArray();
+	endSuccess(result);
+}
+

server/conf/config.d/factory-restui.xml
@@ -30,6 +30,7 @@
 			<add name="handlers" ref="uiRoleHandler" />
 			<add name="handlers" ref="uiWorkHandler" />
 			<add name="handlers" ref="uiDeviceHandler" />
+			<add name="handlers" ref="uiSensorHandler" />
 			<add name="handlers" ref="uiTypeHandler" />
 		</instance>
 
@@ -63,6 +64,12 @@
 		<instance name="uiDeviceHandler" class="BeeeOn::RestUI::DeviceRestHandler">
 			<set name="deviceService" ref="deviceService" />
 			<set name="gatewayService" ref="gatewayService" />
+		</instance>
+
+		<instance name="uiSensorHandler" class="BeeeOn::RestUI::SensorRestHandler">
+			<set name="sensorService" ref="sensorService" />
+			<set name="sensorHistoryService" ref="sensorHistoryService" />
+			<set name="translatorFactory" ref="translatorFactory" />
 		</instance>
 
 		<instance name="uiTypeHandler" class="BeeeOn::RestUI::TypeRestHandler">

server/conf/restui-api.ini
@@ -106,3 +106,15 @@ api.call.types.list.caching = 60
 
 api.call.types.detail.uri = /types/$type_id
 api.call.types.detail.caching = 60
+
+api.call.sensors.list.uri = /gateways/$gateway_id/devices/$device_id/sensors
+api.call.sensors.list.caching = 60
+
+api.call.sensors.detail.uri = /gateways/$gateway_id/devices/$device_id/sensors/$sensor_id
+api.call.sensors.detail.caching = 60
+
+api.call.sensors.current.uri = /gateways/$gateway_id/devices/$device_id/sensors/$sensor_id/current
+api.call.sensors.current.caching = 60
+
+api.call.sensors.history.uri = /gateways/$gateway_id/devices/$device_id/sensors/$sensor_id/history
+api.call.sensors.history.caching = 60

server/src/CMakeLists.txt
@@ -221,6 +221,7 @@ file(GLOB RESTUI_SOURCES
 	restui/ProfileRestHandler.cpp
 	restui/RestValueConsumer.cpp
 	restui/RoleRestHandler.cpp
+	restui/SensorRestHandler.cpp
 	restui/TypeRestHandler.cpp
 	restui/GatewayRestHandler.cpp
 	restui/WorkRestHandler.cpp

server/src/restui/SensorRestHandler.h
@@ -1,0 +1,48 @@
+#ifndef BEEEON_SENSOR_REST_HANDLER_H
+#define BEEEON_SENSOR_REST_HANDLER_H
+
+#include <Poco/Timespan.h>
+
+#include "l10n/Translator.h"
+#include "rest/JSONRestHandler.h"
+#include "service/SensorService.h"
+#include "service/SensorHistoryService.h"
+#include "util/TimeInterval.h"
+
+namespace BeeeOn {
+
+class Sensor;
+
+namespace RestUI {
+
+class SensorRestHandler : public JSONRestHandler {
+public:
+	SensorRestHandler();
+
+	void setSensorService(SensorService::Ptr service);
+	void setSensorHistoryService(SensorHistoryService::Ptr service);
+	void setTranslatorFactory(TranslatorFactory::Ptr factory);
+
+	void list(RestFlow &flow);
+	void detail(RestFlow &flow);
+	void current(RestFlow &flow);
+	void history(RestFlow &flow);
+
+protected:
+	void prepareDetail(RestFlow &flow, Sensor &sensor);
+
+private:
+	TimeInterval parseRange(const std::string &input) const;
+	Poco::Timespan parseInterval(const std::string &input) const;
+	std::string parseAggregation(const std::string &input) const;
+
+private:
+	SensorService::Ptr m_sensorService;
+	SensorHistoryService::Ptr m_sensorHistoryService;
+	TranslatorFactory::Ptr m_translatorFactory;
+};
+
+}
+}
+
+#endif

server/src/restui/Serializing.cpp
@@ -8,9 +8,12 @@
 #include "model/Gateway.h"
 #include "model/LegacyRoleInGateway.h"
 #include "model/Location.h"
+#include "model/ModuleInfo.h"
 #include "model/RoleInGateway.h"
+#include "model/Sensor.h"
 #include "model/User.h"
 #include "model/VerifiedIdentity.h"
+#include "restui/RestValueConsumer.h"
 #include "restui/Serializing.h"
 #include "work/Work.h"
 
@@ -424,6 +427,68 @@ void BeeeOn::RestUI::serialize(Poco::JSON::PrintHandler &output,
 		SharedPtr<TypeInfo> info = *it;
 		serialize(output, translator, *info);
 	}
+
+	output.endArray();
+}
+
+void BeeeOn::RestUI::serialize(Poco::JSON::PrintHandler &output,
+		Translator &translator,
+		const Sensor &sensor)
+{
+	const ModuleInfo &info = sensor.info();
+
+	output.startObject();
+
+	output.key("id");
+	output.value(sensor.id().toString());
+
+	if (!sensor.name().empty()) {
+		output.key("display_name");
+		output.value(sensor.name());
+	}
+	else if (!info.name().empty()) {
+		output.key("display_name");
+		output.value(translator.format(
+			"modules." + info.name() + ".label"));
+	}
+	else {
+		output.key("display_name");
+		output.value(translator.format(
+			"types." + info.type()->name() + ".label"));
+	}
+
+	output.key("type_id");
+	output.value(info.type()->id().toString());
+
+	if (!info.group().empty()) {
+		output.key("group");
+		output.value(translator.format(
+			"modules.groups." + info.group() + ".label"));
+	}
+
+	output.key("order");
+	output.value(info.id().toString());
+
+	if (!sensor.at().isNull()) {
+		output.key("current");
+
+		RestValueConsumer consumer(output);
+		consumer.begin(*sensor.info().type());
+		consumer.single(ValueAt(sensor.at().value(), sensor.value()));
+		consumer.end();
+	}
+
+	output.endObject();
+}
+
+void BeeeOn::RestUI::serialize(Poco::JSON::PrintHandler &output,
+		Translator &translator,
+		const list<Sensor> &sensors)
+{
+	output.startArray();
+
+	for (auto &sensor : sensors)
+		serialize(output, translator, sensor);
 
 	output.endArray();
 }

server/src/restui/Serializing.h
@@ -23,6 +23,7 @@ class LegacyRoleInGateway;
 class Location;
 class VerifiedIdentity;
 class RoleInGateway;
+class Sensor;
 class Translator;
 class Work;
 
@@ -56,6 +57,13 @@ void serialize(Poco::JSON::PrintHandler &output,
 		Translator &translator,
 		const TypeInfoProvider::InfoSet::const_iterator begin,
 		const TypeInfoProvider::InfoSet::const_iterator end);
+
+void serialize(Poco::JSON::PrintHandler &output,
+		Translator &translator,
+		const Sensor &sensor);
+void serialize(Poco::JSON::PrintHandler &output,
+		Translator &translator,
+		const std::list<Sensor> &sensors);
 
 }
 

commit e59bbae35d271e0fd67bc8a8c52bc04c7553643a

SensorRestHandler: remove unused includes
Fixes: 517a0cd46501 ("SensorRestHandler: new class")
Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

server/src/restui/SensorRestHandler.cpp
@@ -10,11 +10,9 @@
 #include "model/ValueAt.h"
 #include "rest/RestFlow.h"
 #include "rest/RestLinker.h"
-#include "restui/JSONGatewayDeserializer.h"
 #include "restui/RestValueConsumer.h"
 #include "restui/SensorRestHandler.h"
 #include "restui/Serializing.h"
-#include "util/JsonUtil.h"
 #include "util/Sanitize.h"
 
 BEEEON_OBJECT_BEGIN(BeeeOn, RestUI, SensorRestHandler)