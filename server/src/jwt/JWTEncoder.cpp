183b3627bc6c6ee1a2595597e1c56c2570de31c4
Peter Kubov <xkubov06@stud.fit.vutbr.cz>
2017-10-10 19:48:19 +0200

    JWTEncoder: new class
    JWTEncoder encodes the incoming JSON Web Token string representations
    into an instane of the JWToken class. It is implemented via the libjwt
    library.
    
    Signed-off-by: Peter Kubov <xkubov06@stud.fit.vutbr.cz>
    Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

---
+++ server/src/jwt/JWTEncoder.cpp
@@ -0,0 +1,175 @@
+#include <Poco/Dynamic/Var.h>
+#include <Poco/Error.h>
+#include <Poco/Exception.h>
+#include <Poco/Logger.h>
+#include <Poco/JSON/Object.h>
+
+#include "di/Injectable.h"
+#include "jwt/JWToken.h"
+#include "jwt/JWTEncoder.h"
+
+using namespace BeeeOn;
+using namespace std;
+using namespace Poco;
+using namespace Poco::JSON;
+
+BEEEON_OBJECT_BEGIN(BeeeOn, JWTEncoder)
+BEEEON_OBJECT_TEXT("secret", &JWTEncoder::setSecret)
+BEEEON_OBJECT_TEXT("algorithm", &JWTEncoder::setAlgorithm)
+BEEEON_OBJECT_END(BeeeOn, JWTEncoder)
+
+JWTEncoder::JWTEncoder():
+	m_algorithm(JWT_ALG_NONE)
+{
+}
+
+JWTEncoder::~JWTEncoder()
+{
+}
+
+void JWTEncoder::setSecret(const std::string &secret)
+{
+	m_secret = secret;
+}
+
+void JWTEncoder::setAlgorithm(const std::string &algorithm)
+{
+	if (algorithm == "HS256")
+		m_algorithm = JWT_ALG_HS256;
+	else if (algorithm == "HS384")
+		m_algorithm = JWT_ALG_HS384;
+	else if (algorithm == "HS512")
+		m_algorithm = JWT_ALG_HS512;
+	else if (algorithm == "RS256")
+		m_algorithm = JWT_ALG_RS256;
+	else if (algorithm == "RS384")
+		m_algorithm = JWT_ALG_RS384;
+	else if (algorithm == "RS512")
+		m_algorithm = JWT_ALG_RS512;
+	else if (algorithm == "NONE")
+		m_algorithm = JWT_ALG_NONE;
+	else
+		throw InvalidArgumentException("unknown algorithm " + algorithm);
+}
+
+static Array::Ptr serializeArray(const vector<string> &array)
+{
+	Array::Ptr arr = new Array;
+
+	for (auto i : array)
+		arr->add(i);
+
+	return arr;
+}
+
+static void insertClaims(jwt_t *tokenRaw, const JWToken &token)
+{
+	int err;
+
+	err = ::jwt_del_grants(tokenRaw, NULL);
+	if (err != 0)
+		throw IllegalStateException(Error::getMessage(err));
+
+	if (!token.issuer().empty()) {
+		err = ::jwt_add_grant(tokenRaw, "iss", token.issuer().c_str());
+		if (err != 0)
+			throw IllegalStateException("iss: " + Error::getMessage(err));
+	}
+
+	if (!token.subject().empty()) {
+		err = ::jwt_add_grant(tokenRaw, "sub", token.subject().c_str());
+		if (err != 0)
+			throw IllegalStateException("sub: " + Error::getMessage(err));
+	}
+
+	if (!token.audience().empty()) {
+		Object::Ptr object = new Object;
+		object->set("aud", serializeArray(token.audience()));
+		const string &aud = Dynamic::Var::toString(object);
+
+		err = ::jwt_add_grants_json(tokenRaw, aud.c_str());
+		if (err != 0)
+			throw IllegalStateException("aud: " + Error::getMessage(err));
+	}
+
+	if (!token.locale().empty()) {
+		err = ::jwt_add_grant(tokenRaw, "locale", token.locale().c_str());
+		if (err != 0)
+			throw IllegalStateException("locale: " + Error::getMessage(err));
+	}
+
+	if (!token.expirationTime().isNull()) {
+		const long exp = token.expirationTime().value().epochTime();
+
+		err = ::jwt_add_grant_int(tokenRaw, "exp", exp);
+		if (err != 0)
+			throw IllegalStateException("exp: " + Error::getMessage(err));
+	}
+
+	if (!token.issuedAt().isNull()) {
+		const long iat = token.issuedAt().value().epochTime();
+
+		err = ::jwt_add_grant_int(tokenRaw, "iat", iat);
+		if (err != 0)
+			throw IllegalStateException("iat: " + Error::getMessage(err));
+	}
+
+	if (!token.notBefore().isNull()) {
+		const long nbf = token.notBefore().value().epochTime();
+
+		err = ::jwt_add_grant_int(tokenRaw, "nbf", nbf);
+		if (err != 0)
+			throw IllegalStateException("nbf: " + Error::getMessage(err));
+	}
+}
+
+TokenID JWTEncoder::encode(const JWToken &token) const
+{
+	string tokenId;
+	char *tokenIdRaw;
+
+	const unsigned char *secret;
+	if (m_secret.empty())
+		secret = NULL;
+	else
+		secret = reinterpret_cast<const unsigned char *>(m_secret.c_str());
+
+	jwt_t *tokenRaw;
+	int err = ::jwt_new(&tokenRaw);
+	if (err != 0)
+		throw IllegalStateException("jwt alloc: " + Error::getMessage(err));
+
+	try {
+		err = ::jwt_set_alg(tokenRaw, m_algorithm, secret, m_secret.length());
+		if (err != 0)
+			throw InvalidArgumentException("algorithm: " + Error::getMessage(err));
+
+		insertClaims(tokenRaw, token);
+
+	} catch (...) {
+		::jwt_free(tokenRaw);
+		throw;
+	}
+
+	tokenIdRaw = ::jwt_encode_str(tokenRaw);
+	::jwt_free(tokenRaw);
+
+	if (tokenIdRaw == NULL)
+		throw IllegalStateException("jwt encode: " + Error::getMessage(Error::last()));
+
+	try {
+		// we are paranoid to accidently not leak resources
+		if (logger().debug())
+			logger().debug("encoded as " + string(tokenIdRaw), __FILE__, __LINE__);
+
+		tokenId = string(tokenIdRaw);
+
+	} catch (...) {
+		::free(tokenIdRaw);
+		throw;
+	}
+
+	::free(tokenIdRaw);
+
+	return TokenID::parse(tokenId);
+}
---
+++ server/test/jwt/JWTEncoderTest.cpp
@@ -0,0 +1,104 @@
+#include <cppunit/extensions/HelperMacros.h>
+
+#include <Poco/Exception.h>
+#include <Poco/Timestamp.h>
+
+#include "cppunit/BetterAssert.h"
+#include "jwt/JWToken.h"
+#include "jwt/JWTConstants.h"
+#include "jwt/JWTEncoder.h"
+
+using namespace std;
+using namespace Poco;
+
+namespace BeeeOn {
+
+class JWTEncoderTest : public CppUnit::TestFixture {
+	CPPUNIT_TEST_SUITE(JWTEncoderTest);
+	CPPUNIT_TEST(testEncodeWithHS256);
+	CPPUNIT_TEST(testEncodeWithHS384);
+	CPPUNIT_TEST(testEncodeWithHS512);
+	CPPUNIT_TEST(testEncodeWithNONE);
+	CPPUNIT_TEST(testNonExistingAlg);
+	CPPUNIT_TEST_SUITE_END();
+public:
+	void setUp();
+	void doTestEncode(const JWToken &input, const TokenID &expect);
+
+	void testEncodeWithHS256();
+	void testEncodeWithHS384();
+	void testEncodeWithHS512();
+	void testEncodeWithNONE();
+	void testNonExistingAlg();
+
+private:
+	JWTEncoder m_encoder;
+	JWToken m_token;
+};
+
+CPPUNIT_TEST_SUITE_REGISTRATION(JWTEncoderTest);
+
+void JWTEncoderTest::setUp()
+{
+	m_encoder.setSecret(""); // clear
+	m_encoder.setAlgorithm("NONE");
+
+	m_token.setIssuer("BeeeOn");
+	m_token.setSubject("be572079-0971-47f0-95df-68476a642911");
+	m_token.setLocale("en_US");
+	m_token.setAudience({"BeeeOn"});
+	m_token.setExpirationTime(Timestamp::TIMEVAL_MAX);
+	m_token.setIssuedAt(Timestamp::TIMEVAL_MIN);
+	m_token.setNotBefore(Timestamp::TIMEVAL_MIN);
+}
+
+void JWTEncoderTest::doTestEncode(const JWToken &input, const TokenID &expect)
+{
+	TokenID result;
+
+	CPPUNIT_ASSERT_NO_THROW(result = m_encoder.encode(input));
+	CPPUNIT_ASSERT_EQUAL(expect.toString(), result.toString());
+}
+
+void JWTEncoderTest::testEncodeWithHS256()
+{
+	m_encoder.setAlgorithm("HS256");
+	m_encoder.setSecret(JWTConstants::HMAC_SECRET);
+
+	doTestEncode(m_token, JWTConstants::HS256_VALID_TOKEN);
+}
+
+void JWTEncoderTest::testEncodeWithHS384()
+{
+	m_encoder.setAlgorithm("HS384");
+	m_encoder.setSecret(JWTConstants::HMAC_SECRET);
+
+	doTestEncode(m_token, JWTConstants::HS384_VALID_TOKEN);
+}
+
+void JWTEncoderTest::testEncodeWithHS512()
+{
+	m_encoder.setAlgorithm("HS512");
+	m_encoder.setSecret(JWTConstants::HMAC_SECRET);
+
+	doTestEncode(m_token, JWTConstants::HS512_VALID_TOKEN);
+}
+
+void JWTEncoderTest::testEncodeWithNONE()
+{
+	m_encoder.setAlgorithm("NONE");
+	m_encoder.setSecret("");
+
+	doTestEncode(m_token, JWTConstants::NONAL_VALID_TOKEN);
+}
+
+/**
+ * Trying to setup encoder with a non-existing algorithm.
+ */
+void JWTEncoderTest::testNonExistingAlg()
+{
+	m_encoder.setSecret("");
+	CPPUNIT_ASSERT_THROW(m_encoder.setAlgorithm("UNICORN_ALG"), InvalidArgumentException);
+}
+
+}
