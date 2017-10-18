BeeeOn/server

commit 36b6039833b12104be73c0c4d538f953a858511c

GatewayCommunicator: new class
The GatewayCommunicator is responsible for communication with gateways
connected to the server. It maintains a table of gateway connections.

Each connection is managed by the Poco::Net::SocketReactor that is
running in a separate thread. The reactor notifies the system about
available readable connections.

Readable connections are queued and worker threads from the associated
thread pool handle these connections.

When a GWMessage is read from a gateway connection, GWMessageHandler is
used to handle it.

Signed-off-by: Jozef Halaj <xhalaj03@stud.fit.vutbr.cz>
Reviewed-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

server/src/gws/GatewayCommunicator.cpp
@@ -1,0 +1,289 @@
+#include <Poco/Logger.h>
+#include <Poco/Net/WebSocket.h>
+#include <Poco/Net/NetException.h>
+
+#include "di/Injectable.h"
+#include "gws/GatewayCommunicator.h"
+
+BEEEON_OBJECT_BEGIN(BeeeOn, GatewayCommunicator)
+BEEEON_OBJECT_CASTABLE(StoppableLoop)
+BEEEON_OBJECT_REF("messageHandler", &GatewayCommunicator::setGWMessageHandler)
+BEEEON_OBJECT_NUMBER("maxMessageSize", &GatewayCommunicator::setMaxMessageSize)
+BEEEON_OBJECT_TIME("receiveTimeout", &GatewayCommunicator::setReceiveTimeout)
+BEEEON_OBJECT_TIME("sendTimeout", &GatewayCommunicator::setSendTimeout)
+BEEEON_OBJECT_NUMBER("minThreads", &GatewayCommunicator::setMinThreads)
+BEEEON_OBJECT_NUMBER("maxThreads", &GatewayCommunicator::setMaxThreads)
+BEEEON_OBJECT_TIME("threadIdleTime", &GatewayCommunicator::setThreadIdleTime)
+BEEEON_OBJECT_END(BeeeOn, GatewayCommunicator)
+
+using namespace std;
+using namespace Poco;
+using namespace Poco::Net;
+using namespace BeeeOn;
+
+GatewayCommunicator::GatewayCommunicator():
+	m_maxMessageSize(4096),
+	m_stop(false),
+	m_workerRunnable(*this, &GatewayCommunicator::runWorker)
+{
+}
+
+GatewayCommunicator::~GatewayCommunicator()
+{
+}
+
+void GatewayCommunicator::addGateway(const GatewayID &gatewayID, WebSocket &webSocket)
+{
+	FastMutex::ScopedLock guard(m_connectionMapMutex);
+
+	auto it = m_connectionMap.find(gatewayID);
+	if (it != m_connectionMap.end()) {
+		if (logger().debug()) {
+			logger().debug("gateway " + gatewayID.toString()
+					+ " found, using new connection");
+		}
+
+		it->second->removeFromReactor();
+		m_connectionMap.erase(it);
+	}
+
+	webSocket.setReceiveTimeout(m_receiveTimeout);
+	webSocket.setSendTimeout(m_sendTimeout);
+
+	GatewayConnection::Ptr connection(
+		new GatewayConnection(
+			gatewayID,
+			webSocket,
+			m_reactor,
+			[this](GatewayConnection::Ptr gatewayConnection)
+			{
+				enqueueReadable(gatewayConnection);
+			},
+			m_maxMessageSize
+	));
+
+	auto emplaced = m_connectionMap.emplace(gatewayID, connection);
+	emplaced.first->second->addToReactor();
+}
+
+void GatewayCommunicator::removeGateway(const GatewayID &id)
+{
+	FastMutex::ScopedLock guard(m_connectionMapMutex);
+
+	auto it = m_connectionMap.find(id);
+	if (it == m_connectionMap.end()) {
+		logger().warning("gateway " + id.toString() + " not found");
+		return;
+	}
+
+	it->second->removeFromReactor();
+	m_connectionMap.erase(it);
+}
+
+void GatewayCommunicator::removeIfInactive(const GatewayID &id,
+	const Timestamp &timeFrom)
+{
+	FastMutex::ScopedLock guard(m_connectionMapMutex);
+
+	auto it = m_connectionMap.find(id);
+	if (it == m_connectionMap.end()) {
+		logger().warning("gateway " + id.toString() + " not found");
+		return;
+	}
+
+	if (timeFrom < it->second->lastReceiveTime())
+		return;
+
+	it->second->removeFromReactor();
+	m_connectionMap.erase(it);
+}
+
+GatewayConnection::Ptr GatewayCommunicator::findConnection(const GatewayID &id)
+{
+	FastMutex::ScopedLock guard(m_connectionMapMutex);
+
+	auto it = m_connectionMap.find(id);
+	if (it == m_connectionMap.end())
+		throw NotFoundException("gateway: " + id.toString());
+
+	return it->second;
+}
+
+void GatewayCommunicator::sendMessage(const GatewayID &gatewayID,
+	const GWMessage::Ptr message)
+{
+	findConnection(gatewayID)->sendMessage(message);
+}
+
+void GatewayCommunicator::start()
+{
+	initPool();
+
+	m_stop = false;
+
+	m_reactorThread.start(m_reactor);
+}
+
+void GatewayCommunicator::stop()
+{
+	m_stop = true;
+
+	m_reactor.stop();
+	logger().information("waiting to join reactor thread");
+	m_reactorThread.join();
+
+	m_connectionReadableQueue.clear();
+
+	logger().information("waiting to join worker threads");
+	pool().joinAll();
+
+	FastMutex::ScopedLock guard(m_connectionMapMutex);
+	logger().notice("destroying " + to_string(m_connectionMap.size())
+			+ " active connections");
+
+	for (auto &it : m_connectionMap)
+		it.second->removeFromReactor();
+
+	m_connectionMap.clear();
+}
+
+
+void GatewayCommunicator::enqueueReadable(GatewayConnection::Ptr connection)
+{
+	connection->removeFromReactor();
+
+	m_connectionReadableQueue.enqueue(connection);
+
+	try {
+		pool().start(m_workerRunnable);
+	}
+	catch (const NoThreadAvailableException &e) {
+		logger().warning(e.displayText(), __FILE__, __LINE__);
+		// All configured threads are busy and thus there is no need to start
+		// threads anymore. The connection is already queued and thus some
+		// thread would serve it soon.
+	}
+}
+
+void GatewayCommunicator::runWorker()
+{
+	while (!m_stop) {
+		GatewayConnection::Ptr connection = m_connectionReadableQueue.dequeue();
+
+		if (!connection.isNull())
+			handleConnectionReadable(connection);
+
+		// Exit when there is nothing to be done. The thread is returned
+		// into the pool.
+		if (m_connectionReadableQueue.empty())
+			break;
+	}
+}
+
+void GatewayCommunicator::handleConnectionReadable(GatewayConnection::Ptr connection)
+{
+	static Occasionally occasionally;
+
+	occasionally.execute([this]() {
+		ScopedLockWithUnlock<FastMutex> guard(m_connectionMapMutex);
+		int connections = m_connectionMap.size();
+		guard.unlock();
+
+		logger().notice(
+			"active connections: %d, "
+			"readable queue: %d, "
+			"thread pool allocated: %d, "
+			"available: %d, "
+			"capacity: %d",
+			connections,
+			m_connectionReadableQueue.size(),
+			pool().allocated(),
+			pool().available(),
+			pool().capacity());
+	});
+
+	Thread::current()->setName("worker-" + connection->gatewayID().toString());
+
+	try {
+		GWMessage::Ptr message = connection->receiveMessage();
+		connection->addToReactor();
+
+		m_messageHandler->handle(message, connection->gatewayID());
+	}
+	catch (const Exception &e) {
+		logger().log(e, __FILE__, __LINE__);
+		removeGateway(connection->gatewayID());
+		return;
+	}
+}
+
+void GatewayCommunicator::setGWMessageHandler(GWMessageHandler::Ptr handler)
+{
+	m_messageHandler = handler;
+}
+
+void GatewayCommunicator::setMaxMessageSize(int size)
+{
+	if (size < 0)
+		throw InvalidArgumentException("size must be non negative");
+
+	m_maxMessageSize = size;
+}
+
+void GatewayCommunicator::setReceiveTimeout(const Poco::Timespan &timeout)
+{
+	if (timeout < 0)
+		throw InvalidArgumentException("receive timeout must be non negative");
+
+	m_receiveTimeout = timeout;
+}
+
+void GatewayCommunicator::setSendTimeout(const Poco::Timespan &timeout)
+{
+	if (timeout < 0)
+		throw InvalidArgumentException("send timeout must be non negative");
+
+	m_sendTimeout = timeout;
+}
+
+GatewayCommunicator::ConnectionReadableQueue::~ConnectionReadableQueue()
+{
+	clear();
+}
+
+void GatewayCommunicator::ConnectionReadableQueue::enqueue(
+	GatewayConnection::Ptr connection)
+{
+	FastMutex::ScopedLock guard(m_mutex);
+	m_queue.push_back(connection);
+}
+
+GatewayConnection::Ptr GatewayCommunicator::ConnectionReadableQueue::dequeue()
+{
+	GatewayConnection::Ptr connection;
+
+	FastMutex::ScopedLock guard(m_mutex);
+	if (!m_queue.empty()) {
+		connection = m_queue.front();
+		m_queue.pop_front();
+	}
+	return connection;
+}
+
+bool GatewayCommunicator::ConnectionReadableQueue::empty() const
+{
+	FastMutex::ScopedLock guard(m_mutex);
+	return m_queue.empty();
+}
+
+unsigned long GatewayCommunicator::ConnectionReadableQueue::size() const
+{
+	FastMutex::ScopedLock guard(m_mutex);
+	return m_queue.size();
+}
+
+void GatewayCommunicator::ConnectionReadableQueue::clear()
+{
+	FastMutex::ScopedLock guard(m_mutex);
+	m_queue.clear();
+}

server/conf/config.d/factory-gws.xml
@@ -1,6 +1,16 @@
 <system>
 	<factory>
 		<instance name="gws" class="BeeeOn::LoopRunner">
+			<add name="loops" ref="gatewayCommunicator" />
+		</instance>
+
+		<instance name="gatewayCommunicator" class="BeeeOn::GatewayCommunicator">
+			<set name="maxMessageSize" number="${gws.comm.maxMessageSize}" />
+			<set name="receiveTimeout" time="${gws.comm.receiveTimeout}" />
+			<set name="sendTimeout" time="${gws.comm.sendTimeout}" />
+			<set name="minThreads" number="${gws.comm.minThreads}" />
+			<set name="maxThreads" number="${gws.comm.maxThreads}" />
+			<set name="threadIdleTime" time="${gws.comm.threadIdleTime}" />
 		</instance>
 	</factory>
 </system>

server/conf/config.d/server.ini
@@ -11,6 +11,12 @@ ssl.enable = false
 ;ssl.verify_level =
 
 [gws]
+comm.maxMessageSize = 4096
+comm.receiveTimeout = 1 s
+comm.sendTimeout = 500 ms
+comm.minThreads = 1
+comm.maxThreads = 16
+comm.threadIdletime = 5 s
 
 [spec]
 devices.xml = ${application.configDir}devices.xml

server/src/CMakeLists.txt
@@ -114,6 +114,7 @@ file(GLOB SOURCES
 	${PROJECT_SOURCE_DIR}/util/Startup.cpp
 	${PROJECT_SOURCE_DIR}/dao/poco/PocoDaoManager.cpp
 	${PROJECT_SOURCE_DIR}/gws/GatewayConnection.cpp
+	${PROJECT_SOURCE_DIR}/gws/GatewayCommunicator.cpp
 	${PROJECT_SOURCE_DIR}/gws/GWMessageHandler.cpp
 	${PROJECT_SOURCE_DIR}/rpc/RPCConnector.cpp
 	${PROJECT_SOURCE_DIR}/rpc/SocketRPCConnector.cpp

server/src/gws/GatewayCommunicator.h
@@ -1,0 +1,201 @@
+#ifndef BEEEON_GATEWAY_COMMUNICATOR_H
+#define BEEEON_GATEWAY_COMMUNICATOR_H
+
+#include <deque>
+#include <functional>
+
+#include <Poco/Mutex.h>
+#include <Poco/NotificationQueue.h>
+#include <Poco/RunnableAdapter.h>
+#include <Poco/SharedPtr.h>
+#include <Poco/Net/WebSocket.h>
+#include <Poco/Net/SocketReactor.h>
+
+#include "gwmessage/GWMessage.h"
+#include "gws/GatewayConnection.h"
+#include "gws/GWMessageHandler.h"
+#include "loop/StoppableLoop.h"
+#include "model/GatewayID.h"
+#include "util/HavingThreadPool.h"
+#include "util/Occasionally.h"
+
+namespace BeeeOn {
+
+/**
+ * @brief GatewayCommunicator is responsible for communication with gateways
+ * connected to the server.
+ *
+ * It maintains a table of {GatewayID, GatewayConnection::Ptr} pairs.
+ * Each connection is managed by the Poco::SocketReactor that is running
+ * in a separate thread. The reactor notifies the system about available
+ * readable connections.
+ *
+ * The ConnectionReadableQueue is used for queueing of readable connections to be
+ * handled by threads allocated from a thread pool. After a connection is marked
+ * as readable (via SocketReactor), it is unregistered from the reactor
+ * and it is queued in the notification queue. When there are some queued
+ * connections, a worker threads are allocated from the associated
+ * thread pool to handle all of them as soon as possible.
+ *
+ * When a message is read from a gateway connection, the connection is again
+ * registered into the reactor to wait for more data,
+ * and the GWMessageHandler::handle(GatewayMessage::Ptr) is called to do the actual
+ * message processing. Any unhandled exception during handling message causes
+ * deleting the gateway connection.
+ *
+ * Handling of gateway connections is fair. This is accomplished by using
+ * the Poco::SocketReactor that watches the open socket connections for changes.
+ */
+class GatewayCommunicator :
+	public StoppableLoop,
+	public HavingThreadPool {
+public:
+	/**
+	 * @brief The ConnectionReadableQueue is intended for queueing of readable
+	 * connections to be handled by worker threads of the GatewayCommunicator.
+	 */
+	class ConnectionReadableQueue {
+	public:
+		~ConnectionReadableQueue();
+
+		/**
+		 * @brief Enqueues the given connection by adding it to the end
+		 * of the queue (FIFO).
+		 */
+		void enqueue(GatewayConnection::Ptr connection);
+
+		/**
+		 * @brief Dequeues the next pending connection.
+		 *
+		 * Returns nullptr if no connection is available.
+		 */
+		GatewayConnection::Ptr dequeue();
+
+		/**
+		 * @brief Returns true if the queue is empty.
+		 */
+		bool empty() const;
+
+		/**
+		 * @brief Returns the number of connections in the queue.
+		 */
+		unsigned long size() const;
+
+		/**
+		 * @brief Removes all connections from the queue.
+		 */
+		void clear();
+
+	private:
+		std::deque<GatewayConnection::Ptr> m_queue;
+		mutable Poco::FastMutex m_mutex;
+	};
+
+	typedef Poco::SharedPtr<GatewayCommunicator> Ptr;
+
+	GatewayCommunicator();
+	~GatewayCommunicator();
+
+	 /**
+	  * @brief Create new GatewayConnection for the given id and add it
+	  * to the communicator.
+	  *
+	  * If there is a GatewayConnection with the given id
+	  * in the GatewayCommunicator, it is replaced by new one.
+	  */
+	void addGateway(const GatewayID &gatewayID, Poco::Net::WebSocket &webSocket);
+
+	/**
+	 * @brief Remove Gateway from the GatewayCommunicator.
+	 *
+	 * If there is no a GatewayConnection with the given id, it is ignored.
+	 */
+	void removeGateway(const GatewayID &gatewayID);
+
+	/**
+	 * @brief Remove Gateway from the GatewayCommunicator,
+	 * if no message was received from the given time till now.
+	 *
+	 * If there is no a GatewayConnection with the given id, it is ignored.
+	 */
+	void removeIfInactive(const GatewayID &id, const Poco::Timestamp &timeFrom);
+
+	/**
+	 * @brief Sends GWMessage to the connected gateway.
+	 *
+	 * @throw NotFoundException if GatewayCommunicator
+	 * has no such GatewayConnection.
+	 */
+	void sendMessage(const GatewayID &gatewayID, const GWMessage::Ptr message);
+
+	void setGWMessageHandler(GWMessageHandler::Ptr handler);
+
+	void setMaxMessageSize(int size);
+	void setReceiveTimeout(const Poco::Timespan &timeout);
+	void setSendTimeout(const Poco::Timespan &timeout);
+
+	void start() override;
+	void stop() override;
+
+private:
+	/**
+	 * @brief Returns GatewayConnection with the given id.
+	 *
+	 * @throw NotFoundException if GatewayCommunicator
+	 * has no such GatewayConnection.
+	 */
+	GatewayConnection::Ptr findConnection(const GatewayID &gatewayID);
+
+	/**
+	 * @brief Enqueue given GatewayConnection to the readable queue
+	 * and start worker in the thread pool.
+	 *
+	 * The GatewayConnection must be unregistered from the reactor.
+	 *
+	 * If there is no available thread in the thread pool, connection is
+	 * just queued and other running worker handles it.
+	 */
+	void enqueueReadable(GatewayConnection::Ptr connection);
+
+	/**
+	 * @brief Receives a GWMessage from the GatewayConnection and handle it
+	 * using GWMessageHandler.
+	 *
+	 * Any exception thrown from the GWConnection receiveMessage() method
+	 * and also from the GWMessageHandler handle() method causes removing
+	 * the connection from the GatewayCommunicator.
+	 */
+	void handleConnectionReadable(GatewayConnection::Ptr connection);
+
+	/**
+	 * @brief The worker dequeues readable connection and handle it.
+	 *
+	 * The worker is started in the thread pool after enqueueing readable
+	 * connection to the queue. It dequeues readable connection and handles it.
+	 * If the queue is not empty, worker continues and handles the next
+	 * readable connection, otherwise it will end.
+	 */
+	void runWorker();
+
+private:
+	typedef std::map<GatewayID, GatewayConnection::Ptr> GatewayConnectionMap;
+
+	size_t m_maxMessageSize;
+	Poco::Timespan m_receiveTimeout;
+	Poco::Timespan m_sendTimeout;
+
+	GWMessageHandler::Ptr m_messageHandler;
+
+	GatewayConnectionMap m_connectionMap;
+	Poco::FastMutex m_connectionMapMutex;
+	ConnectionReadableQueue m_connectionReadableQueue;
+
+	Poco::Net::SocketReactor m_reactor;
+	Poco::Thread m_reactorThread;
+	Poco::AtomicCounter m_stop;
+	Poco::RunnableAdapter<GatewayCommunicator> m_workerRunnable;
+};
+
+}
+
+#endif

commit bcbb272701965f0ffaf1ffae17492cfa0232a242

GatewayCommunicator: fix ERRFMT in logging statistics
Size of readable queue is represented as unsinged long and that
does not fit in %d. This caused invalid output:

[Notice] active connections: 1, readable queue: [ERRFMT],
thread pool allocated: 1, available: 63, capacity: 64

Also __FILE__ and __LINE__ parameters were missing, so rewriting
of the whole message was needed.

Fixes: 36b6039833b1 ("GatewayCommunicator: new class")
Signed-off-by: Jozef Halaj <xhalaj03@stud.fit.vutbr.cz>
Acked-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

server/src/gws/GatewayCommunicator.cpp
@@ -190,16 +190,12 @@ void GatewayCommunicator::handleConnectionReadable(GatewayConnection::Ptr connec
 		guard.unlock();
 
 		logger().notice(
-			"active connections: %d, "
-			"readable queue: %d, "
-			"thread pool allocated: %d, "
-			"available: %d, "
-			"capacity: %d",
-			connections,
-			m_connectionReadableQueue.size(),
-			pool().allocated(),
-			pool().available(),
-			pool().capacity());
+			"active connections: " + to_string(connections)
+			+ ", readable queue: " + to_string(m_connectionReadableQueue.size())
+			+ ", thread pool allocated: " + to_string(pool().allocated())
+			+ ", available: " + to_string(pool().available())
+			+ ", capacity: " + to_string(pool().capacity()),
+			__FILE__, __LINE__);
 	});
 
 	Thread::current()->setName("worker-" + connection->gatewayID().toString());