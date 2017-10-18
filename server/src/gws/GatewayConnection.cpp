BeeeOn/server

commit 220e617aeeecf4e8c12a50a817c90d5b1f387381

GatewayConnection: new class
GatewayConnection represents connection with a specific Gateway.

Signed-off-by: Jozef Halaj <xhalaj03@stud.fit.vutbr.cz>
Reviewed-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

server/src/gws/GatewayConnection.cpp
@@ -1,0 +1,106 @@
+#include <Poco/Logger.h>
+#include <Poco/Net/NetException.h>
+
+#include "gws/GatewayConnection.h"
+
+using namespace std;
+using namespace Poco;
+using namespace Poco::Net;
+using namespace BeeeOn;
+
+GatewayConnection::GatewayConnection(
+		const GatewayID &gatewayID,
+		const WebSocket &webSocket,
+		SocketReactor &reactor,
+		const EnqueueReadable &enqueueReadable,
+		size_t maxMessageSize):
+	m_gatewayID(gatewayID),
+	m_webSocket(webSocket),
+	m_reactor(reactor),
+	m_enqueueReadable(enqueueReadable),
+	m_receiveBuffer(maxMessageSize),
+	m_readableObserver(*this, &GatewayConnection::onReadable)
+{
+	if (logger().debug()) {
+		logger().debug("gateway " + m_gatewayID.toString()
+				+ " connection created");
+	}
+}
+
+GatewayConnection::~GatewayConnection()
+{
+	if (logger().debug()) {
+		logger().debug("gateway " + m_gatewayID.toString()
+				+ " connection destroyed");
+	}
+}
+
+GatewayID GatewayConnection::gatewayID() const
+{
+	return m_gatewayID;
+}
+
+void GatewayConnection::addToReactor() const
+{
+	m_reactor.addEventHandler(m_webSocket, m_readableObserver);
+}
+
+void GatewayConnection::removeFromReactor() const
+{
+	m_reactor.removeEventHandler(m_webSocket, m_readableObserver);
+}
+
+void GatewayConnection::updateLastReceiveTime()
+{
+	FastMutex::ScopedLock guard(m_lastReceiveTimeMutex);
+	m_lastReceiveTime.update();
+}
+
+Poco::Timestamp GatewayConnection::lastReceiveTime()
+{
+	FastMutex::ScopedLock guard(m_lastReceiveTimeMutex);
+	return m_lastReceiveTime;
+}
+
+GWMessage::Ptr GatewayConnection::receiveMessage()
+{
+	int flags;
+
+	int ret = m_webSocket.receiveFrame(
+			m_receiveBuffer.begin(), m_receiveBuffer.size(), flags);
+
+	if (ret <= 0 || (flags & WebSocket::FRAME_OP_CLOSE))
+		throw ConnectionResetException(m_gatewayID.toString());
+
+	updateLastReceiveTime();
+
+	string msg(m_receiveBuffer.begin(), ret);
+
+	if (logger().debug()) {
+		logger().debug("data from gateway "
+				+ m_gatewayID.toString() + ":\n" + msg);
+	}
+
+	return GWMessage::fromJSON(msg);
+}
+
+void GatewayConnection::sendMessage(const GWMessage::Ptr message)
+{
+	FastMutex::ScopedLock guard(m_sendMutex);
+
+	const string &msg = message->toString();
+
+	if (logger().debug()) {
+		logger().debug("message to gateway "
+				+ m_gatewayID.toString() + ":\n" + msg);
+	}
+
+	m_webSocket.sendFrame(msg.c_str(), msg.length());
+}
+
+void GatewayConnection::onReadable(const AutoPtr<ReadableNotification> &notification)
+{
+	m_enqueueReadable(AutoPtr<GatewayConnection>(this, true));
+}
+
+

server/src/CMakeLists.txt
@@ -113,6 +113,7 @@ file(GLOB SOURCES
 	${PROJECT_SOURCE_DIR}/util/Sanitize.cpp
 	${PROJECT_SOURCE_DIR}/util/Startup.cpp
 	${PROJECT_SOURCE_DIR}/dao/poco/PocoDaoManager.cpp
+	${PROJECT_SOURCE_DIR}/gws/GatewayConnection.cpp
 	${PROJECT_SOURCE_DIR}/gws/GWMessageHandler.cpp
 	${PROJECT_SOURCE_DIR}/rpc/RPCConnector.cpp
 	${PROJECT_SOURCE_DIR}/rpc/SocketRPCConnector.cpp

server/src/gws/GatewayConnection.h
@@ -1,0 +1,123 @@
+#ifndef BEEEON_GATEWAY_CONNECTION_H
+#define BEEEON_GATEWAY_CONNECTION_H
+
+#include <functional>
+
+#include <Poco/AutoPtr.h>
+#include <Poco/Buffer.h>
+#include <Poco/NObserver.h>
+#include <Poco/RefCountedObject.h>
+#include <Poco/SharedPtr.h>
+#include <Poco/Timestamp.h>
+#include <Poco/Net/SocketNotification.h>
+#include <Poco/Net/SocketReactor.h>
+#include <Poco/Net/WebSocket.h>
+
+#include "gwmessage/GWMessage.h"
+#include "model/GatewayID.h"
+#include "util/Loggable.h"
+
+namespace BeeeOn {
+
+/**
+ * @brief Represents a connection with the specific gateway.
+ *
+ * The GatewayConnection is created and stored in the GatewayCommunicator.
+ * It is identified by the identifier of the connected gateway. Each connection
+ * is managed by the Poco::Net::SocketReactor. The reactor notifies the system
+ * about available readable connections.
+ *
+ * Once the connection is created it is registered as an event handler
+ * to the reactor. When the reactor detects that the connection is readable,
+ * the connection is unregistered from the reactor, it is queued and worker
+ * threads from the thread pool of the GatewayCommunicator read GWMessage
+ * from it. The worker then register the connection back to the reactor
+ * and handle received message.
+ *
+ * Other parts of the system can access the GatewayConnection through the
+ * GatewayCommunicator and send GWMessage to the gateway.
+ *
+ * The connection is destroyed, if:
+ * - socket is closed or broken
+ * - received message is invalid (unknown message type, missing field, etc.)
+ * - new connection with the same id (socket incorrectly closed and gateway
+ *   reconnected)
+ * - connection is considered inactive (the response to the request has not
+ *   arrived within the timeout and in the meantime no other message came
+ *   from the gateway.)
+ */
+class GatewayConnection :
+		public Poco::RefCountedObject,
+		public Loggable {
+public:
+	typedef Poco::AutoPtr<GatewayConnection> Ptr;
+	typedef std::function<void(GatewayConnection::Ptr)> EnqueueReadable;
+
+	GatewayConnection(
+		const GatewayID &gatewayID,
+		const Poco::Net::WebSocket &webSocket,
+		Poco::Net::SocketReactor &reactor,
+		const EnqueueReadable &enqueueReadable,
+		size_t maxMessageSize
+	);
+
+	~GatewayConnection();
+
+	/**
+	 * @brief Returns id of the connected gateway.
+	 */
+	GatewayID gatewayID() const;
+
+	/**
+	 * @brief Register GatewayConnection as an event handler to the SocketReactor.
+	 */
+	void addToReactor() const;
+
+	/**
+	 * @brief Unregister GatewayConnection as an event handler from the SocketReactor.
+	 */
+	void removeFromReactor() const;
+
+	/**
+	 * @brief Returns timestamp of the last received message.
+	 */
+	Poco::Timestamp lastReceiveTime();
+
+	/**
+	 * @brief Sends GWMessage to the gateway. Method is thread-safe.
+	 */
+	void sendMessage(const GWMessage::Ptr message);
+
+	/**
+	 * @brief Receives a GWMessage and returns it. Method is not thread-safe.
+	 */
+	GWMessage::Ptr receiveMessage();
+
+private:
+	/**
+	 * @brief Set timestamp of the last received message.
+	 */
+	void updateLastReceiveTime();
+
+	/**
+	 * @brief The Callback method invoked from the reactor thread.
+	 *
+	 * This method is intended just to determine there are data on the socket.
+	 * The data reading takes place in the thread pool of the GatewayCommunicator.
+	 */
+	void onReadable(const Poco::AutoPtr<Poco::Net::ReadableNotification> &notification);
+
+private:
+	GatewayID m_gatewayID;
+	Poco::Net::WebSocket m_webSocket;
+	Poco::Net::SocketReactor &m_reactor;
+	EnqueueReadable m_enqueueReadable;
+	Poco::Buffer<char> m_receiveBuffer;
+	Poco::Timestamp m_lastReceiveTime;
+	Poco::FastMutex m_lastReceiveTimeMutex;
+	Poco::FastMutex m_sendMutex;
+	Poco::NObserver<GatewayConnection, Poco::Net::ReadableNotification> m_readableObserver;
+};
+
+}
+#endif

commit e1e353e2f18eff41240d9163a53b4d8dea1ea803

GatewayConnection: logging: fix missing __FILE__, __LINE__
Fixes: 220e617aeeec ("GatewayConnection: new class")
Signed-off-by: Jozef Halaj <xhalaj03@stud.fit.vutbr.cz>
Acked-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

server/src/gws/GatewayConnection.cpp
@@ -23,7 +23,7 @@ GatewayConnection::GatewayConnection(
 {
 	if (logger().debug()) {
 		logger().debug("gateway " + m_gatewayID.toString()
-				+ " connection created");
+				+ " connection created", __FILE__, __LINE__);
 	}
 }
 
@@ -31,7 +31,7 @@ GatewayConnection::~GatewayConnection()
 {
 	if (logger().debug()) {
 		logger().debug("gateway " + m_gatewayID.toString()
-				+ " connection destroyed");
+				+ " connection destroyed", __FILE__, __LINE__);
 	}
 }
 
@@ -78,7 +78,7 @@ GWMessage::Ptr GatewayConnection::receiveMessage()
 
 	if (logger().debug()) {
 		logger().debug("data from gateway "
-				+ m_gatewayID.toString() + ":\n" + msg);
+				+ m_gatewayID.toString() + ":\n" + msg, __FILE__, __LINE__);
 	}
 
 	return GWMessage::fromJSON(msg);
@@ -92,7 +92,7 @@ void GatewayConnection::sendMessage(const GWMessage::Ptr message)
 
 	if (logger().debug()) {
 		logger().debug("message to gateway "
-				+ m_gatewayID.toString() + ":\n" + msg);
+				+ m_gatewayID.toString() + ":\n" + msg, __FILE__, __LINE__);
 	}
 
 	m_webSocket.sendFrame(msg.c_str(), msg.length());