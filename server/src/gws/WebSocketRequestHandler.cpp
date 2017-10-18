BeeeOn/server

commit 42d7f6bbc432d80e333772331dd52c8d915c6ea2

WebSocketServer: new class
WebSocketServer is responsible for handling new WebSocket connections
from the gateways. After the connection is created, the registration
message is expected. Successfuly registered gateway connection is added
to the GatewayCommunicator.

Signed-off-by: Jozef Halaj <xhalaj03@stud.fit.vutbr.cz>
Reviewed-by: Jozef Halaj <xhalaj03@stud.fit.vutbr.cz>

server/src/gws/WebSocketRequestHandler.cpp
@@ -1,0 +1,78 @@
+#include <Poco/Buffer.h>
+#include <Poco/Logger.h>
+#include <Poco/SharedPtr.h>
+#include <Poco/JSON/Object.h>
+#include <Poco/Net/WebSocket.h>
+
+#include "gwmessage/GWGatewayRegister.h"
+#include "gwmessage/GWGatewayAccepted.h"
+#include "gws/WebSocketRequestHandler.h"
+#include "util/Sanitize.h"
+
+using namespace std;
+using namespace Poco;
+using namespace Poco::Net;
+using namespace BeeeOn;
+
+void WebSocketRequestHandler::handleRequest(
+	HTTPServerRequest &request, HTTPServerResponse &response)
+{
+	try {
+		Thread::current()->setName("ws");
+
+		WebSocket ws(request, response);
+
+		Poco::Buffer<char> buffer(m_maxMessageSize);
+		int flags;
+
+		int ret = ws.receiveFrame(buffer.begin(), buffer.size(), flags);
+		if (ret <= 0 || (flags & WebSocket::FRAME_OP_CLOSE)) {
+			if (logger().debug())
+				logger().debug(ws.peerAddress().toString() + " connection closed");
+			return;
+		}
+
+		string data(buffer.begin(), ret);
+		if (logger().trace())
+			logger().trace(data);
+
+		GWMessage::Ptr msg = GWMessage::fromJSON(data);
+		GWGatewayRegister::Ptr registerMsg = msg.cast<GWGatewayRegister>();
+
+		if (registerMsg.isNull()) {
+			logger().warning("invalid message from "
+				+ ws.peerAddress().toString() + ":\n"
+				+ msg->toString());
+			return;
+		}
+
+		Gateway gateway(registerMsg->gatewayID());
+
+		Thread::current()->setName("ws-" + gateway);
+
+		GatewayStatus status;
+		status.setVersion(Sanitize::common(registerMsg->version()));
+		status.setIPAddress(registerMsg->ipAddress());
+
+		if (!m_gatewayService->registerGateway(status, gateway)) {
+			logger().error("failed to register gateway "
+					+ gateway, __FILE__, __LINE__);
+			return;
+		}
+
+		data = GWGatewayAccepted().toString();
+		ws.sendFrame(data.c_str(), data.length());
+
+		m_gatewayCommunicator->addGateway(gateway.id(), ws);
+	}
+	catch (const Exception &e) {
+		logger().log(e, __FILE__, __LINE__);
+	}
+	catch (const exception &e) {
+		logger().critical(e.what(), __FILE__, __LINE__);
+	}
+	catch (...) {
+		logger().critical("unknown error, cought '...'", __FILE__, __LINE__);
+	}
+}
+

server/conf/config.d/factory-gws.xml
@@ -2,6 +2,19 @@
 	<factory>
 		<instance name="gws" class="BeeeOn::LoopRunner">
 			<add name="loops" ref="gatewayCommunicator" />
+			<add name="loops" ref="webSocketServer" />
+		</instance>
+
+		<instance name="webSocketServer" class="BeeeOn::WebSocketServer">
+			<set name="gatewayCommunicator" ref="gatewayCommunicator"/>
+			<set name="sslConfig" ref="gwsSSLServer" if-yes="${gws.ws.ssl.enable}" />
+			<set name="gatewayService" ref="gwsGatewayService" />
+			<set name="port" number="${gws.ws.port}" />
+			<set name="backlog" number="${gws.ws.backlog}" />
+			<set name="maxMessageSize" number="${gws.ws.maxMessageSize}" />
+			<set name="minThreads" number="${gws.ws.minThreads}" />
+			<set name="maxThreads" number="${gws.ws.maxThreads}" />
+			<set name="threadIdleTime" time="${gws.ws.threadIdleTime}" />
 		</instance>
 
 		<instance name="gatewayCommunicator" class="BeeeOn::GatewayCommunicator">

server/conf/config.d/factory-ssl.xml
@@ -22,5 +22,13 @@
 			<set name="disabledProtocols" text="sslv2,sslv3,tlsv1,tlsv1_1" />
 		</instance>
 
+		<instance name="gwsSSLServer" class="BeeeOn::SSLServer">
+			<set name="certificate" text="${gws.ws.ssl.certificate}" />
+			<set name="privateKey" text="${gws.ws.ssl.key}" />
+			<set name="caLocation" text="${gws.ws.ssl.authority}" />
+			<set name="verificationMode" text="${gws.ws.ssl.verify_level}" />
+			<set name="loadDefaultCA" text="false" />
+		</instance>
+
 	</factory>
 </system>

server/conf/config.d/server.ini
@@ -11,6 +11,19 @@ ssl.enable = false
 ;ssl.verify_level =
 
 [gws]
+ws.ssl.enable = false
+;ws.ssl.certificate =
+;ws.ssl.key =
+;ws.ssl.authority =
+;ws.ssl.verify_level =
+ws.port = 8850
+ws.backlog = 256
+;max message size just for register message
+ws.maxMessageSize = 256
+ws.minThreads = 1
+ws.maxThreads = 64
+ws.threadIdleTime = 5 s
+
 comm.maxMessageSize = 4096
 comm.receiveTimeout = 1 s
 comm.sendTimeout = 500 ms

server/src/CMakeLists.txt
@@ -116,6 +116,8 @@ file(GLOB SOURCES
 	${PROJECT_SOURCE_DIR}/gws/GatewayConnection.cpp
 	${PROJECT_SOURCE_DIR}/gws/GatewayCommunicator.cpp
 	${PROJECT_SOURCE_DIR}/gws/GWMessageHandler.cpp
+	${PROJECT_SOURCE_DIR}/gws/WebSocketRequestHandler.cpp
+	${PROJECT_SOURCE_DIR}/gws/WebSocketServer.cpp
 	${PROJECT_SOURCE_DIR}/rpc/RPCConnector.cpp
 	${PROJECT_SOURCE_DIR}/rpc/SocketRPCConnector.cpp
 	${PROJECT_SOURCE_DIR}/rpc/GatewayRPC.cpp

server/src/gws/WebSocketRequestHandler.h
@@ -1,0 +1,86 @@
+#ifndef BEEEON_WEBSOCKET_REQUEST_HANDLER_H
+#define BEEEON_WEBSOCKET_REQUEST_HANDLER_H
+
+#include <Poco/Net/HTTPServerRequest.h>
+#include <Poco/Net/HTTPServerResponse.h>
+#include <Poco/Net/HTTPRequestHandler.h>
+#include <Poco/Net/HTTPRequestHandlerFactory.h>
+
+#include "gws/GatewayCommunicator.h"
+#include "service/GWSGatewayServiceImpl.h"
+#include "util/Loggable.h"
+
+namespace BeeeOn {
+
+/**
+ * @brief Handler for incoming WebSocket connection from the Gateway.
+ *
+ * The WebSocket connection is here initialized, registered and then passed
+ * to the GatewayCommunicator. After the connection is added to the communicator,
+ * this handler is destroyed.
+ */
+class WebSocketRequestHandler :
+	public Poco::Net::HTTPRequestHandler,
+	protected Loggable {
+public:
+	WebSocketRequestHandler(
+			size_t maxMessageSize,
+			GatewayCommunicator::Ptr communicator,
+			GWSGatewayService::Ptr service):
+		m_maxMessageSize(maxMessageSize),
+		m_gatewayCommunicator(communicator),
+		m_gatewayService(service)
+	{
+	}
+
+	/**
+	 * @brief Handles incoming WebSocket connection from the Gateway.
+	 *
+	 * After the connection is created, the register message is expected.
+	 * Successfully registered Gateway connection (using GatewayService)
+	 * is added to the GatewayCommunicator.
+	 */
+	void handleRequest(
+		Poco::Net::HTTPServerRequest &request,
+		Poco::Net::HTTPServerResponse &response);
+
+private:
+	size_t m_maxMessageSize;
+	GatewayCommunicator::Ptr m_gatewayCommunicator;
+	GWSGatewayService::Ptr m_gatewayService;
+};
+
+/**
+ * @brief Factory for WebSocketRequestHandler objects.
+ */
+class WebSocketRequestHandlerFactory :
+	public Poco::Net::HTTPRequestHandlerFactory {
+public:
+	WebSocketRequestHandlerFactory(
+			size_t maxMessageSize,
+			GatewayCommunicator::Ptr communicator,
+			GWSGatewayService::Ptr service):
+		m_maxMessageSize(maxMessageSize),
+		m_gatewayCommunicator(communicator),
+		m_gatewayService(service)
+	{
+	}
+
+	Poco::Net::HTTPRequestHandler *createRequestHandler(
+		const Poco::Net::HTTPServerRequest &request)
+	{
+		return new WebSocketRequestHandler(
+			m_maxMessageSize,
+			m_gatewayCommunicator,
+			m_gatewayService
+		);
+	}
+private:
+	size_t m_maxMessageSize;
+	GatewayCommunicator::Ptr m_gatewayCommunicator;
+	GWSGatewayService::Ptr m_gatewayService;
+};
+
+}
+
+#endif

server/src/gws/WebSocketServer.cpp
@@ -1,0 +1,104 @@
+#include <Poco/Exception.h>
+#include <Poco/Net/SecureServerSocket.h>
+
+#include "di/Injectable.h"
+#include "gws/WebSocketServer.h"
+#include "gws/WebSocketRequestHandler.h"
+
+BEEEON_OBJECT_BEGIN(BeeeOn, WebSocketServer)
+BEEEON_OBJECT_CASTABLE(StoppableLoop)
+BEEEON_OBJECT_REF("gatewayCommunicator", &WebSocketServer::setGatewayCommunicator)
+BEEEON_OBJECT_REF("sslConfig", &WebSocketServer::setSSLConfig)
+BEEEON_OBJECT_REF("gatewayService", &WebSocketServer::setGatewayService)
+BEEEON_OBJECT_NUMBER("port", &WebSocketServer::setPort)
+BEEEON_OBJECT_NUMBER("backlog", &WebSocketServer::setBacklog)
+BEEEON_OBJECT_NUMBER("maxMessageSize", &WebSocketServer::setMaxMessageSize)
+BEEEON_OBJECT_NUMBER("minThreads", &WebSocketServer::setMinThreads)
+BEEEON_OBJECT_NUMBER("maxThreads", &WebSocketServer::setMaxThreads)
+BEEEON_OBJECT_TIME("threadIdleTime", &WebSocketServer::setThreadIdleTime)
+BEEEON_OBJECT_END(BeeeOn, WebSocketServer)
+
+using namespace std;
+using namespace Poco;
+using namespace Poco::Net;
+using namespace BeeeOn;
+
+WebSocketServer::WebSocketServer():
+	m_port(0),
+	m_backlog(64),
+	m_maxMessageSize(256)
+{
+}
+
+void WebSocketServer::setGatewayCommunicator(GatewayCommunicator::Ptr communicator)
+{
+	m_gatewayCommunicator = communicator;
+}
+
+void WebSocketServer::setSSLConfig(SharedPtr<SSLServer> config)
+{
+	m_sslConfig = config;
+}
+
+void WebSocketServer::setGatewayService(GWSGatewayService::Ptr service)
+{
+	m_gatewayService = service;
+}
+
+void WebSocketServer::setPort(int port)
+{
+	if (port < 0)
+		throw InvalidArgumentException("port must be non-negative");
+
+	m_port = port;
+}
+
+void WebSocketServer::setBacklog(int backlog)
+{
+	if (backlog < 0)
+		throw InvalidArgumentException("backlog must be non-negative");
+
+	m_backlog = backlog;
+}
+
+void WebSocketServer::setMaxMessageSize(int size)
+{
+	if (size < 0)
+		throw InvalidArgumentException("size must be non-negative");
+
+	m_maxMessageSize = size;
+}
+
+Poco::Net::HTTPServer *WebSocketServer::createServer()
+{
+	HTTPRequestHandlerFactory::Ptr factory(
+		new WebSocketRequestHandlerFactory(
+			m_maxMessageSize,
+			m_gatewayCommunicator,
+			m_gatewayService
+		));
+
+	return new HTTPServer(factory, pool(), createSocket(), new HTTPServerParams);
+}
+
+ServerSocket WebSocketServer::createSocket()
+{
+	return m_sslConfig.isNull() ? ServerSocket(m_port, m_backlog)
+			: SecureServerSocket(m_port, m_backlog, m_sslConfig->context());
+}
+
+void WebSocketServer::start()
+{
+	m_server = createServer();
+
+	logger().information("starting WebSocket server on port: "
+			+ to_string(m_server->port()));
+
+	m_server->start();
+}
+
+void WebSocketServer::stop()
+{
+	logger().information("stopping WebSocket server");
+	m_server->stop();
+}

server/src/gws/WebSocketServer.h
@@ -1,0 +1,69 @@
+#ifndef BEEEON_WEBSOCKET_SERVER
+#define BEEEON_WEBSOCKET_SERVER
+
+#include <Poco/SharedPtr.h>
+#include <Poco/Net/HTTPServer.h>
+#include <Poco/Net/ServerSocket.h>
+
+#include "gws/GatewayCommunicator.h"
+#include "loop/StoppableLoop.h"
+#include "service/GWSGatewayServiceImpl.h"
+#include "ssl/SSLServer.h"
+
+namespace BeeeOn {
+
+/**
+ * @brief WebSocketServer is responsible for handling new WebSocket connections
+ * from gateways, registering them and passing them to the GatewayCommunicator.
+ *
+ * It uses the Poco::Net::HTTPServer framework, and it is multi-thread server
+ * using thread pool. The WebSocketServer can run unsecured or secured by SSL/TLS.
+ *
+ * For each incoming connection is created the WebSocketRequestHandler by the
+ * WebSocketRequestHandlerFactory, which handle() method is invoked in thread
+ * pool. The WebSocket connection is there created, gateway is registered and
+ * then the connection is passed to the GatewayCommunicator. After the connection
+ * is added to the communicator, handler is destroyed and serving thread is
+ * returned to the thread pool.
+ */
+class WebSocketServer :
+		public StoppableLoop,
+		public HavingThreadPool {
+public:
+	WebSocketServer();
+
+	void start() override;
+	void stop() override;
+
+	void setGatewayCommunicator(GatewayCommunicator::Ptr communicator);
+	void setSSLConfig(Poco::SharedPtr<SSLServer> config);
+	void setGatewayService(GWSGatewayService::Ptr service);
+
+	void setPort(int port);
+	void setBacklog(int backlog);
+
+	/**
+	 * @brief Set max message size just for register message.
+	 */
+	void setMaxMessageSize(int size);
+
+protected:
+	Poco::Net::HTTPServer *createServer();
+
+private:
+	Poco::Net::ServerSocket createSocket();
+
+private:
+	int m_port;
+	int m_backlog;
+	size_t m_maxMessageSize;
+
+	Poco::SharedPtr<SSLServer> m_sslConfig;
+	GatewayCommunicator::Ptr m_gatewayCommunicator;
+	GWSGatewayService::Ptr m_gatewayService;
+	Poco::SharedPtr<Poco::Net::HTTPServer> m_server;
+};
+
+}
+
+#endif

commit 58ed203af891bae2b58e938a8d0b2b764c6af758

WebSocketRequestHandler: logging: fix missing __FILE__, __LINE__
Fixes: 42d7f6bbc432 ("WebSocketServer: new class")
Signed-off-by: Jozef Halaj <xhalaj03@stud.fit.vutbr.cz>
Acked-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

server/src/gws/WebSocketRequestHandler.cpp
@@ -27,8 +27,11 @@ void WebSocketRequestHandler::handleRequest(
 
 		int ret = ws.receiveFrame(buffer.begin(), buffer.size(), flags);
 		if (ret <= 0 || (flags & WebSocket::FRAME_OP_CLOSE)) {
-			if (logger().debug())
-				logger().debug(ws.peerAddress().toString() + " connection closed");
+			if (logger().debug()) {
+				logger().debug(ws.peerAddress().toString()
+					+ " connection closed",
+					__FILE__, __LINE__);
+			}
 			return;
 		}
 
@@ -42,7 +45,8 @@ void WebSocketRequestHandler::handleRequest(
 		if (registerMsg.isNull()) {
 			logger().warning("invalid message from "
 				+ ws.peerAddress().toString() + ":\n"
-				+ msg->toString());
+				+ msg->toString(),
+				__FILE__, __LINE__);
 			return;
 		}
 