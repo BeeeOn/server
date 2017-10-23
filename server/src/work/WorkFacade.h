1287473ca39a44ebb381b1bcaae8ff3e2790310e
Jan Viktorin <iviktorin@fit.vutbr.cz>
2017-10-23 18:37:31 +0200

    WorkFacade: fetch all accessible works for user
    The WorkFacade now allows to fetch all works of a the current user.
    Each work is associated via the owner property.
    
    Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

--- server/src/work/WorkFacade.h
+++ server/src/work/WorkFacade.h
@@ -1,12 +1,15 @@
 #ifndef BEEEON_WORK_FACADE_H
 #define BEEEON_WORK_FACADE_H
 
+#include <set>
+
 #include <Poco/SharedPtr.h>
+
+#include "work/Work.h"
 
 namespace BeeeOn {
 
 class PolicyContext;
-class Work;
 
 class WorkFacade {
 public:
@@ -18,6 +21,7 @@ public:
 	virtual void wakeup(Work &work, const PolicyContext &context) = 0;
 	virtual void cancel(Work &work, const PolicyContext &context) = 0;
 	virtual bool fetch(Work &work, const PolicyContext &context) = 0;
+	virtual void fetch(std::set<Work> &works, const PolicyContext &context) = 0;
 	virtual bool remove(const Work &work, const PolicyContext &context) = 0;
 };
 
