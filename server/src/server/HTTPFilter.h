73cf38eef08b20c6a5ec315e6f01cf2e9aee0c17
Jan Viktorin <iviktorin@fit.vutbr.cz>
2017-10-24 18:51:27 +0200

    HTTPFilter: new interface
    A HTTPFilter can be used to perform some preprocessing prior to reaching
    the appropriate HTTP handler.
    
    Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

---
+++ server/src/server/HTTPFilter.h
@@ -0,0 +1,40 @@
+#ifndef BEEEON_HTTP_FILTER_H
+#define BEEEON_HTTP_FILTER_H
+
+#include <Poco/SharedPtr.h>
+
+namespace Poco {
+namespace Net {
+
+class HTTPServerRequest;
+class HTTPServerResponse;
+
+}
+}
+
+namespace BeeeOn {
+
+/**
+ * @brief Filter HTTP requests. It can inject HTTP headers, handle requests
+ * (and thus avoiding to call the proper handler), precheck some HTTP headers, etc.
+ */
+class HTTPFilter {
+public:
+	typedef Poco::SharedPtr<HTTPFilter> Ptr;
+
+	virtual ~HTTPFilter();
+
+	/**
+	 * Called on request before it is handled. The filter can handle
+	 * the request and generate a response (it that case, it must flush
+	 * response headers) prior to the request handler. The request handler
+	 * is not called in such case.
+	 */
+	virtual void apply(
+		Poco::Net::HTTPServerRequest &req,
+		Poco::Net::HTTPServerResponse &res) = 0;
+};
+
+}
+
+#endif
