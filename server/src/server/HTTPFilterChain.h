11a7cfdf024cb87a505d401382d147028c46b62f
Jan Viktorin <iviktorin@fit.vutbr.cz>
2017-10-25 14:26:31 +0200

    HTTPFilterChain: new class
    The HTTPFilterChain provides a chain of HTTPFilter instances. It works as
    a central point for the filter registration and hides the filtering details
    from the rest of the system.
    
    Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

---
+++ server/src/server/HTTPFilterChain.h
@@ -0,0 +1,31 @@
+#ifndef BEEEON_HTTP_FILTER_CHAIN_H
+#define BEEEON_HTTP_FILTER_CHAIN_H
+
+#include <list>
+
+#include <Poco/SharedPtr.h>
+
+#include "server/HTTPFilter.h"
+
+namespace BeeeOn {
+
+class HTTPFilterChain {
+public:
+	typedef Poco::SharedPtr<HTTPFilterChain> Ptr;
+
+	HTTPFilterChain();
+	~HTTPFilterChain();
+
+	void applyChain(
+		Poco::Net::HTTPServerRequest &req,
+		Poco::Net::HTTPServerResponse &res);
+
+	void registerFilter(HTTPFilter::Ptr filter);
+
+private:
+	std::list<HTTPFilter::Ptr> m_chain;
+};
+
+}
+
+#endif
