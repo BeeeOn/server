BeeeOn/server

commit 5a2bc7dd837fbea097a241bcd28c4fa353ca870d

PocoRestRequestHandler: simple HTTP caching capabilities
If a REST action is configured as cachable, generate the appropriate
HTTP headers. There are 3 possible situations at the moment:

* ignore - no caching control headers are inserted
* 0 - resource is marked as non-cachable
* a positive value - a resource can be cached for the given amount of secons

The internal REST actions are always non-cachable.

Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

server/src/rest/PocoRestRequestHandler.cpp
@@ -1,4 +1,7 @@
 #include <Poco/Exception.h>
+#include <Poco/DateTime.h>
+#include <Poco/DateTimeFormat.h>
+#include <Poco/DateTimeFormatter.h>
 #include <Poco/Logger.h>
 
 #include <Poco/Net/HTTPServerParams.h>
@@ -87,6 +90,7 @@ void PocoRestRequestHandler::prepareInternalAction(
 		HTTPServerRequest &req,
 		HTTPServerResponse &res) const
 {
+	req.set("Cache-Control", "public, no-cache");
 }
 
 void PocoRestRequestHandler::prepareMappedAction(
@@ -94,6 +98,18 @@ void PocoRestRequestHandler::prepareMappedAction(
 		HTTPServerRequest &req,
 		HTTPServerResponse &res) const
 {
+	if (action->caching() == 0) {
+		req.set("Cache-Control", "public, no-cache");
+	}
+	else {
+		const Timespan shift(action->caching(), 0);
+		const DateTime now;
+
+		res.set("Expires",
+			DateTimeFormatter::format(now + shift, DateTimeFormat::HTTP_FORMAT));
+		res.set("Cache-Control",
+			"max-age=" + to_string(shift.totalSeconds()) + ", must-revalidate");
+	}
 }
 
 void PocoRestRequestHandler::handleRequest(

commit 35b4a842c815dce081e68dcfc102bed36e20c771

PocoRestRequestHandler: fix behaviour when caching is negative
When the RestAction::caching() property has a negative value then no
caching headers should be injected.

Fixes: 5a2bc7dd837f ("PocoRestRequestHandler: simple HTTP caching capabilities")
Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

server/src/rest/PocoRestRequestHandler.cpp
@@ -107,7 +107,7 @@ void PocoRestRequestHandler::prepareMappedAction(
 	if (action->caching() == 0) {
 		req.set("Cache-Control", "public, no-cache");
 	}
-	else {
+	else if (action->caching() > 0) {
 		const Timespan shift(action->caching(), 0);
 		const DateTime now;
 