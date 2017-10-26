93480f2c6acfa256c6d5adec86d554781c0387bd
Jan Viktorin <iviktorin@fit.vutbr.cz>
2017-10-27 01:28:21 +0200

    CPSPRestHandler: new class
    The CPSPRestHandler is a way how to integrate HTML pages into the server
    directly without needing any other service running. It utilizes the CPSP
    compiler of Poco to convert HTML pages into C++ classes.
    
    Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

---
+++ server/src/rest/CPSPRestHandler.cpp
@@ -0,0 +1,32 @@
+#include "di/Injectable.h"
+#include "rest/CPSPRestHandler.h"
+#include "rest/RestFlow.h"
+#include "util/ClassInfo.h"
+
+BEEEON_OBJECT_BEGIN(BeeeOn, CPSPRestHandler)
+BEEEON_OBJECT_CASTABLE(RestHandler)
+BEEEON_OBJECT_REF("handlers", &CPSPRestHandler::registerHandler)
+BEEEON_OBJECT_END(BeeeOn, CPSPRestHandler)
+
+using namespace std;
+using namespace Poco;
+using namespace Poco::Net;
+using namespace BeeeOn;
+
+CPSPRestHandler::CPSPRestHandler():
+	RestHandler("cpsp")
+{
+}
+
+void CPSPRestHandler::registerHandler(SharedPtr<HTTPRequestHandler> handler)
+{
+	ClassInfo clazz(*handler);
+
+	registerAction(clazz.name(),
+		[handler](RestFlow &flow) mutable {
+			handler->handleRequest(
+				flow.request().impl(),
+				flow.response().impl());
+		}
+	);
+}
