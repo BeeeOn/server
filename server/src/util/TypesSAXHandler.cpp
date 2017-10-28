BeeeOn/server

commit f19ce002b4f217daad009a39e644ed00efaa5f32

TypeInfo: define levels property
Each type can have a preset common list of levels. Each level represents
some situation that is typical for that type and can be used for certain
detections. The user can be automatically warned about dangerous levels
of CO2, too noisy environment, etc.

Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

server/src/util/TypesSAXHandler.cpp
@@ -17,12 +17,16 @@ TypesSAXHandler::TypesSAXHandler()
 	m_typeExpect.insert("unit");
 	m_typeExpect.insert("range");
 	m_typeExpect.insert("values");
+	m_typeExpect.insert("levels");
 
 	m_valuesExpect.insert("value");
+
+	m_levelsExpect.insert("level");
 
 	m_leafElements.insert("name");
 	m_leafElements.insert("unit");
 	m_leafElements.insert("value");
+	m_leafElements.insert("level");
 }
 
 TypesSAXHandler::~TypesSAXHandler()
@@ -98,6 +102,41 @@ void TypesSAXHandler::startElement(
 		if (logger().debug())
 			logger().debug("parsing value equals to " + to_string(m_lastValue), __FILE__, __LINE__);
 	}
+
+	if (isPathFromRoot("types", "type", "levels", "level")) {
+		double min = NAN;
+		double max = NAN;
+
+		if (getAttributeAsDouble(attrList, "equals", min)) {
+			m_level = TypeInfo::Level(min);
+		}
+		else if (getAttributeAsDouble(attrList, "min", min)) {
+			if (getAttributeAsDouble(attrList, "max", max))
+				m_level = TypeInfo::Level(min, max);
+			else
+				m_level = TypeInfo::Level(min, NAN);
+		}
+		else if (getAttributeAsDouble(attrList, "max", max)) {
+			m_level = TypeInfo::Level(NAN, max);
+		}
+		else {
+			error("missing attribute equals or attributes min and max");
+		}
+
+		XMLString attention;
+		if (getAndTrimAttribute(attrList, "attention", attention)) {
+			if (attention == "none")
+				m_level.setAttention(TypeInfo::Level::NONE);
+			else if (attention == "single")
+				m_level.setAttention(TypeInfo::Level::SINGLE);
+			else if (attention == "repeat")
+				m_level.setAttention(TypeInfo::Level::REPEAT);
+			else if (attention == "alert")
+				m_level.setAttention(TypeInfo::Level::ALERT);
+			else
+				error("unexpected value of attribute attention: " + attention);
+		}
+	}
 }
 
 void TypesSAXHandler::endElement(const SAXElement &element)
@@ -136,6 +175,30 @@ void TypesSAXHandler::endElement(const SAXElement &element)
 			logger().debug("parsing values " + to_string(m_temp.values().size()), __FILE__, __LINE__);
 	}
 
+	if (isPathFromRoot("types", "type", "levels", "level")) {
+		if (!element.content.empty())
+			m_level.setLabel(element.content);
+
+		if (!m_levels.emplace(m_level).second)
+			error("overlapping level: " + to_string(m_level.min()) + ".." + to_string(m_level.max()));
+
+		if (logger().debug()) {
+			logger().debug("parsing level "
+				+ to_string(m_level.min())
+				+ ".."
+				+ to_string(m_level.max()),
+				__FILE__, __LINE__);
+		}
+	}
+
+	if (isPathFromRoot("types", "type", "levels")) {
+		m_temp.setLevels(m_levels);
+		m_levels.clear();
+
+		if (logger().debug())
+			logger().debug("parsing levels " + to_string(m_temp.levels().size()), __FILE__, __LINE__);
+	}
+
 	if (isPathFromRoot("types", "type")) {
 		if (m_temp.name().empty())
 			error("missing name of type " + m_temp);
@@ -164,6 +227,10 @@ bool TypesSAXHandler::expectElement(const SAXElement &element) const
 
 	if (isPathFromRoot("types", "type", "values"))
 		return m_valuesExpect
+			.find(element.qName) != m_valuesExpect.end();
+
+	if (isPathFromRoot("types", "type", "levels"))
+		return m_levelsExpect
 			.find(element.qName) != m_valuesExpect.end();
 
 	return false;

server/conf/types.xml
@@ -12,6 +12,12 @@
 		<name>battery</name>
 		<unit>&#37;</unit>
 		<range min="0" max="100" step="1" />
+		<levels>
+			<level min="0"  max="10" attention="single">critical</level>
+			<level min="11" max="25" attention="single">low</level>
+			<level min="26" max="80">medium</level>
+			<level min="81" max="100">high</level>
+		</levels>
 	</type>
 	<type id="3">
 		<name>bitmap</name>
@@ -27,6 +33,15 @@
 		<!-- https://www.kane.co.uk/knowledge-centre/what-are-safe-levels-of-co-and-co2-in-rooms -->
 		<!-- http://www.engineeringtoolbox.com/co2-comfort-level-d_1024.html -->
 		<range min="0" max="1000000" step="1" />
+		<levels>
+			<level max="450">outdoor</level>
+			<level min="451" max="1000">indoor</level>
+			<level min="1001" max="2500">poor_air</level>
+			<level min="2501" max="5000" attention="single">unhealthy</level>
+			<level min="5001" max="10000" attention="repeat">stay_hours</level>
+			<level min="10001" max="30000" attention="repeat">stay_minutes</level>
+			<level min="30001" attention="alert">dangerous</level>
+		</levels>
 	</type>
 	<type id="6">
 		<name>enum</name>
@@ -37,6 +52,9 @@
 			<value equals="0">no_fire</value>
 			<value equals="1">fire</value>
 		</values>
+		<levels>
+			<level equals="1" attention="alert" />
+		</levels>
 	</type>
 	<type id="8">
 		<name>humidity</name>
@@ -54,6 +72,9 @@
 			<value equals="0">no_motion</value>
 			<value equals="1">motion</value>
 		</values>
+		<levels>
+			<level equals="1" attention="single" />
+		</levels>
 	</type>
 	<type id="11">
 		<name>noise</name>
@@ -61,6 +82,14 @@
 		<!-- http://www.noisehelp.com/noise-level-chart.html -->
 		<!-- http://www.noisehelp.com/noise-dose.html -->
 		<range min="0" max="200" step="1" />
+		<levels>
+			<level max="80">normal</level>
+			<level min="81" max="90">acceptable</level>
+			<level min="91" max="99" attention="single">laud</level>
+			<level min="100" max="111" attention="repeat">dangerous</level>
+			<level min="112" max="139" attention="repeat">very_dangerous</level>
+			<level min="140" attention="alert">damaging</level>
+		</levels>
 	</type>
 	<type id="12">
 		<name>open_close</name>
@@ -80,6 +109,10 @@
 		<name>performance</name>
 		<unit>&#37;</unit>
 		<range min="0" max="100" step="1" />
+		<levels>
+			<level equals="0">idle</level>
+			<level min="95">high_load</level>
+		</levels>
 	</type>
 	<type id="15">
 		<name>pressure</name>
@@ -91,6 +124,11 @@
 		<name>rssi</name>
 		<unit>&#37;</unit>
 		<range min="0" max="100" step="1" />
+		<levels>
+			<level max="25">poor</level>
+			<level min="26" max="80">good</level>
+			<level min="81" max="100">high</level>
+		</levels>
 	</type>
 	<type id="17">
 		<name>security_alert</name>
@@ -98,6 +136,9 @@
 			<value equals="0">ease</value>
 			<value equals="1">alert</value>
 		</values>
+		<levels>
+			<level equals="1" attention="alert" />
+		</levels>
 	</type>
 	<type id="18">
 		<name>shake</name>
@@ -117,5 +158,12 @@
 		<unit>UV</unit>
 		<!-- https://en.wikipedia.org/wiki/Ultraviolet_index -->
 		<range min="0" max="11" step="0.1" />
+		<levels>
+			<level max="2.9">low</level>
+			<level min="3" max="5.9">moderate</level>
+			<level min="6" max="7.9" attention="single">high</level>
+			<level min="8" max="10.9" attention="single">very_high</level>
+			<level min="11" attention="single">extreme</level>
+		</levels>
 	</type>
 </types>

server/dtd/types.dtd
@@ -1,6 +1,6 @@
 <!ELEMENT types (type)*>
 
-<!ELEMENT type (name, unit?, range?, values?)>
+<!ELEMENT type (name, unit?, range?, values?, levels?)>
 <!ATTLIST type id CDATA #REQUIRED>
 
 <!ELEMENT name (#PCDATA)>
@@ -16,3 +16,12 @@
 <!ELEMENT values (value+)>
 <!ELEMENT value (#PCDATA)>
 <!ATTLIST value equals CDATA #REQUIRED>
+
+<!ELEMENT levels (level+)>
+<!ELEMENT level (#PCDATA)>
+<!ATTLIST level
+	min    CDATA #IMPLIED
+	max    CDATA #IMPLIED
+	equals CDATA #IMPLIED
+	attention (none|single|repeat|alert) "none"
+>

server/src/model/TypeInfo.cpp
@@ -59,6 +59,16 @@ const map<int, string> &TypeInfo::values() const
 	return m_values;
 }
 
+void TypeInfo::setLevels(const set<Level> &levels)
+{
+	m_levels = levels;
+}
+
+const set<TypeInfo::Level> &TypeInfo::levels() const
+{
+	return m_levels;
+}
+
 std::string TypeInfo::asString(const double v) const
 {
 	return to_string(v);
@@ -116,4 +126,84 @@ double TypeInfo::Range::step() const
 bool TypeInfo::Range::isValid() const
 {
 	return hasStep() || hasMin() || hasMax();
+}
+
+TypeInfo::Level::Level():
+	m_min(NAN),
+	m_max(NAN),
+	m_attention(NONE)
+{
+}
+
+TypeInfo::Level::Level(double value):
+	m_min(value),
+	m_max(value),
+	m_attention(NONE)
+{
+}
+
+TypeInfo::Level::Level(double min, double max):
+	m_min(min),
+	m_max(max),
+	m_attention(NONE)
+{
+	if (std::isnan(m_min) && std::isnan(m_max))
+		throw InvalidArgumentException("at least min or max must be a number");
+
+	if (!std::isnan(m_min) && !std::isnan(m_max)) {
+		if (m_min > m_max)
+			throw InvalidArgumentException("min must not be greater then max");
+	}
+}
+
+bool TypeInfo::Level::lessThan(const TypeInfo::Level &other) const
+{
+	if (!isValid() && !other.isValid())
+		return false;
+
+	if (!isValid())
+		return true;
+
+	if (std::isnan(m_max) && std::isnan(other.m_min))
+		return false;
+
+	if (m_max < other.m_min)
+		return true;
+
+	return false;
+}
+
+bool TypeInfo::Level::isValid() const
+{
+	return !(std::isnan(m_min) && std::isnan(m_max));
+}
+
+double TypeInfo::Level::min() const
+{
+	return m_min;
+}
+
+double TypeInfo::Level::max() const
+{
+	return m_max;
+}
+
+void TypeInfo::Level::setAttention(const Attention attention)
+{
+	m_attention = attention;
+}
+
+TypeInfo::Level::Attention TypeInfo::Level::attention() const
+{
+	return m_attention;
+}
+
+void TypeInfo::Level::setLabel(const string &label)
+{
+	m_label = label;
+}
+
+string TypeInfo::Level::label() const
+{
+	return m_label;
 }

server/src/model/TypeInfo.h
@@ -2,6 +2,7 @@
 #define BEEEON_TYPE_INFO_H
 
 #include <map>
+#include <set>
 #include <string>
 
 #include "model/Entity.h"
@@ -33,6 +34,57 @@ public:
 		double m_step;
 	};
 
+	class Level {
+	public:
+		enum Attention {
+			/**
+			 * Do not notify user about this level.
+			 */
+			NONE,
+			/**
+			 * Notify the user gently once.
+			 */
+			SINGLE,
+			/**
+			 * Notify the user gently and repeat if the level does not improve.
+			 */
+			REPEAT,
+			/**
+			 * Notify the user loudly.
+			 * The level is serious and can be dangerous.
+			 */
+			ALERT
+		};
+
+		Level();
+		Level(double value);
+		Level(double min, double max);
+
+		bool operator <(const Level &other) const
+		{
+			return lessThan(other);
+		}
+
+		bool lessThan(const Level &other) const;
+
+		bool isValid() const;
+
+		double min() const;
+		double max() const;
+
+		void setAttention(const Attention attention);
+		Attention attention() const;
+
+		void setLabel(const std::string &label);
+		std::string label() const;
+
+	private:
+		double m_min;
+		double m_max;
+		Attention m_attention;
+		std::string m_label;
+	};
+
 	TypeInfo();
 	TypeInfo(const TypeInfo::ID &id);
 
@@ -48,6 +100,9 @@ public:
 	void setValues(const std::map<int, std::string> &values);
 	const std::map<int, std::string> &values() const;
 
+	void setLevels(const std::set<Level> &levels);
+	const std::set<Level> &levels() const;
+
 	std::string asString(const double v) const;
 
 private:
@@ -55,6 +110,7 @@ private:
 	std::string m_unit;
 	Range m_range;
 	std::map<int, std::string> m_values;
+	std::set<Level> m_levels;
 };
 
 typedef TypeInfo::ID TypeInfoID;

server/src/util/TypesSAXHandler.h
@@ -36,10 +36,13 @@ protected:
 private:
 	std::set<Poco::XML::XMLString> m_typeExpect;
 	std::set<Poco::XML::XMLString> m_valuesExpect;
+	std::set<Poco::XML::XMLString> m_levelsExpect;
 	std::set<Poco::XML::XMLString> m_leafElements;
 	std::set<TypeInfo> m_result;
 	std::map<int, std::string> m_values;
 	int m_lastValue;
+	TypeInfo::Level m_level;
+	std::set<TypeInfo::Level> m_levels;
 	TypeInfo m_temp;
 };
 

server/test/util/TypesSAXHandlerTest.cpp
@@ -21,6 +21,8 @@ class TypesSAXHandlerTest : public CppUnit::TestFixture {
 	CPPUNIT_TEST(testParseWithValues);
 	CPPUNIT_TEST(testParseHasBothValuesAndRange);
 	CPPUNIT_TEST(testParseInvalidValues);
+	CPPUNIT_TEST(testParseWithLevels);
+	CPPUNIT_TEST(testParseOverlappingLevels);
 	CPPUNIT_TEST(testParseMissingId);
 	CPPUNIT_TEST(testParseMissingName);
 	CPPUNIT_TEST_SUITE_END();
@@ -33,6 +35,8 @@ public:
 	void testParseWithValues();
 	void testParseHasBothValuesAndRange();
 	void testParseInvalidValues();
+	void testParseWithLevels();
+	void testParseOverlappingLevels();
 	void testParseMissingId();
 	void testParseMissingName();
 
@@ -293,6 +297,111 @@ void TypesSAXHandlerTest::testParseInvalidValues()
 			"</types>"),
 		XMLException
 	);
+}
+
+void TypesSAXHandlerTest::testParseWithLevels()
+{
+	CPPUNIT_ASSERT_NO_THROW(
+		m_parser.parseString(
+			"<types>"
+			"  <type id=\"0x01\">"
+			"    <name>NAME1</name>"
+			"    <levels>"
+			"      <level equals=\"0\">zero</level>"
+			"      <level equals=\"1\">one</level>"
+			"    </levels>"
+			"  </type>"
+			"  <type id=\"0x02\">"
+			"    <name>NAME2</name>"
+			"    <unit>&#176;C</unit>"
+			"    <levels>"
+			"      <level min=\"0\" max=\"1\">0..1</level>"
+			"      <level min=\"2\" max=\"3\">2..3</level>"
+			"      <level equals=\"4\">4</level>"
+			"    </levels>"
+			"  </type>"
+			"  <type id=\"0x03\">"
+			"    <name>NAME3</name>"
+			"    <levels>"
+			"      <level max=\"10\">?..10</level>"
+			"      <level equals=\"11\">minus two</level>"
+			"      <level min=\"12\">12..?</level>"
+			"    </levels>"
+			"  </type>"
+			"</types>")
+	);
+
+	for (auto it : *m_handler) {
+		if (it.id() == TypeInfoID::parse("0x01")) {
+			CPPUNIT_ASSERT_EQUAL(string("NAME1"), it.name());
+			CPPUNIT_ASSERT_EQUAL(string(""), it.unit());
+			CPPUNIT_ASSERT_EQUAL(2, it.levels().size());
+		}
+		else if (it.id() == TypeInfoID::parse("0x02")) {
+			CPPUNIT_ASSERT_EQUAL(string("NAME2"), it.name());
+			CPPUNIT_ASSERT_EQUAL(string("°C"), it.unit());
+			CPPUNIT_ASSERT_EQUAL(3, it.levels().size());
+		}
+		else if (it.id() == TypeInfoID::parse("0x03")) {
+			CPPUNIT_ASSERT_EQUAL(string("NAME3"), it.name());
+			CPPUNIT_ASSERT_EQUAL(string(""), it.unit());
+			CPPUNIT_ASSERT_EQUAL(3, it.levels().size());
+		}
+		else {
+			CPPUNIT_ASSERT(false);
+		}
+	}
+}
+
+void TypesSAXHandlerTest::testParseOverlappingLevels()
+{
+	CPPUNIT_ASSERT_THROW(m_parser.parseString(
+		"<types>"
+		"  <type>"
+		"    <name>NAME</name>"
+		"    <levels>"
+		"      <level equals=\"10\">10</level>"
+		"      <level equals=\"10\">10</level>"
+		"    </levels>"
+		"  </type>"
+		"</types>"),
+		XMLException);
+
+	CPPUNIT_ASSERT_THROW(m_parser.parseString(
+		"<types>"
+		"  <type>"
+		"    <name>NAME</name>"
+		"    <levels>"
+		"      <level min=\"0\" max=\"10\">0..10</level>"
+		"      <level min=\"10\" max=\"20\">10..20</level>"
+		"    </levels>"
+		"  </type>"
+		"</types>"),
+		XMLException);
+
+	CPPUNIT_ASSERT_THROW(m_parser.parseString(
+		"<types>"
+		"  <type>"
+		"    <name>NAME</name>"
+		"    <levels>"
+		"      <level equals=\"10\">10</level>"
+		"      <level min=\"10\" max=\"20\">10..20</level>"
+		"    </levels>"
+		"  </type>"
+		"</types>"),
+		XMLException);
+
+	CPPUNIT_ASSERT_THROW(m_parser.parseString(
+		"<types>"
+		"  <type>"
+		"    <name>NAME</name>"
+		"    <levels>"
+		"      <level max=\"10\">10</level>"
+		"      <level min=\"10\">10</level>"
+		"    </levels>"
+		"  </type>"
+		"</types>"),
+		XMLException);
 }
 
 void TypesSAXHandlerTest::testParseMissingId()

commit 6361b705ae92e4ae9db832007d34b8688c5f757f

TypesSAXHandler: fix missing clear of levels attribute
Without clearing the levels property on the m_temp, also the following parsed
types get the levels accidently.

Fixes: f19ce002b4f2 ("TypeInfo: define levels property")
Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

server/src/util/TypesSAXHandler.cpp
@@ -58,6 +58,7 @@ void TypesSAXHandler::startElement(
 		m_temp.setUnit("");
 		m_temp.setRange(TypeInfo::Range());
 		m_temp.setValues({});
+		m_temp.setLevels({});
 	}
 
 	if (isPathFromRoot("types", "type", "range")) {