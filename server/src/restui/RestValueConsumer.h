deaebfc09ae69c401e0c5f114b3d181b2267079d
Jan Viktorin <iviktorin@fit.vutbr.cz>
2017-10-30 12:13:59 +0100

    RestValueConsumer: log count of consumed values
    Because data are send to the user via streaming, we have possibly no exact
    knowledge of their amount before sending them. Thus count them as they are
    coming. This way, we now, how many data has been send to the client and we
    can log them for debugging purposes.
    
    Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

--- server/src/restui/RestValueConsumer.h
+++ server/src/restui/RestValueConsumer.h
@@ -2,6 +2,7 @@
 #define BEEEON_REST_VALUE_CONSUMER_H
 
 #include "service/ValueConsumer.h"
+#include "util/Loggable.h"
 
 namespace Poco {
 namespace JSON {
@@ -15,7 +16,7 @@ namespace BeeeOn {
 
 namespace RestUI {
 
-class RestValueConsumer : public ValueConsumer {
+class RestValueConsumer : public ValueConsumer, protected Loggable {
 public:
 	RestValueConsumer(Poco::JSON::PrintHandler &output);
 
@@ -29,6 +30,7 @@ protected:
 private:
 	Poco::JSON::PrintHandler &m_output;
 	const TypeInfo *m_info;
+	size_t m_count;
 };
 
 }
