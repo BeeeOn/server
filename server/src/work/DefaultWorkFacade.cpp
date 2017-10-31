BeeeOn/server

commit 50ea5b2445bf1d9c4d7d674cf2c0444cbcd819ce

WorkFacade: implement access policy
The WorkFacade assures permissions while manipulating with Work instances.
Usually, only the owner of a selected Work instance can access it.

Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

server/src/work/DefaultWorkFacade.cpp
@@ -10,6 +10,7 @@ BEEEON_OBJECT_BEGIN(BeeeOn, DefaultWorkFacade)
 BEEEON_OBJECT_CASTABLE(WorkFacade)
 BEEEON_OBJECT_REF("scheduler", &DefaultWorkFacade::setScheduler)
 BEEEON_OBJECT_REF("lockManager", &DefaultWorkFacade::setLockManager)
+BEEEON_OBJECT_REF("accessPolicy", &DefaultWorkFacade::setAccessPolicy)
 BEEEON_OBJECT_END(BeeeOn, DefaultWorkFacade)
 
 using namespace Poco;
@@ -29,6 +30,11 @@ void DefaultWorkFacade::setLockManager(SharedPtr<WorkLockManager> manager)
 	m_lockManager = manager;
 }
 
+void DefaultWorkFacade::setAccessPolicy(WorkAccessPolicy::Ptr policy)
+{
+	m_accessPolicy = policy;
+}
+
 const UserPolicyContext &DefaultWorkFacade::asUserContext(const PolicyContext &context) const
 {
 	if (context.is<UserPolicyContext>())
@@ -45,7 +51,7 @@ void DefaultWorkFacade::apply(Work::Ptr &work, const Work &data) const
 		work->setPriority(data.priority());
 }
 
-void DefaultWorkFacade::schedule(Work &work)
+void DefaultWorkFacade::schedule(Work &work, const PolicyContext &context)
 {
 	Work::Ptr p;
 
@@ -55,10 +61,25 @@ void DefaultWorkFacade::schedule(Work &work)
 		auto it = m_storage.find(work.id());
 		if (it != m_storage.end()) {
 			p = it->second;
+
+			m_accessPolicy->assure(
+				WorkAccessPolicy::ACTION_USER_SCHEDULE,
+				context,
+				*p
+			);
+
 			apply(p, work);
 		}
 		else {
 			p = new Work(work);
+			p->setOwner(asUserContext(context).user());
+
+			m_accessPolicy->assure(
+				WorkAccessPolicy::ACTION_USER_SCHEDULE,
+				context,
+				*p
+			);
+
 			m_storage.emplace(work.id(), p);
 		}
 	}
@@ -83,13 +104,22 @@ void DefaultWorkFacade::wakeup(Work &work, const PolicyContext &context)
 			throw NotFoundException("no such work to wakeup: " + work);
 	}
 
+	{
+		WorkReadGuard guard(m_lockManager->readOnly(work.id()));
+		m_accessPolicy->assure(
+			WorkAccessPolicy::ACTION_USER_WAKEUP,
+			context,
+			*p
+		);
+	}
+
 	m_scheduler->wakeup(p);
 
 	WorkReadGuard guard(m_lockManager->readOnly(work.id()));
 	work = *p;
 }
 
-void DefaultWorkFacade::cancel(Work &work)
+void DefaultWorkFacade::cancel(Work &work, const PolicyContext &context)
 {
 	Work::Ptr p;
 
@@ -103,32 +133,58 @@ void DefaultWorkFacade::cancel(Work &work)
 			throw NotFoundException("no such work to cancel: " + work);
 	}
 
+	{
+		WorkReadGuard guard(m_lockManager->readOnly(work.id()));
+		m_accessPolicy->assure(
+			WorkAccessPolicy::ACTION_USER_CANCEL,
+			context,
+			*p
+		);
+	}
+
 	m_scheduler->cancel(p);
 
 	WorkReadGuard guard(m_lockManager->readOnly(work.id()));
 	work = *p;
 }
 
-bool DefaultWorkFacade::fetch(Work &work)
+bool DefaultWorkFacade::fetch(Work &work, const PolicyContext &context)
 {
 	RWLock::ScopedReadLock guard(m_lock);
 
 	auto it = m_storage.find(work.id());
 	if (it != m_storage.end()) {
 		WorkReadGuard guard(m_lockManager->readOnly(work.id()));
-		work = *(it->second);
+		Work::Ptr p = it->second;
+
+		m_accessPolicy->assure(
+			WorkAccessPolicy::ACTION_USER_CANCEL,
+			context,
+			*p
+		);
+
+		work = *p;
 		return true;
 	}
 
 	return false;
 }
 
-bool DefaultWorkFacade::remove(const Work &work)
+bool DefaultWorkFacade::remove(const Work &work, const PolicyContext &context)
 {
 	RWLock::ScopedWriteLock guard(m_lock);
 
 	auto it = m_storage.find(work.id());
 	if (it != m_storage.end()) {
+		WorkReadGuard guard(m_lockManager->readOnly(work.id()));
+		Work::Ptr p = it->second;
+
+		m_accessPolicy->assure(
+			WorkAccessPolicy::ACTION_USER_REMOVE,
+			context,
+			*p
+		);
+
 		m_storage.erase(it);
 		return true;
 	}

server/conf/config.d/factory-work.xml
@@ -24,6 +24,7 @@
 		<instance name="uiWorkFacade" class="BeeeOn::DefaultWorkFacade">
 			<set name="scheduler" ref="uiWorkScheduler" />
 			<set name="lockManager" ref="uiWorkLockManager" />
+			<set name="accessPolicy" ref="accessPolicy" />
 		</instance>
 	</factory>
 </system>

server/src/service/DeviceServiceImpl.cpp
@@ -130,7 +130,7 @@ Work DeviceServiceImpl::doUnregister(Relation<Device, Gateway> &input)
 	content.setDeviceID(device.id());
 	work.setContent(content);
 
-	m_workFacade->schedule(work);
+	m_workFacade->schedule(work, input);
 	return work;
 }
 

server/src/work/DefaultWorkFacade.h
@@ -6,6 +6,7 @@
 #include <Poco/RWLock.h>
 #include <Poco/SharedPtr.h>
 
+#include "policy/WorkAccessPolicy.h"
 #include "work/Work.h"
 #include "work/WorkFacade.h"
 
@@ -19,14 +20,15 @@ class DefaultWorkFacade : public WorkFacade {
 public:
 	DefaultWorkFacade();
 
-	void schedule(Work &work) override;
-	void wakeup(Work &work) override;
-	void cancel(Work &work) override;
-	bool fetch(Work &work) override;
-	bool remove(const Work &work) override;
+	void schedule(Work &work, const PolicyContext &context) override;
+	void wakeup(Work &work, const PolicyContext &context) override;
+	void cancel(Work &work, const PolicyContext &context) override;
+	bool fetch(Work &work, const PolicyContext &context) override;
+	bool remove(const Work &work, const PolicyContext &context) override;
 
 	void setScheduler(Poco::SharedPtr<WorkScheduler> scheduler);
 	void setLockManager(Poco::SharedPtr<WorkLockManager> manager);
+	void setAccessPolicy(WorkAccessPolicy::Ptr policy);
 
 protected:
 	void apply(Work::Ptr &work, const Work &data) const;
@@ -35,6 +37,7 @@ protected:
 private:
 	mutable Poco::SharedPtr<WorkLockManager> m_lockManager;
 	Poco::SharedPtr<WorkScheduler> m_scheduler;
+	WorkAccessPolicy::Ptr m_accessPolicy;
 	std::map<WorkID, Work::Ptr> m_storage;
 	Poco::RWLock m_lock;
 };

server/src/work/WorkFacade.h
@@ -5,6 +5,7 @@
 
 namespace BeeeOn {
 
+class PolicyContext;
 class Work;
 
 class WorkFacade {
@@ -13,11 +14,11 @@ public:
 
 	virtual ~WorkFacade();
 
-	virtual void schedule(Work &work) = 0;
-	virtual void wakeup(Work &work) = 0;
-	virtual void cancel(Work &work) = 0;
-	virtual bool fetch(Work &work) = 0;
-	virtual bool remove(const Work &work) = 0;
+	virtual void schedule(Work &work, const PolicyContext &context) = 0;
+	virtual void wakeup(Work &work, const PolicyContext &context) = 0;
+	virtual void cancel(Work &work, const PolicyContext &context) = 0;
+	virtual bool fetch(Work &work, const PolicyContext &context) = 0;
+	virtual bool remove(const Work &work, const PolicyContext &context) = 0;
 };
 
 }

commit 0e1bd45324edde0d129fc9c64a88587a0aa62b68

DefaultWorkFacade: fix access policy action on fetch
Fixes: 50ea5b2445bf ("WorkFacade: implement access policy")
Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

server/src/work/DefaultWorkFacade.cpp
@@ -158,7 +158,7 @@ bool DefaultWorkFacade::fetch(Work &work, const PolicyContext &context)
 		Work::Ptr p = it->second;
 
 		m_accessPolicy->assure(
-			WorkAccessPolicy::ACTION_USER_CANCEL,
+			WorkAccessPolicy::ACTION_USER_GET,
 			context,
 			*p
 		);