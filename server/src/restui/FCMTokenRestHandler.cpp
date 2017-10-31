BeeeOn/server

commit 6162962637da5e1a8e0a4f1d4e01ee601aa469f7

FCMTokenRestHandler: new class
Implement REST API endpoints:

* notifications.register    -> POST /notifications
* notifications.unregister  -> DELETE /notifications

Provides an interfacw to access the FCMTokenRestHandler for registration
and unregistration of tokens for Firebase Cloud Messaging service with
BeeeOn server.

Signed-off-by: Peter Kubov <xkubov06@stud.fit.vutbr.cz>
Reviewed-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

server/conf/restui-api.ini
@@ -130,3 +130,9 @@ api.call.controls.current.caching = 60
 
 api.call.controls.request_change.uri = /gateways/$gateway_id/devices/$device_id/controls/$control_id/current
 api.call.controls.request_change.method = POST
+
+api.call.notifications.register.uri = /notifications
+api.call.notifications.register.method = POST
+
+api.call.notifications.unregister.uri = /notifications
+api.call.notifications.unregister.method = DELETE

server/src/restui/FCMTokenRestHandler.cpp
@@ -1,0 +1,77 @@
+#include <Poco/Exception.h>
+#include <Poco/JSON/Object.h>
+#include <Poco/JSON/PrintHandler.h>
+
+#include "di/Injectable.h"
+#include "model/FCMToken.h"
+#include "model/User.h"
+#include "rest/RestFlow.h"
+#include "rest/RestLinker.h"
+#include "restui/FCMTokenRestHandler.h"
+#include "restui/Serializing.h"
+#include "util/JsonUtil.h"
+#include "util/Sanitize.h"
+
+BEEEON_OBJECT_BEGIN(BeeeOn, RestUI, FCMTokenRestHandler)
+BEEEON_OBJECT_CASTABLE(RestHandler)
+BEEEON_OBJECT_REF("fcmTokenService", &FCMTokenRestHandler::setFCMTokenService)
+BEEEON_OBJECT_END(BeeeOn, RestUI, FCMTokenRestHandler)
+
+using namespace std;
+using namespace Poco;
+using namespace Poco::JSON;
+using namespace BeeeOn;
+using namespace BeeeOn::RestUI;
+
+FCMTokenRestHandler::FCMTokenRestHandler():
+	JSONRestHandler("notifications")
+{
+	registerAction<FCMTokenRestHandler>("register", &FCMTokenRestHandler::registerToken);
+	registerAction<FCMTokenRestHandler>("unregister", &FCMTokenRestHandler::unregisterToken, {"fcm_token"});
+}
+
+void FCMTokenRestHandler::setFCMTokenService(FCMTokenService::Ptr service)
+{
+	m_service = service;
+}
+
+FCMToken FCMTokenRestHandler::extractToken(RestFlow &flow) const
+{
+	User user(flow.session()->userID());
+	Object::Ptr object = parseInput(flow);
+
+	if (object->getValue<string>("name") != "fcm")
+		throw InvalidArgumentException("unrecognized attribute name");
+
+	FCMToken token = FCMTokenID::parse(
+		Sanitize::token(object->getValue<string>("id")));
+	token.setUser(user);
+
+	return token;
+}
+
+void FCMTokenRestHandler::registerToken(RestFlow &flow)
+{
+	User user(flow.session()->userID());
+	FCMToken token = extractToken(flow);
+
+	Single<FCMToken> data(token);
+	data.setUser(user);
+
+	m_service->registerToken(data);
+
+	flow.response().setStatus(204);
+}
+
+void FCMTokenRestHandler::unregisterToken(RestFlow &flow)
+{
+	User user(flow.session()->userID());
+	FCMToken token = extractToken(flow);
+
+	Single<FCMToken> data(token);
+	data.setUser(user);
+
+	m_service->unregisterToken(data);
+
+	flow.response().setStatus(204);
+}

server/conf/config.d/factory-restui.xml
@@ -52,6 +52,7 @@
 			<add name="handlers" ref="uiSensorHandler" />
 			<add name="handlers" ref="uiControlHandler" />
 			<add name="handlers" ref="uiTypeHandler" />
+			<add name="handlers" ref="uiFCMTokenHandler" if-yes="${fcm.enable}" />
 		</instance>
 
 		<instance name="uiBuiltinHandler" class="BeeeOn::BuiltinRestHandler" />
@@ -100,6 +101,10 @@
 		<instance name="uiTypeHandler" class="BeeeOn::RestUI::TypeRestHandler">
 			<set name="typeInfoProvider" ref="typeInfoProvider" />
 			<set name="translatorFactory" ref="translatorFactory" />
+		</instance>
+
+		<instance name="uiFCMTokenHandler" class="BeeeOn::RestUI::FCMTokenRestHandler">
+			<set name="fcmTokenService" ref="fcmTokenService" />
 		</instance>
 	</factory>
 </system>

server/src/CMakeLists.txt
@@ -252,6 +252,7 @@ file(GLOB RESTUI_SOURCES
 	restui/JSONRoleInGatewayDeserializer.cpp
 	restui/ControlRestHandler.cpp
 	restui/DeviceRestHandler.cpp
+	restui/FCMTokenRestHandler.cpp
 	restui/LocationRestHandler.cpp
 	restui/ProfileRestHandler.cpp
 	restui/RestValueConsumer.cpp

server/src/restui/FCMTokenRestHandler.h
@@ -1,0 +1,26 @@
+#pragma once
+
+#include "rest/JSONRestHandler.h"
+#include "service/FCMTokenService.h"
+
+namespace BeeeOn {
+namespace RestUI {
+
+class FCMTokenRestHandler : public JSONRestHandler {
+public:
+	FCMTokenRestHandler();
+
+	void setFCMTokenService(FCMTokenService::Ptr service);
+
+	void registerToken(RestFlow &flow);
+	void unregisterToken(RestFlow &flow);
+
+protected:
+	FCMToken extractToken(RestFlow &flow) const;
+
+private:
+	FCMTokenService::Ptr m_service;
+};
+
+}
+}

commit 60231005cf4bb09c9d2957b0fd0bcfe25a65710c

FCMTokenRestHandler: change URI mapping and passing arguments
The URI mapping of the notifications is changed to be more logical:

* the /notifications represents a collection of notification services
* one of the services is /notifications/fcm
* it is possible to register a token with /notifications/fcm
* it is possible to unregister a token from /notifications/fcm

The DELETE /notifications/fcm/$token accepts the token ID via URI
as it is discouraged to pass data in body for DELETE requests.

Fixes: 6162962637da ("FCMTokenRestHandler: new class")
Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

server/conf/restui-api.ini
@@ -133,8 +133,8 @@ api.call.controls.current.caching = 60
 api.call.controls.request_change.uri = /gateways/$gateway_id/devices/$device_id/controls/$control_id/current
 api.call.controls.request_change.method = POST
 
-api.call.notifications.register.uri = /notifications
+api.call.notifications.register.uri = /notifications/fcm
 api.call.notifications.register.method = POST
 
-api.call.notifications.unregister.uri = /notifications
+api.call.notifications.unregister.uri = /notifications/fcm/${token}
 api.call.notifications.unregister.method = DELETE

server/src/restui/FCMTokenRestHandler.cpp
@@ -27,7 +27,7 @@ FCMTokenRestHandler::FCMTokenRestHandler():
 	JSONRestHandler("notifications")
 {
 	registerAction<FCMTokenRestHandler>("register", &FCMTokenRestHandler::registerToken);
-	registerAction<FCMTokenRestHandler>("unregister", &FCMTokenRestHandler::unregisterToken, {"fcm_token"});
+	registerAction<FCMTokenRestHandler>("unregister", &FCMTokenRestHandler::unregisterToken, {"token"});
 }
 
 void FCMTokenRestHandler::setFCMTokenService(FCMTokenService::Ptr service)
@@ -39,9 +39,6 @@ FCMToken FCMTokenRestHandler::extractToken(RestFlow &flow) const
 {
 	User user(flow.session()->userID());
 	Object::Ptr object = parseInput(flow);
-
-	if (object->getValue<string>("name") != "fcm")
-		throw InvalidArgumentException("unrecognized attribute name");
 
 	FCMToken token = FCMTokenID::parse(
 		Sanitize::token(object->getValue<string>("id")));
@@ -66,7 +63,10 @@ void FCMTokenRestHandler::registerToken(RestFlow &flow)
 void FCMTokenRestHandler::unregisterToken(RestFlow &flow)
 {
 	User user(flow.session()->userID());
-	FCMToken token = extractToken(flow);
+	FCMToken token = FCMTokenID::parse(
+		Sanitize::token(flow.param("token"))
+	);
+	token.setUser(user);
 
 	Single<FCMToken> data(token);
 	data.setUser(user);

server/t/restui/t1010-fcm-token-register-unregister.py
@@ -35,21 +35,17 @@ class TestFCMTokenRegisterUnregister(unittest.TestCase):
 	@unittest.skipIf("FCM_AVAILABLE" not in os.environ,
 			"fcm support is disabled")
 	def test_register_unregister_token(self):
-		req = POST(config.ui_host, config.ui_port, "/notifications")
+		req = POST(config.ui_host, config.ui_port, "/notifications/fcm")
 		req.authorize(self.session)
 		req.body(json.dumps(
-			{"id": config.fcm_token, "name": "fcm"}
+			{"id": config.fcm_token}
 		))
 		response, _ = req()
 
 		self.assertEqual(204, response.status)
 
-		req = DELETE(config.ui_host, config.ui_port, "/notifications");
+		req = DELETE(config.ui_host, config.ui_port, "/notifications/fcm/" + config.fcm_token);
 		req.authorize(self.session)
-		req.body(json.dumps(
-			{"id": config.fcm_token, "name": "fcm"}
-		));
-		response, _ = req()
 
 		self.assertEqual(204, response.status)
 