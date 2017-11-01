BeeeOn/server

commit f6beaaf991685be9235372b757e9f0bfee63c761

AsyncGatewayRPC: new class
This class provides asynchronous GatewayRPC intended to be used in the
BeeeOn Server running as monolit (implements GatewayRPC and RPCForwarder
together).

The AsyncGatewayRPC allows to send commands to the gateways and receive
partial results asynchronously. Stored ResultCall for sent request is
executed for every partial result till final result or timeout expiration.

Known issue:
* handling of duplicate response for the same request is probably wrong
  (can happen when ack is not delivered to the gateway properly)

Signed-off-by: Jozef Halaj <xhalaj03@stud.fit.vutbr.cz>
Acked-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

server/src/rpc/AsyncGatewayRPC.cpp
@@ -1,0 +1,346 @@
+#include "di/Injectable.h"
+#include "gwmessage/GWListenRequest.h"
+#include "gwmessage/GWUnpairRequest.h"
+#include "gwmessage/GWPingRequest.h"
+#include "gwmessage/GWSetValueRequest.h"
+#include "rpc/AsyncGatewayRPC.h"
+
+BEEEON_OBJECT_BEGIN(BeeeOn, AsyncGatewayRPC)
+BEEEON_OBJECT_CASTABLE(GatewayRPC)
+BEEEON_OBJECT_CASTABLE(RPCForwarder)
+BEEEON_OBJECT_CASTABLE(StoppableLoop)
+BEEEON_OBJECT_REF("gatewayCommunicator", &AsyncGatewayRPC::setGatewayCommunicator)
+BEEEON_OBJECT_REF("responseExpectedQueue", &AsyncGatewayRPC::setGWResponseExpectedQueue)
+BEEEON_OBJECT_TIME("finalResultTimeout", &AsyncGatewayRPC::setFinalResultTimeout)
+BEEEON_OBJECT_END(BeeeOn, AsyncGatewayRPC)
+
+using namespace std;
+using namespace Poco;
+using namespace BeeeOn;
+
+void AsyncGatewayRPC::sendListen(const ResultCall &resultCall,
+		const Gateway &gateway,
+		const Timespan &duration)
+{
+	CallID callID = CallID::random();
+
+	GWListenRequest::Ptr request = new GWListenRequest;
+	request->setID(callID);
+	request->setDuration(duration);
+
+	sendAndExpectResult(gateway.id(), callID, resultCall, request);
+}
+
+void AsyncGatewayRPC::unpairDevice(const ResultCall &resultCall,
+		const Gateway &gateway,
+		const Device &device)
+{
+	CallID callID = CallID::random();
+
+	GWUnpairRequest::Ptr request = new GWUnpairRequest;
+	request->setID(callID);
+	request->setDeviceID(device.id());
+
+	sendAndExpectResult(gateway.id(), callID, resultCall, request);
+}
+
+void AsyncGatewayRPC::pingGateway(const ResultCall &resultCall,
+		const Gateway &gateway)
+{
+	CallID callID = CallID::random();
+
+	GWPingRequest::Ptr request = new GWPingRequest;
+	request->setID(callID);
+
+	sendAndExpectResult(gateway.id(), callID, resultCall, request);
+}
+
+void AsyncGatewayRPC::updateActor(const ResultCall &resultCall,
+		const Gateway &gateway,
+		const Device &device,
+		const ModuleInfo &module,
+		double value,
+		const Timespan &timeout)
+{
+	CallID callID = CallID::random();
+
+	GWSetValueRequest::Ptr request = new GWSetValueRequest;
+	request->setID(callID);
+	request->setDeviceID(device.id());
+	request->setModuleID((unsigned int) module.id());
+	request->setValue(value);
+	request->setTimeout(timeout);
+
+	sendAndExpectResult(gateway.id(), callID, resultCall, request);
+}
+
+LambdaTimerTask::Ptr AsyncGatewayRPC::createFinalResultMissingTask(
+		const GatewayID &gatewayID, const CallID &callID)
+{
+	return new LambdaTimerTask([gatewayID, callID, this]()
+	{
+		logger().warning("result for call "
+			+ gatewayID.toString()
+			+ " : "
+			+ callID.toString()
+			+ " is missing",
+			__FILE__, __LINE__);
+
+		ScopedLockWithUnlock<FastMutex> guard(m_mutex);
+
+		auto it = m_resultCalls.find(make_pair(gatewayID, callID));
+		if (it == m_resultCalls.end())
+			return;
+
+		const ResultCall &resultCall = it->second.resultCall;
+
+		m_resultCalls.erase(it);
+		guard.unlock();
+
+		GatewayRPCResult::Ptr result = new GatewayRPCResult();
+		result->setStatus(GatewayRPCResult::Status::TIMEOUT);
+
+		executeResultCall(gatewayID, callID, resultCall, result);
+	});
+}
+
+void AsyncGatewayRPC::storeResultCall(const GatewayID &gatewayID,
+		const CallID &callID,
+		const ResultCall &resultCall)
+{
+	ResultCallContext context {
+		resultCall,
+		createFinalResultMissingTask(gatewayID, callID)
+	};
+
+	FastMutex::ScopedLock guard(m_mutex);
+
+	auto it = m_resultCalls.emplace(make_pair(gatewayID, callID), context);
+	if (!it.second) {
+		throw IllegalStateException("duplicate result call "
+			+ gatewayID.toString()
+			+ " : "
+			+ callID.toString());
+	}
+
+	m_timer.schedule(
+		context.finalResultMissingTask, Timestamp() + m_finalResultTimeout);
+}
+
+void AsyncGatewayRPC::removeResultCall(const GatewayID &gatewayID,
+		const CallID &callID)
+{
+	FastMutex::ScopedLock guard(m_mutex);
+
+	auto it = m_resultCalls.find(make_pair(gatewayID, callID));
+	if (it == m_resultCalls.end()) {
+		logger().warning("result call "
+			+ gatewayID.toString()
+			+ " : "
+			+ callID.toString()
+			+ " was not found",
+			__FILE__, __LINE__);
+
+		return;
+	}
+
+	it->second.finalResultMissingTask->cancel();
+	m_resultCalls.erase(it);
+}
+
+void AsyncGatewayRPC::executeResultCall(const GatewayID &gatewayID,
+	const AsyncGatewayRPC::CallID &callID,
+	const ResultCall &resultCall,
+	GatewayRPCResult::Ptr result)
+{
+	if (logger().debug()) {
+		logger().debug("executing result call "
+			+ gatewayID.toString()
+			+ " : "
+			+ callID.toString()
+			+ " with result "
+			+ to_string(result->status()),
+			__FILE__, __LINE__);
+	}
+
+	try {
+		resultCall(result);
+	}
+	catch (const Exception &e) {
+		logger().log(e, __FILE__, __LINE__);
+	}
+	catch (const exception &e) {
+		logger().critical(e.what(), __FILE__, __LINE__);
+	}
+	catch (...) {
+		logger().critical("unknown error", __FILE__, __LINE__);
+	}
+}
+
+void AsyncGatewayRPC::sendAndExpectResult(const GatewayID &gatewayID,
+		const AsyncGatewayRPC::CallID &callID,
+		const GatewayRPC::ResultCall &resultCall,
+		const GWRequest::Ptr request)
+{
+	GatewayRPCResult::Ptr result = new GatewayRPCResult;
+
+	storeResultCall(gatewayID, callID, resultCall);
+	m_responseExpectedQueue->registerResponse(gatewayID, callID);
+
+	try {
+		m_gatewayCommunicator->sendMessage(gatewayID, request);
+	}
+	catch (const NotFoundException &e) {
+		logger().log(e, __FILE__, __LINE__);
+
+		removeResultCall(gatewayID, callID);
+		m_responseExpectedQueue->notifyDelivered(gatewayID, callID);
+
+		result->setStatus(GatewayRPCResult::Status::NOT_CONNECTED);
+		executeResultCall(gatewayID, callID, resultCall, result);
+	}
+	catch (const Exception &e) {
+		logger().log(e, __FILE__, __LINE__);
+
+		removeResultCall(gatewayID, callID);
+		m_responseExpectedQueue->notifyDelivered(gatewayID, callID);
+
+		result->setStatus(GatewayRPCResult::Status::FAILED);
+		executeResultCall(gatewayID, callID, resultCall, result);
+	}
+}
+
+void AsyncGatewayRPC::processResult(const GatewayID &gatewayID,
+		const CallID &callID,
+		GatewayRPCResult::Ptr result)
+{
+	ScopedLockWithUnlock<FastMutex> guard(m_mutex);
+
+	auto it = m_resultCalls.find(make_pair(gatewayID, callID));
+	if (it == m_resultCalls.end()) {
+		logger().warning("result call "
+			+ gatewayID.toString()
+			+ " : "
+			+ callID.toString()
+			+ " was not found",
+			__FILE__, __LINE__);
+
+		return;
+	}
+
+	const ResultCall &resultCall = it->second.resultCall;
+
+	switch (result->status()) {
+	case GatewayRPCResult::ACCEPTED:
+		break;
+	case GatewayRPCResult::TIMEOUT:
+	case GatewayRPCResult::SUCCESS:
+	case GatewayRPCResult::FAILED:
+		it->second.finalResultMissingTask->cancel();
+		m_resultCalls.erase(it);
+		break;
+	default:
+		logger().error("invalid result " + to_string(result->status())
+			+ " for call "
+			+ gatewayID.toString()
+			+ " : "
+			+ callID.toString(),
+			__FILE__, __LINE__);
+
+		return;
+	}
+
+	guard.unlock();
+
+	executeResultCall(gatewayID, callID, resultCall, result);
+}
+
+void AsyncGatewayRPC::forwardResponse(const GatewayID &gatewayID,
+		GWResponse::Ptr response)
+{
+	GatewayRPCResult::Ptr result = new GatewayRPCResult;
+
+	switch (response->status()) {
+	case GWResponse::Status::ACCEPTED:
+		result->setStatus(GatewayRPCResult::Status::ACCEPTED);
+		break;
+	case GWResponse::Status::SUCCESS:
+		result->setStatus(GatewayRPCResult::Status::SUCCESS);
+		break;
+	case GWResponse::Status::FAILED:
+		result->setStatus(GatewayRPCResult::Status::FAILED);
+		break;
+	default:
+		throw InvalidArgumentException("invalid type "
+			+ to_string(response->status())
+			+ " of response "
+			+ gatewayID.toString()
+			+ " : "
+			+ response->id().toString(),
+			__FILE__, __LINE__);
+	}
+
+	processResult(gatewayID, response->id(), result);
+}
+
+void AsyncGatewayRPC::forwardTimeout(const GatewayID &gatewayID,
+		const GlobalID &responseID)
+{
+	GatewayRPCResult::Ptr result = new GatewayRPCResult;
+	result->setStatus(GatewayRPCResult::Status::TIMEOUT);
+
+	processResult(gatewayID, responseID, result);
+}
+
+void AsyncGatewayRPC::start()
+{
+	logger().information("gateway rpc is up", __FILE__, __LINE__);
+}
+
+void AsyncGatewayRPC::stop()
+{
+	logger().information("stopping gateway rpc", __FILE__, __LINE__);
+
+	m_timer.cancel(true);
+
+	FastMutex::ScopedLock guard(m_mutex);
+	for (auto &it : m_resultCalls) {
+		try {
+			GatewayRPCResult::Ptr result = new GatewayRPCResult;
+			result->setStatus(GatewayRPCResult::Status::FAILED);
+
+			it.second.resultCall(result);
+		}
+		catch (const Exception &e) {
+			logger().log(e, __FILE__, __LINE__);
+		}
+		catch (const exception &e) {
+			logger().critical(e.what(), __FILE__, __LINE__);
+		}
+		catch (...) {
+			logger().critical("unknown error", __FILE__, __LINE__);
+		}
+	}
+
+	m_resultCalls.clear();
+
+	logger().information("gateway rpc has stopped", __FILE__, __LINE__);
+}
+
+void AsyncGatewayRPC::setFinalResultTimeout(const Timespan &timeout)
+{
+	if (timeout < 0)
+		throw InvalidArgumentException("result timeout must be non-negative");
+
+	m_finalResultTimeout = timeout;
+}
+
+void AsyncGatewayRPC::setGatewayCommunicator(GatewayCommunicator::Ptr communicator)
+{
+	m_gatewayCommunicator = communicator;
+}
+
+void AsyncGatewayRPC::setGWResponseExpectedQueue(GWResponseExpectedQueue::Ptr queue)
+{
+	m_responseExpectedQueue = queue;
+}

server/conf/config.d/factory-all.xml
@@ -6,6 +6,7 @@
 			<add name="loops" ref="gatewayCommunicator" />
 			<add name="loops" ref="webSocketServer" />
 			<add name="loops" ref="responseExpectedQueue" />
+			<add name="loops" ref="asyncGatewayRPC" />
 			<add name="loops" ref="uiServer" />
 			<add name="loops" ref="xmluiServer" />
 		</instance>

server/conf/config.d/factory-dao.xml
@@ -48,6 +48,12 @@
 			<set name="responseTimeout" time="${gateway-rpc.legacy.responseTimeout}" />
 		</instance>
 
+		<instance name="asyncGatewayRPC" class="BeeeOn::AsyncGatewayRPC">
+			<set name="gatewayCommunicator" ref="gatewayCommunicator" />
+			<set name="responseExpectedQueue" ref="responseExpectedQueue" />
+			<set name="finalResultTimeout" time="${gateway-rpc.async.finalResultTimeout}" />
+		</instance>
+
 		<alias name="gatewayRPC" ref="${gateway-rpc.type}GatewayRPC" />
 
 		<!-- Data access layer -->

server/conf/config.d/factory-gws.xml
@@ -28,6 +28,7 @@
 
 		<instance name="responseExpectedQueue" class="BeeeOn::GWResponseExpectedQueue">
 			<set name="gatewayCommunicator" ref="gatewayCommunicator" />
+			<set name="rpcForwarder" ref="asyncGatewayRPC" />
 			<set name="responseTimeout" time="${gws.queue.responseTimeout}" />
 		</instance>
 	</factory>

server/conf/server-startup.ini
@@ -101,6 +101,8 @@ timeout = -1
 [gateway-rpc]
 type = fake
 
+async.finalResultTimeout = 30 s
+
 [logging]
 channels.split.class = SplitterChannel
 channels.split.channels = async_console, async_system

server/conf/testing-startup.ini
@@ -96,6 +96,8 @@ baseDir = ${application.configDir}../db
 [gateway-rpc]
 type = fake
 
+async.finalResultTimeout = 30 s
+
 legacy.responseTimeout = 1000 ms
 connector.host = localhost
 connector.port = 7081

server/src/CMakeLists.txt
@@ -146,6 +146,7 @@ file(GLOB SOURCES
 	${PROJECT_SOURCE_DIR}/rpc/SocketRPCConnector.cpp
 	${PROJECT_SOURCE_DIR}/rpc/GatewayRPC.cpp
 	${PROJECT_SOURCE_DIR}/rpc/GatewayRPCResult.cpp
+	${PROJECT_SOURCE_DIR}/rpc/AsyncGatewayRPC.cpp
 	${PROJECT_SOURCE_DIR}/rpc/FakeGatewayRPC.cpp
 	${PROJECT_SOURCE_DIR}/rpc/LegacyGatewayRPC.cpp
 	${PROJECT_SOURCE_DIR}/notification/NotificationDispatcher.cpp

server/src/rpc/AsyncGatewayRPC.h
@@ -1,0 +1,138 @@
+#ifndef BEEEON_ASYNC_GATEWAY_RPC_H
+#define BEEEON_ASYNC_GATEWAY_RPC_H
+
+#include <Poco/SharedPtr.h>
+#include <Poco/Util/Timer.h>
+
+#include "gwmessage/GWRequest.h"
+#include "gws/GatewayCommunicator.h"
+#include "gws/GWResponseExpectedQueue.h"
+#include "gws/RPCForwarder.h"
+#include "loop/StoppableLoop.h"
+#include "model/GlobalID.h"
+#include "rpc/GatewayRPC.h"
+#include "rpc/GatewayRPCResult.h"
+#include "util/Loggable.h"
+#include "util/LambdaTimerTask.h"
+
+namespace BeeeOn {
+
+/**
+ * @brief AsyncGatewayRPC provides asynchronous GatewayRPC intended to be used
+ * in the BeeeOn Server running as monolit. It allows to send commands to the
+ * gateways and receive partial results asynchronously.
+ *
+ * After sending request to the gateway, the ResultCall is stored and it is
+ * executed with every received partial result. If final result (TIMEOUT,
+ * SUCCESS, FAILED) is not received till timeout, the stored ResultCall is
+ * executed with result TIMEOUT.
+ *
+ * The final result means that request has been completed (with success or error)
+ * or timeout has expired.
+ */
+class AsyncGatewayRPC :
+	public GatewayRPC,
+	public RPCForwarder,
+	public StoppableLoop,
+	protected Loggable {
+public:
+	typedef Poco::SharedPtr<AsyncGatewayRPC> Ptr;
+
+	struct ResultCallContext {
+		ResultCall resultCall;
+		LambdaTimerTask::Ptr finalResultMissingTask;
+	};
+
+	typedef GlobalID CallID;
+	typedef std::map<std::pair<GatewayID, CallID>, ResultCallContext> ResultCallMap;
+
+	void sendListen(const ResultCall &resultCall,
+			const Gateway &gateway,
+			const Poco::Timespan &duration) override;
+
+	void unpairDevice(const ResultCall &resultCall,
+			const Gateway &gateway,
+			const Device &device) override;
+
+	void pingGateway(const ResultCall &resultCall,
+			const Gateway &gateway) override;
+
+	void updateActor(const ResultCall &resultCall,
+			const Gateway &gateway,
+			const Device &device,
+			const ModuleInfo &module,
+			double value,
+			const Poco::Timespan &timeout) override;
+
+	void forwardResponse(const GatewayID &gatewayID,
+			GWResponse::Ptr response) override;
+	void forwardTimeout(const GatewayID &gatewayID,
+			const GlobalID &responseID) override;
+
+	void start() override;
+	void stop() override;
+
+	void setFinalResultTimeout(const Poco::Timespan &timeout);
+	void setGatewayCommunicator(GatewayCommunicator::Ptr communicator);
+	void setGWResponseExpectedQueue(GWResponseExpectedQueue::Ptr queue);
+
+private:
+	/**
+	 * @brief Creates a final result missing task and returns it.
+	 *
+	 * The task removes the ResultCall from the map and executes it with TIMEOUT.
+	 */
+	LambdaTimerTask::Ptr createFinalResultMissingTask(
+			const GatewayID &gatewayID, const CallID &callID);
+
+	/**
+	 * @brief Stores the ResultCall and schedule final result missing task.
+	 */
+	void storeResultCall(const GatewayID &gatewayID,
+			const CallID &callID,
+			const ResultCall &resultCall);
+
+	/**
+	 * @brief Removes the ResultCall and cancels the final result missing task.
+	 */
+	void removeResultCall(const GatewayID &gatewayID, const CallID &callID);
+
+	/**
+	 * @brief Stores the ResultCall and sends the request to the Gateway.
+	 *
+	 * If gateway is not connected or sending fails, executes the ResultCall
+	 * with NOT_CONNECTED resp. FAILED result.
+	 */
+	void sendAndExpectResult(const GatewayID &gatewayID,
+			const CallID &callID,
+			const ResultCall &resultCall,
+			const GWRequest::Ptr request);
+
+	/**
+	 * @brief Finds the ResultCall for the given result and executes it.
+	 */
+	void processResult(const GatewayID &gatewayID,
+			const GlobalID &callID,
+			GatewayRPCResult::Ptr result);
+
+	/**
+	 * @brief Calls the ResultCall with given result.
+	 */
+	void executeResultCall(const GatewayID &gatewayID,
+			const CallID &callID,
+			const ResultCall &resultCall,
+			GatewayRPCResult::Ptr result);
+
+private:
+	Poco::Timespan m_finalResultTimeout;
+	GatewayCommunicator::Ptr m_gatewayCommunicator;
+	GWResponseExpectedQueue::Ptr m_responseExpectedQueue;
+
+	ResultCallMap m_resultCalls;
+	Poco::FastMutex m_mutex;
+	Poco::Util::Timer m_timer;
+};
+
+}
+
+#endif

commit 39a9aaf8a8f1e82a53812777490205a7fb17af91

AsyncGatewayRPC: fix use-after free while calling lambda
The server could segfault on communication with a gateway. The buggy scenario:

* gateway registers itself successfully
* user assigns the gateway
* user starts device discovery
* gateway does not reply
* timeout exceeds and GWS calls the resultCall

Valgrind stack trace:

Thread 5:
Invalid read of size 8
   at 0x20B02A: std::_Function_base::_M_empty() const (std_function.h:279)
   by 0x5E0B83F: std::function<void (Poco::AutoPtr<BeeeOn::GatewayRPCResult>)>
      ::operator bool() const (std_function.h:583)
   by 0x5E0AB9E: std::function<void (Poco::AutoPtr<BeeeOn::GatewayRPCResult>)>
      ::function(std::function<void (Poco::AutoPtr<BeeeOn::GatewayRPCResult>)> const&)
     (std_function.h:675)
   by 0x5E080D0: BeeeOn::AsyncGatewayRPC::processResult(BeeeOn::GatewayID const&,
      BeeeOn::GlobalID const&, Poco::AutoPtr<BeeeOn::GatewayRPCResult>)
      (AsyncGatewayRPC.cpp:256)
   by 0x5E088D4: BeeeOn::AsyncGatewayRPC::forwardTimeout(BeeeOn::GatewayID const&,
      BeeeOn::GlobalID const&) (AsyncGatewayRPC.cpp:293)
   by 0x5DF4D69: BeeeOn::GWResponseExpectedQueue::createResponseMissingTask(
      BeeeOn::GatewayID const&, BeeeOn::GlobalID const&)::{lambda()#1}::operator()()
      const (GWResponseExpectedQueue.cpp:41)
   by 0x5DF5C93: std::_Function_handler<void (), BeeeOn::GWResponseExpectedQueue
      ::createResponseMissingTask(BeeeOn::GatewayID const&, BeeeOn::GlobalID const&)
      ::{lambda()#1}>::_M_invoke(std::_Any_data const&) (std_function.h:316)
   by 0x51CD77F: std::function<void ()>::operator()() const (std_function.h:706)
   by 0x51D9B57: BeeeOn::LambdaTimerTask::run() (LambdaTimerTask.cpp:7)
   by 0x6A714BD: Poco::Util::TaskNotification::execute() (in /usr/lib/libPocoUtil.so.48)
   by 0x6A70195: Poco::Util::Timer::run() (in /usr/lib/libPocoUtil.so.48)
   by 0x62D0B0E: Poco::ThreadImpl::runnableEntry(void*) (in
      /usr/lib/libPocoFoundation.so.48)

The important path:

  forwardTimeout() -> processResult() -> lambda call

It seems that the corresponding ResultCallContext is erased and this step invalidates
the resultCall reference:

238         case GatewayRPCResult::Status::FAILED:
239                 it->second.finalResultMissingTask->cancel();
240                 m_resultCalls.erase(it);
241                 break;

Copying the lambda resultCall by value and not by reference seems to fix this issue.
The lambda was removed also in createFinalResultMissingTask inside the LambdaTimerTask
to avoid a similar issue there.

Fixes: f6beaaf99168 ("AsyncGatewayRPC: new class")

server/src/rpc/AsyncGatewayRPC.cpp
@@ -92,7 +92,7 @@ LambdaTimerTask::Ptr AsyncGatewayRPC::createFinalResultMissingTask(
 		if (it == m_resultCalls.end())
 			return;
 
-		const ResultCall &resultCall = it->second.resultCall;
+		const ResultCall resultCall = it->second.resultCall;
 
 		m_resultCalls.erase(it);
 		guard.unlock();
@@ -228,7 +228,7 @@ void AsyncGatewayRPC::processResult(const GatewayID &gatewayID,
 		return;
 	}
 
-	const ResultCall &resultCall = it->second.resultCall;
+	const ResultCall resultCall = it->second.resultCall;
 
 	switch (result->status()) {
 	case GatewayRPCResult::Status::ACCEPTED: