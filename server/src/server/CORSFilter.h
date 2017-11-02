BeeeOn/server

commit 9b107e96724dd553eb0bbf7c9118f78251813fa5

CORSFilter: new class
The CORSFilter works as a HTTPFilter. All requests are processed. Requests looking
like CORS preflight requests are processed and not exposed to the rest of the system.
Other requests are either checked for CORS rules (if Origin header is present) or
bypassed normally.

Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

server/src/server/CORSFilter.h
@@ -1,0 +1,113 @@
+#ifndef BEEEON_CORS_FILTER_H
+#define BEEEON_CORS_FILTER_H
+
+#include <list>
+#include <set>
+#include <string>
+
+#include <Poco/Timespan.h>
+
+#include "server/HTTPFilter.h"
+#include "util/Loggable.h"
+
+namespace BeeeOn {
+
+/**
+ * @brief General CORS HTTP filter. It can handle the preflight requests
+ * and check all other requests to meet the given requirements. It works
+ * globally for all requests and their potential handlers.
+ */
+class CORSFilter : public HTTPFilter, Loggable {
+public:
+	CORSFilter();
+
+	/**
+	 * Make the Origin HTTP header mandatory for all requests
+	 * or allow to handle requests without that header.
+	 *
+	 * If disabled, the Origin header is required only for the
+	 * CORS preflight requests. Also, the CORS is not applied
+	 * for requests without the Origin header.
+	 */
+	void setRequireOrigin(bool require);
+
+	/**
+	 * Set origins that are allowed during cross-domain requests.
+	 * If it is empty then the injected CORS response header
+	 * Control-Access-Allow-Origin will be '*'.
+	 */
+	void setAllowedOrigins(const std::list<std::string> &origins);
+
+	/**
+	 * Set methods that are allowed during cross-domain requests.
+	 * If it is empty, all methods are allowed and the CORS response
+	 * header Control-Access-Allow-Methods is not injected.
+	 */
+	void setAllowedMethods(const std::list<std::string> &methods);
+
+	/**
+	 * Set headers that are allowed during cross-domain requests.
+	 */
+	void setAllowedHeaders(const std::list<std::string> &headers);
+
+	/**
+	 * Override simple headers that are allowed during cross-domain requests.
+	 */
+	void setSimpleHeaders(const std::list<std::string> &headers);
+
+	/**
+	 * Set whether credentials is allowed via cross-domain requests.
+	 * If true, the header Access-Control-Allow-Credentials: true is
+	 * injected.
+	 */
+	void setAllowCredentials(bool credentials);
+
+	/**
+	 * Set headers that are allowed to be exposed to the browser client code
+	 * during cross-domain requests.
+	 */
+	void setExposeHeaders(const std::list<std::string> &headers);
+
+	/**
+	 * Set the time period for how long the preflight requests should be cached
+	 * by the browser.
+	 */
+	void setPreflightMaxAge(const Poco::Timespan &maxAge);
+
+	/**
+	 * Handle the preflight OPTIONS requests and check normal
+	 * requests according to the set policy.
+	 * For non-preflight requests, generate CORS response headers.
+	 */
+	void apply(Poco::Net::HTTPServerRequest &req, Poco::Net::HTTPServerResponse &res) override;
+
+protected:
+	/**
+	 * Handle the preflight OPTIONS request and generates HTTP response.
+	 */
+	void handlePreflight(const Poco::Net::HTTPServerRequest &req, Poco::Net::HTTPServerResponse &res) const;
+
+	/**
+	 * Filter all requests accoroding to CORS depending on the m_requireOrigin setting.
+	 * If a request does not meet the CORS requirements, an exception is thrown to
+	 * disallow further request processing.
+	 */
+	void filterRequest(const Poco::Net::HTTPServerRequest &req, Poco::Net::HTTPServerResponse &res) const;
+
+	std::string formatSet(const std::set<std::string> &s,
+			const std::string &separator) const;
+
+private:
+	bool m_requireOrigin;
+	std::set<std::string> m_allowedOrigins;
+	std::set<std::string> m_allowedMethods;
+	std::set<std::string> m_allowedHeaders;
+	std::set<std::string> m_simpleHeaders;
+	bool m_allowCredentials;
+	std::set<std::string> m_exposeHeaders;
+	Poco::Timespan m_preflightMaxAge;
+};
+
+}
+
+#endif

server/conf/config.d/factory-restui.xml
@@ -25,6 +25,16 @@
 		</instance>
 
 		<instance name="uiFilterChain" class="BeeeOn::HTTPFilterChain">
+			<add name="filters" ref="corsFilter" />
+		</instance>
+
+		<instance name="corsFilter" class="BeeeOn::CORSFilter">
+			<set name="requireOrigin" number="0" />
+			<set name="allowedOrigins" list="${restui.cors.origins}" />
+			<set name="allowedMethods" list="${restui.cors.methods}" />
+			<set name="allowedHeaders" list="${restui.cors.headers}" />
+			<set name="allowCredentials" number="1" />
+			<set name="preflightMaxAge" time="${restui.cors.maxAge}" />
 		</instance>
 
 		<instance name="uiRouter" class="BeeeOn::RestRouter">

server/conf/config.d/logging.ini
@@ -243,6 +243,9 @@ loggers.SensorXmlHandler.level = ${logging.default.level}
 
 ; REST-UI API
 
+loggers.CORSFilter.name = BeeeOn::CORSFilter
+loggers.CORSFilter.level = ${logging.default.level}
+
 loggers.RestRouter.name = BeeeOn::RestRouter
 loggers.RestRouter.level = ${logging.default.level}
 

server/conf/server-startup.ini
@@ -13,6 +13,11 @@ minThreads = 1
 maxThreads = 32
 threadIdleTime = 4 s
 
+cors.origins =
+cors.methods = GET, POST, PUT, DELETE
+cors.headers = Content-Type, Authorization, X-Requested-With
+cors.maxAge = 2 h
+
 [xmlui]
 port = 8011
 ssl.enable = true

server/conf/testing-startup.ini
@@ -11,6 +11,11 @@ minThreads = 1
 maxThreads = 4
 threadIdleTime = 2 s
 
+cors.origins = null
+cors.methods = GET, POST, PUT, DELETE
+cors.headers = Content-Type, Authorization, X-Requested-With
+cors.maxAge = 5 s
+
 [xmlui]
 port = 8001
 ssl.enable = false

server/src/CMakeLists.txt
@@ -29,6 +29,7 @@ file(GLOB SOURCES
 	${PROJECT_SOURCE_DIR}/provider/TypeInfoProvider.cpp
 	${PROJECT_SOURCE_DIR}/server/AuthRestHandler.cpp
 	${PROJECT_SOURCE_DIR}/server/ExpirableSession.cpp
+	${PROJECT_SOURCE_DIR}/server/CORSFilter.cpp
 	${PROJECT_SOURCE_DIR}/server/HTTPFilter.cpp
 	${PROJECT_SOURCE_DIR}/server/HTTPFilterChain.cpp
 	${PROJECT_SOURCE_DIR}/server/Session.cpp

server/src/server/CORSFilter.cpp
@@ -1,0 +1,206 @@
+#include <Poco/Exception.h>
+#include <Poco/Logger.h>
+#include <Poco/String.h>
+#include <Poco/StringTokenizer.h>
+
+#include <Poco/Net/HTTPServerRequest.h>
+#include <Poco/Net/HTTPServerResponse.h>
+#include <Poco/Net/NetException.h>
+
+#include "di/Injectable.h"
+#include "server/CORSFilter.h"
+
+BEEEON_OBJECT_BEGIN(BeeeOn, CORSFilter)
+BEEEON_OBJECT_CASTABLE(HTTPFilter)
+BEEEON_OBJECT_NUMBER("requireOrigin", &CORSFilter::setRequireOrigin)
+BEEEON_OBJECT_LIST("allowedOrigins", &CORSFilter::setAllowedOrigins)
+BEEEON_OBJECT_LIST("allowedMethods", &CORSFilter::setAllowedMethods)
+BEEEON_OBJECT_LIST("allowedHeaders", &CORSFilter::setAllowedHeaders)
+BEEEON_OBJECT_LIST("simpleHeaders", &CORSFilter::setSimpleHeaders)
+BEEEON_OBJECT_LIST("exposeHeaders", &CORSFilter::setExposeHeaders)
+BEEEON_OBJECT_TIME("preflightMaxAge", &CORSFilter::setPreflightMaxAge)
+BEEEON_OBJECT_NUMBER("allowCredentials", &CORSFilter::setAllowCredentials)
+BEEEON_OBJECT_END(BeeeOn, CORSFilter)
+
+using namespace std;
+using namespace Poco;
+using namespace Poco::Net;
+using namespace BeeeOn;
+
+CORSFilter::CORSFilter():
+	m_requireOrigin(true),
+	m_allowCredentials(false),
+	m_preflightMaxAge(10 * Timespan::SECONDS)
+{
+	m_simpleHeaders.emplace("Accept");
+	m_simpleHeaders.emplace("Accept-Encoding");
+	m_simpleHeaders.emplace("Accept-Language");
+	m_simpleHeaders.emplace("Connection");
+	m_simpleHeaders.emplace("Content-Language");
+	m_simpleHeaders.emplace("Content-Length");
+	m_simpleHeaders.emplace("DPR");
+	m_simpleHeaders.emplace("Downlink");
+	m_simpleHeaders.emplace("Host");
+	m_simpleHeaders.emplace("Last-Event-ID");
+	m_simpleHeaders.emplace("Origin");
+	m_simpleHeaders.emplace("Save-Data");
+	m_simpleHeaders.emplace("User-Agent");
+	m_simpleHeaders.emplace("Viewport-Width");
+	m_simpleHeaders.emplace("Width");
+}
+
+void CORSFilter::setRequireOrigin(bool require)
+{
+	m_requireOrigin = require;
+}
+
+void CORSFilter::setAllowedOrigins(const list<string> &origins)
+{
+	m_allowedOrigins.clear();
+
+	for (const auto &s : origins)
+		m_allowedOrigins.emplace(s);
+}
+
+void CORSFilter::setAllowedMethods(const list<string> &methods)
+{
+	m_allowedMethods.clear();
+
+	for (const auto &s : methods)
+		m_allowedMethods.emplace(s);
+}
+
+void CORSFilter::setAllowedHeaders(const list<string> &headers)
+{
+	m_allowedHeaders.clear();
+
+	for (const auto &s : headers)
+		m_allowedHeaders.emplace(s);
+}
+
+void CORSFilter::setSimpleHeaders(const list<string> &headers)
+{
+	m_simpleHeaders.clear();
+
+	for (const auto &s : headers)
+		m_simpleHeaders.emplace(s);
+}
+
+void CORSFilter::setAllowCredentials(bool allow)
+{
+	m_allowCredentials = allow;
+}
+
+void CORSFilter::setExposeHeaders(const list<string> &headers)
+{
+	m_exposeHeaders.clear();
+
+	for (const auto &s : headers)
+		m_exposeHeaders.emplace(s);
+}
+
+void CORSFilter::setPreflightMaxAge(const Timespan &maxAge)
+{
+	m_preflightMaxAge = maxAge;
+}
+
+void CORSFilter::apply(HTTPServerRequest &req, HTTPServerResponse &res)
+{
+	if (req.getMethod() == "OPTIONS")
+		handlePreflight(req, res);
+	else if (m_requireOrigin || req.has("Origin"))
+		filterRequest(req, res);
+}
+
+void CORSFilter::handlePreflight(const HTTPServerRequest &req, HTTPServerResponse &res) const
+{
+	if (logger().debug()) {
+		logger().debug("handling preflight request "
+			+ req.getMethod() + " " + req.getURI(),
+			__FILE__, __LINE__);
+	}
+
+	if (!req.has("Origin"))
+		throw InvalidArgumentException("missing Origin header");
+
+	const string &origin = req.get("Origin");
+
+	if (m_allowedOrigins.empty())
+		res.set("Access-Control-Allow-Origin", origin);
+	else if (m_allowedOrigins.find(origin) != m_allowedOrigins.end())
+		res.set("Access-Control-Allow-Origin", origin);
+	else
+		throw InvalidArgumentException("disallowed Origin: " + origin);
+
+	if (!m_allowedMethods.empty())
+		res.set("Access-Control-Allow-Methods", formatSet(m_allowedMethods, ", "));
+
+	if (!m_allowedHeaders.empty())
+		res.set("Access-Control-Allow-Headers", formatSet(m_allowedHeaders, ", "));
+
+	if (m_allowCredentials)
+		res.set("Access-Control-Allow-Credentials", "true");
+
+	if (!m_exposeHeaders.empty())
+		res.set("Access-Control-Expose-Headers", formatSet(m_exposeHeaders, ", "));
+
+	if (m_preflightMaxAge >= 0)
+		res.set("Access-Control-Max-Age", to_string(m_preflightMaxAge.totalSeconds()));
+
+	res.send();
+}
+
+void CORSFilter::filterRequest(const HTTPServerRequest &req, HTTPServerResponse &res) const
+{
+	if (logger().debug()) {
+		logger().debug("filtering request "
+			+ req.getMethod() + " " + req.getURI()
+			+ " for CORS",
+			__FILE__, __LINE__);
+	}
+
+	if (!req.has("Origin"))
+		throw InvalidArgumentException("missing required Origin header");
+
+	const string &origin = req.get("Origin");
+
+	if (!m_allowedOrigins.empty()) { // otherwise, allow any ('*')
+		if (m_allowedOrigins.find(origin) == m_allowedOrigins.end())
+			throw InvalidArgumentException("disallowed Origin: " + origin);
+	}
+
+	res.set("Access-Control-Allow-Origin", origin);
+
+	if (!m_allowedMethods.empty()) { // otherwise, allow any
+		const string &method = req.getMethod();
+
+		if (m_allowedMethods.find(method) == m_allowedMethods.end())
+			throw InvalidArgumentException("disallowed method: " + method);
+	}
+
+	for (const auto &pair : req) {
+		if (m_simpleHeaders.find(pair.first) != m_simpleHeaders.end())
+			continue;
+
+		if (m_allowedHeaders.find(pair.first) == m_allowedHeaders.end())
+			throw InvalidArgumentException("disallowed header: " + pair.first);
+	}
+
+	if (m_allowCredentials)
+		res.set("Access-Control-Allow-Credentials", "true");
+}
+
+string CORSFilter::formatSet(const set<string> &s,
+			const string &separator) const
+{
+	string result;
+
+	for (auto it = s.begin(); it != s.end(); ++it) {
+		if (it != s.begin())
+			result += separator;
+		
+		result += *it;
+	}
+
+	return result;
+}

commit 1e0a6e363e4a3f2b01c1f9b2d36b2ba6302ff032

CORSFilter: fix doc of setAllowedOrigins
The real behaviour is different from the one specified in the documentation.

Fixes: 9b107e96724d ("CORSFilter: new class")
Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

server/src/server/CORSFilter.h
@@ -33,8 +33,7 @@ public:
 
 	/**
 	 * Set origins that are allowed during cross-domain requests.
-	 * If it is empty then the injected CORS response header
-	 * Control-Access-Allow-Origin will be '*'.
+	 * If it is empty then all origins would be allowed.
 	 */
 	void setAllowedOrigins(const std::list<std::string> &origins);
 