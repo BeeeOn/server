ab46e19823f09c38ced9b4fee59b08d8aa13845e
Jan Viktorin <iviktorin@fit.vutbr.cz>
2017-11-08 11:09:30 +0100

    TimeZoneProvider: new interface class
    Provide an API to access the time zone information.
    
    Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

---
+++ server/src/l10n/TimeZoneProvider.h
@@ -0,0 +1,42 @@
+#ifndef BEEEON_TIME_ZONE_PROVIDER_H
+#define BEEEON_TIME_ZONE_PROVIDER_H
+
+#include <string>
+#include <vector>
+
+#include <Poco/Nullable.h>
+#include <Poco/SharedPtr.h>
+
+#include "l10n/Locale.h"
+#include "l10n/TimeZone.h"
+
+namespace BeeeOn {
+
+/**
+ * @brief Provider that gives access methods to query available time zones.
+ *
+ * Every time zone library should have an implementation ofthe TimeZoneProvider. This
+ * allows to access the time zones known for that library. There is usually only one
+ * instance of a TimeZoneProvider in a running system.
+ */
+class TimeZoneProvider {
+public:
+	typedef Poco::SharedPtr<TimeZoneProvider> Ptr;
+
+	virtual ~TimeZoneProvider();
+
+	/**
+	 * @returns time zone information by a specific ID.
+	 * If no such time zone information is found, it returns NULL.
+	 */
+	virtual Poco::Nullable<TimeZone> findById(const std::string &id) = 0;
+
+	/**
+	 * @returns all supported time zone information in the system.
+	 */
+	virtual void listTimeZones(struct std::vector<TimeZone> &zones) = 0;
+};
+
+}
+
+#endif
