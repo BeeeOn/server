9b1b0c0d07be5ed43d4354b15f993f2e68b36681
Jan Viktorin <iviktorin@fit.vutbr.cz>
2017-11-10 22:25:17 +0100

    t: restui: test listing of available auth providers
    The providers are uniquely identified by their IDs.
    
    Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

--- server/t/restui/t0002-auth.py
+++ server/t/restui/t0002-auth.py
@@ -10,7 +10,7 @@ import os
 
 import google
 import facebook
-from rest import POST, DELETE
+from rest import GET, POST, DELETE
 
 def login(body):
 	req = POST(config.ui_host, config.ui_port, "/auth")
@@ -30,6 +30,57 @@ def logout(session):
 	return req()
 
 class TestAuth(unittest.TestCase):
+	def find_provider(self, providers, id):
+		for p in providers:
+			if p["id"] == id:
+				return p
+
+		return None
+
+	"""
+	List available providers. The result depends on the server configuration
+	and thus the test must adapt to it.
+	"""
+	def test1_list_providers(self):
+		req = GET(config.ui_host, config.ui_port, "/auth")
+		response, content = req()
+
+		self.assertEqual(200, response.status)
+
+		data = json.loads(content)
+		self.assertEqual("success", data["status"])
+
+		providers = data["data"]
+		self.assertTrue(len(providers) > 0, "no provider is available")
+
+		no_dups = {}
+		for p in providers:
+			id = p["id"]
+			self.assertFalse(id in no_dups, "duplicate provider %s" % id)
+			no_dups[id] = 1
+
+		if not "PERMIT_UNAVAILABLE" in os.environ:
+			provider = self.find_provider(providers, "permit")
+			self.assertIsNotNone(provider)
+			self.assertEqual("permit", provider["name"])
+			self.assertEqual("Permit Login", provider["display_name"])
+
+		if not google.skip_login():
+			provider = self.find_provider(providers, "google")
+			self.assertIsNotNone(provider)
+			self.assertEqual("google", provider["name"])
+			self.assertEqual("Google Login", provider["display_name"])
+			self.assertTrue("oauth2" in provider)
+			self.assertTrue("client_id" in provider["oauth2"])
+
+		if not facebook.skip_login():
+			provider = self.find_provider(providers, "facebook")
+			self.assertIsNotNone(provider)
+			self.assertEqual("facebook", provider["name"])
+			self.assertEqual("Facebook Login", provider["display_name"])
+			self.assertTrue("oauth2" in provider)
+			self.assertTrue("client_id" in provider["oauth2"])
+
 	"""
 	Test login & logout with the permit provider.
 	Disable this test by setting PERMIT_UNAVAILABLE
@@ -37,7 +88,7 @@ class TestAuth(unittest.TestCase):
 	"""
 	@unittest.skipIf("PERMIT_UNAVAILABLE" in os.environ,
 			"permit provider is disabled")
-	def test1_login_logout_permit(self):
+	def test2_login_logout_permit(self):
 		response, session = login(config.PERMIT_LOGIN)
 		self.assertEqual(200, response.status)
 
@@ -55,7 +106,7 @@ class TestAuth(unittest.TestCase):
 	   automatic login capabilities (via Selenium).
 	"""
 	@unittest.skipIf(google.skip_login(), "Missing configuration to perform Google login")
-	def test2_login_logout_google(self):
+	def test3_login_logout_google(self):
 		GOOGLE_LOGIN = json.dumps({
 			"provider": "google",
 			"code": google.login_auth_code()
@@ -74,7 +125,7 @@ class TestAuth(unittest.TestCase):
 	the test is skipped.
 	"""
 	@unittest.skipIf(facebook.skip_login(), "Missing configuration to perform Facebook login")
-	def test3_login_logout_facebook(self):
+	def test4_login_logout_facebook(self):
 		FACEBOOK_LOGIN = json.dumps({
 			"provider": "facebook",
 			"code": facebook.login_auth_code()
@@ -92,7 +143,7 @@ class TestAuth(unittest.TestCase):
 	"""
 	@unittest.skipIf("PERMIT_UNAVAILABLE" in os.environ,
 			"permit provider is disabled")
-	def test4_reach_max_session_count(self):
+	def test5_reach_max_session_count(self):
 		session = []
 
 		for i in range(0, config.ui_session_per_user):
