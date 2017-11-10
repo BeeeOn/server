1b27722af9b4fe19acfaecc6157f468f4a95a4e4
Jan Viktorin <iviktorin@fit.vutbr.cz>
2017-11-10 11:19:32 +0100

    install: do not perform RPM pre/post on upgrade
    The RPM installation procedure did not consider the upgrade case.
    When upgrading, it is not needed to remove beeeon users and related
    directories.
    
    Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

--- server/dist/rpm/post_uninstall.sh
+++ server/dist/rpm/post_uninstall.sh
@@ -1,11 +1,17 @@
 #! /bin/sh
 
-rmdir --ignore-fail-on-non-empty -v /var/log/beeeon
+case "$1" in
+0) # uninstall
+	rmdir --ignore-fail-on-non-empty -v /var/log/beeeon
 
-/usr/sbin/userdel beeeon_admin
-/usr/sbin/userdel beeeon_user
-/usr/sbin/groupdel beeeon
+	/usr/sbin/userdel beeeon_admin
+	/usr/sbin/userdel beeeon_user
+	/usr/sbin/groupdel beeeon
 
-rmdir --ignore-fail-on-non-empty -v /var/lib/beeeon/server-admin
-rmdir --ignore-fail-on-non-empty -v /var/lib/beeeon/server-user
-rmdir --ignore-fail-on-non-empty -v /var/lib/beeeon
+	rmdir --ignore-fail-on-non-empty -v /var/lib/beeeon/server-admin
+	rmdir --ignore-fail-on-non-empty -v /var/lib/beeeon/server-user
+	rmdir --ignore-fail-on-non-empty -v /var/lib/beeeon
+;;
+1) # upgrade
+;;
+esac
--- server/dist/rpm/pre_install.sh
+++ server/dist/rpm/pre_install.sh
@@ -1,10 +1,16 @@
 #! /bin/sh
 
-/usr/sbin/groupadd -r beeeon
-/usr/bin/mkdir -p /var/lib/beeeon
-/usr/sbin/useradd -r -d /var/lib/beeeon/server-user -m -s /sbin/nologin -g beeeon beeeon_user
-/usr/sbin/useradd -r -d /var/lib/beeeon/server-admin -m -s /sbin/nologin -g beeeon beeeon_admin
+case "$1" in
+1) # install
+	/usr/sbin/groupadd -r beeeon
+	/usr/bin/mkdir -p /var/lib/beeeon
+	/usr/sbin/useradd -r -d /var/lib/beeeon/server-user -m -s /sbin/nologin -g beeeon beeeon_user
+	/usr/sbin/useradd -r -d /var/lib/beeeon/server-admin -m -s /sbin/nologin -g beeeon beeeon_admin
 
-/usr/bin/mkdir -p /var/log/beeeon
-/usr/bin/chown root:beeeon /var/log/beeeon
-/usr/bin/chmod 0770 /var/log/beeeon
+	/usr/bin/mkdir -p /var/log/beeeon
+	/usr/bin/chown root:beeeon /var/log/beeeon
+	/usr/bin/chmod 0770 /var/log/beeeon
+;;
+2) # upgrade
+;;
+esac
