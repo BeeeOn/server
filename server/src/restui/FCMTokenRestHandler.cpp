BeeeOn/server

commit 383699f36810e6ec87dbe46ca22ad014dac10c2d

FCMTokenRestHandler: list available notification services
Introduce new REST-API endpoint:

* notifications.list_services => GET /notifications

Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

server/src/restui/FCMTokenRestHandler.cpp
@@ -15,6 +15,7 @@
 BEEEON_OBJECT_BEGIN(BeeeOn, RestUI, FCMTokenRestHandler)
 BEEEON_OBJECT_CASTABLE(RestHandler)
 BEEEON_OBJECT_REF("fcmTokenService", &FCMTokenRestHandler::setFCMTokenService)
+BEEEON_OBJECT_TEXT("senderId", &FCMTokenRestHandler::setSenderID)
 BEEEON_OBJECT_END(BeeeOn, RestUI, FCMTokenRestHandler)
 
 using namespace std;
@@ -26,6 +27,7 @@ using namespace BeeeOn::RestUI;
 FCMTokenRestHandler::FCMTokenRestHandler():
 	JSONRestHandler("notifications")
 {
+	registerAction<FCMTokenRestHandler>("list_services", &FCMTokenRestHandler::listServices);
 	registerAction<FCMTokenRestHandler>("register", &FCMTokenRestHandler::registerToken);
 	registerAction<FCMTokenRestHandler>("unregister", &FCMTokenRestHandler::unregisterToken, {"token"});
 }
@@ -33,6 +35,35 @@ FCMTokenRestHandler::FCMTokenRestHandler():
 void FCMTokenRestHandler::setFCMTokenService(FCMTokenService::Ptr service)
 {
 	m_service = service;
+}
+
+void FCMTokenRestHandler::setSenderID(const string &senderID)
+{
+	m_senderID = senderID;
+}
+
+void FCMTokenRestHandler::listServices(RestFlow &flow)
+{
+	PrintHandler result(flow.response().stream());
+	beginSuccess(result, 200);
+
+	result.startArray();
+
+	if (!m_senderID.empty()) {
+		result.startObject();
+
+		result.key("name");
+		result.value("fcm");
+
+		result.key("id");
+		result.value(m_senderID);
+
+		result.endObject();
+	}
+
+	result.endArray();
+
+	endSuccess(result);
 }
 
 FCMToken FCMTokenRestHandler::extractToken(RestFlow &flow) const

server/conf/config.d/factory-restui.xml
@@ -105,6 +105,7 @@
 
 		<instance name="uiFCMTokenHandler" class="BeeeOn::RestUI::FCMTokenRestHandler">
 			<set name="fcmTokenService" ref="fcmTokenService" />
+			<set name="senderId" text="${fcm.senderId}" />
 		</instance>
 	</factory>
 </system>

server/conf/restui-api.ini
@@ -133,6 +133,9 @@ api.call.controls.current.caching = 60
 api.call.controls.request_change.uri = /gateways/$gateway_id/devices/$device_id/controls/$control_id/current
 api.call.controls.request_change.method = POST
 
+api.call.notifications.list_services.uri = /notifications
+api.call.notifications.list_services.caching = 60
+
 api.call.notifications.register.uri = /notifications/fcm
 api.call.notifications.register.method = POST
 

server/conf/server-startup.ini
@@ -94,6 +94,7 @@ ssl.authority = /usr/share/beeeon/cert/mozilla-cacert-2016-11-02.pem
 
 [fcm]
 enable = no
+;senderId =
 
 [database]
 backend = odbc

server/conf/testing-startup.ini
@@ -83,6 +83,7 @@ ssl.authority = ${application.configDir}../cert/mozilla-cacert-2016-11-02.pem
 
 [fcm]
 enable = no
+;senderId =
 
 [database]
 backend = odbc

server/src/restui/FCMTokenRestHandler.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <string>
+
 #include "rest/JSONRestHandler.h"
 #include "service/FCMTokenService.h"
 
@@ -11,7 +13,9 @@ public:
 	FCMTokenRestHandler();
 
 	void setFCMTokenService(FCMTokenService::Ptr service);
+	void setSenderID(const std::string &senderID);
 
+	void listServices(RestFlow &flow);
 	void registerToken(RestFlow &flow);
 	void unregisterToken(RestFlow &flow);
 
@@ -20,6 +24,7 @@ protected:
 
 private:
 	FCMTokenService::Ptr m_service;
+	std::string m_senderID;
 };
 
 }

commit d55eb8c69bc6973f441e6d523e2c39e039e9103f

FCMTokenRestHandler: fix reporting fcm service name
The PrintHandler::value() method does not overload input argument
const char *. Values of const char * are converted to int and not
to std::string.

Fixes: 383699f36810 ("FCMTokenRestHandler: list available notification services")
Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

server/src/restui/FCMTokenRestHandler.cpp
@@ -53,7 +53,7 @@ void FCMTokenRestHandler::listServices(RestFlow &flow)
 		result.startObject();
 
 		result.key("name");
-		result.value("fcm");
+		result.value(string("fcm"));
 
 		result.key("id");
 		result.value(m_senderID);