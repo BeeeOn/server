f8a084eb8eaf8a5bb4705518f1ee41fa49c94b1e
Jan Viktorin <iviktorin@fit.vutbr.cz>
2017-11-13 13:25:04 +0100

    t: xmlui: synchronize fcmtoken tests with restui
    The FCM_AVAILABLE environment variable is replaced by FCM_SENDER_ID
    as it is done in restui tests.
    
    Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

--- server/t/xmlui/t1008-fcmtoken-register-unregister.py
+++ server/t/xmlui/t1008-fcmtoken-register-unregister.py
@@ -32,7 +32,7 @@ class TestFCMTokenRegisterUnregister(unittest.TestCase):
 	"""
 	Test register and unregister predefined FCM token.
 	"""
-	@unittest.skipIf("FCM_AVAILABLE" not in os.environ,
+	@unittest.skipIf("FCM_SENDER_ID" not in os.environ,
 			"fcm support is disabled")
 	def test_register_unregister(self):
 		c = Connector(config.xmlui_host, config.xmlui_port, config.xmlui_ssl)
