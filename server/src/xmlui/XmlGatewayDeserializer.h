54825efd4a937a5c8a246b552e798e495ac01191
Jan Viktorin <iviktorin@fit.vutbr.cz>
2017-11-14 14:17:32 +0100

    xmlui: require timezone to be specified for gateway
    The XmlGatewayDeserializer now requires a timezone on full deserialization.
    
    We have to fix tests accordingly - the GatewayRegister class update is
    sufficient. The test1_register_unregister tests whether setting of a
    time zone works.
    
    Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

--- server/src/xmlui/XmlGatewayDeserializer.h
+++ server/src/xmlui/XmlGatewayDeserializer.h
@@ -3,6 +3,8 @@
 
 #include <Poco/DOM/Element.h>
 
+#include "l10n/TimeZone.h"
+#include "l10n/TimeZoneProvider.h"
 #include "service/Deserializer.h"
 #include "model/Gateway.h"
 
@@ -12,11 +14,18 @@ class XmlGatewayDeserializer : public Deserializer<Gateway> {
 public:
 	XmlGatewayDeserializer(const Poco::XML::Element &node);
 
+	void setTimeZoneProvider(TimeZoneProvider::Ptr provider);
+	TimeZoneProvider::Ptr timeZoneProvider() const;
+
 	void partial(Gateway &gateway) const override;
 	void full(Gateway &gateway) const override;
 
+protected:
+	TimeZone extractTimeZone(const Poco::XML::Element &node) const;
+
 private:
 	const Poco::XML::Element &m_node;
+	TimeZoneProvider::Ptr m_timeZoneProvider;
 };
 
 }
