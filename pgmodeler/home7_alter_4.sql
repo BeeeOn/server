f45c3a0b08857f3b2d544c9447f483b9f5036f66
Jan Viktorin <iviktorin@fit.vutbr.cz>
2017-11-16 12:48:12 +0100

    pgmodeler: drop obsolete code
    Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

--- pgmodeler/home7_alter_4.sql
+++
@@ -1,7 +0,0 @@
-DROP INDEX index_mail;
-
-CREATE UNIQUE INDEX index_mail ON public.users
-	USING btree
-	(
-	  mail ASC NULLS FIRST
-	);
--- pgmodeler/home7_create_test_db.sql
+++
@@ -1,8 +0,0 @@
-\i home7.sql
-\i home7_alter_1.sql
-\i home7_alter_2.sql
-\i home7_alter_3.sql
-\i home7_alter_4.sql
-\i db_triggers_functions.sql
-\i home7_functions.sql
-\i db_triggers_functions.sql
