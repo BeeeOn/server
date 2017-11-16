e67a1500575b2b3c210e0ec1908381b8e03d86a0
Jan Viktorin <iviktorin@fit.vutbr.cz>
2017-11-16 12:45:25 +0100

    webapp: drop obsolete code
    Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

--- webapp/client/app/notifications/notifications.controller.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-angular.module('beeeOnWebApp')
-  .controller('NotificationsCtrl', ['$scope','$translate','navbar', function ($scope,$translate,navbar) {
-    navbar.setLocation('GLOBAL.NOTIFICATIONS');
-  }]);
