BeeeOn/server

commit 7e407c0a578d4423ed947170f66c63b910da8e8a

FCMMessage: new class
FCMMessage class represents a notification recognizable by
Firebase Cloud Messaging API server. It supports the following
fields as specified by the documentation [1]:

* registration_ids - list of recipients (tokens or IDs)
* priority - "normal" or "high"
* content_available - to wake up inactive clients on iOS
* time_to_live - validity of notification while the recipient is offline
* notification - standardized payload consisting of title and body

[1] https://developers.google.com/cloud-messaging/http-server-ref

Signed-off-by: Peter Kubov <xkubov06@stud.fit.vutbr.cz>
Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

src/fcm/FCMMessage.cpp
@@ -1,0 +1,98 @@
+#include <Poco/Exception.h>
+#include <Poco/Timestamp.h>
+
+#include "fcm/FCMMessage.h"
+
+using namespace BeeeOn;
+using namespace Poco;
+using namespace std;
+
+const Timespan FCMMessage::MAX_SUPPORTED_TTL = 28 * Timespan::DAYS;
+
+FCMMessage::FCMMessage():
+	m_priority(PRIORITY_NORM),
+	m_contentAvailable(true)
+{
+}
+
+FCMMessage::FCMMessage(const FCMMessage &copy)
+{
+}
+
+void FCMMessage::addRecipient(const FCMTokenID &to)
+{
+	m_recipients.insert(to);
+}
+
+void FCMMessage::setRecipients(const set<FCMTokenID> &recipients)
+{
+	m_recipients = recipients;
+}
+
+const set<FCMTokenID> &FCMMessage::recipients() const
+{
+	return m_recipients;
+}
+
+void FCMMessage::setTitle(const string &title)
+{
+	m_title = title;
+}
+
+const string &FCMMessage::title() const
+{
+	return m_title;
+}
+
+void FCMMessage::setBody(const string &body)
+{
+	m_body = body;
+}
+
+const string &FCMMessage::body() const
+{
+	return m_body;
+}
+
+void FCMMessage::setTimeToLive(const Timespan &timeToLive)
+{
+	if (timeToLive < 0)
+		throw InvalidArgumentException("TTL must not be negative for FCM message");
+
+	if (timeToLive < MAX_SUPPORTED_TTL)
+		m_timeToLive = timeToLive;
+	else
+		m_timeToLive = MAX_SUPPORTED_TTL;
+}
+
+const Timespan &FCMMessage::timeToLive() const
+{
+	return m_timeToLive;
+}
+
+void FCMMessage::setPriority(const Priority &priority)
+{
+	switch (priority) {
+	case PRIORITY_NORM:
+	case PRIORITY_HIGH:
+		m_priority = priority;
+		break;
+	default:
+		throw InvalidArgumentException("invalid priority given");
+	}
+}
+
+FCMMessage::Priority FCMMessage::priority() const
+{
+	return m_priority;
+}
+
+void FCMMessage::setContentAvailable(bool contentAvailable)
+{
+	m_contentAvailable = contentAvailable;
+}
+
+bool FCMMessage::contentAvailable() const
+{
+	return m_contentAvailable;
+}

src/fcm/FCMMessage.h
@@ -1,0 +1,116 @@
+#pragma once
+
+#include <set>
+#include <string>
+
+#include <Poco/Timespan.h>
+
+#include "model/FCMToken.h"
+#include "util/Loggable.h"
+
+namespace BeeeOn {
+
+/**
+ * @brief Provides representation of a notification suitable for sending to
+ * the Firebase Cloud Messaging service.
+ *
+ * @see https://developers.google.com/cloud-messaging/http-server-ref
+ * @see https://firebase.google.com/docs/cloud-messaging/concept-options
+ */
+class FCMMessage {
+public:
+	enum Priority {
+		PRIORITY_NORM,
+		PRIORITY_HIGH,
+	};
+
+	/**
+	 * Maximal time to live in seconds supported by FCM API (28 days).
+	 */
+	static const Poco::Timespan MAX_SUPPORTED_TTL;
+
+	FCMMessage();
+	FCMMessage(const FCMMessage &copy);
+
+	/**
+	 * Adds one recipient into JSON array of recipients
+	 */
+	void addRecipient(const FCMTokenID &to);
+
+	/**
+	 * Sets recipients to array of defined recipients.
+	 */
+	void setRecipients(const std::set<FCMTokenID> &recipients);
+
+	/**
+	 * Returns recipients of the notification in JSON::Array
+	 */
+	const std::set<FCMTokenID> &recipients() const;
+
+	/**
+	 * Title of the notification's text part.
+	 */
+	void setTitle(const std::string &title);
+	const std::string &title() const;
+
+	/**
+	 * Body of the notification's text part.
+	 */
+	void setBody(const std::string &body);
+	const std::string &body() const;
+
+	/**
+	 * time_to_live from FCM documentation:
+	 *
+	 * - This parameter specifies how long (in seconds) the message should be kept in FCM storage if
+	 *   the device is offline.
+	 *
+	 * - The maximum time to live supported is 4 weeks, and the default value is 4 weeks when not set.
+	 *
+	 * - TTL of 0 requires an immediate delivery that is however not guaranteed and such messages
+	 *   can be discarded.
+	 */
+	void setTimeToLive(const Poco::Timespan &timeToLive);
+	const Poco::Timespan &timeToLive() const;
+
+	/**
+	 * priority from FCM documentation:
+	 *
+	 * - Sets the priority of the message. Valid values are "normal" and "high."
+	 *
+	 * - On iOS, these correspond to APNs priorities 5 and 10. By default, notification messages are
+	 *   sent with high priority, and data messages are sent with normal priority.
+	 *
+	 * - Normal priority optimizes the client app's battery consumption and should be used unless
+	 *   immediate delivery is required.
+	 *
+	 * - For messages with normal priority, the app may receive the message with unspecified delay.
+	 *
+	 * - When a message is sent with high priority, it is sent immediately, and the app can wake
+	 *   a sleeping device and open a network connection to your server.
+	 */
+	void setPriority(const Priority &priority);
+	Priority priority() const;
+
+	/**
+	 * content_available from FCM documentation:
+	 *
+	 *  - On iOS, use this field to represent content-available in the APNs payload.
+	 *
+	 *  - When a notification or message is sent and this is set to true, an inactive client app is awoken.
+	 *
+	 *  - On Android, data messages wake the app by default. On Chrome, currently not supported.
+	 */
+	void setContentAvailable(bool contentAvailable);
+	bool contentAvailable() const;
+
+private:
+	std::set<FCMTokenID> m_recipients;
+	std::string m_title;
+	std::string m_body;
+	Poco::Timespan m_timeToLive;
+	Priority m_priority;
+	bool m_contentAvailable;
+};
+
+}

src/CMakeLists.txt
@@ -101,6 +101,7 @@ file(GLOB SOURCES
 	${PROJECT_SOURCE_DIR}/dao/ConnectorLoader.cpp
 	${PROJECT_SOURCE_DIR}/dao/SQLLoader.cpp
 	${PROJECT_SOURCE_DIR}/dao/SQLQuery.cpp
+	${PROJECT_SOURCE_DIR}/fcm/FCMMessage.cpp
 	${PROJECT_SOURCE_DIR}/service/ControlService.cpp
 	${PROJECT_SOURCE_DIR}/service/ControlServiceImpl.cpp
 	${PROJECT_SOURCE_DIR}/service/UserService.cpp

commit 34a3d17aa095215a5692d3ece7b7188a96c2aa85

FCMMessage: remove broken copy constructor
Fixes: 7e407c0a578d ("FCMMessage: new class")
Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

src/fcm/FCMMessage.cpp
@@ -15,10 +15,6 @@ FCMMessage::FCMMessage():
 {
 }
 
-FCMMessage::FCMMessage(const FCMMessage &copy)
-{
-}
-
 void FCMMessage::addRecipient(const FCMTokenID &to)
 {
 	m_recipients.insert(to);

src/fcm/FCMMessage.h
@@ -30,7 +30,6 @@ public:
 	static const Poco::Timespan MAX_SUPPORTED_TTL;
 
 	FCMMessage();
-	FCMMessage(const FCMMessage &copy);
 
 	/**
 	 * Adds one recipient into JSON array of recipients