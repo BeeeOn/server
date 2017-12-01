d3ee07df2d6350d3d7b503d0b9064cbb4d41bcf0
Jan Viktorin <iviktorin@fit.vutbr.cz>
2017-12-01 13:16:21 +0100

    xmlui: Serializing: fix device type info
    The attribute displayName is wrong. Using of methods name, vendor and
    displayName and displayVendor is currently floating in the air. The goal
    is to provide device type info to the client. Currently, the values coming
    from methods name() and vendor() are empty and never filled. Thus, prefer
    methods displayName() and displayVendor().
    
    Also, change the attributes' names. The attribute @displayName is wrong.
    It is not a displayName but full name of the type. This can however be
    constructed from type name and vendor.
    
    The attribute @name is also wrong, it is now renamed to @type_name which
    discribes the purpose better. Currently, it is unused as there is a duplicate
    attribute @name defined earlier.
    
    Fixes: d662a41b1f0b ("xmlui: Serializing: changes in DeviceInfo")
    Fixes: 2eae6567e0c5 ("DeviceInfoProvider: implement findByNameAndVendor using match()")
    
    Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

--- src/xmlui/Serializing.cpp
+++ src/xmlui/Serializing.cpp
@@ -137,9 +137,7 @@ void BeeeOn::XmlUI::serialize(Poco::XML::XMLWriter &output,
 
 	const Poco::SharedPtr<DeviceInfo> info = device.type();
 
-	attrs.addAttribute("", "displayName", "displayName", "",
-			info->vendor() + " " + info->name());
-	attrs.addAttribute("", "name", "name", "", info->displayName());
+	attrs.addAttribute("", "type_name", "type_name", "", info->displayName());
 	attrs.addAttribute("", "vendor", "vendor", "", info->displayVendor());
 
 	output.startElement("", "device", "device", attrs);
