BeeeOn/server

commit e9626bd48ed4b78b58201e10decfd0fcf6d08a1c

LegacyFCMClient: new class
Provides communcation with Firebase Cloud Messaging service based on
HTTP(S) requests. It formats the FCMMessage instances and parses
the received responses.

The included unit test LegacyFCMClientTest demonstrates behaviours:

* sending while missing any recipient
* sending to an invalid recipient
* sending to a live recipient (must be configured from outside)

To perform online tests, some configuration from environment is
needed:

* FCM_SERVER_KEY - gather from FCM console (legacy server key)
* FCM_RECIPIENT  - token from a live recipient

Signed-off-by: Peter Kubov <xkubov06@stud.fit.vutbr.cz>
Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

src/fcm/LegacyFCMClient.cpp
@@ -1,0 +1,344 @@
+#include <Poco/DateTimeParser.h>
+#include <Poco/Logger.h>
+#include <Poco/NumberParser.h>
+#include <Poco/String.h>
+#include <Poco/URI.h>
+
+#include <Poco/Dynamic/Var.h>
+#include <Poco/JSON/Object.h>
+
+#include <Poco/Net/HTTPRequest.h>
+#include <Poco/Net/HTTPClientSession.h>
+#include <Poco/Net/HTTPSClientSession.h>
+#include <Poco/Net/NetException.h>
+
+#include "di/Injectable.h"
+#include "fcm/LegacyFCMClient.h"
+#include "ssl/SSLClient.h"
+#include "util/JsonUtil.h"
+#include "util/Sanitize.h"
+
+using namespace BeeeOn;
+using namespace Poco;
+using namespace Poco::JSON;
+using namespace Poco::Net;
+using namespace std;
+
+BEEEON_OBJECT_BEGIN(BeeeOn, LegacyFCMClient)
+BEEEON_OBJECT_CASTABLE(FCMClient)
+BEEEON_OBJECT_TEXT("fcmApiUri", &LegacyFCMClient::setApiUri)
+BEEEON_OBJECT_REF("sslConfig", &LegacyFCMClient::setSSLConfig)
+BEEEON_OBJECT_END(BeeeOn, LegacyFCMClient)
+
+LegacyFCMClient::LegacyFCMClient():
+	m_apiUri("https://fcm.googleapis.com/fcm/send")
+{
+}
+
+FCMResponse LegacyFCMClient::sendMessage(
+		const FCMMessage &message,
+		const string &serverKey,
+		bool dryRun)
+{
+	const URI uri(m_apiUri);
+	SharedPtr<HTTPClientSession> session = connect(uri);
+
+	const string &content = buildContent(message, dryRun);
+
+	HTTPRequest req(HTTPRequest::HTTP_POST,
+			uri.getPathAndQuery(),
+			HTTPMessage::HTTP_1_1);
+	req.setContentType("application/json");
+	req.set("Authorization", "key=" + serverKey);
+	req.set("Accept", "application/json");
+	req.setContentLength(content.length());
+
+	if (logger().trace()) {
+		ostringstream s;
+		req.write(s);
+		s << content;
+		logger().trace("request: " + s.str(), __FILE__, __LINE__);
+	}
+
+	session->sendRequest(req) << content;
+
+	HTTPEntireResponse response;
+	response.readBody(session->receiveResponse(response));
+
+	if (response.getStatus() >= HTTPEntireResponse::HTTP_BAD_REQUEST) {
+		logger().error(
+			"status: " + to_string(response.getStatus()),
+			__FILE__, __LINE__);
+	}
+	else if (logger().debug()) {
+		logger().information(
+			"status: " + to_string(response.getStatus()),
+			__FILE__, __LINE__);
+	}
+
+	if (logger().debug()) {
+		logger().debug("response: " + response.getBody(),
+				__FILE__, __LINE__);
+	}
+
+	return processResponse(message, response);
+}
+
+string LegacyFCMClient::buildContent(const FCMMessage &message, bool dryRun) const
+{
+	Object::Ptr notification = new Object;
+
+	switch (message.priority()) {
+	case FCMMessage::PRIORITY_NORM:
+		notification->set("priority", "normal");
+		break;
+	case FCMMessage::PRIORITY_HIGH:
+		notification->set("priority", "high");
+		break;
+	default:
+		throw IllegalStateException("invalid FCM notification priority");
+	}
+
+	if (message.recipients().empty())
+		throw IllegalStateException("no recipient in FCM notification");
+
+	Array::Ptr recipients = new Array;
+	for (const auto &recipient : message.recipients())
+		recipients->add(recipient.toString());
+
+	notification->set("registration_ids", recipients);
+
+	notification->set("time_to_live", message.timeToLive().totalSeconds());
+	notification->set("content_available", message.contentAvailable());
+
+	if (dryRun)
+		notification->set("dry_run", true);
+
+	Object::Ptr text = new Object;
+
+	if (message.title().empty())
+		throw IllegalStateException("missing title of FCM notification");
+
+	text->set("title", message.title());
+
+	if (message.body().empty())
+		throw IllegalStateException("missing text of FCM notification");
+
+	text->set("body", message.body());
+	notification->set("notification", text);
+
+	return Dynamic::Var::toString(notification);
+}
+
+Timespan LegacyFCMClient::parseRetryAfter(
+		string content,
+		const Poco::Timestamp &now) const
+{
+	int seconds = 0;
+
+	trimInPlace(content);
+
+	if (NumberParser::tryParse(content, seconds)) {
+		if (seconds < 0)
+			return FCMResponse::UNKNOWN_DELAY;
+
+		return seconds * Timespan::SECONDS;
+	}
+
+	DateTime since;
+	int timeZone = 0;
+
+	if (DateTimeParser::tryParse(content, since, timeZone)) {
+		since.makeUTC(timeZone);
+		const Timespan &diff = since.timestamp() - now;
+
+		if (diff < 0)
+			return FCMResponse::UNKNOWN_DELAY;
+
+		return diff;
+	}
+
+	logger().warning("failed to parse Retry-After",
+			__FILE__, __LINE__);
+
+	return FCMResponse::UNKNOWN_DELAY;
+}
+
+FCMResponse LegacyFCMClient::processResponse(
+		const FCMMessage &message,
+		const HTTPEntireResponse &httpResponse) const
+{
+	FCMResponse response;
+
+	// process the header, it is suitable for various error conditions below
+	if (httpResponse.has("Retry-After"))
+		response.setRetryAfter(parseRetryAfter(httpResponse["Retry-After"]));
+
+	if (httpResponse.getStatus() >= HTTPEntireResponse::HTTP_INTERNAL_SERVER_ERROR) {
+		response.setUnderliveredRecipients(message.recipients());
+		return response;
+	}
+
+	if (httpResponse.getStatus() == HTTPEntireResponse::HTTP_UNAUTHORIZED)
+		throw NotAuthenticatedException("failed to authenticate to FCM server");
+
+	if (httpResponse.getStatus() >= HTTPEntireResponse::HTTP_BAD_REQUEST)
+		throw ProtocolException("invalid body for FCM server");
+
+	return processResponseBody(
+		message,
+		response,
+		JsonUtil::parse(httpResponse.getBody())
+	);
+}
+
+FCMResponse LegacyFCMClient::processResponseBody(
+		const FCMMessage &message,
+		FCMResponse &response,
+		Object::Ptr object) const
+{
+	const int failure = object->getValue<int>("failure");
+	const int canonicalIds = object->getValue<int>("canonical_ids");
+
+	if (logger().debug()) {
+		const int success = object->getValue<int>("success");
+		const Array::Ptr results = object->getArray("results");
+
+		logger().debug("response "
+				+ to_string(success) + "/"
+				+ to_string(failure) + "/"
+				+ to_string(canonicalIds) + "/"
+				+ to_string(results->size()),
+				__FILE__, __LINE__);
+	}
+
+	if (failure == 0 && canonicalIds == 0)
+		return response;
+
+	return processAffectedTokens(message, response, object);
+}
+
+FCMResponse LegacyFCMClient::processAffectedTokens(
+		const FCMMessage &message,
+		FCMResponse &response,
+		Object::Ptr object) const
+{
+	const Array::Ptr results = object->getArray("results");
+
+	set<FCMTokenID> undelivered;
+	set<FCMTokenID> failing;
+	map<FCMTokenID, FCMTokenID> renewed;
+
+	unsigned int index = 0;
+
+	for (const auto &original : message.recipients()) {
+		if (index >= results->size())
+			break;
+
+		const Object::Ptr result = results->getObject(index);
+
+		index += 1;
+
+		if (result->has("error")) {
+			const string &spec = result->getValue<string>("error");
+
+			if (isUndelivered(spec)) {
+				undelivered.emplace(original);
+			}
+			else if (isFailing(spec)) {
+				failing.emplace(original);
+			}
+			else if (isRateExceeded(spec)) {
+				logger().critical(
+					"exceeded message rate",
+					__FILE__, __LINE__);
+			}
+			else if (isSenderMismatch(spec)) {
+				logger().critical(
+					"senderId mismatch, check configuration",
+					__FILE__, __LINE__);
+			}
+			else {
+				logger().warning(
+					"unhandled error type '" + spec + "'",
+					__FILE__, __LINE__);
+			}
+		}
+		else if (result->has("message_id") && result->has("registration_id")) {
+			const string &messageId = result->getValue<string>("message_id");
+			const string &renew = result->getValue<string>("registration_id");
+
+			if (logger().debug()) {
+				logger().debug("renew user token for " + messageId,
+						__FILE__, __LINE__);
+			}
+
+			try {
+				FCMTokenID tokenRenew = FCMTokenID::parse(Sanitize::token(renew));
+				renewed.emplace(original, tokenRenew);
+			}
+			catch (const Exception &e) {
+				logger().log(e, __FILE__, __LINE__);
+			}
+		}
+		else if (result->has("message_id")) {
+			const string &messageId = result->getValue<string>("message_id");
+
+			if (logger().debug()) {
+				logger().debug("no reaction for " + messageId,
+						__FILE__, __LINE__);
+			}
+		}
+		else {
+			logger().warning("unrecognized response", __FILE__, __LINE__);
+		}
+	}
+
+	response.setUnderliveredRecipients(undelivered);
+	response.setFailingRecipients(failing);
+	response.setRenewedRecipients(renewed);
+
+	return response;
+}
+
+bool LegacyFCMClient::isUndelivered(const string &spec) const
+{
+	return spec == "Unavailable";
+}
+
+bool LegacyFCMClient::isFailing(const string &spec) const
+{
+	return spec == "InvalidRegistration" || spec == "NotRegistred";
+}
+
+bool LegacyFCMClient::isRateExceeded(const string &spec) const
+{
+	return spec == "DeviceMessageRateExceeded" || spec == "TopicsMessageRateExceeded";
+}
+
+bool LegacyFCMClient::isSenderMismatch(const string &spec) const
+{
+	return spec == "MismatchSenderId";
+}
+
+SharedPtr<HTTPClientSession> LegacyFCMClient::connect(const URI &uri)
+{
+	if (m_sslConfig.isNull()) {
+		return new HTTPClientSession(
+			uri.getHost(), uri.getPort());
+	}
+	else {
+		return new HTTPSClientSession(
+			uri.getHost(), uri.getPort(), m_sslConfig->context());
+	}
+}
+
+void LegacyFCMClient::setApiUri(const string &apiUri)
+{
+	m_apiUri = apiUri;
+}
+
+void LegacyFCMClient::setSSLConfig(SharedPtr<SSLClient> sslConfig)
+{
+	m_sslConfig = sslConfig;
+}

conf/config.d/factory-misc.xml
@@ -22,5 +22,10 @@
 
 		<instance name="asyncExecutor" class="BeeeOn::AsyncExecutor">
 		</instance>
+
+		<instance name="fcmClient" class="BeeeOn::LegacyFCMClient">
+			<set name="fcmApiUri" text="${fcm.api.uri}" />
+			<set name="sslConfig" ref="fcmApiSSL" />
+		</instance>
 	</factory>
 </system>

conf/config.d/factory-ssl.xml
@@ -38,5 +38,10 @@
 			<set name="loadDefaultCA" text="false" />
 		</instance>
 
+		<instance name="fcmApiSSL" class="BeeeOn::SSLClient">
+			<set name="verificationMode" text="${fcm.ssl.verifyMode}" />
+			<set name="caLocation" text="${fcm.ssl.authority}" />
+			<set name="disabledProtocols" text="sslv2,sslv3,tlsv1,tlsv1_1" />
+		</instance>
 	</factory>
 </system>

conf/config.d/logging.ini
@@ -207,6 +207,9 @@ loggers.DeviceInfoProvider.level = ${logging.default.level}
 loggers.LoggingObserver = BeeeOn::LoggingObserver
 loggers.LoggingObserver.level = ${logging.default.level}
 
+logging.LegacyFCMClient = BeeeOn::LegacyFCMClient
+logging.LegacyFCMClient.level = ${logging.default.level}
+
 ; RPC
 
 loggers.FakeGatewayRPC.name = BeeeOn::FakeGatewayRPC

conf/server-startup.ini
@@ -115,6 +115,11 @@ google.redirectUri =
 [fcm]
 enable = no
 ;senderId =
+;serverKey =
+
+api.uri = https://fcm.googleapis.com/fcm/send
+ssl.verifyMode = relaxed
+ssl.authority = /usr/share/beeeon/cert/mozilla-cacert-2016-11-02.pem
 
 [database]
 backend = odbc

conf/testing-startup.ini
@@ -104,6 +104,11 @@ google.redirectUri =
 [fcm]
 enable = no
 ;senderId =
+;serverKey =
+
+api.uri = https://fcm.googleapis.com/fcm/send
+ssl.verifyMode = relaxed
+ssl.authority = ${application.configDir}../cert/mozilla-cacert-2016-11-02.pem
 
 [database]
 backend = odbc

src/CMakeLists.txt
@@ -104,6 +104,7 @@ file(GLOB SOURCES
 	${PROJECT_SOURCE_DIR}/fcm/FCMClient.cpp
 	${PROJECT_SOURCE_DIR}/fcm/FCMMessage.cpp
 	${PROJECT_SOURCE_DIR}/fcm/FCMResponse.cpp
+	${PROJECT_SOURCE_DIR}/fcm/LegacyFCMClient.cpp
 	${PROJECT_SOURCE_DIR}/service/ControlService.cpp
 	${PROJECT_SOURCE_DIR}/service/ControlServiceImpl.cpp
 	${PROJECT_SOURCE_DIR}/service/UserService.cpp

src/fcm/LegacyFCMClient.h
@@ -1,0 +1,122 @@
+#pragma once
+
+#include <Poco/Timespan.h>
+#include <Poco/Timestamp.h>
+#include <Poco/URI.h>
+#include <Poco/JSON/Object.h>
+#include <Poco/Net/HTTPSClientSession.h>
+
+#include "fcm/FCMClient.h"
+#include "fcm/FCMMessage.h"
+#include "fcm/FCMResponse.h"
+#include "net/HTTPEntireResponse.h"
+#include "util/Loggable.h"
+
+namespace BeeeOn {
+
+class SSLClient;
+
+/**
+ * @brief LegacyFCMClient is a reusable thread-safe implementation of a client to
+ * talk to the Firebase Cloud Messaging.
+ *
+ * The LegacyFCMClient implements the legacy HTTP(s) communication protocol when
+ * talking to the FCM server. To perform a valid request, each notification must be
+ * authorized via the server key obtained from the Firebase developers console
+ * (legacy key of the cloud messaging service).
+ */
+class LegacyFCMClient : public FCMClient, protected Loggable {
+public:
+	typedef Poco::SharedPtr<LegacyFCMClient> Ptr;
+
+	LegacyFCMClient();
+
+	/**
+	 * Sends the given notification to FCM server with a specific server key
+	 * authorized to send notifications.
+	 */
+	FCMResponse sendMessage(
+			const FCMMessage &message,
+			const std::string &serverKey,
+			bool dryRun = false);
+
+	void setApiUri(const std::string &apiUri);
+	void setSSLConfig(Poco::SharedPtr<SSLClient> sslConfig);
+
+protected:
+	/**
+	 * Convert the notification to JSON Object as expected by the Firebase Cloud
+	 * Messaging service.
+	 *
+	 * @see https://developers.google.com/cloud-messaging/http-server-ref
+	 */
+	std::string buildContent(const FCMMessage &message, bool dryRun) const;
+
+	/**
+	 * Process HTTP status and then parse the response body. Here, the Retry-After is
+	 * extracted if present. If a HTTP status represents a client error, an exception
+	 * is thrown.
+	 *
+	 * HTTP status 5xx returns a response with all original senders set as undelivered.
+	 * Such message should be retried later.
+	 *
+	 * @throws Poco::Net::NotAuthenticatedException on status 401
+	 * @throws Poco::ProtocolException on status 4xx
+	 */
+	FCMResponse processResponse(
+			const FCMMessage &message,
+			const HTTPEntireResponse &response) const;
+
+	/**
+	 * Detect errors or token renews and if any, process them.
+	 * Otherwise, such response denotes tha that the message was
+	 * successfully delivered and no more processing is done.
+	 */
+	FCMResponse processResponseBody(
+			const FCMMessage &message,
+			FCMResponse &response,
+			Poco::JSON::Object::Ptr object) const;
+
+	/**
+	 * Process all response errors and token renews.
+	 */
+	FCMResponse processAffectedTokens(
+			const FCMMessage &message,
+			FCMResponse &response,
+			Poco::JSON::Object::Ptr object) const;
+
+	/**
+	 * Connect to the Firebase Cloud service via HTTP or HTTPS.
+	 */
+	Poco::SharedPtr<Poco::Net::HTTPClientSession> connect(const Poco::URI &uri);
+
+	/**
+	 * Parse the header Retry-After that can come in an HTTP response.
+	 * The Retry-After can be expressed either as seconds or as a date time.
+	 *
+	 * @param content - value of the Retry-After header
+	 * @param now - reference time, can be omitted to use the current time
+	 *
+	 * @see https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Retry-After
+	 */
+	Poco::Timespan parseRetryAfter(
+			std::string content,
+			const Poco::Timestamp &now = {}) const;
+
+	bool isUndelivered(const std::string &spec) const;
+	bool isFailing(const std::string &spec) const;
+	bool isRateExceeded(const std::string &spec) const;
+	bool isSenderMismatch(const std::string &spec) const;
+
+private:
+	/**
+	 * Uri of FCM server recieving notifications
+	 */
+	std::string m_apiUri;
+
+	/**
+	 * SSL configuration
+	 */
+	Poco::SharedPtr<SSLClient> m_sslConfig;
+};
+}

commit a83138dba09aee12b850192891dd8ab98b18a514

LegacyFCMClient: fix name of failing FCM response
Fixes: e9626bd48ed4 ("LegacyFCMClient: new class")
Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

src/fcm/LegacyFCMClient.cpp
@@ -309,7 +309,7 @@ bool LegacyFCMClient::isUndelivered(const string &spec) const
 
 bool LegacyFCMClient::isFailing(const string &spec) const
 {
-	return spec == "InvalidRegistration" || spec == "NotRegistred";
+	return spec == "InvalidRegistration" || spec == "NotRegistered";
 }
 
 bool LegacyFCMClient::isRateExceeded(const string &spec) const