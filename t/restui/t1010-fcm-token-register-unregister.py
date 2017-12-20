BeeeOn/server

commit cecc4c3b0f7a58b77de90af52c2f81b3c2bf7a07

FCMTokenServiceImpl: new class
Provide implementation of FCMTokenService for manipulation with
user authorization tokens for use with Firebase Cloud Messaging
notitification service. Service is used to provide interface for users
request for registration and unregistration tokens for receiving
notifications from BeeeOn server.

Signed-off-by: Peter Kubov <xkubov06@stud.fit.vutbr.cz>
Reviewed-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

server/src/service/FCMTokenServiceImpl.cpp
@@ -1,0 +1,40 @@
+#include "di/Injectable.h"
+#include "service/FCMTokenServiceImpl.h"
+
+using namespace std;
+using namespace BeeeOn;
+
+BEEEON_OBJECT_BEGIN(BeeeOn, FCMTokenServiceImpl)
+BEEEON_OBJECT_CASTABLE(FCMTokenService)
+BEEEON_OBJECT_REF("fcmTokenDao", &FCMTokenServiceImpl::setFCMTokenDao)
+BEEEON_OBJECT_REF("transactionManager", &FCMTokenServiceImpl::setTransactionManager)
+BEEEON_OBJECT_REF("accessPolicy", &FCMTokenServiceImpl::setAccessPolicy)
+BEEEON_OBJECT_END(BeeeOn, FCMTokenServiceImpl)
+
+FCMTokenServiceImpl::FCMTokenServiceImpl()
+{
+}
+
+void FCMTokenServiceImpl::setFCMTokenDao(FCMTokenDao::Ptr dao)
+{
+	m_dao = dao;
+}
+
+void FCMTokenServiceImpl::setAccessPolicy(FCMTokenAccessPolicy::Ptr accessPolicy)
+{
+	m_accessPolicy = accessPolicy;
+}
+
+void FCMTokenServiceImpl::doRegisterToken(Single<FCMToken> &input)
+{
+	m_accessPolicy->assure(
+		FCMTokenAccessPolicy::ACTION_USER_REGISTER, input, input.target());
+	m_dao->insert(input.target());
+}
+
+void FCMTokenServiceImpl::doUnregisterToken(Single<FCMToken> &input)
+{
+	m_accessPolicy->assure(
+		FCMTokenAccessPolicy::ACTION_USER_UNREGISTER, input, input.target());
+	m_dao->remove(input.target());
+}

server/conf/config.d/factory-services.xml
@@ -98,6 +98,11 @@
 			<set name="accessPolicy" ref="accessPolicy" />
 		</instance>
 
+		<instance name="fcmTokenService" class="BeeeOn::FCMTokenServiceImpl">
+			<set name="transactionManager" ref="transactionManager" />
+			<set name="accessPolicy" ref="accessPolicy" />
+		</instance>
+
 		<instance name="workService" class="BeeeOn::WorkServiceImpl">
 			<set name="facade" ref="uiWorkFacade" />
 		</instance>

server/src/CMakeLists.txt
@@ -102,6 +102,7 @@ file(GLOB SOURCES
 	${PROJECT_SOURCE_DIR}/service/DeviceService.cpp
 	${PROJECT_SOURCE_DIR}/service/DeviceServiceImpl.cpp
 	${PROJECT_SOURCE_DIR}/service/FCMTokenService.cpp
+	${PROJECT_SOURCE_DIR}/service/FCMTokenServiceImpl.cpp
 	${PROJECT_SOURCE_DIR}/service/GWSDeviceService.cpp
 	${PROJECT_SOURCE_DIR}/service/GWSDeviceServiceImpl.cpp
 	${PROJECT_SOURCE_DIR}/service/GatewayService.cpp

server/src/service/FCMTokenServiceImpl.h
@@ -1,0 +1,36 @@
+#pragma once
+
+#include "dao/FCMTokenDao.h"
+#include "policy/FCMTokenAccessPolicy.h"
+#include "service/FCMTokenService.h"
+#include "transaction/Transactional.h"
+
+namespace BeeeOn {
+
+class FCMTokenServiceImpl : public FCMTokenService, public Transactional {
+public:
+	FCMTokenServiceImpl();
+
+	void setFCMTokenDao(FCMTokenDao::Ptr dao);
+	void setAccessPolicy(FCMTokenAccessPolicy::Ptr accessPolicy);
+
+	void registerToken(Single<FCMToken> &input) override
+	{
+		BEEEON_TRANSACTION(doRegisterToken(input));
+	}
+
+	void unregisterToken(Single<FCMToken> &input) override
+	{
+		BEEEON_TRANSACTION(doUnregisterToken(input));
+	}
+
+protected:
+	void doRegisterToken(Single<FCMToken> &input);
+	void doUnregisterToken(Single<FCMToken> &input);
+
+private:
+	FCMTokenDao::Ptr m_dao;
+	FCMTokenAccessPolicy::Ptr m_accessPolicy;
+};
+
+}

commit 33e6f74ac09f09ebb7958c98cc24c700e5705520

FCMTokenServiceImpl: fix handling of existing token
When registering an already existing token, the register service call
fails and the server returns HTTP status 500. This is not what we
would like to see.

This changes the behaviour to be silent while registering a duplicate
token. However, if such token already exists but for a different user
(this should never happen however), we fail with InvalidArgumentException
(thus 400 Bad request).

Fixes: cecc4c3b0f7a ("FCMTokenServiceImpl: new class")
Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

src/service/FCMTokenServiceImpl.cpp
@@ -1,7 +1,10 @@
+#include <Poco/Exception.h>
+
 #include "di/Injectable.h"
 #include "service/FCMTokenServiceImpl.h"
 
 using namespace std;
+using namespace Poco;
 using namespace BeeeOn;
 
 BEEEON_OBJECT_BEGIN(BeeeOn, FCMTokenServiceImpl)
@@ -29,7 +32,18 @@ void FCMTokenServiceImpl::doRegisterToken(Single<FCMToken> &input)
 {
 	m_accessPolicy->assure(
 		FCMTokenAccessPolicy::ACTION_USER_REGISTER, input, input.target());
-	m_dao->insert(input.target());
+
+	FCMToken token(input.target());
+
+	if (!m_dao->fetch(token)) {
+		m_dao->insert(input.target());
+		return;
+	}
+
+	if (token.user().id() == input.target().user().id())
+		return;
+
+	throw InvalidArgumentException("same token for different users");
 }
 
 void FCMTokenServiceImpl::doUnregisterToken(Single<FCMToken> &input)

t/restui/t1010-fcm-token-register-unregister.py
@@ -65,6 +65,18 @@ class TestFCMTokenRegisterUnregister(unittest.TestCase):
 
 		self.assertEqual(204, response.status)
 
+	@unittest.skipIf("FCM_SENDER_ID" not in os.environ,
+			"fcm support is disabled")
+	def test3_register_duplicate_token(self):
+		req = POST(config.ui_host, config.ui_port, "/notifications/fcm")
+		req.authorize(self.session)
+		req.body(json.dumps(
+			{"id": "TESTING_FCM_TOKEN"}
+		))
+		response, _ = req()
+
+		self.assertEqual(204, response.status)
+
 if __name__ == '__main__':
 	import sys
 	import taprunner

t/testing-data.sql
@@ -18,6 +18,12 @@ VALUES
 	'en_US'
 );
 
+INSERT INTO beeeon.fcm_tokens (token, user_id)
+VALUES (
+	'TESTING_FCM_TOKEN',
+	'99e54009-1b7d-4408-9bb9-d9fb0c69d5b4'
+);
+
 INSERT INTO beeeon.identities (id, email)
 VALUES
 ('be572079-0971-47f0-95df-68476a642911', 'joe.doe@example.org'),