BeeeOn/server

commit e3c58b4482cdce09a24656f568a14c5736839ac0

GatewayScanController: new class
Provide a control logic of the scanning process on a gateway.
The controller manages instances of GatewayScan and makes them
accessible to users.

Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

src/gws/GatewayScanController.cpp
@@ -1,0 +1,296 @@
+#include <Poco/Clock.h>
+#include <Poco/Exception.h>
+#include <Poco/DateTimeFormatter.h>
+#include <Poco/Logger.h>
+
+#include "di/Injectable.h"
+#include "gws/GatewayScanController.h"
+#include "util/LambdaTimerTask.h"
+
+BEEEON_OBJECT_BEGIN(BeeeOn, GatewayScanController)
+BEEEON_OBJECT_CASTABLE(StoppableLoop)
+BEEEON_OBJECT_PROPERTY("gatewayRPC", &GatewayScanController::setGatewayRPC)
+BEEEON_OBJECT_PROPERTY("defaultDuration", &GatewayScanController::setDefaultDuration)
+BEEEON_OBJECT_PROPERTY("cleanupInterval", &GatewayScanController::setCleanupInterval)
+BEEEON_OBJECT_PROPERTY("cleanupOlderThan", &GatewayScanController::setCleanupOlderThan)
+BEEEON_OBJECT_END(BeeeOn, GatewayScanController)
+
+using namespace std;
+using namespace Poco;
+using namespace BeeeOn;
+
+const static Timespan MAX_SCAN_DURATION = 10 * Timespan::MINUTES;
+
+GatewayScanController::ScanWrapper::ScanWrapper()
+{
+}
+
+const GatewayScan &GatewayScanController::ScanWrapper::scan() const
+{
+	ScopedLock guard(const_cast<ScanWrapper&>(*this));
+	return m_scan;
+}
+
+GatewayScan &GatewayScanController::ScanWrapper::scan()
+{
+	ScopedLock guard(const_cast<ScanWrapper&>(*this));
+	return m_scan;
+}
+
+GatewayScanController::CleanUpTask::CleanUpTask(
+		FastMutex &lock,
+		GatewayScanController::ScanMap &scanMap):
+	m_olderThen(30 * Timespan::MINUTES),
+	m_lock(lock),
+	m_scanMap(scanMap)
+{
+}
+
+void GatewayScanController::CleanUpTask::setOlderThan(
+		const Poco::Timespan &olderThen)
+{
+	if (olderThen < 0)
+		throw InvalidArgumentException("cleanup older-than timeout must be positive");
+
+	m_olderThen = olderThen;
+}
+
+void GatewayScanController::CleanUpTask::run()
+{
+	FastMutex::ScopedLock guard(m_lock);
+	const Clock started;
+	const size_t total = m_scanMap.size();
+	size_t count = 0;
+
+	logger().information("cleaning up scan records ("
+			+ to_string(total) + ")",
+			__FILE__, __LINE__);
+
+	for (auto it = m_scanMap.begin(); it != m_scanMap.end();) {
+		ScanWrapper::Ptr context = it->second;
+		ScanWrapper::ScopedLock guard(*context);
+
+		if (logger().debug()) {
+			logger().debug("cleaning up scan record for "
+					+ it->first.toString(),
+					__FILE__, __LINE__);
+		}
+
+		const auto &started = context->scan().started();
+		const auto &duration = context->scan().duration();
+		const Timestamp now;
+
+		switch (context->scan().state()) {
+		case GatewayScan::SCAN_IDLE:
+			it = m_scanMap.erase(it);
+			count += 1;
+			break;
+
+		case GatewayScan::SCAN_FINISHED:
+		case GatewayScan::SCAN_FAILED:
+			if (started + duration < now - m_olderThen) {
+				it = m_scanMap.erase(it);
+				count += 1;
+			}
+			else {
+				++it;
+			}
+
+			break;
+
+		case GatewayScan::SCAN_WAITING:
+		case GatewayScan::SCAN_PROCESSING:
+			if (started + duration < now) {
+				logger().warning("scan record for " + it->first.toString()
+						+ " was not stopped on time, marking as failed",
+						__FILE__, __LINE__);
+				context->scan().changeState(GatewayScan::SCAN_FAILED);
+			}
+
+			++it;
+			break;
+
+		default:
+			++it;
+		}
+	}
+
+	logger().notice("cleaned up "
+			+ to_string(count) + "/" + to_string(total)
+			+ " scan records in "
+			+ to_string(started.elapsed()) + "us",
+			__FILE__, __LINE__);
+}
+
+GatewayScanController::GatewayScanController():
+	m_defaultDuration(2 * Timespan::MINUTES),
+	m_cleanupInterval(15 * Timespan::MINUTES),
+	m_cleanupTask(new CleanUpTask(m_lock, m_scanMap))
+{
+}
+
+void GatewayScanController::setDefaultDuration(const Poco::Timespan &duration)
+{
+	if (duration.totalSeconds() <= 0)
+		throw InvalidArgumentException("duration must be at least a second");
+
+	if (duration > MAX_SCAN_DURATION)
+		throw InvalidArgumentException("too long default duration");
+
+	m_defaultDuration = duration;
+}
+
+void GatewayScanController::setCleanupInterval(const Timespan &interval)
+{
+	if (interval.totalMilliseconds() <= 0)
+		throw InvalidArgumentException("cleanup interval must be at least a millisecond");
+
+	m_cleanupInterval = interval;
+}
+
+void GatewayScanController::setCleanupOlderThan(const Timespan &time)
+{
+	m_cleanupTask->setOlderThan(time);
+}
+
+void GatewayScanController::setGatewayRPC(GatewayRPC::Ptr rpc)
+{
+	m_rpc = rpc;
+}
+
+void GatewayScanController::start()
+{
+	logger().information("starting gateway scan controller");
+
+	m_timer.scheduleAtFixedRate(
+		m_cleanupTask,
+		m_cleanupInterval.totalMilliseconds(),
+		m_cleanupInterval.totalMilliseconds());
+}
+
+void GatewayScanController::stop()
+{
+	logger().information("stopping gateway scan controller");
+
+	m_timer.cancel(true);
+
+	FastMutex::ScopedLock guard(m_lock);
+	m_scanMap.clear();
+}
+
+GatewayScan GatewayScanController::scan(const Gateway &gateway, const Timespan &timeout)
+{
+	ScopedLockWithUnlock<FastMutex> guard(m_lock);
+
+	auto it = m_scanMap.find(gateway.id());
+	if (it != m_scanMap.end())
+		return it->second->scan();
+
+	const auto id = gateway.id();
+	const Timespan &duration = timeout <= 0? m_defaultDuration : timeout;
+
+	if (duration > MAX_SCAN_DURATION)
+		throw InvalidArgumentException("too big scanning timeout given");
+
+	ScanWrapper::Ptr context = new ScanWrapper;
+	context->scan().setDuration(duration);
+
+	m_scanMap.emplace(id, context);
+
+	guard.unlock();
+
+	const Clock start;
+
+	m_rpc->sendListen([id, context](GatewayRPCResult::Ptr result) mutable
+		{
+			Logger &logger = Loggable::forClass(typeid(GatewayScanController));
+
+			ScanWrapper::ScopedLock guard(*context);
+			GatewayScan &scan = context->scan();
+
+			switch (result->status()) {
+			case GatewayRPCResult::Status::PENDING:
+			case GatewayRPCResult::Status::ACCEPTED:
+				logger.information("scanning " + id.toString() + " is starting",
+						__FILE__, __LINE__);
+				scan.changeState(GatewayScan::SCAN_PROCESSING);
+				break;
+
+			case GatewayRPCResult::Status::SUCCESS:
+				logger.information("scanning " + id.toString() + " in progress",
+						__FILE__, __LINE__);
+				scan.changeState(GatewayScan::SCAN_PROCESSING);
+				break;
+
+			case GatewayRPCResult::Status::FAILED:
+				logger.error("scanning " + id.toString() + " has failed",
+						__FILE__, __LINE__);
+
+				scan.changeState(GatewayScan::SCAN_FAILED);
+				break;
+
+			case GatewayRPCResult::Status::TIMEOUT:
+			case GatewayRPCResult::Status::NOT_CONNECTED:
+				logger.error("scanning " + id.toString() + " did not start on time",
+						__FILE__, __LINE__);
+
+				scan.changeState(GatewayScan::SCAN_FAILED);
+				break;
+			}
+		},
+		gateway,
+		duration
+	);
+
+	m_timer.schedule(new LambdaTimerTask([start, id, context]() mutable
+		{
+			Logger &logger = Loggable::forClass(typeid(GatewayScanController));
+
+			ScanWrapper::ScopedLock guard(*context);
+			GatewayScan &scan = context->scan();
+
+			const Timespan &duration = scan.started().elapsed();
+
+			switch (scan.state()) {
+			case GatewayScan::SCAN_FINISHED:
+				logger.warning("scanning of "
+						+ id.toString()
+						+ " is already finished",
+						__FILE__, __LINE__);
+				break;
+
+			case GatewayScan::SCAN_WAITING:
+			case GatewayScan::SCAN_PROCESSING:
+				logger.information("scanning of "
+						+ id.toString()
+						+ " was finished after "
+						+ DateTimeFormatter::format(duration),
+						__FILE__, __LINE__);
+
+				scan.changeState(GatewayScan::SCAN_FINISHED);
+				break;
+
+			default:
+				if (logger.debug()) {
+					logger.debug("skipping explicit finish of "
+							+ id.toString() + " scan",
+							__FILE__, __LINE__);
+				}
+				break;
+			}
+		}),
+		start + duration.totalMicroseconds());
+
+	return context->scan();
+}
+
+GatewayScan GatewayScanController::find(const Gateway &gateway)
+{
+	FastMutex::ScopedLock guard(m_lock);
+
+	auto it = m_scanMap.find(gateway.id());
+	if (it != m_scanMap.end())
+		return it->second->scan();
+
+	return GatewayScan::createIdle();
+}

conf/config.d/factory-all.xml
@@ -5,6 +5,7 @@
 			<add name="runnables" ref="uiWorkScheduler" />
 			<add name="runnables" ref="fcmSender" if-yes="${fcm.enable}" />
 			<add name="loops" ref="gatewayCommunicator" />
+			<add name="loops" ref="gatewayScanController" />
 			<add name="loops" ref="webSocketServer" />
 			<add name="loops" ref="responseExpectedQueue" />
 			<add name="loops" ref="asyncGatewayRPC" />

conf/config.d/factory-gws.xml
@@ -42,5 +42,11 @@
 			<set name="deviceService" ref="gwsDeviceService" />
 			<set name="sensorHistoryService" ref="gwsSensorHistoryService" />
 		</instance>
+
+		<instance name="gatewayScanController" class="BeeeOn::GatewayScanController">
+			<set name="gatewayRPC" ref="gatewayRPC" />
+			<set name="cleanupInterval" time="15 m" />
+			<set name="cleanupOlderThan" time="30 m" />
+		</instance>
 	</factory>
 </system>

conf/config.d/factory-restui.xml
@@ -2,6 +2,7 @@
 	<factory>
 		<instance name="restui" class="BeeeOn::LoopRunner">
 			<add name="loops" ref="uiServer" />
+			<add name="loops" ref="gatewayScanController" />
 			<add name="runnables" ref="uiWorkScheduler" />
 			<add name="runnables" ref="asyncExecutor" />
 		</instance>

conf/config.d/factory-xmlui.xml
@@ -2,6 +2,7 @@
 	<factory>
 		<instance name="xmlui" class="BeeeOn::LoopRunner">
 			<add name="loops" ref="xmluiServer" />
+			<add name="loops" ref="gatewayScanController" />
 			<add name="runnables" ref="uiWorkScheduler" />
 			<add name="runnables" ref="asyncExecutor" />
 		</instance>

conf/config.d/logging.ini
@@ -320,6 +320,9 @@ loggers.GatewayCommunicator.level = ${logging.default.level}
 loggers.GatewayConnection.name = BeeeOn::GatewayConnection
 loggers.GatewayConnection.level = ${logging.default.level}
 
+loggers.GatewayScanController.name = BeeeOn::GatewayScanController
+loggers.GatewayScanController.level = ${logging.default.level}
+
 loggers.WebSocketServer.name = BeeeOn::WebSocketServer
 loggers.WebSocketServer.level = ${logging.default.level}
 

src/CMakeLists.txt
@@ -176,6 +176,7 @@ file(GLOB SOURCES
 	${PROJECT_SOURCE_DIR}/gws/GatewayConnection.cpp
 	${PROJECT_SOURCE_DIR}/gws/GatewayCommunicator.cpp
 	${PROJECT_SOURCE_DIR}/gws/GatewayListener.cpp
+	${PROJECT_SOURCE_DIR}/gws/GatewayScanController.cpp
 	${PROJECT_SOURCE_DIR}/gws/GWMessageHandler.cpp
 	${PROJECT_SOURCE_DIR}/gws/GWMessageHandlerImpl.cpp
 	${PROJECT_SOURCE_DIR}/gws/GWResponseExpectedQueue.cpp

src/gws/GatewayScanController.h
@@ -1,0 +1,144 @@
+#pragma once
+
+#include <map>
+
+#include <Poco/Mutex.h>
+#include <Poco/SharedPtr.h>
+#include <Poco/SynchronizedObject.h>
+#include <Poco/Timespan.h>
+
+#include <Poco/Util/Timer.h>
+
+#include "loop/StoppableLoop.h"
+#include "model/Gateway.h"
+#include "model/GatewayScan.h"
+#include "rpc/GatewayRPC.h"
+#include "transaction/Transactional.h"
+
+namespace BeeeOn {
+
+/**
+ * @brief Implementation of the gateway scanning control logic.
+ * The GatewayScanController manages a set of GatewayScan instances
+ * to maintain the currently running or finished scans.
+ *
+ * Each GatewayScan instance is maintained until it reaches its finish
+ * or failing state. Finished or failed scans are either restarted by
+ * a user or garbage collected by the GatewayScanController itself after
+ * a predefined time period.
+ *
+ * A gateway scan process is always executed for a certain period of time.
+ * After that period, the process is finished. The GatewayScanController
+ * manages timers to finish each scanning process after that period.
+ */
+class GatewayScanController : public Transactional, public StoppableLoop {
+public:
+	typedef Poco::SharedPtr<GatewayScanController> Ptr;
+
+	GatewayScanController();
+
+	/**
+	 * Default duration to use when the scan is called with negative
+	 * or 0 timeout.
+	 */
+	void setDefaultDuration(const Poco::Timespan &duration);
+
+	/**
+	 * Set the interval between the CleanUpTask executions.
+	 */
+	void setCleanupInterval(const Poco::Timespan &interval);
+
+	/**
+	 * @see CleanUpTask::setOlderThan()
+	 */
+	void setCleanupOlderThan(const Poco::Timespan &time);
+
+	void setGatewayRPC(GatewayRPC::Ptr rpc);
+
+	/**
+	 * Start the background cleanup task.
+	 */
+	void start() override;
+
+	/**
+	 * Stop all tasks.
+	 */
+	void stop() override;
+
+	/**
+	 * Initialize a new GatewayScan if none is in progress. If there is a GatewayScan
+	 * instance, it is reused. This means that there cannot be 2 running scans on a
+	 * single gateway.
+	 */
+	GatewayScan scan(const Gateway &gateway, const Poco::Timespan &timeout);
+
+	/**
+	 * Find a GatewayScan for the given gateway. If there is none, a new idle one
+	 * is returned.
+	 */
+	GatewayScan find(const Gateway &gateway);
+
+private:
+	/**
+	 * @brief Wrapper around the GatewayScan that adds the synchronized
+	 * object properties and dynamic allocation management via the SharedPtr.
+	 */
+	class ScanWrapper : public Poco::SynchronizedObject {
+	public:
+		typedef Poco::SharedPtr<ScanWrapper> Ptr;
+
+		ScanWrapper();
+
+		const GatewayScan &scan() const;
+		GatewayScan &scan();
+
+	private:
+		GatewayScan m_scan;
+	};
+
+	/**
+	 * Mapping Gateway IDs to their particular GatewayScan records.
+	 */
+	typedef std::map<GatewayID, ScanWrapper::Ptr> ScanMap;
+
+	/**
+	 * @brief CleanUpTask is responsible for cleaning up the obsolete
+	 * GatewayScan instances. When started, it removes GatewayScans
+	 * which are idle, or which have finished/failed (must be in this
+	 * state for at least olderThen period).
+	 *
+	 * GatewayScan instances that are still waiting or processing and
+	 * are seems to not finished on time (due some bug) are changed to
+	 * the state failed. This allows to garbage collect them during the
+	 * next start of the CleanUpTask.
+	 */
+	class CleanUpTask : public Poco::Util::TimerTask, Loggable {
+	public:
+		typedef Poco::AutoPtr<CleanUpTask> Ptr;
+
+		CleanUpTask(Poco::FastMutex &lock, ScanMap &scanMap);
+		void run() override;
+
+		/**
+		 * Set the time period we have to way for each GatewayScan
+		 * before they can be deleted after finish/failure.
+		 */
+		void setOlderThan(const Poco::Timespan &olderThen);
+
+	private:
+		Poco::Timespan m_olderThen;
+		Poco::FastMutex &m_lock;
+		ScanMap &m_scanMap;
+	};
+
+private:
+	Poco::FastMutex m_lock;
+	ScanMap m_scanMap;
+	GatewayRPC::Ptr m_rpc;
+	Poco::Timespan m_defaultDuration;
+	Poco::Timespan m_cleanupInterval;
+	CleanUpTask::Ptr m_cleanupTask;
+	Poco::Util::Timer m_timer;
+};
+
+}

commit f2276357a401969cfeb7d318ebb9956eab6e726a

GatewayScanController: restart scan if not in progress
The gateway scan should be restared when it is in the idle, finished
or failed state. Otherwise, its current state is returned. This fixes
a bug when trying to restart scan immediately after the previous one
was finished.

Fixes: e3c58b4482cd ("GatewayScanController: new class")
Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

src/gws/GatewayScanController.cpp
@@ -253,16 +253,33 @@ GatewayScan GatewayScanController::scan(const Gateway &gateway, const Timespan &
 
 	ScopedLockWithUnlock<FastMutex> guard(m_lock);
 
+	ScanHandler::Ptr context;
+
 	auto it = m_scanMap.find(id);
-	if (it != m_scanMap.end())
-		return it->second->scan();
+	if (it != m_scanMap.end()) {
+		context = it->second;
 
-	ScanHandler::Ptr context = new ScanHandler(id);
-	context->scan().setDuration(duration);
+		const GatewayScan scan = context->scan();
 
-	m_scanMap.emplace(id, context);
+		switch (scan.state()) {
+		case GatewayScan::SCAN_PROCESSING:
+		case GatewayScan::SCAN_WAITING:
+			return scan;
+		default:
+			// restart scan
+			break;
+		}
+	}
+	else {
+		context = new ScanHandler(id);
+		m_scanMap.emplace(id, context);
+	}
 
+	ScanHandler::ScopedLock scanGuard(*context);
 	guard.unlock();
+
+	context->scan().setDuration(duration);
+	context->scan().changeState(GatewayScan::SCAN_WAITING);
 
 	const Clock start;
 