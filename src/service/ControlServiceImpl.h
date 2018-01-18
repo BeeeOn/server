f9cf1e8767e11a9416f481112a0e26e48fe6c65f
Jan Viktorin <iviktorin@fit.vutbr.cz>
2018-01-18 14:19:37 +0100

    ControlService: provide method requestChange()
    The method requestChange() ensures setting of a value for the selected
    control instance.
    
    Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

--- src/service/ControlServiceImpl.h
+++ src/service/ControlServiceImpl.h
@@ -5,7 +5,9 @@
 
 #include "dao/ControlDao.h"
 #include "dao/DeviceDao.h"
+#include "gws/ControlChangeHandler.h"
 #include "policy/ControlAccessPolicy.h"
+#include "rpc/GatewayRPC.h"
 #include "service/ControlService.h"
 #include "transaction/Transactional.h"
 
@@ -17,6 +19,7 @@ public:
 
 	void setDeviceDao(DeviceDao::Ptr dao);
 	void setControlDao(ControlDao::Ptr dao);
+	void setGatewayRPC(GatewayRPC::Ptr rpc);
 	void setAccessPolicy(ControlAccessPolicy::Ptr policy);
 
 	bool fetch(Relation<Control, Device> &data) override
@@ -29,15 +32,50 @@ public:
 		BEEEON_TRANSACTION(doFetchMany(data));
 	}
 
+	void requestChange(Relation<Control, Device> &data,
+			double value,
+			const Poco::Timespan &timeout) override
+	{
+		ControlChangeHandler::Ptr handler =
+			BEEEON_TRANSACTION_RETURN(
+				ControlChangeHandler::Ptr,
+				doRequestChange(data, value, timeout));
+
+		if (handler.isNull())
+			return;
+
+		m_gatewayRPC->updateActor(
+				handler,
+				handler->device().gateway(),
+				handler->device(),
+				handler->control().info(),
+				value,
+				timeout);
+	}
+
 	void fixupControls();
 
 protected:
 	bool doFetch(Relation<Control, Device> &data);
 	void doFetchMany(Relation<std::list<Control>, Device> &data);
+	ControlChangeHandler::Ptr doRequestChange(
+			Relation<Control, Device> &data,
+			double value,
+			const Poco::Timespan &timeout);
+
+	void startRequest(
+			const UserPolicyContext &policyContext,
+			const Device &device,
+			Control &control,
+			double value);
+	void cancelRequest(
+			const Device &device,
+			Control &control);
 
 private:
 	DeviceDao::Ptr m_deviceDao;
 	ControlDao::Ptr m_controlDao;
+	GatewayRPC::Ptr m_gatewayRPC;
 	ControlAccessPolicy::Ptr m_accessPolicy;
 };
 
