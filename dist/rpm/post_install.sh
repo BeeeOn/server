BeeeOn/server

commit 60bf511cea5d6e689542406e1b6672f3f211469d

install: override postgresql dependency on post_install
The post_install allows to override the current systemd service
dependency on postgresql by providing BEEEON_DEP_PGSQL variable
while installing.

Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

dist/rpm/post_install.sh
@@ -1,9 +1,48 @@
 #! /bin/sh
 
+list_systemd_deps()
+{
+	pgsql_count=`systemctl list-unit-files --plain | grep postgresql | grep -v '^postgresql\.service' | wc -l`
+
+	if [ "${pgsql_count}" > 0 ]; then
+		echo "found non-standard postgresql services:"
+		systemctl list-unit-files | awk '
+			/^postgresql/ && $2 == "enabled" {printf "  %s", $1}
+			$1 == "postgresql.service" {printf " (*)"}
+			/^postgresql/ {print ""; next}
+		'
+		echo ""
+		echo "  you can change the dependency on postgresql systemd service"
+		echo "  during installation by setting the environment variable"
+		echo "  BEEEON_DEP_PGSQL (sudo might require -E option)"
+		echo ""
+	fi
+}
+
+fixup_systemd_deps()
+{
+	if [ -n "${BEEEON_DEPS_PGSQL}" ]; then
+		if ! grep '/\.service$/' <<< "${BEEEON_DEPS_PGSQL}" >/dev/null 2>/dev/null; then
+			BEEEON_DEPS_PGSQL="${BEEEON_DEPS_PGSQL}.service"
+		fi
+
+		echo "fixup systemd postgresql service to '${BEEEON_DEPS_PGSQL}'"
+
+		sed -i "s/=postgresql\.service/=${BEEEON_DEPS_PGSQL}/" \
+			/usr/lib/systemd/system/beeeon-server.service
+
+		echo ""
+	fi
+}
+
 case "$1" in
 1) # install
+	list_systemd_deps
+	fixup_systemd_deps
 ;;
 2) # upgrade
+	list_systemd_deps
+	fixup_systemd_deps
 ;;
 esac
 

commit 5881f5b7d7b48c53874f7e6ed56a34da6070bbc0

install: fix listing of postgresql services
When a postgresql service is not enabled, the script does not
print it but for "postgresql.service", the (*) is printed always.
Fix this behaviour to report also disabled postgresql services.

Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>
Fixes: 60bf511cea5d ("install: override postgresql dependency on post_install")

dist/rpm/post_install.sh
@@ -8,6 +8,7 @@ list_systemd_deps()
 		echo "found non-standard postgresql services:"
 		systemctl list-unit-files | awk '
 			/^postgresql/ && $2 == "enabled" {printf "  %s", $1}
+			/^postgresql/ && $2 != "enabled" {printf "  %s [%s]", $1, $2}
 			$1 == "postgresql.service" {printf " (*)"}
 			/^postgresql/ {print ""; next}
 		'