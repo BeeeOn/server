BeeeOn/server

commit abb76bc26deede2b8dd6d3a2b1e7237d7ed2e2c2

SocketGatewayPeerVerifierFactory: new interface class
Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

src/gws/SocketGatewayPeerVerifierFactory.cpp
@@ -1,0 +1,41 @@
+#include <Poco/Exception.h>
+
+#include "gws/SocketGatewayPeerVerifierFactory.h"
+
+using namespace BeeeOn;
+using namespace Poco;
+using namespace Poco::Net;
+
+SocketGatewayPeerVerifierFactory::~SocketGatewayPeerVerifierFactory()
+{
+}
+
+const SecureStreamSocket &SocketGatewayPeerVerifierFactory::asSecure(
+		const StreamSocket &socket)
+{
+	try {
+		return dynamic_cast<const SecureStreamSocket &>(socket);
+	}
+	catch (...) {
+		throw BadCastException("socket is not inherited from SecureStreamSocket");
+	}
+}
+
+SharedPtr<X509Certificate> SocketGatewayPeerVerifierFactory::extractPeerCert(
+		const StreamSocket &socket)
+{
+	SharedPtr<X509Certificate> certificate;
+
+	try {
+		const SecureStreamSocket &secureSocket = asSecure(socket);
+
+		if (!secureSocket.havePeerCertificate())
+			return certificate;
+
+		certificate = new X509Certificate(secureSocket.peerCertificate());
+	}
+	catch (const BadCastException &e) {
+	}
+
+	return certificate;
+}

src/gws/SocketGatewayPeerVerifierFactory.h
@@ -1,0 +1,51 @@
+#pragma once
+
+#include <Poco/SharedPtr.h>
+#include <Poco/Net/SecureStreamSocket.h>
+#include <Poco/Net/StreamSocket.h>
+#include <Poco/Net/X509Certificate.h>
+
+#include "gws/GatewayPeerVerifier.h"
+
+namespace BeeeOn {
+
+/**
+ * @brief Factory to create GatewayPeerVerifier instances based on StreamSockets.
+ */
+class SocketGatewayPeerVerifierFactory {
+public:
+	typedef Poco::SharedPtr<SocketGatewayPeerVerifierFactory> Ptr;
+
+	virtual ~SocketGatewayPeerVerifierFactory();
+
+	/**
+	 * Pre-verifies the given socket and creates an instance
+	 * of the GatewayPeerVerifier. The pre-verification can
+	 * e.g. test whether the given connection is secure or
+	 * whether it provides a peer certificate.
+	 *
+	 * @throws Poco::Net::NotAuthenticatedException if the pre-verification fails
+	 */
+	virtual GatewayPeerVerifier::Ptr preverifyAndCreate(
+			const Poco::Net::StreamSocket &socket) = 0;
+
+protected:
+	/**
+	 * Convert the given instance of StreamSocket into the SecureStreamSocket
+	 * if possible.
+	 *
+	 * @throws Poco::BadCastException if the StreamSocket cannot be casted
+	 */
+	static const Poco::Net::SecureStreamSocket &asSecure(
+			const Poco::Net::StreamSocket &socket);
+
+	/**
+	 * Extract the peer certificate from the given StreamSocket instance
+	 * if possible. It returns null result when the socket is not secure
+	 * or there is no peer certificate available.
+	 */
+	static Poco::SharedPtr<Poco::Net::X509Certificate> extractPeerCert(
+			const Poco::Net::StreamSocket &socket);
+};
+
+}

src/CMakeLists.txt
@@ -186,6 +186,7 @@ file(GLOB SOURCES
 	${PROJECT_SOURCE_DIR}/gws/GWMessageHandlerImpl.cpp
 	${PROJECT_SOURCE_DIR}/gws/GWResponseExpectedQueue.cpp
 	${PROJECT_SOURCE_DIR}/gws/RPCForwarder.cpp
+	${PROJECT_SOURCE_DIR}/gws/SocketGatewayPeerVerifierFactory.cpp
 	${PROJECT_SOURCE_DIR}/gws/WebSocketRequestHandler.cpp
 	${PROJECT_SOURCE_DIR}/gws/WebSocketServer.cpp
 	${PROJECT_SOURCE_DIR}/rpc/RPCConnector.cpp

commit 6b9c2a1337f82795eec19a501a2a57090c666b0a

SocketGatewayPeerVerifierFactory: fix method extractPeerCert
The original method extractPeerCert() assumed calling asSecure() to convert
a StreamSocket instance to SecureStreamSocket instance. This however cannot
work due to intensive using of copy-constructors without preserving the
original class type. Instead, the underlying SocketImpl pointer (which is
still polymorhic) is copied. Thus, the fixed implementation just ask for
the SocketImpl * and converts it to the SecureStreamSocketImpl *. The result
is much simplier :).

Fixes: abb76bc26dee ("SocketGatewayPeerVerifierFactory: new interface class")
Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

src/gws/SocketGatewayPeerVerifierFactory.cpp
@@ -1,4 +1,5 @@
 #include <Poco/Exception.h>
+#include <Poco/Net/SecureStreamSocketImpl.h>
 
 #include "gws/SocketGatewayPeerVerifierFactory.h"
 
@@ -10,32 +11,16 @@ SocketGatewayPeerVerifierFactory::~SocketGatewayPeerVerifierFactory()
 {
 }
 
-const SecureStreamSocket &SocketGatewayPeerVerifierFactory::asSecure(
-		const StreamSocket &socket)
-{
-	try {
-		return dynamic_cast<const SecureStreamSocket &>(socket);
-	}
-	catch (...) {
-		throw BadCastException("socket is not inherited from SecureStreamSocket");
-	}
-}
-
 SharedPtr<X509Certificate> SocketGatewayPeerVerifierFactory::extractPeerCert(
 		const StreamSocket &socket)
 {
 	SharedPtr<X509Certificate> certificate;
 
-	try {
-		const SecureStreamSocket &secureSocket = asSecure(socket);
+	SecureStreamSocketImpl *impl =
+		dynamic_cast<SecureStreamSocketImpl *>(socket.impl());
 
-		if (!secureSocket.havePeerCertificate())
-			return certificate;
-
-		certificate = new X509Certificate(secureSocket.peerCertificate());
-	}
-	catch (const BadCastException &e) {
-	}
+	if (impl && impl->havePeerCertificate())
+		certificate = new X509Certificate(impl->peerCertificate());
 
 	return certificate;
 }

src/gws/SocketGatewayPeerVerifierFactory.h
@@ -1,7 +1,6 @@
 #pragma once
 
 #include <Poco/SharedPtr.h>
-#include <Poco/Net/SecureStreamSocket.h>
 #include <Poco/Net/StreamSocket.h>
 #include <Poco/Net/X509Certificate.h>
 
@@ -30,15 +29,6 @@ public:
 			const Poco::Net::StreamSocket &socket) = 0;
 
 protected:
-	/**
-	 * Convert the given instance of StreamSocket into the SecureStreamSocket
-	 * if possible.
-	 *
-	 * @throws Poco::BadCastException if the StreamSocket cannot be casted
-	 */
-	static const Poco::Net::SecureStreamSocket &asSecure(
-			const Poco::Net::StreamSocket &socket);
-
 	/**
 	 * Extract the peer certificate from the given StreamSocket instance
 	 * if possible. It returns null result when the socket is not secure