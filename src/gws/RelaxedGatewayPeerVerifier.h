a270d953b62afd8d772c68c0cb94823c78ff7588
Jan Viktorin <iviktorin@fit.vutbr.cz>
2018-02-05 13:49:23 +0100

    RelaxedGatewayPeerVerifier: refactor to be only a wrapper
    The RelaxedGatewayPeerVerifier and its factory are now just wrappers.
    They wrap any other GatewayPeerVerifier and SocketGatewayPeerVerifierFactory
    and relax their verification operations to be logging-only and accepting all
    peers.
    
    This way, it is possible to test any GatewayPeerVerifier without
    accidently denying access for gateways. The relaxed implementation is
    considered for testing purposes only.
    
    Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

--- src/gws/RelaxedGatewayPeerVerifier.h
+++ src/gws/RelaxedGatewayPeerVerifier.h
@@ -3,44 +3,47 @@
 #include <Poco/SharedPtr.h>
 #include <Poco/Net/X509Certificate.h>
 
-#include "gws/GatewayPeerVerifier.h"
 #include "gws/SocketGatewayPeerVerifierFactory.h"
 #include "util/Loggable.h"
 
 namespace BeeeOn {
 
 /**
- * @brief The GatewayPeerVerifier implementation that never fails and thus accepts
- * all peers with or without a ceritifate. The implementation is intended for
- * testing.
+ * @brief WRapper around any GatewayPeerVerifier implementation. It relaxes
+ * verification process of the underlying verifier to just logging its behaviour.
+ * It accepts all gateway peers. The implementation is intended for testing.
  */
 class RelaxedGatewayPeerVerifier : public GatewayPeerVerifier, Loggable {
 public:
-	RelaxedGatewayPeerVerifier(Poco::SharedPtr<Poco::Net::X509Certificate> peerCertificate);
+	RelaxedGatewayPeerVerifier(GatewayPeerVerifier::Ptr verifier);
 
 	/**
-	 * Try to verify the peer certificate against the given gateway and report
-	 * details and success of the operation. The implementation does not throw
-	 * exceptions in case of a failure (accepts all peers).
+	 * Use another GatewayPeerVerifier instance to verify the gateway but
+	 * never fail (accept all peers).
 	 */
 	void verifyPeer(const Gateway &gateway) const override;
 
 private:
-	Poco::SharedPtr<Poco::Net::X509Certificate> m_peerCertificate;
+	GatewayPeerVerifier::Ptr m_verifier;
 };
 
 /**
- * @brief The SocketGatewayPeerVerifierFactory implementation designed to accept all
- * secured or insecured peers. The pre-verification just reports the details and success
- * of the operation.
+ * @brief The SocketGatewayPeerVerifierFactory implementation designed to wrap another
+ * SocketGatewayPeerVerifierFactory. If the underlying preverifyAndCreate() call fails,
+ * it using the AcceptingGatewayPeerVerifier instance instead to accept all gateway peers.
  */
 class RelaxedGatewayPeerVerifierFactory : public SocketGatewayPeerVerifierFactory, Loggable {
 public:
+	RelaxedGatewayPeerVerifierFactory();
+
 	GatewayPeerVerifier::Ptr preverifyAndCreate(
 			const Poco::Net::StreamSocket &socket) override;
-protected:
-	Poco::SharedPtr<Poco::Net::X509Certificate> preverify(
-			const Poco::Net::StreamSocket &socket) const;
+
+	void setVerifierFactory(SocketGatewayPeerVerifierFactory::Ptr factory);
+
+private:
+	SocketGatewayPeerVerifierFactory::Ptr m_factory;
+	GatewayPeerVerifier::Ptr m_acceptingVerifier;
 };
 
 }
