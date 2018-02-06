BeeeOn/server

commit 868c654b2180633cd84105ddaf36052279207e09

X509GatewayPeerVerifier: new implementation
Implement the common verification procedure of gateways by using
the X509 certificates.

Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

src/gws/X509GatewayPeerVerifier.h
@@ -1,0 +1,63 @@
+#pragma once
+
+#include <Poco/DateTime.h>
+#include <Poco/SharedPtr.h>
+#include <Poco/Net/StreamSocket.h>
+#include <Poco/Net/X509Certificate.h>
+
+#include "gws/GatewayPeerVerifier.h"
+#include "gws/SocketGatewayPeerVerifierFactory.h"
+#include "model/Gateway.h"
+#include "util/Loggable.h"
+
+namespace BeeeOn {
+
+/**
+ * @brief Implementation of the GatewayPeerVerifier interface using
+ * the X509 certificates received via SSL/TLS layer.
+ */
+class X509GatewayPeerVerifier :
+	public GatewayPeerVerifier,
+	Loggable {
+public:
+	typedef Poco::SharedPtr<X509GatewayPeerVerifier> Ptr;
+
+	X509GatewayPeerVerifier(const Poco::Net::X509Certificate &cert);
+
+	/**
+	 * @brief Verify that the given gateways'ID match the CN field of
+	 * the underlying certificate.
+	 *
+	 * @throws Poco::Net::NotAuthenticatedException if the peer certificate is missing
+	 * @throws Poco::Net::CertificateValidationException if the gateway ID and cert CN do not match
+	 */
+	void verifyPeer(const Gateway &gateway) const override;
+
+	/**
+	 * @return the underlying X509 certificate.
+	 */
+	const Poco::Net::X509Certificate &certificate() const;
+
+	/**
+	 * @returns Gateway ID extracted from the CN of the underlying X509 certificate.
+	 */
+	GatewayID extractID() const;
+
+	static std::string formatDT(const Poco::DateTime &dt);
+	static std::string formatCert(const Poco::Net::X509Certificate &cert);
+
+private:
+	const Poco::Net::X509Certificate m_peerCertificate;
+};
+
+class X509GatewayPeerVerifierFactory : public SocketGatewayPeerVerifierFactory, Loggable {
+public:
+	/**
+	 * @returns X509GatewayPeerVerifier instance from for given socket that is
+	 * expected to be a TLS/SSL socket.
+	 */
+	GatewayPeerVerifier::Ptr preverifyAndCreate(
+			const Poco::Net::StreamSocket &socket) override;
+};
+
+}

conf/config.d/factory-gws.xml
@@ -48,6 +48,9 @@
 
 		<alias name="gatewayPeerVerifierFactory" ref="${gws.peer.verifier}GatewayPeerVerifierFactory" />
 
+		<instance name="strictGatewayPeerVerifierFactory"
+			class="BeeeOn::X509GatewayPeerVerifierFactory" />
+
 		<instance name="relaxedGatewayPeerVerifierFactory"
 			class="BeeeOn::RelaxedGatewayPeerVerifierFactory" />
 	</factory>

conf/config.d/logging.ini
@@ -320,6 +320,12 @@ loggers.WebSocketServer.level = ${logging.default.level}
 loggers.WebSocketRequestHandler.name = BeeeOn::WebSocketRequestHandler
 loggers.WebSocketRequestHandler.level = ${logging.default.level}
 
+loggers.X509GatewayPeerVerifierFactory.name = BeeeOn::X509GatewayPeerVerifierFactory
+loggers.X509GatewayPeerVerifierFactory.level = ${logging.default.level}
+
+loggers.X509GatewayPeerVerifier.name = BeeeOn::X509GatewayPeerVerifier
+loggers.X509GatewayPeerVerifier.level = ${logging.default.level}
+
 loggers.RelaxedGatewayPeerVerifierFactory.name = BeeeOn::RelaxedGatewayPeerVerifierFactory
 loggers.RelaxedGatewayPeerVerifierFactory.level = ${logging.default.level}
 

src/CMakeLists.txt
@@ -190,6 +190,7 @@ file(GLOB SOURCES
 	${PROJECT_SOURCE_DIR}/gws/SocketGatewayPeerVerifierFactory.cpp
 	${PROJECT_SOURCE_DIR}/gws/WebSocketRequestHandler.cpp
 	${PROJECT_SOURCE_DIR}/gws/WebSocketServer.cpp
+	${PROJECT_SOURCE_DIR}/gws/X509GatewayPeerVerifier.cpp
 	${PROJECT_SOURCE_DIR}/rpc/RPCConnector.cpp
 	${PROJECT_SOURCE_DIR}/rpc/SocketRPCConnector.cpp
 	${PROJECT_SOURCE_DIR}/rpc/GatewayRPC.cpp

src/gws/X509GatewayPeerVerifier.cpp
@@ -1,0 +1,119 @@
+#include <Poco/DateTimeFormat.h>
+#include <Poco/DateTimeFormatter.h>
+#include <Poco/Logger.h>
+#include <Poco/Net/NetException.h>
+#include <Poco/Net/SecureStreamSocket.h>
+#include <Poco/Net/SecureStreamSocketImpl.h>
+#include <Poco/Net/SSLException.h>
+
+#include "di/Injectable.h"
+#include "gws/X509GatewayPeerVerifier.h"
+#include "ssl/X509Fingerprint.h"
+
+BEEEON_OBJECT_BEGIN(BeeeOn, X509GatewayPeerVerifierFactory)
+BEEEON_OBJECT_CASTABLE(SocketGatewayPeerVerifierFactory)
+BEEEON_OBJECT_END(BeeeOn, X509GatewayPeerVerifierFactory)
+
+using namespace std;
+using namespace Poco;
+using namespace Poco::Net;
+using namespace BeeeOn;
+
+X509GatewayPeerVerifier::X509GatewayPeerVerifier(
+		const X509Certificate &cert):
+	m_peerCertificate(cert)
+{
+}
+
+const X509Certificate &X509GatewayPeerVerifier::certificate() const
+{
+	return m_peerCertificate;
+}
+
+GatewayID X509GatewayPeerVerifier::extractID() const
+{
+	return GatewayID::parse(m_peerCertificate.commonName());
+}
+
+string X509GatewayPeerVerifier::formatDT(const DateTime &dt)
+{
+	return DateTimeFormatter::format(dt, DateTimeFormat::ISO8601_FORMAT);
+}
+
+string X509GatewayPeerVerifier::formatCert(const X509Certificate &cert)
+{
+	return "CN: " + cert.commonName() + "\n"
+		+ "Issuer: " + cert.issuerName() + "\n"
+		+ "Subject: " + cert.subjectName() + "\n"
+		+ "Valid from: " + formatDT(cert.validFrom()) + "\n"
+		+ "Expires on: " + formatDT(cert.expiresOn()) + "\n"
+		+ "SHA256: " + X509Fingerprint(&cert).digestToHex("SHA256");
+}
+
+void X509GatewayPeerVerifier::verifyPeer(const Gateway &gateway) const
+{
+	if (logger().debug()) {
+		logger().debug("verifying certificate:\n"
+				+ formatCert(certificate()),
+				__FILE__, __LINE__);
+	}
+
+	const auto &id = extractID();
+	if (id != gateway.id()) {
+		throw CertificateValidationException(
+			"ID "
+			+ id.toString()
+			+ " does not match expected "
+			+ gateway.id().toString());
+	}
+}
+
+GatewayPeerVerifier::Ptr X509GatewayPeerVerifierFactory::preverifyAndCreate(
+		const StreamSocket &socket)
+{
+	if (!socket.secure()) {
+		throw InvalidSocketException(
+			"socket "
+			+ socket.peerAddress().toString()
+			+ " is not secure");
+	}
+
+	SecureStreamSocketImpl *impl =
+		dynamic_cast<SecureStreamSocketImpl *>(socket.impl());
+	if (!impl) {
+		throw InvalidSocketException(
+			"socket impl is not SecureStreamSocketImpl");
+	}
+
+	if (!impl->havePeerCertificate()) {
+		throw InvalidCertificateException(
+			"no peer certificate for "
+			+ socket.peerAddress().toString());
+	}
+
+	const X509Certificate &cert = impl->peerCertificate();
+
+	const DateTime now;
+
+	try {
+		if (cert.expiresOn() < now) {
+			throw InvalidCertificateException(
+				"certificate has expired on: "
+				+ X509GatewayPeerVerifier::formatDT(cert.expiresOn()));
+		}
+
+		if (cert.validFrom() > now) {
+			throw InvalidCertificateException(
+				"certificate is not valid yet: "
+				+ X509GatewayPeerVerifier::formatDT(cert.validFrom()));
+		}
+	}
+	catch (const Exception &e) {
+		logger().error("failing peer certificate:\n"
+				+ X509GatewayPeerVerifier::formatCert(cert),
+				__FILE__, __LINE__);
+		e.rethrow();
+	}
+
+	return new X509GatewayPeerVerifier(cert);
+}

commit bcbe4ecba89daf3aab6a944ea39a8934353e456e

X509GatewayPeerVerifier: fix comment typo
Fixes: 868c654b2180 ("X509GatewayPeerVerifier: new implementation")
Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

src/gws/X509GatewayPeerVerifier.h
@@ -25,7 +25,7 @@ public:
 	X509GatewayPeerVerifier(const Poco::Net::X509Certificate &cert);
 
 	/**
-	 * @brief Verify that the given gateways'ID match the CN field of
+	 * @brief Verify that the given gateway's ID match the CN field of
 	 * the underlying certificate.
 	 *
 	 * @throws Poco::Net::NotAuthenticatedException if the peer certificate is missing