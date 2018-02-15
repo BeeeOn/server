BeeeOn/server

commit 8b74394796f5d3f29a5e2626c2258f6b91600352

OAuth2AuthProvider: refactoring of HTTP methods
The convertResponseToString() method is removed because the HTTPEntireResponse
class can convert the response to a string. Also the connectSecure() method
is deleted because the connection is created in method HTTPUtil::makeRequest().
It is not necessary to set URI to request because the HTTPUtil's methods take care
about it. Simplify the makeRequest() method that only uses HTTP::makeRequest()
to send the request.

Signed-off-by: David Bednařík <xbedna62@stud.fit.vutbr.cz>
Reviewed-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

src/provider/OAuth2AuthProvider.cpp
@@ -7,13 +7,12 @@
 #include <Poco/Net/InvalidCertificateHandler.h>
 #include <Poco/Net/AcceptCertificateHandler.h>
 #include <Poco/Net/HTMLForm.h>
-#include <Poco/Net/HTTPResponse.h>
 #include <Poco/Net/HTTPRequest.h>
-#include <Poco/Net/HTTPSClientSession.h>
 #include <Poco/Net/NetException.h>
 #include <Poco/StreamCopier.h>
 #include <Poco/URI.h>
 
+#include "net/HTTPUtil.h"
 #include "provider/OAuth2AuthProvider.h"
 #include "ssl/SSLClient.h"
 
@@ -65,25 +64,8 @@ void OAuth2AuthProvider::initSSL() const
 				"missing sslConfig, cannot use OAuth");
 }
 
-SharedPtr<HTTPSClientSession> OAuth2AuthProvider::connectSecure(
-		const std::string &host,
-		unsigned int port) const
+string OAuth2AuthProvider::handleResponse(HTTPEntireResponse &response) const
 {
-	try {
-		initSSL();
-		return new HTTPSClientSession(
-				host, port, m_sslConfig->context());
-	} catch (const Exception &e) {
-		logger().log(e, __FILE__, __LINE__);
-		throw;
-	}
-}
-
-string OAuth2AuthProvider::handleResponse(HTTPSClientSession &session) const
-{
-	HTTPResponse response;
-	istream &rs = session.receiveResponse(response);
-
 	if (logger().debug()) {
 		logger().debug("response status: "
 			+ to_string(response.getStatus())
@@ -91,22 +73,18 @@ string OAuth2AuthProvider::handleResponse(HTTPSClientSession &session) const
 			+ response.getReason(), __FILE__, __LINE__);
 	}
 
-	string receiveResponse = convertResponseToString(rs);
-
 	if (logger().debug()) {
 		logger().debug("response: "
-			+ receiveResponse, __FILE__, __LINE__);
+			+ response.getBody(), __FILE__, __LINE__);
 	}
 
-	return receiveResponse;
+	return response.getBody();
 }
 
 string OAuth2AuthProvider::makeRequest(const string &method, URI &host, HTMLForm &requestForm) const
 {
-	SharedPtr<HTTPSClientSession>session;
-	session = connectSecure(host.getHost(), host.getPort());
-
-	HTTPRequest request(method, host.getPathAndQuery(), HTTPMessage::HTTP_1_1);
+	HTTPRequest request(HTTPMessage::HTTP_1_1);
+	request.setMethod(method);
 	requestForm.prepareSubmit(request);
 
 	ostringstream requestQuery;
@@ -115,19 +93,15 @@ string OAuth2AuthProvider::makeRequest(const string &method, URI &host, HTMLForm
 	if (logger().debug())
 		logger().debug("request: " + requestQuery.str(), __FILE__, __LINE__);
 
-	if (method == HTTPRequest::HTTP_POST)
-		session->sendRequest(request) << requestQuery.str();
-	else
-		session->sendRequest(request);
+	try {
+		initSSL();
+	} catch (const Exception &e) {
+		logger().log(e, __FILE__, __LINE__);
+		throw;
+	}
 
-	return handleResponse(*session);
-}
+	HTTPEntireResponse response = HTTPUtil::makeRequest(
+		request, host, requestQuery.str(), m_sslConfig);
 
-string OAuth2AuthProvider::convertResponseToString(istream &rs) const
-{
-	string response;
-	stringstream ss;
-	StreamCopier::copyToString(rs, response);
-
-	return response;
+	return handleResponse(response);
 }

src/provider/OAuth2AuthProvider.h
@@ -4,6 +4,7 @@
 #include <Poco/SharedPtr.h>
 #include <Poco/Net/Context.h>
 
+#include "net/HTTPEntireResponse.h"
 #include "provider/AuthProvider.h"
 
 namespace Poco {
@@ -11,7 +12,6 @@ class URI;
 namespace Net {
 
 class HTMLForm;
-class HTTPSClientSession;
 
 }
 }
@@ -76,21 +76,9 @@ protected:
 			AuthResult &info);
 
 	/**
-	 * Open HTTPS connection to the given host:port.
+	 * Handle response.
 	 */
-	Poco::SharedPtr<Poco::Net::HTTPSClientSession> connectSecure(
-			const std::string &host,
-			unsigned int port) const;
-
-	/**
-	 * Handle response based on given session.
-	 */
-	std::string handleResponse(Poco::Net::HTTPSClientSession &session) const;
-
-	/**
-	 * Convert istream response body to string
-	 */
-	std::string convertResponseToString(std::istream &rs) const;
+	std::string handleResponse(HTTPEntireResponse &session) const;
 
 	/**
 	 * Creates session and makes on method based request and returns handled response

commit f32ce966e9b4905b5dd6b149747a6e442642a6d1

OAuth2AuthProvider: fix setting URI to request
The wrong URI setting to the HTTP request is fixed. The bug caused bad response
because the request had empty URI.

Fixes: 8b743947 ("OAuth2AuthProvider: refactoring of HTTP methods")

Signed-off-by: David Bednařík <xbedna62@stud.fit.vutbr.cz>
Reviewed-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

src/provider/OAuth2AuthProvider.cpp
@@ -83,8 +83,7 @@ string OAuth2AuthProvider::handleResponse(HTTPEntireResponse &response) const
 
 string OAuth2AuthProvider::makeRequest(const string &method, URI &host, HTMLForm &requestForm) const
 {
-	HTTPRequest request(HTTPMessage::HTTP_1_1);
-	request.setMethod(method);
+	HTTPRequest request(method, host.getPathAndQuery(), HTTPMessage::HTTP_1_1);
 	requestForm.prepareSubmit(request);
 
 	ostringstream requestQuery;