BeeeOn/server

commit 5754729a1578acb42352163dac214be3e0af7f4f

SensorXmlHandler: implement setstate
Implement XML-UI message:

* ns="devices" type="setstate"

Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

src/xmlui/SensorXmlHandler.cpp
@@ -5,6 +5,7 @@
 #include "di/Injectable.h"
 #include "xmlui/SensorXmlHandler.h"
 #include "xmlui/XmlValueConsumer.h"
+#include "model/Control.h"
 #include "model/Device.h"
 #include "model/ModuleInfo.h"
 #include "service/Relation.h"
@@ -21,9 +22,13 @@ using namespace BeeeOn::XmlUI;
 SensorXmlHandler::SensorXmlHandler(const StreamSocket &socket,
 		const AutoPtr<Document> input,
 		Session::Ptr session,
-		SensorHistoryService &sensorService):
+		ControlService &controlService,
+		SensorHistoryService &sensorService,
+		const Timespan &setStateTimeout):
 	SessionXmlHandler("devices", socket, input, session),
-	m_sensorService(sensorService)
+	m_controlService(controlService),
+	m_sensorService(sensorService),
+	m_setStateTimeout(setStateTimeout)
 {
 }
 
@@ -40,6 +45,10 @@ void SensorXmlHandler::handleInputImpl()
 		Element *logsNode = root->getChildElement("logs");
 		handleGetLog(gateid, logsNode);
 		return;
+	}
+	else if (type == "setstate") {
+		Element *deviceNode = root->getChildElement("device");
+		handleSetState(gateid, deviceNode);
 	}
 
 	resultInvalidInput();
@@ -79,15 +88,46 @@ void SensorXmlHandler::handleGetLog(const string &gateid,
 	resultDataEnd();
 }
 
-SensorXmlHandlerResolver::SensorXmlHandlerResolver():
-	SessionXmlHandlerResolver("devices")
+void SensorXmlHandler::handleSetState(const string &gateid,
+		Element *deviceNode)
 {
+	Gateway gateway(GatewayID::parse(gateid));
+	Device device(DeviceID::parse(deviceNode->getAttribute("euid")));
+	device.setGateway(gateway);
+	Control control;
+	control.setId(ControlID::parse(deviceNode->getAttribute("moduleid")));
+
+	const double value = NumberParser::parseFloat(deviceNode->getAttribute("value"));
+
+	Relation<Control, Device> input(control, device);
+	User user(session()->userID());
+	input.setUser(user);
+
+	m_controlService.requestChange(input, value, m_setStateTimeout);
+	resultSuccess();
+}
+
+SensorXmlHandlerResolver::SensorXmlHandlerResolver():
+	SessionXmlHandlerResolver("devices"),
+	m_setStateTimeout(25 * Timespan::SECONDS)
+{
+}
+
+void SensorXmlHandlerResolver::setControlService(
+		ControlService::Ptr service)
+{
+	m_controlService = service;
 }
 
 void SensorXmlHandlerResolver::setSensorHistoryService(
 		SensorHistoryService::Ptr service)
 {
 	m_sensorService = service;
+}
+
+void SensorXmlHandlerResolver::setSetStateTimeout(const Timespan &timeout)
+{
+	m_setStateTimeout = timeout;
 }
 
 bool SensorXmlHandlerResolver::canHandle(
@@ -100,6 +140,8 @@ bool SensorXmlHandlerResolver::canHandle(
 
 	if (type == "getlog")
 		return true;
+	if (type == "setstate")
+		return true;
 
 	return false;
 }
@@ -111,13 +153,18 @@ XmlRequestHandler *SensorXmlHandlerResolver::createHandler(
 	Session::Ptr session = lookupSession(
 			*m_sessionManager, input);
 	return new SensorXmlHandler(
-			socket, input, session, *m_sensorService);
+			socket, input, session,
+			*m_controlService,
+			*m_sensorService,
+			m_setStateTimeout);
 }
 
 BEEEON_OBJECT_BEGIN(BeeeOn, XmlUI, SensorXmlHandlerResolver)
 BEEEON_OBJECT_CASTABLE(SessionXmlHandlerResolver)
 BEEEON_OBJECT_CASTABLE(AbstractXmlHandlerResolver)
 BEEEON_OBJECT_CASTABLE(XmlRequestHandlerResolver)
+BEEEON_OBJECT_REF("controlService", &SensorXmlHandlerResolver::setControlService)
 BEEEON_OBJECT_REF("sensorHistoryService", &SensorXmlHandlerResolver::setSensorHistoryService)
 BEEEON_OBJECT_REF("sessionManager", &SensorXmlHandlerResolver::setSessionManager)
+BEEEON_OBJECT_TIME("setStateTimeout", &SensorXmlHandlerResolver::setSetStateTimeout)
 BEEEON_OBJECT_END(BeeeOn, XmlUI, SensorXmlHandlerResolver)

conf/config.d/factory-xmlui.xml
@@ -55,8 +55,10 @@
 
 		<instance name="sensorXmlHandler"
 				class="BeeeOn::XmlUI::SensorXmlHandlerResolver">
+			<set name="controlService" ref="controlService" />
 			<set name="sensorHistoryService" ref="sensorHistoryService" />
 			<set name="sessionManager" ref="sessionManager" />
+			<set name="setStateTimeout" time="25 s" />
 		</instance>
 
 		<instance name="profileXmlHandler"

src/xmlui/SensorXmlHandler.h
@@ -1,6 +1,9 @@
 #ifndef BEEEON_XMLUI_SENSOR_XML_HANDLER_H
 #define BEEEON_XMLUI_SENSOR_XML_HANDLER_H
 
+#include <Poco/Timespan.h>
+
+#include "service/ControlService.h"
 #include "service/SensorHistoryService.h"
 
 #include "xmlui/SessionXmlHandler.h"
@@ -14,14 +17,20 @@ public:
 	SensorXmlHandler(const Poco::Net::StreamSocket &socket,
 			const Poco::AutoPtr<Poco::XML::Document> input,
 			BeeeOn::Session::Ptr session,
-			SensorHistoryService &sensorService);
+			ControlService &controlService,
+			SensorHistoryService &sensorService,
+			const Poco::Timespan &setStateTimeout);
 	void handleInputImpl() override;
 
 	void handleGetLog(const std::string &gateid,
 			Poco::XML::Element *logsNode);
+	void handleSetState(const std::string &gateid,
+			Poco::XML::Element *deviceNode);
 
 private:
+	ControlService &m_controlService;
 	SensorHistoryService &m_sensorService;
+	Poco::Timespan m_setStateTimeout;
 };
 
 class SensorXmlHandlerResolver : public SessionXmlHandlerResolver {
@@ -33,7 +42,9 @@ public:
 			const Poco::Net::StreamSocket &socket,
 			const Poco::AutoPtr<Poco::XML::Document> input);
 
+	void setControlService(ControlService::Ptr service);
 	void setSensorHistoryService(SensorHistoryService::Ptr service);
+	void setSetStateTimeout(const Poco::Timespan &timeout);
 
 	void setSessionManager(BeeeOn::SessionManager *manager)
 	{
@@ -41,8 +52,10 @@ public:
 	}
 
 private:
+	ControlService::Ptr m_controlService;
 	SensorHistoryService::Ptr m_sensorService;
 	BeeeOn::SessionManager *m_sessionManager;
+	Poco::Timespan m_setStateTimeout;
 };
 
 }

commit 6aca6ada0f2886b37b9d9b719710b135b9b62900

SensorXmlHandler: fix result of setstate
The omitted 'return' was appending error response to the result.
There were two messages:

 <response ns="devices" result="ok" type="setstate" version="1.0.0"/><response
 errcode="998" ns="devices" result="error" type="setstate" version="1.0.0"/>

Fixes: 5754729a1578 ("SensorXmlHandler: implement setstate")
Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

src/xmlui/SensorXmlHandler.cpp
@@ -49,6 +49,7 @@ void SensorXmlHandler::handleInputImpl()
 	else if (type == "setstate") {
 		Element *deviceNode = root->getChildElement("device");
 		handleSetState(gateid, deviceNode);
+		return;
 	}
 
 	resultInvalidInput();