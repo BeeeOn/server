BeeeOn/server

commit 0cc3b6af80769f3a149e91d4c9c63ab134626a35

xmlui: Serializing: each module tells its controllable state
Each module now holds information whether it can be controlled by user.
This is represented by attribute actuator="yes". For non-controllable
modules, the attribute is omitted.

Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

src/xmlui/Serializing.cpp
@@ -203,6 +203,9 @@ void BeeeOn::XmlUI::serialize(Poco::XML::XMLWriter &output,
 		if (!module.group().empty())
 			attrs.addAttribute("", "group", "group", "", module.group());
 
+		if (module.isControllable())
+			attrs.addAttribute("", "actuator", "actuator", "", "yes");
+		
 		output.emptyElement("", "module", "module", attrs);
 	}
 

commit 1f917d103e135af7da28ccd8473155abd86f2338

xmlui: Serializing: fix redundant whitespace
Fixes: 0cc3b6af8076 ("xmlui: Serializing: each module tells...")
Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

src/xmlui/Serializing.cpp
@@ -252,7 +252,7 @@ void BeeeOn::XmlUI::serialize(Poco::XML::XMLWriter &output,
 
 		if (module.isControllable())
 			attrs.addAttribute("", "actuator", "actuator", "", "yes");
-		
+
 		output.emptyElement("", "module", "module", attrs);
 	}
 