BeeeOn/server

commit ebe905a97ffbd743700c3660de9ee7ed66545cf6

FilesystemQueryLoader: new class
FilesystemQueryLoader loads query from a file in filesystem with given
extension.

Example:

- mysql/users/find-by-id.sql
- mysql/users/insert.sql
- pgsql/users/find-by-id.sql
- pgsql/users/insert.sql

The queries are addressable as e.g. "user.find-by-id". The target
database is selected via setRootPath() and extension is selected by setExtension().

Signed-off-by: Petr Kohout <xkohou14@stud.fit.vutbr.cz>
Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

src/dao/FilesystemQueryLoader.cpp
@@ -1,0 +1,96 @@
+#include <Poco/DirectoryIterator.h>
+#include <Poco/File.h>
+#include <Poco/FileStream.h>
+#include <Poco/RegularExpression.h>
+#include <Poco/StreamCopier.h>
+#include <Poco/StringTokenizer.h>
+
+#include "di/Injectable.h"
+#include "dao/FilesystemQueryLoader.h"
+
+using namespace std;
+using namespace Poco;
+using namespace BeeeOn;
+
+BEEEON_OBJECT_BEGIN(BeeeOn, FilesystemQueryLoader)
+BEEEON_OBJECT_CASTABLE(QueryLoader)
+BEEEON_OBJECT_PROPERTY("rootPath", &FilesystemQueryLoader::setRootPath)
+BEEEON_OBJECT_PROPERTY("extension", &FilesystemQueryLoader::setExtension)
+BEEEON_OBJECT_PROPERTY("removeComments", &FilesystemQueryLoader::setRemoveComments)
+BEEEON_OBJECT_PROPERTY("removeWhitespace", &FilesystemQueryLoader::setRemoveWhitespace)
+BEEEON_OBJECT_PROPERTY("preserveUnneededLines", &FilesystemQueryLoader::setPreserveUnneededLines)
+BEEEON_OBJECT_END(BeeeOn, FilesystemQueryLoader)
+
+FilesystemQueryLoader::FilesystemQueryLoader()
+{
+	m_pre.setRemoveComments(true);
+	m_pre.setRemoveWhitespace(true);
+	m_pre.setPreserveUnneededLines(false);
+}
+
+FilesystemQueryLoader::~FilesystemQueryLoader()
+{
+}
+
+string FilesystemQueryLoader::find(const string &key) const
+{
+	string query;
+	Path p(keyAsFile(key));
+
+	try {
+		Poco::FileInputStream istr(p.toString());
+		StreamCopier::copyToString(istr, query);
+	} catch (const Exception &e) {
+		throw NotFoundException("failed to read query " + key, e);
+	}
+
+	return m_pre.process(query);
+}
+
+void FilesystemQueryLoader::setRemoveComments(bool remove)
+{
+	m_pre.setRemoveComments(remove);
+}
+
+void FilesystemQueryLoader::setRemoveWhitespace(bool remove)
+{
+	m_pre.setRemoveWhitespace(remove);
+}
+
+void FilesystemQueryLoader::setPreserveUnneededLines(bool preserve)
+{
+	m_pre.setPreserveUnneededLines(preserve);
+}
+
+void FilesystemQueryLoader::setRootPath(const string &rootPath)
+{
+	m_rootPath = Path(rootPath);
+}
+
+void FilesystemQueryLoader::setExtension(const string &extension)
+{
+	m_extension = extension;
+}
+
+Path FilesystemQueryLoader::keyAsFile(const string& key) const
+{
+	StringTokenizer tokenizer(key, ".",
+		StringTokenizer::TOK_IGNORE_EMPTY | StringTokenizer::TOK_TRIM);
+	size_t parts = tokenizer.count();
+
+	Path result(m_rootPath);
+
+	for (const auto &fragment : tokenizer) {
+		if (parts == 1) // last part of path
+			result.setFileName(fragment);
+		else
+			result.pushDirectory(fragment);
+
+		parts--;
+	}
+
+	result.setExtension(m_extension);
+
+	return result;
+}
+

src/CMakeLists.txt
@@ -117,6 +117,7 @@ file(GLOB SOURCES
 	${PROJECT_SOURCE_DIR}/dao/poco/PocoSQLSensorHistoryDao.cpp
 	${PROJECT_SOURCE_DIR}/dao/AbstractConfigurationQueryLoader.cpp
 	${PROJECT_SOURCE_DIR}/dao/ConnectorLoader.cpp
+	${PROJECT_SOURCE_DIR}/dao/FilesystemQueryLoader.cpp
 	${PROJECT_SOURCE_DIR}/dao/IniFileQueryLoader.cpp
 	${PROJECT_SOURCE_DIR}/dao/LoggingQueryProfiler.cpp
 	${PROJECT_SOURCE_DIR}/dao/Query.cpp

src/dao/FilesystemQueryLoader.h
@@ -1,0 +1,85 @@
+#pragma once
+
+#include <Poco/AutoPtr.h>
+#include <Poco/Path.h>
+
+#include "dao/QueryLoader.h"
+#include "util/SQLPreprocessor.h"
+
+namespace BeeeOn {
+
+/**
+ * @brief FilesystemQueryLoader is an implementation of the QueryLoader
+ * that reads queries from files. Each query is expected to be stored
+ * in a single file.
+ *
+ * Queries are identified by keys. A key is a dot-separated string.
+ * The key is mapped into a file by replacing dots with path separators.
+ * A file extension can be configured to be appended for the lookup.
+ *
+ * All queries are expected to be placed into a directory or a hierarchy
+ * of directories. The lookup is always performed from the
+ * configured root path.
+ */
+class FilesystemQueryLoader: public QueryLoader {
+public:
+	FilesystemQueryLoader();
+	virtual ~FilesystemQueryLoader();
+
+	std::string find(const std::string &key) const;
+
+	/**
+	 * @brief Configure to discard comments from all queries.
+	 * This feature is enabled by default.
+	 */
+	void setRemoveComments(bool remove);
+
+	/**
+	 * @brief Configure to discard unneeded whitespace from all queries.
+	 * This feature is enabled by default.
+	 */
+	void setRemoveWhitespace(bool remove);
+
+	/**
+	 * @brief Configure behaviour while loading queries with comments. If
+	 * set to true, the comments are removed but empty lines are preserved
+	 * for reference. Otherwise, all comments are removed.
+	 */
+	void setPreserveUnneededLines(bool preserve);
+
+	/**
+	 * @brief Configure root path to denote where to load queries from.
+	 * Example: for rootPath <code>~/src/</code> and key <code>database.server.select</code>,
+	 * the resulted dir for lookup of the query would be <code>~/src/database/server</code>.
+	 */
+	void setRootPath(const std::string &rootPath);
+
+	/**
+	 * @brief Configure extension of the files containing queries.
+	 *
+	 * Given a key <code>database.server.select</code> and extension
+	 * <code>sql</code>, the query in file
+	 * <code>$rootPath/database/server/select.sql</code> would be loaded.
+	 *
+	 * If the extension is set to an empty string, no extension is
+	 * considered. In that case,
+	 * <code>$rootPath/database/server/select<code> would be loaded.
+	 */
+	void setExtension(const std::string &extension);
+
+private:
+	/**
+	 * Convert key <code>database.server.select</code> into the appropriate
+	 * file path by considering the rootPath and extension.
+	 */
+	Poco::Path keyAsFile(const std::string& key) const;
+
+private:
+	Poco::Path m_rootPath;
+	std::string m_extension;
+	SQLPreprocessor m_pre;
+	bool m_preserveUnneededLines;
+};
+
+}
+

test/CMakeLists.txt
@@ -17,6 +17,7 @@ find_library (JANSSON jansson)
 find_library (ZEROMQ zmq)
 
 file(GLOB TEST_SOURCES
+	${PROJECT_SOURCE_DIR}/dao/FilesystemQueryLoaderTest.cpp
 	${PROJECT_SOURCE_DIR}/fcm/FCMSenderTest.cpp
 	${PROJECT_SOURCE_DIR}/fcm/LegacyFCMClientTest.cpp
 	${PROJECT_SOURCE_DIR}/gws/X509GatewayPeerVerifierTest.cpp

test/dao/FilesystemQueryLoaderTest.cpp
@@ -1,0 +1,259 @@
+#include <cppunit/extensions/HelperMacros.h>
+
+#include <Poco/Environment.h>
+
+#include "cppunit/BetterAssert.h"
+#include "dao/FilesystemQueryLoader.h"
+
+using namespace std;
+using namespace Poco;
+
+namespace BeeeOn {
+
+class FilesystemQueryLoaderTest : public CppUnit::TestFixture {
+	CPPUNIT_TEST_SUITE(FilesystemQueryLoaderTest);
+	CPPUNIT_TEST(testEmpty);
+	CPPUNIT_TEST(testWhitespace);
+	CPPUNIT_TEST(testComments);
+	CPPUNIT_TEST(testCommentsAndWhitespace);
+	CPPUNIT_TEST(testQuerySingleLine);
+	CPPUNIT_TEST(testQueryMultiLine);
+	CPPUNIT_TEST(testQueryWithComments);
+	CPPUNIT_TEST(testQueryWithCommentsAndWhitespace);
+	CPPUNIT_TEST(testQueryEndingWithWhitespace);
+	CPPUNIT_TEST(testLoadWithExtension);
+	CPPUNIT_TEST(testNotFound);
+	CPPUNIT_TEST_SUITE_END();
+
+public:
+	void setUp();
+
+	void testEmpty();
+	void testWhitespace();
+	void testComments();
+	void testCommentsAndWhitespace();
+	void testQuerySingleLine();
+	void testQueryMultiLine();
+	void testQueryWithComments();
+	void testQueryWithCommentsAndWhitespace();
+	void testQueryEndingWithWhitespace();
+	void testLoadWithExtension();
+	void testNotFound();
+
+private:
+	FilesystemQueryLoader loader;
+};
+
+CPPUNIT_TEST_SUITE_REGISTRATION(FilesystemQueryLoaderTest);
+
+void FilesystemQueryLoaderTest::setUp()
+{
+	static const string ROOT_PATH = "test/dao/";
+	loader.setRootPath(Environment::get("QUERIES_PATH", ROOT_PATH));
+	loader.setPreserveUnneededLines(false);
+}
+
+/**
+ * Test we can load an empty file.
+ */
+void FilesystemQueryLoaderTest::testEmpty()
+{
+	const string &empty = loader.find("queries.empty.empty");
+	CPPUNIT_ASSERT(empty.empty());
+}
+
+/**
+ * Test we can load a file containing only whitespace. As default,
+ * the whitespace is discarded. When preserving unneeded lines, the
+ * whitespace is also discarded, however, *empty* lines are still
+ * preserved.
+ */
+void FilesystemQueryLoaderTest::testWhitespace()
+{
+	const string &empty = loader.find("queries.empty.whitespace");
+	CPPUNIT_ASSERT(empty.empty());
+
+	loader.setPreserveUnneededLines(true);
+	const string lines = loader.find("queries.empty.whitespace");
+	CPPUNIT_ASSERT(!lines.empty());
+	CPPUNIT_ASSERT_EQUAL("\n\n\n\n\n", lines);
+}
+
+/**
+ * Test we can load file containing only comments. The comments
+ * are discarded as default. When preserving unneeded lines, the
+ * comments are discarded but *empty* lines stays there.
+ */
+void FilesystemQueryLoaderTest::testComments()
+{
+	const string empty = loader.find("queries.empty.comments");
+	CPPUNIT_ASSERT(empty.empty());
+
+	loader.setPreserveUnneededLines(true);
+	const string lines = loader.find("queries.empty.comments");
+
+	CPPUNIT_ASSERT(!lines.empty());
+	CPPUNIT_ASSERT_EQUAL("\n\n\n", lines);
+}
+
+/**
+ * Test behaviour of loading file containing only whitespace and comments.
+ * Both are discarded as default. When preserving unneeded lines, *empty*
+ * lines (free of whitespace and comments) are preserved.
+ */
+void FilesystemQueryLoaderTest::testCommentsAndWhitespace()
+{
+	const string empty = loader.find("queries.empty.comments_and_whitespace");
+	CPPUNIT_ASSERT(empty.empty());
+
+	loader.setPreserveUnneededLines(true);
+	const string lines = loader.find("queries.empty.comments_and_whitespace");
+
+	CPPUNIT_ASSERT(!lines.empty());
+	CPPUNIT_ASSERT_EQUAL("\n\n\n\n\n\n", lines);
+}
+
+/**
+ * Test we can load a single-line content.
+ * No EOL character is to be present at the end of the loaded query
+ * unless we preserve unneeded lines.
+ */
+void FilesystemQueryLoaderTest::testQuerySingleLine()
+{
+	const string query = loader.find("queries.sql.single_line");
+	CPPUNIT_ASSERT_EQUAL("SELECT * FROM users WHERE id = ?", query);
+
+	loader.setPreserveUnneededLines(true);
+	const string preserved = loader.find("queries.sql.single_line");
+	CPPUNIT_ASSERT_EQUAL("SELECT * FROM users WHERE id = ?\n", preserved);
+}
+
+/**
+ * Test we can load a multi-line content.
+ * No EOL character is to be present at the end of the loaded query
+ * unless we preserve unneeded lines.
+ */
+void FilesystemQueryLoaderTest::testQueryMultiLine()
+{
+	const string query = loader.find("queries.sql.multi_line");
+	CPPUNIT_ASSERT_EQUAL(
+		"SELECT\n\tname,\n\tage\nFROM\n\tusers\nWHERE\n\tid = ?",
+		query);
+
+	loader.setPreserveUnneededLines(true);
+	const string preserved = loader.find("queries.sql.multi_line");
+	CPPUNIT_ASSERT_EQUAL(
+		"SELECT\n\tname,\n\tage\nFROM\n\tusers\nWHERE\n\tid = ?\n",
+		preserved);
+}
+
+/**
+ * Test we can load a query containing some comments that are discarded
+ * as default. When preserving unneeded lines, only empty lines are
+ * preserved instead of those comments.
+ */
+void FilesystemQueryLoaderTest::testQueryWithComments()
+{   
+	const string query = loader.find("queries.sql.with_comments");
+	CPPUNIT_ASSERT_EQUAL(
+		"SELECT\n\tname,\n\temail\nFROM\n\tusers\nWHERE\n\tid = ?",
+		query);
+
+	loader.setPreserveUnneededLines(true);
+	const string preserved = loader.find("queries.sql.with_comments");
+	CPPUNIT_ASSERT_EQUAL(
+		"\n\nSELECT\n\tname,\n\temail\nFROM\n\tusers\nWHERE\n\tid = ?\n",
+		preserved);
+}
+
+/**
+ * Test we can load a query mixed with comments and whitespace.
+ * As default *all* comments are discarded and all empty lines
+ * (or empty lines containing only whitespace) are discarded.
+ * When preserving unneeded lines, the lines after discarding
+ * of comments and whitespaces are left in the query.
+ */
+void FilesystemQueryLoaderTest::testQueryWithCommentsAndWhitespace()
+{
+	const string query = loader.find("queries.sql.with_comments_and_whitespace");
+	CPPUNIT_ASSERT_EQUAL(
+		"SELECT\nname, age\nFROM\n\tusers\nWHERE\n\tid = ?",
+		query);
+
+	loader.setPreserveUnneededLines(true);
+	const string preserved = loader.find("queries.sql.with_comments_and_whitespace");
+	CPPUNIT_ASSERT_EQUAL(
+		"\n\n\nSELECT\n\nname, age\n\nFROM\n\tusers\n\nWHERE\n\tid = ?\n",
+		preserved);
+}
+
+/**
+ * Test that whitespace *after* the query is discarded unless preserving
+ * unneeded lines.
+ */
+void FilesystemQueryLoaderTest::testQueryEndingWithWhitespace()
+{
+	const string query = loader.find("queries.sql.ending_with_whitespace");
+	CPPUNIT_ASSERT_EQUAL(
+		"SELECT\n\tname\nFROM\n\tusers\nWHERE\n\tid = ?",
+		query);
+
+	loader.setPreserveUnneededLines(true);
+	const string preserved = loader.find("queries.sql.ending_with_whitespace");
+	CPPUNIT_ASSERT_EQUAL(
+		"SELECT\n\tname\nFROM\n\tusers\nWHERE\n\tid = ?\n\n\n",
+		preserved);
+
+}
+
+/**
+ * Test that when the loader is configured to load files with an extension,
+ * we can load such queries without any error.
+ */
+void FilesystemQueryLoaderTest::testLoadWithExtension()
+{
+	loader.setExtension("sql");
+
+	string empty1;
+	CPPUNIT_ASSERT_NO_THROW(
+		empty1 = loader.find("queries.empty.empty1"));
+	CPPUNIT_ASSERT(empty1.empty());
+
+	string select;
+	CPPUNIT_ASSERT_NO_THROW(
+		select = loader.find("queries.sql.select"));
+	CPPUNIT_ASSERT_EQUAL(
+		"SELECT * FROM users\nWHERE id = ?",
+		select);
+}
+
+/**
+ * Test the NotFoundException is thrown when a query is
+ * not found. Test the same behaviour for queries stored
+ * in files with an extension.
+ *
+ * Note that when a query exists and its file has not any
+ * extension, we must fail when loading queries *with*
+ * a configured extension.
+ */
+void FilesystemQueryLoaderTest::testNotFound()
+{
+	CPPUNIT_ASSERT_THROW(
+		loader.find("queries.sql.insert"),
+		NotFoundException);
+
+	// exists but has extension .sql
+	CPPUNIT_ASSERT_THROW(
+		loader.find("queries.sql.select"),
+		NotFoundException);
+
+	loader.setExtension("sql");
+
+	// exists but only without extension .sql
+	CPPUNIT_ASSERT_THROW(
+		loader.find("queries.sql.with_comments"),
+		NotFoundException);
+}
+
+}
+

test/dao/queries/empty/comments
@@ -1,0 +1,3 @@
+-- Few lines of comments
+--
+-- This file is intentionally useless.

test/dao/queries/empty/comments_and_whitespace
@@ -1,0 +1,6 @@
+-- This file contains comments
+
+-- and empty lines and
+     
+-- lots of whitespaces
+    -- even in this way

test/dao/queries/empty/whitespace
@@ -1,0 +1,5 @@
+
+    
+    
+
+    

commit 93092d3c9b7c38bc5cd09053cfda0040f9cef48b

FilesystemQueryLoader: fix setting rootPath
The rootPath must always be treated as directory. If makeDirectory()
is not called and the path is given as "path/dir" (without an ending
slash "/"), the last part "dir" would be dropped which is unexpected.

Fixes: ebe905a97ffb ("FilesystemQueryLoader: new class")
Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

src/dao/FilesystemQueryLoader.cpp
@@ -76,6 +76,7 @@ void FilesystemQueryLoader::setPreserveUnneededLines(bool preserve)
 void FilesystemQueryLoader::setRootPath(const string &rootPath)
 {
 	m_rootPath = Path(rootPath);
+	m_rootPath.makeDirectory();
 }
 
 void FilesystemQueryLoader::setExtension(const string &extension)