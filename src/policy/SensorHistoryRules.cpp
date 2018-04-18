BeeeOn/server

commit d15565ab9fd782d5e766092945e32a87b4d5c95a

SensorHistoryRules: new class
SensorHistoryRules provides a way how to restrict history queries
(currently) based on the parameters: time range and time interval.

The time range specifies how long is the history period we are
interested in. Thus, it directly affects the data amount to work
with.

The time interval specifies the aggregation granularity. The bigger
the time interval is, the better aggregation is performed and less
data amounts are processed.

Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

src/policy/SensorHistoryRules.cpp
@@ -1,0 +1,224 @@
+#include <Poco/Exception.h>
+#include <Poco/Logger.h>
+#include <Poco/String.h>
+
+#include "di/Injectable.h"
+#include "policy/SensorHistoryRules.h"
+#include "util/TimespanParser.h"
+
+BEEEON_OBJECT_BEGIN(BeeeOn, SensorHistoryRules)
+BEEEON_OBJECT_PROPERTY("rules", &SensorHistoryRules::parseAndSetRules)
+BEEEON_OBJECT_PROPERTY("acceptZeroInterval", &SensorHistoryRules::setAcceptZeroInterval)
+BEEEON_OBJECT_END(BeeeOn, SensorHistoryRules)
+
+using namespace std;
+using namespace Poco;
+using namespace BeeeOn;
+
+SensorHistoryRules::Span::Span(
+		const Timespan &_min,
+		const Timespan &_max):
+	min(_min),
+	max(_max)
+{
+	if (min > max)
+		throw InvalidArgumentException("min > max for " + toString());
+}
+
+bool SensorHistoryRules::Span::operator <(const Span &other) const
+{
+	return (min < other.min) && (max <= other.min);
+}
+
+string SensorHistoryRules::Span::toString() const
+{
+	if (min == max)
+		return SensorHistoryRules::asString(min);
+
+	return SensorHistoryRules::asString(min)
+		+ ".."
+		+ SensorHistoryRules::asString(max);
+}
+
+string SensorHistoryRules::asString(const Timespan &time)
+{
+	if (time == 0)
+		return "0";
+
+	if (time.microseconds() > 0)
+		return to_string(time.totalMicroseconds()) + " us";
+	if (time.milliseconds() > 0)
+		return to_string(time.totalMilliseconds()) + " ms";
+	if (time.seconds() > 0)
+		return to_string(time.totalSeconds()) + " s";
+	if (time.minutes() > 0)
+		return to_string(time.totalMinutes()) + " m";
+	if (time.hours() > 0)
+		return to_string(time.totalHours()) + " h";
+
+	return to_string(time.days()) + " d";
+}
+
+SensorHistoryRules::SensorHistoryRules():
+	m_acceptZeroInterval(false)
+{
+}
+
+SensorHistoryRules::~SensorHistoryRules()
+{
+}
+
+void SensorHistoryRules::parseAndSetRules(const std::map<std::string, std::string> &rules)
+{
+	map<Span, Span> spans;
+	SharedPtr<Span> missing;
+
+	for (const auto &pair : rules) {
+		const string &key = trim(pair.first);
+		const string &value = trim(pair.second);
+
+		if (key == "*") {
+			if (!missing.isNull())
+				throw ExistsException("duplicate '*' rule");
+
+			missing = new Span(parseSpan(value));
+		}
+		else {
+			const Span &span = parseSpan(key);
+			const Span &interval = parseSpan(value);
+
+			logger().information(
+				"registering rule "
+				+ span.toString()
+				+ " -> "
+				+ interval.toString(),
+				__FILE__, __LINE__);
+
+			const auto result = spans.emplace(span, interval);
+
+			if (!result.second) {
+				const auto conflict = result.first->first;
+
+				throw ExistsException(
+					"span "
+					+ span.toString()
+					+ " is overlapping with "
+					+ conflict.toString());
+			}
+		}
+	}
+
+	setRules(spans);
+	setMissing(missing);
+}
+
+void SensorHistoryRules::setRules(const std::map<Span, Span> &rules)
+{
+	m_rules = rules;
+}
+
+void SensorHistoryRules::setMissing(SharedPtr<Span> interval)
+{
+	m_missing = interval;
+}
+
+void SensorHistoryRules::setAcceptZeroInterval(bool accept)
+{
+	m_acceptZeroInterval = accept;
+}
+
+bool SensorHistoryRules::accept(
+		const TimeInterval &range,
+		const Timespan &interval) const
+{
+	const Timespan &span = range.end() - range.start();
+	const Span &needle = {span, span};
+
+	if (interval == 0 && !m_acceptZeroInterval) {
+		if (logger().debug()) {
+			logger().debug(
+				"zero interval not allowed for "
+				+ needle.toString(),
+				__FILE__, __LINE__);
+		}
+
+		return false;
+	}
+
+	const auto it = m_rules.find(needle);
+	if (it == m_rules.end() && m_missing.isNull()) {
+		if (logger().debug()) {
+			logger().debug(
+				"no such rule for "
+				+ needle.toString(),
+				__FILE__, __LINE__);
+		}
+
+		return false;
+	}
+	else if (it == m_rules.end()) {
+		if (logger().debug()) {
+			logger().debug(
+				"applying the rule 'missing' for "
+				+ needle.toString(),
+				__FILE__, __LINE__);
+		}
+	}
+
+	const auto &rule = it == m_rules.end() ? *m_missing : it->second;
+	const bool result = interval >= rule.min && interval <= rule.max;
+
+	if (logger().debug()) {
+		if (result) {
+			logger().debug(
+				"interval "
+				+ asString(interval)
+				+ " is not allowed for "
+				+ needle.toString(),
+				__FILE__, __LINE__);
+		}
+		else {
+			logger().debug(
+				"interval "
+				+ asString(interval)
+				+ " allowed for "
+				+ needle.toString(),
+				__FILE__, __LINE__);
+		}
+	}
+
+	return result;
+}
+
+Timespan SensorHistoryRules::parseTime(const string &input, bool max)
+{
+	static const Timespan TDIFF_MAX = Timestamp::TIMEVAL_MAX;
+
+	const auto part = trim(input);
+	if (max && part == "max")
+		return TDIFF_MAX;
+
+	const bool hasPlus = part.back() == '+';
+	const string &noPlus = hasPlus ?
+		trim(string(part.begin(), part.end() - 1)) : part;
+
+	const Timespan &time = TimespanParser::parse(noPlus);
+	return hasPlus ? time + 1 : time;
+}
+
+SensorHistoryRules::Span SensorHistoryRules::parseSpan(const string &input)
+{
+	const auto sep = input.find("..");
+	if (sep == string::npos) {
+		if (input.find("+") != string::npos)
+			throw SyntaxException("'+' is not allowed for " + input);
+
+		const auto span = parseTime(input, true);
+		return {span, span};
+	}
+
+	const auto left = parseTime(input.substr(0, sep));
+	const auto right = parseTime({input.begin() + sep + 2, input.end()}, true);
+
+	return {left, right};
+}

conf/config.d/factory-services.xml
@@ -118,6 +118,12 @@
 
 		<!-- Access policy -->
 
+		<instance name="defaultSensorHistoryRules" class="BeeeOn::SensorHistoryRules">
+			<set name="rules">
+				<pair key="*" text="1 s..max" />
+			</set>
+		</instance>
+
 		<instance name="accessPolicy"
 				class="BeeeOn::DefaultAccessPolicy">
 			<set name="userDao" ref="userDao" />

src/CMakeLists.txt
@@ -176,6 +176,7 @@ file(GLOB SOURCES
 	${PROJECT_SOURCE_DIR}/policy/DeviceAccessPolicy.cpp
 	${PROJECT_SOURCE_DIR}/policy/RoleAccessPolicy.cpp
 	${PROJECT_SOURCE_DIR}/policy/SensorHistoryAccessPolicy.cpp
+	${PROJECT_SOURCE_DIR}/policy/SensorHistoryRules.cpp
 	${PROJECT_SOURCE_DIR}/policy/DefaultAccessPolicy.cpp
 	${PROJECT_SOURCE_DIR}/policy/PolicyContext.cpp
 	${PROJECT_SOURCE_DIR}/policy/UserPolicyContext.cpp

src/policy/SensorHistoryRules.h
@@ -1,0 +1,95 @@
+#pragma once
+
+#include <map>
+
+#include <Poco/SharedPtr.h>
+#include <Poco/Timespan.h>
+
+#include "util/Loggable.h"
+#include "util/TimeInterval.h"
+
+namespace BeeeOn {
+
+/**
+ * @brief SensorHistoryRules implements check of parameters used when
+ * asking for sensor history data. In this way, it is possible to
+ * restrict requests to a supported range of request or to forbid
+ * requests which are too computational intensive.
+ */
+class SensorHistoryRules : Loggable {
+public:
+	typedef Poco::SharedPtr<SensorHistoryRules> Ptr;
+
+	struct Span {
+		const Poco::Timespan min;
+		const Poco::Timespan max;
+
+		Span(const Poco::Timespan &min, const Poco::Timespan &max);
+
+		bool operator<(const Span &) const;
+		std::string toString() const;
+	};
+
+	SensorHistoryRules();
+	virtual ~SensorHistoryRules();
+
+	bool accept(
+		const TimeInterval &range,
+		const Poco::Timespan &interval) const;
+
+	/**
+	 * Set rules by parsing the map keys and values.
+	 * It recognizes rule '*' as the missing rule. If the '*' rule is
+	 * not present, the missing rule is cleared.
+	 *
+	 * @see SensorHistoryRules::parseSpan()
+	 */
+	void parseAndSetRules(const std::map<std::string, std::string> &rules);
+
+	void setRules(const std::map<Span, Span> &rules);
+	void setMissing(Poco::SharedPtr<Span> interval);
+	void setAcceptZeroInterval(bool accept);
+
+	/**
+	 * @brief Parse the given span specification.
+	 *
+	 * It accepts inputs like: <code>1 s</code>, <code>1 m</code>,
+	 * <code>5 s..1 m</code>, <code>5 s..1 m+</code>, <code>5 m..max</code>.
+	 *
+	 * The '+' suffix is used to determine inclusive interval. Consider
+	 * that <code>1 s..1 m</code> is equivalent to <code>1000000 us..60000000 us</code>.
+	 * But <code>1 s..1 m+<code> is equivalent to <code>1000000 us..60000001 us</code>.
+	 *
+	 * The syntax rules:
+	 *
+	 * - <code>NUMBER UNIT</code>
+	 * - <code>max</code>
+	 * - <code>NUMBER UNIT PLUS?..NUMBER UNIT PLUS?</code>
+	 * - <code>NUMBER UNIT PLUS?..max</code>
+	 *
+	 * The NUMBER is treated as <code>[0-9]+</code>.
+	 *
+	 * The UNIT is treated as <code>{us|ms|s|m|h|d|}</code> (empty is possble).
+	 *
+	 * The PLUS is treated as <code>\+</code>
+	 *
+	 * @throws Poco::SyntaxException
+	 * @throws Poco::InvalidArgumentException when left > right.
+	 *
+	 * @see TimespanParser::parse()
+	 */
+	static Span parseSpan(const std::string &input);
+
+protected:
+	static std::string asString(const Poco::Timespan &time);
+	static Poco::Timespan parseTime(
+		const std::string &input,
+		bool max = false);
+
+private:
+	std::map<Span, Span> m_rules;
+	Poco::SharedPtr<Span> m_missing;
+	bool m_acceptZeroInterval;
+};
+
+}

test/CMakeLists.txt
@@ -30,6 +30,7 @@ file(GLOB TEST_SOURCES
 	${PROJECT_SOURCE_DIR}/model/BitmapInfoTest.cpp
 	${PROJECT_SOURCE_DIR}/model/DeviceTest.cpp
 	${PROJECT_SOURCE_DIR}/model/DevicePropertyTest.cpp
+	${PROJECT_SOURCE_DIR}/policy/SensorHistoryRulesTest.cpp
 	${PROJECT_SOURCE_DIR}/server/CORSFilterTest.cpp
 	${PROJECT_SOURCE_DIR}/server/HTTPCustomHeadersFilterTest.cpp
 	${PROJECT_SOURCE_DIR}/server/SessionManagerTest.cpp

test/policy/SensorHistoryRulesTest.cpp
@@ -1,0 +1,358 @@
+#include <cppunit/extensions/HelperMacros.h>
+
+#include "cppunit/BetterAssert.h"
+#include "policy/SensorHistoryRules.h"
+
+using namespace std;
+using namespace Poco;
+
+namespace BeeeOn {
+
+class SensorHistoryRulesTest : public CppUnit::TestFixture {
+	CPPUNIT_TEST_SUITE(SensorHistoryRulesTest);
+	CPPUNIT_TEST(testSpanLessThan);
+	CPPUNIT_TEST(testParseValidSpan);
+	CPPUNIT_TEST(testParseInvalidSpan);
+	CPPUNIT_TEST(testSetValidRules);
+	CPPUNIT_TEST(testSetInvalidRules);
+	CPPUNIT_TEST(testAccept);
+	CPPUNIT_TEST(testNotAccept);
+	CPPUNIT_TEST_SUITE_END();
+public:
+	void testSpanLessThan();
+	void testParseValidSpan();
+	void testParseInvalidSpan();
+	void testSetValidRules();
+	void testSetInvalidRules();
+	void testAccept();
+	void testNotAccept();
+
+protected:
+	TimeInterval minutes(unsigned int m, bool almost = false) const;
+	TimeInterval hours(unsigned int h, bool almost = false) const;
+	TimeInterval days(unsigned int d, bool almost = false) const;
+
+private:
+};
+
+CPPUNIT_TEST_SUITE_REGISTRATION(SensorHistoryRulesTest);
+
+TimeInterval SensorHistoryRulesTest::minutes(unsigned int m, bool almost) const
+{
+	return {0, m * Timespan::MINUTES - (almost ? 1 : 0)};
+}
+
+TimeInterval SensorHistoryRulesTest::hours(unsigned int h, bool almost) const
+{
+	return {0, h * Timespan::HOURS - (almost ? 1 : 0)};
+}
+
+TimeInterval SensorHistoryRulesTest::days(unsigned int d, bool almost) const
+{
+	return {0, d * Timespan::DAYS - (almost ? 1 : 0)};
+}
+
+void SensorHistoryRulesTest::testSpanLessThan()
+{
+	using Span = SensorHistoryRules::Span;
+
+	CPPUNIT_ASSERT(Span(0, 0) < Span(1, 1));
+	CPPUNIT_ASSERT(Span(0, 0) < Span(1, 2));
+	CPPUNIT_ASSERT(Span(0, 1) < Span(1, 1));
+	CPPUNIT_ASSERT(Span(0, 1) < Span(2, 2));
+
+	CPPUNIT_ASSERT(!(Span(0, 0) < Span(0, 1)));
+	CPPUNIT_ASSERT(!(Span(0, 0) < Span(0, 2)));
+	CPPUNIT_ASSERT(!(Span(0, 1) < Span(0, 1)));
+	CPPUNIT_ASSERT(!(Span(0, 1) < Span(0, 0)));
+
+	// check representation 0..1, 1..1, 1..2
+	// 0..1 and 1..1 can coexist
+	CPPUNIT_ASSERT((Span(0, 1) < Span(1, 1)));
+	// 1..1 and 1..2 are overlapping
+	CPPUNIT_ASSERT(!(Span(1, 1) < Span(1, 2)));
+}
+
+void SensorHistoryRulesTest::testParseValidSpan()
+{
+	const auto a = SensorHistoryRules::parseSpan("1 s");
+	CPPUNIT_ASSERT(a.min == 1 * Timespan::SECONDS);
+	CPPUNIT_ASSERT(a.max == 1 * Timespan::SECONDS);
+
+	const auto b = SensorHistoryRules::parseSpan("1 m");
+	CPPUNIT_ASSERT(b.min == 1 * Timespan::MINUTES);
+	CPPUNIT_ASSERT(b.max == 1 * Timespan::MINUTES);
+
+	const auto c = SensorHistoryRules::parseSpan("1 m..1 h");
+	CPPUNIT_ASSERT(c.min == 1 * Timespan::MINUTES);
+	CPPUNIT_ASSERT(c.max == 1 * Timespan::HOURS);
+
+	const auto d = SensorHistoryRules::parseSpan("1 h..2 h");
+	CPPUNIT_ASSERT(d.min == 1 * Timespan::HOURS);
+	CPPUNIT_ASSERT(d.max == 2 * Timespan::HOURS);
+
+	const auto e = SensorHistoryRules::parseSpan("0 h");
+	CPPUNIT_ASSERT(e.min == 0);
+	CPPUNIT_ASSERT(e.max == 0);
+
+	const auto f = SensorHistoryRules::parseSpan("15 h");
+	CPPUNIT_ASSERT(f.min == 15 * Timespan::HOURS);
+	CPPUNIT_ASSERT(f.max == 15 * Timespan::HOURS);
+
+	const auto g = SensorHistoryRules::parseSpan("15 h..15 h");
+	CPPUNIT_ASSERT(g.min == 15 * Timespan::HOURS);
+	CPPUNIT_ASSERT(g.max == 15 * Timespan::HOURS);
+
+	const auto h = SensorHistoryRules::parseSpan("max");
+	CPPUNIT_ASSERT(h.min == Timestamp::TIMEVAL_MAX);
+	CPPUNIT_ASSERT(h.max == Timestamp::TIMEVAL_MAX);
+
+	const auto i = SensorHistoryRules::parseSpan("0..max");
+	CPPUNIT_ASSERT(i.min == 0);
+	CPPUNIT_ASSERT(i.max == Timestamp::TIMEVAL_MAX);
+
+	const auto j = SensorHistoryRules::parseSpan("1 s..1 s+");
+	CPPUNIT_ASSERT(j.min == 1 * Timespan::SECONDS);
+	CPPUNIT_ASSERT(j.max == 1 * Timespan::SECONDS + 1);
+
+	const auto k = SensorHistoryRules::parseSpan("1 s..1 h+");
+	CPPUNIT_ASSERT(k.min == 1 * Timespan::SECONDS);
+	CPPUNIT_ASSERT(k.max == 1 * Timespan::HOURS + 1);
+
+	const auto l = SensorHistoryRules::parseSpan("1 s+..1 h");
+	CPPUNIT_ASSERT(l.min == 1 * Timespan::SECONDS + 1);
+	CPPUNIT_ASSERT(l.max == 1 * Timespan::HOURS);
+
+	const auto m = SensorHistoryRules::parseSpan("1 s+..1 s+");
+	CPPUNIT_ASSERT(m.min == 1 * Timespan::SECONDS + 1);
+	CPPUNIT_ASSERT(m.max == 1 * Timespan::SECONDS + 1);
+}
+
+void SensorHistoryRulesTest::testParseInvalidSpan()
+{
+	CPPUNIT_ASSERT_THROW(
+		SensorHistoryRules::parseSpan("1 h..2 h.."),
+		SyntaxException);
+
+	CPPUNIT_ASSERT_THROW(
+		SensorHistoryRules::parseSpan(""),
+		SyntaxException);
+
+	CPPUNIT_ASSERT_THROW(
+		SensorHistoryRules::parseSpan("1.5 s..1.2 s"),
+		SyntaxException);
+
+	CPPUNIT_ASSERT_THROW(
+		SensorHistoryRules::parseSpan("..1.2 s"),
+		SyntaxException);
+
+	CPPUNIT_ASSERT_THROW(
+		SensorHistoryRules::parseSpan("5 s..1 s"),
+		InvalidArgumentException);
+
+	CPPUNIT_ASSERT_THROW(
+		SensorHistoryRules::parseSpan("max..max"),
+		SyntaxException);
+
+	CPPUNIT_ASSERT_THROW(
+		SensorHistoryRules::parseSpan("1 s+"),
+		SyntaxException);
+
+	CPPUNIT_ASSERT_THROW(
+		SensorHistoryRules::parseSpan("1 s+..1 s"),
+		InvalidArgumentException);
+}
+
+void SensorHistoryRulesTest::testSetValidRules()
+{
+	SensorHistoryRules rules;
+
+	CPPUNIT_ASSERT_NO_THROW(
+		rules.parseAndSetRules({
+			{"0", "0"},
+			{"1", "0"},
+			{"2", "1"},
+			{"3", "1"},
+			{"4..5", "5"},
+			{"5..6", "1"},
+		}));
+}
+
+void SensorHistoryRulesTest::testSetInvalidRules()
+{
+	SensorHistoryRules rules;
+
+	CPPUNIT_ASSERT_THROW(
+		rules.parseAndSetRules({
+			{"0", "0"},
+			{"0 us", "0"}, // conflicting
+		}),
+		ExistsException);
+
+	CPPUNIT_ASSERT_THROW(
+		rules.parseAndSetRules({
+			{"0", "0"},
+			{"1 m", "0"},
+			{"60 s", "0"}, // conflicting
+		}),
+		ExistsException);
+
+	CPPUNIT_ASSERT_THROW(
+		rules.parseAndSetRules({
+			{"0..1 s", "0"},
+			{"1 s",    "0"},
+			{"1000 ms..2 s", "0"}, // conflicting
+		}),
+		ExistsException);
+
+	CPPUNIT_ASSERT_THROW(
+		rules.parseAndSetRules({
+			{"1..0", "0"}, // min > max
+		}),
+		InvalidArgumentException);
+
+	CPPUNIT_ASSERT_THROW(
+		rules.parseAndSetRules({
+			{"*", "0"},
+			{"1", "0"},
+			{"* ", "0"}, // duplicate
+		}),
+		ExistsException);
+}
+
+/**
+ * Define a set of rules and test boundary conditions of each.
+ * There are 2 kinds if tests:
+ *
+ * - test that the rule exists
+ * - test that the interval is valid for the given rule
+ *
+ * The rules does not contain holes except of the 60 d.
+ * Holes would be however covered by the rule "*".
+ */
+void SensorHistoryRulesTest::testAccept()
+{
+	SensorHistoryRules rules;
+	rules.setAcceptZeroInterval(true);
+
+	CPPUNIT_ASSERT_NO_THROW(
+		rules.parseAndSetRules({
+			{"0..10 m", "0"},
+			{"10 m..1 h", "5 s..1 h"},
+			{"1 h..5 h", "30 s..1 h"},
+			{"5 h..10 h", "30 s..5 h"},
+			{"10 h..1 d", "5 m..5 h"},
+			{"1 d..7 d", "1 h..1 d"},
+			{"7 d..30 d", "1 d..7 d"},
+			{"60 d", "10 d"},
+			{"*", "1 d..30 d"},
+		}));
+
+	// 0 is globally accepted
+	CPPUNIT_ASSERT(rules.accept(minutes(0), 0));
+	CPPUNIT_ASSERT(rules.accept(minutes(10, true), 0));
+
+	// inside 5 s..1 h
+	CPPUNIT_ASSERT(rules.accept(minutes(10), 5 * Timespan::SECONDS));
+	CPPUNIT_ASSERT(rules.accept(minutes(10), 1 * Timespan::HOURS));
+	CPPUNIT_ASSERT(rules.accept(hours(1, true), 5 * Timespan::SECONDS));
+	CPPUNIT_ASSERT(rules.accept(hours(1, true), 1 * Timespan::HOURS));
+
+	// inside 30 s..1 h
+	CPPUNIT_ASSERT(rules.accept(hours(1), 30 * Timespan::SECONDS));
+	CPPUNIT_ASSERT(rules.accept(hours(1), 1 * Timespan::HOURS));
+	CPPUNIT_ASSERT(rules.accept(hours(5, true), 30 * Timespan::SECONDS));
+	CPPUNIT_ASSERT(rules.accept(hours(5, true), 1 * Timespan::HOURS));
+
+	// inside 5 m..5 h
+	CPPUNIT_ASSERT(rules.accept(hours(10), 5 * Timespan::MINUTES));
+	CPPUNIT_ASSERT(rules.accept(hours(10), 5 * Timespan::HOURS));
+	CPPUNIT_ASSERT(rules.accept(hours(24, true), 5 * Timespan::MINUTES));
+	CPPUNIT_ASSERT(rules.accept(hours(24, true), 5 * Timespan::HOURS));
+
+	// inside 1 h..1 d
+	CPPUNIT_ASSERT(rules.accept(days(1), 1 * Timespan::HOURS));
+	CPPUNIT_ASSERT(rules.accept(days(1), 1 * Timespan::DAYS));
+	CPPUNIT_ASSERT(rules.accept(days(7, true), 1 * Timespan::HOURS));
+	CPPUNIT_ASSERT(rules.accept(days(7, true), 1 * Timespan::DAYS));
+
+	// inside 1 d..7 d
+	CPPUNIT_ASSERT(rules.accept(days(7), 1 * Timespan::DAYS));
+	CPPUNIT_ASSERT(rules.accept(days(7), 7 * Timespan::DAYS));
+	CPPUNIT_ASSERT(rules.accept(days(30, true), 1 * Timespan::DAYS));
+	CPPUNIT_ASSERT(rules.accept(days(30, true), 7 * Timespan::DAYS));
+
+	// inside 1 d..30 d
+	CPPUNIT_ASSERT(rules.accept(days(30), 1 * Timespan::DAYS));
+	CPPUNIT_ASSERT(rules.accept(days(30), 30 * Timespan::DAYS));
+	CPPUNIT_ASSERT(rules.accept(days(100), 1 * Timespan::DAYS));
+	CPPUNIT_ASSERT(rules.accept(days(100), 30 * Timespan::DAYS));
+
+	// exactly 10 d
+	CPPUNIT_ASSERT(rules.accept(days(60), 10 * Timespan::DAYS));
+}
+
+void SensorHistoryRulesTest::testNotAccept()
+{
+	SensorHistoryRules rules;
+	rules.setAcceptZeroInterval(false);
+
+	CPPUNIT_ASSERT_NO_THROW(
+		rules.parseAndSetRules({
+			{"0..10 m", "0"},
+			{"10 m..1 h", "5 s..1 h"},
+			{"1 h..5 h", "30 s..1 h"},
+			{"5 h..10 h", "30 s..5 h"},
+			{"10 h..1 d", "5 m..5 h"},
+			{"1 d..7 d", "1 h..1 d"},
+			{"7 d..30 d", "1 d..7 d"},
+			{"60 d", "10 d"},
+			{"*", "1 d..30 d"},
+		}));
+
+	// 0 interval is globally denied for those rules
+	CPPUNIT_ASSERT(!rules.accept(minutes(0), 0));
+	CPPUNIT_ASSERT(!rules.accept(minutes(10, true), 0));
+
+	// out of 5 s..1 h
+	CPPUNIT_ASSERT(!rules.accept(minutes(10), 5 * Timespan::SECONDS - 1));
+	CPPUNIT_ASSERT(!rules.accept(minutes(10), 1 * Timespan::HOURS + 1));
+	CPPUNIT_ASSERT(!rules.accept(hours(1, true), 5 * Timespan::SECONDS - 1));
+	CPPUNIT_ASSERT(!rules.accept(hours(1, true), 1 * Timespan::HOURS + 1));
+
+	// out of 30 s..1 h
+	CPPUNIT_ASSERT(!rules.accept(hours(1), 30 * Timespan::SECONDS - 1));
+	CPPUNIT_ASSERT(!rules.accept(hours(1), 1 * Timespan::HOURS + 1));
+	CPPUNIT_ASSERT(!rules.accept(hours(5, true), 30 * Timespan::SECONDS - 1));
+	CPPUNIT_ASSERT(!rules.accept(hours(5, true), 1 * Timespan::HOURS + 1));
+
+	// out of 5 m..5 h
+	CPPUNIT_ASSERT(!rules.accept(hours(10), 5 * Timespan::MINUTES - 1));
+	CPPUNIT_ASSERT(!rules.accept(hours(10), 5 * Timespan::HOURS + 1));
+	CPPUNIT_ASSERT(!rules.accept(hours(24, true), 5 * Timespan::MINUTES - 1));
+	CPPUNIT_ASSERT(!rules.accept(hours(24, true), 5 * Timespan::HOURS + 1));
+
+	// out of 1 h..1 d
+	CPPUNIT_ASSERT(!rules.accept(days(1), 1 * Timespan::HOURS - 1));
+	CPPUNIT_ASSERT(!rules.accept(days(1), 1 * Timespan::DAYS + 1));
+	CPPUNIT_ASSERT(!rules.accept(days(7, true), 1 * Timespan::HOURS - 1));
+	CPPUNIT_ASSERT(!rules.accept(days(7, true), 1 * Timespan::DAYS + 1));
+
+	// out of 1 d..7 d
+	CPPUNIT_ASSERT(!rules.accept(days(7), 1 * Timespan::DAYS - 1));
+	CPPUNIT_ASSERT(!rules.accept(days(7), 7 * Timespan::DAYS + 1));
+	CPPUNIT_ASSERT(!rules.accept(days(30, true), 1 * Timespan::DAYS - 1));
+	CPPUNIT_ASSERT(!rules.accept(days(30, true), 7 * Timespan::DAYS + 1));
+
+	// out of 1 d..30 d
+	CPPUNIT_ASSERT(!rules.accept(days(30), 1 * Timespan::DAYS - 1));
+	CPPUNIT_ASSERT(!rules.accept(days(30), 30 * Timespan::DAYS + 1));
+	CPPUNIT_ASSERT(!rules.accept(days(100), 1 * Timespan::DAYS - 1));
+	CPPUNIT_ASSERT(!rules.accept(days(100), 30 * Timespan::DAYS + 1));
+
+	// out of 10 d
+	CPPUNIT_ASSERT(!rules.accept(days(60), 10 * Timespan::DAYS - 1));
+	CPPUNIT_ASSERT(!rules.accept(days(60), 10 * Timespan::DAYS + 1));
+}
+
+}

commit ff93beccd8212c422f208ee0118f46504a9527ab

SensorHistoryRules: fix swapped logging of success
Mistakenly, the "not allowed" message is logged when the result
is true - allowed.

Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>
Fixes: d15565ab9fd7 ("SensorHistoryRules: new class")

src/policy/SensorHistoryRules.cpp
@@ -178,7 +178,7 @@ bool SensorHistoryRules::accept(
 	const auto &rule = it == m_rules.end() ? *m_missing : it->second;
 	const bool result = interval >= rule.min && interval <= rule.max;
 
-	if (result) {
+	if (!result) {
 		if (logger().error()) {
 			logger().error(
 				"interval "