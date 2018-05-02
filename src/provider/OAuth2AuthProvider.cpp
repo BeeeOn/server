9e4b37075b26d92cf1e83ecff973390fb3a566f1
Jan Viktorin <iviktorin@fit.vutbr.cz>
2018-05-02 17:51:15 +0200

    OAuth2AuthProvider: rework to be generic and non-abstract
    The OAuth2AuthProvider is now implemented via OAuth2CodeExchanger
    and OAuth2UserFetcher components and thus it just converts the given
    credentials to the appropriate form. The OAuth2AuthProvider is not
    an abstract class anymore.
    
    This change required to drop the existing OAuth2 implementations
    GoogleAuthProvider and FacebookAuthProvider as they depend on it.
    To replace them, create an instance of OAuth2AuthProvider and
    use either GoogleOAuth2Provider or FacebookOAuth2Provider that
    implement the OAuth2CodeExchanger and OAuth2UserFetcher interfaces.
    
    The AuthRestHandler has been slighly changed to not advertise
    OAuth2AuthProvider instance without clientId. Such situation can
    occur if an OAuth2AuthProvider instance is not configured for
    authorization code flow.
    
    Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

--- src/provider/OAuth2AuthProvider.cpp
+++ src/provider/OAuth2AuthProvider.cpp
@@ -1,99 +1,110 @@
-#include <sstream>
-
 #include <Poco/Exception.h>
-#include <Poco/Logger.h>
-#include <Poco/Net/Context.h>
-#include <Poco/Net/SSLManager.h>
-#include <Poco/Net/InvalidCertificateHandler.h>
-#include <Poco/Net/AcceptCertificateHandler.h>
-#include <Poco/Net/HTMLForm.h>
-#include <Poco/Net/HTTPRequest.h>
 #include <Poco/Net/NetException.h>
-#include <Poco/StreamCopier.h>
-#include <Poco/URI.h>
 
-#include "net/HTTPUtil.h"
+#include "di/Injectable.h"
 #include "provider/OAuth2AuthProvider.h"
-#include "ssl/SSLClient.h"
 
 using namespace std;
 using namespace BeeeOn;
 using namespace Poco;
 using namespace Poco::Net;
 
-OAuth2AuthProvider::OAuth2AuthProvider(const string &name):
-		AbstractAuthProvider(name),
-		m_sslConfig(0)
+BEEEON_OBJECT_BEGIN(BeeeOn, OAuth2AuthProvider)
+BEEEON_OBJECT_CASTABLE(AuthProvider)
+BEEEON_OBJECT_PROPERTY("id", &OAuth2AuthProvider::setId)
+BEEEON_OBJECT_PROPERTY("name", &OAuth2AuthProvider::setName)
+BEEEON_OBJECT_PROPERTY("codeExchanger", &OAuth2AuthProvider::setCodeExchanger)
+BEEEON_OBJECT_PROPERTY("userFetcher", &OAuth2AuthProvider::setUserFetcher)
+BEEEON_OBJECT_PROPERTY("tokenType", &OAuth2AuthProvider::setTokenType)
+BEEEON_OBJECT_END(BeeeOn, OAuth2AuthProvider)
+
+OAuth2AuthProvider::OAuth2AuthProvider():
+	m_tokenType("Bearer")
 {
+}
+
+void OAuth2AuthProvider::setId(const string &id)
+{
+	m_id = id;
+}
+
+const string &OAuth2AuthProvider::id() const
+{
+	return m_id;
+}
+
+void OAuth2AuthProvider::setName(const string &name)
+{
+	m_name = name;
+}
+
+const string &OAuth2AuthProvider::name() const
+{
+	return m_name;
+}
+
+string OAuth2AuthProvider::clientId() const
+{
+	return m_fetcher->clientId();
+}
+
+string OAuth2AuthProvider::redirectURI() const
+{
+	if (m_exchanger.isNull())
+		return "";
+
+	return m_exchanger->redirectUri();
+}
+
+void OAuth2AuthProvider::setCodeExchanger(OAuth2CodeExchanger::Ptr exchanger)
+{
+	m_exchanger = exchanger;
+}
+
+void OAuth2AuthProvider::setUserFetcher(OAuth2UserFetcher::Ptr fetcher)
+{
+	m_fetcher = fetcher;
+}
+
+void OAuth2AuthProvider::setTokenType(const string &type)
+{
+	m_tokenType = type;
 }
 
 bool OAuth2AuthProvider::authorize(const Credentials &cred, AuthResult &result)
 {
+	OAuth2CodeExchanger::Tokens tokens;
+
 	if (typeid(cred) == typeid(AuthCodeCredentials)) {
+		if (m_exchanger.isNull())
+			throw NotAuthenticatedException("AuthCodeCredentials not configured for " + id());
+
 		const AuthCodeCredentials &authCodeCredentials =
 			static_cast<const AuthCodeCredentials &>(cred);
 
+		tokens = m_exchanger->exchange(authCodeCredentials);
+		m_fetcher->fetch(tokens, result);
 		result.setProvider(name());
-		return verifyAuthCode(authCodeCredentials, result);
+		return true;
 	}
 	if (typeid(cred) == typeid(AccessTokenCredentials)) {
 		const AccessTokenCredentials &accessTokenCredentials =
 			dynamic_cast<const AccessTokenCredentials &>(cred);
 
+		tokens.accessToken = accessTokenCredentials.accessToken().toString();
+		tokens.tokenType = m_tokenType;
+
+		/*
+		 * We have no idea about the real expire time unless the access token
+		 * is decoded (which is not generally possible unless fetch is called).
+		 * Thus, behave like the the access token seems to be valid.
+		 */
+		tokens.expiresIn = 1;
+
+		m_fetcher->fetch(tokens, result);
 		result.setProvider(name());
-		return verifyAccessToken(accessTokenCredentials, result);
+		return true;
 	}
 
 	throw NotAuthenticatedException("unrecognized credentials type");
-}
-
-bool OAuth2AuthProvider::verifyAuthCode(const AuthCodeCredentials &, AuthResult &)
-{
-	throw NotImplementedException("auth-code capability is not implemented");
-}
-
-bool OAuth2AuthProvider::verifyAccessToken(const AccessTokenCredentials &, AuthResult &)
-{
-	throw NotImplementedException("access-token capability is not implemented");
-}
-
-void OAuth2AuthProvider::validateSSL()
-{
-	if (m_sslConfig.isNull())
-		throw IllegalStateException(
-				"missing sslConfig, cannot use OAuth");
-}
-
-string OAuth2AuthProvider::handleResponse(HTTPEntireResponse &response) const
-{
-	if (logger().debug()) {
-		logger().debug("response status: "
-			+ to_string(response.getStatus())
-			+ " "
-			+ response.getReason(), __FILE__, __LINE__);
-	}
-
-	if (logger().debug()) {
-		logger().debug("response: "
-			+ response.getBody(), __FILE__, __LINE__);
-	}
-
-	return response.getBody();
-}
-
-string OAuth2AuthProvider::makeRequest(const string &method, URI &host, HTMLForm &requestForm) const
-{
-	HTTPRequest request(method, host.getPathAndQuery(), HTTPMessage::HTTP_1_1);
-	requestForm.prepareSubmit(request);
-
-	ostringstream requestQuery;
-	requestForm.write(requestQuery);
-
-	if (logger().debug())
-		logger().debug("request: " + requestQuery.str(), __FILE__, __LINE__);
-
-	HTTPEntireResponse response = HTTPUtil::makeRequest(
-		request, host, requestQuery.str(), m_sslConfig);
-
-	return handleResponse(response);
 }
--- src/provider/OAuth2AuthProvider.h
+++ src/provider/OAuth2AuthProvider.h
@@ -2,99 +2,81 @@
 #define BEEEON_OAUTH2_AUTH_PROVIDER_H
 
 #include <Poco/SharedPtr.h>
-#include <Poco/Net/Context.h>
 
-#include "net/HTTPEntireResponse.h"
+#include "oauth2/OAuth2CodeExchanger.h"
+#include "oauth2/OAuth2UserFetcher.h"
 #include "provider/AuthProvider.h"
-
-namespace Poco {
-class URI;
-namespace Net {
-
-class HTMLForm;
-
-}
-}
 
 namespace BeeeOn {
 
-class SSLClient;
-
 /**
- * An OAuth 2 authorization provider performs authorization by using an
- * auth code. The abstract class extracts the appropriate credentials and
- * verifies it against a 3rd party authorization service.
+ * @brief OAuth2AuthProvider is a generic provider that implements the OAuth2
+ * flow based on the OAuth2CodeExchanger and OAuth2UserFetcher instances.
+ * This allows to easily add new OAuth2 identity providers by reusing the
+ * existing code base as much as possible.
+ *
+ * If the codeExchanger property is not set, only access tokens are supported
+ * for authentication if the given OAuth2UserFetcher supports exchange of
+ * access tokens for user info.
  */
-class OAuth2AuthProvider : public AbstractAuthProvider {
+class OAuth2AuthProvider : public AuthProvider {
 public:
-	OAuth2AuthProvider(const std::string &name);
+	OAuth2AuthProvider();
 
+	/**
+	 * @brief Set ID as presented to users. This allows to choose the proper
+	 * authorization flow by users. The ID should be unique among AuthProvider
+	 * instances.
+	 */
+	void setId(const std::string &id);
+	const std::string &id() const override;
+
+	/**
+	 * @brief Set name of the authorization method. The name do not have to be
+	 * unique and sometimes, it is desirable that multiple AuthProvider instances
+	 * have the same name. The name usually refers to the identity provider.
+	 */
+	void setName(const std::string &name);
+	const std::string &name() const override;
+
+	std::string clientId() const;
+	std::string redirectURI() const;
+
+	/**
+	 * Set OAuth2CodeExchanger instance that adds support for
+	 * the authorization code verification. Otherwise, only
+	 * access tokens can be used.
+	 */
+	void setCodeExchanger(OAuth2CodeExchanger::Ptr exchanger);
+
+	/**
+	 * Set OAuth2UserFetcher that allows to fetch user info
+	 * based on any of the given tokens (depends on what the
+	 * given OAuth2UserFetcher supports).
+	 */
+	void setUserFetcher(OAuth2UserFetcher::Ptr fetcher);
+
+	/**
+	 * Configure token type that would be passed into the
+	 * OAuth2UserFetcher for fetching user info. The value
+	 * is usually "Bearer".
+	 */
+	void setTokenType(const std::string &type);
+
+	/**
+	 * Implement the OAuth2 authorization process for the given credentials.
+	 * AuthCodeCredentials and AccessTokenCredentials are supported but they
+	 * might not be supported by the configuration of OAuth2CodeExchanger and
+	 * OAuth2UserFetcher.
+	 */
 	bool authorize(const Credentials &cred, AuthResult &result) override;
 
-	void setClientId(const std::string &clientId)
-	{
-		m_clientId = clientId;
-	}
-
-	std::string clientId() const
-	{
-		return m_clientId;
-	}
-
-	void setClientSecret(const std::string &clientSecret)
-	{
-		m_clientSecret = clientSecret;
-	}
-
-	void setRedirectURI(const std::string &uri)
-	{
-		m_redirectURI = uri;
-	}
-
-	std::string redirectURI() const
-	{
-		return m_redirectURI;
-	}
-
-	void setSSLConfig(Poco::SharedPtr<SSLClient> config)
-	{
-		m_sslConfig = config;
-	}
-
-	/**
-	 * Initialize SSL context for upcoming HTTPS requests
-	 */
-	void validateSSL();
-
-protected:
-	/**
-	 * Verification against a 3rd party.
-	 */
-	virtual bool verifyAuthCode(
-			const AuthCodeCredentials &credentials,
-			AuthResult &info);
-	/**
-	 * Verification against a 3rd party.
-	 */
-	virtual bool verifyAccessToken(
-			const AccessTokenCredentials &credentials,
-			AuthResult &info);
-
-	/**
-	 * Handle response.
-	 */
-	std::string handleResponse(HTTPEntireResponse &session) const;
-
-	/**
-	 * Creates session and makes on method based request and returns handled response
-	 */
-	std::string makeRequest(const std::string &method, Poco::URI &host, Poco::Net::HTMLForm &requestForm) const;
-
-protected:
-	std::string m_clientId;
-	std::string m_clientSecret;
-	std::string m_redirectURI;
-	Poco::SharedPtr<SSLClient> m_sslConfig;
+private:
+	std::string m_id;
+	std::string m_name;
+	std::string m_tokenType;
+	OAuth2CodeExchanger::Ptr m_exchanger;
+	OAuth2UserFetcher::Ptr m_fetcher;
 };
 
 }
