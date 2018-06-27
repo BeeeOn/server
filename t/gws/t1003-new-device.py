cf3807b98089f9e01493a1749320b8ab2220c5f4
Jan Viktorin <iviktorin@fit.vutbr.cz>
2018-06-27 15:16:11 +0200

    t: gws: expect ZMQ events in new-device tests
    Expect on-new-device and on-refused-new-device events to come in
    certain tests.
    
    Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

--- t/gws/t1003-new-device.py
+++ t/gws/t1003-new-device.py
@@ -8,17 +8,31 @@ import websocket
 import json
 import uuid
 
-from gws import assureIsClosed, assureNotClosed, registerGateway
+from gws import assureIsClosed, assureNotClosed, registerGateway, ZMQConnection
 
 class TestNewDevice(unittest.TestCase):
 	def setUp(self):
+		self.zmq = ZMQConnection(config.gws_zmq_endpoint)
+		self.zmq.open()
+
 		self.ws = websocket.WebSocket()
 		self.ws.connect(config.gws_ws_uri)
 
 		registerGateway(self, self.ws, config.gateway_id)
 
+		event = self.zmq.pop_data(timeout = 5)
+		self.assertEqual("on-connected", event["event"])
+		self.assertEqual(config.gateway_id, event["gateway_id"])
+
 	def tearDown(self):
 		self.ws.close()
+
+		try:
+			event = self.zmq.pop_data(timeout = 5)
+			self.assertEqual("on-disconnected", event["event"])
+			self.assertEqual(config.gateway_id, event["gateway_id"])
+		finally:
+			self.zmq.close()
 
 	"""
 	Register device with a valid type. It should be accepted without any issue.
@@ -50,6 +64,11 @@ class TestNewDevice(unittest.TestCase):
 		self.assertEqual(id, response["id"])
 		self.assertEqual(1, response["status"])
 		assureNotClosed(self, self.ws)
+
+		event = self.zmq.pop_data(timeout = 5)
+		self.assertEqual("on-new-device", event["event"])
+		self.assertEqual("0xa123123412341234", event["device_id"])
+		self.assertEqual(config.gateway_id, event["gateway_id"])
 
 	"""
 	Register device that does not match to any types known on the
@@ -83,6 +102,11 @@ class TestNewDevice(unittest.TestCase):
 		self.assertEqual(id, response["id"])
 		self.assertEqual(2, response["status"])
 		assureNotClosed(self, self.ws)
+
+		event = self.zmq.pop_data(timeout = 5)
+		self.assertEqual("on-refused-new-device", event["event"])
+		self.assertEqual("0xa123123412349999", event["device_id"])
+		self.assertEqual(config.gateway_id, event["gateway_id"])
 
 	"""
 	Server must deny accepting of device with an invalid device ID.
@@ -144,6 +168,11 @@ class TestNewDevice(unittest.TestCase):
 		self.assertEqual(2, response["status"])
 		assureNotClosed(self, self.ws)
 
+		event = self.zmq.pop_data(timeout = 5)
+		self.assertEqual("on-refused-new-device", event["event"])
+		self.assertEqual("0xa123123412341111", event["device_id"])
+		self.assertEqual(config.gateway_id, event["gateway_id"])
+
 	"""
 	Register device with 2 modules, but this device type has just one module
 	'pressure' known on the server. The server must fail
@@ -180,6 +209,11 @@ class TestNewDevice(unittest.TestCase):
 		self.assertEqual(id, response["id"])
 		self.assertEqual(2, response["status"])
 		assureNotClosed(self, self.ws)
+
+		event = self.zmq.pop_data(timeout = 5)
+		self.assertEqual("on-refused-new-device", event["event"])
+		self.assertEqual("0xa123123412342222", event["device_id"])
+		self.assertEqual(config.gateway_id, event["gateway_id"])
 
 if __name__ == '__main__':
 	import sys
