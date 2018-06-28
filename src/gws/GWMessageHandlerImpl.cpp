BeeeOn/server

commit e195d82e16c574c668997b5e5e5e0ea4b8b22f7b

GWMessageHandlerImpl: fire events for SensorDataListeners
Everytime a message with sensor data comes, copy its contents into
the SensorDataEvent and deliver it to the listening objects.

Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

src/gws/GWMessageHandlerImpl.cpp
@@ -15,6 +15,8 @@ BEEEON_OBJECT_REF("responseExpectedQueue", &GWMessageHandlerImpl::setGWResponseE
 BEEEON_OBJECT_REF("rpcForwarder", &GWMessageHandlerImpl::setRPCForwarder)
 BEEEON_OBJECT_REF("deviceService", &GWMessageHandlerImpl::setDeviceService)
 BEEEON_OBJECT_REF("sensorHistoryService", &GWMessageHandlerImpl::setSensorHistoryService)
+BEEEON_OBJECT_REF("eventsExecutor", &GWMessageHandlerImpl::setEventsExecutor)
+BEEEON_OBJECT_REF("dataListeners", &GWMessageHandlerImpl::registerDataListener)
 BEEEON_OBJECT_HOOK("cleanup", &GWMessageHandlerImpl::cleanup)
 BEEEON_OBJECT_END(BeeeOn, GWMessageHandlerImpl)
 
@@ -105,6 +107,14 @@ void GWMessageHandlerImpl::handleSensorData(GWSensorDataExport::Ptr dataExport,
 
 			moduleValues.push_back(moduleValue);
 		}
+
+		SensorDataEvent e;
+		e.setGatewayID(gatewayID);
+		e.setDeviceID(device.id());
+		e.setStamp(it.timestamp());
+		e.setData(moduleValues);
+
+		m_dataEventSource.fireEvent(e, &SensorDataListener::onReceived);
 
 		try {
 			m_sensorHistoryService->insertMany(
@@ -258,4 +268,14 @@ void GWMessageHandlerImpl::setSensorHistoryService(
 		GWSSensorHistoryService::Ptr service)
 {
 	m_sensorHistoryService = service;
+}
+
+void GWMessageHandlerImpl::registerDataListener(SensorDataListener::Ptr listener)
+{
+	m_dataEventSource.addListener(listener);
+}
+
+void GWMessageHandlerImpl::setEventsExecutor(AsyncExecutor::Ptr executor)
+{
+	m_dataEventSource.setAsyncExecutor(executor);
 }

conf/config.d/factory-gws.xml
@@ -37,6 +37,7 @@
 			<set name="rpcForwarder" ref="asyncGatewayRPC" />
 			<set name="deviceService" ref="gwsDeviceService" />
 			<set name="sensorHistoryService" ref="gwsSensorHistoryService" />
+			<set name="eventsExecutor" ref="asyncExecutor" />
 		</instance>
 
 		<instance name="gatewayScanController" class="BeeeOn::GatewayScanController">

src/gws/GWMessageHandlerImpl.h
@@ -11,8 +11,10 @@
 #include "gws/GWMessageHandler.h"
 #include "gws/GWResponseExpectedQueue.h"
 #include "gws/RPCForwarder.h"
+#include "gws/SensorDataListener.h"
 #include "service/GWSDeviceServiceImpl.h"
 #include "service/GWSSensorHistoryServiceImpl.h"
+#include "util/EventSource.h"
 #include "util/Loggable.h"
 
 namespace BeeeOn {
@@ -39,6 +41,9 @@ public:
 	void setRPCForwarder(RPCForwarder::Ptr forwarder);
 	void setDeviceService(GWSDeviceService::Ptr service);
 	void setSensorHistoryService(GWSSensorHistoryService::Ptr service);
+
+	void registerDataListener(SensorDataListener::Ptr listener);
+	void setEventsExecutor(AsyncExecutor::Ptr executor);
 
 	/**
 	 * Break dependency cycles prior destruction.
@@ -79,6 +84,7 @@ private:
 	RPCForwarder::Ptr m_rpcForwarder;
 	GWSDeviceService::Ptr m_deviceService;
 	GWSSensorHistoryService::Ptr m_sensorHistoryService;
+	EventSource<SensorDataListener> m_dataEventSource;
 };
 
 }

commit 212789fdfb0c796e3565d2e36b2cc3b985cf15b1

GWMessageHandlerImpl: fix onReceived event generation
The event onReceived was generated even in the case when the actual
data insertion has failed. E.g. we can receive a malformed list of
values:

{
 ...
 "module_id": 0, "value": 30
 "module_id": 0, "value": 60
}

here with duplicated module 0. Such insertion would fail for the second
attempt to insert value for module 0 with the same timestamp. For such
data, we should generate the following event:

{
 ...
 "data": {"0": 30}
}

but we would have generated:

{
 ...
 "data": {"0": 30, "0": 60}
}

which is semantically wrong anyway. The event listener would see wrong
event and would either drop it all (loosing data) or process it in an
unexpected way.

By moving the event generation *after* calling to insertMany(), we
generate an event for only the values that have been successfully
inserted.

Fixes: e195d82e16c5 ("GWMessageHandlerImpl: fire events for SensorDataListeners")
Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

src/gws/GWMessageHandlerImpl.cpp
@@ -114,17 +114,20 @@ void GWMessageHandlerImpl::handleSensorData(GWSensorDataExport::Ptr dataExport,
 			moduleValues.push_back(moduleValue);
 		}
 
-		SensorDataEvent e;
-		e.setGatewayID(gatewayID);
-		e.setDeviceID(device.id());
-		e.setStamp(it.timestamp());
-		e.setData(moduleValues);
-
-		m_dataEventSource.fireEvent(e, &SensorDataListener::onReceived);
-
 		try {
 			m_sensorHistoryService->insertMany(
 				device, it.timestamp(), moduleValues);
+
+			if (moduleValues.empty())
+				continue;
+
+			SensorDataEvent e;
+			e.setGatewayID(gatewayID);
+			e.setDeviceID(device.id());
+			e.setStamp(it.timestamp());
+			e.setData(moduleValues);
+
+			m_dataEventSource.fireEvent(e, &SensorDataListener::onReceived);
 		}
 		catch (const Exception &e) {
 			logger().log(e, __FILE__, __LINE__);