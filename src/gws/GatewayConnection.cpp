BeeeOn/server

commit 373d2a37d7eeada5d063a3501b8d6ca01c92adf8

GatewayConnection: handle PING frames
When received a valid PING frame respond with a PONG frame.
A PONG frame must contain the same payload as received in the
appropriate PING frame.

Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>
Reviewed-by: Richard Wolfert <xwolfe00@stud.fit.vutbr.cz>
Reviewed-by: Jozef Halaj <xhalaj03@stud.fit.vutbr.cz>

src/gws/GatewayConnection.cpp
@@ -104,12 +104,33 @@ GWMessage::Ptr GatewayConnection::receiveMessage()
 
 		return GWMessage::fromJSON(msg);
 
+	case WebSocket::FRAME_OP_PING:
+		sendPong(msg);
+		break;
+
 	default:
 		throw ProtocolException("unhandled websocket opcode: "
 				+ NumberFormatter::formatHex(opcode, true));
 	}
 
 	return nullptr;
+}
+
+void GatewayConnection::sendPong(const std::string &requestData)
+{
+	if (logger().debug()) {
+		logger().debug(
+			"reply PONG frame of size "
+			+ to_string(requestData.size()),
+			__FILE__, __LINE__);
+	}
+
+	FastMutex::ScopedLock guard(m_sendMutex);
+
+	m_webSocket.sendFrame(
+		requestData.c_str(),
+		requestData.length(),
+		WebSocket::FRAME_OP_PONG);
 }
 
 void GatewayConnection::sendMessage(const GWMessage::Ptr message)

src/gws/GatewayConnection.h
@@ -95,6 +95,14 @@ public:
 
 private:
 	/**
+	 * @brief Sends back the PONG frame. Such frame must contain the same
+	 * application data as it was received.
+	 *
+	 * @see https://tools.ietf.org/html/rfc6455#section-5.5.2
+	 */
+	void sendPong(const std::string &requestData);
+
+	/**
 	 * @brief Set timestamp of the last received message.
 	 */
 	void updateLastReceiveTime();

t/gws/t1002-test-control-frames.py
@@ -57,6 +57,16 @@ class TestControlFrames(unittest.TestCase):
 		self.ws.ping("X" * 1024)
 		assureIsClosed(self, self.ws)
 
+	"""
+	Valid PING frame should lead to an appropriate PONG response.
+	"""
+	def test4_ping_pong(self):
+		self.ws.ping("testing connection")
+		frame = self.ws.recv_frame()
+
+		self.assertEqual(websocket.ABNF.OPCODE_PONG, frame.opcode)
+		self.assertEqual("testing connection", str(frame.data, "utf-8"))
+
 if __name__ == '__main__':
 	import sys
 	import taprunner

commit 6dbcad12d96091d6a372fe2cbc4c9fb78ef3532f

GatewayConnection: fix setting FIN bit in pong frames
Control frame of The WebSocket Protocol (as defined in RFC6455)
must not be fragmented due to its ability to be interleaved
with (possible fragmented) data messages. Because of this,
we have to set the FIN bit of pong frame, making it unfragmented
by definition in RFC6455.

https://tools.ietf.org/html/rfc6455#page-34

Fixes: 373d2a37d7ee ("GatewayConnection: handle PING frames")
Signed-off-by: Richard Wolfert <xwolfe00@stud.fit.vutbr.cz>
Acked-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

src/gws/GatewayConnection.cpp
@@ -162,7 +162,7 @@ void GatewayConnection::sendPong(const std::string &requestData)
 	m_webSocket.sendFrame(
 		requestData.c_str(),
 		requestData.length(),
-		WebSocket::FRAME_OP_PONG);
+		WebSocket::FRAME_OP_PONG | WebSocket::FRAME_FLAG_FIN);
 }
 
 void GatewayConnection::sendMessage(const GWMessage::Ptr message)