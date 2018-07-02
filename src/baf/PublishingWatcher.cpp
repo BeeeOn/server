BeeeOn/server

commit f04b0fb34105f93950edde81747a1a669863b80b

PublishingWatcher: publish pairing events with status
Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

src/baf/PublishingWatcher.cpp
@@ -39,6 +39,36 @@ void PublishingWatcher::onRefusedNewDevice(const DeviceEvent &e)
 	publishEvent(e, "on-refused-new-device");
 }
 
+void PublishingWatcher::onPairRequested(const DeviceEvent &e)
+{
+	publishEvent(e, "on-pair-device", "request");
+}
+
+void PublishingWatcher::onPairConfirmed(const DeviceEvent &e)
+{
+	publishEvent(e, "on-pair-device", "confirmed");
+}
+
+void PublishingWatcher::onPairFailed(const DeviceEvent &e)
+{
+	publishEvent(e, "on-pair-device", "failed");
+}
+
+void PublishingWatcher::onUnpairRequested(const DeviceEvent &e)
+{
+	publishEvent(e, "on-unpair-device", "request");
+}
+
+void PublishingWatcher::onUnpairConfirmed(const DeviceEvent &e)
+{
+	publishEvent(e, "on-unpair-device", "confirmed");
+}
+
+void PublishingWatcher::onUnpairFailed(const DeviceEvent &e)
+{
+	publishEvent(e, "on-unpair-device", "failed");
+}
+
 void PublishingWatcher::onConnected(const GatewayEvent &e)
 {
 	publishEvent(e, "on-connected");

src/baf/PublishingWatcher.h
@@ -29,6 +29,12 @@ public:
 
 	void onNewDevice(const DeviceEvent &e) override;
 	void onRefusedNewDevice(const DeviceEvent &e) override;
+	void onPairRequested(const DeviceEvent &e) override;
+	void onPairConfirmed(const DeviceEvent &e) override;
+	void onPairFailed(const DeviceEvent &e) override;
+	void onUnpairRequested(const DeviceEvent &e) override;
+	void onUnpairConfirmed(const DeviceEvent &e) override;
+	void onUnpairFailed(const DeviceEvent &e) override;
 
 	void onConnected(const GatewayEvent &e) override;
 	void onReconnected(const GatewayEvent &e) override;

commit 04d7c8ad653b5b08ac481abf3a10bdb20d3fe9fa

PublishingWatcher: fix typo status "request" -> "requested"
The status informs about something that has already happened and all
other status texts are expressed in the post form.

Fixes: f04b0fb34105 ("PublishingWatcher: publish pairing events with status")
Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

src/baf/PublishingWatcher.cpp
@@ -41,7 +41,7 @@ void PublishingWatcher::onRefusedNewDevice(const DeviceEvent &e)
 
 void PublishingWatcher::onPairRequested(const DeviceEvent &e)
 {
-	publishEvent(e, "on-pair-device", "request");
+	publishEvent(e, "on-pair-device", "requested");
 }
 
 void PublishingWatcher::onPairConfirmed(const DeviceEvent &e)
@@ -56,7 +56,7 @@ void PublishingWatcher::onPairFailed(const DeviceEvent &e)
 
 void PublishingWatcher::onUnpairRequested(const DeviceEvent &e)
 {
-	publishEvent(e, "on-unpair-device", "request");
+	publishEvent(e, "on-unpair-device", "requested");
 }
 
 void PublishingWatcher::onUnpairConfirmed(const DeviceEvent &e)