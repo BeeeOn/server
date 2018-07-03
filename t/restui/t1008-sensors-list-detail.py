BeeeOn/server

commit 7f4b15e079452052d87867d43ce1c5e88abc9932

SensorRestHandler: accept list of aggregators
When requesting sensor history, it is possible to pass a list of
aggregator functions to be performed at once. Examples:

 GET ...?aggregation=avg
 GET ...?aggregation=avg,min
 GET ...?aggregation=max,avg,min
 GET ...?aggregation=max,max,max

The aggregator freq cannot be combined with the other ones due to
implementation limitations at the database level.

Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

src/restui/SensorRestHandler.cpp
@@ -148,18 +148,26 @@ Timespan SensorRestHandler::parseInterval(const string &input) const
 	return NumberParser::parseUnsigned(input) * Timespan::SECONDS;
 }
 
-string SensorRestHandler::parseAggregation(const string &input) const
+vector<string> SensorRestHandler::parseAggregation(const string &input) const
 {
-	if (input == "avg")
-		return input;
-	if (input == "min")
-		return input;
-	if (input == "max")
-		return input;
-	if (input == "freq")
-		return input;
+	StringTokenizer list(input, ",",
+		StringTokenizer::TOK_TRIM | StringTokenizer::TOK_IGNORE_EMPTY);
+	vector<string> result;
 
-	throw InvalidArgumentException("invalid aggregation: " + input);
+	for (const auto &agg : list) {
+		if (agg == "avg")
+			result.emplace_back(agg);
+		else if (agg == "min")
+			result.emplace_back(agg);
+		else if (agg == "max")
+			result.emplace_back(agg);
+		else if (agg == "freq")
+			result.emplace_back(agg);
+		else
+			throw InvalidArgumentException("invalid aggregation: " + input);
+	}
+
+	return result;
 }
 
 void SensorRestHandler::history(RestFlow &flow)
@@ -176,7 +184,7 @@ void SensorRestHandler::history(RestFlow &flow)
 	URI::QueryParameters queryParams = flow.uri().getQueryParameters();
 
 	TimeInterval range = TimeInterval::past(1 * Timespan::HOURS);
-	string aggregation = "avg";
+	vector<string> aggregation;
 	Timespan interval = 15 * Timespan::SECONDS;
 
 	for (auto &item : queryParams) {
@@ -189,6 +197,9 @@ void SensorRestHandler::history(RestFlow &flow)
 		else
 			continue; // ignore unknown parameters
 	}
+
+	if (aggregation.empty())
+		aggregation.emplace_back("avg");
 
 	RestValueConsumer consumer([&]() -> ostream& {return flow.response().stream();});
 

src/restui/SensorRestHandler.h
@@ -34,7 +34,7 @@ protected:
 private:
 	TimeInterval parseRange(const std::string &input) const;
 	Poco::Timespan parseInterval(const std::string &input) const;
-	std::string parseAggregation(const std::string &input) const;
+	std::vector<std::string> parseAggregation(const std::string &input) const;
 
 private:
 	SensorService::Ptr m_sensorService;

commit 16ac6a6c2b11a126b85a0b50123fe8af6c12f1a0

SensorRestHandler: aggregation field is validated as a list of entries
The URI parameter aggregation can contain commas (,) as separators. This was
however not allowed by the Sanitize::strict() validator before. Now, the
validation accepts also the comma and thus it allows inputs like

 - avg
 - avg,min
 - min,max
 - avg,min,max

without any issues.

Tests are extended to make sure that it works and the response contains all
the requested data.

Fixes: 7f4b15e07945 ("SensorRestHandler: accept list of aggregators")
Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

src/restui/SensorRestHandler.cpp
@@ -191,7 +191,7 @@ void SensorRestHandler::history(RestFlow &flow)
 		if (item.first == "range")
 			range = parseRange(Sanitize::common(item.second));
 		else if (item.first == "aggregation")
-			aggregation = parseAggregation(Sanitize::strict(item.second));
+			aggregation = parseAggregation(Sanitize::strict(item.second, ","));
 		else if (item.first == "interval")
 			interval = parseInterval(Sanitize::strict(item.second));
 		else

t/restui/t1008-sensors-list-detail.py
@@ -256,6 +256,34 @@ class TestSensorsListDetail(unittest.TestCase):
 		self.assertEqual("success", result["status"])
 		self.assertEqual(14, len(result["data"]))
 
+		# again, but ask for min and max
+		req = GET(config.ui_host, config.ui_port,
+				"/gateways/1284174504043136/devices/0xa335d00019f5234e/sensors/0/history" +
+				"?range=hour&aggregation=min,max")
+		req.authorize(self.session)
+		response, content = req()
+
+		self.assertEqual(200, response.status)
+
+		result = json.loads(content)
+		self.assertEqual("success", result["status"])
+		self.assertEqual(14, len(result["data"]))
+		self.assertEqual(2, len(result["data"][0]["values"]))
+
+		# again, but ask for min, max and avg
+		req = GET(config.ui_host, config.ui_port,
+				"/gateways/1284174504043136/devices/0xa335d00019f5234e/sensors/0/history" +
+				"?range=hour&aggregation=min,max,avg")
+		req.authorize(self.session)
+		response, content = req()
+
+		self.assertEqual(200, response.status)
+
+		result = json.loads(content)
+		self.assertEqual("success", result["status"])
+		self.assertEqual(14, len(result["data"]))
+		self.assertEqual(3, len(result["data"][0]["values"]))
+
 		# the same request with explicit timestamps
 		start = datetime.now() - timedelta(0, 60 * 60)
 		end = datetime.now()