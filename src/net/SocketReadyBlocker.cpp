3828b856c434f61ba22f667e27108bf3ff90c3e0
Jan Viktorin <iviktorin@fit.vutbr.cz>
2018-08-23 14:16:39 +0200

    SocketReadyBlocker: optionally test a payload ping
    The SocketReadyBlocker was previously only pinging a selected TCP
    port. With this change, it is possible to configure an initial
    request-response to be tested against the remote port. This makes
    possible to test e.g. whether the remote server really provides the
    expected service.
    
    It can be configured to only send some request and have no expectations
    about any response. Also vice-versa, to not send any request and expect
    some response. And both sending a request and expecting some response.
    Response can be checked as:
    
    * whether its header matches
    * whether whole response matches
    * whether the response has the expected size
    
    Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

--- src/net/SocketReadyBlocker.cpp
+++ src/net/SocketReadyBlocker.cpp
@@ -1,5 +1,8 @@
 #include <Poco/Exception.h>
+#include <Poco/FIFOBuffer.h>
 #include <Poco/Logger.h>
+#include <Poco/NumberFormatter.h>
+#include <Poco/NumberParser.h>
 #include <Poco/Net/NetException.h>
 #include <Poco/Net/StreamSocket.h>
 
@@ -12,6 +15,10 @@ BEEEON_OBJECT_PROPERTY("host", &SocketReadyBlocker::setHost)
 BEEEON_OBJECT_PROPERTY("port", &SocketReadyBlocker::setPort)
 BEEEON_OBJECT_PROPERTY("timeout", &SocketReadyBlocker::setTimeout)
 BEEEON_OBJECT_PROPERTY("interval", &SocketReadyBlocker::setInterval)
+BEEEON_OBJECT_PROPERTY("request", &SocketReadyBlocker::setRequest)
+BEEEON_OBJECT_PROPERTY("responseHeader", &SocketReadyBlocker::setResponseHeader)
+BEEEON_OBJECT_PROPERTY("responseBody", &SocketReadyBlocker::setResponseBody)
+BEEEON_OBJECT_PROPERTY("responseSize", &SocketReadyBlocker::setResponseSize)
 BEEEON_OBJECT_END(BeeeOn, SocketReadyBlocker)
 
 using namespace std;
@@ -55,6 +62,59 @@ void SocketReadyBlocker::setInterval(const Timespan &interval)
 	m_interval = interval;
 }
 
+vector<uint8_t> SocketReadyBlocker::parsePayload(const list<string> &payload)
+{
+	vector<uint8_t> bytes;
+
+	for (const auto &hex : payload) {
+		const auto byte = NumberParser::parseHex(hex);
+
+		if (byte < 0 || byte > 255)
+			throw InvalidArgumentException("payload must contain only values in range 00..FF");
+
+		bytes.emplace_back(static_cast<uint8_t>(byte));
+	}
+
+	return bytes;
+}
+
+string SocketReadyBlocker::payloadToString(const vector<uint8_t> &bytes)
+{
+	string payload;
+
+	for (auto it = bytes.begin(); it != bytes.end(); ++it) {
+		if (it == bytes.begin())
+			payload += NumberFormatter::formatHex(*it, 2);
+		else
+			payload += " " + NumberFormatter::formatHex(*it, 2);
+	}
+
+	return payload;
+}
+
+void SocketReadyBlocker::setRequest(const list<string> &payload)
+{
+	m_request = parsePayload(payload);
+}
+
+void SocketReadyBlocker::setResponseHeader(const list<string> &payload)
+{
+	m_responseHeader = parsePayload(payload);
+}
+
+void SocketReadyBlocker::setResponseBody(const list<string> &payload)
+{
+	m_responseBody = parsePayload(payload);
+}
+
+void SocketReadyBlocker::setResponseSize(int size)
+{
+	if (size < 0)
+		m_responseSize.clear();
+	else
+		m_responseSize = size;
+}
+
 void SocketReadyBlocker::checkTimeout(const Clock &clock) const
 {
 	if (m_timeout < 0)
@@ -81,13 +141,118 @@ bool SocketReadyBlocker::ping() const
 
 	try {
 		socket.connect({m_host, (uint16_t) m_port}, CONNECT_TIMEOUT);
-
-		logger().notice("remote " + remote() + " seems to be up");
-		return true;
 	}
-	BEEEON_CATCH_CHAIN(logger())
+	BEEEON_CATCH_CHAIN_ACTION(logger(),
+		return false)
 
-	return false;
+	try {
+		if (!m_request.empty())
+			sendRequest(socket);
+		if (!m_responseSize.isNull() || !m_responseHeader.empty() || !m_responseBody.empty())
+			checkResponse(socket);
+	}
+	BEEEON_CATCH_CHAIN_ACTION(logger(),
+		return false)
+
+	logger().notice("remote " + remote() + " seems to be up");
+	return true;
+}
+
+void SocketReadyBlocker::sendRequest(StreamSocket &socket) const
+{
+	if (logger().trace()) {
+		logger().dump(
+			"sending payload of size " + to_string(m_request.size()) + " B",
+			m_request.data(),
+			m_request.size(),
+			Message::PRIO_TRACE);
+	}
+	else if (logger().debug()) {
+		logger().debug(
+			"sending payload of size " + to_string(m_request.size()) + " B",
+			__FILE__, __LINE__);
+	}
+
+	socket.sendBytes(m_request.data(), m_request.size());
+}
+
+void SocketReadyBlocker::checkResponse(StreamSocket &socket) const
+{
+	FIFOBuffer buffer(1024);
+	const auto ret = socket.receiveBytes(buffer);
+	poco_assert(ret >= 0);
+
+	const vector<uint8_t> response = {buffer.begin(), buffer.begin() + ret};
+
+	if (logger().trace()) {
+		logger().dump(
+			"received payload of size "
+				+ to_string(response.size()) + " B",
+			response.data(),
+			response.size(),
+			Message::PRIO_TRACE);
+	}
+	else if (logger().debug()) {
+		logger().debug(
+			"received payload of size "
+				+ to_string(response.size()) + " B",
+			__FILE__, __LINE__);
+	}
+
+	if (!m_responseSize.isNull()) {
+		if (response.size() != m_responseSize) {
+			throw ProtocolException(
+				"received "
+				+ to_string(response.size()) + " B but expected "
+				+ to_string(m_responseSize) + " B");
+		}
+
+		if (logger().debug())
+			logger().debug("received payload size matches", __FILE__, __LINE__);
+	}
+
+	if (!m_responseHeader.empty()) {
+		if (response.size() < m_responseHeader.size()) {
+			throw ProtocolException("response is shorter ("
+					+ to_string(response.size())
+					+ " B) than expected header ("
+					+ to_string(m_responseHeader.size()) + " B)");
+		}
+
+		const vector<uint8_t> slice =
+			{response.begin(), response.begin() + m_responseHeader.size()};
+
+		if (slice != m_responseHeader) {
+			throw ProtocolException(
+				"received unexpected header: "
+				+ payloadToString(slice));
+		}
+
+		if (logger().debug())
+			logger().debug("received header matches", __FILE__, __LINE__);
+	}
+
+	if (!m_responseBody.empty()) {
+		if (response.size() < m_responseHeader.size() + m_responseBody.size()) {
+			throw ProtocolException("response is shorter ("
+					+ to_string(response.size())
+					+ " B) than expected header and body ("
+					+ to_string(m_responseHeader.size() + m_responseBody.size())
+					+ " B)");
+		}
+
+		const vector<uint8_t> slice =
+			{response.begin() + m_responseHeader.size(), response.end()};
+
+		if (slice != m_responseBody) {
+			throw ProtocolException(
+				"received unexpected body: "
+				+ payloadToString(slice));
+		}
+
+		if (logger().debug())
+			logger().debug("received body matches", __FILE__, __LINE__);
+	}
 }
 
 void SocketReadyBlocker::start()
--- src/net/SocketReadyBlocker.h
+++ src/net/SocketReadyBlocker.h
@@ -1,10 +1,14 @@
 #pragma once
 
 #include <string>
+#include <vector>
 
 #include <Poco/Clock.h>
 #include <Poco/Event.h>
+#include <Poco/Nullable.h>
 #include <Poco/Timespan.h>
+
+#include <Poco/Net/StreamSocket.h>
 
 #include "loop/StoppableLoop.h"
 #include "util/Loggable.h"
@@ -44,6 +48,37 @@ public:
 	void setInterval(const Poco::Timespan &interval);
 
 	/**
+	 * @brief Set payload to send to the remote server in case when the TCP connection
+	 * successfully opens.
+	 *
+	 * Format: a list of bytes is expected in format "HH", "HH", ...
+	 */
+	void setRequest(const std::list<std::string> &payload);
+
+	/**
+	 * @brief Set expected response header from the remote server. If set,
+	 * the response must contain at least the header at its beginning.
+	 *
+	 * Format: a list of bytes is expected in format "HH", "HH", ...
+	 */
+	void setResponseHeader(const std::list<std::string> &payload);
+
+	/**
+	 * @brief Set expected response body from the remote server. If set,
+	 * the response must contain the header (if set) and just after the
+	 * header, the rest of the response payload must match the body.
+	 *
+	 * Format: a list of bytes is expected in format "HH", "HH", ...
+	 */
+	void setResponseBody(const std::list<std::string> &payload);
+
+	/**
+	 * @brief Set count of bytes to expect to receive. If negative,
+	 * the response size is not considered.
+	 */
+	void setResponseSize(int size);
+
+	/**
 	 * @brief Start connecting to the remote system until it is up or a timeout
 	 * exceeds. The method intentionally blocks.
 	 *
@@ -59,14 +94,22 @@ public:
 
 protected:
 	bool ping() const;
+	void sendRequest(Poco::Net::StreamSocket &socket) const;
+	void checkResponse(Poco::Net::StreamSocket &socket) const;
 	void checkTimeout(const Poco::Clock &clock) const;
 	std::string remote() const;
+	static std::vector<uint8_t> parsePayload(const std::list<std::string> &payload);
+	static std::string payloadToString(const std::vector<uint8_t> &payload);
 
 private:
 	std::string m_host;
 	int m_port;
 	Poco::Timespan m_timeout;
 	Poco::Timespan m_interval;
+	std::vector<uint8_t> m_request;
+	std::vector<uint8_t> m_responseHeader;
+	std::vector<uint8_t> m_responseBody;
+	Poco::Nullable<size_t> m_responseSize;
 	Poco::Event m_event;
 };
 
