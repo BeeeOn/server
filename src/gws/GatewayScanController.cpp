BeeeOn/server

commit f2276357a401969cfeb7d318ebb9956eab6e726a

GatewayScanController: restart scan if not in progress
The gateway scan should be restared when it is in the idle, finished
or failed state. Otherwise, its current state is returned. This fixes
a bug when trying to restart scan immediately after the previous one
was finished.

Fixes: e3c58b4482cd ("GatewayScanController: new class")
Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

src/gws/GatewayScanController.cpp
@@ -253,16 +253,33 @@ GatewayScan GatewayScanController::scan(const Gateway &gateway, const Timespan &
 
 	ScopedLockWithUnlock<FastMutex> guard(m_lock);
 
+	ScanHandler::Ptr context;
+
 	auto it = m_scanMap.find(id);
-	if (it != m_scanMap.end())
-		return it->second->scan();
+	if (it != m_scanMap.end()) {
+		context = it->second;
 
-	ScanHandler::Ptr context = new ScanHandler(id);
-	context->scan().setDuration(duration);
+		const GatewayScan scan = context->scan();
 
-	m_scanMap.emplace(id, context);
+		switch (scan.state()) {
+		case GatewayScan::SCAN_PROCESSING:
+		case GatewayScan::SCAN_WAITING:
+			return scan;
+		default:
+			// restart scan
+			break;
+		}
+	}
+	else {
+		context = new ScanHandler(id);
+		m_scanMap.emplace(id, context);
+	}
 
+	ScanHandler::ScopedLock scanGuard(*context);
 	guard.unlock();
+
+	context->scan().setDuration(duration);
+	context->scan().changeState(GatewayScan::SCAN_WAITING);
 
 	const Clock start;
 

commit e945c12d2a71e690c2b2ee5fb773e225c219254b

GatewayScanController: fix missing update of timestamp
The timestamp of the last started scan was never updated.

Fixes: f2276357a401 ("GatewayScanController: restart scan if not in progress")
Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

src/gws/GatewayScanController.cpp
@@ -278,6 +278,7 @@ GatewayScan GatewayScanController::scan(const Gateway &gateway, const Timespan &
 	ScanHandler::ScopedLock scanGuard(*context);
 	guard.unlock();
 
+	context->scan().setStarted({});
 	context->scan().setDuration(duration);
 	context->scan().changeState(GatewayScan::SCAN_WAITING);
 