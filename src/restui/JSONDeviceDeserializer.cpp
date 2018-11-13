BeeeOn/server

commit 7bd90e93184bbd5bd873383507eacc1301962f8b

JSONDeviceDeserializer: apply refresh time with stricter rules
When accepting a request to change the refresh time from clients,
we must be careful with the refresh time semantics. If a device
does not support refresh time than we must not change it. Same
is the opposite. If the refresh time is supported by a device,
we must not disable it.

We consider that refresh >= 0 is updatable while refresh < 0 is
non-updatable (disabled at all).

Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

src/restui/JSONDeviceDeserializer.cpp
@@ -15,6 +15,20 @@ BeeeOn::JSONDeviceDeserializer::JSONDeviceDeserializer(
 {
 }
 
+void BeeeOn::JSONDeviceDeserializer::applyRefresh(Device &device, int refresh) const
+{
+	if (device.hasRefresh() && refresh < 0) {
+		throw InvalidArgumentException(
+			"refresh_time must be negative for device " + device);
+	}
+	else if (!device.hasRefresh() && refresh >= 0) {
+		throw InvalidArgumentException(
+			"refresh_time must be non-negative for device " + device);
+	}
+
+	device.setRefresh(refresh);
+}
+
 void BeeeOn::JSONDeviceDeserializer::partial(BeeeOn::Device &device) const
 {
 	if (m_object->has("name"))
@@ -27,7 +41,7 @@ void BeeeOn::JSONDeviceDeserializer::partial(BeeeOn::Device &device) const
 	}
 
 	if (m_object->has("refresh_time"))
-		device.setRefresh(m_object->getValue<unsigned int>("refresh_time"));
+		applyRefresh(device, m_object->getValue<int>("refresh_time"));
 }
 
 void BeeeOn::JSONDeviceDeserializer::full(BeeeOn::Device &device) const
@@ -47,5 +61,5 @@ void BeeeOn::JSONDeviceDeserializer::full(BeeeOn::Device &device) const
 	if (m_object->has("refresh_time"))
 		throw InvalidArgumentException("missing refresh_time for device");
 
-	device.setRefresh(m_object->getValue<unsigned int>("refresh_time"));
+	applyRefresh(device, m_object->getValue<int>("refresh_time"));
 }

src/restui/JSONDeviceDeserializer.h
@@ -15,6 +15,18 @@ public:
 	void partial(Device &device) const override;
 	void full(Device &device) const override;
 
+protected:
+	/**
+	 * Refresh can be applied with certain rules only:
+	 *
+	 * - If the device proclaims that it does not support refresh
+	 *   time changes, only a negative value is accepted.
+	 *
+	 * - If the device proclaims it supports refresh time changes,
+	 *   only a non-negative value is accepted.
+	 */
+	void applyRefresh(Device &device, int refresh) const;
+
 private:
 	const Poco::JSON::Object::Ptr m_object;
 };

t/restui/t2004-devices-update-activate.py
@@ -189,6 +189,36 @@ class TestDevicesUpdateActivate(unittest.TestCase):
 
 		self.assertEqual("inactive", result["data"]["state"])
 
+	"""
+	The Temperature sensor has non-updatable refresh time (-1).
+	Thus, we must be unable to change it to be updatable.
+	"""
+	def test6_update_non_updatable_refresh(self):
+		req = PUT(config.ui_host, config.ui_port, "/gateways/" + config.gateway_id + "/devices/0xa335d00019f5234e")
+		req.authorize(self.session)
+		req.body(json.dumps({
+			"refresh_time": 10
+		}))
+
+		response, content = req()
+		result = json.loads(content)
+		self.assertEqual(400, response.status)
+		self.assertEqual(400, result["code"])
+		self.assertEqual("invalid input data", result["message"])
+
+		# leave it non-updatable should work
+		req = PUT(config.ui_host, config.ui_port, "/gateways/" + config.gateway_id + "/devices/0xa335d00019f5234e")
+		req.authorize(self.session)
+		req.body(json.dumps({
+			"refresh_time": -1
+		}))
+
+		response, content = req()
+		result = json.loads(content)
+		self.assertEqual(200, response.status)
+		self.assertEqual("success", result["status"])
+		self.assertNotIn("refresh_time", result["data"])
+
 if __name__ == '__main__':
 	import sys
 	import taprunner

commit e3bc839babce4552b07fc199c1dddd2b7cabbb4b

JSONDeviceDeserializer: fix error messages
The message have inverted logic.

Fixes: 7bd90e93184b ("JSONDeviceDeserializer: apply refresh time with stricter rules")
Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

src/restui/JSONDeviceDeserializer.cpp
@@ -19,11 +19,11 @@ void BeeeOn::JSONDeviceDeserializer::applyRefresh(Device &device, int refresh) c
 {
 	if (device.hasRefresh() && refresh < 0) {
 		throw InvalidArgumentException(
-			"refresh_time must be negative for device " + device);
+			"refresh_time must not be negative for device " + device);
 	}
 	else if (!device.hasRefresh() && refresh >= 0) {
 		throw InvalidArgumentException(
-			"refresh_time must be non-negative for device " + device);
+			"refresh_time must be negative for device " + device);
 	}
 
 	device.setRefresh(refresh);