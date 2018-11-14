BeeeOn/server

commit f9cf1e8767e11a9416f481112a0e26e48fe6c65f

ControlService: provide method requestChange()
The method requestChange() ensures setting of a value for the selected
control instance.

Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

src/service/ControlServiceImpl.cpp
@@ -7,6 +7,7 @@ BEEEON_OBJECT_BEGIN(BeeeOn, ControlServiceImpl)
 BEEEON_OBJECT_CASTABLE(ControlService)
 BEEEON_OBJECT_REF("controlDao", &ControlServiceImpl::setControlDao)
 BEEEON_OBJECT_REF("deviceDao", &ControlServiceImpl::setDeviceDao)
+BEEEON_OBJECT_REF("gatewayRPC", &ControlServiceImpl::setGatewayRPC)
 BEEEON_OBJECT_REF("accessPolicy", &ControlServiceImpl::setAccessPolicy)
 BEEEON_OBJECT_REF("transactionManager", &ControlServiceImpl::setTransactionManager)
 BEEEON_OBJECT_HOOK("done", &ControlServiceImpl::fixupControls)
@@ -16,6 +17,9 @@ using namespace std;
 using namespace Poco;
 using namespace BeeeOn;
 
+static const Timespan REQUEST_TIMEOUT_MIN = 1 * Timespan::SECONDS;
+static const Timespan REQUEST_TIMEOUT_MAX = 2 * Timespan::MINUTES;
+
 void ControlServiceImpl::setDeviceDao(DeviceDao::Ptr dao)
 {
 	m_deviceDao = dao;
@@ -24,6 +28,11 @@ void ControlServiceImpl::setDeviceDao(DeviceDao::Ptr dao)
 void ControlServiceImpl::setControlDao(ControlDao::Ptr dao)
 {
 	m_controlDao = dao;
+}
+
+void ControlServiceImpl::setGatewayRPC(GatewayRPC::Ptr rpc)
+{
+	m_gatewayRPC = rpc;
 }
 
 void ControlServiceImpl::setAccessPolicy(ControlAccessPolicy::Ptr policy)
@@ -84,4 +93,100 @@ void ControlServiceImpl::fixupControls()
 				+ " control requests",
 			__FILE__, __LINE__);
 	}
+}
+
+void ControlServiceImpl::startRequest(
+		const UserPolicyContext &policyContext,
+		const Device &device,
+		Control &control,
+		double value)
+{
+	const Timestamp now;
+
+	Control::RequestedValue request(policyContext.user(), now, value);
+	control.setRequestedValue(request);
+
+	if (logger().debug()) {
+		logger().debug(
+			"requesting change of control " + control,
+			__FILE__, __LINE__);
+	}
+
+	m_controlDao->insert(request, control, device);
+}
+
+void ControlServiceImpl::cancelRequest(
+		const Device &device,
+		Control &control)
+{
+	const Timestamp now;
+
+	Control::RequestedValue oldRequest(control.requestedValue());
+	oldRequest.setResult(Control::RequestedValue::RESULT_FAILURE);
+	oldRequest.setFinishedAt(now);
+
+	if (logger().debug()) {
+		logger().debug(
+			"cancel current request and start another"
+			" for control "
+			+ control,
+			__FILE__, __LINE__);
+	}
+
+	m_controlDao->update(oldRequest, control, device);
+}
+
+ControlChangeHandler::Ptr ControlServiceImpl::doRequestChange(
+		Relation<Control, Device> &data,
+		double value,
+		const Poco::Timespan &timeout)
+{
+	if (timeout < REQUEST_TIMEOUT_MIN)
+		throw InvalidArgumentException("too short timeout for control change");
+	if (timeout > REQUEST_TIMEOUT_MAX)
+		throw InvalidArgumentException("too long timeout for control change");
+
+	Device device = data.base();
+	Control &control = data.target();
+
+	m_accessPolicy->assure(
+		ControlAccessPolicy::ACTION_USER_SET,
+		data, control, device);
+
+	if (!m_deviceDao->fetch(device, device.gateway()))
+		throw NotFoundException("device " + device);
+
+	if (!m_controlDao->fetch(control, device))
+		throw NotFoundException("control " + control);
+
+	const double recentValue = control.recentValue().value();
+	const auto &requestedValue = control.requestedValue();
+
+	if (recentValue == value) {
+		logger().debug("requesting to set last reported value"
+			" for control " + control + ", ignoring...",
+			__FILE__, __LINE__);
+
+		return nullptr;
+	}
+
+	if (requestedValue.value() == value && requestedValue.isActive()) {
+		logger().debug("requesting to set value that is already"
+			" in progress for control " + control
+			+ ", ignoring...",
+			__FILE__, __LINE__);
+
+		return nullptr;
+	}
+
+	if (requestedValue.isActive() || requestedValue.hasStarted())
+		cancelRequest(device, control);
+
+	startRequest(data, device, control, value);
+
+	ControlChangeHandler::Ptr handler = new ControlChangeHandler(
+			control, device, m_controlDao);
+	handler->setTransactionManager(transactionManager());
+
+	return handler;
 }

conf/config.d/factory-services.xml
@@ -90,6 +90,7 @@
 		<instance name="controlService" class="BeeeOn::ControlServiceImpl">
 			<set name="deviceDao" ref="deviceDao" />
 			<set name="controlDao" ref="controlDao" />
+			<set name="gatewayRPC" ref="gatewayRPC" />
 			<set name="transactionManager" ref="transactionManager" />
 			<set name="accessPolicy" ref="accessPolicy" />
 		</instance>

src/service/ControlService.h
@@ -2,6 +2,7 @@
 #define BEEEON_CONTROL_SERVICE_H
 
 #include <Poco/SharedPtr.h>
+#include <Poco/Timespan.h>
 
 #include "model/Control.h"
 #include "model/Device.h"
@@ -17,6 +18,9 @@ public:
 
 	virtual bool fetch(Relation<Control, Device> &data) = 0;
 	virtual void fetchMany(Relation<std::list<Control>, Device> &data) = 0;
+	virtual void requestChange(Relation<Control, Device> &data,
+			double value,
+			const Poco::Timespan &timeout) = 0;
 };
 
 }

src/service/ControlServiceImpl.h
@@ -5,7 +5,9 @@
 
 #include "dao/ControlDao.h"
 #include "dao/DeviceDao.h"
+#include "gws/ControlChangeHandler.h"
 #include "policy/ControlAccessPolicy.h"
+#include "rpc/GatewayRPC.h"
 #include "service/ControlService.h"
 #include "transaction/Transactional.h"
 
@@ -17,6 +19,7 @@ public:
 
 	void setDeviceDao(DeviceDao::Ptr dao);
 	void setControlDao(ControlDao::Ptr dao);
+	void setGatewayRPC(GatewayRPC::Ptr rpc);
 	void setAccessPolicy(ControlAccessPolicy::Ptr policy);
 
 	bool fetch(Relation<Control, Device> &data) override
@@ -29,15 +32,50 @@ public:
 		BEEEON_TRANSACTION(doFetchMany(data));
 	}
 
+	void requestChange(Relation<Control, Device> &data,
+			double value,
+			const Poco::Timespan &timeout) override
+	{
+		ControlChangeHandler::Ptr handler =
+			BEEEON_TRANSACTION_RETURN(
+				ControlChangeHandler::Ptr,
+				doRequestChange(data, value, timeout));
+
+		if (handler.isNull())
+			return;
+
+		m_gatewayRPC->updateActor(
+				handler,
+				handler->device().gateway(),
+				handler->device(),
+				handler->control().info(),
+				value,
+				timeout);
+	}
+
 	void fixupControls();
 
 protected:
 	bool doFetch(Relation<Control, Device> &data);
 	void doFetchMany(Relation<std::list<Control>, Device> &data);
+	ControlChangeHandler::Ptr doRequestChange(
+			Relation<Control, Device> &data,
+			double value,
+			const Poco::Timespan &timeout);
+
+	void startRequest(
+			const UserPolicyContext &policyContext,
+			const Device &device,
+			Control &control,
+			double value);
+	void cancelRequest(
+			const Device &device,
+			Control &control);
 
 private:
 	DeviceDao::Ptr m_deviceDao;
 	ControlDao::Ptr m_controlDao;
+	GatewayRPC::Ptr m_gatewayRPC;
 	ControlAccessPolicy::Ptr m_accessPolicy;
 };
 

commit a3ad67051e827dd868e5aba38f2b367265029833

ControlServiceImpl: check device is paired on requestChange()
Requesting of changes for controllable devices is possible only if
such device is active.

Fixes: f9cf1e8767e1 ("ControlService: provide method requestChange()")
Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

src/service/ControlServiceImpl.cpp
@@ -164,6 +164,9 @@ ControlChangeHandler::Ptr ControlServiceImpl::doRequestChange(
 	if (!m_deviceDao->fetch(device, device.gateway()))
 		throw NotFoundException("device " + device);
 
+	if (!device.status().active())
+		throw NotFoundException("device " + device + " is inactive");
+
 	if (!m_controlDao->fetch(control, device))
 		throw NotFoundException("control " + control);
 