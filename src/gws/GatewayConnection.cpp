BeeeOn/server

commit 29e826baa14b71e825bc2b673da7259365d6deaa

WebSocketConnection: log about creation and destruction
WebSocketConnection now logs when it is created and destroyed.
The GatewayConnection does not have to do it anymore (now it
would just duplicate this).

This change requires to remake the method id() to be non-virtual
because virtual methods cannot be called from constructor safely.

Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

src/gws/GatewayConnection.cpp
@@ -17,6 +17,7 @@ GatewayConnection::GatewayConnection(
 		const EnqueueReadable &enqueueReadable,
 		size_t maxMessageSize):
 	WebSocketConnection(
+		gatewayID.toString(),
 		webSocket,
 		reactor,
 		[enqueueReadable](WebSocketConnection::Ptr c) {
@@ -26,23 +27,10 @@ GatewayConnection::GatewayConnection(
 	m_gatewayID(gatewayID),
 	m_rateLimiter(rateLimiter)
 {
-	if (logger().debug()) {
-		logger().debug("gateway " + m_gatewayID.toString()
-				+ " connection created", __FILE__, __LINE__);
-	}
 }
 
 GatewayConnection::~GatewayConnection()
 {
-	if (logger().debug()) {
-		logger().debug("gateway " + m_gatewayID.toString()
-				+ " connection destroyed", __FILE__, __LINE__);
-	}
-}
-
-string GatewayConnection::id() const
-{
-	return m_gatewayID.toString();
 }
 
 GatewayID GatewayConnection::gatewayID() const

src/server/WebSocketConnection.cpp
@@ -17,20 +17,37 @@ using namespace BeeeOn;
 #define FRAME_OP_CONTROL_MASK 0x08
 
 WebSocketConnection::WebSocketConnection(
+		const string &id,
 		const WebSocket &webSocket,
 		SocketReactor &reactor,
 		const EnqueueReadable &enqueueReadable,
 		const size_t maxFrameSize):
+	m_id(id),
 	m_webSocket(webSocket),
 	m_reactor(reactor),
 	m_enqueueReadable(enqueueReadable),
 	m_maxFrameSize(maxFrameSize),
 	m_readableObserver(*this, &WebSocketConnection::onReadable)
 {
+	if (logger().information()) {
+		logger().information(
+			"created connection to " + this->id(),
+			__FILE__, __LINE__);
+	}
 }
 
 WebSocketConnection::~WebSocketConnection()
 {
+	if (logger().information()) {
+		logger().information(
+			"destroyed connection to " + id(),
+			__FILE__, __LINE__);
+	}
+}
+
+string WebSocketConnection::id() const
+{
+	return m_id;
 }
 
 void WebSocketConnection::addToReactor() const

src/server/WebSocketConnection.h
@@ -24,6 +24,7 @@ public:
 	typedef std::function<void(WebSocketConnection::Ptr)> EnqueueReadable;
 
 	WebSocketConnection(
+		const std::string &id,
 		const Poco::Net::WebSocket &webSocket,
 		Poco::Net::SocketReactor &reactor,
 		const EnqueueReadable &enqueueReadable,
@@ -39,7 +40,7 @@ protected:
 	 * @returns string identification of the connection that used
 	 * for logging and similar purposes.
 	 */
-	virtual std::string id() const = 0;
+	std::string id() const;
 
 	/**
 	 * @brief Receive a frame from the associated websocket.
@@ -94,6 +95,7 @@ private:
 	void checkOverflow(const size_t bufferSize, size_t length) const;
 
 private:
+	std::string m_id;
 	Poco::Net::WebSocket m_webSocket;
 	Poco::Net::SocketReactor &m_reactor;
 	EnqueueReadable m_enqueueReadable;

src/gws/GatewayConnection.h
@@ -83,11 +83,6 @@ public:
 
 protected:
 	/**
-	 * @returns ID of gateway this connection serves.
-	 */
-	std::string id() const override;
-
-	/**
 	 * @brief Filter pings by the GatewayRateLimiter.
 	 */
 	void handlePing(const Poco::Buffer<char> &request, size_t length) override;

commit fdb809d857fa103ce0a2bb5d174df16079e15d2b

WebSocketConnection: fix logging in constructor
Calling logger() from inside of WebSocketConnection would initialize
the logger to use logger BeeeOn::WebSocketConnection instead of
the inherited class. We have to delay the logger initialization.

The inherited class has to call afterConstructed() inside its constructor.
This way, the logger would be initialized correctly.

Fixes: 29e826baa14b ("WebSocketConnection: log about creation and destruction")
Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

src/gws/GatewayConnection.cpp
@@ -27,6 +27,7 @@ GatewayConnection::GatewayConnection(
 	m_gatewayID(gatewayID),
 	m_rateLimiter(rateLimiter)
 {
+	afterConstructed();
 }
 
 GatewayConnection::~GatewayConnection()

src/server/WebSocketConnection.cpp
@@ -29,6 +29,10 @@ WebSocketConnection::WebSocketConnection(
 	m_maxFrameSize(maxFrameSize),
 	m_readableObserver(*this, &WebSocketConnection::onReadable)
 {
+}
+
+void WebSocketConnection::afterConstructed() const
+{
 	if (logger().information()) {
 		logger().information(
 			"created connection to " + this->id(),

src/server/WebSocketConnection.h
@@ -38,6 +38,13 @@ public:
 
 protected:
 	/**
+	 * @brief To be called by the derived class in its constructor.
+	 * Inside this function it is already clear, of what type is the
+	 * pointer _this_.
+	 */
+	void afterConstructed() const;
+
+	/**
 	 * @returns string identification of the connection that used
 	 * for logging and similar purposes.
 	 */