BeeeOn/server

commit f0ff276ef8b983c35bda75dd2c0875bec46086c7

SlidingWindowRateLimiter: new class
SlidingWindowRateLimiter implements rate limiting of incoming message
of a single gateway. It keeps track of 1 hour traffic stored as 30 seconds
long slots. For each slot, the number of seen messages is accumulated.
The limits max-rate-per-minute and max-rate-per-hour are applied to
drop messages of too greedy gateways.

Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

src/policy/SlidingWindowRateLimiter.cpp
@@ -1,0 +1,202 @@
+#include <Poco/Clock.h>
+#include <Poco/Exception.h>
+#include <Poco/Logger.h>
+
+#include "di/Injectable.h"
+#include "policy/SlidingWindowRateLimiter.h"
+
+BEEEON_OBJECT_BEGIN(BeeeOn, SlidingWindowRateLimiterFactory)
+BEEEON_OBJECT_CASTABLE(GatewayRateLimiterFactory)
+BEEEON_OBJECT_PROPERTY("maxRatePerMinute", &SlidingWindowRateLimiterFactory::setMaxRatePerMinute)
+BEEEON_OBJECT_PROPERTY("maxRatePerHour", &SlidingWindowRateLimiterFactory::setMaxRatePerHour)
+BEEEON_OBJECT_HOOK("done", &SlidingWindowRateLimiterFactory::initialize)
+BEEEON_OBJECT_END(BeeeOn, SlidingWindowRateLimiterFactory)
+
+using namespace std;
+using namespace Poco;
+using namespace BeeeOn;
+
+SlidingWindowRateLimiter::SlidingWindowRateLimiter(
+		const GatewayID &id,
+		size_t maxRatePerMinute,
+		size_t maxRatePerHour):
+	GatewayRateLimiter(id),
+	m_maxRatePerMinute(maxRatePerMinute),
+	m_maxRatePerHour(maxRatePerHour),
+	m_overlimit(false)
+{
+}
+
+bool SlidingWindowRateLimiter::accept(const GWMessage::Ptr)
+{
+	return doAccept(Clock().microseconds());
+}
+
+bool SlidingWindowRateLimiter::accept()
+{
+	return doAccept(Clock().microseconds());
+}
+
+bool SlidingWindowRateLimiter::doAccept(const Poco::Timespan &now)
+{
+	FastMutex::ScopedLock guard(m_lock);
+
+	shiftWindow(now);
+
+	if (overlimit(now)) {
+		m_overlimit = true;
+		return false;
+	}
+
+	if (m_overlimit && logger().notice()) {
+		logger().notice(
+			"gateway " + gatewayID().toString()
+			+ " is not overlimit anymore"
+			+ " (with " + to_string(m_window.size()) + " entries)",
+			__FILE__, __LINE__);
+	}
+
+	m_overlimit = false;
+
+	auto result = m_window.emplace(align(now), 1);
+	if (!result.second)
+		result.first->second += 1;
+
+	return true;
+}
+
+Timespan SlidingWindowRateLimiter::align(const Timespan &timespan) const
+{
+	const int sec30 = timespan.seconds() / 30;
+
+	return {
+		timespan.days(),
+		timespan.hours(),
+		timespan.minutes(),
+		(30 * sec30) % 60,
+		0
+	};
+}
+
+bool SlidingWindowRateLimiter::overlimit(const Timespan &now) const
+{
+	static const auto ONE_MIN = 1 * Timespan::MINUTES;
+	static const auto ONE_HOUR = 1 * Timespan::HOURS;
+
+	if (overlimitGeneric(now, ONE_MIN, m_maxRatePerMinute, "limit-per-minute"))
+		return true;
+
+	return overlimitGeneric(now, ONE_HOUR, m_maxRatePerHour, "limit-per-hour");
+}
+
+bool SlidingWindowRateLimiter::overlimitGeneric(
+		const Timespan &now,
+		const Timespan &range,
+		const size_t maxLimit,
+		const string &label) const
+{
+	if (maxLimit == 0)
+		return false;
+
+	const auto sum = sumRange(now - range, now);
+	logger().debug(label + ": " + to_string(sum) + "/" + to_string(maxLimit));
+	if (sum < maxLimit)
+		return false;
+
+	if (!m_overlimit && logger().warning()) {
+		logger().warning(
+			"gateway " + gatewayID().toString()
+			+ " is over " + label
+			+ " (with " + to_string(m_window.size()) + " entries"
+			+ ", " + to_string(sum) + " requests)",
+			__FILE__, __LINE__);
+	}
+
+	return true;
+}
+
+size_t SlidingWindowRateLimiter::sumRange(
+		const Timespan &from,
+		const Timespan &to) const
+{
+	const auto end = m_window.upper_bound(to);
+	size_t sum = 0;
+
+	for (auto it = m_window.lower_bound(from); it != end; ++it)
+		sum += it->second;
+
+	return sum;
+}
+
+void SlidingWindowRateLimiter::shiftWindow(const Timespan &now)
+{
+	poco_assert_msg(
+		begin(m_window) == end(m_window)
+		|| now >= begin(m_window)->first,
+		"seems like time is not monotonic");
+
+	const auto upperBound = m_window.upper_bound(now - 1 * Timespan::HOURS);
+
+	if (logger().debug()) {
+		const auto dist = distance(begin(m_window), upperBound);
+
+		logger().debug(
+			"shifting window by " + to_string(dist)
+			+ " of total " + to_string(m_window.size()),
+			__FILE__, __LINE__);
+	}
+
+	m_window.erase(begin(m_window), upperBound);
+}
+
+SlidingWindowRateLimiterFactory::SlidingWindowRateLimiterFactory():
+	m_maxRatePerMinute(0),
+	m_maxRatePerHour(0)
+{
+}
+
+void SlidingWindowRateLimiterFactory::setMaxRatePerMinute(const int rate)
+{
+	if (rate < 0)
+		throw InvalidArgumentException("maxRatePerMinute must not be negative");
+
+
+	m_maxRatePerMinute = rate;
+}
+
+void SlidingWindowRateLimiterFactory::setMaxRatePerHour(const int rate)
+{
+	if (rate < 0)
+		throw InvalidArgumentException("maxRatePerHour must not be negative");
+
+	m_maxRatePerHour = rate;
+}
+
+GatewayRateLimiter::Ptr SlidingWindowRateLimiterFactory::create(const GatewayID &id)
+{
+	return new SlidingWindowRateLimiter(
+			id,
+			m_maxRatePerMinute,
+			m_maxRatePerHour);
+}
+
+void SlidingWindowRateLimiterFactory::initialize()
+{
+	logger().information(
+		"initializing sliding-window rate limiting of gateways to "
+		+ to_string(m_maxRatePerMinute) + " req/min"
+		+ ", " + to_string(m_maxRatePerHour) + " req/h",
+		__FILE__, __LINE__);
+
+	if (m_maxRatePerMinute == 0) {
+		logger().warning(
+			"maxRatePerMinute limiting is turned off",
+			__FILE__, __LINE__);
+	}
+
+	if (m_maxRatePerHour == 0) {
+		logger().warning(
+			"maxRatePerHour limiting is turned off",
+			__FILE__, __LINE__);
+	}
+}

src/CMakeLists.txt
@@ -122,6 +122,7 @@ file(GLOB SOURCES
 	${PROJECT_SOURCE_DIR}/policy/RuledSensorHistoryAccessPolicy.cpp
 	${PROJECT_SOURCE_DIR}/policy/SensorHistoryAccessPolicy.cpp
 	${PROJECT_SOURCE_DIR}/policy/SensorHistoryRules.cpp
+	${PROJECT_SOURCE_DIR}/policy/SlidingWindowRateLimiter.cpp
 	${PROJECT_SOURCE_DIR}/policy/DefaultAccessPolicy.cpp
 	${PROJECT_SOURCE_DIR}/policy/PolicyContext.cpp
 	${PROJECT_SOURCE_DIR}/policy/UserPolicyContext.cpp

src/policy/SlidingWindowRateLimiter.h
@@ -1,0 +1,165 @@
+#pragma once
+
+#include <map>
+
+#include <Poco/Mutex.h>
+#include <Poco/SharedPtr.h>
+#include <Poco/Timespan.h>
+
+#include "policy/GatewayRateLimiter.h"
+#include "util/Loggable.h"
+
+namespace BeeeOn {
+
+/**
+ * @brief SlidingWindowRateLimiter keeps track of a recent amount of messages
+ * received from a certain gateway. The amount of messages is defined by
+ * the size of the sliding window which is currently fixed to 1 hour.
+ * The SlidingWindowRateLimiter supports policing of messages based of
+ * traffic per minute and/or traffic per hour.
+ *
+ * Principle:
+ *
+ * - sliding window is divided into slots of size 30 seconds
+ *
+ * - for each slot, the number of seen messages is measured
+ *
+ * - when for the most recent minute (usually 2 slots) the measured
+ *   number of messages reaches the max-rate-per-minute limit, no more
+ *   messages are accepted until the sliding window is moved enough
+ *
+ * - when for the most recent hour (usually 120 slots) the measured
+ *   number of measured reaches the max-rate-per-hour limit, no more
+ *   messages are accepted until the sliding window is moved enough
+ *
+ * - if the max-rate-per-minute starts dropping, the max-rate-per-hour
+ *   is left intact and thus it is not being exhausted by high message
+ *   rate during a single minute
+ *
+ * Example:
+ *
+ * - consider max rates: 20 msg/min (M), 70 msg/h (H)
+ *
+ * <pre>
+ * slot: 0    30   60   90  120  150  180  210  240  270  300 ... 3600
+ * cnt:  |  0 | 15 |  5 |  8 | 12 |  4 | 16 |  4 |  6 |  0 |  ... | 5 |
+ *                    M         M         M    M    H    H
+ * </pre>
+ */
+class SlidingWindowRateLimiter :
+	Loggable,
+	public GatewayRateLimiter {
+public:
+	typedef Poco::SharedPtr<SlidingWindowRateLimiter> Ptr;
+
+	SlidingWindowRateLimiter(
+		const GatewayID &id,
+		size_t maxRatePerMinute,
+		size_t maxRatePerHour);
+
+	/**
+	 * @brief Accepting of all messages uses the same algorithm.
+	 * The call is thread-safe.
+	 *
+	 * @see SlidingWindowRateLimiter::doAccept()
+	 */
+	bool accept(const GWMessage::Ptr msg) override;
+
+	/**
+	 * @brief Accepting of all messages uses the same algorithm.
+	 * The call is thread-safe.
+	 *
+	 * @see SlidingWindowRateLimiter::doAccept()
+	 */
+	bool accept() override;
+
+protected:
+	/**
+	 * @brief Move the sliding window according to the current time
+	 * represented as Poco::Timespan and check whether the current
+	 * message fits into all limits.
+	 */
+	bool doAccept(const Poco::Timespan &now);
+
+	/**
+	 * @brief Align the given time to multiples of 30 seconds.
+	 */
+	Poco::Timespan align(const Poco::Timespan &timespan) const;
+
+	/**
+	 * @brief Test overlimit of the current message at the given
+	 * time point.
+	 */
+	bool overlimit(const Poco::Timespan &now) const;
+
+	/**
+	 * @brief Generic algorithm for any time window. It computes
+	 * number of messages received in the given time range until now.
+	 * If the amount is greater then the given maxLimit, it returns
+	 * true.
+	 *
+	 * @returns true if the current message is overlimit, otherwise false.
+	 */
+	bool overlimitGeneric(
+		const Poco::Timespan &now,
+		const Poco::Timespan &range,
+		const size_t maxLimit,
+		const std::string &label) const;
+
+	/**
+	 * @brief Sum messages amounts in the given time interval.
+	 */
+	size_t sumRange(const Poco::Timespan &b, const Poco::Timespan &e) const;
+
+	/**
+	 * @brief Remove old records from the sliding window. Records older
+	 * then 1 hour are simply erased.
+	 */
+	void shiftWindow(const Poco::Timespan &now);
+
+private:
+	const size_t m_maxRatePerMinute;
+	const size_t m_maxRatePerHour;
+	bool m_overlimit;
+	std::map<Poco::Timespan, size_t> m_window;
+	mutable Poco::FastMutex m_lock;
+};
+
+/**
+ * @brief Factory to create SlidingWindowRateLimiter instances
+ * with the shared configuration.
+ */
+class SlidingWindowRateLimiterFactory :
+	Loggable,
+	public GatewayRateLimiterFactory {
+public:
+	SlidingWindowRateLimiterFactory();
+
+	/**
+	 * @brief Configure max-rate-per-minute. The value 0 would
+	 * turn this limit off (it would be ignored).
+	 */
+	void setMaxRatePerMinute(const int rate);
+
+	/**
+	 * @brief Configure max-rate-per-hour. The value 0 would
+	 * turn this limit off (it would be ignored).
+	 */
+	void setMaxRatePerHour(const int rate);
+
+	/**
+	 * @returns new instance of SlidingWindowRateLimiter.
+	 */
+	GatewayRateLimiter::Ptr create(const GatewayID &id) override;
+
+	/**
+	 * @brief Log and validate configuration.
+	 */
+	void initialize();
+
+private:
+	size_t m_maxRatePerMinute;
+	size_t m_maxRatePerHour;
+};
+
+}

test/CMakeLists.txt
@@ -37,6 +37,7 @@ file(GLOB TEST_SOURCES
 	${PROJECT_SOURCE_DIR}/oauth2/GoogleOAuth2ProviderTest.cpp
 	${PROJECT_SOURCE_DIR}/oauth2/OpenIDUserDecoderTest.cpp
 	${PROJECT_SOURCE_DIR}/policy/SensorHistoryRulesTest.cpp
+	${PROJECT_SOURCE_DIR}/policy/SlidingWindowRateLimiterTest.cpp
 	${PROJECT_SOURCE_DIR}/server/CORSFilterTest.cpp
 	${PROJECT_SOURCE_DIR}/server/HTTPCustomHeadersFilterTest.cpp
 	${PROJECT_SOURCE_DIR}/server/SessionManagerTest.cpp

test/policy/SlidingWindowRateLimiterTest.cpp
@@ -1,0 +1,165 @@
+#include <cppunit/extensions/HelperMacros.h>
+
+#include "cppunit/BetterAssert.h"
+#include "policy/SlidingWindowRateLimiter.h"
+
+using namespace std;
+using namespace Poco;
+
+namespace BeeeOn {
+
+class SlidingWindowRateLimiterTest : public CppUnit::TestFixture {
+	CPPUNIT_TEST_SUITE(SlidingWindowRateLimiterTest);
+	CPPUNIT_TEST(testUnlimited);
+	CPPUNIT_TEST(testLimitPerMinute);
+	CPPUNIT_TEST(testLimitPerHour);
+	CPPUNIT_TEST(testLimitPerMinAndHour);
+	CPPUNIT_TEST_SUITE_END();
+public:
+	void testUnlimited();
+	void testLimitPerMinute();
+	void testLimitPerHour();
+	void testLimitPerMinAndHour();
+};
+
+CPPUNIT_TEST_SUITE_REGISTRATION(SlidingWindowRateLimiterTest);
+
+class TestableSlidingWindowRateLimiter : public SlidingWindowRateLimiter {
+public:
+	using SlidingWindowRateLimiter::doAccept;
+
+	TestableSlidingWindowRateLimiter(
+			size_t maxPerMinute,
+			size_t maxPerHour):
+		SlidingWindowRateLimiter(GatewayID::random(), maxPerMinute, maxPerHour)
+	{
+	}
+};
+
+/**
+ * @brief Test (nearly) all messages are accepted when both limit checks
+ * are turned off.
+ */
+void SlidingWindowRateLimiterTest::testUnlimited()
+{
+	TestableSlidingWindowRateLimiter limiter(0, 0);
+
+	CPPUNIT_ASSERT(limiter.doAccept(0 * Timespan::SECONDS));
+	CPPUNIT_ASSERT(limiter.doAccept(1 * Timespan::SECONDS));
+	CPPUNIT_ASSERT(limiter.doAccept(2 * Timespan::SECONDS));
+	CPPUNIT_ASSERT(limiter.doAccept(3 * Timespan::SECONDS));
+
+	for (Timespan::TimeDiff i = 4 * Timespan::SECONDS;
+			i < 1 * Timespan::HOURS;
+			i += 1 * Timespan::SECONDS)
+		CPPUNIT_ASSERT(limiter.doAccept(i));
+}
+
+/**
+ * @brief Test limiting of message rate to max 5 msg/min. Everytime
+ * the limit is reached, no more messages are accepted until the
+ * sliding window is moved (time advances). When the sliding window
+ * is moved, messages are accepted again.
+ */
+void SlidingWindowRateLimiterTest::testLimitPerMinute()
+{
+	TestableSlidingWindowRateLimiter limiter(5, 0);
+
+	CPPUNIT_ASSERT(limiter.doAccept(0 * Timespan::SECONDS));
+	CPPUNIT_ASSERT(limiter.doAccept(1 * Timespan::SECONDS));
+	CPPUNIT_ASSERT(limiter.doAccept(2 * Timespan::SECONDS));
+	CPPUNIT_ASSERT(limiter.doAccept(3 * Timespan::SECONDS));
+	CPPUNIT_ASSERT(limiter.doAccept(4 * Timespan::SECONDS));
+	CPPUNIT_ASSERT(!limiter.doAccept(5 * Timespan::SECONDS));
+
+	for (Timespan::TimeDiff i = 6 * Timespan::SECONDS;
+			i < 1 * Timespan::MINUTES;
+			i += 1 * Timespan::SECONDS)
+		CPPUNIT_ASSERT(!limiter.doAccept(i));
+
+	CPPUNIT_ASSERT(limiter.doAccept(2 * Timespan::MINUTES));
+	CPPUNIT_ASSERT(limiter.doAccept(2 * Timespan::MINUTES + 1));
+	CPPUNIT_ASSERT(limiter.doAccept(2 * Timespan::MINUTES + 2));
+	CPPUNIT_ASSERT(limiter.doAccept(2 * Timespan::MINUTES + 3));
+	CPPUNIT_ASSERT(limiter.doAccept(2 * Timespan::MINUTES + 4));
+	CPPUNIT_ASSERT(!limiter.doAccept(2 * Timespan::MINUTES + 5));
+}
+
+/**
+ * @brief Test limiting of message rate to max 5 msg/hour. Everytime
+ * the limit is reached, no more messages are accepted until the
+ * sliding window is moved (time advances). When the sliding window
+ * is moved, messages are accepted again.
+ */
+void SlidingWindowRateLimiterTest::testLimitPerHour()
+{
+	TestableSlidingWindowRateLimiter limiter(0, 5);
+
+	CPPUNIT_ASSERT(limiter.doAccept(0 * Timespan::MINUTES));
+	CPPUNIT_ASSERT(limiter.doAccept(1 * Timespan::MINUTES));
+	CPPUNIT_ASSERT(limiter.doAccept(2 * Timespan::MINUTES));
+	CPPUNIT_ASSERT(limiter.doAccept(3 * Timespan::MINUTES));
+	CPPUNIT_ASSERT(limiter.doAccept(4 * Timespan::MINUTES));
+	CPPUNIT_ASSERT(!limiter.doAccept(5 * Timespan::MINUTES));
+
+	for (Timespan::TimeDiff i = 6 * Timespan::MINUTES;
+			i < 1 * Timespan::HOURS;
+			i += 1 * Timespan::MINUTES)
+		CPPUNIT_ASSERT(!limiter.doAccept(i));
+
+	CPPUNIT_ASSERT(limiter.doAccept(2 * Timespan::HOURS));
+	CPPUNIT_ASSERT(limiter.doAccept(2 * Timespan::HOURS + 1));
+	CPPUNIT_ASSERT(limiter.doAccept(2 * Timespan::HOURS + 2));
+	CPPUNIT_ASSERT(limiter.doAccept(2 * Timespan::HOURS + 3));
+	CPPUNIT_ASSERT(limiter.doAccept(2 * Timespan::HOURS + 4));
+	CPPUNIT_ASSERT(!limiter.doAccept(2 * Timespan::HOURS + 5));
+}
+
+/**
+ * @brief Test combination of using both rate limits set to
+ * 3 msg/min, 10 msg/hour.
+ */
+void SlidingWindowRateLimiterTest::testLimitPerMinAndHour()
+{
+	TestableSlidingWindowRateLimiter limiter(3, 10);
+
+	CPPUNIT_ASSERT(limiter.doAccept(0 * Timespan::MINUTES));
+	CPPUNIT_ASSERT(limiter.doAccept(1 * Timespan::MINUTES));
+	CPPUNIT_ASSERT(limiter.doAccept(2 * Timespan::MINUTES));
+	CPPUNIT_ASSERT(limiter.doAccept(3 * Timespan::MINUTES));
+
+	CPPUNIT_ASSERT(limiter.doAccept(4 * Timespan::MINUTES + 1));
+
+	// hard minute...
+	CPPUNIT_ASSERT(limiter.doAccept(4 * Timespan::MINUTES + 2));
+	CPPUNIT_ASSERT(limiter.doAccept(4 * Timespan::MINUTES + 3));
+
+	// hour rate is not affected by the following (refusing) calls
+	CPPUNIT_ASSERT(!limiter.doAccept(4 * Timespan::MINUTES + 4));
+	CPPUNIT_ASSERT(!limiter.doAccept(4 * Timespan::MINUTES + 5));
+	CPPUNIT_ASSERT(!limiter.doAccept(4 * Timespan::MINUTES + 6));
+
+	// now we can send again
+	CPPUNIT_ASSERT(limiter.doAccept(6 * Timespan::MINUTES));
+
+	// another hard minute, but hour limit is applied first...
+	CPPUNIT_ASSERT(limiter.doAccept(7 * Timespan::MINUTES + 1));
+	CPPUNIT_ASSERT(limiter.doAccept(7 * Timespan::MINUTES + 2));
+	CPPUNIT_ASSERT(!limiter.doAccept(7 * Timespan::MINUTES + 3));
+	CPPUNIT_ASSERT(!limiter.doAccept(7 * Timespan::MINUTES + 4));
+
+	// nearly allowed by hour limit again
+	CPPUNIT_ASSERT(!limiter.doAccept(1 * Timespan::HOURS - 1));
+
+	// new 1 allowed per hour, consume it
+	CPPUNIT_ASSERT(limiter.doAccept(1 * Timespan::HOURS));
+	CPPUNIT_ASSERT(!limiter.doAccept(1 * Timespan::HOURS + 1));
+
+	// we are getting 3 requests from 3 shifted minutes per hour...
+	CPPUNIT_ASSERT(limiter.doAccept(1 * Timespan::HOURS + 3 * Timespan::MINUTES));
+	CPPUNIT_ASSERT(limiter.doAccept(1 * Timespan::HOURS + 3 * Timespan::MINUTES + 1));
+	CPPUNIT_ASSERT(limiter.doAccept(1 * Timespan::HOURS + 3 * Timespan::MINUTES + 2));
+	CPPUNIT_ASSERT(!limiter.doAccept(1 * Timespan::HOURS + 3 * Timespan::MINUTES + 3));
+}
+
+}

commit a0b525834f529ba285bc51de10a6b0db5fd74d33

SlidingWindowRateLimiter: fix debug log
The forgotten debug log was in bad form.

Fixes: f0ff276ef8b9 ("SlidingWindowRateLimiter: new class")
Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

src/policy/SlidingWindowRateLimiter.cpp
@@ -99,7 +99,13 @@ bool SlidingWindowRateLimiter::overlimitGeneric(
 		return false;
 
 	const auto sum = sumRange(now - range, now);
-	logger().debug(label + ": " + to_string(sum) + "/" + to_string(maxLimit));
+
+	if (logger().debug()) {
+		logger().debug(
+			label + ": " + to_string(sum) + "/" + to_string(maxLimit),
+			__FILE__, __LINE__);
+	}
+
 	if (sum < maxLimit)
 		return false;
 