BeeeOn/server

commit 0b0c4f1431cc5fc944ee25f5bc3fa161ba5a0b5e

WebSocketConnection: extract new abstract class
The WebSocketConnection implements the common logic needed for
management of long running websocket connections.

Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

src/server/WebSocketConnection.cpp
@@ -1,0 +1,172 @@
+#include <Poco/Exception.h>
+#include <Poco/Logger.h>
+#include <Poco/Nullable.h>
+#include <Poco/NumberFormatter.h>
+
+#include <Poco/Net/NetException.h>
+
+#include "server/WebSocketConnection.h"
+
+using namespace std;
+using namespace Poco;
+using namespace Poco::Net;
+using namespace BeeeOn;
+
+/// @see https://tools.ietf.org/html/rfc6455#section-5.5
+#define CONTROL_PAYLOAD_LIMIT 125
+#define FRAME_OP_CONTROL_MASK 0x08
+
+WebSocketConnection::WebSocketConnection(
+		const WebSocket &webSocket,
+		SocketReactor &reactor,
+		const EnqueueReadable &enqueueReadable,
+		const size_t maxFrameSize):
+	m_webSocket(webSocket),
+	m_reactor(reactor),
+	m_enqueueReadable(enqueueReadable),
+	m_maxFrameSize(maxFrameSize),
+	m_readableObserver(*this, &WebSocketConnection::onReadable)
+{
+}
+
+WebSocketConnection::~WebSocketConnection()
+{
+}
+
+void WebSocketConnection::addToReactor() const
+{
+	m_reactor.addEventHandler(m_webSocket, m_readableObserver);
+}
+
+void WebSocketConnection::removeFromReactor() const
+{
+	m_reactor.removeEventHandler(m_webSocket, m_readableObserver);
+}
+
+Nullable<string> WebSocketConnection::receiveFrame()
+{
+	Buffer<char> buffer(m_maxFrameSize);
+	int flags = 0;
+
+	int ret = m_webSocket.receiveFrame(
+			buffer.begin(), buffer.size(), flags);
+
+	const int opcode = flags & WebSocket::FRAME_OP_BITMASK;
+
+	if (ret <= 0 || opcode == WebSocket::FRAME_OP_CLOSE)
+		throw ConnectionResetException("received opcode CLOSE");
+
+	const size_t length = static_cast<size_t>(ret);
+
+	if (logger().trace()) {
+		logger().dump(
+			"received frame of size "
+			+ to_string(length) + " B ("
+			+ NumberFormatter::formatHex(flags, true) + ")"
+			+ " from " + id(),
+			buffer.begin(),
+			length,
+			Message::PRIO_TRACE);
+	}
+	else if (logger().debug()) {
+		logger().debug(
+			"received frame of size "
+			+ to_string(length) + " B ("
+			+ NumberFormatter::formatHex(flags, true) + ")"
+			+ " from " + id(),
+			__FILE__, __LINE__);
+	}
+
+	if (opcode & FRAME_OP_CONTROL_MASK && length > CONTROL_PAYLOAD_LIMIT)
+		throw ProtocolException("too long payload for a control frame");
+
+	switch (opcode) {
+	case WebSocket::FRAME_OP_TEXT:
+		if (!(flags & WebSocket::FRAME_FLAG_FIN))
+			throw ProtocolException("multi-fragment messages are unsupported");
+
+		return string{buffer.begin(), length};
+
+	case WebSocket::FRAME_OP_PING:
+		handlePing(buffer, length);
+		break;
+
+	default:
+		throw ProtocolException("unhandled websocket opcode: "
+				+ NumberFormatter::formatHex(opcode, true));
+	}
+
+	static const Nullable<string> null;
+	return null;
+}
+
+void WebSocketConnection::handlePing(const Buffer<char> &request, size_t length)
+{
+	sendPong(request, length);
+}
+
+void WebSocketConnection::sendFrame(const string &buffer, size_t length)
+{
+	checkBuffer(buffer, length);
+
+	if (logger().trace()) {
+		logger().dump(
+			"sending frame of size " + to_string(length) + " B"
+			+ " to " + id(),
+			buffer.data(),
+			length,
+			Message::PRIO_TRACE);
+	}
+	else if (logger().debug()) {
+		logger().debug(
+			"sending frame of size " + to_string(length) + " B"
+			+ " to " + id(),
+			__FILE__, __LINE__);
+	}
+
+	FastMutex::ScopedLock guard(m_sendLock);
+	m_webSocket.sendFrame(buffer.data(), buffer.length());
+}
+
+void WebSocketConnection::sendPong(const Buffer<char> &request, size_t length)
+{
+	checkBuffer(request, length);
+
+	if (logger().trace()) {
+		logger().dump(
+			"reply PONG frame of size "
+			+ to_string(request.size())
+			+ " to " + id(),
+			request.begin(),
+			length,
+			Message::PRIO_TRACE);
+	}
+	else if (logger().debug()) {
+		logger().debug(
+			"reply PONG frame of size "
+			+ to_string(request.size())
+			+ " to " + id(),
+			__FILE__, __LINE__);
+	}
+
+	FastMutex::ScopedLock guard(m_sendLock);
+	m_webSocket.sendFrame(
+		request.begin(),
+		length,
+		WebSocket::FRAME_OP_PONG | WebSocket::FRAME_FLAG_FIN);
+}
+
+void WebSocketConnection::checkOverflow(const size_t size, size_t length) const
+{
+	if (length > size) {
+		throw RangeException(
+			"length " + to_string(length)
+			+ " is greater than buffer size "
+			+ to_string(size));
+	}
+}
+
+void WebSocketConnection::onReadable(const AutoPtr<ReadableNotification> &notification)
+{
+	m_enqueueReadable(AutoPtr<WebSocketConnection>(this, true));
+}

src/CMakeLists.txt
@@ -151,6 +151,7 @@ file(GLOB SOURCES
 	${PROJECT_SOURCE_DIR}/server/AccessLevel.cpp
 	${PROJECT_SOURCE_DIR}/server/XmlRequestHandler.cpp
 	${PROJECT_SOURCE_DIR}/server/SocketServer.cpp
+	${PROJECT_SOURCE_DIR}/server/WebSocketConnection.cpp
 	${PROJECT_SOURCE_DIR}/server/WebSocketRequestHandler.cpp
 	${PROJECT_SOURCE_DIR}/server/WebSocketServer.cpp
 	${PROJECT_SOURCE_DIR}/service/ControlService.cpp

src/gws/GatewayConnection.cpp
@@ -9,10 +9,6 @@ using namespace Poco;
 using namespace Poco::Net;
 using namespace BeeeOn;
 
-/// @see https://tools.ietf.org/html/rfc6455#section-5.5
-#define CONTROL_PAYLOAD_LIMIT 125
-#define FRAME_OP_CONTROL_MASK 0x08
-
 GatewayConnection::GatewayConnection(
 		const GatewayID &gatewayID,
 		const WebSocket &webSocket,
@@ -20,13 +16,15 @@ GatewayConnection::GatewayConnection(
 		GatewayRateLimiter::Ptr rateLimiter,
 		const EnqueueReadable &enqueueReadable,
 		size_t maxMessageSize):
+	WebSocketConnection(
+		webSocket,
+		reactor,
+		[enqueueReadable](WebSocketConnection::Ptr c) {
+			enqueueReadable(c.cast<GatewayConnection>());
+		},
+		maxMessageSize),
 	m_gatewayID(gatewayID),
-	m_webSocket(webSocket),
-	m_reactor(reactor),
-	m_rateLimiter(rateLimiter),
-	m_maxFrameSize(maxMessageSize),
-	m_enqueueReadable(enqueueReadable),
-	m_readableObserver(*this, &GatewayConnection::onReadable)
+	m_rateLimiter(rateLimiter)
 {
 	if (logger().debug()) {
 		logger().debug("gateway " + m_gatewayID.toString()
@@ -42,6 +40,11 @@ GatewayConnection::~GatewayConnection()
 	}
 }
 
+string GatewayConnection::id() const
+{
+	return m_gatewayID.toString();
+}
+
 GatewayID GatewayConnection::gatewayID() const
 {
 	return m_gatewayID;
@@ -50,16 +53,6 @@ GatewayID GatewayConnection::gatewayID() const
 GatewayRateLimiter::Ptr GatewayConnection::rateLimiter() const
 {
 	return m_rateLimiter;
-}
-
-void GatewayConnection::addToReactor() const
-{
-	m_reactor.addEventHandler(m_webSocket, m_readableObserver);
-}
-
-void GatewayConnection::removeFromReactor() const
-{
-	m_reactor.removeEventHandler(m_webSocket, m_readableObserver);
 }
 
 void GatewayConnection::updateLastReceiveTime()
@@ -91,60 +84,22 @@ GWMessage::Ptr GatewayConnection::filterMessage(GWMessage::Ptr msg)
 
 GWMessage::Ptr GatewayConnection::receiveMessage()
 {
-	Buffer<char> buffer(m_maxFrameSize);
-	int flags;
+	Nullable<string> message;
 
-	int ret = m_webSocket.receiveFrame(
-			buffer.begin(), buffer.size(), flags);
-
-	const int opcode = flags & WebSocket::FRAME_OP_BITMASK;
-
-	if (ret <= 0 || opcode == WebSocket::FRAME_OP_CLOSE)
-		throw ConnectionResetException(m_gatewayID.toString());
-
-	const size_t length = static_cast<size_t>(ret);
-
-	if (logger().trace()) {
-		logger().dump(
-			"received frame of size "
-			+ to_string(length) + " B ("
-			+ NumberFormatter::formatHex(flags, true) + ")"
-			+ " from " + m_gatewayID.toString(),
-			buffer.begin(),
-			length,
-			Message::PRIO_TRACE);
+	try {
+		message = receiveFrame();
 	}
-	else if (logger().debug()) {
-		logger().debug(
-			"received frame of size "
-			+ to_string(length) + " B ("
-			+ NumberFormatter::formatHex(flags, true) + ")"
-			+ " from " + m_gatewayID.toString(),
-			__FILE__, __LINE__);
+	catch (const ProtocolException &e) {
+		updateLastReceiveTime();
+		e.rethrow();
 	}
 
 	updateLastReceiveTime();
 
-	if (opcode & FRAME_OP_CONTROL_MASK && ret > CONTROL_PAYLOAD_LIMIT)
-		throw ProtocolException("too long payload for a control frame");
+	if (message.isNull())
+		return nullptr;
 
-	switch (opcode) {
-	case WebSocket::FRAME_OP_TEXT:
-		if (!(flags & WebSocket::FRAME_FLAG_FIN))
-			throw ProtocolException("multi-fragment messages are unsupported");
-
-		return filterMessage(GWMessage::fromJSON({buffer.begin(), length}));
-
-	case WebSocket::FRAME_OP_PING:
-		handlePing(buffer, length);
-		break;
-
-	default:
-		throw ProtocolException("unhandled websocket opcode: "
-				+ NumberFormatter::formatHex(opcode, true));
-	}
-
-	return nullptr;
+	return filterMessage(GWMessage::fromJSON(message));
 }
 
 void GatewayConnection::handlePing(const Buffer<char> &request, size_t length)
@@ -162,64 +117,8 @@ void GatewayConnection::handlePing(const Buffer<char> &request, size_t length)
 	sendPong(request, length);
 }
 
-void GatewayConnection::sendPong(const Buffer<char> &request, size_t length)
-{
-	if (logger().trace()) {
-		logger().dump(
-			"reply PONG frame of size "
-			+ to_string(length)
-			+ " to " + m_gatewayID.toString(),
-			request.begin(),
-			length,
-			Message::PRIO_TRACE);
-	}
-	else if (logger().debug()) {
-		logger().debug(
-			"reply PONG frame of size "
-			+ to_string(length)
-			+ " to " + m_gatewayID.toString(),
-			__FILE__, __LINE__);
-	}
-
-	FastMutex::ScopedLock guard(m_sendMutex);
-
-	m_webSocket.sendFrame(
-		request.begin(),
-		length,
-		WebSocket::FRAME_OP_PONG | WebSocket::FRAME_FLAG_FIN);
-}
-
-void GatewayConnection::sendFrame(const string &msg)
-{
-	FastMutex::ScopedLock guard(m_sendMutex);
-
-	if (logger().trace()) {
-		logger().dump(
-			"sending frame of size " + to_string(msg.size()) + " B"
-			+ " to " + m_gatewayID.toString(),
-			msg.data(),
-			msg.size(),
-			Message::PRIO_TRACE);
-	}
-	else if (logger().debug()) {
-		logger().debug(
-			"sending frame of size " + to_string(msg.size()) + " B"
-			+ " to " + m_gatewayID.toString(),
-			__FILE__, __LINE__);
-	}
-
-	m_webSocket.sendFrame(msg.c_str(), msg.length());
-}
-
 void GatewayConnection::sendMessage(const GWMessage::Ptr message)
 {
 	const auto &msg = message->toString();
-	sendFrame(msg);
+	sendFrame(msg, msg.size());
 }
-
-void GatewayConnection::onReadable(const AutoPtr<ReadableNotification> &notification)
-{
-	m_enqueueReadable(AutoPtr<GatewayConnection>(this, true));
-}
-
-

src/gws/GatewayConnection.h
@@ -3,19 +3,13 @@
 #include <functional>
 
 #include <Poco/AutoPtr.h>
-#include <Poco/Buffer.h>
-#include <Poco/NObserver.h>
-#include <Poco/RefCountedObject.h>
 #include <Poco/SharedPtr.h>
 #include <Poco/Timestamp.h>
-#include <Poco/Net/SocketNotification.h>
-#include <Poco/Net/SocketReactor.h>
-#include <Poco/Net/WebSocket.h>
 
 #include "gwmessage/GWMessage.h"
 #include "model/GatewayID.h"
 #include "policy/GatewayRateLimiter.h"
-#include "util/Loggable.h"
+#include "server/WebSocketConnection.h"
 
 namespace BeeeOn {
 
@@ -46,9 +40,7 @@ namespace BeeeOn {
  *   arrived within the timeout and in the meantime no other message came
  *   from the gateway.)
  */
-class GatewayConnection :
-		public Poco::RefCountedObject,
-		public Loggable {
+class GatewayConnection : public WebSocketConnection {
 public:
 	typedef Poco::AutoPtr<GatewayConnection> Ptr;
 	typedef std::function<void(GatewayConnection::Ptr)> EnqueueReadable;
@@ -75,16 +67,6 @@ public:
 	GatewayRateLimiter::Ptr rateLimiter() const;
 
 	/**
-	 * @brief Register GatewayConnection as an event handler to the SocketReactor.
-	 */
-	void addToReactor() const;
-
-	/**
-	 * @brief Unregister GatewayConnection as an event handler from the SocketReactor.
-	 */
-	void removeFromReactor() const;
-
-	/**
 	 * @brief Returns timestamp of the last received message.
 	 */
 	Poco::Timestamp lastReceiveTime() const;
@@ -101,51 +83,29 @@ public:
 
 protected:
 	/**
-	 * @brief Filter pings by the GatewayRateLimiter.
+	 * @returns ID of gateway this connection serves.
 	 */
-	void handlePing(const Poco::Buffer<char> &request, size_t length);
+	std::string id() const override;
 
 	/**
-	 * @brief Send the given message via the associated
-	 * websocket as a single frame.
+	 * @brief Filter pings by the GatewayRateLimiter.
 	 */
-	void sendFrame(const std::string &buffer);
+	void handlePing(const Poco::Buffer<char> &request, size_t length) override;
 
 private:
 	GWMessage::Ptr filterMessage(GWMessage::Ptr message);
-
-	/**
-	 * @brief Sends back the PONG frame. Such frame must contain the same
-	 * application data as it was received.
-	 *
-	 * @see https://tools.ietf.org/html/rfc6455#section-5.5.2
-	 */
-	void sendPong(const Poco::Buffer<char> &request, size_t length);
 
 	/**
 	 * @brief Set timestamp of the last received message.
 	 */
 	void updateLastReceiveTime();
 
-	/**
-	 * @brief The Callback method invoked from the reactor thread.
-	 *
-	 * This method is intended just to determine there are data on the socket.
-	 * The data reading takes place in the thread pool of the GatewayCommunicator.
-	 */
-	void onReadable(const Poco::AutoPtr<Poco::Net::ReadableNotification> &notification);
-
 private:
 	GatewayID m_gatewayID;
-	Poco::Net::WebSocket m_webSocket;
-	Poco::Net::SocketReactor &m_reactor;
 	GatewayRateLimiter::Ptr m_rateLimiter;
-	size_t m_maxFrameSize;
 	EnqueueReadable m_enqueueReadable;
 	Poco::Timestamp m_lastReceiveTime;
 	mutable Poco::FastMutex m_lastReceiveTimeMutex;
-	Poco::FastMutex m_sendMutex;
-	Poco::NObserver<GatewayConnection, Poco::Net::ReadableNotification> m_readableObserver;
 };
 
 }

src/server/WebSocketConnection.h
@@ -1,0 +1,105 @@
+#pragma once
+
+#include <functional>
+
+#include <Poco/AutoPtr.h>
+#include <Poco/Buffer.h>
+#include <Poco/Mutex.h>
+#include <Poco/NObserver.h>
+#include <Poco/RefCountedObject.h>
+
+#include <Poco/Net/SocketNotification.h>
+#include <Poco/Net/SocketReactor.h>
+#include <Poco/Net/WebSocket.h>
+
+#include "util/Loggable.h"
+
+namespace BeeeOn {
+
+class WebSocketConnection :
+	public Poco::RefCountedObject,
+	protected Loggable {
+public:
+	typedef Poco::AutoPtr<WebSocketConnection> Ptr;
+	typedef std::function<void(WebSocketConnection::Ptr)> EnqueueReadable;
+
+	WebSocketConnection(
+		const Poco::Net::WebSocket &webSocket,
+		Poco::Net::SocketReactor &reactor,
+		const EnqueueReadable &enqueueReadable,
+		const size_t maxFrameSize);
+
+	virtual ~WebSocketConnection();
+
+	void addToReactor() const;
+	void removeFromReactor() const;
+
+protected:
+	/**
+	 * @returns string identification of the connection that used
+	 * for logging and similar purposes.
+	 */
+	virtual std::string id() const = 0;
+
+	/**
+	 * @brief Receive a frame from the associated websocket.
+	 * @returns null if the received frame was a control frame,
+	 * otherwise it returns the received payload.
+	 */
+	Poco::Nullable<std::string> receiveFrame();
+
+	/**
+	 * @brief Handle incoming ping request. The default action is
+	 * to reply pong.
+	 */
+	virtual void handlePing(const Poco::Buffer<char> &request, size_t length);
+
+	/**
+	 * @brief Send the given buffer of the given length via
+	 * the associated websocket as a single frame.
+	 */
+	void sendFrame(const std::string &buffer, size_t length);
+
+	/**
+	 * @brief Sends back the PONG frame. Such frame must contain the same
+	 * application data as it was received.
+	 *
+	 * @see https://tools.ietf.org/html/rfc6455#section-5.5.2
+	 */
+	void sendPong(const Poco::Buffer<char> &request, size_t length);
+
+	/**
+	 * @brief Check that the length does not overflow hte buffer.
+	 * @throws Poco::RangeException in case of buffer overflow
+	 */
+	template <typename Buf>
+	void checkBuffer(const Buf &buffer, size_t length) const
+	{
+		checkOverflow(buffer.size(), length);
+	}
+
+	/**
+	 * @brief The Callback method invoked from the reactor thread.
+	 *
+	 * This method is intended just to determine there are data on the socket.
+	 * The data reading takes place in the thread pool of the GatewayCommunicator.
+	 */
+	void onReadable(const Poco::AutoPtr<Poco::Net::ReadableNotification> &notification);
+
+private:
+	/**
+	 * @brief Implementation of buffer overflow test.
+	 * @see checkBuffer()
+	 */
+	void checkOverflow(const size_t bufferSize, size_t length) const;
+
+private:
+	Poco::Net::WebSocket m_webSocket;
+	Poco::Net::SocketReactor &m_reactor;
+	EnqueueReadable m_enqueueReadable;
+	size_t m_maxFrameSize;
+	Poco::FastMutex m_sendLock;
+	Poco::NObserver<WebSocketConnection, Poco::Net::ReadableNotification> m_readableObserver;
+};
+
+}

commit c7f15f734fed4b61c453d8e9ac8f92db4b8d0bee

WebSocketConnection: fix length used for sending
We have to use the length argument and not value buffer.length().

Fixes: 0b0c4f1431cc ("WebSocketConnection: extract new abstract class")
Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

src/server/WebSocketConnection.cpp
@@ -125,7 +125,7 @@ void WebSocketConnection::sendFrame(const string &buffer, size_t length)
 	}
 
 	FastMutex::ScopedLock guard(m_sendLock);
-	m_webSocket.sendFrame(buffer.data(), buffer.length());
+	m_webSocket.sendFrame(buffer.data(), length);
 }
 
 void WebSocketConnection::sendPong(const Buffer<char> &request, size_t length)