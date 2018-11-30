BeeeOn/server

commit e457ca2124b7dac818aef189ad3c1f83c55c4b74

GatewayCommunicator: measure handling of messages
Report the duration of the handling process into the log.

Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

src/gws/GatewayCommunicator.cpp
@@ -1,3 +1,4 @@
+#include <Poco/Clock.h>
 #include <Poco/Logger.h>
 #include <Poco/Net/WebSocket.h>
 #include <Poco/Net/NetException.h>
@@ -231,6 +232,7 @@ void GatewayCommunicator::handleConnectionReadable(GatewayConnection::Ptr connec
 	});
 
 	Thread::current()->setName("worker-" + connection->gatewayID().toString());
+	const Clock started;
 
 	try {
 		GWMessage::Ptr message = connection->receiveMessage();
@@ -244,7 +246,12 @@ void GatewayCommunicator::handleConnectionReadable(GatewayConnection::Ptr connec
 	catch (const Exception &e) {
 		logger().log(e, __FILE__, __LINE__);
 		removeGateway(connection->gatewayID());
-		return;
+	}
+
+	if (logger().information()) {
+		logger().information("duration: "
+			+ to_string(started.elapsed()) + "us",
+			__FILE__, __LINE__);
 	}
 }
 

commit 8f5b65f3191378fd3f1d32c5745c34b8d0d8550f

GatewayCommunicator: fix omitted measure of processing duration
If a low-level message comes (nullptr), the duration was not reported
due to premature return.

Fixes: e457ca2124b7 ("GatewayCommunicator: measure handling of messages")
Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

src/gws/GatewayCommunicator.cpp
@@ -269,10 +269,8 @@ void GatewayCommunicator::handleConnectionReadable(GatewayConnection::Ptr connec
 		GWMessage::Ptr message = connection->receiveMessage();
 		m_reactor.addEventHandler(connection->socket(), m_readableObserver);
 
-		if (message.isNull())
-			return;
-
-		m_messageHandler->handle(message, connection->gatewayID());
+		if (!message.isNull())
+			m_messageHandler->handle(message, connection->gatewayID());
 	}
 	BEEEON_CATCH_CHAIN_ACTION(logger(), removeGateway(connection->gatewayID()));
 