09b98f788264b32d01a7b8399a0a0eb746d8fed2
Jan Viktorin <iviktorin@fit.vutbr.cz>
2018-12-04 20:41:05 +0100

    BuiltinRestHandler: document REST-UI entry methods
    Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

--- src/rest/BuiltinRestHandler.h
+++ src/rest/BuiltinRestHandler.h
@@ -9,7 +9,18 @@ class BuiltinRestHandler : public JSONRestHandler {
 public:
 	BuiltinRestHandler();
 
+	/**
+	 * @brief It always returns HTTP code 401 and generates HTTP header
+	 * WWW-Authenticate: Basic realm="BeeeOn Server" (the realm is configurable
+	 * via the used RestRouter).
+	 * @throws Poco::Net::NotAuthenticatedException - always
+	 */
 	void unauthorized(RestFlow &flow);
+
+	/**
+	 * @brief It always returns HTTP code 404.
+	 * @throws Poco::NotFoundException - always
+	 */
 	void noRoute(RestFlow &flow);
 };
 
