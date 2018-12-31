BeeeOn/server

commit b4eac9672896684987576c2a4f35a93faeac7150

GatewayMessageServiceImpl: new class
Implementation of GatewayMessageService interface. The GatewayMessageServiceImpl
provides access to the stored messages and also ensures inserting new messages
based on events that occours inside the system.

Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

src/service/GatewayMessageServiceImpl.cpp
@@ -1,0 +1,124 @@
+#include <Poco/Exception.h>
+
+#include "di/Injectable.h"
+#include "service/GatewayMessageServiceImpl.h"
+
+BEEEON_OBJECT_BEGIN(BeeeOn, GatewayMessageServiceImpl)
+BEEEON_OBJECT_CASTABLE(GatewayMessageService)
+BEEEON_OBJECT_CASTABLE(DeviceListener)
+BEEEON_OBJECT_PROPERTY("gatewayMessageDao", &GatewayMessageServiceImpl::setGatewayMessageDao)
+BEEEON_OBJECT_PROPERTY("accessPolicy", &GatewayMessageServiceImpl::setAccessPolicy)
+BEEEON_OBJECT_PROPERTY("transactionManager", &GatewayMessageServiceImpl::setTransactionManager)
+BEEEON_OBJECT_PROPERTY("gatewayMessagesLimit", &GatewayMessageServiceImpl::setGatewayMessagesLimit)
+BEEEON_OBJECT_END(BeeeOn, GatewayMessageServiceImpl)
+
+using namespace std;
+using namespace Poco;
+using namespace Poco::JSON;
+using namespace BeeeOn;
+
+GatewayMessageServiceImpl::GatewayMessageServiceImpl()
+{
+}
+
+void GatewayMessageServiceImpl::setAccessPolicy(GatewayMessageAccessPolicy::Ptr policy)
+{
+	m_policy = policy;
+}
+
+void GatewayMessageServiceImpl::setGatewayMessageDao(GatewayMessageDao::Ptr dao)
+{
+	m_dao = dao;
+}
+
+void GatewayMessageServiceImpl::setGatewayMessagesLimit(int limit)
+{
+	if (limit < 1)
+		throw InvalidArgumentException("gatewayMessagesLimit must be at least 1");
+
+	m_gatewayMessagesLimit = limit;
+}
+
+bool GatewayMessageServiceImpl::doFetch(
+		Relation<GatewayMessage, Gateway> &input)
+{
+	m_policy->assure(
+		GatewayMessageAccessPolicy::ACTION_USER_GET,
+		input,
+		input.base());
+
+	return m_dao->fetch(input.target(), input.base());
+}
+
+void GatewayMessageServiceImpl::doFetchMany(
+		Relation<vector<GatewayMessage>, Gateway> &input)
+{
+	m_policy->assure(
+		GatewayMessageAccessPolicy::ACTION_USER_GET,
+		input,
+		input.base());
+
+	m_dao->fetchBy(input.target(), input.base());
+}
+
+bool GatewayMessageServiceImpl::doRemove(
+		Relation<GatewayMessage, Gateway> &input)
+{
+	m_policy->assure(
+		GatewayMessageAccessPolicy::ACTION_USER_REMOVE,
+		input,
+		input.base());
+
+	return m_dao->remove(input.target(), input.base());
+}
+
+void GatewayMessageServiceImpl::onRefusedNewDevice(const DeviceEvent &e)
+{
+	GatewayMessage message;
+	message.setGateway({e.gatewayID()});
+	message.setAt({});
+	message.setSeverity(GatewayMessage::SEVERITY_WARN);
+	message.setKey("gateway.new_device_refused");
+
+	Object::Ptr context = new Object;
+	context->set("gateway_id", e.gatewayID().toString());
+	context->set("device_id", e.deviceID().toString());
+
+	message.setContext(context);
+
+	BEEEON_TRANSACTION(m_dao->insert(message, m_gatewayMessagesLimit));
+}
+
+void GatewayMessageServiceImpl::onPairFailed(const DeviceEvent &e)
+{
+	GatewayMessage message;
+	message.setGateway({e.gatewayID()});
+	message.setAt({});
+	message.setSeverity(GatewayMessage::SEVERITY_ERROR);
+	message.setKey("gateway.pair_device_failed");
+
+	Object::Ptr context = new Object;
+	context->set("gateway_id", e.gatewayID().toString());
+	context->set("device_id", e.deviceID().toString());
+
+	message.setContext(context);
+
+	BEEEON_TRANSACTION(m_dao->insert(message, m_gatewayMessagesLimit));
+}
+
+void GatewayMessageServiceImpl::onUnpairFailed(const DeviceEvent &e)
+{
+	GatewayMessage message;
+	message.setGateway({e.gatewayID()});
+	message.setAt({});
+	message.setSeverity(GatewayMessage::SEVERITY_ERROR);
+	message.setKey("gateway.unpair_device_failed");
+
+	Object::Ptr context = new Object;
+	context->set("gateway_id", e.gatewayID().toString());
+	context->set("device_id", e.deviceID().toString());
+
+	message.setContext(context);
+
+	BEEEON_TRANSACTION(m_dao->insert(message, m_gatewayMessagesLimit));
+}

conf/config.d/factory-gws.xml
@@ -52,6 +52,7 @@
 			<set name="eventsExecutor" ref="eventsExecutor" />
 			<add name="dataListeners" ref="publishingWatcher" if-yes="${baf.publish.data.enable}" />
 			<add name="deviceListeners" ref="publishingWatcher" if-yes="${baf.publish.device.enable}" />
+			<add name="deviceListeners" ref="gatewayMessageService" />
 		</instance>
 
 		<instance name="gatewayScanController" class="BeeeOn::GatewayScanController">

conf/config.d/factory-services.xml
@@ -48,6 +48,13 @@
 			<set name="scanController" ref="gatewayScanController" />
 		</instance>
 
+		<instance name="gatewayMessageService" class="BeeeOn::GatewayMessageServiceImpl">
+			<set name="gatewayMessageDao" ref="gatewayMessageDao" />
+			<set name="accessPolicy" ref="accessPolicy" />
+			<set name="transactionManager" ref="transactionManager" />
+			<set name="gatewayMessagesLimit" number="64" />
+		</instance>
+
 		<instance name="locationService"
 				class="BeeeOn::LocationServiceImpl">
 			<set name="locationDao" ref="locationDao" />

conf/config.d/logging.ini
@@ -163,6 +163,9 @@ loggers.BeeeOn::GatewayScanController.level = ${logging.default.level}
 loggers.BeeeOn::GatewayServiceImpl.name = BeeeOn::GatewayServiceImpl
 loggers.BeeeOn::GatewayServiceImpl.level = ${logging.default.level}
 
+loggers.BeeeOn::GatewayMessageServiceImpl.name = BeeeOn::GatewayMessageServiceImpl
+loggers.BeeeOn::GatewayMessageServiceImpl.level = trace
+
 loggers.BeeeOn::GoogleOAuth2Provider.name = BeeeOn::GoogleOAuth2Provider
 loggers.BeeeOn::GoogleOAuth2Provider.level = ${logging.default.level}
 

src/CMakeLists.txt
@@ -173,6 +173,7 @@ file(GLOB SOURCES
 	${PROJECT_SOURCE_DIR}/service/GatewayService.cpp
 	${PROJECT_SOURCE_DIR}/service/GatewayServiceImpl.cpp
 	${PROJECT_SOURCE_DIR}/service/GatewayMessageService.cpp
+	${PROJECT_SOURCE_DIR}/service/GatewayMessageServiceImpl.cpp
 	${PROJECT_SOURCE_DIR}/service/GWSGatewayService.cpp
 	${PROJECT_SOURCE_DIR}/service/GWSSensorHistoryService.cpp
 	${PROJECT_SOURCE_DIR}/service/IdentityListener.cpp

src/service/GatewayMessageServiceImpl.h
@@ -1,0 +1,52 @@
+#pragma once
+
+#include "dao/GatewayMessageDao.h"
+#include "gws/DeviceListener.h"
+#include "policy/GatewayMessageAccessPolicy.h"
+#include "service/GatewayMessageService.h"
+#include "transaction/Transactional.h"
+
+namespace BeeeOn {
+
+class GatewayMessageServiceImpl :
+	public GatewayMessageService,
+	public DeviceListener,
+	public Transactional {
+public:
+	GatewayMessageServiceImpl();
+
+	void setAccessPolicy(GatewayMessageAccessPolicy::Ptr policy);
+	void setGatewayMessageDao(GatewayMessageDao::Ptr dao);
+	void setGatewayMessagesLimit(int limit);
+
+	bool fetch(Relation<GatewayMessage, Gateway> &input) override
+	{
+		return BEEEON_TRANSACTION_RETURN(bool, doFetch(input));
+	}
+
+	void fetchMany(Relation<std::vector<GatewayMessage>, Gateway> &input) override
+	{
+		BEEEON_TRANSACTION(doFetchMany(input));
+	}
+
+	bool remove(Relation<GatewayMessage, Gateway> &input) override
+	{
+		return BEEEON_TRANSACTION_RETURN(bool, doRemove(input));
+	}
+
+	void onRefusedNewDevice(const DeviceEvent &e) override;
+	void onPairFailed(const DeviceEvent &e) override;
+	void onUnpairFailed(const DeviceEvent &e) override;
+
+protected:
+	bool doFetch(Relation<GatewayMessage, Gateway> &input);
+	void doFetchMany(Relation<std::vector<GatewayMessage>, Gateway> &input);
+	bool doRemove(Relation<GatewayMessage, Gateway> &input);
+
+private:
+	GatewayMessageAccessPolicy::Ptr m_policy;
+	GatewayMessageDao::Ptr m_dao;
+	size_t m_gatewayMessagesLimit;
+};
+
+}

commit 0338e276786981f6446dbcc50955cd21c049c80d

GatewayMessageServiceImpl: fix missing initial value of gatewayMessagesLimit
Fixes: b4eac9672896 ("GatewayMessageServiceImpl: new class")
Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

src/service/GatewayMessageServiceImpl.cpp
@@ -17,7 +17,8 @@ using namespace Poco;
 using namespace Poco::JSON;
 using namespace BeeeOn;
 
-GatewayMessageServiceImpl::GatewayMessageServiceImpl()
+GatewayMessageServiceImpl::GatewayMessageServiceImpl():
+	m_gatewayMessagesLimit(64)
 {
 }
 