BeeeOn/server

commit 731662dd39eb8a72b6593cc68d9f5434361c1d18

DeviceServiceImpl: convert values of unknown type for gateway
When requesting devices by prefix via GWSDeviceService::fetchActiveWithPrefix(),
we have to convert certain values into their raw forms if there is the unknown
conversion defined for a module.

Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

src/service/DeviceServiceImpl.cpp
@@ -555,6 +555,30 @@ void DeviceServiceImpl::doFetchActiveWithPrefix(vector<DeviceWithData> &devices,
 
 		try {
 			valuesFor(device);
+
+			SharedPtr<DeviceInfo> info = device.type();
+			if (info.isNull())
+				continue;
+
+			Zip<const set<ModuleInfo>, const vector<ValueAt>> zip(
+				info->modules(), device.values());
+			vector<ValueAt> raw;
+
+			for (const auto &pair : zip) {
+				const auto &info = pair.first;
+				auto value = pair.second;
+
+				if (value.isValid() && !info.toUnknown().empty()) {
+					value.setValue(m_unknownEvaluator.toRaw(
+							info, value.value()));
+					raw.emplace_back(value);
+				}
+				else {
+					raw.emplace_back(value);
+				}
+			}
+
+			device.setValues(raw);
 		}
 		catch (const Exception &e) {
 			logger().log(e, __FILE__, __LINE__);

src/service/DeviceServiceImpl.h
@@ -17,6 +17,7 @@
 #include "service/GWSDeviceService.h"
 #include "transaction/Transactional.h"
 #include "util/EventSource.h"
+#include "util/UnknownEvaluator.h"
 
 namespace BeeeOn {
 
@@ -221,6 +222,7 @@ private:
 	GatewayRPC::Ptr m_gatewayRPC;
 	DeviceAccessPolicy::Ptr m_policy;
 	Poco::SharedPtr<EventSource<DeviceListener>> m_eventSource;
+	UnknownEvaluator m_unknownEvaluator;
 };
 
 }

commit 26e386ec97981c3cead3c9f5da07e7c8e206f5d4

DeviceServiceImpl: fix read-after-free induced by invalid reference
The Zip construct accepts reference to a container, however device.values()
returns a temporary object that is destroyed after the Zip is constructed.

Discovered by valgrind:

 Invalid read of size 8
    at 0x5140E13: BeeeOn::DeviceServiceImpl::doFetchActiveWithPrefix(
	std::vector<BeeeOn::DeviceWithData,
	std::allocator<BeeeOn::DeviceWithData> >&,
	BeeeOn::Gateway const&,
	BeeeOn::Enum<BeeeOn::DevicePrefixEnum,
	BeeeOn::DevicePrefixEnum::Raw,
	BeeeOn::DevicePrefixNamesInitializer> const&) (DeviceServiceImpl.cpp:591)
    by 0x5142936: BeeeOn::DeviceServiceImpl::fetchActiveWithPrefix(
	std::vector<BeeeOn::DeviceWithData,
	std::allocator<BeeeOn::DeviceWithData> >&,
	BeeeOn::Gateway const&,
	BeeeOn::Enum<BeeeOn::DevicePrefixEnum,
	BeeeOn::DevicePrefixEnum::Raw,
	BeeeOn::DevicePrefixNamesInitializer> const&)::{
	lambda()#1}::operator()() const (DeviceServiceImpl.h:157)
    ...

Fixes: 731662dd39eb ("DeviceServiceImpl: convert values of unknown type for gateway")
Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

src/service/DeviceServiceImpl.cpp
@@ -582,8 +582,9 @@ void DeviceServiceImpl::doFetchActiveWithPrefix(vector<DeviceWithData> &devices,
 			if (info.isNull())
 				continue;
 
+			const auto &values = device.values();
 			Zip<const set<ModuleInfo>, const vector<ValueAt>> zip(
-				info->modules(), device.values());
+				info->modules(), values);
 			vector<ValueAt> raw;
 
 			for (const auto &pair : zip) {