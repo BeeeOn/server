BeeeOn/server

commit 40a4c5db684ced7d0e3071b6ade9e5313fad54e1

AsyncGatewayRPC: propagate device properties when pairing
The device-accept request now contains also device properties that
are marked as readable for gateway. This new feature can be disabled
(backwards compatiblity) by not setting the cryptoConfig property
to AsyncGatewayRPC instance.

Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

conf/config.d/factory-dao.xml
@@ -48,6 +48,9 @@
 			<set name="gatewayCommunicator" ref="gatewayCommunicator" />
 			<set name="responseExpectedQueue" ref="responseExpectedQueue" />
 			<set name="defaultTimeout" time="${gateway-rpc.async.defaultTimeout}" />
+			<set name="devicePropertyDao" ref="devicePropertyDao" />
+			<set name="transactionManager" ref="transactionManager" />
+			<set name="cryptoConfig" ref="devicePropertiesCryptoConfig" />
 		</instance>
 
 		<alias name="gatewayRPC" ref="${gateway-rpc.type}GatewayRPC" />

src/rpc/AsyncGatewayRPC.cpp
@@ -14,6 +14,9 @@ BEEEON_OBJECT_CASTABLE(StoppableLoop)
 BEEEON_OBJECT_PROPERTY("gatewayCommunicator", &AsyncGatewayRPC::setGatewayCommunicator)
 BEEEON_OBJECT_PROPERTY("responseExpectedQueue", &AsyncGatewayRPC::setGWResponseExpectedQueue)
 BEEEON_OBJECT_PROPERTY("defaultTimeout", &AsyncGatewayRPC::setDefaultTimeout)
+BEEEON_OBJECT_PROPERTY("transactionManager", &AsyncGatewayRPC::setTransactionManager)
+BEEEON_OBJECT_PROPERTY("devicePropertyDao", &AsyncGatewayRPC::setDevicePropertyDao)
+BEEEON_OBJECT_PROPERTY("cryptoConfig", &AsyncGatewayRPC::setCryptoConfig)
 BEEEON_OBJECT_END(BeeeOn, AsyncGatewayRPC)
 
 using namespace std;
@@ -43,6 +46,37 @@ void AsyncGatewayRPC::pairDevice(GatewayRPCHandler::Ptr handler,
 	request->setID(callID);
 	request->setDeviceID(device.id());
 	request->setRefresh(device.refresh());
+
+	map<string, string> properties;
+
+	if (!m_cryptoConfig.isNull()) {
+		list<DeviceProperty> tmp;
+		BEEEON_TRANSACTION(m_propertyDao->fetchByDevice(tmp, device));
+
+		for (const auto &property : tmp) {
+			if (!property.key().isGatewayReadable())
+				continue;
+
+			if (logger().debug()) {
+				logger().debug(
+					property.key().toString() + ": '"
+						+ property.value() + "' '"
+						+ property.params().toString() + "'",
+					__FILE__, __LINE__);
+			}
+
+			try {
+				DecryptedDeviceProperty decrypted(property, m_cryptoConfig);
+
+				properties.emplace(
+					decrypted.key().toString(),
+					decrypted.asString());
+			}
+			BEEEON_CATCH_CHAIN(logger())
+		}
+	}
+
+	request->setProperties(properties);
 
 	sendAndExpectResult(gateway.id(), callID, handler, request, m_defaultTimeout);
 }
@@ -374,4 +408,14 @@ void AsyncGatewayRPC::setGatewayCommunicator(GatewayCommunicator::Ptr communicat
 void AsyncGatewayRPC::setGWResponseExpectedQueue(GWResponseExpectedQueue::Ptr queue)
 {
 	m_responseExpectedQueue = queue;
+}
+
+void AsyncGatewayRPC::setDevicePropertyDao(DevicePropertyDao::Ptr dao)
+{
+	m_propertyDao = dao;
+}
+
+void AsyncGatewayRPC::setCryptoConfig(SharedPtr<CryptoConfig> config)
+{
+	m_cryptoConfig = config;
 }

src/rpc/AsyncGatewayRPC.h
@@ -3,6 +3,7 @@
 #include <Poco/SharedPtr.h>
 #include <Poco/Util/Timer.h>
 
+#include "dao/DevicePropertyDao.h"
 #include "gwmessage/GWRequest.h"
 #include "gws/GatewayCommunicator.h"
 #include "gws/GWResponseExpectedQueue.h"
@@ -11,6 +12,8 @@
 #include "model/GlobalID.h"
 #include "rpc/GatewayRPC.h"
 #include "rpc/GatewayRPCResult.h"
+#include "transaction/Transactional.h"
+#include "util/CryptoConfig.h"
 #include "util/Loggable.h"
 #include "util/LambdaTimerTask.h"
 
@@ -33,7 +36,7 @@ class AsyncGatewayRPC :
 	public GatewayRPC,
 	public RPCForwarder,
 	public StoppableLoop,
-	protected Loggable {
+	public Transactional {
 public:
 	typedef Poco::SharedPtr<AsyncGatewayRPC> Ptr;
 
@@ -76,6 +79,8 @@ public:
 	void setDefaultTimeout(const Poco::Timespan &timeout);
 	void setGatewayCommunicator(GatewayCommunicator::Ptr communicator);
 	void setGWResponseExpectedQueue(GWResponseExpectedQueue::Ptr queue);
+	void setDevicePropertyDao(DevicePropertyDao::Ptr dao);
+	void setCryptoConfig(Poco::SharedPtr<CryptoConfig> config);
 
 private:
 	/**
@@ -138,6 +143,8 @@ private:
 	Poco::Timespan m_defaultTimeout;
 	GatewayCommunicator::Ptr m_gatewayCommunicator;
 	GWResponseExpectedQueue::Ptr m_responseExpectedQueue;
+	DevicePropertyDao::Ptr m_propertyDao;
+	Poco::SharedPtr<CryptoConfig> m_cryptoConfig;
 
 	ContextMap m_contexts;
 	Poco::FastMutex m_mutex;

commit 42b4b2564d0662f8eac1a8dbc1051e66ba58f70f

AsyncGatewayRPC: remove BEEEON_TRANSACTION macro from pairDevice()
Fixes: 40a4c5db ("AsyncGatewayRPC: propagate device properties when pairing")

Signed-off-by: David Bednařík <xbedna62@stud.fit.vutbr.cz>
Reviewed-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

conf/config.d/factory-dao.xml
@@ -49,7 +49,6 @@
 			<set name="responseExpectedQueue" ref="responseExpectedQueue" />
 			<set name="defaultTimeout" time="${gateway-rpc.async.defaultTimeout}" />
 			<set name="devicePropertyDao" ref="devicePropertyDao" />
-			<set name="transactionManager" ref="transactionManager" />
 			<set name="cryptoConfig" ref="devicePropertiesCryptoConfig" />
 		</instance>
 

src/rpc/AsyncGatewayRPC.cpp
@@ -14,7 +14,6 @@ BEEEON_OBJECT_CASTABLE(StoppableLoop)
 BEEEON_OBJECT_PROPERTY("gatewayCommunicator", &AsyncGatewayRPC::setGatewayCommunicator)
 BEEEON_OBJECT_PROPERTY("responseExpectedQueue", &AsyncGatewayRPC::setGWResponseExpectedQueue)
 BEEEON_OBJECT_PROPERTY("defaultTimeout", &AsyncGatewayRPC::setDefaultTimeout)
-BEEEON_OBJECT_PROPERTY("transactionManager", &AsyncGatewayRPC::setTransactionManager)
 BEEEON_OBJECT_PROPERTY("devicePropertyDao", &AsyncGatewayRPC::setDevicePropertyDao)
 BEEEON_OBJECT_PROPERTY("cryptoConfig", &AsyncGatewayRPC::setCryptoConfig)
 BEEEON_OBJECT_END(BeeeOn, AsyncGatewayRPC)
@@ -51,7 +50,7 @@ void AsyncGatewayRPC::pairDevice(GatewayRPCHandler::Ptr handler,
 
 	if (!m_cryptoConfig.isNull()) {
 		list<DeviceProperty> tmp;
-		BEEEON_TRANSACTION(m_propertyDao->fetchByDevice(tmp, device));
+		m_propertyDao->fetchByDevice(tmp, device);
 
 		for (const auto &property : tmp) {
 			if (!property.key().isGatewayReadable())

src/rpc/AsyncGatewayRPC.h
@@ -12,7 +12,6 @@
 #include "model/GlobalID.h"
 #include "rpc/GatewayRPC.h"
 #include "rpc/GatewayRPCResult.h"
-#include "transaction/Transactional.h"
 #include "util/CryptoConfig.h"
 #include "util/Loggable.h"
 #include "util/LambdaTimerTask.h"
@@ -36,7 +35,7 @@ class AsyncGatewayRPC :
 	public GatewayRPC,
 	public RPCForwarder,
 	public StoppableLoop,
-	public Transactional {
+	protected Loggable {
 public:
 	typedef Poco::SharedPtr<AsyncGatewayRPC> Ptr;
 